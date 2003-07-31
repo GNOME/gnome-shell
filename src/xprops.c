fb5f3f6bb0640e7d14f19bc9bab7044a57a78c5e
Arvind Samptur <arvind.samptur@wipro.com>
2003-07-31 09:35:56 +0000

    Number of strings we are processing is one more than required.
    2003-07-29  Arvind Samptur  <arvind.samptur@wipro.com>
    
            * src/xprops.c (utf8_list_from_results): Number of
            strings we are processing is one more than required.
    
            Also get the string count right even without a null byte at the end.
            Pointed out by Havoc.

--- src/xprops.c
+++ src/xprops.c
@@ -451,7 +451,7 @@ utf8_list_from_results (GetPropertyResults *results,
    * property is nul-separated
    */
   i = 0;
-  n_strings = 1;
+  n_strings = 0;
   while (i < (int) results->n_items)
     {
       if (results->prop[i] == '\0')
@@ -459,6 +459,9 @@ utf8_list_from_results (GetPropertyResults *results,
       ++i;
     }
 
+  if (results->prop[results->n_items - 1] != '\0')
+    ++n_strings;
+ 
   /* we're guaranteed that results->prop has a nul on the end
    * by XGetWindowProperty
    */
