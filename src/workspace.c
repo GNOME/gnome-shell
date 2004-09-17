7ee43e5a36b1bfc5ba1771031c7b6d4e9eb1cd74
Elijah Newren <newren@math.utah.edu>
2004-09-17 19:49:32 +0000

    Don't focus a window that is minimized (fixes #147947)
    2004-09-17  Elijah Newren  <newren@math.utah.edu>
    
    	* src/workspace.c (meta_workspace_focus_mru_window): Don't focus a
    	window that is minimized (fixes #147947)

--- src/workspace.c
+++ src/workspace.c
@@ -841,6 +841,7 @@ meta_workspace_focus_mru_window (MetaWorkspace *workspace,
       MetaWindow* tmp_window;
       tmp_window = ((MetaWindow*) tmp->data);
       if (tmp_window != not_this_one           &&
+          !tmp_window->minimized               &&
           tmp_window->type != META_WINDOW_DOCK &&
           tmp_window->type != META_WINDOW_DESKTOP)
         {
