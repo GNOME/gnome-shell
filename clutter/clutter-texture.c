9e9240b2a5a11baabf46886d88d18a9a28e44bd5
Iain Holmes <iain@openedhand.com>
2006-06-08 22:19:32 +0000

    2006-06-08  Iain Holmes  <iain@openedhand.com>
            * configure.ac:
            Add gdk-pixbuf-xlib cflags/libs to clutter libs
            Build the gtk-clutter.pc file.
    
            * clutter.pc.in:
            Add the gdk-pixbuf-xlib depends.
    
            * gtk/gtk-clutter.pc.in:
            pkg-config stuff for gtk-clutter
    
            * gtk/Makefile.am:
            Install gtk-clutter.pc

--- clutter/clutter-texture.c
+++ clutter/clutter-texture.c
@@ -887,6 +887,7 @@ clutter_texture_init (ClutterTexture *self)
   priv->repeat_x     = FALSE;
   priv->repeat_y     = FALSE;
   priv->pixbuf = NULL;
+  priv->pixel_type = PIXEL_TYPE;
 
 
   self->priv  = priv;
