f68b04cad4a61254d91012d603c96b94e4b75279
Matthew Allum <mallum@openedhand.com>
2007-02-15 23:23:32 +0000

    2007-02-15  Matthew Allum  <mallum@openedhand.com>
            * clutter/clutter-group.h:
            Add missing clutter_group_remove_all declaration.

--- clutter/clutter-group.h
+++ clutter/clutter-group.h
@@ -100,6 +100,7 @@ void          clutter_group_add_many         (ClutterGroup    *self,
 					      ...) G_GNUC_NULL_TERMINATED;
 void          clutter_group_remove           (ClutterGroup    *self,
 					      ClutterActor    *actor); 
+void          clutter_group_remove_all       (ClutterGroup *self);
 #ifndef CLUTTER_DISABLE_DEPRECATED
 void          clutter_group_show_all         (ClutterGroup    *self);
 void          clutter_group_hide_all         (ClutterGroup    *self);
