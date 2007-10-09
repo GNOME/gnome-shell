b1ed23e0dfce4cfb30ae1ea1ee3b59b94a10e19b
Emmanuele Bassi <ebassi@openedhand.com>
2007-10-09 16:44:44 +0000

    2007-10-09  Emmanuele Bassi  <ebassi@openedhand.com>
    	* tests/Makefile.am:
    	* tests/test-script.c:
    	* tests/test-script.json: Move part of the UI definition into
    	its own file and exercise clutter_script_load_from_file().

--- tests/test-script.c
+++ tests/test-script.c
@@ -37,60 +37,6 @@ static const gchar *test_behaviour =
 "  }"
 "]";
 
-static const gchar *test_ui =
-"{"
-"  \"Scene\" : {"
-"    \"id\"       : \"main-stage\","
-"    \"type\"     : \"ClutterStage\","
-"    \"color\"    : \"white\","
-"    \"width\"    : 500,"
-"    \"height\"   : 200,"
-"    \"children\" : ["
-"      {"
-"        \"id\"       : \"red-button\","
-"        \"type\"     : \"ClutterRectangle\","
-"        \"color\"    : \"#ff0000ff\","
-"        \"x\"        : 50,"
-"        \"y\"        : 50,"
-"        \"width\"    : 100,"
-"        \"height\"   : 100,"
-"        \"visible\"  : true,"
-"      },"
-"      {"
-"        \"id\"       : \"green-button\","
-"        \"type\"     : \"ClutterRectangle\","
-"        \"color\"    : \"#00ff00ff\","
-"        \"x\"        : 200,"
-"        \"y\"        : 50,"
-"        \"width\"    : 100,"
-"        \"height\"   : 100,"
-"        \"visible\"  : true,"
-"        \"behaviours\" : [ \"fade-behaviour\" ]"
-"      },"
-"      {"
-"        \"id\"       : \"blue-button\","
-"        \"type\"     : \"ClutterRectangle\","
-"        \"color\"    : \"#0000ffff\","
-"        \"x\"        : 350,"
-"        \"y\"        : 50,"
-"        \"width\"    : 100,"
-"        \"height\"   : 100,"
-"        \"visible\"  : true,"
-"      },"
-"      {"
-"        \"id\"         : \"red-hand\","
-"        \"type\"       : \"ClutterTexture\","
-"        \"pixbuf\"     : \"redhand.png\","
-"        \"x\"          : 50,"
-"        \"y\"          : 50,"
-"        \"opacity\"    : 100,"
-"        \"visible\"    : true,"
-"        \"behaviours\" : [ \"rotate-behaviour\", \"fade-behaviour\" ]"
-"      }"
-"    ]"
-"  }"
-"}";
-
 int
 main (int argc, char *argv[])
 {
@@ -113,7 +59,7 @@ main (int argc, char *argv[])
       return EXIT_FAILURE;
     }
   
-  clutter_script_load_from_data (script, test_ui, -1, &error);
+  clutter_script_load_from_file (script, "test-script.json", &error);
   if (error)
     {
       g_print ("*** Error:\n"
