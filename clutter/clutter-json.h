389e1470a52143e18234fcade6fd9041cc5dcfc7
Emmanuele Bassi <ebassi@openedhand.com>
2008-04-23 13:58:58 +0000

    2008-04-23  Emmanuele Bassi  <ebassi@openedhand.com>
    	* configure.ac:
    	* clutter/Makefile.am: Do not build the internal JSON-GLib
    	copy if there is a system installed one.
    
    	* clutter/clutter-json.h: Remove.
    
    	* clutter/clutter-json.h.in: Template for the clutter-json.h
    	header file, used to opaquely include the JSON-GLib types.

--- clutter/clutter-json.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CLUTTER_JSON_H__
-#define __CLUTTER_JSON_H__
-
-#include "json/json-types.h"
-#include "json/json-parser.h"
-#include "json/json-generator.h"
-
-#endif /* __CLUTTER_JSON_H__ */
