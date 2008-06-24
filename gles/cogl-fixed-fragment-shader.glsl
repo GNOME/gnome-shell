cbd77008ff02787320d47003767390f2fe5d7f97
Neil Roberts <neil@openedhand.com>
2008-06-24 16:21:40 +0000

    * clutter/cogl/gles/cogl-gles2-wrapper.h: 	* clutter/cogl/gles/cogl-gles2-wrapper.c: All of the settings and 	uniforms are now proxied into COGL variables instead of setting 	the GL uniforms directly. Just before glDrawArrays is executed a 	shader is generated using the given settings to avoid using 'if' 	statements. The shaders are cached.
    	* clutter/cogl/gles/cogl-fixed-vertex-shader.glsl: 
    	* clutter/cogl/gles/cogl-fixed-fragment-shader.glsl: The shaders
    	are now split into parts using comments instead of 'if' statements
    	so that the simplest shader can be generated on the fly.
    
    	* clutter/cogl/gles/stringify.sh: Now splits up the shader sources
    	into separate C strings where deliminated by special comments.
    
    	* clutter/cogl/gles/cogl-program.h: 
    	* clutter/cogl/gles/cogl-program.c: A custom shader can no longer
    	be directly linked with the fixed-functionality replacement
    	because the replacement changes depending on the settings. Instead
    	the bound shader is linked with the appropriate replacement shader
    	just before glDrawArrays is executed. The custom uniform variables
    	must also be proxied through COGL variables because their location
    	can change when relinked.

--- gles/cogl-fixed-fragment-shader.glsl
+++ gles/cogl-fixed-fragment-shader.glsl
@@ -1,3 +1,5 @@
+/*** cogl_fixed_fragment_shader_start ***/
+
 /* There is no default precision for floats in fragment shaders in
    GLES 2 so we need to define one */
 precision mediump float;
@@ -8,86 +10,64 @@ varying vec2       tex_coord;
 varying float      fog_amount;
 
 /* Texturing options */
-uniform bool       texture_2d_enabled;
 uniform sampler2D  texture_unit;
-uniform bool       alpha_only;
 
 /* Fogging options */
-uniform bool       fog_enabled;
 uniform vec4       fog_color;
 
 /* Alpha test options */
-uniform bool       alpha_test_enabled;
-uniform int        alpha_test_func;
 uniform float      alpha_test_ref;
-
-/* Alpha test functions */
-const int GL_NEVER    = 0x0200;
-const int GL_LESS     = 0x0201;
-const int GL_EQUAL    = 0x0202;
-const int GL_LEQUAL   = 0x0203;
-const int GL_GREATER  = 0x0204;
-const int GL_NOTEQUAL = 0x0205;
-const int GL_GEQUAL   = 0x0206;
 
 void
 main (void)
 {
-  if (texture_2d_enabled)
-    {
-      if (alpha_only)
-	{
-	  /* If the texture only has an alpha channel (eg, with the
-	     textures from the pango renderer) then the RGB components
-	     will be black. We want to use the RGB from the current
-	     color in that case */
-	  gl_FragColor = frag_color;
-	  gl_FragColor.a *= texture2D (texture_unit, tex_coord).a;
-	}
-      else
-	gl_FragColor = frag_color * texture2D (texture_unit, tex_coord);
-    }
-  else
-    gl_FragColor = frag_color;
+  /*** cogl_fixed_fragment_shader_texture_alpha_only ***/
 
-  if (fog_enabled)
-    /* Mix the calculated color with the fog color */
-    gl_FragColor.rgb = mix (fog_color.rgb, gl_FragColor.rgb, fog_amount);
+  /* If the texture only has an alpha channel (eg, with the textures
+     from the pango renderer) then the RGB components will be
+     black. We want to use the RGB from the current color in that
+     case */
+  gl_FragColor = frag_color;
+  gl_FragColor.a *= texture2D (texture_unit, tex_coord).a;
+
+  /*** cogl_fixed_fragment_shader_texture ***/
+
+  /* This pointless extra variable is needed to work around an
+     apparent bug in the PowerVR drivers. Without it the alpha
+     blending seems to stop working */
+  vec4 frag_color_copy = frag_color;
+  gl_FragColor = frag_color_copy * texture2D (texture_unit, tex_coord);
+
+  /*** cogl_fixed_fragment_shader_solid_color ***/
+  gl_FragColor = frag_color;
+
+  /*** cogl_fixed_fragment_shader_fog ***/
+
+  /* Mix the calculated color with the fog color */
+  gl_FragColor.rgb = mix (fog_color.rgb, gl_FragColor.rgb, fog_amount);
 
   /* Alpha testing */
-  if (alpha_test_enabled)
-    {
-      if (alpha_test_func == GL_NEVER)
-	discard;
-      else if (alpha_test_func == GL_LESS)
-	{
-	  if (gl_FragColor.a >= alpha_test_ref)
-	    discard;
-	}
-      else if (alpha_test_func == GL_EQUAL)
-	{
-	  if (gl_FragColor.a != alpha_test_ref)
-	    discard;
-	}
-      else if (alpha_test_func == GL_LEQUAL)
-	{
-	  if (gl_FragColor.a > alpha_test_ref)
-	    discard;
-	}
-      else if (alpha_test_func == GL_GREATER)
-	{
-	  if (gl_FragColor.a <= alpha_test_ref)
-	    discard;
-	}
-      else if (alpha_test_func == GL_NOTEQUAL)
-	{
-	  if (gl_FragColor.a == alpha_test_ref)
-	    discard;
-	}
-      else if (alpha_test_func == GL_GEQUAL)
-	{
-	  if (gl_FragColor.a < alpha_test_ref)
-	    discard;
-	}
-    }
+
+  /*** cogl_fixed_fragment_shader_alpha_never ***/
+  discard;
+  /*** cogl_fixed_fragment_shader_alpha_less ***/
+  if (gl_FragColor.a >= alpha_test_ref)
+    discard;
+  /*** cogl_fixed_fragment_shader_alpha_equal ***/
+  if (gl_FragColor.a != alpha_test_ref)
+    discard;
+  /*** cogl_fixed_fragment_shader_alpha_lequal ***/
+  if (gl_FragColor.a > alpha_test_ref)
+    discard;
+  /*** cogl_fixed_fragment_shader_alpha_greater ***/
+  if (gl_FragColor.a <= alpha_test_ref)
+    discard;
+  /*** cogl_fixed_fragment_shader_alpha_notequal ***/
+  if (gl_FragColor.a == alpha_test_ref)
+    discard;
+  /*** cogl_fixed_fragment_shader_alpha_gequal ***/
+  if (gl_FragColor.a < alpha_test_ref)
+    discard;
+
+  /*** cogl_fixed_fragment_shader_end ***/
 }
