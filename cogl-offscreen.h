d3d257b9136c56aeff06230ea7ab4c7bc155387a
Emmanuele Bassi <ebassi@openedhand.com>
2008-10-30 17:57:41 +0000

    2008-10-30  Emmanuele Bassi  <ebassi@linux.intel.com>
    	* clutter/cogl/cogl-color.h:
    	* clutter/cogl/cogl-fixed.h:
    	* clutter/cogl/cogl-offscreen.h:
    	* clutter/cogl/cogl-path.h:
    	* clutter/cogl/cogl-shader.h:
    	* clutter/cogl/cogl-texture.h:
    	* clutter/cogl/cogl-types.h: Add copyright and licensing
    	notice to the newly added files.

--- cogl-offscreen.h
+++ cogl-offscreen.h
@@ -1,3 +1,22 @@
+/* cogl-offscreen.h: Offscreen objects
+ * This file is part of Clutter
+ *
+ * Copyright (C) 2008  Intel Corporation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #if !defined(__COGL_H_INSIDE__) && !defined(CLUTTER_COMPILATION)
 #error "Only <cogl/cogl.h> can be included directly."
 #endif
