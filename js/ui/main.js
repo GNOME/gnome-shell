0cea92e46affb7c9b5ca933cd6bd42b66b6a6f0c
Owen Taylor <otaylor@src.gnome.org>
2008-10-31 18:29:42 +0000

    Clean up initial appearance a bit
    Set a less strange background color than white and remove the
    "Yessir. The compositor is running" message from the overlay.
    
    svn path=/trunk/; revision=13

--- js/ui/main.js
+++ js/ui/main.js
@@ -5,8 +5,24 @@ const Clutter = imports.gi.Clutter;
 
 const Panel = imports.ui.panel;
 
+const DEFAULT_BACKGROUND_COLOR = new Clutter.Color();
+DEFAULT_BACKGROUND_COLOR.from_pixel(0x2266bbff);
+
 let panel = null;
 
 function start() {
+    let global = Shell.global_get();
+
+    // The background color really only matters if there is no desktop
+    // window (say, nautilus) running. We set it mostly so things look good
+    // when we are running inside Xephyr.
+    global.stage.color = DEFAULT_BACKGROUND_COLOR;
+
+    // Mutter currently hardcodes putting "Yessir. The compositor is running""
+    // in the overlay. Clear that out.
+    children = global.overlay_group.get_children();
+    for (let i = 0; i < children.length; i++)
+	children[i].destroy();
+
     panel = new Panel.Panel();
 }
