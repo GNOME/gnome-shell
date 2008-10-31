d6dae414a4b8e1aa2c334025d95f0df99716de18
Owen Taylor <otaylor@src.gnome.org>
2008-10-31 05:25:28 +0000

    Add jhbuild setup script and moduleset
    gnome-shell.modules: moduleset to build gnome-shell and dependencies
    
    gnome-shell-build-setup.sh: Script to set up a jhbuild for gnome-shell,
      modelled after and partially copied from gtk-osx-build-setup.sh
    jhbuildrc-gnome-shell: ~/.jhbuildrc for the build
    jhbuildrc-custom-example: Example ~/.jhbuildrc-custom
    
    svn path=/trunk/; revision=3

---
+++ tools/build/gnome-shell-build-setup.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+# Script that sets up jhbuild to build gnome-shell. Run this to
+# checkout jhbuild and the required configuration. 
+#
+# Copyright (C) 2008, Red Hat, Inc.
+#
+# Some ideas and code taken from gtk-osx-build
+#
+# Copyright (C) 2006, 2007, 2008 Imendio AB
+#
+SOURCE=$HOME/Source
+BASEURL=http://svn.gnome.org/svn/gnome-shell/trunk/tools/build
+
+if [ -d $SOURCE ] ; then : ; else
+    mkdir $SOURCE
+    echo "Created $SOURCE"
+fi
+
+echo -n "Checking out jhbuild into $SOURCE/jhbuild ... "
+cd $SOURCE
+svn co http://svn.gnome.org/svn/jhbuild/trunk jhbuild > /dev/null
+echo "done"
+
+echo "Installing jhbuild..."
+(cd $SOURCE/jhbuild && make -f Makefile.plain DISABLE_GETTEXT=1 install >/dev/null)
+
+if [ -e $HOME/.jhbuildrc ] ; then
+    if grep JHBUILDRC_GNOME_SHELL $HOME/.jhbuildrc > /dev/null ; then : ; else
+	mv $HOME/.jhbuildrc $HOME/.jhbuildrc.bak
+	echo "Saved ~/.jhbuildrc as ~/.jhbuildrc.bak"
+    fi
+fi
+
+echo -n "Writing ~/.jhbuildrc ... "
+curl -s -o $HOME/.jhbuildrc $BASEURL/jhbuildrc-gnome-shell
+echo "done"
+
+if [ ! -f $HOME/.jhbuildrc-custom ]; then
+    echo -n "Writing example ~/.jhbuildrc-custom ... "
+    curl -s -o $HOME/.jhbuildrc-custom $BASEURL/jhbuildrc-custom-example
+    echo "done"
+fi
+
+if test "x`echo $PATH | grep $HOME/bin`" == x; then
+    echo "PATH does not contain $HOME/bin, it is recommended that you add that."
+    echo
+fi
+
+echo "Done."
+
