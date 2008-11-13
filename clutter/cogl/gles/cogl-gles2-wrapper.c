0ae150e462059ba818c65516ec350856bfb7e026
Neil Roberts <neil@openedhand.com>
2008-11-13 14:44:27 +0000

    * clutter/cogl/gles/cogl-gles2-wrapper.h: 	* clutter/cogl/gles/cogl-gles2-wrapper.c: 	Initialise the 'tex' sampler uniform to 0. The GLSL spec 	specifically says that you must initialize sampler uniforms. This 	fixes texturing for GLES 2 when using the PowerVR simulator via 	software Mesa.

--- clutter/cogl/gles/cogl-gles2-wrapper.c
+++ clutter/cogl/gles/cogl-gles2-wrapper.c
@@ -453,6 +453,9 @@ cogl_gles2_wrapper_get_uniforms (GLuint program,
 
   uniforms->alpha_test_ref_uniform
     = glGetUniformLocation (program, "alpha_test_ref");
+
+  uniforms->texture_unit_uniform
+    = glGetUniformLocation (program, "texture_unit");
 }
 
 void
@@ -884,6 +887,10 @@ cogl_wrap_glDrawArrays (GLenum mode, GLint first, GLsizei count)
 	  && program->uniforms.alpha_test_ref_uniform != -1)
 	glUniform1f (program->uniforms.alpha_test_ref_uniform,
 		     w->alpha_test_ref);
+
+      if ((w->dirty_uniforms & COGL_GLES2_DIRTY_TEXTURE_UNIT)
+          && program->uniforms.texture_unit_uniform != -1)
+        glUniform1i (program->uniforms.texture_unit_uniform, 0);
 
       w->dirty_uniforms = 0;
     }
