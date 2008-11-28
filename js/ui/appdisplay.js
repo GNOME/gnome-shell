3b9aa49f2288f89348d03951d9074d7df1af1171
Colin Walters <walters@src.gnome.org>
2008-11-28 20:12:20 +0000

    Convert all JS style to be uniform, add Eclipse settings bits
    Previous style was all over the place; this commit attempts to bring
    uniformity.  Overall, the style is:
    
    * 4 spaces only, no tabs
    * Prototypes do not create a new block
    * Constructor property continuations only indent one block
    
    svn path=/trunk/; revision=87

--- js/ui/appdisplay.js
+++ js/ui/appdisplay.js
@@ -1,4 +1,4 @@
-/* -*- mode: js2; js2-basic-offset: 4; -*- */
+/* -*- mode: js2; js2-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil -*- */
 
 const Signals = imports.signals;
 const Clutter = imports.gi.Clutter;
@@ -10,31 +10,31 @@ const Tidy = imports.gi.Tidy;
 const Meta = imports.gi.Meta;
 const Shell = imports.gi.Shell;
 
-// TODO - move this into GConf once we're not a plugin anymore
-// but have taken over metacity
-// This list is taken from GNOME Online popular applications
-// http://online.gnome.org/applications
-// but with nautilus removed
+//TODO - move this into GConf once we're not a plugin anymore
+//but have taken over metacity
+//This list is taken from GNOME Online popular applications
+//http://online.gnome.org/applications
+//but with nautilus removed
 const DEFAULT_APPLICATIONS = [
-    'mozilla-firefox.desktop',
-    'gnome-terminal.desktop',
-    'evolution.desktop',
-    'evince.desktop',
-    'gedit.desktop',
-    'mozilla-thunderbird.desktop',
-    'totem.desktop',
-    'gnome-file-roller.desktop',
-    'rhythmbox.desktop',
-    'epiphany.desktop',
-    'xchat.desktop',
-    'openoffice.org-1.9-writer.desktop',
-    'emacs.desktop',
-    'gnome-system-monitor.desktop',
-    'openoffice.org-1.9-calc.desktop',
-    'eclipse.desktop',
-    'openoffice.org-1.9-impress.desktop',
-    'vncviewer.desktop'
-];
+                              'mozilla-firefox.desktop',
+                              'gnome-terminal.desktop',
+                              'evolution.desktop',
+                              'evince.desktop',
+                              'gedit.desktop',
+                              'mozilla-thunderbird.desktop',
+                              'totem.desktop',
+                              'gnome-file-roller.desktop',
+                              'rhythmbox.desktop',
+                              'epiphany.desktop',
+                              'xchat.desktop',
+                              'openoffice.org-1.9-writer.desktop',
+                              'emacs.desktop',
+                              'gnome-system-monitor.desktop',
+                              'openoffice.org-1.9-calc.desktop',
+                              'eclipse.desktop',
+                              'openoffice.org-1.9-impress.desktop',
+                              'vncviewer.desktop'
+                              ];
 
 const APPDISPLAY_NAME_COLOR = new Clutter.Color();
 APPDISPLAY_NAME_COLOR.from_pixel(0xffffffff);
@@ -47,224 +47,223 @@ const APPDISPLAY_HEIGHT = 50;
 const APPDISPLAY_PADDING = 4;
 
 function AppDisplayItem(node, width) {
-    this._init(node, width);
+	this._init(node, width);
 }
 
 AppDisplayItem.prototype = {
-    _init: function(appinfo, width) {
+_init: function(appinfo, width) {
 	let me = this;
-        this._appinfo = appinfo;
+	this._appinfo = appinfo;
 
 	let name = appinfo.get_name();
 
 	let icontheme = Gtk.IconTheme.get_default();
 
 	this._group = new Clutter.Group({reactive: true,
-					 width: width,
-					 height: APPDISPLAY_HEIGHT});
+		width: width,
+		height: APPDISPLAY_HEIGHT});
 	this._group.connect('button-press-event', function(group, e) {
-	    me.emit('launch');
-	    return true;
+		me.emit('launch');
+		return true;
 	});
 	this._bg = new Clutter.Rectangle({ color: APPDISPLAY_BACKGROUND_COLOR,
-	 				   x: 0, y: 0 });
+		x: 0, y: 0 });
 	this._group.add_actor(this._bg);
 
-        this._icon = new Clutter.Texture({ width: 48, height: 48, x: 0, y: 0 });
+	this._icon = new Clutter.Texture({ width: 48, height: 48, x: 0, y: 0 });
 	let gicon = appinfo.get_icon();
 	let path = null;
-        if (gicon != null) {
-            let iconinfo = icontheme.lookup_by_gicon(gicon, 48, Gtk.IconLookupFlags.NO_SVG);
-	    if (iconinfo)
-	    	path = iconinfo.get_filename();
-        }
+	if (gicon != null) {
+		let iconinfo = icontheme.lookup_by_gicon(gicon, 48, Gtk.IconLookupFlags.NO_SVG);
+		if (iconinfo)
+			path = iconinfo.get_filename();
+	}
 
 	if (path)
-	    this._icon.set_from_file(path);
+		this._icon.set_from_file(path);
 	this._group.add_actor(this._icon);
 
 	let comment = appinfo.get_description();
 	let text_width = width - me._icon.width + 4;
 	this._name = new Clutter.Label({ color: APPDISPLAY_NAME_COLOR,
-                                     font_name: "Sans 14px",
-                                     width: text_width,
-                                     ellipsize: Pango.EllipsizeMode.END,
-		     		     text: name,
-				     x: this._icon.width + 4,
-				     y: 0});
+		font_name: "Sans 14px",
+		width: text_width,
+		ellipsize: Pango.EllipsizeMode.END,
+		text: name,
+		x: this._icon.width + 4,
+		y: 0});
 	this._group.add_actor(this._name);
 	this._comment = new Clutter.Label({ color: APPDISPLAY_COMMENT_COLOR,
-                                             font_name: "Sans 12px",
-                                             width: text_width,
-                                             ellipsize: Pango.EllipsizeMode.END,
-		     		             text: comment,
-					     x: this._name.x,
-					     y: this._name.height + 4})
+		font_name: "Sans 12px",
+		width: text_width,
+		ellipsize: Pango.EllipsizeMode.END,
+		text: comment,
+		x: this._name.x,
+		y: this._name.height + 4})
 	this._group.add_actor(this._comment);
-
-        this.actor = this._group;
-    }
+	this.actor = this._group;
+}
 }
 Signals.addSignalMethods(AppDisplayItem.prototype);
 
 function AppDisplay(x, y, width, height) {
-    this._init(x, y, width, height);
+	this._init(x, y, width, height);
 }
 
 AppDisplay.prototype = {
-    _init : function(x, y, width, height) {
+_init : function(x, y, width, height) {
 	let me = this;
 	let global = Shell.Global.get();
-        this._search = '';
-        this._x = x;
+	this._search = '';
+	this._x = x;
 	this._y = y;
 	this._width = width;
 	this._height = height;
 	this._appmonitor = new Shell.AppMonitor();
 	this._appsStale = true;
 	this._appmonitor.connect('changed', function(mon) {
-            me._appsStale = true;
+		me._appsStale = true;
 	});
-        this._grid = new Tidy.Grid({x: x, y: y, width: width, height: height});
+	this._grid = new Tidy.Grid({x: x, y: y, width: width, height: height});
 	global.stage.add_actor(this._grid);
-        this._appset = {}; // Map<appid, appinfo>
+	this._appset = {}; // Map<appid, appinfo>
 	this._displayed = {} // Map<appid, AppDisplay>
 	this._max_items = this._height / (APPDISPLAY_HEIGHT + APPDISPLAY_PADDING);
-    },
+},
 
-    _refreshCache: function() {
-        let me = this;
-
-        if (!this._appsStale)
-	    return;
-        for (id in this._displayed)
-            this._displayed[id].destroy();
-	this._appset = {};
-	this._displayed = {};
-        let apps = Gio.app_info_get_all();
-	let i = 0;
-	for (i = 0; i < apps.length; i++) {
-	    let appinfo = apps[i];
-	    let appid = appinfo.get_id();
-	    this._appset[appid] = appinfo;
-        }
-        this._appsStale = false;
-    },
-
-    _removeItem: function(appid) {
-        let item = this._displayed[appid];
-        let group = item.actor;
-	group.destroy();
-	delete this._displayed[appid];
-    },
-
-    _removeAll: function() {
-        for (appid in this._displayed)
-            this._removeItem(appid);
-     },
-
-    _setDefaultList: function() {
-        this._removeAll();
-        let added = 0;
-	for (let i = 0; i < DEFAULT_APPLICATIONS.length && added < this._max_items; i++) {
-	    let appid = DEFAULT_APPLICATIONS[i];
-	    let appinfo = this._appset[appid];
-	    if (appinfo) {
-	      this._filterAdd(appid);
-              added += 1;
-            }
-        }
-    },
-
-    _filterAdd: function(appid) {
+_refreshCache: function() {
 	let me = this;
 
-        let appinfo = this._appset[appid];
+	if (!this._appsStale)
+		return;
+	for (id in this._displayed)
+		this._displayed[id].destroy();
+	this._appset = {};
+	this._displayed = {};
+	let apps = Gio.app_info_get_all();
+	let i = 0;
+	for (i = 0; i < apps.length; i++) {
+		let appinfo = apps[i];
+		let appid = appinfo.get_id();
+		this._appset[appid] = appinfo;
+	}
+	this._appsStale = false;
+},
+
+_removeItem: function(appid) {
+	let item = this._displayed[appid];
+	let group = item.actor;
+	group.destroy();
+	delete this._displayed[appid];
+},
+
+_removeAll: function() {
+	for (appid in this._displayed)
+		this._removeItem(appid);
+},
+
+_setDefaultList: function() {
+	this._removeAll();
+	let added = 0;
+	for (let i = 0; i < DEFAULT_APPLICATIONS.length && added < this._max_items; i++) {
+		let appid = DEFAULT_APPLICATIONS[i];
+		let appinfo = this._appset[appid];
+		if (appinfo) {
+			this._filterAdd(appid);
+			added += 1;
+		}
+	}
+},
+
+_filterAdd: function(appid) {
+	let me = this;
+
+	let appinfo = this._appset[appid];
 	let name = appinfo.get_name();
 	let index = 0; for (i in this._displayed) { index += 1; }
 
-        let appdisplay = new AppDisplayItem(appinfo, this._width);
+	let appdisplay = new AppDisplayItem(appinfo, this._width);
 	appdisplay.connect('launch', function() {
-	    appinfo.launch([], null);
-	    me.emit('activated');
-        });
+		appinfo.launch([], null);
+		me.emit('activated');
+	});
 	let group = appdisplay.actor;
-        this._grid.add_actor(group);
+	this._grid.add_actor(group);
 	this._displayed[appid] = appdisplay;
-    },
+},
 
-    _filterRemove: function(appid) {
-        // In the future, do some sort of fade out or other effect here
-        let item = this._displayed[appid];
-        this._removeItem(item);
-    },
+_filterRemove: function(appid) {
+	// In the future, do some sort of fade out or other effect here
+	let item = this._displayed[appid];
+	this._removeItem(item);
+},
 
-    _appinfoMatches: function(appinfo, search) {
-        if (search == null || search == '')
-            return true;
-        let name = appinfo.get_name().toLowerCase();
+_appinfoMatches: function(appinfo, search) {
+	if (search == null || search == '')
+		return true;
+	let name = appinfo.get_name().toLowerCase();
 	if (name.indexOf(search) >= 0)
-	    return true;
-        let description = appinfo.get_description();
+		return true;
+	let description = appinfo.get_description();
 	if (description) {
-            description = description.toLowerCase();
-            if (description.indexOf(search) >= 0)
-	        return true;
-        }
-        let exec = appinfo.get_executable().toLowerCase();
-        if (exec.indexOf(search) >= 0)
-            return true;
+		description = description.toLowerCase();
+		if (description.indexOf(search) >= 0)
+			return true;
+	}
+	let exec = appinfo.get_executable().toLowerCase();
+	if (exec.indexOf(search) >= 0)
+		return true;
 	return false;
-    },
+},
 
-    _sortApps: function(appids) {
-        let me = this;
-        return appids.sort(function (a,b) {
-            let appA = me._appset[a];
-            let appB = me._appset[b];
-            return appA.get_name().localeCompare(appB.get_name());
-        });
-    },
+_sortApps: function(appids) {
+	let me = this;
+	return appids.sort(function (a,b) {
+		let appA = me._appset[a];
+		let appB = me._appset[b];
+		return appA.get_name().localeCompare(appB.get_name());
+	});
+},
 
-    _doSearchFilter: function() {
-        this._removeAll();
-        let matchedApps = [];
-        for (appid in this._appset) {
-	    if (matchedApps.length >= this._max_items)
-	        break;
-            if (appid in this._displayed)
-                continue;
-            let app = this._appset[appid];
-            if (this._appinfoMatches(app, this._search))
-	        matchedApps.push(appid);
-        }
-        this._sortApps(matchedApps);
-        for (let i = 0; i < matchedApps.length; i++) {
-            this._filterAdd(matchedApps[i]);
-        }
-    },
+_doSearchFilter: function() {
+	this._removeAll();
+	let matchedApps = [];
+	for (appid in this._appset) {
+		if (matchedApps.length >= this._max_items)
+			break;
+		if (appid in this._displayed)
+			continue;
+		let app = this._appset[appid];
+		if (this._appinfoMatches(app, this._search))
+			matchedApps.push(appid);
+	}
+	this._sortApps(matchedApps);
+	for (let i = 0; i < matchedApps.length; i++) {
+		this._filterAdd(matchedApps[i]);
+	}
+},
 
-    _redisplay: function() {
-        this._refreshCache();
-        if (!this._search)
-            this._setDefaultList();
-        else
-            this._doSearchFilter();
-    },
+_redisplay: function() {
+	this._refreshCache();
+	if (!this._search)
+		this._setDefaultList();
+	else
+		this._doSearchFilter();
+},
 
-    setSearch: function(text) {
-        this._search = text.toLowerCase();
-        this._redisplay();
-    },
+setSearch: function(text) {
+	this._search = text.toLowerCase();
+	this._redisplay();
+},
 
-    show: function() {
-        this._redisplay();
-        this._grid.show();
-    },
+show: function() {
+	this._redisplay();
+	this._grid.show();
+},
 
-    hide: function() {
-    	this._grid.hide();
-    }
+hide: function() {
+	this._grid.hide();
+}
 }
 Signals.addSignalMethods(AppDisplay.prototype);
 
--- js/ui/main.js
+++ js/ui/main.js
@@ -1,4 +1,4 @@
-/* -*- mode: js2; js2-basic-offset: 4; -*- */
+/* -*- mode: js2; js2-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil -*- */
 
 const Clutter = imports.gi.Clutter;
 const Shell = imports.gi.Shell;
@@ -18,67 +18,67 @@ let overlay = null;
 let run_dialog = null;
 let wm = null;
 
-// The "FrameTicker" object is an object used to feed new frames to Tweener
-// so it can update values and redraw. The default frame ticker for
-// Tweener just uses a simple timeout at a fixed frame rate and has no idea
-// of "catching up" by dropping frames.
-//
-// We substitute it with custom frame ticker here that connects Tweener to
-// a Clutter.TimeLine. Now, Clutter.Timeline itself isn't a whole lot more
-// sophisticated than a simple timeout at a fixed frame rate, but at least
-// it knows how to drop frames. (See HippoAnimationManager for a more
-// sophisticated view of continous time updates; even better is to pay
-// attention to the vertical vblank and sync to that when possible.)
-//
+//The "FrameTicker" object is an object used to feed new frames to Tweener
+//so it can update values and redraw. The default frame ticker for
+//Tweener just uses a simple timeout at a fixed frame rate and has no idea
+//of "catching up" by dropping frames.
+
+//We substitute it with custom frame ticker here that connects Tweener to
+//a Clutter.TimeLine. Now, Clutter.Timeline itself isn't a whole lot more
+//sophisticated than a simple timeout at a fixed frame rate, but at least
+//it knows how to drop frames. (See HippoAnimationManager for a more
+//sophisticated view of continous time updates; even better is to pay
+//attention to the vertical vblank and sync to that when possible.)
+
 function ClutterFrameTicker() {
     this._init();
 }
 
 ClutterFrameTicker.prototype = {
-    TARGET_FRAME_RATE : 60,
+TARGET_FRAME_RATE : 60,
 
-    _init : function() {
-	// We don't have a finite duration; tweener will tell us to stop
-	// when we need to stop, so use 1000 seconds as "infinity"
-	this._timeline = new Clutter.Timeline({ fps: this.TARGET_FRAME_RATE,
-	                                        duration: 1000*1000 });
-	this._frame = 0;
+_init : function() {
+    // We don't have a finite duration; tweener will tell us to stop
+    // when we need to stop, so use 1000 seconds as "infinity"
+    this._timeline = new Clutter.Timeline({ fps: this.TARGET_FRAME_RATE,
+        duration: 1000*1000 });
+    this._frame = 0;
 
-	let me = this;
-	this._timeline.connect('new-frame',
-	    function(timeline, frame) {
-		me._onNewFrame(frame);
-	    });
-    },
+    let me = this;
+    this._timeline.connect('new-frame',
+            function(timeline, frame) {
+        me._onNewFrame(frame);
+    });
+},
 
-    _onNewFrame : function(frame) {
-	// Unfortunately the interface to to send a new frame to tweener
-	// is a simple "next frame" and there is no provision for signaling
-	// that frames have been skipped or just telling it the new time.
-	// But what it actually does internally is just:
-	//
-	//  _currentTime += 1000/_ticker.FRAME_RATE;
-	//
-	// So by dynamically adjusting the value of FRAME_RATE we can trick
-	// it into dealing with dropped frames.
+_onNewFrame : function(frame) {
+    // Unfortunately the interface to to send a new frame to tweener
+    // is a simple "next frame" and there is no provision for signaling
+    // that frames have been skipped or just telling it the new time.
+    // But what it actually does internally is just:
+    //
+    //  _currentTime += 1000/_ticker.FRAME_RATE;
+    //
+    // So by dynamically adjusting the value of FRAME_RATE we can trick
+    // it into dealing with dropped frames.
 
-	let delta = frame - this._frame;
-	if (delta == 0)
-	    this.FRAME_RATE = this.TARGET_FRAME_RATE;
-	else
-	    this.FRAME_RATE = this.TARGET_FRAME_RATE / delta;
-	this._frame = frame;
-	this.emit('prepare-frame');
-    },
+    let delta = frame - this._frame;
+    if (delta == 0)
+        this.FRAME_RATE = this.TARGET_FRAME_RATE;
+    else
+        this.FRAME_RATE = this.TARGET_FRAME_RATE / delta;
+    this._frame = frame;
+    this.emit('prepare-frame');
+},
 
-    start : function() {
-	this._timeline.start();
-    },
+start : function() {
+    this._timeline.start();
+},
 
-    stop : function() {
-	this._timeline.stop();
-	this._frame = 0;
-    }
+stop : function() {
+    this._timeline.stop();
+    this._frame = 0;
+}
 };
 
 Signals.addSignalMethods(ClutterFrameTicker.prototype);
@@ -97,20 +97,20 @@ function start() {
     // in the overlay. Clear that out.
     let children = global.overlay_group.get_children();
     for (let i = 0; i < children.length; i++)
-	children[i].destroy();
+        children[i].destroy();
 
     global.connect('panel-run-dialog', function(panel) {
         // Make sure not more than one run dialog is shown.
         if (!run_dialog) {
             run_dialog = new RunDialog.RunDialog();
-	    let end_handler = function() {
+            let end_handler = function() {
                 run_dialog.destroy();
                 run_dialog = null;
             };
             run_dialog.connect('run', end_handler);
             run_dialog.connect('cancel', end_handler);
             if (!run_dialog.show())
-		end_handler();
+                end_handler();
         }
     });
 
@@ -121,14 +121,14 @@ function start() {
     wm = new WindowManager.WindowManager();
 }
 
-// Used to go into a mode where all keyboard and mouse input goes to
-// the stage. Returns true if we successfully grabbed the keyboard and
-// went modal, false otherwise
+//Used to go into a mode where all keyboard and mouse input goes to
+//the stage. Returns true if we successfully grabbed the keyboard and
+//went modal, false otherwise
 function startModal() {
     let global = Shell.Global.get();
 
     if (!global.grab_keyboard())
-	return false;
+        return false;
 
     global.set_stage_input_area(0, 0, global.screen_width, global.screen_height);
 
@@ -144,7 +144,7 @@ function endModal() {
 
 function show_overlay() {
     if (startModal())
-	overlay.show();
+        overlay.show();
 }
 
 function hide_overlay() {
