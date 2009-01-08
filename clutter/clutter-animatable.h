d340de8e00b5de125ed94b2b26b9bcaa08a19675
Emmanuele Bassi <ebassi@linux.intel.com>
2009-01-08 13:18:00 +0000

    Add license notice to ClutterAnimation files

--- clutter/clutter-animatable.h
+++ clutter/clutter-animatable.h
@@ -1,3 +1,27 @@
+/*
+ * Clutter.
+ *
+ * An OpenGL based 'interactive canvas' library.
+ *
+ * Copyright (C) 2009  Intel Corporation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author:
+ *   Emmanuele Bassi <ebassi@linux.intel.com>
+ */
+
 #ifndef __CLUTTER_ANIMATABLE_H__
 #define __CLUTTER_ANIMATABLE_H__
 
