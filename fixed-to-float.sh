010fd71059c073a95d41b023be67000d1dfc1a4c
Robert Bragg <robert@linux.intel.com>
2009-01-12 17:07:55 +0000

    [cogl.h.in.0.patch] Fixes the cogl_rotatex prototype to take float x,y,z params
    The cogl_rotatex was incorrectly defined to take gint x,y,z params as opposed
    to CoglFixed. This patch changes them directly to floats.

--- fixed-to-float.sh
+++ fixed-to-float.sh
@@ -150,6 +150,7 @@ patch -p1<fixed-to-float-patches/gles-cogl.c.0.patch
 patch -p1<fixed-to-float-patches/gles-cogl-gles2-wrapper.h.0.patch
 patch -p1<fixed-to-float-patches/gles-cogl-primitives.c.0.patch
 patch -p1<fixed-to-float-patches/gles-cogl-texture.c.0.patch
+patch -p1<fixed-to-float-patches/cogl.h.in.0.patch
 
 #XXX: COGL_PANGO_UNIT_TO_FIXED
 
