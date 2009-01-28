e30c5b33b90ee27a95d46d156a41e5d14c6c69ed
Neil Roberts <neil@linux.intel.com>
2009-01-28 16:24:02 +0000

    Undeprecate clutter_actor_set_scale_with_gravity
    The scale center has returned so this function is available again but
    it was still deprecated so you couldn't use it.

--- clutter/clutter-deprecated.h
+++ clutter/clutter-deprecated.h
@@ -33,8 +33,7 @@
 #define clutter_actor_get_rxangx                     clutter_actor_get_rxangx_DEPRECATED_BY_clutter_actor_get_rotationx
 #define clutter_actor_get_ryangx                     clutter_actor_get_ryangx_DEPRECATED_BY_clutter_actor_get_rotationx
 #define clutter_actor_get_rzangx                     clutter_actor_get_rzangx_DEPRECATED_BY_clutter_actor_get_rotationx
-#define clutter_actor_set_scale_with_gravity         clutter_actor_set_scale_with_gravity_DEPRECATED_BY_clutter_actor_set_anchor_point_from_gravity
-#define clutter_actor_set_scale_with_gravityx        clutter_actor_set_scale_with_gravity_DEPRECATED_BY_clutter_actor_set_anchor_point_from_gravity
+#define clutter_actor_set_scale_with_gravityx        clutter_actor_set_scale_with_gravityx_DEPRECATED_BY_clutter_actor_set_scale_with_gravity
 
 #define clutter_entry_set_position                   clutter_entry_set_position_REPLACED_BY_clutter_entry_set_cursor_position
 #define clutter_entry_get_position                   clutter_entry_get_position_REPLACED_BY_clutter_entry_get_cursor_position
