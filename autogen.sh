cd0fdbb7d50d2fae6b6d8b9099aa4a43105538e3
Owen W. Taylor <otaylor@fishsoup.net>
2009-02-13 14:14:43 -0500

    Honor ACLOCAL_FLAGS in autogen.sh
    autoreconf doesn't pay attention to the ACLOCAL_FLAGS variable that
    jhbuild (for example) sets. Pass those flags into autoreconf by
    setting ACLOCAL appropriately.

--- autogen.sh
+++ autogen.sh
@@ -33,7 +33,7 @@ if test -z $AUTORECONF; then
         echo "*** No autoreconf found ***"
         exit 1
 else
-        autoreconf -v --install || exit $?
+        ACLOCAL="${ACLOCAL-aclocal} $ACLOCAL_FLAGS" autoreconf -v --install || exit $?
 fi
 
 ./configure "$@" && echo "Now type 'make' to compile $PROJECT."
