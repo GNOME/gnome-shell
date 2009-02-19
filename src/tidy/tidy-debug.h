d61401429e186dd8706f36f9fc05e13cffda7dcd
Owen W. Taylor <otaylor@redhat.com>
2009-02-19 13:42:08 -0500

    Remove the parts of Tidy that we aren't using
    Remove TidyButton/TidyFrame and the utility classes they use.
    We aren't using them and this avoids porting them to Clutter-0.9.

--- src/tidy/tidy-debug.h
+++
@@ -1,4 +0,0 @@
-#ifndef __TIDY_DEBUG_H__
-#define __TIDY_DEBUG_H__
-
-#endif /* __TIDY_DEBUG_H__ */
--- src/tidy/tidy-util.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TIDY_UTIL
-#define _TIDY_UTIL
-
-#include <clutter/clutter.h>
-
-void
-tidy_util_preload_glyphs (char *font, ...);
-
-#endif
