97431b46700fbbe966f33fae13f9728d77cb2687
Neil Roberts <neil@linux.intel.com>
2009-02-20 11:07:23 +0000

    Replaced ClutterFixed constants in color_{darken,lighten} with float
    Bug 1463 - clutter_color_lighten(darken) unexpected because of
               ClutterFixed=>float
    
    These two functions contained ClutterFixed constants represented as
    integers which are no longer valid since the cogl-float branch
    merge. They are now converted to ClutterFixed from float constants
    using CLUTTER_FLOAT_TO_FIXED (which is now a no-op).
    
    Thanks to Zhang Wei for reporting.

--- clutter/clutter-color.c
+++ clutter/clutter-color.c
@@ -112,8 +112,7 @@ void
 clutter_color_lighten (const ClutterColor *src,
 		       ClutterColor       *dest)
 {
-  /* 0x14ccd is ClutterFixed for 1.3 */
-  clutter_color_shadex (src, dest, 0x14ccd);
+  clutter_color_shadex (src, dest, CLUTTER_FLOAT_TO_FIXED (1.3));
 }
 
 /**
@@ -128,8 +127,7 @@ void
 clutter_color_darken (const ClutterColor *src,
 		      ClutterColor       *dest)
 {
-  /* 0xb333 is ClutterFixed for 0.7 */
-  clutter_color_shadex (src, dest, 0xb333);
+  clutter_color_shadex (src, dest, CLUTTER_FLOAT_TO_FIXED (0.7));
 }
 
 /**
