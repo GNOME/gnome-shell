ed0f7de4c031bae0d8598489f2dd7846531c30ae
Marina Zhurakhinskaya <marinaz@redhat.com>
2009-04-09 09:54:58 -0400

    Add gnome-menus-devel to the Fedora build dependencies.
    It's needed by the new code for displaying application categories.

--- tools/build/gnome-shell-build-setup.sh
+++ tools/build/gnome-shell-build-setup.sh
@@ -30,7 +30,7 @@ fi
 # libtool, pkgconfig
 #
 # Devel packages needed by gnome-shell and its deps:
-# dbus-glib, gconf, GL, gtk, libffi, libgnomeui, librsvg, libwnck,
+# dbus-glib, gconf, GL, gnome-menus, gtk, libffi, libgnomeui, librsvg, libwnck,
 # python, readline, spidermonkey ({mozilla,firefox,xulrunner}-js),
 # xdamage
 #
@@ -81,7 +81,7 @@ if test x$system = xFedora ; then
     binutils curl gcc make subversion \
     automake bison flex git gnome-common gnome-doc-utils intltool \
     libtool pkgconfig \
-    dbus-glib-devel GConf2-devel gtk2-devel libffi-devel libgnomeui-devel \
+    dbus-glib-devel GConf2-devel gnome-menus-devel gtk2-devel libffi-devel libgnomeui-devel \
     librsvg2-devel libwnck-devel mesa-libGL-devel python-devel readline-devel \
     xulrunner-devel libXdamage-devel \
     gdb glx-utils xorg-x11-apps xorg-x11-server-Xephyr xterm zenity \
