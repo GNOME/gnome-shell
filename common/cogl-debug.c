121bcd6dfe9aa7126dfa5d88aa0cf5b5c61a456d
Robert Bragg <robert@linux.intel.com>
2009-06-17 01:59:28 +0100

    [Cogl journal] Adds a --cogl-debug=journal option for tracing the journal
    When this option is used Cogl will print a trace of all quads that get
    logged into the journal, and a trace of quads as they get flushed.
    
    If you are seeing a bug with the geometry being drawn by Cogl this may give
    some clues by letting you sanity check the numbers being logged vs the
    numbers being emitted.

--- common/cogl-debug.c
+++ common/cogl-debug.c
@@ -43,7 +43,8 @@ static const GDebugKey cogl_debug_keys[] = {
   { "blend-strings", COGL_DEBUG_BLEND_STRINGS },
   { "disable-batching", COGL_DEBUG_DISABLE_BATCHING },
   { "client-side-matrices", COGL_DEBUG_FORCE_CLIENT_SIDE_MATRICES },
-  { "disable-vbos", COGL_DEBUG_DISABLE_VBOS }
+  { "disable-vbos", COGL_DEBUG_DISABLE_VBOS },
+  { "journal", COGL_DEBUG_JOURNAL }
 };
 
 static const gint n_cogl_debug_keys = G_N_ELEMENTS (cogl_debug_keys);
