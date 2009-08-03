45dc34bfa2163b409affdc17fbed495f0b571aa2
Owen W. Taylor <otaylor@fishsoup.net>
2009-08-03 15:12:03 -0400

    Add mesa-utils to build-setup.sh for Debian/Utils
    At least on Ubuntu 9.04 and Debian Squeeze, glxinfo is in the
    mesa-utils package, so pull that in.
    
    Reported by Flamarion Jorge
    http://bugzilla.gnome.org/show_bug.cgi?id=590480

--- tools/build/gnome-shell-build-setup.sh
+++ tools/build/gnome-shell-build-setup.sh
@@ -63,7 +63,7 @@ if test x$system = xUbuntu -o x$system = xDebian ; then
     automake bison flex git-core gnome-common gtk-doc-tools \
     libdbus-glib-1-dev libgconf2-dev libgtk2.0-dev libffi-dev \
     libgnome-menu-dev libgnomeui-dev librsvg2-dev libwnck-dev libgl1-mesa-dev \
-    mesa-common-dev python-dev libreadline5-dev xulrunner-dev \
+    mesa-common-dev mesa-utils python-dev libreadline5-dev xulrunner-dev \
     xserver-xephyr libxss-dev \
     libgstreamer0.10-dev gstreamer0.10-plugins-base gstreamer0.10-plugins-good \
     ; do
