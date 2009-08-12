8c49e227536fabddd2cbf81aa94e1624d30ca312
Emmanuele Bassi <ebassi@linux.intel.com>
2009-08-12 10:30:11 +0100

    Add copyright and license notice
    The clutter-script-parser.c does not have a copyright and license
    notices; even though the LGPL is a per-project license and not a
    per-file license, having those notices in every source file is a
    good idea.

--- clutter/clutter-script-parser.c
+++ clutter/clutter-script-parser.c
@@ -1,3 +1,31 @@
+/*
+ * Clutter.
+ *
+ * An OpenGL based 'interactive canvas' library.
+ *
+ * Copyright (C) 2006, 2007, 2008 OpenedHand Ltd
+ * Copyright (C) 2009 Intel Corportation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Original author:
+ *
+ *      Emmanuele Bassi <ebassi@linux.intel.com>
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -68,6 +96,10 @@ clutter_script_get_type_from_class (const gchar *name)
        * see:
        *
        * http://mail.gnome.org/archives/gtk-devel-list/2007-June/msg00022.html
+       *
+       * and:
+       *
+       * http://git.gnome.org/cgit/gtk+/plain/gtk/gtkbuilderparser.c
        */
 
       if ((c == g_ascii_toupper (c) &&
