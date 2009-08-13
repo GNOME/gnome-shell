5c8ae6d2dcdb592a56f33927dcec2369a9147396
Emmanuele Bassi <ebassi@linux.intel.com>
2009-08-13 18:05:38 +0100

    [json] Fix compilation of the internal JSON-GLib
    Define the symbols needed to pass the single include policy when
    compiling the static object.

--- clutter/json/json-glib.h
+++ clutter/json/json-glib.h
@@ -1,8 +1,12 @@
 #ifndef __JSON_GLIB_H__
 #define __JSON_GLIB_H__
 
+#define __JSON_GLIB_INSIDE__
+
 #include "json-types.h"
 #include "json-generator.h"
 #include "json-parser.h"
+
+#undef __JSON_GLIB_INSIDE__
 
 #endif /* __JSON_GLIB_H__ */
