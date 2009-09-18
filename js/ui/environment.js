b18a8ebcae4280a78983651f744fbf295b5ecaf9
Owen W. Taylor <otaylor@fishsoup.net>
2009-09-18 15:51:15 -0400

    Add some structure for interactive tests of UI components
    js/ui/environment.js: Split out initial UI setup (Tweener initialization,
      ClutterContainer monkey-patching) into a separate file we can import from tests.
    
    tests/: Directory for various types of tests
    tests/run-test.sh: Shell script that to run tests with an appropriate
      environment set up.
    
    tests/testcommon/: Common modules and data for tests
    tests/interactive/: Interactive tests
    
    tests/interactive/box-layout.js: A sample test of StLayout
    
    https://bugzilla.gnome.org/show_bug.cgi?id=595987

---
+++ js/ui/environment.js
@@ -0,0 +1,32 @@
+/* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+const St = imports.gi.St;
+
+const Tweener = imports.ui.tweener;
+
+// "monkey patch" in some varargs ClutterContainer methods; we need
+// to do this per-container class since there is no representation
+// of interfaces in Javascript
+function _patchContainerClass(containerClass) {
+    // This one is a straightforward mapping of the C method
+    containerClass.prototype.child_set = function(actor, props) {
+        let meta = this.get_child_meta(actor);
+        for (prop in props)
+            meta[prop] = props[prop];
+    };
+
+    // clutter_container_add() actually is a an add-many-actors
+    // method. We conveniently, but somewhat dubiously, take the
+    // this opportunity to make it do something more useful.
+    containerClass.prototype.add = function(actor, props) {
+        this.add_actor(actor);
+        if (props)
+            this.child_set(actor, props);
+    };
+}
+
+_patchContainerClass(St.BoxLayout);
+
+function init() {
+    Tweener.init();
+}
--- js/ui/main.js
+++ js/ui/main.js
@@ -13,13 +13,13 @@ const Signals = imports.signals;
 const St = imports.gi.St;
 
 const Chrome = imports.ui.chrome;
+const Environment = imports.ui.environment;
 const Overview = imports.ui.overview;
 const Panel = imports.ui.panel;
 const RunDialog = imports.ui.runDialog;
 const LookingGlass = imports.ui.lookingGlass;
 const ShellDBus = imports.ui.shellDBus;
 const Sidebar = imports.ui.sidebar;
-const Tweener = imports.ui.tweener;
 const WindowManager = imports.ui.windowManager;
 
 const DEFAULT_BACKGROUND_COLOR = new Clutter.Color();
@@ -37,28 +37,6 @@ let shellDBusService = null;
 let modalCount = 0;
 let modalActorFocusStack = [];
 
-// "monkey patch" in some varargs ClutterContainer methods; we need
-// to do this per-container class since there is no representation
-// of interfaces in Javascript
-function _patchContainerClass(containerClass) {
-    // This one is a straightforward mapping of the C method
-    containerClass.prototype.child_set = function(actor, props) {
-        let meta = this.get_child_meta(actor);
-        for (prop in props)
-            meta[prop] = props[prop];
-    };
-
-    // clutter_container_add() actually is a an add-many-actors
-    // method. We conveniently, but somewhat dubiously, take the
-    // this opportunity to make it do something more useful.
-    containerClass.prototype.add = function(actor, props) {
-        this.add_actor(actor);
-        if (props)
-            this.child_set(actor, props);
-    };
-}
-_patchContainerClass(St.BoxLayout);
-
 function start() {
     // Add a binding for "global" in the global JS namespace; (gjs
     // keeps the web browser convention of having that namespace be
@@ -75,7 +53,7 @@ function start() {
     // back into sync ones.
     DBus.session.flush();
 
-    Tweener.init();
+    Environment.init();
 
     // Ensure ShellAppMonitor is initialized; this will
     // also initialize ShellAppSystem first.  ShellAppSystem
