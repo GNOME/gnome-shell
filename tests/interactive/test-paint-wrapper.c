3b075b6dc4197ed78f6386ee7e9a6e307839590f
Emmanuele Bassi <ebassi@linux.intel.com>
2009-12-09 01:46:52 +0000

    tests: Allow setting the stage opacity in test-paint-wrapper
    This allows testing the Stage:use-alpha property and the blending done
    by a compositor.

--- tests/interactive/test-paint-wrapper.c
+++ tests/interactive/test-paint-wrapper.c
@@ -31,6 +31,7 @@ typedef struct SuperOH
 } SuperOH;
 
 static gint n_hands = NHANDS;
+static gint use_alpha = 255;
 
 static GOptionEntry super_oh_entries[] = {
   {
@@ -38,6 +39,12 @@ static GOptionEntry super_oh_entries[] = {
     0,
     G_OPTION_ARG_INT, &n_hands,
     "Number of hands", "HANDS"
+  },
+  {
+    "use-alpha", 'a',
+    0,
+    G_OPTION_ARG_INT, &use_alpha,
+    "Stage opacity", "VALUE"
   },
   { NULL }
 };
@@ -204,6 +211,14 @@ test_paint_wrapper_main (int argc, char *argv[])
 
   stage = clutter_stage_get_default ();
   clutter_actor_set_size (stage, 800, 600);
+
+  if (use_alpha != 255)
+    {
+      g_object_set (G_OBJECT (stage),
+                    "use-alpha", TRUE,
+                    "opacity", use_alpha,
+                    NULL);
+    }
 
   clutter_stage_set_title (CLUTTER_STAGE (stage), "Paint Test");
   clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
