850dd5a38fe17e82b16e19144db4ac516e349dbb
Emmanuele Bassi <ebassi@linux.intel.com>
2010-01-15 11:40:01 +0000

    tests: Print the device name, as well as its Id
    The test-devices interactive test should display the device name along
    with the id.

--- tests/interactive/test-devices.c
+++ tests/interactive/test-devices.c
@@ -79,7 +79,8 @@ test_devices_main (int argc, char **argv)
       device_type = clutter_input_device_get_device_type (device);
       if (device_type == CLUTTER_POINTER_DEVICE)
         {
-          g_print ("got a pointer device with id %d...\n",
+          g_print ("got a pointer device '%s' with id %d...\n",
+                   clutter_input_device_get_device_name (device),
                    clutter_input_device_get_device_id (device));
 
           hand = clutter_texture_new_from_file (TESTS_DATADIR
