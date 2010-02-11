f74f4bdc2bcf885c53d8c07c9a25dac7d5ce9505
Neil Roberts <neil@linux.intel.com>
2010-02-11 14:20:48 +0000

    cogl: Add a fallback for when the signbit macro is missing
    The signbit macro is defined in C99 so it should be available but some
    versions of GCC don't appear to define it by default. If it's not
    available we can use a hack to test the bit directly.

--- cogl/cogl-util.h
+++ cogl/cogl-util.h
@@ -3,7 +3,7 @@
  *
  * An object oriented GL/GLES Abstraction/Utility Layer
  *
- * Copyright (C) 2007,2008,2009 Intel Corporation.
+ * Copyright (C) 2007,2008,2009,2010 Intel Corporation.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -24,7 +24,32 @@
 #ifndef __COGL_UTIL_H
 #define __COGL_UTIL_H
 
+#include <glib.h>
+#include <math.h>
+
 int
 cogl_util_next_p2 (int a);
+
+/* The signbit macro is defined by ISO C99 so it should be available,
+   however if it's not we can fallback to an evil hack */
+#ifdef signbit
+#define cogl_util_float_signbit(x) signbit(x)
+#else
+/* This trick was stolen from here:
+   http://lists.boost.org/Archives/boost/2006/08/108731.php
+
+   It xors the integer reinterpretations of -1.0f and 1.0f. In theory
+   they should only differ by the signbit so that gives a mask for the
+   sign which we can just test against the value */
+static inline gboolean
+cogl_util_float_signbit (float x)
+{
+  static const union { float f; guint32 i; } negative_one = { -1.0f };
+  static const union { float f; guint32 i; } positive_one = { +1.0f };
+  union { float f; guint32 i; } value = { x };
+
+  return !!((negative_one.i ^ positive_one.i) & value.i);
+}
+#endif
 
 #endif /* __COGL_UTIL_H */
