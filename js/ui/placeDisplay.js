GNOME/gnome-shell

commit 1c4c3afb270bbfcfb76b93f50cb252dca1c4f3e9

Add eject buttons in places menu for places which support it
https://bugzilla.gnome.org/show_bug.cgi?id=602976

js/ui/placeDisplay.js
@@ -54,8 +54,53 @@ PlaceInfo.prototype = {
                 mtype = Search.MatchType.SUBSTRING;
         }
         return mtype;
+    },
+
+    isRemovable: function() {
+        return false;
     }
 }
+
+function PlaceDeviceInfo(mount) {
+    this._init(mount);
+}
+
+PlaceDeviceInfo.prototype = {
+    __proto__: PlaceInfo.prototype,
+
+    _init: function(mount) {
+        this._mount = mount;
+        this.name = mount.get_name();
+        this._lowerName = this.name.toLowerCase();
+        this.id = "mount:" + mount.get_root().get_uri();
+    },
+
+    iconFactory: function(size) {
+        let icon = this._mount.get_icon();
+        return Shell.TextureCache.get_default().load_gicon(icon, size);
+    },
+
+    launch: function() {
+        Gio.app_info_launch_default_for_uri(this._mount.get_root().get_uri(),
+                                            global.create_app_launch_context());
+    },
+
+    isRemovable: function() {
+        return this._mount.can_unmount();
+    },
+
+    remove: function() {
+        if (!this.isRemovable())
+            return;
+
+        this._mount.unmount(0, null, Lang.bind(this, this._removeFinish), null);
+    },
+
+    _removeFinish: function(o, res, data) {
+        this._mount.unmount_finish(res);
+    }
+}
+
 
 function PlacesManager() {
     this._init();
@@ -288,18 +333,7 @@ PlacesManager.prototype = {
     },
 
     _addMount: function(mount) {
-        let mountLabel = mount.get_name();
-        let mountIcon = mount.get_icon();
-        let root = mount.get_root();
-        let mountUri = root.get_uri();
-        let devItem = new PlaceInfo('mount:' + mountUri,
-                                     mountLabel,
-               function(size) {
-                        return Shell.TextureCache.get_default().load_gicon(mountIcon, size);
-               },
-               function() {
-                        Gio.app_info_launch_default_for_uri(mountUri, global.create_app_launch_context());
-               });
+        let devItem = new PlaceDeviceInfo(mount);
         this._mounts.push(devItem);
     },
 
@@ -358,21 +392,35 @@ DashPlaceDisplayItem.prototype = {
         this._info = info;
         this._icon = info.iconFactory(PLACES_ICON_SIZE);
         this.actor = new Big.Box({ orientation: Big.BoxOrientation.HORIZONTAL,
-                                   reactive: true,
                                    spacing: 4 });
-        this.actor.connect('button-release-event', Lang.bind(this, function (b, e) {
-            this._info.launch();
-            Main.overview.hide();
-        }));
-        let text = new St.Label({ style_class: 'places-item',
-                                  text: info.name });
-        let iconBox = new Big.Box({ y_align: Big.BoxAlignment.CENTER });
-        iconBox.append(this._icon, Big.BoxPackFlags.NONE);
+        let text = new St.Button({ style_class: 'places-item',
+                                   label: info.name,
+                                   x_align: St.Align.START });
+        text.connect('clicked', Lang.bind(this, this._onClicked));
+        let iconBox = new St.Bin({ child: this._icon, reactive: true });
+        iconBox.connect('button-release-event',
+                        Lang.bind(this, this._onClicked));
         this.actor.append(iconBox, Big.BoxPackFlags.NONE);
         this.actor.append(text, Big.BoxPackFlags.EXPAND);
 
+        if (info.isRemovable()) {
+            let removeIcon = Shell.TextureCache.get_default().load_icon_name ('media-eject', PLACES_ICON_SIZE);
+            let removeIconBox = new St.Button({ child: removeIcon,
+                                                reactive: true });
+            this.actor.append(removeIconBox, Big.BoxPackFlags.NONE);
+            removeIconBox.connect('clicked',
+                                  Lang.bind(this, function() {
+                                                  this._info.remove();
+                                            }));
+        }
+
         this.actor._delegate = this;
         let draggable = DND.makeDraggable(this.actor);
+    },
+
+    _onClicked: function(b) {
+        this._info.launch();
+        Main.overview.hide();
     },
 
     getDragActorSource: function() {

commit ddfe9442fa5d928e871d12f4f3c1144ec8c26129

[placeDisplay] Fix dnd regression
DND from the places section broke with 1c4c3afb when St.Label was
replaced with St.Button.
To fix, replace St.Button with St.Clickable and use a fake_release
when starting a drag.

https://bugzilla.gnome.org/show_bug.cgi?id=609218

data/theme/gnome-shell.css
@@ -516,8 +516,16 @@ StTooltip {
 
 /* Places */
 
-.places-actions {
+.places-item-box {
     spacing: 4px;
+}
+
+.places-item {
+    padding: 2px;
+}
+
+.places-actions {
+    spacing: 2px;
 }
 
 #placesDevices {

js/ui/placeDisplay.js
@@ -408,36 +408,77 @@ DashPlaceDisplayItem.prototype = {
         this.name = info.name;
         this._info = info;
         this._icon = info.iconFactory(PLACES_ICON_SIZE);
-        this.actor = new Big.Box({ orientation: Big.BoxOrientation.HORIZONTAL,
-                                   spacing: 4 });
-        let text = new St.Button({ style_class: 'places-item',
-                                   label: info.name,
-                                   x_align: St.Align.START });
-        text.connect('clicked', Lang.bind(this, this._onClicked));
-        let iconBox = new St.Bin({ child: this._icon, reactive: true });
-        iconBox.connect('button-release-event',
-                        Lang.bind(this, this._onClicked));
-        this.actor.append(iconBox, Big.BoxPackFlags.NONE);
-        this.actor.append(text, Big.BoxPackFlags.EXPAND);
+
+        this.actor = new St.Clickable({ style_class: 'places-item',
+                                        reactive: true,
+                                        x_align: St.Align.START,
+                                        x_fill: true });
+
+        let box = new St.BoxLayout({ style_class: 'places-item-box' });
+        this.actor.set_child(box);
+
+        let bin = new St.Bin({ child: this._icon });
+        box.add(bin);
+
+        let text = new St.Label({ text: info.name });
+        box.add(text, { expand: true, x_fill: true });
 
         if (info.isRemovable()) {
             let removeIcon = St.TextureCache.get_default().load_icon_name ('media-eject', PLACES_ICON_SIZE);
-            let removeIconBox = new St.Button({ child: removeIcon,
-                                                reactive: true });
-            this.actor.append(removeIconBox, Big.BoxPackFlags.NONE);
+            let removeIconBox = new St.Clickable({ child: removeIcon,
+                                                   reactive: true });
+            box.add(removeIconBox);
             removeIconBox.connect('clicked',
                                   Lang.bind(this, function() {
                                                   this._info.remove();
                                             }));
         }
 
+        this.actor.connect('clicked', Lang.bind(this, this._onClicked));
+        this.actor.connect('notify::hover',
+                           Lang.bind(this, this._onHoverChanged));
+        this.actor.connect('button-press-event',
+                           Lang.bind(this, this._onButtonPress));
+        this.actor.connect('button-release-event',
+                           Lang.bind(this, this._onButtonRelease));
+
         this.actor._delegate = this;
-        let draggable = DND.makeDraggable(this.actor);
+        this._dragStartX = null;
+        this._dragStartY = null;
+        this._draggable = DND.makeDraggable(this.actor, true);
     },
 
     _onClicked: function(b) {
         this._info.launch();
         Main.overview.hide();
+    },
+
+    _onButtonPress: function(actor, event) {
+        if (event.get_button() != 1)
+            return false;
+
+        let [stageX, stageY] = event.get_coords();
+        this._dragStartX = stageX;
+        this._dragStartY = stageY;
+    },
+
+    _onButtonRelease: function(actor, event) {
+        if (event.get_button() != 1)
+            return false;
+
+        this._dragStartX = null;
+        this._dragStartY = null;
+    },
+
+    _onHoverChanged: function(button) {
+        let hover = button.hover;
+        if (!hover) {
+            if (button.pressed && this._dragStartX != null) {
+                button.fake_release();
+                this._draggable.startDrag(this._dragStartX, this._dragStartY,
+                                          global.get_current_time());
+            }
+        }
     },
 
     getDragActorSource: function() {