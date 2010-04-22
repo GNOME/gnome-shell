GNOME/gnome-shell

commit 8146d8d08deafd6cf86238a4a8c2fa6149e5c691

profile: avoid segfault generating reports with no stats
The clutter-profile.c print_report() code would crash if no stats had
been gathered because uprof would return NULL for the "Redrawing" timer
which we then dereferenced.

This changes the code to start by checking for the "Mainloop",
"Redrawing" and "Do Pick" timers and if none are present it returns
immediately without generating any report.

clutter/clutter-profile.c
@@ -57,8 +57,6 @@ print_report (UProfReport *report, UProfContext *context)
   float fps;
   ClutterUProfReportState state;
 
-  g_print ("\n");
-
   /* FIXME: We need to fix the way Clutter initializes the uprof library
    * (we don't currently call uprof_init()) and add a mechanism to know
    * if uprof_init hasn't been called so we can simply bail out of report
@@ -66,23 +64,25 @@ print_report (UProfReport *report, UProfContext *context)
    * Probably we can just have uprof_report_print bail out if uprof wasn't
    * initialized, so we don't have to care here.
    */
-
+  mainloop_timer = uprof_context_get_timer_result (context, "Mainloop");
+  if (!mainloop_timer)
+    return;
   stage_paint_timer = uprof_context_get_timer_result (context, "Redrawing");
-#if 0
   if (!stage_paint_timer)
-    g_critical ("Failed to find \"Redrawing\" timer "
-                "(you need to update print_report code if you rename it)\n");
-#endif
+    return;
+  do_pick_timer = uprof_context_get_timer_result (context, "Do pick");
+  if (!do_pick_timer)
+    return;
+
+  g_print ("\n");
 
   state.n_frames = uprof_timer_result_get_start_count (stage_paint_timer);
   g_print ("Frame count = %lu\n", state.n_frames);
 
-  mainloop_timer = uprof_context_get_timer_result (context, "Mainloop");
   fps = (float)state.n_frames / (uprof_timer_result_get_total_msecs (mainloop_timer)
                                  / 1000.0);
   g_print ("Average fps = %5.2f\n", fps);
 
-  do_pick_timer = uprof_context_get_timer_result (context, "Do pick");
   if (do_pick_timer)
     {
       int n_picks = uprof_timer_result_get_start_count (do_pick_timer);

commit 5608a54708c3ab1665840c24bdfcb9c2b38fa78b

profile: Fix the report generation
Timers and counters might not exist, so make every section of the
profile report depend on the object that it is querying.

This fixes the profile report generation that was broken by commit
8146d8d08deafd6cf86238a4a8c2fa6149e5c691.

clutter/clutter-profile.c
@@ -65,27 +65,27 @@ print_report (UProfReport *report, UProfContext *context)
    * initialized, so we don't have to care here.
    */
   mainloop_timer = uprof_context_get_timer_result (context, "Mainloop");
-  if (!mainloop_timer)
-    return;
   stage_paint_timer = uprof_context_get_timer_result (context, "Redrawing");
-  if (!stage_paint_timer)
-    return;
   do_pick_timer = uprof_context_get_timer_result (context, "Do pick");
-  if (!do_pick_timer)
-    return;
 
-  g_print ("\n");
+  if (stage_paint_timer)
+    {
+      g_print ("\n");
 
-  state.n_frames = uprof_timer_result_get_start_count (stage_paint_timer);
-  g_print ("Frame count = %lu\n", state.n_frames);
+      state.n_frames = uprof_timer_result_get_start_count (stage_paint_timer);
+      g_print ("Frame count = %lu\n", state.n_frames);
 
-  fps = (float)state.n_frames / (uprof_timer_result_get_total_msecs (mainloop_timer)
+      fps = (float) state.n_frames
+          / (uprof_timer_result_get_total_msecs (mainloop_timer)
                                  / 1000.0);
-  g_print ("Average fps = %5.2f\n", fps);
+      g_print ("Average fps = %5.2f\n", fps);
+    }
 
   if (do_pick_timer)
     {
       int n_picks = uprof_timer_result_get_start_count (do_pick_timer);
+
+      g_print ("\n");
 
       g_print ("Pick Stats:\n");
       g_print ("Pick count = %d\n", n_picks);
@@ -94,9 +94,13 @@ print_report (UProfReport *report, UProfContext *context)
       g_print ("Average Msecs per pick = %3.2f\n",
                (float)uprof_timer_result_get_total_msecs (do_pick_timer)
                / (float)n_picks);
-
-      g_print ("\n");
     }
+
+  root_timers = uprof_context_get_root_timer_results (context);
+  if (!root_timers)
+    return;
+
+  g_print ("\n");
 
   /* XXX: UProfs default reporting code now supports dynamic sizing for the Name
    * column, the only thing it's missing is support for adding custom columns but
@@ -111,7 +115,6 @@ print_report (UProfReport *report, UProfContext *context)
 
   g_print ("\n");
   g_print ("Timers:\n");
-  root_timers = uprof_context_get_root_timer_results (context);
   for (l = root_timers; l != NULL; l = l->next)
     uprof_timer_result_print_and_children ((UProfTimerResult *)l->data,
                                            print_timer_fields,