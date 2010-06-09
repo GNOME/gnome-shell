f76dae5ec84780647838212d9184f7a9e665d9b6
Colin Walters <walters@verbum.org>
2010-06-09 16:23:37 -0400

    Add missing shell-window-tracker-private.h file

---
+++ src/shell-window-tracker-private.h
@@ -0,0 +1,9 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+#ifndef __SHELL_WINDOW_TRACKER_PRIVATE_H__
+#define __SHELL_WINDOW_TRACKER_PRIVATE_H__
+
+#include "shell-window-tracker.h"
+
+void _shell_window_tracker_notify_app_state_changed (ShellWindowTracker *tracker, ShellApp *self);
+
+#endif
