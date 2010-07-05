ae43fe9968d901524003b2d19e6d016d8715a6fd
Robert Bragg <robert@linux.intel.com>
2010-07-05 16:17:34 +0100

    conform: don't reference old test_cogl_pixel_buffer symbol
    Since CoglPixelBuffer was renamed to CoglPixelArray the test entry point
    was also renamed to test_cogl_pixel_array, but mistakenly the
    corresponding test-conform-main.c change wasn't pushed at the same time.

--- tests/conform/test-conform-main.c
+++ tests/conform/test-conform-main.c
@@ -204,7 +204,7 @@ main (int argc, char **argv)
   TEST_CONFORM_SIMPLE ("/cogl/texture", test_cogl_multitexture);
   TEST_CONFORM_SIMPLE ("/cogl/texture", test_cogl_texture_mipmaps);
   TEST_CONFORM_SIMPLE ("/cogl/texture", test_cogl_sub_texture);
-  TEST_CONFORM_SIMPLE ("/cogl/texture", test_cogl_pixel_buffer);
+  TEST_CONFORM_SIMPLE ("/cogl/texture", test_cogl_pixel_array);
   TEST_CONFORM_SIMPLE ("/cogl/texture", test_cogl_texture_rectangle);
   TEST_CONFORM_SIMPLE ("/cogl/texture", test_cogl_wrap_modes);
   TEST_CONFORM_SIMPLE ("/cogl/texture", test_cogl_texture_pixmap_x11);
