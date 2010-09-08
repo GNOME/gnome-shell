94c17dba51d9836c0add5f0b4bbd5b2306bfda8a
Emmanuele Bassi <ebassi@linux.intel.com>
2010-09-08 12:23:50 +0100

    build: Update autogen.sh
    Remove the fix-ups for shave and gtk-doc with libtool 1.5: we don't use
    shave any more, and we require libtool 2.2.
    
    Also remove the unnecessary autopoint: we use autoreconf, which calls it
    automatically.

--- autogen.sh
+++ autogen.sh
@@ -17,15 +17,7 @@ if test -z $GTKDOCIZE; then
         echo "*** No gtk-doc support ***"
         echo "EXTRA_DIST =" > gtk-doc.make
 else
-        gtkdocize || exit $?
-        # we need to patch gtk-doc.make to support pretty output with
-        # libtool 1.x.  Should be fixed in the next version of gtk-doc.
-        # To be more resilient with the various versions of gtk-doc one
-        # can find, just sed gkt-doc.make rather than patch it.
-        sed -e 's#) --mode=compile#) --tag=CC --mode=compile#' gtk-doc.make > gtk-doc.temp \
-                && mv gtk-doc.temp gtk-doc.make
-        sed -e 's#) --mode=link#) --tag=CC --mode=link#' gtk-doc.make > gtk-doc.temp \
-                && mv gtk-doc.temp gtk-doc.make
+        ${GTKDOCIZE} || exit $?
 fi
 
 AUTORECONF=`which autoreconf`
@@ -33,10 +25,8 @@ if test -z $AUTORECONF; then
         echo "*** No autoreconf found ***"
         exit 1
 else
-        ACLOCAL="${ACLOCAL-aclocal} $ACLOCAL_FLAGS" autoreconf -v --install || exit $?
+        ACLOCAL="${ACLOCAL-aclocal} $ACLOCAL_FLAGS" ${AUTORECONF} -f -v --install || exit $?
 fi
-
-autopoint --force || exit $?
 
 if test -z "$NOCONFIGURE"; then
         ./configure "$@" && echo "Now type 'make' to compile $PROJECT."
