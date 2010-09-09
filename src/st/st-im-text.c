178c8c50a0ad42fd58496fc70ca17597f8c20754
Maxim Ermilov <zaspire@rambler.ru>
2010-09-09 18:25:30 +0400

    Fix build with recent gtk3
    use new keysyms names
    https://bugzilla.gnome.org/show_bug.cgi?id=629128

--- src/st/st-im-text.c
+++ src/st/st-im-text.c
@@ -231,31 +231,31 @@ key_is_modifier (guint16 keyval)
 
   switch (keyval)
     {
-    case GDK_Shift_L:
-    case GDK_Shift_R:
-    case GDK_Control_L:
-    case GDK_Control_R:
-    case GDK_Caps_Lock:
-    case GDK_Shift_Lock:
-    case GDK_Meta_L:
-    case GDK_Meta_R:
-    case GDK_Alt_L:
-    case GDK_Alt_R:
-    case GDK_Super_L:
-    case GDK_Super_R:
-    case GDK_Hyper_L:
-    case GDK_Hyper_R:
-    case GDK_ISO_Lock:
-    case GDK_ISO_Level2_Latch:
-    case GDK_ISO_Level3_Shift:
-    case GDK_ISO_Level3_Latch:
-    case GDK_ISO_Level3_Lock:
-    case GDK_ISO_Level5_Shift:
-    case GDK_ISO_Level5_Latch:
-    case GDK_ISO_Level5_Lock:
-    case GDK_ISO_Group_Shift:
-    case GDK_ISO_Group_Latch:
-    case GDK_ISO_Group_Lock:
+    case GDK_KEY_Shift_L:
+    case GDK_KEY_Shift_R:
+    case GDK_KEY_Control_L:
+    case GDK_KEY_Control_R:
+    case GDK_KEY_Caps_Lock:
+    case GDK_KEY_Shift_Lock:
+    case GDK_KEY_Meta_L:
+    case GDK_KEY_Meta_R:
+    case GDK_KEY_Alt_L:
+    case GDK_KEY_Alt_R:
+    case GDK_KEY_Super_L:
+    case GDK_KEY_Super_R:
+    case GDK_KEY_Hyper_L:
+    case GDK_KEY_Hyper_R:
+    case GDK_KEY_ISO_Lock:
+    case GDK_KEY_ISO_Level2_Latch:
+    case GDK_KEY_ISO_Level3_Shift:
+    case GDK_KEY_ISO_Level3_Latch:
+    case GDK_KEY_ISO_Level3_Lock:
+    case GDK_KEY_ISO_Level5_Shift:
+    case GDK_KEY_ISO_Level5_Latch:
+    case GDK_KEY_ISO_Level5_Lock:
+    case GDK_KEY_ISO_Group_Shift:
+    case GDK_KEY_ISO_Group_Latch:
+    case GDK_KEY_ISO_Group_Lock:
       return TRUE;
     default:
       return FALSE;
