32fc25a3accadd3f43248aa0132943429c4f855b
Owen W. Taylor <otaylor@fishsoup.net>
2010-11-12 15:52:20 -0500

    StIcon: Adopt copyright notice to standard
    - Replace FSF address with URL
    - Remove explicit Authors: list
    
    https://bugzilla.gnome.org/show_bug.cgi?id=633865

--- src/st/st-icon.c
+++ src/st/st-icon.c
@@ -14,12 +14,7 @@
  * more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Written by: Thomas Wood <thomas.wood@intel.com>,
- *             Chris Lord <chris@linux.intel.com>
- *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
