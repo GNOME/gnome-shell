7d840de18f6bc44287e93112870ed02b614f421b
Neil Roberts <neil@linux.intel.com>
2010-11-30 22:15:40 +0000

    cogl-pipeline: Add more ignored state changes for program generation
    For shader generation backends we don't need to worry about changes to
    the texture object and changing the user matrix. The missing user
    matrix flag was causing test-cogl-multitexture to regenerate the
    shader every frame.

--- cogl/cogl-pipeline-arbfp.c
+++ cogl/cogl-pipeline-arbfp.c
@@ -946,7 +946,10 @@ _cogl_pipeline_backend_arbfp_layer_pre_change_notify (
   CoglPipelineBackendARBfpPrivate *priv;
   static const unsigned long not_fragment_op_changes =
     COGL_PIPELINE_LAYER_STATE_COMBINE_CONSTANT |
-    COGL_PIPELINE_LAYER_STATE_TEXTURE;
+    COGL_PIPELINE_LAYER_STATE_TEXTURE |
+    COGL_PIPELINE_LAYER_STATE_FILTERS |
+    COGL_PIPELINE_LAYER_STATE_WRAP_MODES |
+    COGL_PIPELINE_LAYER_STATE_USER_MATRIX;
 
   priv = get_arbfp_priv (owner);
   if (!priv)
