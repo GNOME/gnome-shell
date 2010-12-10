58a22613877fc041549eddeafbc364d369c5e445
Robert Bragg <robert@linux.intel.com>
2010-12-10 11:34:02 +0000

    matrix-stack: Make header style consistent
    Cogl doesn't follow the Clutter/GTK+ columnized function prototype style
    so this updates cogl-matrix-stack.h to match the other Cogl headers.

--- clutter/cogl/cogl/cogl-matrix-stack.c
+++ clutter/cogl/cogl/cogl-matrix-stack.c
@@ -3,7 +3,7 @@
  *
  * An object oriented GL/GLES Abstraction/Utility Layer
  *
- * Copyright (C) 2009 Intel Corporation.
+ * Copyright (C) 2009,2010 Intel Corporation.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
--- clutter/cogl/cogl/cogl-matrix-stack.h
+++ clutter/cogl/cogl/cogl-matrix-stack.h
@@ -3,7 +3,7 @@
  *
  * An object oriented GL/GLES Abstraction/Utility Layer
  *
- * Copyright (C) 2009 Intel Corporation.
+ * Copyright (C) 2009,2010 Intel Corporation.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,12 +16,14 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ * License along with this library. If not, see
+ * <http://www.gnu.org/licenses/>.
  *
  *
  *
  * Authors:
  *   Havoc Pennington <hp@pobox.com> for litl
+ *   Robert Bragg <robert@linux.intel.com>
  */
 
 #ifndef __COGL_MATRIX_STACK_H
@@ -37,56 +39,78 @@ typedef enum {
   COGL_MATRIX_TEXTURE
 } CoglMatrixMode;
 
-CoglMatrixStack* _cogl_matrix_stack_new           (void);
-void             _cogl_matrix_stack_destroy       (CoglMatrixStack  *stack);
-void             _cogl_matrix_stack_push          (CoglMatrixStack  *stack);
-void             _cogl_matrix_stack_pop           (CoglMatrixStack  *stack);
-void             _cogl_matrix_stack_load_identity (CoglMatrixStack  *stack);
-void             _cogl_matrix_stack_scale         (CoglMatrixStack  *stack,
-                                                   float             x,
-                                                   float             y,
-                                                   float             z);
-void             _cogl_matrix_stack_translate     (CoglMatrixStack  *stack,
-                                                   float             x,
-                                                   float             y,
-                                                   float             z);
-void             _cogl_matrix_stack_rotate        (CoglMatrixStack  *stack,
-                                                   float             angle,
-                                                   float             x,
-                                                   float             y,
-                                                   float             z);
-void             _cogl_matrix_stack_multiply      (CoglMatrixStack  *stack,
-                                                   const CoglMatrix *matrix);
-void             _cogl_matrix_stack_frustum       (CoglMatrixStack  *stack,
-                                                   float             left,
-                                                   float             right,
-                                                   float             bottom,
-                                                   float             top,
-                                                   float             z_near,
-                                                   float             z_far);
-void             _cogl_matrix_stack_perspective   (CoglMatrixStack  *stack,
-                                                   float             fov_y,
-                                                   float             aspect,
-                                                   float             z_near,
-                                                   float             z_far);
-void             _cogl_matrix_stack_ortho         (CoglMatrixStack  *stack,
-                                                   float             left,
-                                                   float             right,
-                                                   float             bottom,
-                                                   float             top,
-                                                   float             z_near,
-                                                   float             z_far);
+CoglMatrixStack *
+_cogl_matrix_stack_new (void);
 
-gboolean         _cogl_matrix_stack_get_inverse   (CoglMatrixStack  *stack,
-                                                   CoglMatrix       *inverse);
-void             _cogl_matrix_stack_get           (CoglMatrixStack  *stack,
-                                                   CoglMatrix       *matrix);
-void             _cogl_matrix_stack_set           (CoglMatrixStack  *stack,
-                                                   const CoglMatrix *matrix);
-void             _cogl_matrix_stack_flush_to_gl   (CoglMatrixStack  *stack,
-                                                   CoglMatrixMode    mode);
-void             _cogl_matrix_stack_dirty         (CoglMatrixStack  *stack);
+void
+_cogl_matrix_stack_destroy (CoglMatrixStack *stack);
 
-unsigned int     _cogl_matrix_stack_get_age       (CoglMatrixStack *stack);
+void
+_cogl_matrix_stack_push (CoglMatrixStack *stack);
+
+void
+_cogl_matrix_stack_pop (CoglMatrixStack *stack);
+
+void
+_cogl_matrix_stack_load_identity (CoglMatrixStack *stack);
+
+void
+_cogl_matrix_stack_scale (CoglMatrixStack *stack,
+                          float x,
+                          float y,
+                          float z);
+void
+_cogl_matrix_stack_translate (CoglMatrixStack *stack,
+                              float x,
+                              float y,
+                              float z);
+void
+_cogl_matrix_stack_rotate (CoglMatrixStack *stack,
+                           float angle,
+                           float x,
+                           float y,
+                           float z);
+void
+_cogl_matrix_stack_multiply (CoglMatrixStack *stack,
+                             const CoglMatrix *matrix);
+void
+_cogl_matrix_stack_frustum (CoglMatrixStack *stack,
+                            float left,
+                            float right,
+                            float bottom,
+                            float top,
+                            float z_near,
+                            float z_far);
+void
+_cogl_matrix_stack_perspective (CoglMatrixStack *stack,
+                                float fov_y,
+                                float aspect,
+                                float z_near,
+                                float z_far);
+void
+_cogl_matrix_stack_ortho (CoglMatrixStack *stack,
+                          float left,
+                          float right,
+                          float bottom,
+                          float top,
+                          float z_near,
+                          float z_far);
+gboolean
+_cogl_matrix_stack_get_inverse (CoglMatrixStack *stack,
+                                CoglMatrix *inverse);
+void
+_cogl_matrix_stack_get (CoglMatrixStack *stack,
+                        CoglMatrix *matrix);
+void
+_cogl_matrix_stack_set (CoglMatrixStack *stack,
+                        const CoglMatrix *matrix);
+void
+_cogl_matrix_stack_flush_to_gl (CoglMatrixStack *stack,
+                                CoglMatrixMode mode);
+void
+_cogl_matrix_stack_dirty (CoglMatrixStack  *stack);
+
+unsigned int
+_cogl_matrix_stack_get_age (CoglMatrixStack *stack);
 
 #endif /* __COGL_MATRIX_STACK_H */
