d648de7d1492b9ae69aac6702a99fccb8f92b165
Neil Roberts <neil@linux.intel.com>
2010-12-14 14:44:45 +0000

    cogl-journal: Avoid enabling blending if possible
    The vertex attribute API assumes that if there is a color array
    enabled then we can't determine if the colors are opaque so we have to
    enable blending. The journal always uses a color array to avoid
    switching color state between rectangles. Since the journal switched
    to using vertex attributes this means we effectively always enable
    blending from the journal. To fix this there is now a new flag for
    _cogl_draw_vertex_attributes to specify that the color array is known
    to only contain opaque colors which causes the draw function not to
    copy the pipeline. If the pipeline has blending disabled then the
    journal passes this flag.
    
    http://bugzilla.clutter-project.org/show_bug.cgi?id=2481

--- clutter/cogl/cogl/cogl-vertex-attribute-private.h
+++ clutter/cogl/cogl/cogl-vertex-attribute-private.h
@@ -61,7 +61,14 @@ typedef enum
 {
   COGL_DRAW_SKIP_JOURNAL_FLUSH = 1 << 0,
   COGL_DRAW_SKIP_PIPELINE_VALIDATION = 1 << 1,
-  COGL_DRAW_SKIP_FRAMEBUFFER_FLUSH = 1 << 2
+  COGL_DRAW_SKIP_FRAMEBUFFER_FLUSH = 1 << 2,
+  /* By default the vertex attribute drawing code will assume that if
+     there is a color attribute array enabled then we can't determine
+     if the colors will be opaque so we need to enabling
+     blending. However when drawing from the journal we know what the
+     contents of the color array is so we can override this by passing
+     this flag. */
+  COGL_DRAW_COLOR_ATTRIBUTE_IS_OPAQUE = 1 << 3
 } CoglDrawFlags;
 
 CoglVertexAttribute *
