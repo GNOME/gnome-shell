b922ac605962df6b1cf86a0e710294be114f62c4
Damien Lespiau <damien.lespiau@intel.com>
2011-01-11 14:15:46 +0000

    test-shader: Fix the edge-detect shader
    cogl_text_coord_in in a array to access the texture units. In this case
    we want to sample texture unit 0, where the texture is.

--- tests/interactive/test-shader.c
+++ tests/interactive/test-shader.c
@@ -144,7 +144,7 @@ static ShaderSource shaders[]=
      FRAGMENT_SHADER_VARS
      "float get_avg_rel(sampler2D texB, float dx, float dy)"
      "{"
-     "  vec4 colorB = texture2D (texB, cogl_tex_coord_in.st + vec2(dx, dy));"
+     "  vec4 colorB = texture2D (texB, cogl_tex_coord_in[0].st + vec2(dx, dy));"
      "  return (colorB.r + colorB.g + colorB.b) / 3.0;"
      "}"
      FRAGMENT_SHADER_BEGIN
