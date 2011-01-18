ee15d0d2d01a02e426cbb8c340738e4f8411fc4f
Emmanuele Bassi <ebassi@linux.intel.com>
2011-01-18 13:05:17 +0000

    Add copyright and licensing blurbs

--- clutter/x11/clutter-input-device-xi2.h
+++ clutter/x11/clutter-input-device-xi2.h
@@ -1,3 +1,26 @@
+/*
+ * Clutter.
+ *
+ * An OpenGL based 'interactive canvas' library.
+ *
+ * Copyright © 2011  Intel Corp.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Emmanuele Bassi <ebassi@linux.intel.com>
+ */
+
 #ifndef __CLUTTER_INPUT_DEVICE_XI2_H__
 #define __CLUTTER_INPUT_DEVICE_XI2_H__
 
