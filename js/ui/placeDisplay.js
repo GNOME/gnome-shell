GNOME/gnome-shell

commit 0d32017ffc6813546ba68c484c3902213df9bd84

Don't switch to a workspace when dragging it to launch on that workspace
With workspace thumbnails, we don't switch workspaces when dragging windows
between workspaces or adding new workspaces, so we also shouldn't switch
on launch.

 * Add workspace parameters to shell_doc_system_open(),
   shell_app_activate, shell_app_open_new_window()

 * Pass a 'params' object when activating items in the overview with
   two currently defined parameters: workspace and timestamp. (timestamp
   is only implemented where it is easy and doesn't require interface
   changes - using the global current timestamp for the shell is almost
   always right or at least good enough.)

https://bugzilla.gnome.org/show_bug.cgi?id=640996

js/ui/placeDisplay.js
@@ -12,6 +12,7 @@ const _ = Gettext.gettext;
 
 const DND = imports.ui.dnd;
 const Main = imports.ui.main;
+const Params = imports.misc.params;
 const Search = imports.ui.search;
 const Util = imports.misc.util;
 
@@ -58,6 +59,21 @@ PlaceInfo.prototype = {
     }
 };
 
+// Helper function to translate launch parameters into a GAppLaunchContext
+function _makeLaunchContext(params)
+{
+    params = Params.parse(params, { workspace: null,
+                                    timestamp: null });
+
+    let launchContext = global.create_app_launch_context();
+    if (params.workspace != null)
+        launchContext.set_desktop(params.workspace.index());
+    if (params.timestamp != null)
+        launchContext.set_timestamp(params.timestamp);
+
+    return launchContext;
+}
+
 function PlaceDeviceInfo(mount) {
     this._init(mount);
 }
@@ -77,9 +93,9 @@ PlaceDeviceInfo.prototype = {
         return St.TextureCache.get_default().load_gicon(null, icon, size);
     },
 
-    launch: function() {
+    launch: function(param) {
         Gio.app_info_launch_default_for_uri(this._mount.get_root().get_uri(),
-                                            global.create_app_launch_context());
+                                            _makeLaunchContex(params));
     },
 
     isRemovable: function() {
@@ -111,7 +127,6 @@ PlaceDeviceInfo.prototype = {
     }
 };
 
-
 function PlacesManager() {
     this._init();
 }
@@ -130,8 +145,8 @@ PlacesManager.prototype = {
             function(size) {
                 return St.TextureCache.get_default().load_gicon(null, homeIcon, size);
             },
-            function() {
-                Gio.app_info_launch_default_for_uri(homeUri, global.create_app_launch_context());
+            function(params) {
+                Gio.app_info_launch_default_for_uri(homeUri, _makeLaunchContext(params));
             });
 
         let desktopPath = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DESKTOP);
@@ -143,8 +158,8 @@ PlacesManager.prototype = {
             function(size) {
                 return St.TextureCache.get_default().load_gicon(null, desktopIcon, size);
             },
-            function() {
-                Gio.app_info_launch_default_for_uri(desktopUri, global.create_app_launch_context());
+            function(params) {
+                Gio.app_info_launch_default_for_uri(desktopUri, _makeLaunchContext(params));
             });
 
         this._connect = new PlaceInfo('special:connect', _("Connect to..."),
@@ -153,7 +168,11 @@ PlacesManager.prototype = {
                                      icon_type: St.IconType.FULLCOLOR,
                                      icon_size: size });
             },
-            function () {
+            function (params) {
+                // BUG: nautilus-connect-server doesn't have a desktop file, so we can't
+                // launch it with the workspace from params. It's probably pretty rare
+                // and odd to drag this place onto a workspace in any case
+
                 Util.spawn(['nautilus-connect-server']);
             });
 
@@ -173,8 +192,12 @@ PlacesManager.prototype = {
                 function(size) {
                     return networkApp.create_icon_texture(size);
                 },
-                function () {
-                    networkApp.launch();
+                function (params) {
+                    params = Params.parse(params, { workspace: null,
+                                                    timestamp: 0 });
+
+                    networkApp.launch_full(params.timestamp, [],
+                                           params.workspace ? params.workspace.index() : -1);
                 });
         }
 
@@ -314,8 +337,8 @@ PlacesManager.prototype = {
                 function(size) {
                     return St.TextureCache.get_default().load_gicon(null, icon, size);
                 },
-                function() {
-                    Gio.app_info_launch_default_for_uri(bookmark, global.create_app_launch_context());
+                function(params) {
+                    Gio.app_info_launch_default_for_uri(bookmark, _makeLaunchContext(params));
                 });
             this._bookmarks.push(item);
         }
@@ -395,9 +418,9 @@ PlaceSearchProvider.prototype = {
                  'icon': placeInfo.iconFactory(Search.RESULT_ICON_SIZE) };
     },
 
-    activateResult: function(id) {
+    activateResult: function(id, params) {
         let placeInfo = Main.placesManager.lookupPlaceById(id);
-        placeInfo.launch();
+        placeInfo.launch(params);
     },
 
     _compareResultMeta: function (idA, idB) {

js/misc/docInfo.js
@@ -29,8 +29,8 @@ DocInfo.prototype = {
         return St.TextureCache.get_default().load_recent_thumbnail(size, this.recentInfo);
     },
 
-    launch : function() {
-        Shell.DocSystem.get_default().open(this.recentInfo);
+    launch : function(workspaceIndex) {
+        Shell.DocSystem.get_default().open(this.recentInfo, workspaceIndex);
     },
 
     matchTerms: function(terms) {

js/ui/appDisplay.js
@@ -223,14 +223,20 @@ BaseAppSearchProvider.prototype = {
                  'icon': app.create_icon_texture(Search.RESULT_ICON_SIZE)};
     },
 
-    activateResult: function(id) {
+    activateResult: function(id, params) {
+        params = Params.parse(params, { workspace: null,
+                                        timestamp: null });
+
         let app = this._appSys.get_app(id);
-        app.activate();
+        app.activate(params.workspace ? params.workspace.index() : -1);
     },
 
-    dragActivateResult: function(id) {
+    dragActivateResult: function(id, params) {
+        params = Params.parse(params, { workspace: null,
+                                        timestamp: null });
+
         let app = this._appSys.get_app(id);
-        app.open_new_window();
+        app.open_new_window(params.workspace ? params.workspace.get_index() : -1);
     }
 };
 
@@ -401,7 +407,7 @@ AppWellIcon.prototype = {
             let launchWorkspace = global.screen.get_workspace_by_index(global.screen.n_workspaces - 1);
             launchWorkspace.activate(global.get_current_time());
             this.emit('launching');
-            this.app.open_new_window();
+            this.app.open_new_window(-1);
             Main.overview.hide();
         }
         return false;
@@ -486,9 +492,9 @@ AppWellIcon.prototype = {
 
         if (modifiers & Clutter.ModifierType.CONTROL_MASK
             && this.app.state == Shell.AppState.RUNNING) {
-            this.app.open_new_window();
+            this.app.open_new_window(-1);
         } else {
-            this.app.activate();
+            this.app.activate(-1);
         }
         Main.overview.hide();
     },
@@ -498,8 +504,11 @@ AppWellIcon.prototype = {
         return this._menu.menuEventFilter(event);
     },
 
-    shellWorkspaceLaunch : function() {
-        this.app.open_new_window();
+    shellWorkspaceLaunch : function(params) {
+        params = Params.parse(params, { workspace: null,
+                                        timestamp: null });
+
+        this.app.open_new_window(params.workspace ? params.workspace.index() : -1);
     },
 
     getDragActor: function() {
@@ -668,7 +677,7 @@ AppIconMenu.prototype = {
             let metaWindow = child._window;
             this.emit('activate-window', metaWindow);
         } else if (child == this._newWindowMenuItem) {
-            this._source.app.open_new_window();
+            this._source.app.open_new_window(-1);
             this.emit('activate-window', null);
         } else if (child == this._toggleFavoriteMenuItem) {
             let favs = AppFavorites.getAppFavorites();

js/ui/docDisplay.js
@@ -4,6 +4,7 @@ const Gettext = imports.gettext.domain('gnome-shell');
 const _ = Gettext.gettext;
 
 const DocInfo = imports.misc.docInfo;
+const Params = imports.misc.params;
 const Search = imports.ui.search;
 
 
@@ -28,9 +29,12 @@ DocSearchProvider.prototype = {
                  'icon': docInfo.createIcon(Search.RESULT_ICON_SIZE)};
     },
 
-    activateResult: function(id) {
+    activateResult: function(id, params) {
+        params = Params.parse(params, { workspace: null,
+                                        timestamp: null });
+
         let docInfo = this._docManager.lookupByUri(id);
-        docInfo.launch();
+        docInfo.launch(params.workspace ? params.workspace.index() : -1);
     },
 
     getInitialResultSet: function(terms) {

js/ui/endSessionDialog.js
@@ -171,7 +171,7 @@ ListItem.prototype = {
 
     _onClicked: function() {
         this.emit('activate');
-        this._app.activate();
+        this._app.activate(-1);
     }
 };
 Signals.addSignalMethods(ListItem.prototype);

js/ui/search.js
@@ -267,7 +267,7 @@ OpenSearchSystem.prototype = {
         return lang != null;
     },
 
-    activateResult: function(id) {
+    activateResult: function(id, params) {
         let searchTerms = this._terms.join(' ');
 
         let url = this._providers[id].url.replace('{searchTerms}', encodeURIComponent(searchTerms));

js/ui/searchDisplay.js
@@ -81,11 +81,11 @@ SearchResult.prototype = {
         return new Clutter.Clone({ source: this.metaInfo['icon'] });
     },
 
-    shellWorkspaceLaunch: function() {
+    shellWorkspaceLaunch: function(params) {
         if (this.provider.dragActivateResult)
-            this.provider.dragActivateResult(this.metaInfo.id);
+            this.provider.dragActivateResult(this.metaInfo.id, params);
         else
-            this.provider.activateResult(this.metaInfo.id);
+            this.provider.activateResult(this.metaInfo.id, params);
     }
 };
 

js/ui/workspace.js
@@ -1431,8 +1431,8 @@ Workspace.prototype = {
                                                  time);
             return true;
         } else if (source.shellWorkspaceLaunch) {
-            this.metaWorkspace.activate(time);
-            source.shellWorkspaceLaunch();
+            source.shellWorkspaceLaunch({ workspace: this.metaWorkspace,
+                                          timestamp: time });
             return true;
         }
 

commit 03729a71f49382cadd8fe7372c25626959cf4339

placeDisplay: Fix typo preventing places from being launched
0d32017ffc6813546ba68c484c3902213df9bd84 accidentally introduced
a typo when launching places.

https://bugzilla.gnome.org/show_bug.cgi?id=642207

js/ui/placeDisplay.js
@@ -95,7 +95,7 @@ PlaceDeviceInfo.prototype = {
 
     launch: function(param) {
         Gio.app_info_launch_default_for_uri(this._mount.get_root().get_uri(),
-                                            _makeLaunchContex(params));
+                                            _makeLaunchContext(params));
     },
 
     isRemovable: function() {