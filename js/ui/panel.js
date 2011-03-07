797368bf9f0b985f12c4c4c7ceb39fc912d9f589
Dan Winship <danw@gnome.org>
2011-03-07 13:06:34 -0500

    Revert "panel: remove accessibility icon"
    pending further discussion in
    https://bugzilla.gnome.org/show_bug.cgi?id=639762
    
    This reverts commit 7790a07c08c870e3ef04715f3ed6c60bb6dc8a09.

--- js/ui/panel.js
+++ js/ui/panel.js
@@ -38,7 +38,7 @@ const SPINNER_SPEED = 0.02;
 
 const STANDARD_TRAY_ICON_ORDER = ['a11y', 'display', 'keyboard', 'volume', 'bluetooth', 'network', 'battery'];
 const STANDARD_TRAY_ICON_SHELL_IMPLEMENTATION = {
-    // 'a11y': imports.ui.status.accessibility.ATIndicator,
+    'a11y': imports.ui.status.accessibility.ATIndicator,
     'volume': imports.ui.status.volume.Indicator,
     'battery': imports.ui.status.power.Indicator,
     'keyboard': imports.ui.status.keyboard.XKBIndicator
