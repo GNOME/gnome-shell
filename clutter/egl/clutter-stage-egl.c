951cb82ec5e76a6e67b0cb7c2050d6ea5703d4ba
Robert Bragg <robert@linux.intel.com>
2011-05-09 01:09:39 +0100

    egl: fix args to cogl_onscreen_x11_set_foreign_window_xid
    We hadn't updated the egl backend inline with a change to the arguments
    that cogl_onscreen_x11_set_foreign_window_xid would expect and that was
    causing a compilation error.

--- clutter/egl/clutter-stage-egl.c
+++ clutter/egl/clutter-stage-egl.c
@@ -78,8 +78,14 @@ clutter_stage_egl_realize (ClutterStageWindow *stage_window)
                                            width, height);
 #ifdef COGL_HAS_XLIB_SUPPORT
   if (stage_x11->xwin != None)
-    cogl_onscreen_x11_set_foreign_window_xid (stage_egl->onscreen,
-                                              stage_x11->xwin);
+    {
+      cogl_onscreen_x11_set_foreign_window_xid (
+                                  stage_egl->onscreen,
+                                  stage_x11->xwin,
+                                  _clutter_stage_x11_update_foreign_event_mask,
+                                  stage_x11);
+
+    }
 #endif
 
   clutter_vblank = _clutter_backend_egl_get_vblank ();
