71d0872b76ec6678f03fddf4731f394eaa06ba9f
Robert Bragg <robert@linux.intel.com>
2011-07-01 01:44:13 +0100

    don't call cogl_set_default_context since it's been removed
    cogl_set_default_context has been removed from Cogl so this updates
    Clutter in-line with that change.

--- clutter/win32/clutter-backend-win32.c
+++ clutter/win32/clutter-backend-win32.c
@@ -252,10 +252,6 @@ clutter_backend_win32_create_context (ClutterBackend  *backend,
   if (!backend->cogl_context)
     goto error;
 
-  /* XXX: eventually this should go away but a lot of Cogl code still
-   * depends on a global default context. */
-  cogl_set_default_context (backend->cogl_context);
-
   return TRUE;
 
 error:
