4831d9c3a3b480fb4496a678ce5af79f650dd73c
Jasper St. Pierre <jstpierre@mecheye.net>
2011-10-04 16:50:51 -0400

    lookingGlass: Fix referencing undefined variables

--- js/ui/lookingGlass.js
+++ js/ui/lookingGlass.js
@@ -159,7 +159,7 @@ function objectToString(o) {
         // special case this since the default is way, way too verbose
         return '<js function>';
     } else {
-        return o.toString();
+        return '' + o;
     }
 }
 
