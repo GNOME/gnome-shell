92585be4d805a1cb0b60df92a0b235fd1a2824be
Emmanuele Bassi <ebassi@gnome.org>
2011-11-18 17:32:01 +0000

    debug: Remove CLUTTER_GLERR
    The macro hasn't been used in a long while, as Cogl had its own version
    of it; and Clutter hasn't been using pure GL for some time now.

--- clutter/clutter-debug.h
+++ clutter/clutter-debug.h
@@ -71,18 +71,9 @@ typedef enum {
           }                                           } G_STMT_END
 #endif
 
-#define CLUTTER_GLERR()                         G_STMT_START {  \
-        if (clutter_debug_flags & CLUTTER_DEBUG_GL) {           \
-          GLenum _err = glGetError (); /* roundtrip */          \
-          if (_err != GL_NO_ERROR)                              \
-            g_warning (G_STRLOC ": GL Error %x", _err);         \
-        }                                       } G_STMT_END
-
-
 #else /* !CLUTTER_ENABLE_DEBUG */
 
 #define CLUTTER_NOTE(type,...)         G_STMT_START { } G_STMT_END
-#define CLUTTER_GLERR()                G_STMT_START { } G_STMT_END
 #define CLUTTER_HAS_DEBUG(type)        FALSE
 
 #endif /* CLUTTER_ENABLE_DEBUG */
