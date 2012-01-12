1bfd1d85eb2dd4e2093c5c7e6833da4196110177
Robert Bragg <robert@linux.intel.com>
2012-01-12 20:36:16 +0000

    Updates in line with latest wayland protocol
    The shm buffer format enum values were renamed and the explicitly
    premultiplied format was dropped since it's now assumed if the buffer
    has an alpha component then it's premultiplied.
    
    Reviewed-by: Neil Roberts <neil@linux.intel.com>
    Reviewed-by: Emmanuele Bassi <ebassi@linux.intel.com>

--- clutter/wayland/clutter-wayland-surface.c
+++ clutter/wayland/clutter-wayland-surface.c
@@ -577,19 +577,17 @@ clutter_wayland_surface_damage_buffer (ClutterWaylandSurface *self,
       switch (wl_shm_buffer_get_format (buffer))
         {
 #if G_BYTE_ORDER == G_BIG_ENDIAN
-          case WL_SHM_FORMAT_PREMULTIPLIED_ARGB32:
+          case WL_SHM_FORMAT_ARGB8888:
             format = COGL_PIXEL_FORMAT_ARGB_8888_PRE;
             break;
-          case WL_SHM_FORMAT_ARGB32:
-          case WL_SHM_FORMAT_XRGB32:
+          case WL_SHM_FORMAT_XRGB8888:
             format = COGL_PIXEL_FORMAT_ARGB_8888;
             break;
 #elif G_BYTE_ORDER == G_LITTLE_ENDIAN
-          case WL_SHM_FORMAT_PREMULTIPLIED_ARGB32:
+          case WL_SHM_FORMAT_ARGB8888:
             format = COGL_PIXEL_FORMAT_BGRA_8888_PRE;
             break;
-          case WL_SHM_FORMAT_ARGB32:
-          case WL_SHM_FORMAT_XRGB32:
+          case WL_SHM_FORMAT_XRGB8888:
             format = COGL_PIXEL_FORMAT_BGRA_8888;
             break;
 #endif
