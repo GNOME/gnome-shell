GNOME/gnome-shell

commit 26991988cb76a9bd2f43700a98212099bdb023fb

browser-plugin: Refactor plugin_object_set_property, and fix a bug
If the user did "obj.onchanged = 1;" or passed another sort of invalid type,
then we would clear the old listener as well as throw an exception.

https://bugzilla.gnome.org/show_bug.cgi?id=668517

browser-plugin/browser-plugin.c
@@ -810,48 +810,37 @@ plugin_object_get_property (NPObject     *npobj,
 }
 
 static bool
+plugin_object_set_callback (NPObject        **listener,
+                            const NPVariant  *value)
+{
+  if (!NPVARIANT_IS_OBJECT (*value) && !NPVARIANT_IS_NULL (*value))
+    return FALSE;
+
+  if (*listener)
+    funcs.releaseobject (*listener);
+  *listener = NULL;
+
+  if (NPVARIANT_IS_OBJECT (*value))
+    {
+      funcs.retainobject (*listener);
+      *listener = NPVARIANT_TO_OBJECT (*value);
+    }
+
+  return TRUE;
+}
+
+static bool
 plugin_object_set_property (NPObject        *npobj,
                             NPIdentifier     name,
                             const NPVariant *value)
 {
   PluginObject *obj;
 
-  if (!plugin_object_has_property (npobj, name))
-    return FALSE;
-
   if (name == onextension_changed_id)
-    {
-      obj = (PluginObject*) npobj;
-      if (obj->listener)
-        funcs.releaseobject (obj->listener);
-
-      obj->listener = NULL;
-      if (NPVARIANT_IS_OBJECT (*value))
-        {
-          obj->listener = NPVARIANT_TO_OBJECT (*value);
-          funcs.retainobject (obj->listener);
-          return TRUE;
-        }
-      else if (NPVARIANT_IS_NULL (*value))
-        return TRUE;
-    }
+    return plugin_object_set_callback (&obj->listener, value);
 
   if (name == onrestart_id)
-    {
-      obj = (PluginObject*) npobj;
-      if (obj->restart_listener)
-        funcs.releaseobject (obj->restart_listener);
-
-      obj->restart_listener = NULL;
-      if (NPVARIANT_IS_OBJECT (*value))
-        {
-          obj->restart_listener = NPVARIANT_TO_OBJECT (*value);
-          funcs.retainobject (obj->restart_listener);
-          return TRUE;
-        }
-      else if (NPVARIANT_IS_NULL (*value))
-        return TRUE;
-    }
+    return plugin_object_set_callback (&obj->restart_listener, value);
 
   return FALSE;
 }

commit 2c9e6bb589e0063e89a9e2730b4323282958af69

browser-plugin: Fix the browser plugin
commit 26991988cb76a9bd2f43700a98212099bdb023fb broke the browser plugin
by trying to reference a uninitialized pointer and making the NPAPI retain
a NULL object.

https://bugzilla.gnome.org/show_bug.cgi?id=668517

browser-plugin/browser-plugin.c
@@ -822,8 +822,8 @@ plugin_object_set_callback (NPObject        **listener,
 
   if (NPVARIANT_IS_OBJECT (*value))
     {
-      funcs.retainobject (*listener);
       *listener = NPVARIANT_TO_OBJECT (*value);
+      funcs.retainobject (*listener);
     }
 
   return TRUE;
@@ -835,6 +835,8 @@ plugin_object_set_property (NPObject        *npobj,
                             const NPVariant *value)
 {
   PluginObject *obj;
+
+  obj = (PluginObject *)npobj;
 
   if (name == onextension_changed_id)
     return plugin_object_set_callback (&obj->listener, value);