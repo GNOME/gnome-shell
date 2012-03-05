GNOME/gnome-shell

commit d856338f863f9c8ccb6a691a657955a2940f0891

bluetooth: Fix undefined variable issue
https://bugzilla.gnome.org/show_bug.cgi?id=663891

js/ui/status/bluetooth.js
@@ -204,9 +204,10 @@ Indicator.prototype = {
 
     _buildDeviceSubMenu: function(item, device) {
         if (device.can_connect) {
+            let menuitem = new PopupMenu.PopupSwitchMenuItem(_("Connection"), device.connected);
             item._connected = device.connected;
-            item._connectedMenuitem = new PopupMenu.PopupSwitchMenuItem(_("Connection"), device.connected);
-            item._connectedMenuitem.connect('toggled', Lang.bind(this, function() {
+            item._connectedMenuItem = menuitem;
+            menuitem.connect('toggled', Lang.bind(this, function() {
                 if (item._connected > ConnectionState.CONNECTED) {
                     // operation already in progress, revert
                     // (should not happen anyway)
@@ -241,7 +242,7 @@ Indicator.prototype = {
                 }
             }));
 
-            item.menu.addMenuItem(item._connectedMenuitem);
+            item.menu.addMenuItem(menuitem);
         }
 
         if (device.capabilities & GnomeBluetoothApplet.Capabilities.OBEX_PUSH) {

commit f32ab20267e7279777b4fa1aaaec5a9653a9b75f

bluetooth: Fix connectedMenuItem variable case.
This fixes a regression introduced in d856338f86.

js/ui/status/bluetooth.js
@@ -181,7 +181,7 @@ const Indicator = new Lang.Class({
 
         // update connected property
         if (device.can_connect)
-            item._connectedMenuitem.setToggleState(device.connected);
+            item._connectedMenuItem.setToggleState(device.connected);
     },
 
     _createDeviceItem: function(device) {