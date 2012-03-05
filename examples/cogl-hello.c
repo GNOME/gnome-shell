ae091f2cbbae3b7035d0c8aeffbf604834e83932
Robert Bragg <robert@linux.intel.com>
2012-03-05 12:24:38 +0000

    cogl-hello: use glib mainloop for portability
    To be a portable example this updates cogl-hello to use the glib
    mainloop instead of using g_poll. On OSX we plan to provide custom
    mainloop integration for glib which can't be abstracted by just using
    g_poll.

--- examples/cogl-hello.c
+++ examples/cogl-hello.c
@@ -2,51 +2,80 @@
 #include <glib.h>
 #include <stdio.h>
 
-CoglColor black;
+typedef struct _Data
+{
+    CoglContext *ctx;
+    CoglFramebuffer *fb;
+    CoglPrimitive *triangle;
+    CoglPipeline *pipeline;
+} Data;
+
+static gboolean
+paint_cb (void *user_data)
+{
+    Data *data = user_data;
+
+    cogl_framebuffer_clear4f (data->fb, COGL_BUFFER_BIT_COLOR, 0, 0, 0, 1);
+    cogl_framebuffer_draw_primitive (data->fb, data->pipeline, data->triangle);
+    cogl_onscreen_swap_buffers (COGL_ONSCREEN (data->fb));
+
+    /* If the driver can deliver swap complete events then we can remove
+     * the idle paint callback until we next get a swap complete event
+     * otherwise we keep the idle paint callback installed and simply
+     * paint as fast as the driver will allow... */
+    if (cogl_has_feature (data->ctx, COGL_FEATURE_ID_SWAP_BUFFERS_EVENT))
+      return FALSE; /* remove the callback */
+    else
+      return TRUE;
+}
+
+static void
+swap_complete_cb (CoglFramebuffer *framebuffer, void *user_data)
+{
+    g_idle_add (paint_cb, user_data);
+}
 
 int
 main (int argc, char **argv)
 {
-    CoglContext *ctx;
+    Data data;
     CoglOnscreen *onscreen;
-    CoglFramebuffer *fb;
-    CoglPipeline *pipeline;
     GError *error = NULL;
     CoglVertexP2C4 triangle_vertices[] = {
         {0, 0.7, 0xff, 0x00, 0x00, 0x80},
         {-0.7, -0.7, 0x00, 0xff, 0x00, 0xff},
         {0.7, -0.7, 0x00, 0x00, 0xff, 0xff}
     };
-    CoglPrimitive *triangle;
+    GSource *cogl_source;
+    GMainLoop *loop;
 
-    ctx = cogl_context_new (NULL, &error);
-    if (!ctx) {
+    data.ctx = cogl_context_new (NULL, &error);
+    if (!data.ctx) {
         fprintf (stderr, "Failed to create context: %s\n", error->message);
         return 1;
     }
 
-    onscreen = cogl_onscreen_new (ctx, 640, 480);
+    onscreen = cogl_onscreen_new (data.ctx, 640, 480);
     cogl_onscreen_show (onscreen);
-    fb = COGL_FRAMEBUFFER (onscreen);
+    data.fb = COGL_FRAMEBUFFER (onscreen);
 
-    triangle = cogl_primitive_new_p2c4 (ctx, COGL_VERTICES_MODE_TRIANGLES,
-                                        3, triangle_vertices);
+    data.triangle = cogl_primitive_new_p2c4 (data.ctx,
+                                             COGL_VERTICES_MODE_TRIANGLES,
+                                             3, triangle_vertices);
+    data.pipeline = cogl_pipeline_new (data.ctx);
 
-    pipeline = cogl_pipeline_new (ctx);
+    cogl_source = cogl_glib_source_new (data.ctx, G_PRIORITY_DEFAULT);
 
-    for (;;) {
-        CoglPollFD *poll_fds;
-        int n_poll_fds;
-        gint64 timeout;
+    g_source_attach (cogl_source, NULL);
 
-        cogl_framebuffer_clear4f (fb, COGL_BUFFER_BIT_COLOR, 0, 0, 0, 1);
-        cogl_framebuffer_draw_primitive (fb, pipeline, triangle);
-        cogl_onscreen_swap_buffers (onscreen);
+    if (cogl_has_feature (data.ctx, COGL_FEATURE_ID_SWAP_BUFFERS_EVENT))
+      cogl_onscreen_add_swap_buffers_callback (COGL_ONSCREEN (data.fb),
+                                               swap_complete_cb, &data);
 
-        cogl_poll_get_info (ctx, &poll_fds, &n_poll_fds, &timeout);
-        g_poll ((GPollFD *) poll_fds, n_poll_fds, 0);
-        cogl_poll_dispatch (ctx, poll_fds, n_poll_fds);
-    }
+    g_idle_add (paint_cb, &data);
+
+    loop = g_main_loop_new (NULL, TRUE);
+    g_main_loop_run (loop);
 
     return 0;
 }
