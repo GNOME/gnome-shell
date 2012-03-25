f563fb124ec9dae29695daefba6f543981f5f36d
Jasper St. Pierre <jstpierre@mecheye.net>
2012-03-25 07:07:15 -0300

    shell-gtk-embed: Fix NULL pointer dereference
    Clutter will try to unmap during a dispose if we have a parent, so if we
    set our own actor to NULL before the chain up, we're going to attempt to
    unmap our own NULL actor. Fix that by swapping the order in which we
    chain up.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=672790

--- src/shell-gtk-embed.c
+++ src/shell-gtk-embed.c
@@ -223,9 +223,9 @@ shell_gtk_embed_dispose (GObject *object)
 {
   ShellGtkEmbed *embed = SHELL_GTK_EMBED (object);
 
-  shell_gtk_embed_set_window (embed, NULL);
-
   G_OBJECT_CLASS (shell_gtk_embed_parent_class)->dispose (object);
+
+  shell_gtk_embed_set_window (embed, NULL);
 }
 
 static void
