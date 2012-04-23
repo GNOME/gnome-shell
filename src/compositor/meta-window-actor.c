65f2e2937544c7de68c3b3e4dfbe60c2be1e3286
Jasper St. Pierre <jstpierre@mecheye.net>
2012-04-23 18:54:46 -0400

    window-actor: Make the meta-window property construct-only
    We don't deal with setting this property at all, and it's unlikely
    to come up in future settings, so just don't allow setting it at all.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=678989

--- src/compositor/meta-window-actor.c
+++ src/compositor/meta-window-actor.c
@@ -226,7 +226,7 @@ meta_window_actor_class_init (MetaWindowActorClass *klass)
                                "MetaWindow",
                                "The displayed MetaWindow",
                                META_TYPE_WINDOW,
-                               G_PARAM_READWRITE | G_PARAM_CONSTRUCT);
+                               G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY);
 
   g_object_class_install_property (object_class,
                                    PROP_META_WINDOW,
@@ -488,11 +488,7 @@ meta_window_actor_set_property (GObject      *object,
   switch (prop_id)
     {
     case PROP_META_WINDOW:
-      {
-        if (priv->window)
-          g_object_unref (priv->window);
-        priv->window = g_value_dup_object (value);
-      }
+      priv->window = g_value_dup_object (value);
       break;
     case PROP_NO_SHADOW:
       {
