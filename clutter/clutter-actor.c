a2f54c59f5de9d9a577e1de44c2002c160ecc13b
Emmanuele Bassi <ebassi@linux.intel.com>
2012-04-26 14:02:07 +0100

    Revert "actor: Unconditionally set show_on_set_parent"
    This reverts commit 81b19a78f5e02ae8ffd9235054d80b172496415b.
    
    The commit breaks the conformance test unit for the invariants we
    guarantee for the 1.x API:
    
    ERROR:actor-invariants.c:307:actor_show_on_set_parent: assertion failed: (show_on_set_parent)

--- clutter/clutter-actor.c
+++ clutter/clutter-actor.c
@@ -1554,9 +1554,12 @@ set_show_on_set_parent (ClutterActor *self,
   if (priv->show_on_set_parent == set_show)
     return;
 
-  priv->show_on_set_parent = set_show;
-  g_object_notify_by_pspec (G_OBJECT (self),
-                            obj_props[PROP_SHOW_ON_SET_PARENT]);
+  if (priv->parent == NULL)
+    {
+      priv->show_on_set_parent = set_show;
+      g_object_notify_by_pspec (G_OBJECT (self),
+                                obj_props[PROP_SHOW_ON_SET_PARENT]);
+    }
 }
 
 /**
