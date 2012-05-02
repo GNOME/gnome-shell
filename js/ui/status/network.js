GNOME/gnome-shell

commit 723a1c843a9d573fdd94403c08d4b72c2dac19b1

Refactor show()/hide() sequences
We seem to have a lot of code that does something along the lines of:

    if (condition)
        actor.show();
    else
        actor.hide();

ClutterActor already has such a thing for exactly this purpose: the 'visible'
property. Use it instead of the mess above.

https://bugzilla.gnome.org/show_bug.cgi?id=672272

js/ui/status/network.js
@@ -707,10 +707,7 @@ const NMDeviceWired = new Lang.Class({
         // the device
         // we can do it here because addConnection and removeConnection
         // both call _createSection at some point
-        if (this._connections.length <= 1)
-            this.section.actor.hide();
-        else
-            this.section.actor.show();
+        this.section.actor.visible = this._connections.length <= 1;
     },
 
     _createAutomaticConnection: function() {
@@ -1038,13 +1035,8 @@ const NMDeviceWireless = new Lang.Class({
     },
 
     setEnabled: function(enabled) {
-        if (enabled) {
-            this.statusItem.actor.show();
-            this.section.actor.show();
-        } else {
-            this.statusItem.actor.hide();
-            this.section.actor.hide();
-        }
+        this.statusItem.actor.visible = enabled;
+        this.section.actor.visible = enabled;
     },
 
     activate: function() {

js/gdm/powerMenu.js
@@ -60,10 +60,8 @@ const PowerMenuButton = new Lang.Class({
     },
 
     _updateVisibility: function() {
-        if (!this._haveSuspend && !this._haveShutdown && !this._haveRestart)
-            this.actor.hide();
-        else
-            this.actor.show();
+        let shouldBeVisible = (this._haveSuspend || this._haveShutdown || this._haveRestart);
+        this.actor.visible = shouldBeVisible;
     },
 
     _updateHaveShutdown: function() {
@@ -76,11 +74,7 @@ const PowerMenuButton = new Lang.Class({
                     else
                         this._haveShutdown = false;
 
-                    if (this._haveShutdown)
-                        this._powerOffItem.actor.show();
-                    else
-                        this._powerOffItem.actor.hide();
-
+                    this._powerOffItem.actor.visible = this._haveShutdown;
                     this._updateVisibility();
                 }));
         } else {
@@ -91,12 +85,7 @@ const PowerMenuButton = new Lang.Class({
                     else
                         this._haveShutdown = false;
 
-                    if (this._haveShutdown) {
-                        this._powerOffItem.actor.show();
-                    } else {
-                        this._powerOffItem.actor.hide();
-                    }
-
+                    this._powerOffItem.actor.visible = this._haveShutdown;
                     this._updateVisibility();
                 }));
         }
@@ -112,11 +101,7 @@ const PowerMenuButton = new Lang.Class({
                     else
                         this._haveRestart = false;
 
-                    if (this._haveRestart)
-                        this._restartItem.actor.show();
-                    else
-                        this._restartItem.actor.hide();
-
+                    this._restartItem.actor.visible = this._haveRestart;
                     this._updateVisibility();
                 }));
         } else {
@@ -127,12 +112,7 @@ const PowerMenuButton = new Lang.Class({
                     else
                         this._haveRestart = false;
 
-                    if (this._haveRestart) {
-                        this._restartItem.actor.show();
-                    } else {
-                        this._restartItem.actor.hide();
-                    }
-
+                    this._restartItem.actor.visible = this._haveRestart;
                     this._updateVisibility();
                 }));
         }
@@ -140,12 +120,7 @@ const PowerMenuButton = new Lang.Class({
 
     _updateHaveSuspend: function() {
         this._haveSuspend = this._upClient.get_can_suspend();
-
-        if (this._haveSuspend)
-            this._suspendItem.actor.show();
-        else
-            this._suspendItem.actor.hide();
-
+        this._suspendItem.actor.visible = this._haveSuspend;
         this._updateVisibility();
     },
 

js/ui/magnifier.js
@@ -1228,10 +1228,7 @@ const Crosshairs = new Lang.Class({
                     crosshairsActor = new Clutter.Clone({ source: this._actor });
                     this._clones.push(crosshairsActor);
                 }
-                if (this._actor.visible)
-                    crosshairsActor.show();
-                else
-                    crosshairsActor.hide();
+                crosshairsActor.visible = this._actor.visible;
 
                 container.add_actor(crosshairsActor);
                 container.raise_child(magnifiedMouse, crosshairsActor);

js/ui/status/bluetooth.js
@@ -106,10 +106,7 @@ const Indicator = new Lang.Class({
             /* TRANSLATORS: this means that bluetooth was disabled by hardware rfkill */
             this._killswitch.setStatus(_("hardware disabled"));
 
-        if (has_adapter)
-            this.actor.show();
-        else
-            this.actor.hide();
+        this.actor.visible = has_adapter;
 
         if (on) {
             this._discoverable.actor.show();

js/ui/status/volume.js
@@ -149,13 +149,9 @@ const Indicator = new Lang.Class({
                 }
             }
         }
-        if (showInput) {
-            this._inputTitle.actor.show();
-            this._inputSlider.actor.show();
-        } else {
-            this._inputTitle.actor.hide();
-            this._inputSlider.actor.hide();
-        }
+
+        this._inputTitle.actor.visible = showInput;
+        this._inputSlider.actor.visible = showInput;
     },
 
     _volumeToIcon: function(volume) {

js/ui/userMenu.js
@@ -554,18 +554,12 @@ const UserMenuButton = new Lang.Class({
 
     _updateLogout: function() {
         let allowLogout = !this._lockdownSettings.get_boolean(DISABLE_LOG_OUT_KEY);
-        if (allowLogout)
-            this._logoutItem.actor.show();
-        else
-            this._logoutItem.actor.hide();
+        this._logoutItem.actor.visible = allowLogout;
     },
 
     _updateLockScreen: function() {
         let allowLockScreen = !this._lockdownSettings.get_boolean(DISABLE_LOCK_SCREEN_KEY);
-        if (allowLockScreen)
-            this._lockScreenItem.actor.show();
-        else
-            this._lockScreenItem.actor.hide();
+        this._logoutItem.actor.visible = allowLockScreen;
     },
 
     _updateHaveShutdown: function() {
@@ -584,10 +578,7 @@ const UserMenuButton = new Lang.Class({
         if (!this._suspendOrPowerOffItem)
             return;
 
-        if (!this._haveShutdown && !this._haveSuspend)
-            this._suspendOrPowerOffItem.actor.hide();
-        else
-            this._suspendOrPowerOffItem.actor.show();
+        this._suspendOrPowerOffItem.actor.visible = this._haveShutdown || this._haveSuspend;
 
         // If we can't suspend show Power Off... instead
         // and disable the alt key

js/ui/workspacesView.js
@@ -844,10 +844,7 @@ const WorkspacesDisplay = new Lang.Class({
                         if (!primaryView)
                             return;
                         primaryView.actor.opacity = opacity;
-                        if (opacity == 0)
-                            primaryView.actor.hide();
-                        else
-                            primaryView.actor.show();
+                        primaryView.actor.visible = opacity != 0;
                     }));
         }));
     },

commit a5ac183d86f9522aa71e604d255c9c04428b4fc2

network: Fix an accidental copy-paste error
Logic swap accidentally introduced in commit 723a1c843.

https://bugzilla.gnome.org/show_bug.cgi?id=672272

js/ui/status/network.js
@@ -707,7 +707,7 @@ const NMDeviceWired = new Lang.Class({
         // the device
         // we can do it here because addConnection and removeConnection
         // both call _createSection at some point
-        this.section.actor.visible = this._connections.length <= 1;
+        this.section.actor.visible = this._connections.length > 1;
     },
 
     _createAutomaticConnection: function() {