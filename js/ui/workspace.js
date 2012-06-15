2404d2935dc524fce4bdcebb77b25853fd82fb81
Florian Müllner <fmuellner@gnome.org>
2012-06-15 15:49:24 +0200

    workspace: Remove unused method
    This one hasn't been used for quite a while now ...
    
    https://bugzilla.gnome.org/show_bug.cgi?id=678416

--- js/ui/workspace.js
+++ js/ui/workspace.js
@@ -1121,16 +1121,6 @@ const Workspace = new Lang.Class({
         }
     },
 
-    _showAllOverlays: function() {
-        let currentWorkspace = global.screen.get_active_workspace();
-        for (let i = 0; i < this._windows.length; i++) {
-            let clone = this._windows[i];
-            let overlay = this._windowOverlays[i];
-            this._showWindowOverlay(clone, overlay,
-                                    this.metaWorkspace == null || this.metaWorkspace == currentWorkspace);
-        }
-    },
-
     _hideAllOverlays: function() {
         for (let i = 0; i < this._windows.length; i++) {
             let clone = this._windows[i];
