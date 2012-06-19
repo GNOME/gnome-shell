de73128d47959cd8938b0ea0d7ed767251355427
Cosimo Cecchi <cosimoc@gnome.org>
2012-06-19 16:41:39 -0400

    mount-operation: close the password entry on operation abort
    If the device disappears in the middle of a mount operation, make sure
    to dismiss the password prompt automatically.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=673787

--- js/ui/shellMountOperation.js
+++ js/ui/shellMountOperation.js
@@ -157,11 +157,19 @@ const ShellMountOperation = new Lang.Class({
     },
 
     _onAborted: function(op) {
-        if (!this._dialog)
+        if (!this._dialog && !this._source)
             return;
 
-        this._dialog.close(global.get_current_time());
-        this._dialog = null;
+        if (this._dialog) {
+            this._dialog.close(global.get_current_time());
+            this._dialog = null;
+        }
+
+        if (this._source) {
+            this._notificationShowing = false;
+            this._source.destroy();
+            this._source = null;
+        }
     },
 
     _onShowProcesses2: function(op) {
