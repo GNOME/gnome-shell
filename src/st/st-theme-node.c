GNOME/gnome-shell

commit 7eaf231e566e8a8470589e743bac10475c1ce987

st-theme-node: Don't hold a ref to the theme context or the theme
This would just be a circular reference, which we need to avoid.

https://bugzilla.gnome.org/show_bug.cgi?id=678737

src/st/st-theme-node.c
@@ -67,18 +67,6 @@ st_theme_node_dispose (GObject *gobject)
 {
   StThemeNode *node = ST_THEME_NODE (gobject);
 
-  if (node->context)
-    {
-      g_object_unref (node->context);
-      node->context = NULL;
-    }
-
-  if (node->theme)
-    {
-      g_object_unref (node->theme);
-      node->theme = NULL;
-    }
-
   if (node->parent_node)
     {
       g_object_unref (node->parent_node);
@@ -194,7 +182,7 @@ st_theme_node_new (StThemeContext    *context,
 
   node = g_object_new (ST_TYPE_THEME_NODE, NULL);
 
-  node->context = g_object_ref (context);
+  node->context = context;
   if (parent_node != NULL)
     node->parent_node = g_object_ref (parent_node);
   else
@@ -204,7 +192,7 @@ st_theme_node_new (StThemeContext    *context,
     theme = parent_node->theme;
 
   if (theme != NULL)
-    node->theme = g_object_ref (theme);
+    node->theme = theme;
 
   node->element_type = element_type;
   node->element_id = g_strdup (element_id);

commit 1b8d03f94509f5cd8f23d6a1e178873aa3020343

St: reference the StTheme from StThemeNode
This is a partial revert of 7eaf231e566e8a8470589e743bac10475c1ce987,
which caused segfaults. For some reason, StThemeNodes seem to survive
style changes.

src/st/st-theme-node.c
@@ -85,6 +85,8 @@ st_theme_node_dispose (GObject *gobject)
       node->icon_colors = NULL;
     }
 
+  g_clear_object (&node->theme);
+
   G_OBJECT_CLASS (st_theme_node_parent_class)->dispose (gobject);
 }
 
@@ -193,6 +195,9 @@ st_theme_node_new (StThemeContext    *context,
 
   if (theme != NULL)
     node->theme = theme;
+
+  if (node->theme != NULL)
+    g_object_ref (node->theme);
 
   node->element_type = element_type;
   node->element_id = g_strdup (element_id);