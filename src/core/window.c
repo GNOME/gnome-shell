10d53fc7d200b9cc86ca7302c410558e5d917976
Florian Müllner <fmuellner@gnome.org>
2012-07-03 21:52:24 +0200

    window: Do not restore tiling on unmaximize
    When we consider tiling a special case of maximization, it makes
    more sense to always unmaximize to the normal state rather than
    restoring a previous tile state.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=677565

--- src/core/window.c
+++ src/core/window.c
@@ -3889,15 +3889,6 @@ void
 meta_window_unmaximize (MetaWindow        *window,
                         MetaMaximizeFlags  directions)
 {
-  /* Restore tiling if necessary */
-  if (window->tile_mode == META_TILE_LEFT ||
-      window->tile_mode == META_TILE_RIGHT)
-    {
-      window->maximized_horizontally = FALSE;
-      meta_window_tile (window);
-      return;
-    }
-
   meta_window_unmaximize_internal (window, directions, &window->saved_rect,
                                    NorthWestGravity);
 }
