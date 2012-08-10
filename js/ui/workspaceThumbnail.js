e7fcce3484bff036d960f20fd3a411d1cd700dde
Tanner Doshier <doshitan@gmail.com>
2012-08-10 13:13:39 -0500

    overview, workspacesView: Use ThumbnailsBox for independent workspace thumbnails
    Both WorkspacesDisplay and ThumbnailsBox need to know when windows have been
    restacked. Instead of each tracking changes on their own or trying to call
    each other, have the overview keep track and do the calculations, emitting
    a signal with the result.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=682050

--- js/ui/workspaceThumbnail.js
+++ js/ui/workspaceThumbnail.js
@@ -738,6 +738,9 @@ const ThumbnailsBox = new Lang.Class({
         this._nWorkspacesNotifyId =
             global.screen.connect('notify::n-workspaces',
                                   Lang.bind(this, this._workspacesChanged));
+        this._syncStackingId =
+            Main.overview.connect('sync-window-stacking',
+                                  Lang.bind(this, this.syncStacking));
 
         this._targetScale = 0;
         this._scale = 0;
@@ -769,6 +772,10 @@ const ThumbnailsBox = new Lang.Class({
         if (this._nWorkspacesNotifyId > 0) {
             global.screen.disconnect(this._nWorkspacesNotifyId);
             this._nWorkspacesNotifyId = 0;
+        }
+        if (this._syncStackingId > 0) {
+            Main.overview.disconnect(this._syncStackingId);
+            this._syncStackingId = 0;
         }
 
         for (let w = 0; w < this._thumbnails.length; w++)
@@ -882,7 +889,7 @@ const ThumbnailsBox = new Lang.Class({
         this._queueUpdateStates();
     },
 
-    syncStacking: function(stackIndices) {
+    syncStacking: function(actor, stackIndices) {
         for (let i = 0; i < this._thumbnails.length; i++)
             this._thumbnails[i].syncStacking(stackIndices);
     },
