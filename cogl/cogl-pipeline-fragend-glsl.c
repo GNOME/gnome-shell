57f420e21e3480d95fb4963b156bf284a1005390
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2012-08-10 09:43:45 +0200

    Remove one final user of cogl-handle and cogl-handle.h itself
    Reviewed-by: Robert Bragg <robert@linux.intel.com>

--- cogl/cogl-pipeline-fragend-glsl.c
+++ cogl/cogl-pipeline-fragend-glsl.c
@@ -42,7 +42,7 @@
 
 #include "cogl-internal.h"
 #include "cogl-context-private.h"
-#include "cogl-handle.h"
+#include "cogl-object-private.h"
 #include "cogl-shader-private.h"
 #include "cogl-program-private.h"
 #include "cogl-pipeline-cache.h"
