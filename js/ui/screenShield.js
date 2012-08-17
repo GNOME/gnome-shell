b9226fbcbddc2f76663b1355c189e4e9b46801de
Florian Müllner <fmuellner@gnome.org>
2012-08-17 19:30:46 +0200

    screenShield: Add missing import

--- js/ui/screenShield.js
+++ js/ui/screenShield.js
@@ -2,6 +2,7 @@
 
 const Clutter = imports.gi.Clutter;
 const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
 const GnomeDesktop = imports.gi.GnomeDesktop;
 const Lang = imports.lang;
 const Meta = imports.gi.Meta;
