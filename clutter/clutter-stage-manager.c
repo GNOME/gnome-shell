ca2f1b68185c06f8c8a7705cf9b88f96a0923705
Emmanuele Bassi <ebassi@gnome.org>
2012-08-24 08:27:39 +0100

    2.0: Remove deprecated API from ClutterStageManager

--- clutter/clutter-stage-manager.c
+++ clutter/clutter-stage-manager.c
@@ -47,8 +47,6 @@
 #include "clutter-private.h"
 #include "clutter-version.h"  
 
-#include "deprecated/clutter-stage-manager.h"
-
 enum
 {
   PROP_0,
@@ -186,23 +184,6 @@ clutter_stage_manager_get_default (void)
     context->stage_manager = g_object_new (CLUTTER_TYPE_STAGE_MANAGER, NULL);
 
   return context->stage_manager;
-}
-
-/**
- * clutter_stage_manager_set_default_stage:
- * @stage_manager: a #ClutterStageManager
- * @stage: a #ClutterStage
- *
- * Sets @stage as the default stage.
- *
- * Since: 0.8
- *
- * Deprecated: 1.2: Calling this function has no effect
- */
-void
-clutter_stage_manager_set_default_stage (ClutterStageManager *stage_manager,
-                                         ClutterStage        *stage)
-{
 }
 
 /*< private >
