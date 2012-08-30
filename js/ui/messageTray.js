GNOME/gnome-shell

commit c21b1e5fe00a99936f67d358a931f3083304ebc8

st: Remove StIconType
GTK+ works by explicitly specifying a -symbolic suffix for all icons.
Do the same here.

https://bugzilla.gnome.org/show_bug.cgi?id=682540

js/ui/messageTray.js
@@ -1034,10 +1034,9 @@ const Source = new Lang.Class({
 
     SOURCE_ICON_SIZE: 48,
 
-    _init: function(title, iconName, iconType) {
+    _init: function(title, iconName) {
         this.title = title;
         this.iconName = iconName;
-        this.iconType = iconType;
 
         this.isTransient = false;
         this.isChat = false;
@@ -1084,7 +1083,6 @@ const Source = new Lang.Class({
     // something more fancy.
     createIcon: function(size) {
         return new St.Icon({ icon_name: this.iconName,
-                             icon_type: this.iconType,
                              icon_size: size });
     },
 
@@ -2461,7 +2459,7 @@ const SystemNotificationSource = new Lang.Class({
     Extends: Source,
 
     _init: function() {
-        this.parent(_("System Information"), 'dialog-information', St.IconType.SYMBOLIC);
+        this.parent(_("System Information"), 'dialog-information-symbolic');
         this.setTransient(true);
     },
 

js/ui/userMenu.js
@@ -67,7 +67,6 @@ const UserAvatarWidget = new Lang.Class({
         } else {
             this.actor.style = null;
             this.actor.child = new St.Icon({ icon_name: 'avatar-default',
-                                             icon_type: St.IconType.SYMBOLIC,
                                              icon_size: DIALOG_ICON_SIZE });
         }
     }
@@ -164,22 +163,22 @@ const IMStatusChooserItem = new Lang.Class({
 
         let item;
 
-        item = new IMStatusItem(_("Available"), 'user-available');
+        item = new IMStatusItem(_("Available"), 'user-available-symbolic');
         this._combo.addMenuItem(item, IMStatus.AVAILABLE);
 
-        item = new IMStatusItem(_("Busy"), 'user-busy');
+        item = new IMStatusItem(_("Busy"), 'user-busy-symbolic');
         this._combo.addMenuItem(item, IMStatus.BUSY);
 
-        item = new IMStatusItem(_("Invisible"), 'user-invisible');
+        item = new IMStatusItem(_("Invisible"), 'user-invisible-symbolic');
         this._combo.addMenuItem(item, IMStatus.HIDDEN);
 
-        item = new IMStatusItem(_("Away"), 'user-away');
+        item = new IMStatusItem(_("Away"), 'user-away-symbolic');
         this._combo.addMenuItem(item, IMStatus.AWAY);
 
-        item = new IMStatusItem(_("Idle"), 'user-idle');
+        item = new IMStatusItem(_("Idle"), 'user-idle-symbolic');
         this._combo.addMenuItem(item, IMStatus.IDLE);
 
-        item = new IMStatusItem(_("Unavailable"), 'user-offline');
+        item = new IMStatusItem(_("Unavailable"), 'user-offline-symbolic');
         this._combo.addMenuItem(item, IMStatus.OFFLINE);
 
         this._combo.connect('active-item-changed',
@@ -480,17 +479,17 @@ const UserMenuButton = new Lang.Class({
         box.add(this._iconBox, { y_align: St.Align.MIDDLE, y_fill: false });
 
         let textureCache = St.TextureCache.get_default();
-        this._offlineIcon = new St.Icon({ icon_name: 'user-offline',
+        this._offlineIcon = new St.Icon({ icon_name: 'user-offline-symbolic',
                                           style_class: 'popup-menu-icon' });
-        this._availableIcon = new St.Icon({ icon_name: 'user-available',
+        this._availableIcon = new St.Icon({ icon_name: 'user-available-symbolic',
                                             style_class: 'popup-menu-icon' });
-        this._busyIcon = new St.Icon({ icon_name: 'user-busy',
+        this._busyIcon = new St.Icon({ icon_name: 'user-busy-symbolic',
                                        style_class: 'popup-menu-icon' });
-        this._invisibleIcon = new St.Icon({ icon_name: 'user-invisible',
+        this._invisibleIcon = new St.Icon({ icon_name: 'user-invisible-symbolic',
                                             style_class: 'popup-menu-icon' });
-        this._awayIcon = new St.Icon({ icon_name: 'user-away',
+        this._awayIcon = new St.Icon({ icon_name: 'user-away-symbolic',
                                        style_class: 'popup-menu-icon' });
-        this._idleIcon = new St.Icon({ icon_name: 'user-idle',
+        this._idleIcon = new St.Icon({ icon_name: 'user-idle-symbolic',
                                        style_class: 'popup-menu-icon' });
         this._pendingIcon = new St.Icon({ icon_name: 'user-status-pending',
                                           style_class: 'popup-menu-icon' });

js/ui/autorunManager.js
@@ -261,7 +261,7 @@ const AutorunResidentSource = new Lang.Class({
     Extends: MessageTray.Source,
 
     _init: function() {
-        this.parent(_("Removable Devices"), 'media-removable', St.IconType.FULLCOLOR);
+        this.parent(_("Removable Devices"), 'media-removable');
         this.showInLockScreen = false;
 
         this._mounts = [];

js/ui/ctrlAltTab.js
@@ -318,7 +318,6 @@ const CtrlAltTabSwitcher = new Lang.Class({
         let icon = item.iconActor;
         if (!icon) {
             icon = new St.Icon({ icon_name: item.iconName,
-                                 icon_type: St.IconType.SYMBOLIC,
                                  icon_size: POPUP_APPICON_SIZE });
         }
         box.add(icon, { x_fill: false, y_fill: false } );

js/ui/dash.js
@@ -256,10 +256,9 @@ const ShowAppsIcon = new Lang.Class({
 
     _createIcon: function(size) {
         this._iconActor = new St.Icon({ icon_name: 'view-grid-symbolic',
+                                        icon_size: size,
                                         style_class: 'show-apps-icon',
-                                        track_hover: true,
-                                        icon_type: St.IconType.SYMBOLIC,
-                                        icon_size: size });
+                                        track_hover: true });
         return this._iconActor;
     },
 

js/ui/endSessionDialog.js
@@ -89,7 +89,7 @@ const shutdownDialogContent = {
                        label:  C_("button", "Restart") },
                      { signal: 'ConfirmedShutdown',
                        label:  C_("button", "Power Off") }],
-    iconName: 'system-shutdown',
+    iconName: 'system-shutdown-symbolic',
     iconStyleClass: 'end-session-dialog-shutdown-icon'
 };
 
@@ -104,7 +104,7 @@ const restartDialogContent = {
     endDescription: _("Restarting the system."),
     confirmButtons: [{ signal: 'ConfirmedReboot',
                        label:  C_("button", "Restart") }],
-    iconName: 'system-shutdown',
+    iconName: 'system-shutdown-symbolic',
     iconStyleClass: 'end-session-dialog-shutdown-icon'
 };
 
@@ -327,7 +327,6 @@ const EndSessionDialog = new Lang.Class({
             let textureCache = St.TextureCache.get_default();
             let icon = textureCache.load_icon_name(this._iconBin.get_theme_node(),
                                                    iconName,
-                                                   St.IconType.SYMBOLIC,
                                                    _DIALOG_ICON_SIZE);
 
             this._iconBin.child = icon;

js/ui/keyboard.js
@@ -542,7 +542,7 @@ const KeyboardSource = new Lang.Class({
 
     _init: function(keyboard) {
         this._keyboard = keyboard;
-        this.parent(_("Keyboard"), 'input-keyboard', St.IconType.SYMBOLIC);
+        this.parent(_("Keyboard"), 'input-keyboard-symbolic');
     },
 
     handleSummaryClick: function() {

js/ui/lookingGlass.js
@@ -869,7 +869,6 @@ const LookingGlass = new Lang.Class({
         let toolbar = new St.BoxLayout({ name: 'Toolbar' });
         this.actor.add_actor(toolbar);
         let inspectIcon = new St.Icon({ icon_name: 'gtk-color-picker',
-                                        icon_type: St.IconType.FULLCOLOR,
                                         icon_size: 24 });
         toolbar.add_actor(inspectIcon);
         inspectIcon.reactive = true;

commit 2beff9896a1cfd2e61d345f58c8f810dd9e75b50

Fix a couple of symbolic icons
Some fallout from commit c21b1e5fe00a ...

js/ui/messageTray.js
@@ -2021,7 +2021,7 @@ const MessageTray = new Lang.Class({
         // Don't move the windows up if we are in the overview,
         // but show the tray in the ctrl+alt+tab list.
         if (this._overviewVisible) {
-            Main.ctrlAltTabManager.addGroup(this._summary, _("Message Tray"), 'start-here',
+            Main.ctrlAltTabManager.addGroup(this._summary, _("Message Tray"), 'start-here-symbolic',
                                             { sortGroup: CtrlAltTab.SortGroup.BOTTOM });
             return;
         }

js/ui/userMenu.js
@@ -66,7 +66,7 @@ const UserAvatarWidget = new Lang.Class({
             this.actor.style = 'background-image: url("%s");'.format(iconFile);
         } else {
             this.actor.style = null;
-            this.actor.child = new St.Icon({ icon_name: 'avatar-default',
+            this.actor.child = new St.Icon({ icon_name: 'avatar-default-symbolic',
                                              icon_size: DIALOG_ICON_SIZE });
         }
     }
@@ -491,7 +491,7 @@ const UserMenuButton = new Lang.Class({
                                        style_class: 'popup-menu-icon' });
         this._idleIcon = new St.Icon({ icon_name: 'user-idle-symbolic',
                                        style_class: 'popup-menu-icon' });
-        this._pendingIcon = new St.Icon({ icon_name: 'user-status-pending',
+        this._pendingIcon = new St.Icon({ icon_name: 'user-status-pending-symbolic',
                                           style_class: 'popup-menu-icon' });
 
         this._accountMgr.connect('most-available-presence-changed',