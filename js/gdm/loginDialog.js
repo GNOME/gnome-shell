GNOME/gnome-shell

commit cc6744055f0a6a009baf68e2e0faff9b8a1043cd

gdm: Don't fade in the log in dialog
Instead, directly show it.

https://bugzilla.gnome.org/show_bug.cgi?id=682428

js/gdm/loginDialog.js
@@ -625,8 +625,8 @@ const LoginDialog = new Lang.Class({
     _init: function(parentActor) {
         this.parent({ shellReactive: true,
                       styleClass: 'login-dialog',
-                      parentActor: parentActor
-                    });
+                      parentActor: parentActor,
+                      shouldFade: false });
         this.connect('destroy',
                      Lang.bind(this, this._onDestroy));
         this.connect('opened',

js/ui/modalDialog.js
@@ -37,12 +37,13 @@ const ModalDialog = new Lang.Class({
     _init: function(params) {
         params = Params.parse(params, { shellReactive: false,
                                         styleClass: null,
-                                        parentActor: Main.uiGroup
-                                      });
+                                        parentActor: Main.uiGroup,
+                                        shouldFadeIn: true });
 
         this.state = State.CLOSED;
         this._hasModal = false;
         this._shellReactive = params.shellReactive;
+        this._shouldFadeIn = params.shouldFadeIn;
 
         this._group = new St.Widget({ visible: false,
                                       x: 0,
@@ -210,7 +211,7 @@ const ModalDialog = new Lang.Class({
         this._group.show();
         Tweener.addTween(this._group,
                          { opacity: 255,
-                           time: OPEN_AND_CLOSE_TIME,
+                           time: this._shouldFadeIn ? OPEN_AND_CLOSE_TIME : 0,
                            transition: 'easeOutQuad',
                            onComplete: Lang.bind(this,
                                function() {

commit 51bdc44352cd9c48ca3a53903f0acc9b9245f684

gdm: Fix property typo
Introduced by cc6744055f0a6a009baf68e2e0faff9b8a1043cd

js/gdm/loginDialog.js
@@ -626,7 +626,7 @@ const LoginDialog = new Lang.Class({
         this.parent({ shellReactive: true,
                       styleClass: 'login-dialog',
                       parentActor: parentActor,
-                      shouldFade: false });
+                      shouldFadeIn: false });
         this.connect('destroy',
                      Lang.bind(this, this._onDestroy));
         this.connect('opened',