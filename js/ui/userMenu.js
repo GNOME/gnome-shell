0e01a81219e16969011860be134bccd111930368
Florian Müllner <fmuellner@gnome.org>
2012-09-28 18:52:12 +0200

    userMenu: Remove 'Switch Session' item
    GDM does not allow concurrent logins of a single user, so making
    'Switch Session' a user switch operation does not work - in order
    to choose a different session, users have to log out.
    Rather than making 'Switch Session' an alias of 'Log out' (which
    is available anyway when multiple sessions are defined), remove
    the item altogether - 'Switch Session' suggests an operation that
    does not loose state, and we currently favor 'Switch Session' over
    'Switch User', so on systems that have both multiple users and
    multiple sessions, the latter would become unavailable.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=685062

--- js/ui/userMenu.js
+++ js/ui/userMenu.js
@@ -613,11 +613,8 @@ const UserMenuButton = new Lang.Class({
     _updateSwitchUser: function() {
         let allowSwitch = !this._lockdownSettings.get_boolean(DISABLE_USER_SWITCH_KEY);
         let multiUser = this._userManager.can_switch() && this._userManager.has_multiple_users;
-        let multiSession = Gdm.get_session_ids().length > 1;
 
-        this._loginScreenItem.label.set_text(multiUser ? _("Switch User")
-                                                       : _("Switch Session"));
-        this._loginScreenItem.actor.visible = allowSwitch && (multiUser || multiSession);
+        this._loginScreenItem.actor.visible = allowSwitch && multiUser;
     },
 
     _updateLogout: function() {
