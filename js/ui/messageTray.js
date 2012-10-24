GNOME/gnome-shell

commit 448517032e5de87a756dadf815f0df80c8cb724c

messageTray: Show the message tray in Ctrl+Alt+Tab outside of the overview
Since the message tray is accessible outside of the overview, it doesn't
make sense to show it only there.

https://bugzilla.gnome.org/show_bug.cgi?id=684633

js/ui/messageTray.js
@@ -1483,7 +1483,6 @@ const MessageTray = new Lang.Class({
         this._reNotifyAfterHideNotification = null;
         this._inFullscreen = false;
         this._desktopClone = null;
-        this._inCtrlAltTab = false;
 
         this._lightbox = new Lightbox.Lightbox(global.window_group,
                                                { inhibitEvents: true,
@@ -1530,6 +1529,10 @@ const MessageTray = new Lang.Class({
         this._trayDwellTimeoutId = 0;
         this._trayDwelling = false;
         this._trayDwellUserTime = 0;
+
+        Main.ctrlAltTabManager.addGroup(this._summary, _("Message Tray"), 'start-here-symbolic',
+                                        { focusCallback: Lang.bind(this, this.toggleAndNavigate),
+                                          sortGroup: CtrlAltTab.SortGroup.BOTTOM });
     },
 
     _checkTrayDwell: function(x, y) {
@@ -2085,10 +2088,6 @@ const MessageTray = new Lang.Class({
                     });
 
         if (this._overviewVisible) {
-            Main.ctrlAltTabManager.addGroup(this._summary, _("Message Tray"), 'start-here-symbolic',
-                                            { sortGroup: CtrlAltTab.SortGroup.BOTTOM });
-            this._inCtrlAltTab = true;
-        } else {
             this._lightbox.show();
         }
     },
@@ -2133,13 +2132,7 @@ const MessageTray = new Lang.Class({
         // which would happen if GrabHelper ungrabbed for us.
         // This is a no-op in that case.
         this._grabHelper.ungrab({ actor: this.actor });
-
-        if (this._inCtrlAltTab) {
-            Main.ctrlAltTabManager.removeGroup(this._summary);
-            this._inCtrlAltTab = false;
-        } else {
-            this._lightbox.hide();
-        }
+        this._lightbox.hide();
     },
 
     _hideDesktopClone: function(now) {

commit 3a453c5f735a85f9d95eb1691d23f3833c321853

messageTray: Fix lightbox
Commit 448517032e5 accidentally reverted the condition for showing
the lightbox. Fix that.

https://bugzilla.gnome.org/show_bug.cgi?id=686728

js/ui/messageTray.js
@@ -2101,9 +2101,8 @@ const MessageTray = new Lang.Class({
                       transition: 'easeOutQuad'
                     });
 
-        if (this._overviewVisible) {
+        if (!this._overviewVisible)
             this._lightbox.show();
-        }
 
         return true;
     },