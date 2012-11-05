5ef5e25a7f1ee89973788550b10784dd6ac11f89
Jean-François Fortin Tam <nekohayo@gmail.com>
2012-11-05 11:32:45 -0500

    Use "Decline" instead of "Reject" for incoming calls
    This makes GNOME Shell more polite
    Fixes bug #661194

--- js/ui/components/telepathyClient.js
+++ js/ui/components/telepathyClient.js
@@ -1135,7 +1135,7 @@ const AudioVideoNotification = new Lang.Class({
         this.parent(source, title, null, { customContent: true });
         this.setResident(true);
 
-        this.addButton('reject', _("Reject"));
+        this.addButton('reject', _("Decline"));
         /* translators: this is a button label (verb), not a noun */
         this.addButton('answer', _("Answer"));
 
