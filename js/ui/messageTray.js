GNOME/gnome-shell

commit 1a27d7dfc02c06bd0ccf4258a9108207b8832ce4

MessageTray: remove all tweens when tweening for state parameters
MessageTray._tween sets the state variable to the in-progress value,
so it must be sure that at the end of the animation the value will
be the corresponding final and nothing else will happen in between.

https://bugzilla.gnome.org/show_bug.cgi?id=683986

js/ui/messageTray.js
@@ -2108,6 +2108,8 @@ const MessageTray = new Lang.Class({
         params.onCompleteScope = this;
         params.onCompleteParams = [statevar, value, onComplete, onCompleteScope, onCompleteParams];
 
+        // Remove other tweens that could mess with the state machine
+        Tweener.removeTweens(actor);
         Tweener.addTween(actor, params);
 
         let valuing = (value == State.SHOWN) ? State.SHOWING : State.HIDING;
@@ -2429,8 +2431,6 @@ const MessageTray = new Lang.Class({
         if (this._notificationWidget.y < expandedY) {
             this._notificationWidget.y = expandedY;
         } else if (this._notification.y != expandedY) {
-            // Remove any other tween that could mess with the state machine
-            Tweener.removeTweens(this._notificationWidget);
             // Tween also opacity here, to override a possible tween that's
             // currently hiding the notification.
             this._tween(this._notificationWidget, '_notificationState', State.SHOWN,

commit 41f933b89e275d06936bba2df8796a16e02baf41

MessageTray: fix notification height expansion
MessageTray._tween removes all tweens now, so it removes also
those setting the height to expanded for critical notifications.

This is a regression from 1a27d7dfc02c06bd0ccf4258a9108207b8832ce4

https://bugzilla.gnome.org/show_bug.cgi?id=689295

js/ui/messageTray.js
@@ -2263,40 +2263,28 @@ const MessageTray = new Lang.Class({
     _updateShowingNotification: function() {
         this._notification.acknowledged = true;
 
-        Tweener.removeTweens(this._notificationWidget);
-
         // We auto-expand notifications with CRITICAL urgency.
-        // We use Tweener.removeTweens() to remove a tween that was hiding the notification we are
-        // updating, in case that notification was in the process of being hidden. However,
-        // Tweener.removeTweens() would also remove a tween that was updating the position of the
-        // notification we are updating, in case that notification was already expanded and its height
-        // changed. Therefore we need to call this._expandNotification() for expanded notifications
-        // to make sure their position is updated.
-        if (this._notification.urgency == Urgency.CRITICAL || this._notification.expanded)
+        if (this._notification.urgency == Urgency.CRITICAL)
             this._expandNotification(true);
 
         // We tween all notifications to full opacity. This ensures that both new notifications and
         // notifications that might have been in the process of hiding get full opacity.
         //
-        // We tween any notification showing in the banner mode to banner height
-        // (this._notificationWidget.y = -this._notificationWidget.height).
+        // We tween any notification showing in the banner mode to the appropriate height
+        // (which is banner height or expanded height, depending on the notification state)
         // This ensures that both new notifications and notifications in the banner mode that might
-        // have been in the process of hiding are shown with the banner height.
+        // have been in the process of hiding are shown with the correct height.
         //
         // We use this._showNotificationCompleted() onComplete callback to extend the time the updated
         // notification is being shown.
-        //
-        // We don't set the y parameter for the tween for expanded notifications because
-        // this._expandNotification() will result in getting this._notificationWidget.y set to the appropriate
-        // fully expanded value.
+
         let tweenParams = { opacity: 255,
+                            y: -this._notificationWidget.height,
                             time: ANIMATION_TIME,
                             transition: 'easeOutQuad',
                             onComplete: this._showNotificationCompleted,
                             onCompleteScope: this
                           };
-        if (!this._notification.expanded)
-            tweenParams.y = -this._notificationWidget.height;
 
         this._tween(this._notificationWidget, '_notificationState', State.SHOWN, tweenParams);
    },