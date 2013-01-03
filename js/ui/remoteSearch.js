51726d8de710583152a5316ec22ef3fa50751c8e
Giovanni Campagna <gcampagna@src.gnome.org>
2013-01-03 02:48:22 +0100

    Fix regression from 428876123579589a228770faab57887169fea343

--- js/ui/remoteSearch.js
+++ js/ui/remoteSearch.js
@@ -1,5 +1,6 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 
+const GdkPixbuf = imports.gi.GdkPixbuf;
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
 const Lang = imports.lang;
