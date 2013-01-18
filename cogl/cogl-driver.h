rib/rig

framebuffer: split GL code out from cogl-framebuffer.c
This splits out most of the OpenGL specific code from cogl-framebuffer.c
into cogl-framebuffer-gl.c and extends the CoglDriverVtable interface
for cogl-framebuffer.c to use.

There are hopes to support several different backends for Cogl
eventually to hopefully get us closer to the metal so this makes some
progress in organizing which parts of Cogl are OpenGL specific so these
parts can potentially be switched out later.

The only remaining use of OpenGL still in cogl-framebuffer.c is to
handle cogl_framebuffer_read_pixels.

cogl/cogl-driver.h
@@ -25,6 +25,8 @@
 #define __COGL_DRIVER_H
 
 #include "cogl-context.h"
+#include "cogl-offscreen.h"
+#include "cogl-framebuffer-private.h"
 
 typedef struct _CoglDriverVtable CoglDriverVtable;
 
@@ -45,6 +47,61 @@ struct _CoglDriverVtable
   CoglBool
   (* update_features) (CoglContext *context,
                        CoglError **error);
+
+  CoglBool
+  (* offscreen_allocate) (CoglOffscreen *offscreen,
+                          CoglError **error);
+
+  void
+  (* offscreen_free) (CoglOffscreen *offscreen);
+
+  void
+  (* framebuffer_flush_state) (CoglFramebuffer *draw_buffer,
+                               CoglFramebuffer *read_buffer,
+                               CoglFramebufferState state);
+
+  void
+  (* framebuffer_clear) (CoglFramebuffer *framebuffer,
+                         unsigned long buffers,
+                         float red,
+                         float green,
+                         float blue,
+                         float alpha);
+
+  void
+  (* framebuffer_query_bits) (CoglFramebuffer *framebuffer,
+                              int *red,
+                              int *green,
+                              int *blue,
+                              int *alpha);
+
+  void
+  (* framebuffer_finish) (CoglFramebuffer *framebuffer);
+
+  void
+  (* framebuffer_discard_buffers) (CoglFramebuffer *framebuffer,
+                                   unsigned long buffers);
+
+  void
+  (* framebuffer_draw_attributes) (CoglFramebuffer *framebuffer,
+                                   CoglPipeline *pipeline,
+                                   CoglVerticesMode mode,
+                                   int first_vertex,
+                                   int n_vertices,
+                                   CoglAttribute **attributes,
+                                   int n_attributes,
+                                   CoglDrawFlags flags);
+
+  void
+  (* framebuffer_draw_indexed_attributes) (CoglFramebuffer *framebuffer,
+                                           CoglPipeline *pipeline,
+                                           CoglVerticesMode mode,
+                                           int first_vertex,
+                                           int n_vertices,
+                                           CoglIndices *indices,
+                                           CoglAttribute **attributes,
+                                           int n_attributes,
+                                           CoglDrawFlags flags);
 };
 
 #endif /* __COGL_DRIVER_H */

cogl/cogl-framebuffer-gl-private.h
@@ -1,0 +1,91 @@
+/*
+ * Cogl
+ *
+ * An object oriented GL/GLES Abstraction/Utility Layer
+ *
+ * Copyright (C) 2008,2009,2010,2011 Intel Corporation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ *
+ * Authors:
+ *   Robert Bragg <robert@linux.intel.com>
+ */
+
+#ifndef __COGL_FRAMEBUFFER_GL_PRIVATE_H__
+#define __COGL_FRAMEBUFFER_GL_PRIVATE_H__
+
+CoglBool
+_cogl_offscreen_gl_allocate (CoglOffscreen *offscreen,
+                             CoglError **error);
+
+void
+_cogl_offscreen_gl_free (CoglOffscreen *offscreen);
+
+void
+_cogl_framebuffer_gl_flush_state (CoglFramebuffer *draw_buffer,
+                                  CoglFramebuffer *read_buffer,
+                                  CoglFramebufferState state);
+
+void
+_cogl_framebuffer_gl_clear (CoglFramebuffer *framebuffer,
+                            unsigned long buffers,
+                            float red,
+                            float green,
+                            float blue,
+                            float alpha);
+
+void
+_cogl_framebuffer_gl_query_bits (CoglFramebuffer *framebuffer,
+                                 int *red,
+                                 int *green,
+                                 int *blue,
+                                 int *alpha);
+
+void
+_cogl_framebuffer_gl_finish (CoglFramebuffer *framebuffer);
+
+void
+_cogl_framebuffer_gl_discard_buffers (CoglFramebuffer *framebuffer,
+                                      unsigned long buffers);
+
+void
+_cogl_framebuffer_gl_bind (CoglFramebuffer *framebuffer, GLenum target);
+
+void
+_cogl_framebuffer_gl_draw_attributes (CoglFramebuffer *framebuffer,
+                                      CoglPipeline *pipeline,
+                                      CoglVerticesMode mode,
+                                      int first_vertex,
+                                      int n_vertices,
+                                      CoglAttribute **attributes,
+                                      int n_attributes,
+                                      CoglDrawFlags flags);
+
+void
+_cogl_framebuffer_gl_draw_indexed_attributes (CoglFramebuffer *framebuffer,
+                                              CoglPipeline *pipeline,
+                                              CoglVerticesMode mode,
+                                              int first_vertex,
+                                              int n_vertices,
+                                              CoglIndices *indices,
+                                              CoglAttribute **attributes,
+                                              int n_attributes,
+                                              CoglDrawFlags flags);
+
+#endif /* __COGL_FRAMEBUFFER_GL_PRIVATE_H__ */
+
+

GNOME/gnome-shell

cogl/cogl-driver.h
@@ -25,6 +25,8 @@
 #define __COGL_DRIVER_H
 
 #include "cogl-context.h"
+#include "cogl-offscreen.h"
+#include "cogl-framebuffer-private.h"
 
 typedef struct _CoglDriverVtable CoglDriverVtable;
 
@@ -45,6 +47,61 @@ struct _CoglDriverVtable
   CoglBool
   (* update_features) (CoglContext *context,
                        GError **error);
+
+  CoglBool
+  (* offscreen_allocate) (CoglOffscreen *offscreen,
+                          GError **error);
+
+  void
+  (* offscreen_free) (CoglOffscreen *offscreen);
+
+  void
+  (* framebuffer_flush_state) (CoglFramebuffer *draw_buffer,
+                               CoglFramebuffer *read_buffer,
+                               CoglFramebufferState state);
+
+  void
+  (* framebuffer_clear) (CoglFramebuffer *framebuffer,
+                         unsigned long buffers,
+                         float red,
+                         float green,
+                         float blue,
+                         float alpha);
+
+  void
+  (* framebuffer_query_bits) (CoglFramebuffer *framebuffer,
+                              int *red,
+                              int *green,
+                              int *blue,
+                              int *alpha);
+
+  void
+  (* framebuffer_finish) (CoglFramebuffer *framebuffer);
+
+  void
+  (* framebuffer_discard_buffers) (CoglFramebuffer *framebuffer,
+                                   unsigned long buffers);
+
+  void
+  (* framebuffer_draw_attributes) (CoglFramebuffer *framebuffer,
+                                   CoglPipeline *pipeline,
+                                   CoglVerticesMode mode,
+                                   int first_vertex,
+                                   int n_vertices,
+                                   CoglAttribute **attributes,
+                                   int n_attributes,
+                                   CoglDrawFlags flags);
+
+  void
+  (* framebuffer_draw_indexed_attributes) (CoglFramebuffer *framebuffer,
+                                           CoglPipeline *pipeline,
+                                           CoglVerticesMode mode,
+                                           int first_vertex,
+                                           int n_vertices,
+                                           CoglIndices *indices,
+                                           CoglAttribute **attributes,
+                                           int n_attributes,
+                                           CoglDrawFlags flags);
 };
 
 #endif /* __COGL_DRIVER_H */

cogl/cogl-framebuffer-gl-private.h
@@ -1,0 +1,91 @@
+/*
+ * Cogl
+ *
+ * An object oriented GL/GLES Abstraction/Utility Layer
+ *
+ * Copyright (C) 2008,2009,2010,2011 Intel Corporation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ *
+ * Authors:
+ *   Robert Bragg <robert@linux.intel.com>
+ */
+
+#ifndef __COGL_FRAMEBUFFER_GL_PRIVATE_H__
+#define __COGL_FRAMEBUFFER_GL_PRIVATE_H__
+
+CoglBool
+_cogl_offscreen_gl_allocate (CoglOffscreen *offscreen,
+                             GError **error);
+
+void
+_cogl_offscreen_gl_free (CoglOffscreen *offscreen);
+
+void
+_cogl_framebuffer_gl_flush_state (CoglFramebuffer *draw_buffer,
+                                  CoglFramebuffer *read_buffer,
+                                  CoglFramebufferState state);
+
+void
+_cogl_framebuffer_gl_clear (CoglFramebuffer *framebuffer,
+                            unsigned long buffers,
+                            float red,
+                            float green,
+                            float blue,
+                            float alpha);
+
+void
+_cogl_framebuffer_gl_query_bits (CoglFramebuffer *framebuffer,
+                                 int *red,
+                                 int *green,
+                                 int *blue,
+                                 int *alpha);
+
+void
+_cogl_framebuffer_gl_finish (CoglFramebuffer *framebuffer);
+
+void
+_cogl_framebuffer_gl_discard_buffers (CoglFramebuffer *framebuffer,
+                                      unsigned long buffers);
+
+void
+_cogl_framebuffer_gl_bind (CoglFramebuffer *framebuffer, GLenum target);
+
+void
+_cogl_framebuffer_gl_draw_attributes (CoglFramebuffer *framebuffer,
+                                      CoglPipeline *pipeline,
+                                      CoglVerticesMode mode,
+                                      int first_vertex,
+                                      int n_vertices,
+                                      CoglAttribute **attributes,
+                                      int n_attributes,
+                                      CoglDrawFlags flags);
+
+void
+_cogl_framebuffer_gl_draw_indexed_attributes (CoglFramebuffer *framebuffer,
+                                              CoglPipeline *pipeline,
+                                              CoglVerticesMode mode,
+                                              int first_vertex,
+                                              int n_vertices,
+                                              CoglIndices *indices,
+                                              CoglAttribute **attributes,
+                                              int n_attributes,
+                                              CoglDrawFlags flags);
+
+#endif /* __COGL_FRAMEBUFFER_GL_PRIVATE_H__ */
+
+