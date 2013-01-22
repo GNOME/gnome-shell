rib/cogl

texture: Adds cogl_texture_set_data convenience api
This adds a cogl_texture_set_data function that is basically just a
convenience wrapper around cogl_texture_set_region. In the common case
where you want to upload the full contents of a mipmap level though this
api takes 4 less arguments (6 in total) so it's a bit simpler.

cogl/cogl-texture.c
@@ -482,6 +482,34 @@ cogl_texture_set_region (CoglTexture *texture,
   return ret;
 }
 
+CoglBool
+cogl_texture_set_data (CoglTexture *texture,
+                       CoglPixelFormat format,
+                       int rowstride,
+                       const uint8_t *data,
+                       int level,
+                       CoglError **error)
+{
+  int level_width;
+  int level_height;
+
+  _cogl_texture_get_level_size (texture,
+                                level,
+                                &level_width,
+                                &level_height,
+                                NULL);
+
+  return cogl_texture_set_region (texture,
+                                  level_width,
+                                  level_height,
+                                  format,
+                                  rowstride,
+                                  data,
+                                  0, 0, /* dest x, y */
+                                  level,
+                                  error);
+}
+
 /* Reads back the contents of a texture by rendering it to the framebuffer
  * and reading back the resulting pixels.
  *

GNOME/gnome-shell

Reviewed-by: Neil Roberts <neil@linux.intel.com>
(cherry picked from commit e651dbdc4e4f03016a3dee513e3680270a4a9142)

cogl/cogl-texture.c
@@ -610,6 +610,34 @@ cogl_texture_set_region (CoglTexture *texture,
   return status;
 }
 
+CoglBool
+cogl_texture_set_data (CoglTexture *texture,
+                       CoglPixelFormat format,
+                       int rowstride,
+                       const uint8_t *data,
+                       int level,
+                       CoglError **error)
+{
+  int level_width;
+  int level_height;
+
+  _cogl_texture_get_level_size (texture,
+                                level,
+                                &level_width,
+                                &level_height,
+                                NULL);
+
+  return _cogl_texture_set_region (texture,
+                                   level_width,
+                                   level_height,
+                                   format,
+                                   rowstride,
+                                   data,
+                                   0, 0, /* dest x, y */
+                                   level,
+                                   error);
+}
+
 /* Reads back the contents of a texture by rendering it to the framebuffer
  * and reading back the resulting pixels.
  *