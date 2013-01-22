rib/rig

Allow propogation of OOM errors to apps
This allows apps to catch out-of-memory errors when allocating textures.

Textures can be pretty huge at times and so it's quite possible for an
application to try and allocate more memory than is available. It's also
very possible that the application can take some action in response to
reduce memory pressure (such as freeing up texture caches perhaps) so
we shouldn't just automatically abort like we do for trivial heap
allocations.

These public functions now take a CoglError argument so applications can
catch out of memory errors:

cogl_buffer_map
cogl_buffer_map_range
cogl_buffer_set_data
cogl_framebuffer_read_pixels_into_bitmap
cogl_pixel_buffer_new
cogl_texture_new_from_data
cogl_texture_new_from_bitmap

Note: we've been quite conservative with how many apis we let throw OOM
CoglErrors since we don't really want to put a burdon on developers to
be checking for errors with every cogl api call. So long as there is
some lower level api for apps to use that let them catch OOM errors
for everything necessary that's enough and we don't have to make more
convenient apis more awkward to use.

The main focus is on bitmaps and texture allocations since they
can be particularly large and prone to failing.

A new cogl_attribute_buffer_new_with_size() function has been added in
case developers need to catch OOM errors when allocating attribute buffers
whereby they can first use _buffer_new_with_size() (which doesn't take a
CoglError) followed by cogl_buffer_set_data() which will lazily allocate
the buffer storage and report OOM errors.

Reviewed-by: Neil Roberts <neil@linux.intel.com>

cogl/cogl-attribute-buffer.c
@@ -39,28 +39,49 @@ static void _cogl_attribute_buffer_free (CoglAttributeBuffer *array);
 COGL_BUFFER_DEFINE (AttributeBuffer, attribute_buffer);
 
 CoglAttributeBuffer *
-cogl_attribute_buffer_new (CoglContext *context,
-                           size_t bytes,
-                           const void *data)
+cogl_attribute_buffer_new_with_size (CoglContext *context,
+                                     size_t bytes)
 {
-  CoglAttributeBuffer *array = g_slice_new (CoglAttributeBuffer);
+  CoglAttributeBuffer *buffer = g_slice_new (CoglAttributeBuffer);
 
   /* parent's constructor */
-  _cogl_buffer_initialize (COGL_BUFFER (array),
+  _cogl_buffer_initialize (COGL_BUFFER (buffer),
                            context,
                            bytes,
                            COGL_BUFFER_BIND_TARGET_ATTRIBUTE_BUFFER,
                            COGL_BUFFER_USAGE_HINT_ATTRIBUTE_BUFFER,
                            COGL_BUFFER_UPDATE_HINT_STATIC);
 
-  _cogl_attribute_buffer_object_new (array);
+  return _cogl_attribute_buffer_object_new (buffer);
+}
 
-  if (data)
-    cogl_buffer_set_data (COGL_BUFFER (array),
-                          0,
-                          data,
-                          bytes);
-  return array;
+CoglAttributeBuffer *
+cogl_attribute_buffer_new (CoglContext *context,
+                           size_t bytes,
+                           const void *data)
+{
+  CoglAttributeBuffer *buffer;
+
+  _COGL_RETURN_VAL_IF_FAIL (data, NULL);
+
+  buffer = cogl_attribute_buffer_new_with_size (context, bytes);
+
+  /* Note: to keep the common cases simple this API doesn't throw
+   * CoglErrors, so developers can assume this function never returns
+   * NULL and we will simply abort on error.
+   *
+   * Developers wanting to catch errors can use
+   * cogl_attribute_buffer_new_with_size() and catch errors when later
+   * calling cogl_buffer_set_data() or cogl_buffer_map().
+   */
+
+  cogl_buffer_set_data (COGL_BUFFER (buffer),
+                        0,
+                        data,
+                        bytes,
+                        NULL);
+
+  return buffer;
 }
 
 static void

GNOME/gnome-shell

(cherry picked from commit f7735e141ad537a253b02afa2a8238f96340b978)
Note: since we can't break the API for Cogl 1.x then actually the main
purpose of cherry picking this patch is to keep in-line with changes
on the master branch so that we can easily cherry-pick patches.
All the api changes relating stable apis released on the 1.12 branch
have been reverted as part of cherry-picking this patch so this most
just applies all the internal plumbing changes that enable us to
correctly propagate OOM errors.

cogl/cogl-attribute-buffer.c
@@ -39,28 +39,50 @@ static void _cogl_attribute_buffer_free (CoglAttributeBuffer *array);
 COGL_BUFFER_DEFINE (AttributeBuffer, attribute_buffer);
 
 CoglAttributeBuffer *
-cogl_attribute_buffer_new (CoglContext *context,
-                           size_t bytes,
-                           const void *data)
+cogl_attribute_buffer_new_with_size (CoglContext *context,
+                                     size_t bytes)
 {
-  CoglAttributeBuffer *array = g_slice_new (CoglAttributeBuffer);
+  CoglAttributeBuffer *buffer = g_slice_new (CoglAttributeBuffer);
 
   /* parent's constructor */
-  _cogl_buffer_initialize (COGL_BUFFER (array),
+  _cogl_buffer_initialize (COGL_BUFFER (buffer),
                            context,
                            bytes,
                            COGL_BUFFER_BIND_TARGET_ATTRIBUTE_BUFFER,
                            COGL_BUFFER_USAGE_HINT_ATTRIBUTE_BUFFER,
                            COGL_BUFFER_UPDATE_HINT_STATIC);
 
-  _cogl_attribute_buffer_object_new (array);
+  return _cogl_attribute_buffer_object_new (buffer);
+}
 
+CoglAttributeBuffer *
+cogl_attribute_buffer_new (CoglContext *context,
+                           size_t bytes,
+                           const void *data)
+{
+  CoglAttributeBuffer *buffer;
+
+  buffer = cogl_attribute_buffer_new_with_size (context, bytes);
+
+  /* Note: to keep the common cases simple this API doesn't throw
+   * CoglErrors, so developers can assume this function never returns
+   * NULL and we will simply abort on error.
+   *
+   * Developers wanting to catch errors can use
+   * cogl_attribute_buffer_new_with_size() and catch errors when later
+   * calling cogl_buffer_set_data() or cogl_buffer_map().
+   */
+
+  /* XXX: NB: for Cogl 2.0 we don't allow NULL data here but we can't
+   * break the api for 1.x and so we keep the check for now. */
   if (data)
-    cogl_buffer_set_data (COGL_BUFFER (array),
-                          0,
-                          data,
-                          bytes);
-  return array;
+    _cogl_buffer_set_data (COGL_BUFFER (buffer),
+                           0,
+                           data,
+                           bytes,
+                           NULL);
+
+  return buffer;
 }
 
 static void