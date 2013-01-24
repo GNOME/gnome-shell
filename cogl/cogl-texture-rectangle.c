a749c7c1abf6f4f9ee47aa688221f5ddbafff3b5
Neil Roberts <neil@linux.intel.com>
2013-01-24 11:51:06 +0000

    Query rectangle tex parameters when creating a foreign texture on GL3
    Previously when creating a foreign rectangle texture it would ignore
    the passed in texture information and query the texture directly when
    using COGL_DRIVER_GL. However this should also work for
    COGL_DRIVER_GL3. This patch changes it to check the private feature
    flags for the texture querying feature instead of directly checking
    the driver value.
    
    Reviewed-by: Robert Bragg <robert@linux.intel.com>
    
    (cherry picked from commit 258c98b82027cb5074afe7844ff3954bbe928757)

--- cogl/cogl-texture-rectangle.c
+++ cogl/cogl-texture-rectangle.c
@@ -389,7 +389,8 @@ cogl_texture_rectangle_new_from_foreign (CoglContext *ctx,
   /* Obtain texture parameters */
 
 #ifdef HAVE_COGL_GL
-  if (ctx->driver == COGL_DRIVER_GL)
+  if ((ctx->private_feature_flags &
+       COGL_PRIVATE_FEATURE_QUERY_TEXTURE_PARAMETERS))
     {
       GLint val;
 
