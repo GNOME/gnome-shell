c0469601c71f24fbd75d5414a3f44c8138085060
Rui Matos <tiagomatos@gmail.com>
2013-01-31 23:11:23 +0100

    x11/device-manager-xi2: Fix slave to master association
    A slave is associated to a master device, not the other way around.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=692971

--- clutter/x11/clutter-device-manager-xi2.c
+++ clutter/x11/clutter-device-manager-xi2.c
@@ -1345,8 +1345,8 @@ relate_slaves (gpointer key,
   ClutterDeviceManagerXI2 *manager_xi2 = data;
   ClutterInputDevice *master, *slave;
 
-  master = g_hash_table_lookup (manager_xi2->devices_by_id, key);
-  slave = g_hash_table_lookup (manager_xi2->devices_by_id, value);
+  slave = g_hash_table_lookup (manager_xi2->devices_by_id, key);
+  master = g_hash_table_lookup (manager_xi2->devices_by_id, value);
 
   _clutter_input_device_set_associated_device (slave, master);
   _clutter_input_device_add_slave (master, slave);
