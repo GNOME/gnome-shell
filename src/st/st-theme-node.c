GNOME/gnome-shell

commit dc2ec0a8f95baa21aa460d49ed1ac36d398f62f4

Keep similar theme nodes so we don't have to recompute CSS so often
Because we calculate and cache CSS properties once per StThemeNode,
and only a certain set of attributes can affect the CSS properties,
it's advantageous for as many widgets as possible to share a single
StThemeNode. Similarly, if a widget changes state and then changes back
(e.g. gaining and losing the :hover pseudo-class), it should ideally
get its original StThemeNode back again when it returns to the old
state.

Here, I'm using the StThemeContext as the location for a cache.
StThemeNodes are currently never freed: this seems OK for Shell's usage
(a finite number of IDs, classes, pseudo-classes and types).

Bug: https://bugzilla.gnome.org/show_bug.cgi?id=687465
Reviewed-by: Jasper St. Pierre <jstpierre@mecheye.net>

src/st/st-theme-node.c
@@ -390,6 +390,40 @@ st_theme_node_equal (StThemeNode *node_a, StThemeNode *node_b)
   return TRUE;
 }
 
+guint
+st_theme_node_hash (StThemeNode *node)
+{
+  guint hash = GPOINTER_TO_UINT (node->parent_node);
+
+  hash = hash * 33 + GPOINTER_TO_UINT (node->context);
+  hash = hash * 33 + GPOINTER_TO_UINT (node->theme);
+  hash = hash * 33 + ((guint) node->element_type);
+
+  if (node->element_id != NULL)
+    hash = hash * 33 + g_str_hash (node->element_id);
+
+  if (node->inline_style != NULL)
+    hash = hash * 33 + g_str_hash (node->inline_style);
+
+  if (node->element_classes != NULL)
+    {
+      gchar **it;
+
+      for (it = node->element_classes; *it != NULL; it++)
+        hash = hash * 33 + g_str_hash (*it) + 1;
+    }
+
+  if (node->pseudo_classes != NULL)
+    {
+      gchar **it;
+
+      for (it = node->pseudo_classes; *it != NULL; it++)
+        hash = hash * 33 + g_str_hash (*it) + 1;
+    }
+
+  return hash;
+}
+
 static void
 ensure_properties (StThemeNode *node)
 {

src/st/st-theme-context.c
@@ -31,6 +31,9 @@ struct _StThemeContext {
   PangoFontDescription *font;
   StThemeNode *root_node;
   StTheme *theme;
+
+  /* set of StThemeNode */
+  GHashTable *nodes;
 };
 
 struct _StThemeContextClass {
@@ -66,6 +69,8 @@ st_theme_context_finalize (GObject *object)
                                         (gpointer) st_theme_context_changed,
                                         context);
 
+  if (context->nodes)
+    g_hash_table_unref (context->nodes);
   if (context->root_node)
     g_object_unref (context->root_node);
   if (context->theme)
@@ -105,6 +110,10 @@ st_theme_context_init (StThemeContext *context)
                             "resolution-changed",
                             G_CALLBACK (st_theme_context_changed),
                             context);
+
+  context->nodes = g_hash_table_new_full ((GHashFunc) st_theme_node_hash,
+                                          (GEqualFunc) st_theme_node_equal,
+                                          g_object_unref, NULL);
 }
 
 /**
@@ -146,6 +155,7 @@ st_theme_context_changed (StThemeContext *context)
 {
   StThemeNode *old_root = context->root_node;
   context->root_node = NULL;
+  g_hash_table_remove_all (context->nodes);
 
   emit_changed (context);
 
@@ -298,4 +308,28 @@ st_theme_context_get_root_node (StThemeContext *context)
                                             G_TYPE_NONE, NULL, NULL, NULL, NULL);
 
   return context->root_node;
+}
+
+/**
+ * st_theme_context_intern_node:
+ * @context: a #StThemeContext
+ * @node: a #StThemeNode
+ *
+ * Return an existing node matching @node, or if that isn't possible,
+ * @node itself.
+ *
+ * Return value: (transfer none): a node with the same properties as @node
+ */
+StThemeNode *
+st_theme_context_intern_node (StThemeContext *context,
+                              StThemeNode    *node)
+{
+  StThemeNode *mine = g_hash_table_lookup (context->nodes, node);
+
+  /* this might be node or not - it doesn't actually matter */
+  if (mine != NULL)
+    return mine;
+
+  g_hash_table_add (context->nodes, g_object_ref (node));
+  return node;
 }

src/st/st-theme-context.h
@@ -62,6 +62,9 @@ const PangoFontDescription *st_theme_context_get_font       (StThemeContext     
 
 StThemeNode *               st_theme_context_get_root_node  (StThemeContext             *context);
 
+StThemeNode *               st_theme_context_intern_node    (StThemeContext             *context,
+                                                             StThemeNode                *node);
+
 G_END_DECLS
 
 #endif /* __ST_THEME_CONTEXT_H__ */

src/st/st-theme-node.h
@@ -110,6 +110,7 @@ StThemeNode *st_theme_node_get_parent (StThemeNode *node);
 StTheme *st_theme_node_get_theme (StThemeNode *node);
 
 gboolean    st_theme_node_equal (StThemeNode *node_a, StThemeNode *node_b);
+guint       st_theme_node_hash  (StThemeNode *node);
 
 GType       st_theme_node_get_element_type  (StThemeNode *node);
 const char *st_theme_node_get_element_id    (StThemeNode *node);

src/st/st-widget.c
@@ -589,6 +589,8 @@ st_widget_get_theme_node (StWidget *widget)
 
   if (priv->theme_node == NULL)
     {
+      StThemeContext *context;
+      StThemeNode *tmp_node;
       StThemeNode *parent_node = NULL;
       ClutterStage *stage = NULL;
       ClutterActor *parent;
@@ -629,16 +631,20 @@ st_widget_get_theme_node (StWidget *widget)
       else
         pseudo_class = direction_pseudo_class;
 
-      priv->theme_node = st_theme_node_new (st_theme_context_get_for_stage (stage),
-                                            parent_node, priv->theme,
-                                            G_OBJECT_TYPE (widget),
-                                            clutter_actor_get_name (CLUTTER_ACTOR (widget)),
-                                            priv->style_class,
-                                            pseudo_class,
-                                            priv->inline_style);
+      context = st_theme_context_get_for_stage (stage);
+      tmp_node = st_theme_node_new (context, parent_node, priv->theme,
+                                    G_OBJECT_TYPE (widget),
+                                    clutter_actor_get_name (CLUTTER_ACTOR (widget)),
+                                    priv->style_class,
+                                    pseudo_class,
+                                    priv->inline_style);
 
       if (pseudo_class != direction_pseudo_class)
         g_free (pseudo_class);
+
+      priv->theme_node = g_object_ref (st_theme_context_intern_node (context,
+                                                                     tmp_node));
+      g_object_unref (tmp_node);
     }
 
   return priv->theme_node;

commit 655dce6a4b2d5ad02a3e108b6fbc118f4a3ae2d2

st-theme-node: Recalculate properties on stylesheet changes
As theme nodes keep a cache of matched properties, we need to make
sure to update it when the list of stylesheets changes. In particular
this fixes a regression from commit dc2ec0a8f95baa, which caused
extensions with stylesheets to crash the shell when re-enabled (for
instances when coming back from the lock screen).

https://bugzilla.gnome.org/show_bug.cgi?id=692994

src/st/st-theme-node.c
@@ -61,6 +61,14 @@ st_theme_node_class_init (StThemeNodeClass *klass)
   object_class->finalize = st_theme_node_finalize;
 }
 
+static void
+on_custom_stylesheets_changed (StTheme *theme,
+                               gpointer data)
+{
+  StThemeNode *node = data;
+  node->properties_computed = FALSE;
+}
+
 
 static void
 st_theme_node_dispose (GObject *gobject)
@@ -84,6 +92,10 @@ st_theme_node_dispose (GObject *gobject)
       st_icon_colors_unref (node->icon_colors);
       node->icon_colors = NULL;
     }
+
+  if (node->theme)
+    g_signal_handlers_disconnect_by_func (node->theme,
+                                          on_custom_stylesheets_changed, node);
 
   g_clear_object (&node->theme);
 
@@ -221,10 +233,11 @@ st_theme_node_new (StThemeContext    *context,
     theme = parent_node->theme;
 
   if (theme != NULL)
-    node->theme = theme;
-
-  if (node->theme != NULL)
-    g_object_ref (node->theme);
+    {
+      node->theme = g_object_ref (theme);
+      g_signal_connect (node->theme, "custom-stylesheets-changed",
+                        G_CALLBACK (on_custom_stylesheets_changed), node);
+    }
 
   node->element_type = element_type;
   node->element_id = g_strdup (element_id);