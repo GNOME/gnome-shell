GNOME/gnome-shell

commit fcc32fca7eff7939d75a4f7e3993aca6d85e987e

sessionMode: Add stylesheetName property
This will allow to visually differentiate modes by providing
separate styling.

https://bugzilla.gnome.org/show_bug.cgi?id=693219

js/ui/main.js
@@ -95,6 +95,8 @@ function start() {
     Gio.DesktopAppInfo.set_desktop_env('GNOME');
 
     sessionMode = new SessionMode.SessionMode();
+    sessionMode.connect('updated', _loadDefaultStylesheet);
+
     shellDBusService = new ShellDBus.GnomeShell();
     shellMountOpDBusService = new ShellMountOperation.GnomeShellMountOpHandler();
 
@@ -117,8 +119,7 @@ function start() {
     global.stage.color = DEFAULT_BACKGROUND_COLOR;
     global.stage.no_clear_hint = true;
 
-    _defaultCssStylesheet = global.datadir + '/theme/gnome-shell.css';
-    loadTheme();
+    _loadDefaultStylesheet();
 
     // Set up stage hierarchy to group all UI actors under one container.
     uiGroup = new Shell.GenericContainer({ name: 'uiGroup' });
@@ -377,6 +378,18 @@ function _nWorkspacesChanged() {
     _queueCheckWorkspaces();
 
     return false;
+}
+
+function _loadDefaultStylesheet() {
+    if (!Main.sessionMode.isPrimary)
+        return;
+
+    let stylesheet = global.datadir + '/theme/' + sessionMode.stylesheetName;
+    if (_defaultCssStylesheet == stylesheet)
+        return;
+
+    _defaultCssStylesheet = stylesheet;
+    loadTheme();
 }
 
 /**

js/ui/sessionMode.js
@@ -15,6 +15,7 @@ const DEFAULT_MODE = 'restrictive';
 const _modes = {
     'restrictive': {
         parentMode: null,
+        stylesheetName: 'gnome-shell.css',
         hasOverview: false,
         showCalendarEvents: false,
         allowSettings: false,

commit 67c7abaac3d8b75a5127e52a1f1167052b9dd25b

main: Fix typo
This fixes a regression from commit fcc32fca.

js/ui/main.js
@@ -381,7 +381,7 @@ function _nWorkspacesChanged() {
 }
 
 function _loadDefaultStylesheet() {
-    if (!Main.sessionMode.isPrimary)
+    if (!sessionMode.isPrimary)
         return;
 
     let stylesheet = global.datadir + '/theme/' + sessionMode.stylesheetName;