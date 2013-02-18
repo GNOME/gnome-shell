GNOME/gnome-shell

commit ccb81919f79f9f684234deb8f6b1fcdaae3e7965

overviewControls: update slide in translations from pageEmpty
Now that we unified animation times, move in controls together with the
new view.

https://bugzilla.gnome.org/show_bug.cgi?id=694064

js/ui/overviewControls.js
@@ -133,12 +133,12 @@ const SlidingControl = new Lang.Class({
             return visibleWidth;
     },
 
-    _updateTranslation: function(slidingIn) {
+    _updateTranslation: function() {
         let translationStart = 0;
         let translationEnd = 0;
         let translation = this._getTranslation();
 
-        if (slidingIn) {
+        if (this.visible) {
             translationStart = translation;
         } else {
             translationEnd = translation;
@@ -171,14 +171,13 @@ const SlidingControl = new Lang.Class({
     },
 
     slideIn: function() {
-        this._updateTranslation(true);
         this.visible = true;
-        // we will update slideX from pageEmpty
+        // we will update slideX and the translation from pageEmpty
     },
 
     slideOut: function() {
         this.visible = false;
-        this._updateTranslation(false);
+        this._updateTranslation();
         // we will update slideX from pageEmpty
     },
 
@@ -188,6 +187,7 @@ const SlidingControl = new Lang.Class({
         // the next page, since slideIn or slideOut might have been called,
         // changing the visiblity
         this.layout.slideX = this.getSlide();
+        this._updateTranslation();
     }
 });
 

commit 3056fc471008ca6f0afe199cc16961bd1cda4bcf

overviewControls: don't perform translation when target is met already
Fixes a regression from ccb81919f79f9f684234deb8f6b1fcdaae3e7965

js/ui/overviewControls.js
@@ -144,6 +144,9 @@ const SlidingControl = new Lang.Class({
             translationEnd = translation;
         }
 
+        if (this.actor.translation_x == translationEnd)
+            return;
+
         this.actor.translation_x = translationStart;
         Tweener.addTween(this.actor, { translation_x: translationEnd,
                                        time: SIDE_CONTROLS_ANIMATION_TIME,