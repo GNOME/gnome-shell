e85a681788096b9923e0b598d3f06f3fb854fd76
Neil Roberts <neil@linux.intel.com>
2013-04-24 18:00:49 +0100

    cogl-gst: Remove ‘FLOP’
    This is an inappropriately pessimistic remark for the header of
    CoglGstVideoSink.
    
    Reviewed-by: Robert Bragg <robert@linux.intel.com>
    
    (cherry picked from commit ce2a70165054fea3e9d992c14650ba8cccf60e97)

--- cogl-gst/cogl-gst-video-sink.h
+++ cogl-gst/cogl-gst-video-sink.h
@@ -29,7 +29,6 @@
  * License along with this library; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
- FLOP
  */
 
 #ifndef __COGL_GST_VIDEO_SINK_H__
