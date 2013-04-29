GNOME/gnome-shell

commit 59801ef854016cc85c6bcf8b6ed8a77081f433fb

drag-action: fix press coords transform with drag_handle
When setting a drag handle, transform the original press
coordinates using the drag handle as reference instead of the
associated actor.

This causes the initial misplacement of drag handle in
example/drag-action when holding down the Shift key: the handle
gets placed at the main actor origin on the first drag event,
instead of following the mouse pointer.

All subsequent motion events already use the right actor when
transforming the coordinates, thus they are not affected.

https://bugzilla.gnome.org/show_bug.cgi?id=681746

clutter/clutter-drag-action.c
@@ -1106,6 +1106,12 @@ clutter_drag_action_set_drag_handle (ClutterDragAction *action,
 
   priv->drag_handle = handle;
 
+  priv->transformed_press_x = priv->press_x;
+  priv->transformed_press_y = priv->press_y;
+  clutter_actor_transform_stage_point (handle, priv->press_x, priv->press_y,
+                                       &priv->transformed_press_x,
+                                       &priv->transformed_press_y);
+
   if (priv->drag_handle != NULL)
     g_signal_connect (priv->drag_handle, "destroy",
                       G_CALLBACK (on_drag_handle_destroy),

commit a0d9eaf15da5d2227bc95c6946fca5ed356fdfdb

fix crash when destroying an actor during a drag-action with a drag_handle
Bug introduced by commit 59801ef8 (drag-action: fix press coords transform
with drag_handle) for bgo#681746.

See also https://bugzilla.gnome.org/show_bug.cgi?id=681814

clutter/clutter-drag-action.c
@@ -1139,16 +1139,19 @@ clutter_drag_action_set_drag_handle (ClutterDragAction *action,
 
   priv->drag_handle = handle;
 
-  priv->transformed_press_x = priv->press_x;
-  priv->transformed_press_y = priv->press_y;
-  clutter_actor_transform_stage_point (handle, priv->press_x, priv->press_y,
-                                       &priv->transformed_press_x,
-                                       &priv->transformed_press_y);
-
   if (priv->drag_handle != NULL)
-    g_signal_connect (priv->drag_handle, "destroy",
-                      G_CALLBACK (on_drag_handle_destroy),
-                      action);
+    {
+      priv->transformed_press_x = priv->press_x;
+      priv->transformed_press_y = priv->press_y;
+      clutter_actor_transform_stage_point (priv->drag_handle, priv->press_x,
+                                           priv->press_y,
+                                           &priv->transformed_press_x,
+                                           &priv->transformed_press_y);
+
+      g_signal_connect (priv->drag_handle, "destroy",
+                        G_CALLBACK (on_drag_handle_destroy),
+                        action);
+    }
 
   g_object_notify_by_pspec (G_OBJECT (action), drag_props[PROP_DRAG_HANDLE]);
 }