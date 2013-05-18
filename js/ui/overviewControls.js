d658ec8de27ee1a320ec58b8a87f62a2842c3ba6
Florian Müllner <fmuellner@gnome.org>
2013-05-18 20:59:57 +0200

    overviewControls: Move reactive flag to main actor
    The main overview actor was made reactive to catch scroll-events
    and propagate them; after some code shuffling, the actor that
    catches scroll events ended up not being the same actor that's
    supposed to propagate this, which broke using the scroll wheel
    to switch workspaces.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=700595

--- js/ui/overviewControls.js
+++ js/ui/overviewControls.js
@@ -506,10 +506,10 @@ const ControlsManager = new Lang.Class({
         this.indicatorActor = this._indicator.actor;
 
         this.actor = new St.Widget({ layout_manager: new Clutter.BinLayout(),
+                                     reactive: true,
                                      x_expand: true, y_expand: true,
                                      clip_to_allocation: true });
         this._group = new St.BoxLayout({ name: 'overview-group',
-                                        reactive: true,
                                         x_expand: true, y_expand: true });
         this.actor.add_actor(this._group);
 
