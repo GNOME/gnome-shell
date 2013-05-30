f88f51dd99b8a70ec3669ad8da4498e6b79c2c1f
Adel Gadllah <adel.gadllah@gmail.com>
2013-05-30 10:43:23 +0200

    osdWindow: Disable unredirection while showing the OSD
    This allows OSDs to be visible even when displayed on top
    of unredirected windows.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=701224

--- js/ui/osdWindow.js
+++ js/ui/osdWindow.js
@@ -136,6 +136,7 @@ const OsdWindow = new Lang.Class({
             return;
 
         if (!this.actor.visible) {
+            Meta.disable_unredirect_for_screen(global.screen);
             this.actor.show();
             this.actor.opacity = 0;
             this.actor.get_parent().set_child_above_sibling(this.actor, null);
@@ -173,6 +174,7 @@ const OsdWindow = new Lang.Class({
         this.actor.hide();
         this.setLabel(null);
         this.setLevel(null);
+        Meta.enable_unredirect_for_screen(global.screen);
     },
 
     _monitorsChanged: function() {
