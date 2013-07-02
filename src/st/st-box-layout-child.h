144f7d6813a9c2e43bc2b17055a5337db97d05d3
Florian Müllner <fmuellner@gnome.org>
2013-07-02 23:08:10 +0200

    st: Translate StBoxLayoutChild properties to ClutterBoxChild
    With StBoxLayout using the Clutter layout manager, it will now respect
    actors' expand/align properties. However for the change to be transparent,
    we need to support the existing child meta properties as well. Do
    this by simply translating them to the corresponding ClutterBoxChild
    properties.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=703810

--- src/st/st-box-layout-child.h
+++ src/st/st-box-layout-child.h
@@ -63,11 +63,8 @@ struct _StBoxLayoutChild
   /*< private >*/
   ClutterChildMeta parent;
 
-  gboolean expand;
-  gboolean x_fill : 1;
-  gboolean y_fill : 1;
-  StAlign x_align;
-  StAlign y_align;
+  gboolean x_fill_set;
+  gboolean y_fill_set;
 };
 
 struct _StBoxLayoutChildClass
