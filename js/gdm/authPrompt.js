58ca6ec6aa8706dc8f3ef47b3ead55b303578838
Ray Strode <rstrode@redhat.com>
2013-07-28 16:15:43 -0400

    authPrompt: don't muck with cancelButton in onAskQuestion
    onAskQuestion has this code:
    
        if (this.verifyingUser)
            this.cancelButton.show();
        else
            this.cancelButton.hide();
    
    but onAskQuestion can only be called when this.verifyingUser is true.
    Also, cancelButton is public, and it only ever otherwise gets hidden
    from callers.
    
    This commit drops mucking with cancelButton visibility, leaving it
    entirely up to the callers to deal with.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=683437

--- js/gdm/authPrompt.js
+++ js/gdm/authPrompt.js
@@ -203,11 +203,6 @@ const AuthPrompt = new Lang.Class({
         this.setPasswordChar(passwordChar);
         this.setQuestion(question);
 
-        if (this.verifyingUser)
-            this.cancelButton.show();
-        else
-            this.cancelButton.hide();
-
         if (passwordChar) {
             if (this._userVerifier.reauthenticating)
                 this.nextButton.label = _("Unlock");
