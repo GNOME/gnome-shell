rib/rig

texture-2d: Add _new_from_file api
This adds a cogl_texture_2d_new_from_file() api since we are planning to
remove cogl_texture_new_from_file() but don't want to loose the
convenience it had.

Reviewed-by: Neil Roberts <neil@linux.intel.com>

cogl/cogl-texture-2d.c
@@ -192,6 +192,31 @@ cogl_texture_2d_new_from_bitmap (CoglBitmap *bmp,
 }
 
 CoglTexture2D *
+cogl_texture_2d_new_from_file (CoglContext *ctx,
+                               const char *filename,
+                               CoglPixelFormat internal_format,
+                               CoglError **error)
+{
+  CoglBitmap *bmp;
+  CoglTexture2D *tex_2d = NULL;
+
+  _COGL_RETURN_VAL_IF_FAIL (error == NULL || *error == NULL, NULL);
+
+  bmp = cogl_bitmap_new_from_file (ctx, filename, error);
+  if (bmp == NULL)
+    return NULL;
+
+  tex_2d = _cogl_texture_2d_new_from_bitmap (bmp,
+                                             internal_format,
+                                             TRUE, /* can convert in-place */
+                                             error);
+
+  cogl_object_unref (bmp);
+
+  return tex_2d;
+}
+
+CoglTexture2D *
 cogl_texture_2d_new_from_data (CoglContext *ctx,
                                int width,
                                int height,

GNOME/gnome-shell

(cherry picked from commit 10e91aa513123ed277a8d45976f8d75445d7dc9c)

cogl/cogl-texture-2d.c
@@ -192,6 +192,31 @@ cogl_texture_2d_new_from_bitmap (CoglBitmap *bmp,
 }
 
 CoglTexture2D *
+cogl_texture_2d_new_from_file (CoglContext *ctx,
+                               const char *filename,
+                               CoglPixelFormat internal_format,
+                               CoglError **error)
+{
+  CoglBitmap *bmp;
+  CoglTexture2D *tex_2d = NULL;
+
+  _COGL_RETURN_VAL_IF_FAIL (error == NULL || *error == NULL, NULL);
+
+  bmp = _cogl_bitmap_from_file (ctx, filename, error);
+  if (bmp == NULL)
+    return NULL;
+
+  tex_2d = _cogl_texture_2d_new_from_bitmap (bmp,
+                                             internal_format,
+                                             TRUE, /* can convert in-place */
+                                             error);
+
+  cogl_object_unref (bmp);
+
+  return tex_2d;
+}
+
+CoglTexture2D *
 cogl_texture_2d_new_from_data (CoglContext *ctx,
                                int width,
                                int height,