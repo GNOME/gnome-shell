ab4c929a071bf19985e1060cd621d14d68d35788
Jasper St. Pierre <jstpierre@mecheye.net>
2013-08-27 16:17:34 -0400

    shaped-texture: Turn blending off when drawing entirely opaque regions
    When drawing entirely opaque regions, we traditionally kept blending on
    simply because it made the code more convenient and obvious to handle.
    However, this can cause lots of performance issues on GPUs that aren't
    too powerful, as they have to readback the buffer underneath.
    
    Keep track of the opaque region set by windows (through _NET_WM_OPAQUE_REGION,
    standard RGB32 frame masks or similar), and draw those rectangles
    separately through a different path with blending turned off.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=706930

--- src/meta/meta-shaped-texture.h
+++ src/meta/meta-shaped-texture.h
@@ -87,6 +87,9 @@ void meta_shaped_texture_set_mask_texture (MetaShapedTexture *stex,
 void meta_shaped_texture_set_clip_region (MetaShapedTexture *stex,
 					  cairo_region_t    *clip_region);
 
+void meta_shaped_texture_set_opaque_region (MetaShapedTexture *stex,
+                                            cairo_region_t    *opaque_region);
+
 cairo_surface_t * meta_shaped_texture_get_image (MetaShapedTexture     *stex,
                                                  cairo_rectangle_int_t *clip);
 
