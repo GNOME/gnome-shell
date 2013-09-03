6e3e14bb504867d6f33d859e14bd19f2fcb60bd3
Lionel Landwerlin <llandwerlin@gmail.com>
2013-09-03 14:06:41 +0100

    framebuffer: add missing transfer annotation
    Reviewed-by: Robert Bragg <robert@linux.intel.com>

--- cogl/cogl-framebuffer.h
+++ cogl/cogl-framebuffer.h
@@ -1015,8 +1015,8 @@ cogl_framebuffer_resolve_samples_region (CoglFramebuffer *framebuffer,
  * instantiated within. This is the #CoglContext that was passed to
  * cogl_onscreen_new() for example.
  *
- * Return value: The #CoglContext that the given @framebuffer was
- *               instantiated within.
+ * Return value: (transfer none): The #CoglContext that the given
+ *               @framebuffer was instantiated within.
  * Since: 1.8
  * Stability: unstable
  */
@@ -1731,7 +1731,7 @@ cogl_framebuffer_read_pixels (CoglFramebuffer *framebuffer,
  * Gets the current #CoglFramebuffer as set using
  * cogl_push_framebuffer()
  *
- * Return value: The current #CoglFramebuffer
+ * Return value: (transfer none): The current #CoglFramebuffer
  * Stability: unstable
  * Since: 1.8
  */
