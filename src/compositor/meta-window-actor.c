2d67b01c3a706b45d1c2a32b4d5c5c5487fe06f6
Adel Gadllah <adel.gadllah@gmail.com>
2013-09-04 19:43:08 +0200

    window-actor: Use more conservative paint volume
    We know which part of the window is not obscured, so take advantaged of that
    to limit the size of the paint volume that we report.

--- src/compositor/meta-window-actor.c
+++ src/compositor/meta-window-actor.c
@@ -767,6 +767,9 @@ meta_window_actor_get_paint_volume (ClutterActor       *actor,
       gdk_rectangle_union (&bounds, &shadow_bounds, &bounds);
     }
 
+  if (priv->unobscured_region)
+    cairo_region_intersect_rectangle (priv->unobscured_region, &bounds);
+
   origin.x = bounds.x;
   origin.y = bounds.y;
   origin.z = 0.0f;
