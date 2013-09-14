3691e8ddd75889791a5d3e6aa2518fb5ceda5d71
Joanmarie Diggs <jdiggs@igalia.com>
2013-09-14 13:43:45 -0400

    Remove "Menu" from the name of the Settings menu
    Its presence makes Orca present "Settings Menu menu". Removing it does
    not introduce any new strings.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=708080

--- js/ui/panel.js
+++ js/ui/panel.js
@@ -804,7 +804,7 @@ const AggregateMenu = new Lang.Class({
     Extends: PanelMenu.Button,
 
     _init: function() {
-        this.parent(0.0, _("Settings Menu"), false);
+        this.parent(0.0, _("Settings"), false);
         this.menu.actor.add_style_class_name('aggregate-menu');
 
         this._indicators = new St.BoxLayout({ style_class: 'panel-status-indicators-box' });
