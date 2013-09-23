GNOME/gnome-shell

commit 7b25c8f5caad6f09543f001da3630e1bc595b4ab

attribute: add missing transfer annotations
Reviewed-by: Robert Bragg <robert@linux.intel.com>

cogl/cogl-attribute-buffer.c
@@ -37,6 +37,7 @@
 static void _cogl_attribute_buffer_free (CoglAttributeBuffer *array);
 
 COGL_BUFFER_DEFINE (AttributeBuffer, attribute_buffer);
+COGL_GTYPE_DEFINE_CLASS (AttributeBuffer, attribute_buffer);
 
 CoglAttributeBuffer *
 cogl_attribute_buffer_new_with_size (CoglContext *context,

cogl/cogl-attribute.h
@@ -130,8 +130,9 @@ COGL_BEGIN_DECLS
  * mapped into the GPU which can be a bottlneck when dealing with
  * a large number of vertices.
  *
- * Returns: A newly allocated #CoglAttribute describing the
- *          layout for a list of attribute values stored in @array.
+ * Return value: (transfer full): A newly allocated #CoglAttribute
+ *          describing the layout for a list of attribute values
+ *          stored in @array.
  *
  * Since: 1.4
  * Stability: Unstable
@@ -163,8 +164,8 @@ cogl_attribute_new (CoglAttributeBuffer *attribute_buffer,
  * attribute float name;
  * |]
  *
- * Returns: A newly allocated #CoglAttribute representing the given
- *          constant @value.
+ * Return value: (transfer full): A newly allocated #CoglAttribute
+ *          representing the given constant @value.
  */
 CoglAttribute *
 cogl_attribute_new_const_1f (CoglContext *context,
@@ -190,8 +191,8 @@ cogl_attribute_new_const_1f (CoglContext *context,
  * attribute vec2 name;
  * |]
  *
- * Returns: A newly allocated #CoglAttribute representing the given
- *          constant vector.
+ * Return value: (transfer full): A newly allocated #CoglAttribute
+ *          representing the given constant vector.
  */
 CoglAttribute *
 cogl_attribute_new_const_2f (CoglContext *context,
@@ -222,8 +223,8 @@ cogl_attribute_new_const_2f (CoglContext *context,
  * unless the built in name "cogl_normal_in" is being used where no
  * explicit GLSL declaration need be made.
  *
- * Returns: A newly allocated #CoglAttribute representing the given
- *          constant vector.
+ * Return value: (transfer full): A newly allocated #CoglAttribute
+ *          representing the given constant vector.
  */
 CoglAttribute *
 cogl_attribute_new_const_3f (CoglContext *context,
@@ -257,8 +258,8 @@ cogl_attribute_new_const_3f (CoglContext *context,
  * "cogl_tex_coord0_in or "cogl_tex_coord1_in" etc is being used where
  * no explicit GLSL declaration need be made.
  *
- * Returns: A newly allocated #CoglAttribute representing the given
- *          constant vector.
+ * Return value: (transfer full): A newly allocated #CoglAttribute
+ *          representing the given constant vector.
  */
 CoglAttribute *
 cogl_attribute_new_const_4f (CoglContext *context,
@@ -286,8 +287,8 @@ cogl_attribute_new_const_4f (CoglContext *context,
  * attribute vec2 name;
  * |]
  *
- * Returns: A newly allocated #CoglAttribute representing the given
- *          constant vector.
+ * Return value: (transfer full): A newly allocated #CoglAttribute
+ *          representing the given constant vector.
  */
 CoglAttribute *
 cogl_attribute_new_const_2fv (CoglContext *context,
@@ -315,8 +316,8 @@ cogl_attribute_new_const_2fv (CoglContext *context,
  * unless the built in name "cogl_normal_in" is being used where no
  * explicit GLSL declaration need be made.
  *
- * Returns: A newly allocated #CoglAttribute representing the given
- *          constant vector.
+ * Return value: (transfer full): A newly allocated #CoglAttribute
+ *          representing the given constant vector.
  */
 CoglAttribute *
 cogl_attribute_new_const_3fv (CoglContext *context,
@@ -345,8 +346,8 @@ cogl_attribute_new_const_3fv (CoglContext *context,
  * "cogl_tex_coord0_in or "cogl_tex_coord1_in" etc is being used where
  * no explicit GLSL declaration need be made.
  *
- * Returns: A newly allocated #CoglAttribute representing the given
- *          constant vector.
+ * Return value: (transfer full): A newly allocated #CoglAttribute
+ *          representing the given constant vector.
  */
 CoglAttribute *
 cogl_attribute_new_const_4fv (CoglContext *context,
@@ -378,8 +379,8 @@ cogl_attribute_new_const_4fv (CoglContext *context,
  * around the diagonal of the matrix such that the first column
  * becomes the first row and the second column becomes the second row.
  *
- * Returns: A newly allocated #CoglAttribute representing the given
- *          constant matrix.
+ * Return value: (transfer full): A newly allocated #CoglAttribute
+ *          representing the given constant matrix.
  */
 CoglAttribute *
 cogl_attribute_new_const_2x2fv (CoglContext *context,
@@ -413,8 +414,8 @@ cogl_attribute_new_const_2x2fv (CoglContext *context,
  * becomes the first row and the second column becomes the second row
  * etc.
  *
- * Returns: A newly allocated #CoglAttribute representing the given
- *          constant matrix.
+ * Return value: (transfer full): A newly allocated #CoglAttribute
+ *          representing the given constant matrix.
  */
 CoglAttribute *
 cogl_attribute_new_const_3x3fv (CoglContext *context,
@@ -448,8 +449,8 @@ cogl_attribute_new_const_3x3fv (CoglContext *context,
  * becomes the first row and the second column becomes the second row
  * etc.
  *
- * Returns: A newly allocated #CoglAttribute representing the given
- *          constant matrix.
+ * Return value: (transfer full): A newly allocated #CoglAttribute
+ *          representing the given constant matrix.
  */
 CoglAttribute *
 cogl_attribute_new_const_4x4fv (CoglContext *context,
@@ -496,8 +497,8 @@ cogl_attribute_get_normalized (CoglAttribute *attribute);
  * cogl_attribute_get_buffer:
  * @attribute: A #CoglAttribute
  *
- * Return value: the #CoglAttributeBuffer that was set with
- * cogl_attribute_set_buffer() or cogl_attribute_new().
+ * Return value: (transfer none): the #CoglAttributeBuffer that was
+ *        set with cogl_attribute_set_buffer() or cogl_attribute_new().
  *
  * Stability: unstable
  * Since: 1.10

commit 3b14acdbcc39a43cdeac65fd9e49775f887fe5ef

remove spurious COGL_GTYPE_DEFINE_CLASS reference
Commit 7b25c8f5caad mistakenly added a reference to a
COGL_GTYPE_DEFINE_CLASS() macro that does not yet exist so this patch
removes it.

Reviewed-by: Neil Roberts <neil@linux.intel.com>

cogl/cogl-attribute-buffer.c
@@ -37,7 +37,6 @@
 static void _cogl_attribute_buffer_free (CoglAttributeBuffer *array);
 
 COGL_BUFFER_DEFINE (AttributeBuffer, attribute_buffer);
-COGL_GTYPE_DEFINE_CLASS (AttributeBuffer, attribute_buffer);
 
 CoglAttributeBuffer *
 cogl_attribute_buffer_new_with_size (CoglContext *context,