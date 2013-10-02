8737b065592a7b2e9a6307e5a1e44074b10eb44e
Jasper St. Pierre <jstpierre@mecheye.net>
2013-10-02 13:09:14 -0400

    loginDialog: MessageType is now in GdmUtil
    https://bugzilla.gnome.org/show_bug.cgi?id=709286

--- js/gdm/loginDialog.js
+++ js/gdm/loginDialog.js
@@ -619,7 +619,7 @@ const LoginDialog = new Lang.Class({
 
         // Translators: this message is shown below the username entry field
         // to clue the user in on how to login to the local network realm
-        this._authPrompt.setMessage(_("(e.g., user or %s)").format(hint), AuthPrompt.MessageType.HINT);
+        this._authPrompt.setMessage(_("(e.g., user or %s)").format(hint), GdmUtil.MessageType.HINT);
     },
 
     _askForUsernameAndBeginVerification: function() {
