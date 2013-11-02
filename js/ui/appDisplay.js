3cc711228361e0163957730260b80e23e7421c8e
Matthias Clasen <mclasen@redhat.com>
2013-11-02 15:36:34 -0400

    Add a 'Show Details' menuitem in the app picker
    Add a menu item that shows the details of the application
    in gnome-software.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=643043

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1602,6 +1602,26 @@ const AppIconMenu = new Lang.Class({
                     favs.addFavorite(this._source.app.get_id());
                 }));
             }
+
+            if (Shell.AppSystem.get_default().lookup_app('org.gnome.Software.desktop')) {
+                this._appendSeparator();
+                let item = this._appendMenuItem(_("Show Details"));
+                item.connect('activate', Lang.bind(this, function() {
+                    let id = this._source.app.get_id();
+                    let args = GLib.Variant.new('(ss)', [id, '']);
+                    Gio.DBus.get(Gio.BusType.SESSION, null,
+                        function(o, res) {
+                            let bus = Gio.DBus.get_finish(res);
+                            bus.call('org.gnome.Software',
+                                     '/org/gnome/Software',
+                                     'org.gtk.Actions', 'Activate',
+                                     GLib.Variant.new('(sava{sv})',
+                                                      ['details', [args], null]),
+                                     null, 0, -1, null, null);
+                            Main.overview.hide();
+                        });
+                }));
+            }
         }
     },
 
