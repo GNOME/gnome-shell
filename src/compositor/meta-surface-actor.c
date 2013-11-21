7d88b3593bec4fea3c00fe35b72fc61848a58197
Jasper St. Pierre <jstpierre@mecheye.net>
2013-11-21 17:29:38 -0500

    surface-actor: Remove unused field

--- src/compositor/meta-surface-actor.c
+++ src/compositor/meta-surface-actor.c
@@ -22,7 +22,6 @@ struct _MetaSurfaceActorPrivate
 {
   MetaShapedTexture *texture;
   MetaWaylandBuffer *buffer;
-  Pixmap pixmap;
 };
 
 G_DEFINE_TYPE (MetaSurfaceActor, meta_surface_actor, CLUTTER_TYPE_ACTOR);
