rib/rig

build: fix building introspection data
This fixes the build with --enable-introspection. I'm not sure why
g-ir-scanner seems to parse all public headers in isolation instead of
being able take a more limited list of top-level public headers and
automatically parse all necessary #include directives but this means we
have to special case how we define and undefine __COGL_H_INSIDE__ to
subvert the guards we have in place for detecting misuse of the headers.

Reviewed-by: Neil Roberts <neil@linux.intel.com>

cogl/cogl-xlib-renderer.h
@@ -37,8 +37,16 @@
  * definitions
  */
 #ifndef COGL_COMPILATION
+
+/* Note: When building Cogl .gir we explicitly define
+ * __COGL_H_INSIDE__ */
+#ifndef __COGL_H_INSIDE__
 #define __COGL_H_INSIDE__
+#define __COGL_MUST_UNDEF_COGL_H_INSIDE__
 #endif
+
+#endif /* COGL_COMPILATION */
+
 #include <cogl/cogl-renderer.h>
 
 COGL_BEGIN_DECLS
@@ -157,5 +165,18 @@ Display *
 cogl_xlib_renderer_get_display (CoglRenderer *renderer);
 
 COGL_END_DECLS
+
+/* The gobject introspection scanner seems to parse public headers in
+ * isolation which means we need to be extra careful about how we
+ * define and undefine __COGL_H_INSIDE__ used to detect when internal
+ * headers are incorrectly included by developers. In the gobject
+ * introspection case we have to manually define __COGL_H_INSIDE__ as
+ * a commandline argument for the scanner which means we must be
+ * careful not to undefine it in a header...
+ */
+#ifdef __COGL_MUST_UNDEF_COGL_H_INSIDE__
+#undef __COGL_H_INSIDE__
+#undef __COGL_MUST_UNDEF_COGL_H_INSIDE__
+#endif
 
 #endif /* __COGL_XLIB_RENDERER_H__ */

cogl/cogl-xlib.h
@@ -33,9 +33,21 @@
  * definitions
  */
 #ifndef COGL_COMPILATION
+
+/* Note: When building Cogl .gir we explicitly define
+ * __COGL_XLIB_H_INSIDE__ */
+#ifndef __COGL_XLIB_H_INSIDE__
 #define __COGL_XLIB_H_INSIDE__
-#define __COGL_H_INSIDE__
 #endif
+
+/* Note: When building Cogl .gir we explicitly define
+ * __COGL_H_INSIDE__ */
+#ifndef __COGL_H_INSIDE__
+#define __COGL_H_INSIDE__
+#define __COGL_MUST_UNDEF_COGL_H_INSIDE__
+#endif
+
+#endif /* COGL_COMPILATION */
 
 #include <cogl/cogl-types.h>
 #include <cogl/cogl-clutter-xlib.h>
@@ -64,6 +76,19 @@ cogl_xlib_handle_event (XEvent *xevent);
 
 COGL_END_DECLS
 
+
+/* The gobject introspection scanner seems to parse public headers in
+ * isolation which means we need to be extra careful about how we
+ * define and undefine __COGL_H_INSIDE__ used to detect when internal
+ * headers are incorrectly included by developers. In the gobject
+ * introspection case we have to manually define __COGL_H_INSIDE__ as
+ * a commandline argument for the scanner which means we must be
+ * careful not to undefine it in a header...
+ */
+#ifdef __COGL_MUST_UNDEF_COGL_H_INSIDE__
+#undef __COGL_H_INSIDE__
 #undef __COGL_XLIB_H_INSIDE__
+#undef __COGL_MUST_UNDEF_COGL_H_INSIDE__
+#endif
 
 #endif /* __COGL_XLIB_H__ */

GNOME/gnome-shell

(cherry picked from commit e0b2255876c1cf11d124d5ae37cbe9a6e43777f1)

cogl/cogl-xlib-renderer.h
@@ -37,8 +37,16 @@
  * definitions
  */
 #ifndef COGL_COMPILATION
+
+/* Note: When building Cogl .gir we explicitly define
+ * __COGL_H_INSIDE__ */
+#ifndef __COGL_H_INSIDE__
 #define __COGL_H_INSIDE__
+#define __COGL_XLIB_RENDERER_H_MUST_UNDEF_COGL_H_INSIDE__
 #endif
+
+#endif /* COGL_COMPILATION */
+
 #include <cogl/cogl-renderer.h>
 
 COGL_BEGIN_DECLS
@@ -157,5 +165,18 @@ Display *
 cogl_xlib_renderer_get_display (CoglRenderer *renderer);
 
 COGL_END_DECLS
+
+/* The gobject introspection scanner seems to parse public headers in
+ * isolation which means we need to be extra careful about how we
+ * define and undefine __COGL_H_INSIDE__ used to detect when internal
+ * headers are incorrectly included by developers. In the gobject
+ * introspection case we have to manually define __COGL_H_INSIDE__ as
+ * a commandline argument for the scanner which means we must be
+ * careful not to undefine it in a header...
+ */
+#ifdef __COGL_XLIB_RENDERER_H_MUST_UNDEF_COGL_H_INSIDE__
+#undef __COGL_H_INSIDE__
+#undef __COGL_XLIB_RENDERER_H_MUST_UNDEF_COGL_H_INSIDE__
+#endif
 
 #endif /* __COGL_XLIB_RENDERER_H__ */

cogl/cogl-xlib.h
@@ -33,9 +33,21 @@
  * definitions
  */
 #ifndef COGL_COMPILATION
+
+/* Note: When building Cogl .gir we explicitly define
+ * __COGL_XLIB_H_INSIDE__ */
+#ifndef __COGL_XLIB_H_INSIDE__
 #define __COGL_XLIB_H_INSIDE__
-#define __COGL_H_INSIDE__
 #endif
+
+/* Note: When building Cogl .gir we explicitly define
+ * __COGL_H_INSIDE__ */
+#ifndef __COGL_H_INSIDE__
+#define __COGL_H_INSIDE__
+#define __COGL_XLIB_H_MUST_UNDEF_COGL_H_INSIDE__
+#endif
+
+#endif /* COGL_COMPILATION */
 
 #include <cogl/cogl-types.h>
 #include <cogl/deprecated/cogl-clutter-xlib.h>
@@ -97,6 +109,19 @@ cogl_xlib_handle_event (XEvent *xevent);
 
 COGL_END_DECLS
 
+
+/* The gobject introspection scanner seems to parse public headers in
+ * isolation which means we need to be extra careful about how we
+ * define and undefine __COGL_H_INSIDE__ used to detect when internal
+ * headers are incorrectly included by developers. In the gobject
+ * introspection case we have to manually define __COGL_H_INSIDE__ as
+ * a commandline argument for the scanner which means we must be
+ * careful not to undefine it in a header...
+ */
+#ifdef __COGL_XLIB_H_MUST_UNDEF_COGL_H_INSIDE__
+#undef __COGL_H_INSIDE__
 #undef __COGL_XLIB_H_INSIDE__
+#undef __COGL_XLIB_H_MUST_UNDEF_COGL_H_INSIDE__
+#endif
 
 #endif /* __COGL_XLIB_H__ */