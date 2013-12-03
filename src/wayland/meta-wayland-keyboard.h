b2c18c4a7846bf5e358b18e10b1bb7f2bfa67402
Jasper St. Pierre <jstpierre@mecheye.net>
2013-12-03 10:46:57 -0500

    Revert "keyboard: Remove focus listener"
    This reverts commit 0ac142d39e27e2692cbe0b18136a41bee424a6b0.

--- src/wayland/meta-wayland-keyboard.h
+++ src/wayland/meta-wayland-keyboard.h
@@ -94,6 +94,7 @@ struct _MetaWaylandKeyboard
   struct wl_list resource_list;
   MetaWaylandSurface *focus;
   struct wl_resource *focus_resource;
+  struct wl_listener focus_listener;
   uint32_t focus_serial;
 
   MetaWaylandKeyboardGrab *grab;
