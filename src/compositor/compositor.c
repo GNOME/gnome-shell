0a81314337dce250f05c5e42e9f09df8464e7769
Jasper St. Pierre <jstpierre@mecheye.net>
2013-12-06 17:36:09 -0500

    window-actor: Flip set_redirected around
    I know it's confusing with the triple negative, but unredirected is how
    we track it elsewhere: we have an 'unredirected' flag, and 'should_unredirect'.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=720631

--- src/compositor/compositor.c
+++ src/compositor/compositor.c
@@ -815,7 +815,7 @@ set_unredirected_window (MetaCompScreen *info,
   if (info->unredirected_window != NULL)
     {
       MetaWindowActor *window_actor = META_WINDOW_ACTOR (meta_window_get_compositor_private (info->unredirected_window));
-      meta_window_actor_set_redirected (window_actor, TRUE);
+      meta_window_actor_set_unredirected (window_actor, FALSE);
     }
 
   info->unredirected_window = window;
@@ -823,7 +823,7 @@ set_unredirected_window (MetaCompScreen *info,
   if (info->unredirected_window != NULL)
     {
       MetaWindowActor *window_actor = META_WINDOW_ACTOR (meta_window_get_compositor_private (info->unredirected_window));
-      meta_window_actor_set_redirected (window_actor, FALSE);
+      meta_window_actor_set_unredirected (window_actor, TRUE);
     }
 
   meta_shape_cow_for_window (info->screen, info->unredirected_window);
