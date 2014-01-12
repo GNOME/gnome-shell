9b21346427cfb9432b2727350f3af6129c1eb47a
Andika Triwidada <andika@gmail.com>
2014-01-12 08:42:06 +0700

    Changed obsolete FSF postal address into generic URL. Fix https://bugzilla.gnome.org/show_bug.cgi?id=721517 #2

--- tools/ppa-magic.py
+++ tools/ppa-magic.py
@@ -16,9 +16,7 @@
 # General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-# 02111-1307, USA.
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 import time
 import os
@@ -123,8 +121,7 @@ License:
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this package; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+   along with this package; if not, see <http://www.gnu.org/licenses/>.
 
 On Debian systems, the complete text of the GNU General
 Public License can be found in `/usr/share/common-licenses/GPL'.
