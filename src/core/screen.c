f6e58be4b0582476f443360168e533ae39427aa3
Jasper St. Pierre <jstpierre@mecheye.net>
2014-01-31 14:09:26 -0500

    screen: Make the guard window an InputOnly window
    Using a full InputOutput window causes us to make a full Wayland surface
    for it, and go through the X server. As the goal of the guard window is
    a window for us to stack minimized windows under so we can prevent them
    from getting input, it makes sense to use an InputOnly window here.

--- src/core/screen.c
+++ src/core/screen.c
@@ -452,10 +452,9 @@ create_guard_window (Display *xdisplay, MetaScreen *screen)
   XSetWindowAttributes attributes;
   Window guard_window;
   gulong create_serial;
-  
+
   attributes.event_mask = NoEventMask;
   attributes.override_redirect = True;
-  attributes.background_pixel = BlackPixel (xdisplay, screen->number);
 
   /* We have to call record_add() after we have the new window ID,
    * so save the serial for the CreateWindow request until then */
@@ -468,10 +467,10 @@ create_guard_window (Display *xdisplay, MetaScreen *screen)
 		   screen->rect.width,
 		   screen->rect.height,
 		   0, /* border width */
-		   CopyFromParent, /* depth */
-		   CopyFromParent, /* class */
+		   0, /* depth */
+		   InputOnly, /* class */
 		   CopyFromParent, /* visual */
-		   CWEventMask|CWOverrideRedirect|CWBackPixel,
+		   CWEventMask|CWOverrideRedirect,
 		   &attributes);
 
   /* https://bugzilla.gnome.org/show_bug.cgi?id=710346 */
