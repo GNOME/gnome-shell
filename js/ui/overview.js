GNOME/gnome-shell

commit b97f3a9ecf52183e86506af622a08eaa7e3846bb

overview: Fix DESKTOP clone position
The DESKTOP window might not be located at (0,0), in particular
on multi-monitor setups. While we already consider this by setting
the clone's position, we then stuff the clone into a container which
ignores it - meh ...

https://bugzilla.gnome.org/show_bug.cgi?id=723306

js/ui/overview.js
@@ -131,7 +131,7 @@ const Overview = new Lang.Class({
         Main.layoutManager.overviewGroup.add_child(this._backgroundGroup);
         this._bgManagers = [];
 
-        this._desktopFade = new St.Bin();
+        this._desktopFade = new St.Widget();
         Main.layoutManager.overviewGroup.add_child(this._desktopFade);
 
         this._activationTime = 0;
@@ -493,8 +493,8 @@ const Overview = new Lang.Class({
     },
 
     fadeOutDesktop: function() {
-        if (!this._desktopFade.child)
-            this._desktopFade.child = this._getDesktopClone();
+        if (!this._desktopFade.get_n_children())
+            this._desktopFade.add_child(this._getDesktopClone());
 
         this._desktopFade.opacity = 255;
         this._desktopFade.show();

commit 3fc478a14bed00ab746c55e3c98382957e105616

overview: don't add a null desktop clone
If there is no desktop window, getDesktopClone() returns null.
In that case, we know that no animation is needed.

Fixes a regression from b97f3a9ecf52183e86506af622a08eaa7e3846bb

https://bugzilla.gnome.org/show_bug.cgi?id=723523

js/ui/overview.js
@@ -493,8 +493,13 @@ const Overview = new Lang.Class({
     },
 
     fadeOutDesktop: function() {
-        if (!this._desktopFade.get_n_children())
-            this._desktopFade.add_child(this._getDesktopClone());
+        if (!this._desktopFade.get_n_children()) {
+            let clone = this._getDesktopClone();
+            if (!clone)
+                return;
+
+            this._desktopFade.add_child(clone);
+        }
 
         this._desktopFade.opacity = 255;
         this._desktopFade.show();