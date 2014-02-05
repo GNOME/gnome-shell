a8336669a3bba4bd3ffeeccff6849260c766ebbb
Rico Tzschichholz <ricotz@ubuntu.com>
2014-02-05 22:50:34 +0100

    shaped-texture: Fix unused variable warning
    (cherry picked from commit 101a13c86daaea0a6cd0b7421f7a186fbf4ca3f9)

--- src/compositor/meta-shaped-texture.c
+++ src/compositor/meta-shaped-texture.c
@@ -668,7 +668,6 @@ meta_shaped_texture_get_unobscured_bounds (MetaShapedTexture     *self,
 gboolean
 meta_shaped_texture_is_obscured (MetaShapedTexture *self)
 {
-  MetaShapedTexturePrivate *priv = self->priv;
   cairo_region_t *unobscured_region = effective_unobscured_region (self);
 
   if (unobscured_region)
