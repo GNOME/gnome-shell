GNOME/gnome-shell

commit 7101cc3170bf727aebf918f37debff94ae705c85

osdWindow: Don't tween to undefined
Check that we got a valid level before setting the level bar to it, to
prevent a Tweener warning.

js/ui/osdWindow.js
@@ -125,13 +125,15 @@ const OsdWindow = new Lang.Class({
 
     setLevel: function(level) {
         this._level.actor.visible = (level != undefined);
-        if (this.actor.visible)
-            Tweener.addTween(this._level,
-                             { level: level,
-                               time: LEVEL_ANIMATION_TIME,
-                               transition: 'easeOutQuad' });
-        else
-            this._level.level = level;
+        if (level) {
+            if (this.actor.visible)
+                Tweener.addTween(this._level,
+                                 { level: level,
+                                   time: LEVEL_ANIMATION_TIME,
+                                   transition: 'easeOutQuad' });
+            else
+                this._level.level = level;
+        }
     },
 
     show: function() {

commit 7113821964b504bde9a0de01d724877fd9c41ea2

osdWindow: Fix setting a zero-level in osdWindow
Commit 7101cc3170bf727aebf918f37debff94ae705c85 caused a small
regression insomuch that it checks for a valid "level" but simply using
"if (level)" which will be false if level is undefined and if
level == 0.

Check for not undefined instead.

https://bugzilla.gnome.org/show_bug.cgi?id=727384

js/ui/osdWindow.js
@@ -125,7 +125,7 @@ const OsdWindow = new Lang.Class({
 
     setLevel: function(level) {
         this._level.actor.visible = (level != undefined);
-        if (level) {
+        if (level != undefined) {
             if (this.actor.visible)
                 Tweener.addTween(this._level,
                                  { level: level,