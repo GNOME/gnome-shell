GNOME/mutter

Convert window menus to a compositor implementation

src/compositor/compositor.c
@@ -1723,3 +1723,15 @@ meta_compositor_hide_tile_preview (MetaCompositor *compositor,
 
   meta_plugin_manager_hide_tile_preview (info->plugin_mgr);
 }
+
+void
+meta_compositor_show_window_menu (MetaCompositor *compositor,
+                                  MetaWindow     *window)
+{
+  MetaCompScreen *info = meta_screen_get_compositor_data (window->screen);
+
+  if (!info->plugin_mgr)
+    return;
+
+  meta_plugin_manager_show_window_menu (info->plugin_mgr, window);
+}

GNOME/gnome-shell

src/compositor/compositor.c
@@ -1356,3 +1356,10 @@ meta_compositor_hide_tile_preview (MetaCompositor *compositor)
 {
   meta_plugin_manager_hide_tile_preview (compositor->plugin_mgr);
 }
+
+void
+meta_compositor_show_window_menu (MetaCompositor *compositor,
+                                  MetaWindow     *window)
+{
+  meta_plugin_manager_show_window_menu (compositor->plugin_mgr, window);
+}