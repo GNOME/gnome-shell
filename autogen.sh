7cb30ced238a3faf33830b2821f5c9c1ca579bb8
Rico Tzschichholz <ricotz@ubuntu.com>
2014-06-05 17:11:36 +0200

    configure.ac: Modernize autoconf/libtool dependencies and macro usage

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="mutter"
-REQUIRED_AUTOMAKE_VERSION=1.10
+REQUIRED_AUTOMAKE_VERSION=1.11
 
 (test -f $srcdir/configure.ac \
   && test -d $srcdir/src) || {
