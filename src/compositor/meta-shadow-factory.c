d0cafab836a455ac3ff5b8a5f69166bcd9048518
Nikita Churaev <lamefun.x0r@gmail.com>
2014-06-18 22:34:05 +0400

    Change shadow radiuses to match new Adwaita more or less
    Exact simmilarity is not possible yet due to different blurring algorithms.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=731866

--- src/compositor/meta-shadow-factory.c
+++ src/compositor/meta-shadow-factory.c
@@ -121,17 +121,17 @@ static guint signals[LAST_SIGNAL] = { 0 };
 /* The first element in this array also defines the default parameters
  * for newly created classes */
 MetaShadowClassInfo default_shadow_classes[] = {
-  { "normal",       { 6, -1, 0, 3, 128 }, { 3, -1, 0, 3, 32 } },
-  { "dialog",       { 6, -1, 0, 3, 128 }, { 3, -1, 0, 3, 32 } },
-  { "modal_dialog", { 6, -1, 0, 1, 128 }, { 3, -1, 0, 3, 32 } },
+  { "normal",       { 3, -1, 0, 3, 128 }, { 3, -1, 0, 3, 32 } },
+  { "dialog",       { 3, -1, 0, 3, 128 }, { 3, -1, 0, 3, 32 } },
+  { "modal_dialog", { 3, -1, 0, 1, 128 }, { 3, -1, 0, 3, 32 } },
   { "utility",      { 3, -1, 0, 1, 128 }, { 3, -1, 0, 1, 32 } },
-  { "border",       { 6, -1, 0, 3, 128 }, { 3, -1, 0, 3, 32 } },
-  { "menu",         { 6, -1, 0, 3, 128 }, { 3, -1, 0, 0, 32 } },
+  { "border",       { 3, -1, 0, 3, 128 }, { 3, -1, 0, 3, 32 } },
+  { "menu",         { 3, -1, 0, 3, 128 }, { 3, -1, 0, 0, 32 } },
 
-  { "popup-menu",    { 1, -1, 0, 1, 128 }, { 1, -1, 0, 1, 128 } },
+  { "popup-menu",    { 1, 0, 0, 1, 128 }, { 1, -1, 0, 1, 128 } },
 
   { "dropdown-menu", { 1, 10, 0, 1, 128 }, { 1, 10, 0, 1, 128 } },
-  { "attached",      { 1, -1, 0, 1, 128 }, { 1, -1, 0, 1, 128 } }
+  { "attached",      { 1, 0, 0, 1, 128 }, { 1, -1, 0, 1, 128 } }
 };
 
 G_DEFINE_TYPE (MetaShadowFactory, meta_shadow_factory, G_TYPE_OBJECT);
