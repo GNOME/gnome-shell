6edcd82103a25e102f29e434e9ce478ac30d4c70
Florian Müllner <fmuellner@gnome.org>
2014-06-25 19:42:35 +0200

    tweener: Also consider delay when disabling animations
    'time' is not the only timing parameter for tweens, in order to
    really disable animations, we need to override 'delay' as well.

--- js/ui/tweener.js
+++ js/ui/tweener.js
@@ -53,8 +53,10 @@ function _wrapTweening(target, tweeningParameters) {
         }
     }
 
-    if (!Gtk.Settings.get_default().gtk_enable_animations)
+    if (!Gtk.Settings.get_default().gtk_enable_animations) {
         tweeningParameters['time'] = 0.000001;
+        tweeningParameters['delay'] = 0.000001;
+    }
 
     _addHandler(target, tweeningParameters, 'onComplete', _tweenCompleted);
 }
