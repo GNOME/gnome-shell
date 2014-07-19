6ee5a1437c3af4f4d6dc46798d98d6a297f099c1
Adel Gadllah <adel.gadllah@gmail.com>
2014-07-19 18:45:47 +0200

    x11: Move event filter to backend connection

--- src/compositor/compositor.c
+++ src/compositor/compositor.c
@@ -741,9 +741,6 @@ meta_compositor_process_event (MetaCompositor *compositor,
                                XEvent         *event,
                                MetaWindow     *window)
 {
-  if (meta_plugin_manager_xevent_filter (compositor->plugin_mgr, event))
-    return TRUE;
-
   if (!meta_is_wayland_compositor () &&
       event->type == meta_display_get_damage_event_base (compositor->display) + XDamageNotify)
     {
