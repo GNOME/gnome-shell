d836194e319186bebb932fb1f54982afac14327f
Florian Müllner <fmuellner@gnome.org>
2014-08-07 18:38:36 +0200

    appDisplay: Remove pointless return value
    StButton::clicked does not have a return type, so don't return anything.

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1501,7 +1501,6 @@ const AppIcon = new Lang.Class({
             this.app.open_new_window(-1);
             Main.overview.hide();
         }
-        return false;
     },
 
     _onKeyboardPopupMenu: function() {
