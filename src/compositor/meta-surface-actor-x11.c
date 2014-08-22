GNOME/gnome-shell

commit e73c46ce033eb3b63c77a90eb2dabd9a2e7e3444

surface-actor-x11: Use the existing check for "visibility"
Instead of doing our own thing here.

src/compositor/meta-surface-actor-x11.c
@@ -205,8 +205,7 @@ meta_surface_actor_x11_process_damage (MetaSurfaceActor *actor,
         priv->does_full_damage = TRUE;
     }
 
-  /* Drop damage event for unredirected windows */
-  if (priv->unredirected)
+  if (!is_visible (actor))
     return;
 
   cogl_texture_pixmap_x11_update_area (priv->texture, x, y, width, height);

commit ecc254c659d670df0f24ec5202d895b90b4ad4fb

surface-actor-x11: Fix "incompatible pointer type" error
Caused by e73c46ce033eb3b63c77a90eb2dabd9a2e7e3444

src/compositor/meta-surface-actor-x11.c
@@ -205,7 +205,7 @@ meta_surface_actor_x11_process_damage (MetaSurfaceActor *actor,
         priv->does_full_damage = TRUE;
     }
 
-  if (!is_visible (actor))
+  if (!is_visible (self))
     return;
 
   cogl_texture_pixmap_x11_update_area (priv->texture, x, y, width, height);