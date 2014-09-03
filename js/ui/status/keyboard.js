19e3f794f82183a0ca9515b8e7524eace9455df6
Rui Matos <tiagomatos@gmail.com>
2014-09-03 17:19:11 +0200

    status/keyboard: Remove unused variable
    https://bugzilla.gnome.org/show_bug.cgi?id=735976

--- js/ui/status/keyboard.js
+++ js/ui/status/keyboard.js
@@ -457,7 +457,6 @@ const InputSourceIndicator = new Lang.Class({
         }
 
         let popup = new InputSourcePopup(this._mruSources, this._keybindingAction, this._keybindingActionBackward);
-        let modifiers = binding.get_modifiers();
         if (!popup.show(binding.is_reversed(), binding.get_name(), binding.get_mask()))
             popup.destroy();
     },
