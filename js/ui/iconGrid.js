GNOME/gnome-shell

commit 9e5704b49863d1699030387cb6091dfb9947ac39

iconGrid: Make actors non reactive while animating
Although the actor are with opacity 0, they were reactive, so the if the
user quickly make a click on the grid while animating it can launch an
app accidentally.
To avoid that make the actors non reactive while animating.

js/ui/iconGrid.js
@@ -432,6 +432,7 @@ const IconGrid = new Lang.Class({
         for (let index = 0; index < actors.length; index++) {
             let actor = actors[index];
             actor.opacity = 0;
+            actor.reactive = false;
 
             let delay = index / actors.length * ANIMATION_MAX_DELAY_FOR_ITEM;
             let [originalX, originalY] = actor.get_transformed_position();
@@ -465,6 +466,7 @@ const IconGrid = new Lang.Class({
                                                             this._animationDone();
 
                                                         actor.opacity = 255;
+                                                        actor.reactive = true;
                                                         actorClone.destroy();
                                                     })
                                                    });
@@ -508,6 +510,7 @@ const IconGrid = new Lang.Class({
         for (let index = 0; index < actors.length; index++) {
             let actor = actors[index];
             actor.opacity = 0;
+            actor.reactive = false;
 
             let actorClone = new Clutter.Clone({ source: actor });
             Main.uiGroup.add_actor(actorClone);
@@ -542,6 +545,7 @@ const IconGrid = new Lang.Class({
                                            this._animationDone();
 
                                        actor.opacity = 255;
+                                       actor.reactive = true;
                                        actorClone.destroy();
                                    })};
                 fadeParams = { time: ANIMATION_FADE_IN_TIME_FOR_ITEM,

commit d44c3d0cd3e434941db3c18264ed34594a6f1c81

iconGrid: Make actors reactive on animating out as well
Commit 9e5704b introduced a regression due to a sloppy mistake not
making actors reactive on animation out.
Fix that making actors reactive when animating out as well.

js/ui/iconGrid.js
@@ -569,6 +569,7 @@ const IconGrid = new Lang.Class({
                                            this._animationDone();
                                            this._restoreItemsOpacity();
                                        }
+                                       actor.reactive = true;
                                        actorClone.destroy();
                                    })};
                 fadeParams = { time: ANIMATION_FADE_IN_TIME_FOR_ITEM,