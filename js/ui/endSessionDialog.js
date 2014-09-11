GNOME/gnome-shell

commit 0258c7a51843538fe2634b1a3e62e9f5e5e86c2d

endSessionDialog: Use DialogType constants instead of magic numbers

js/ui/endSessionDialog.js
@@ -134,11 +134,18 @@ const restartInstallDialogContent = {
     showOtherSessions: true,
 };
 
+const DialogType = {
+  LOGOUT: 0 /* GSM_SHELL_END_SESSION_DIALOG_TYPE_LOGOUT */,
+  SHUTDOWN: 1 /* GSM_SHELL_END_SESSION_DIALOG_TYPE_SHUTDOWN */,
+  RESTART: 2 /* GSM_SHELL_END_SESSION_DIALOG_TYPE_RESTART */,
+  UPDATE_RESTART: 3
+};
+
 const DialogContent = {
-    0 /* GSM_SHELL_END_SESSION_DIALOG_TYPE_LOGOUT */: logoutDialogContent,
-    1 /* GSM_SHELL_END_SESSION_DIALOG_TYPE_SHUTDOWN */: shutdownDialogContent,
-    2 /* GSM_SHELL_END_SESSION_DIALOG_TYPE_RESTART */: restartDialogContent,
-    3: restartInstallDialogContent
+    DialogType.LOGOUT: logoutDialogContent,
+    DialogType.SHUTDOWN: shutdownDialogContent,
+    DialogType.RESTART: restartDialogContent,
+    DialogType.UPDATE_RESTART: restartInstallDialogContent
 };
 
 const MAX_USERS_IN_SESSION_DIALOG = 5;
@@ -677,8 +684,9 @@ const EndSessionDialog = new Lang.Class({
         this._totalSecondsToStayOpen = totalSecondsToStayOpen;
         this._type = type;
 
-        if (this._type == 2 && this._updatesFile.query_exists(null))
-            this._type = 3;
+        if (this._type == DialogType.RESTART &&
+            this._updatesFile.query_exists(null))
+            this._type = DialogType.UPDATE_RESTART;
 
         this._applications = [];
         this._applicationList.destroy_all_children();

commit a4c1b55111724abc3bda95c8e366a0f68f22d4ae

endSessionDialog: Fix DialogContent const
Fallout from commit 0258c7a518 ...

js/ui/endSessionDialog.js
@@ -140,10 +140,10 @@ const DialogType = {
 };
 
 const DialogContent = {
-    DialogType.LOGOUT: logoutDialogContent,
-    DialogType.SHUTDOWN: shutdownDialogContent,
-    DialogType.RESTART: restartDialogContent,
-    DialogType.UPDATE_RESTART: restartInstallDialogContent
+    0 /* DialogType.LOGOUT */: logoutDialogContent,
+    1 /* DialogType.SHUTDOWN */: shutdownDialogContent,
+    2 /* DialogType.RESTART */: restartDialogContent,
+    3 /* DialogType.UPDATE_RESTART */: restartInstallDialogContent
 };
 
 const MAX_USERS_IN_SESSION_DIALOG = 5;