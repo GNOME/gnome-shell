GNOME/gnome-shell

commit 1d374ac8bd496f6fa6f4c55ffd207bd30bd50075

screenShield: focus login screen after lifting shield
This commit ensures the login screen gets focused after
the screen shield is raised.

The code affects the unlock screen as well, but it's
less important since the unlock screen gets destroyed
and recreated each time the curtain moves, so it
has an opportunity to take focus on its own.

https://bugzilla.gnome.org/show_bug.cgi?id=708105

js/ui/screenShield.js
@@ -919,6 +919,9 @@ const ScreenShield = new Lang.Class({
 
         this._lockScreenState = MessageTray.State.HIDDEN;
         this._lockScreenGroup.hide();
+
+        this._dialog.actor.grab_key_focus();
+        this._dialog.actor.navigate_focus(null, Gtk.DirectionType.TAB_FORWARD, false);
     },
 
     _hideLockScreen: function(animate, velocity) {

commit 4e8c4761530cc686c09c25ae3fda2dc4a024aa6c

screenShield: fix trace back when unlocking session from another vt
commit 1d374ac8bd496f6fa6f4c55ffd207bd30bd50075 introduced a bug that
prevents unlock from working when initiated from another VT
(user switching).

This commit fixes the exception raised.

https://bugzilla.gnome.org/show_bug.cgi?id=708105

js/ui/screenShield.js
@@ -920,8 +920,10 @@ const ScreenShield = new Lang.Class({
         this._lockScreenState = MessageTray.State.HIDDEN;
         this._lockScreenGroup.hide();
 
-        this._dialog.actor.grab_key_focus();
-        this._dialog.actor.navigate_focus(null, Gtk.DirectionType.TAB_FORWARD, false);
+        if (this._dialog) {
+            this._dialog.actor.grab_key_focus();
+            this._dialog.actor.navigate_focus(null, Gtk.DirectionType.TAB_FORWARD, false);
+        }
     },
 
     _hideLockScreen: function(animate, velocity) {