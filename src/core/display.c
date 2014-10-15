GNOME/gnome-shell

commit 2f9c601ae40482a623edd931ae9c7a5558434de5

display: Clean up end_grab_op

src/core/display.c
@@ -1994,29 +1994,23 @@ meta_display_end_grab_op (MetaDisplay *display,
   g_signal_emit (display, display_signals[GRAB_OP_END], 0,
                  display->screen, display->grab_window, display->grab_op);
 
-  if (display->grab_window != NULL)
-    display->grab_window->shaken_loose = FALSE;
-
   if (display->grab_window != NULL &&
-      !meta_prefs_get_raise_on_click () &&
       meta_grab_op_is_moving_or_resizing (display->grab_op))
     {
+      /* Clear out the edge cache */
+      meta_display_cleanup_edges (display);
+
+      display->grab_window->shaken_loose = FALSE;
+
       /* Only raise the window in orthogonal raise
        * ('do-not-raise-on-click') mode if the user didn't try to move
        * or resize the given window by at least a threshold amount.
        * For raise on click mode, the window was raised at the
        * beginning of the grab_op.
        */
-      if (!display->grab_threshold_movement_reached)
+      if (!meta_prefs_get_raise_on_click () &&
+          display->grab_threshold_movement_reached)
         meta_window_raise (display->grab_window);
-    }
-
-  /* If this was a move or resize clear out the edge cache */
-  if (meta_grab_op_is_moving_or_resizing (display->grab_op))
-    {
-      meta_topic (META_DEBUG_WINDOW_OPS,
-                  "Clearing out the edges for resistance/snapping");
-      meta_display_cleanup_edges (display);
     }
 
   if (display->grab_old_window_stacking != NULL)

commit 34516aeab6a60896a4c4df2575bb8790c0e8dd21

display: Fix accidental inversion from 2f9c601
Commit 2f9c601 accidentally changed the logic here, changing the grab
behavior when not using raise-on-click. Fix this.

Spotted-by: Adam Goode <adam@spicenitz.org>

src/core/display.c
@@ -1965,7 +1965,7 @@ meta_display_end_grab_op (MetaDisplay *display,
        * beginning of the grab_op.
        */
       if (!meta_prefs_get_raise_on_click () &&
-          display->grab_threshold_movement_reached)
+          !display->grab_threshold_movement_reached)
         meta_window_raise (display->grab_window);
 
       meta_window_grab_op_ended (grab_window, grab_op);