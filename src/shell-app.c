GNOME/gnome-shell

commit 086fcd4a32ffff1755a8349b78516f2cb12e043c

shell-app: Only raise windows on the same workspace when activating them
For some reason, we never did this, even though this is what the comment
says we should be doing.

src/shell-app.c
@@ -582,7 +582,7 @@ shell_app_activate_window (ShellApp     *app,
         {
           MetaWindow *other_window = iter->data;
 
-          if (other_window != window)
+          if (other_window != window && meta_window_get_workspace (other_window) == workspace)
             meta_window_raise (other_window);
         }
       g_slist_free (windows_reversed);

commit ead4c2b863fe8221a27ff17d1515e8517caadaa3

shell-app: Fix typo
Commit 086fcd4a32 broke the build, use the correct variable to fix.

https://bugzilla.gnome.org/show_bug.cgi?id=743729

src/shell-app.c
@@ -582,7 +582,8 @@ shell_app_activate_window (ShellApp     *app,
         {
           MetaWindow *other_window = iter->data;
 
-          if (other_window != window && meta_window_get_workspace (other_window) == workspace)
+          if (other_window != window &&
+              meta_window_get_workspace (other_window) == active);
             meta_window_raise (other_window);
         }
       g_slist_free (windows_reversed);