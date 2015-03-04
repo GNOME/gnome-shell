eab8ea9a1eaa9e8e5e7c5d8cd9a2e3155e76757f
Michele <micxgx@gmail.com>
2015-03-04 00:51:19 +0000

    appDisplay: Fix leaking signal connections
    The overview has a longer life-time than dash items, so we are leaking a
    signal connection each time an item is destroyed.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=745570

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1658,7 +1658,10 @@ const AppIcon = new Lang.Class({
                 if (!isPoppedUp)
                     this._onMenuPoppedDown();
             }));
-            Main.overview.connect('hiding', Lang.bind(this, function () { this._menu.close(); }));
+            let id = Main.overview.connect('hiding', Lang.bind(this, function () { this._menu.close(); }));
+            this.actor.connect('destroy', function() {
+                Main.overview.disconnect(id);
+            });
 
             this._menuManager.addMenu(this._menu);
         }
