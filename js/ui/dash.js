GNOME/gnome-shell

commit e69cc20fc7dc10b9c04c3a0224c01c8cd23671cf

dash: Improve feedback when favorite-apps key is locked
In a lockdown scenario, where the favorite-apps GSettings key is not
writable, hide the menu items for adding and removing favorites from the
dash menu. Additionally, reject drops to the dash for DND.

https://bugzilla.gnome.org/show_bug.cgi?id=741325

js/ui/dash.js
@@ -1,6 +1,7 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 
 const Clutter = imports.gi.Clutter;
+const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
 const Signals = imports.signals;
 const Lang = imports.lang;
@@ -269,6 +270,9 @@ const ShowAppsIcon = new Lang.Class({
         if (app == null)
             return false;
 
+        if (!this._settings.is_writable('favorite-apps'))
+            return false;
+
         let id = app.get_id();
         let isFavorite = AppFavorites.getAppFavorites().isFavorite(id);
         return isFavorite;
@@ -423,6 +427,8 @@ const Dash = new Lang.Class({
             }));
 
         this._workId = Main.initializeDeferredWork(this._box, Lang.bind(this, this._redisplay));
+
+        this._settings = new Gio.Settings({ schema_id: 'org.gnome.shell' });
 
         this._appSystem = Shell.AppSystem.get_default();
 
@@ -850,6 +856,9 @@ const Dash = new Lang.Class({
         if (app == null || app.is_window_backed())
             return DND.DragMotionResult.NO_DROP;
 
+        if (!this._settings.is_writable('favorite-apps'))
+            return DND.DragMotionResult.NO_DROP;
+
         let favorites = AppFavorites.getAppFavorites().getFavorites();
         let numFavorites = favorites.length;
 
@@ -925,6 +934,9 @@ const Dash = new Lang.Class({
         if (app == null || app.is_window_backed()) {
             return false;
         }
+
+        if (!this._settings.is_writable('favorite-apps'))
+            return false;
 
         let id = app.get_id();
 

js/ui/appDisplay.js
@@ -1748,6 +1748,8 @@ const AppIconMenu = new Lang.Class({
 
         this.actor.add_style_class_name('app-well-menu');
 
+        this._settings = new Gio.Settings({ schema_id: 'org.gnome.shell' });
+
         // Chain our visibility and lifecycle to that of the source
         source.actor.connect('notify::mapped', Lang.bind(this, function () {
             if (!source.actor.mapped)
@@ -1807,22 +1809,27 @@ const AppIconMenu = new Lang.Class({
                     this.emit('activate-window', null);
                 }));
             }
-            this._appendSeparator();
 
-            let isFavorite = AppFavorites.getAppFavorites().isFavorite(this._source.app.get_id());
+            let canFavorite = this._settings.is_writable('favorite-apps');
 
-            if (isFavorite) {
-                let item = this._appendMenuItem(_("Remove from Favorites"));
-                item.connect('activate', Lang.bind(this, function() {
-                    let favs = AppFavorites.getAppFavorites();
-                    favs.removeFavorite(this._source.app.get_id());
-                }));
-            } else {
-                let item = this._appendMenuItem(_("Add to Favorites"));
-                item.connect('activate', Lang.bind(this, function() {
-                    let favs = AppFavorites.getAppFavorites();
-                    favs.addFavorite(this._source.app.get_id());
-                }));
+            if (canFavorite) {
+                this._appendSeparator();
+
+                let isFavorite = AppFavorites.getAppFavorites().isFavorite(this._source.app.get_id());
+
+                if (isFavorite) {
+                    let item = this._appendMenuItem(_("Remove from Favorites"));
+                    item.connect('activate', Lang.bind(this, function() {
+                        let favs = AppFavorites.getAppFavorites();
+                        favs.removeFavorite(this._source.app.get_id());
+                    }));
+                } else {
+                    let item = this._appendMenuItem(_("Add to Favorites"));
+                    item.connect('activate', Lang.bind(this, function() {
+                        let favs = AppFavorites.getAppFavorites();
+                        favs.addFavorite(this._source.app.get_id());
+                    }));
+                }
             }
 
             if (Shell.AppSystem.get_default().lookup_app('org.gnome.Software.desktop')) {

commit 4affa5528af537927877c810bc95e55decd2e1fb

dash: Fix show-apps button drop target
Fallout from commit e69cc20fc7dc1 ...

https://bugzilla.gnome.org/show_bug.cgi?id=745666

js/ui/dash.js
@@ -242,6 +242,8 @@ const ShowAppsIcon = new Lang.Class({
     _init: function() {
         this.parent();
 
+        this._settings = new Gio.Settings({ schema_id: 'org.gnome.shell' });
+
         this.toggleButton = new St.Button({ style_class: 'show-apps',
                                             track_hover: true,
                                             can_focus: true,