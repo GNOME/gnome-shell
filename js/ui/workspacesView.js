e87656af16af14c4741b555b31c9a027adc965fd
Giovanni Campagna <gcampagna@gnome.org>
2015-03-20 12:58:01 -0700

    WorkspacesView: remove unused GSettings
    https://bugzilla.gnome.org/show_bug.cgi?id=746509

--- js/ui/workspacesView.js
+++ js/ui/workspacesView.js
@@ -97,8 +97,6 @@ const WorkspacesView = new Lang.Class({
         this._scrolling = false; // swipe-scrolling
         this._animatingScroll = false; // programatically updating the adjustment
 
-        this._settings = new Gio.Settings({ schema_id: OVERRIDE_SCHEMA });
-
         let activeWorkspaceIndex = global.screen.get_active_workspace_index();
         this.scrollAdjustment = new St.Adjustment({ value: activeWorkspaceIndex,
                                                     lower: 0,
