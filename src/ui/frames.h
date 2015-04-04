dd060d78ceee53cad106418374f8f4bd4d250cde
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2015-04-04 10:03:26 +0200

    prevent duplicate declaration of MetaUIFrame
    fixes bug 747326

--- src/ui/frames.h
+++ src/ui/frames.h
@@ -27,6 +27,7 @@
 #include <meta/common.h>
 #include <meta/types.h>
 #include "theme-private.h"
+#include "ui.h"
 
 typedef enum
 {
@@ -68,8 +69,6 @@ typedef enum
 
 typedef struct _MetaFrames        MetaFrames;
 typedef struct _MetaFramesClass   MetaFramesClass;
-
-typedef struct _MetaUIFrame         MetaUIFrame;
 
 struct _MetaUIFrame
 {
