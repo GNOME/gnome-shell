01374989b1d72721aec1cd2294c4260da8278e15
Florian Müllner <fmuellner@gnome.org>
2015-06-11 14:39:36 +0200

    messageTray: Remove _fixMarkup() function
    Commit 053e54f944 copied it to calendar, and since commit 15e42c4d5,
    the original is no longer used.

--- js/ui/messageTray.js
+++ js/ui/messageTray.js
@@ -69,26 +69,6 @@ const Urgency = {
     CRITICAL: 3
 };
 
-function _fixMarkup(text, allowMarkup) {
-    if (allowMarkup) {
-        // Support &amp;, &quot;, &apos;, &lt; and &gt;, escape all other
-        // occurrences of '&'.
-        let _text = text.replace(/&(?!amp;|quot;|apos;|lt;|gt;)/g, '&amp;');
-
-        // Support <b>, <i>, and <u>, escape anything else
-        // so it displays as raw markup.
-        _text = _text.replace(/<(?!\/?[biu]>)/g, '&lt;');
-
-        try {
-            Pango.parse_markup(_text, -1, '');
-            return _text;
-        } catch (e) {}
-    }
-
-    // !allowMarkup, or invalid markup
-    return GLib.markup_escape_text(text, -1);
-}
-
 const FocusGrabber = new Lang.Class({
     Name: 'FocusGrabber',
 
