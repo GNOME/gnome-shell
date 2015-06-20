7c2ac712f73b26508fa5948dc266259b26fad0fa
Kalev Lember <klember@redhat.com>
2015-06-20 18:55:01 +0200

    docs: Fix a typo in ClutterImage example

--- clutter/clutter-image.c
+++ clutter/clutter-image.c
@@ -220,7 +220,7 @@ clutter_image_new (void)
  *
  *   clutter_image_set_data (CLUTTER_IMAGE (image),
  *                           gdk_pixbuf_get_pixels (pixbuf),
- *                           gdk_pixbuf_has_alpha (pixbuf)
+ *                           gdk_pixbuf_get_has_alpha (pixbuf)
  *                             ? COGL_PIXEL_FORMAT_RGBA_8888
  *                             : COGL_PIXEL_FORMAT_RGB_888,
  *                           gdk_pixbuf_get_width (pixbuf),
