GNOME/gnome-shell

commit 09120132ef87061d527b5324a14ab5950db3dc0a

core: Refrain from showing wayland windows when we don't have a buffer yet
The "calc showing" operation is queued in a few places alongside MetaWindow
creation, we should be ignoring these until there is a buffer to show.

https://bugzilla.gnome.org/show_bug.cgi?id=750552

src/core/window.c
@@ -58,6 +58,7 @@
 
 #ifdef HAVE_WAYLAND
 #include "wayland/meta-window-wayland.h"
+#include "wayland/meta-wayland-surface.h"
 #include "wayland/meta-wayland-private.h"
 #endif
 
@@ -723,6 +724,10 @@ meta_window_should_attach_to_parent (MetaWindow *window)
 static gboolean
 client_window_should_be_mapped (MetaWindow *window)
 {
+  if (window->client_type == META_WINDOW_CLIENT_TYPE_WAYLAND &&
+      !window->surface->buffer)
+    return FALSE;
+
   return !window->shaded;
 }
 
@@ -1533,6 +1538,10 @@ meta_window_showing_on_its_workspace (MetaWindow *window)
 gboolean
 meta_window_should_be_showing (MetaWindow  *window)
 {
+  if (window->client_type == META_WINDOW_CLIENT_TYPE_WAYLAND &&
+      !window->surface->buffer)
+    return FALSE;
+
   /* Windows should be showing if they're located on the
    * active workspace and they're showing on their own workspace. */
   return (meta_window_located_on_workspace (window, window->screen->active_workspace) &&

commit 6b82f61dba6a569a5a2c240d08d3a600fd35b94d

build: Fix non-wayland build
This fixes error introduced in 09120132ef87061d527b5324a14ab5950db3dc0a.

https://bugzilla.gnome.org/show_bug.cgi?id=750552

src/core/window.c
@@ -724,9 +724,11 @@ meta_window_should_attach_to_parent (MetaWindow *window)
 static gboolean
 client_window_should_be_mapped (MetaWindow *window)
 {
+#ifdef HAVE_WAYLAND
   if (window->client_type == META_WINDOW_CLIENT_TYPE_WAYLAND &&
       !window->surface->buffer)
     return FALSE;
+#endif
 
   return !window->shaded;
 }
@@ -1538,9 +1540,11 @@ meta_window_showing_on_its_workspace (MetaWindow *window)
 gboolean
 meta_window_should_be_showing (MetaWindow  *window)
 {
+#ifdef HAVE_WAYLAND
   if (window->client_type == META_WINDOW_CLIENT_TYPE_WAYLAND &&
       !window->surface->buffer)
     return FALSE;
+#endif
 
   /* Windows should be showing if they're located on the
    * active workspace and they're showing on their own workspace. */