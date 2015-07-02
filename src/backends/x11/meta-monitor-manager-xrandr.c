659b8ed471b49b583e8bc30d99bbdb6bb8d44c3e
Cosimo Cecchi <cosimoc@gnome.org>
2015-07-01 17:59:36 -0700

    monitor-manager-xrandr: don't set underscan property when not supported
    We should not be setting random output properties like this.
    Use the function we just introduced to only set the underscan flag when
    it's actually supported.

--- src/backends/x11/meta-monitor-manager-xrandr.c
+++ src/backends/x11/meta-monitor-manager-xrandr.c
@@ -1209,9 +1209,10 @@ meta_monitor_manager_xrandr_apply_configuration (MetaMonitorManager *manager,
                                       output_info->output,
                                       output_info->is_presentation);
 
-      output_set_underscanning_xrandr (manager_xrandr,
-                                       output_info->output,
-                                       output_info->is_underscanning);
+      if (output_get_supports_underscanning_xrandr (manager_xrandr, output_info->output))
+        output_set_underscanning_xrandr (manager_xrandr,
+                                         output_info->output,
+                                         output_info->is_underscanning);
 
       output->is_primary = output_info->is_primary;
       output->is_presentation = output_info->is_presentation;
