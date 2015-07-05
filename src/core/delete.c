a3fbbaabe84d2421cb508535fc2412d380c38512
Jasper St. Pierre <jstpierre@mecheye.net>
2015-07-05 14:29:56 -0700

    delete: Remove old code
    It doesn't compile

--- src/core/delete.c
+++ src/core/delete.c
@@ -180,34 +180,6 @@ meta_window_delete (MetaWindow  *window,
   META_WINDOW_GET_CLASS (window)->delete (window, timestamp);
 
   meta_window_check_alive (window, timestamp);
-
-  if (window->has_focus)
-    {
-      /* FIXME Clean this up someday
-       * http://bugzilla.gnome.org/show_bug.cgi?id=108706
-       */
-#if 0
-      /* This is unfortunately going to result in weirdness
-       * if the window doesn't respond to the delete event.
-       * I don't know how to avoid that though.
-       */
-      meta_topic (META_DEBUG_FOCUS,
-                  "Focusing default window because focus window %s was deleted/killed\n",
-                  window->desc);
-      meta_workspace_focus_default_window (window->screen->active_workspace,
-                                           window);
-#else
-      meta_topic (META_DEBUG_FOCUS,
-                  "Not unfocusing %s on delete/kill\n",
-                  window->desc);
-#endif
-    }
-  else
-    {
-      meta_topic (META_DEBUG_FOCUS,
-                  "Window %s was deleted/killed but didn't have focus\n",
-                  window->desc);
-    }
 }
 
 void
