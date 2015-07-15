GNOME/gnome-shell

commit ba7c524a18d73679ba1e2b8835254774f7726e8d

MetaSurfaceActorWayland: Unset the MetaWaylandSurface pointer when it goes away
We may access it during painting even if it has been freed. For now,
manually unset it during the MetaWaylandSurface cleanup; in the future
make MetaWaylandSurface a GObject and make the surface pointer a weak
reference.

https://bugzilla.gnome.org/show_bug.cgi?id=744453

src/compositor/meta-surface-actor-wayland.c
@@ -110,6 +110,9 @@ meta_surface_actor_wayland_get_scale (MetaSurfaceActorWayland *actor)
    MetaWindow *window = surface->window;
    int output_scale = 1;
 
+   if (!priv->surface)
+     return 1;
+
    while (surface)
     {
       if (surface->window)
@@ -300,4 +303,13 @@ meta_surface_actor_wayland_get_surface (MetaSurfaceActorWayland *self)
 {
   MetaSurfaceActorWaylandPrivate *priv = meta_surface_actor_wayland_get_instance_private (self);
   return priv->surface;
+}
+
+void
+meta_surface_actor_wayland_surface_destroyed (MetaSurfaceActorWayland *self)
+{
+  MetaSurfaceActorWaylandPrivate *priv =
+    meta_surface_actor_wayland_get_instance_private (self);
+
+  priv->surface = NULL;
 }

src/compositor/meta-surface-actor-wayland.h
@@ -57,6 +57,7 @@ GType meta_surface_actor_wayland_get_type (void);
 
 MetaSurfaceActor * meta_surface_actor_wayland_new (MetaWaylandSurface *surface);
 MetaWaylandSurface * meta_surface_actor_wayland_get_surface (MetaSurfaceActorWayland *self);
+void meta_surface_actor_wayland_surface_destroyed (MetaSurfaceActorWayland *self);
 
 void meta_surface_actor_wayland_set_texture (MetaSurfaceActorWayland *self,
                                              CoglTexture *texture);

src/compositor/meta-window-actor.c
@@ -616,8 +616,10 @@ meta_window_actor_get_shape_bounds (MetaWindowActor       *self,
 #ifdef HAVE_WAYLAND
   if (META_IS_SURFACE_ACTOR_WAYLAND (priv->surface))
     {
-      double scale = priv->surface ?
-                     meta_surface_actor_wayland_get_scale (META_SURFACE_ACTOR_WAYLAND (priv->surface)) : 1.;
+      MetaSurfaceActorWayland *surface_actor =
+        META_SURFACE_ACTOR_WAYLAND (priv->surface);
+      double scale = meta_surface_actor_wayland_get_scale (surface_actor);
+
       bounds->x *= scale;
       bounds->y *= scale;
       bounds->width *= scale;

src/wayland/meta-wayland-surface.c
@@ -802,6 +802,9 @@ wl_surface_destructor (struct wl_resource *resource)
   if (surface->input_region)
     cairo_region_destroy (surface->input_region);
 
+  meta_surface_actor_wayland_surface_destroyed (
+    META_SURFACE_ACTOR_WAYLAND (surface->surface_actor));
+
   g_object_unref (surface->surface_actor);
 
   meta_wayland_compositor_destroy_frame_callbacks (compositor, surface);

commit 5d1019691935bc24758aff4571d2ef5cae48720a

MetaSurfaceActorWayland: Don't dereference surface before NULL check
Fixes regression introduced in ba7c524a18d73679ba1e2b8835254774f7726e8d.

https://bugzilla.gnome.org/show_bug.cgi?id=744453

src/compositor/meta-surface-actor-wayland.c
@@ -108,10 +108,10 @@ meta_surface_actor_wayland_get_scale (MetaSurfaceActorWayland *actor)
 {
    MetaSurfaceActorWaylandPrivate *priv = meta_surface_actor_wayland_get_instance_private (actor);
    MetaWaylandSurface *surface = priv->surface;
-   MetaWindow *window = surface->window;
+   MetaWindow *window = NULL;
    int output_scale = 1;
 
-   if (!priv->surface)
+   if (!surface)
      return 1;
 
    while (surface)