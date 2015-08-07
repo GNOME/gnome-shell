GNOME/gnome-shell

commit fb0cf64536a35d0d018d2f2bd506a9f02af57b14

viewSelector: Don't focus search entry on whitespace
We recently started to trim leading and trailing whitespace from
the search string, and not to trigger a search when the resulting
string was empty. However we still allow whitespace to trigger
type-ahead-find, so that the key focus is moved briefly to the
search entry and back to the stage, resulting in a disruptive
flickering of the entry.
Fix this by excluding whitespace from triggering type-ahead-find.

https://bugzilla.gnome.org/show_bug.cgi?id=694475

js/ui/viewSelector.js
@@ -279,8 +279,7 @@ const ViewSelector = new Lang.Class({
             else
                 Main.overview.hide();
             return true;
-        } else if (Clutter.keysym_to_unicode(symbol) ||
-                   (symbol == Clutter.BackSpace && this._searchActive)) {
+        } else if (this._shouldTriggerSearch(symbol)) {
             this.startSearch(event);
         } else if (!this._searchActive) {
             if (symbol == Clutter.Tab || symbol == Clutter.Down) {
@@ -343,6 +342,17 @@ const ViewSelector = new Lang.Class({
                 global.stage.disconnect(this._capturedEventId);
             this._capturedEventId = 0;
         }
+    },
+
+    _shouldTriggerSearch: function(symbol) {
+        let unicode = Clutter.keysym_to_unicode(symbol);
+        if (unicode == 0)
+            return false;
+
+        if (getTermsForSearchString(String.fromCharCode(unicode)).length > 0)
+            return true;
+
+        return symbol == Clutter.BackSpace && this._searchActive;
     },
 
     startSearch: function(event) {

commit 45a6e2c3056895599070cf9f98c78c1c4f577c7c

viewSelector: Make backspace focus the search entry
Commit fb0cf64536a35d0d018d2f2bd506a9f02af57b14 regressed this because
there's no unicode character for backspace.

https://bugzilla.gnome.org/show_bug.cgi?id=753319

js/ui/viewSelector.js
@@ -465,6 +465,9 @@ const ViewSelector = new Lang.Class({
     },
 
     _shouldTriggerSearch: function(symbol) {
+        if (symbol == Clutter.BackSpace && this._searchActive)
+            return true;
+
         let unicode = Clutter.keysym_to_unicode(symbol);
         if (unicode == 0)
             return false;
@@ -472,7 +475,7 @@ const ViewSelector = new Lang.Class({
         if (getTermsForSearchString(String.fromCharCode(unicode)).length > 0)
             return true;
 
-        return symbol == Clutter.BackSpace && this._searchActive;
+        return false;
     },
 
     startSearch: function(event) {