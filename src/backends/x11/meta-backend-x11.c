cba2ab445e9f1b23de979917981ee1f3912d08fb
Florian Müllner <fmuellner@gnome.org>
2015-08-10 15:42:35 +0200

    backend-x11: Don't assert on something that might happen
    While we shouldn't normally receive crossing events for any windows
    except the stage when running nested, we do in case we hold a pointer
    grab - just ignore those events instead of taking down the user's
    session.

--- src/backends/x11/meta-backend-x11.c
+++ src/backends/x11/meta-backend-x11.c
@@ -155,10 +155,9 @@ translate_crossing_event (MetaBackendX11 *x11,
     }
 
   Window stage_window = meta_backend_x11_get_xwindow (x11);
-  if (enter_event->event != stage_window)
+  if (enter_event->event != stage_window &&
+      priv->mode == META_BACKEND_X11_MODE_COMPOSITOR)
     {
-      /* See above for the rationale for this... */
-      g_assert (priv->mode == META_BACKEND_X11_MODE_COMPOSITOR);
       enter_event->event = meta_backend_x11_get_xwindow (x11);
       enter_event->event_x = enter_event->root_x;
       enter_event->event_y = enter_event->root_y;
