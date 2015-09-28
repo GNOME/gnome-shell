04d526e9f5a5ecbfec61f7f72e6c34c2741d5278
Ray Strode <rstrode@redhat.com>
2015-09-28 19:56:53 -0400

    gdm: only unlock with smartcard, if smartcard used for login
    If a smartcard is used for login, we need to make sure the smartcard
    gets used for unlock, too.

--- js/gdm/util.js
+++ js/gdm/util.js
@@ -134,7 +134,6 @@ var ShellUserVerifier = new Lang.Class({
         this._settings = new Gio.Settings({ schema_id: LOGIN_SCREEN_SCHEMA });
         this._settings.connect('changed',
                                this._updateDefaultService.bind(this));
-        this._updateDefaultService();
 
         this._fprintManager = Fprint.FprintManager();
         this._smartcardManager = SmartcardManager.getSmartcardManager();
@@ -145,6 +144,8 @@ var ShellUserVerifier = new Lang.Class({
         // after a user has been picked.
         this.smartcardDetected = false;
         this._checkForSmartcard();
+
+        this._updateDefaultService();
 
         this._smartcardInsertedId = this._smartcardManager.connect('smartcard-inserted',
                                                                    this._checkForSmartcard.bind(this));
@@ -412,7 +413,9 @@ var ShellUserVerifier = new Lang.Class({
     },
 
     _updateDefaultService() {
-        if (this._settings.get_boolean(PASSWORD_AUTHENTICATION_KEY))
+        if (this._smartcardManager.loggedInWithToken())
+            this._defaultService = SMARTCARD_SERVICE_NAME;
+        else if (this._settings.get_boolean(PASSWORD_AUTHENTICATION_KEY))
             this._defaultService = PASSWORD_SERVICE_NAME;
         else if (this._settings.get_boolean(SMARTCARD_AUTHENTICATION_KEY))
             this._defaultService = SMARTCARD_SERVICE_NAME;
