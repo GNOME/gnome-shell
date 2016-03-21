3a4ae679ea398dba437b9750910b17891894899e
Jasper St. Pierre <jstpierre@mecheye.net>
2016-03-21 13:45:11 -0700

    common: Remove META_LAYER_FOCUSED_WINDOW
    Usage of this was removed 14 years ago in 25ca0e2. Let's remove the
    constant, too.

--- src/meta/common.h
+++ src/meta/common.h
@@ -520,7 +520,6 @@ void meta_frame_borders_clear (MetaFrameBorders *self);
  * @META_LAYER_TOP: Top layer
  * @META_LAYER_DOCK: Dock layer
  * @META_LAYER_FULLSCREEN: Fullscreen layer
- * @META_LAYER_FOCUSED_WINDOW: Focused window layer
  * @META_LAYER_OVERRIDE_REDIRECT: Override-redirect layer
  * @META_LAYER_LAST: Marks the end of the #MetaStackLayer enumeration
  *
@@ -535,7 +534,6 @@ typedef enum
   META_LAYER_TOP	       = 4, /* Same as DOCK; see EWMH and bug 330717 */
   META_LAYER_DOCK	       = 4,
   META_LAYER_FULLSCREEN	       = 5,
-  META_LAYER_FOCUSED_WINDOW    = 6,
   META_LAYER_OVERRIDE_REDIRECT = 7,
   META_LAYER_LAST	       = 8
 } MetaStackLayer;
