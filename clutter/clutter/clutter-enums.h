ec708fc1a9cf9a5be25dc1101e8b429ec86941da
Carlos Garnacho <carlosg@gnome.org>
2016-03-29 14:33:40 +0200

    enums: Add rotation/slider axes
    These will be useful for the tablet tools that have these features.

--- clutter/clutter/clutter-enums.h
+++ clutter/clutter/clutter-enums.h
@@ -946,6 +946,8 @@ typedef enum {
  * @CLUTTER_INPUT_AXIS_YTILT: The tile on the Y axis
  * @CLUTTER_INPUT_AXIS_WHEEL: A wheel
  * @CLUTTER_INPUT_AXIS_DISTANCE: Distance (Since 1.12)
+ * @CLUTTER_INPUT_AXIS_ROTATION: Rotation along the z-axis (Since 1.28)
+ * @CLUTTER_INPUT_AXIS_SLIDER: A slider (Since 1.28)
  * @CLUTTER_INPUT_AXIS_LAST: Last value of the enumeration; this value is
  *   useful when iterating over the enumeration values (Since 1.12)
  *
@@ -963,6 +965,8 @@ typedef enum {
   CLUTTER_INPUT_AXIS_YTILT,
   CLUTTER_INPUT_AXIS_WHEEL,
   CLUTTER_INPUT_AXIS_DISTANCE,
+  CLUTTER_INPUT_AXIS_ROTATION,
+  CLUTTER_INPUT_AXIS_SLIDER,
 
   CLUTTER_INPUT_AXIS_LAST
 } ClutterInputAxis;
