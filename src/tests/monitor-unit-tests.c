449320cd23d6d61f4586a4eaef1fc5bf9ef739e5
Jonas Ådahl <jadahl@gmail.com>
2017-01-13 14:42:03 +0800

    monitor-unit-tests: Check that a stored config is preferred
    Check that the stored config (where the logical monitors are placed
    vertically) is preferred to the otherwise generated linear config.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=777732

--- src/tests/monitor-unit-tests.c
+++ src/tests/monitor-unit-tests.c
@@ -1807,6 +1807,136 @@ meta_test_monitor_no_outputs (void)
   check_monitor_configuration (&test_case);
 }
 
+static void
+meta_test_monitor_custom_vertical_config (void)
+{
+  MonitorTestCase test_case = {
+    .setup = {
+      .modes = {
+        {
+          .width = 1024,
+          .height = 768,
+          .refresh_rate = 60.000495910644531
+        },
+        {
+          .width = 800,
+          .height = 600,
+          .refresh_rate = 60.000495910644531
+        }
+      },
+      .n_modes = 2,
+      .outputs = {
+        {
+          .crtc = 0,
+          .modes = { 0 },
+          .n_modes = 1,
+          .preferred_mode = 0,
+          .possible_crtcs = { 0 },
+          .n_possible_crtcs = 1,
+          .width_mm = 222,
+          .height_mm = 125
+        },
+        {
+          .crtc = 1,
+          .modes = { 1 },
+          .n_modes = 1,
+          .preferred_mode = 1,
+          .possible_crtcs = { 1 },
+          .n_possible_crtcs = 1,
+          .width_mm = 220,
+          .height_mm = 124
+        }
+      },
+      .n_outputs = 2,
+      .crtcs = {
+        {
+          .current_mode = 0
+        },
+        {
+          .current_mode = 0
+        }
+      },
+      .n_crtcs = 2
+    },
+
+    .expect = {
+      .monitors = {
+        {
+          .outputs = { 0 },
+          .n_outputs = 1,
+          .modes = {
+            {
+              .width = 1024,
+              .height = 768,
+              .crtc_modes = {
+                {
+                  .output = 0,
+                  .crtc_mode = 0
+                }
+              }
+            }
+          },
+          .n_modes = 1,
+          .current_mode = 0,
+          .width_mm = 222,
+          .height_mm = 125
+        },
+        {
+          .outputs = { 1 },
+          .n_outputs = 1,
+          .modes = {
+            {
+              .width = 800,
+              .height = 600,
+              .crtc_modes = {
+                {
+                  .output = 1,
+                  .crtc_mode = 1
+                }
+              }
+            }
+          },
+          .n_modes = 1,
+          .current_mode = 0,
+          .width_mm = 220,
+          .height_mm = 124
+        }
+      },
+      .n_monitors = 2,
+      .logical_monitors = {
+        {
+          .layout = { .x = 0, .y = 0, .width = 1024, .height = 768 },
+          .scale = 1
+        },
+        {
+          .layout = { .x = 0, .y = 768, .width = 800, .height = 600 },
+          .scale = 1
+        }
+      },
+      .n_logical_monitors = 2,
+      .n_outputs = 2,
+      .n_crtcs = 2,
+      .n_tiled_monitors = 0,
+      .screen_width = 1024,
+      .screen_height = 768 + 600
+    }
+  };
+  MetaMonitorTestSetup *test_setup;
+
+  if (!is_using_monitor_config_manager ())
+    {
+      g_test_skip ("Not using MetaMonitorConfigManager");
+      return;
+    }
+
+  test_setup = create_monitor_test_setup (&test_case,
+                                          MONITOR_TEST_FLAG_NONE);
+  set_custom_monitor_config ("vertical.xml");
+  emulate_hotplug (test_setup);
+
+  check_monitor_configuration (&test_case);
+}
+
 void
 init_monitor_tests (void)
 {
@@ -1840,4 +1970,7 @@ init_monitor_tests (void)
                    meta_test_monitor_lid_closed_no_external);
   g_test_add_func ("/backends/monitor/no-outputs",
                    meta_test_monitor_no_outputs);
+
+  g_test_add_func ("/backends/monitor/custom/vertical-config",
+                   meta_test_monitor_custom_vertical_config);
 }
