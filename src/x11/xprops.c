GNOME/gnome-shell

commit f041b35b9b739522b49eb980a6d2af5b3ef7cb60

xprops: Fix STRING types as well

src/x11/xprops.c
@@ -339,8 +339,7 @@ latin1_string_from_results (GetPropertyResults *results,
   if (!validate_or_free_results (results, 8, XA_STRING, FALSE))
     return FALSE;
 
-  *str_p = (char*) results->prop;
-  results->prop = NULL;
+  *str_p = g_strndup ((char *) results->prop, results->n_items);
 
   return TRUE;
 }

commit 5ba38a4ab6913fc5a4005ec3195df6bff5e89821

x11/xprops: Plug a few memory leaks
Commits 6dbec6f8, 734402e1 and f041b35b introduced memory leaks by
switching to returning copies instead of the original buffers but
forgetting to free those original buffers.

Some error cases were also not freeing the ->prop buffer as they
should.

https://bugzilla.gnome.org/show_bug.cgi?id=642652

src/x11/xprops.c
@@ -304,6 +304,8 @@ motif_hints_from_results (GetPropertyResults *results,
 
   if (results->type == None || results->n_items <= 0)
     {
+      g_free (results->prop);
+      results->prop = NULL;
       meta_verbose ("Motif hints had unexpected type or n_items\n");
       return FALSE;
     }
@@ -314,10 +316,18 @@ motif_hints_from_results (GetPropertyResults *results,
    */
   *hints_p = calloc (1, sizeof (MotifWmHints));
   if (*hints_p == NULL)
-    return FALSE;
+    {
+      g_free (results->prop);
+      results->prop = NULL;
+      return FALSE;
+    }
 
   memcpy(*hints_p, results->prop, MIN (sizeof (MotifWmHints),
                                        results->n_items * sizeof (uint32_t)));
+
+  g_free (results->prop);
+  results->prop = NULL;
+
   return TRUE;
 }
 
@@ -348,6 +358,9 @@ latin1_string_from_results (GetPropertyResults *results,
     return FALSE;
 
   *str_p = g_strndup ((char *) results->prop, results->n_items);
+
+  g_free (results->prop);
+  results->prop = NULL;
 
   return TRUE;
 }
@@ -395,6 +408,9 @@ utf8_string_from_results (GetPropertyResults *results,
     }
 
   *str_p = g_strndup ((char *) results->prop, results->n_items);
+
+  g_free (results->prop);
+  results->prop = NULL;
 
   return TRUE;
 }
@@ -772,7 +788,11 @@ size_hints_from_results (GetPropertyResults *results,
     return FALSE;
 
   if (results->n_items < OldNumPropSizeElements)
-    return FALSE;
+    {
+      g_free (results->prop);
+      results->prop = NULL;
+      return FALSE;
+    }
 
   raw = (xPropSizeHints*) results->prop;
 