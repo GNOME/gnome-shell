9cf8aa4584f056678ff9dd93ec247f03665a04b7
Florian Müllner <fmuellner@gnome.org>
2017-04-28 22:25:54 +0200

    cally: Fix compilation without X11 backend
    Commit 0fd9e38175 fixed setting the out parameter for the x coordinate
    when using the X11 backend, but broke compilation when the backend is
    not available ...
    Really fix the issue by running the X11-specific code when the X11
    backend is available and in use, and display the one-time warning
    otherwise.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=781902

--- clutter/clutter/cally/cally-actor.c
+++ clutter/clutter/cally/cally-actor.c
@@ -780,8 +780,8 @@ _cally_actor_get_top_level_origin (ClutterActor *actor,
         g_warning ("[x11] We were not able to get proper absolute "
                    "position of the stage");
     }
-#else
   else
+#endif
     {
       static gboolean yet_warned = FALSE;
 
@@ -793,7 +793,6 @@ _cally_actor_get_top_level_origin (ClutterActor *actor,
                      "atk_component_get_extents() with ATK_XY_SCREEN.");
         }
     }
-#endif
 
   if (xp)
     *xp = x;
