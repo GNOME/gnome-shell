GNOME/gnome-shell

commit ffe4eaf00d8780bdc6fb3d7aca3eda847e887007

st: Use G_DECLARE_*_TYPE
Cut down on boilerplate by using the (no longer that) new helper
macros. We don't care about breaking ABI in private libraries, so
use G_DECLARE_FINAL_TYPE even where the class struct used to be
exposed in the header, except for types we inherit from ourselves
(obviously) or where the class exposes any vfuncs (where changes
could affect inheritance in extensions).

src/st/st-entry.c
@@ -87,9 +87,10 @@ enum
   LAST_SIGNAL
 };
 
-#define ST_ENTRY_PRIV(x) ((StEntry *) x)->priv
+#define ST_ENTRY_PRIV(x) st_entry_get_instance_private ((StEntry *) x)
 
 
+typedef struct _StEntryPrivate StEntryPrivate;
 struct _StEntryPrivate
 {
   ClutterActor *entry;
@@ -182,7 +183,8 @@ st_entry_get_property (GObject    *gobject,
 static void
 show_capslock_feedback (StEntry *entry)
 {
-  if (entry->priv->secondary_icon == NULL)
+  StEntryPrivate *priv = ST_ENTRY_PRIV (entry);
+  if (priv->secondary_icon == NULL)
     {
       ClutterActor *icon = g_object_new (ST_TYPE_ICON,
                                          "style-class", "capslock-warning",
@@ -190,17 +192,18 @@ show_capslock_feedback (StEntry *entry)
                                          NULL);
 
       st_entry_set_secondary_icon (entry, icon);
-      entry->priv->capslock_warning_shown = TRUE;
+      priv->capslock_warning_shown = TRUE;
     }
 }
 
 static void
 remove_capslock_feedback (StEntry *entry)
 {
-  if (entry->priv->capslock_warning_shown)
+  StEntryPrivate *priv = ST_ENTRY_PRIV (entry);
+  if (priv->capslock_warning_shown)
     {
       st_entry_set_secondary_icon (entry, NULL);
-      entry->priv->capslock_warning_shown = FALSE;
+      priv->capslock_warning_shown = FALSE;
     }
 }
 
@@ -209,8 +212,9 @@ keymap_state_changed (GdkKeymap *keymap,
                       gpointer   user_data)
 {
   StEntry *entry = ST_ENTRY (user_data);
+  StEntryPrivate *priv = ST_ENTRY_PRIV (entry);
 
-  if (clutter_text_get_password_char (CLUTTER_TEXT (entry->priv->entry)) != 0)
+  if (clutter_text_get_password_char (CLUTTER_TEXT (priv->entry)) != 0)
     {
       if (gdk_keymap_get_caps_lock_state (keymap))
         show_capslock_feedback (entry);
@@ -223,7 +227,7 @@ static void
 st_entry_dispose (GObject *object)
 {
   StEntry *entry = ST_ENTRY (object);
-  StEntryPrivate *priv = entry->priv;
+  StEntryPrivate *priv = ST_ENTRY_PRIV (entry);
   GdkKeymap *keymap;
 
   if (priv->entry)
@@ -479,7 +483,7 @@ clutter_text_focus_in_cb (ClutterText  *text,
                           ClutterActor *actor)
 {
   StEntry *entry = ST_ENTRY (actor);
-  StEntryPrivate *priv = entry->priv;
+  StEntryPrivate *priv = ST_ENTRY_PRIV (entry);
   GdkKeymap *keymap;
 
   /* remove the hint if visible */
@@ -505,7 +509,7 @@ clutter_text_focus_out_cb (ClutterText  *text,
                            ClutterActor *actor)
 {
   StEntry *entry = ST_ENTRY (actor);
-  StEntryPrivate *priv = entry->priv;
+  StEntryPrivate *priv = ST_ENTRY_PRIV (entry);
   GdkKeymap *keymap;
 
   st_widget_remove_style_pseudo_class (ST_WIDGET (actor), "focus");
@@ -531,8 +535,9 @@ clutter_text_password_char_cb (GObject    *object,
                                gpointer    user_data)
 {
   StEntry *entry = ST_ENTRY (user_data);
+  StEntryPrivate *priv = ST_ENTRY_PRIV (entry);
 
-  if (clutter_text_get_password_char (CLUTTER_TEXT (entry->priv->entry)) == 0)
+  if (clutter_text_get_password_char (CLUTTER_TEXT (priv->entry)) == 0)
     remove_capslock_feedback (entry);
 }
 
@@ -541,7 +546,8 @@ st_entry_clipboard_callback (StClipboard *clipboard,
                              const gchar *text,
                              gpointer     data)
 {
-  ClutterText *ctext = (ClutterText*)((StEntry *) data)->priv->entry;
+  StEntryPrivate *priv = ST_ENTRY_PRIV (data);
+  ClutterText *ctext = (ClutterText*)priv->entry;
   gint cursor_pos;
 
   if (!text)
@@ -720,14 +726,15 @@ st_entry_set_cursor (StEntry  *entry,
 {
   cursor_func (entry, use_ibeam, cursor_func_data);
 
-  entry->priv->has_ibeam = use_ibeam;
+  ((StEntryPrivate *)ST_ENTRY_PRIV (entry))->has_ibeam = use_ibeam;
 }
 
 static gboolean
 st_entry_enter_event (ClutterActor         *actor,
                       ClutterCrossingEvent *event)
 {
-  if (event->source == ST_ENTRY (actor)->priv->entry && event->related != NULL)
+  StEntryPrivate *priv = ST_ENTRY_PRIV (actor);
+  if (event->source == priv->entry && event->related != NULL)
     st_entry_set_cursor (ST_ENTRY (actor), TRUE);
 
   return CLUTTER_ACTOR_CLASS (st_entry_parent_class)->enter_event (actor, event);
@@ -737,7 +744,8 @@ static gboolean
 st_entry_leave_event (ClutterActor         *actor,
                       ClutterCrossingEvent *event)
 {
-  if (event->source == ST_ENTRY (actor)->priv->entry && event->related != NULL)
+  StEntryPrivate *priv = ST_ENTRY_PRIV (actor);
+  if (event->source == priv->entry && event->related != NULL)
     st_entry_set_cursor (ST_ENTRY (actor), FALSE);
 
   return CLUTTER_ACTOR_CLASS (st_entry_parent_class)->leave_event (actor, event);
@@ -746,7 +754,8 @@ st_entry_leave_event (ClutterActor         *actor,
 static void
 st_entry_unmap (ClutterActor *actor)
 {
-  if (ST_ENTRY (actor)->priv->has_ibeam)
+  StEntryPrivate *priv = ST_ENTRY_PRIV (actor);
+  if (priv->has_ibeam)
     st_entry_set_cursor (ST_ENTRY (actor), FALSE);
 
   CLUTTER_ACTOR_CLASS (st_entry_parent_class)->unmap (actor);
@@ -855,7 +864,7 @@ st_entry_init (StEntry *entry)
 {
   StEntryPrivate *priv;
 
-  priv = entry->priv = st_entry_get_instance_private (entry);
+  priv = st_entry_get_instance_private (entry);
 
   priv->entry = g_object_new (ST_TYPE_IM_TEXT,
                               "line-alignment", PANGO_ALIGN_LEFT,
@@ -917,12 +926,15 @@ st_entry_new (const gchar *text)
 const gchar *
 st_entry_get_text (StEntry *entry)
 {
+  StEntryPrivate *priv;
+
   g_return_val_if_fail (ST_IS_ENTRY (entry), NULL);
 
-  if (entry->priv->hint_visible)
+  priv = st_entry_get_instance_private (entry);
+  if (priv->hint_visible)
     return "";
   else
-    return clutter_text_get_text (CLUTTER_TEXT (entry->priv->entry));
+    return clutter_text_get_text (CLUTTER_TEXT (priv->entry));
 }
 
 /**
@@ -940,7 +952,7 @@ st_entry_set_text (StEntry     *entry,
 
   g_return_if_fail (ST_IS_ENTRY (entry));
 
-  priv = entry->priv;
+  priv = st_entry_get_instance_private (entry);
 
   /* set a hint if we are blanking the entry */
   if (priv->hint
@@ -977,7 +989,7 @@ st_entry_get_clutter_text (StEntry *entry)
 {
   g_return_val_if_fail (ST_ENTRY (entry), NULL);
 
-  return entry->priv->entry;
+  return ((StEntryPrivate *)ST_ENTRY_PRIV (entry))->entry;
 }
 
 /**
@@ -997,7 +1009,7 @@ st_entry_set_hint_text (StEntry     *entry,
 
   g_return_if_fail (ST_IS_ENTRY (entry));
 
-  priv = entry->priv;
+  priv = st_entry_get_instance_private (entry);
 
   g_free (priv->hint);
 
@@ -1027,7 +1039,7 @@ st_entry_get_hint_text (StEntry *entry)
 {
   g_return_val_if_fail (ST_IS_ENTRY (entry), NULL);
 
-  return entry->priv->hint;
+  return ((StEntryPrivate *)ST_ENTRY_PRIV (entry))->hint;
 }
 
 /**
@@ -1043,11 +1055,13 @@ void
 st_entry_set_input_purpose (StEntry        *entry,
                             GtkInputPurpose purpose)
 {
+  StEntryPrivate *priv;
   StIMText *imtext;
 
   g_return_if_fail (ST_IS_ENTRY (entry));
 
-  imtext = ST_IM_TEXT (entry->priv->entry);
+  priv = st_entry_get_instance_private (entry);
+  imtext = ST_IM_TEXT (priv->entry);
 
   if (st_im_text_get_input_purpose (imtext) != purpose)
     {
@@ -1066,9 +1080,12 @@ st_entry_set_input_purpose (StEntry        *entry,
 GtkInputPurpose
 st_entry_get_input_purpose (StEntry *entry)
 {
+  StEntryPrivate *priv;
+
   g_return_val_if_fail (ST_IS_ENTRY (entry), GTK_INPUT_PURPOSE_FREE_FORM);
 
-  return st_im_text_get_input_purpose (ST_IM_TEXT (entry->priv->entry));
+  priv = st_entry_get_instance_private (entry);
+  return st_im_text_get_input_purpose (ST_IM_TEXT (priv->entry));
 }
 
 /**
@@ -1083,11 +1100,13 @@ void
 st_entry_set_input_hints (StEntry      *entry,
                           GtkInputHints hints)
 {
+  StEntryPrivate *priv;
   StIMText *imtext;
 
   g_return_if_fail (ST_IS_ENTRY (entry));
 
-  imtext = ST_IM_TEXT (entry->priv->entry);
+  priv = st_entry_get_instance_private (entry);
+  imtext = ST_IM_TEXT (priv->entry);
 
   if (st_im_text_get_input_hints (imtext) != hints)
     {
@@ -1106,9 +1125,12 @@ st_entry_set_input_hints (StEntry      *entry,
 GtkInputHints
 st_entry_get_input_hints (StEntry *entry)
 {
+  StEntryPrivate *priv;
+
   g_return_val_if_fail (ST_IS_ENTRY (entry), GTK_INPUT_HINT_NONE);
 
-  return st_im_text_get_input_hints (ST_IM_TEXT (entry->priv->entry));
+  priv = st_entry_get_instance_private (entry);
+  return st_im_text_get_input_hints (ST_IM_TEXT (priv->entry));
 }
 
 static gboolean
@@ -1116,7 +1138,7 @@ _st_entry_icon_press_cb (ClutterActor       *actor,
                          ClutterButtonEvent *event,
                          StEntry            *entry)
 {
-  StEntryPrivate *priv = entry->priv;
+  StEntryPrivate *priv = ST_ENTRY_PRIV (actor);
 
   if (actor == priv->primary_icon)
     g_signal_emit (entry, entry_signals[PRIMARY_ICON_CLICKED], 0);
@@ -1168,7 +1190,7 @@ st_entry_set_primary_icon (StEntry      *entry,
 
   g_return_if_fail (ST_IS_ENTRY (entry));
 
-  priv = entry->priv;
+  priv = st_entry_get_instance_private (entry);
 
   _st_entry_set_icon (entry, &priv->primary_icon, icon);
 }
@@ -1188,7 +1210,7 @@ st_entry_set_secondary_icon (StEntry      *entry,
 
   g_return_if_fail (ST_IS_ENTRY (entry));
 
-  priv = entry->priv;
+  priv = st_entry_get_instance_private (entry);
 
   _st_entry_set_icon (entry, &priv->secondary_icon, icon);
 }
@@ -1239,6 +1261,7 @@ static gint
 st_entry_accessible_get_n_children (AtkObject *obj)
 {
   StEntry *entry = NULL;
+  StEntryPrivate *priv;
 
   g_return_val_if_fail (ST_IS_ENTRY_ACCESSIBLE (obj), 0);
 
@@ -1247,7 +1270,8 @@ st_entry_accessible_get_n_children (AtkObject *obj)
   if (entry == NULL)
     return 0;
 
-  if (entry->priv->entry == NULL)
+  priv = st_entry_get_instance_private (entry);
+  if (priv->entry == NULL)
     return 0;
   else
     return 1;
@@ -1258,6 +1282,7 @@ st_entry_accessible_ref_child (AtkObject *obj,
                                gint       i)
 {
   StEntry *entry = NULL;
+  StEntryPrivate *priv;
   AtkObject *result = NULL;
 
   g_return_val_if_fail (ST_IS_ENTRY_ACCESSIBLE (obj), NULL);
@@ -1268,10 +1293,11 @@ st_entry_accessible_ref_child (AtkObject *obj,
   if (entry == NULL)
     return NULL;
 
-  if (entry->priv->entry == NULL)
+  priv = st_entry_get_instance_private (entry);
+  if (priv->entry == NULL)
     return NULL;
 
-  result = clutter_actor_get_accessible (entry->priv->entry);
+  result = clutter_actor_get_accessible (priv->entry);
   g_object_ref (result);
 
   return result;

src/st/st-adjustment.c
@@ -37,6 +37,8 @@
 #include "st-adjustment.h"
 #include "st-private.h"
 
+typedef struct _StAdjustmentPrivate StAdjustmentPrivate;
+
 struct _StAdjustmentPrivate
 {
   /* Do not sanity-check values while constructing,
@@ -90,6 +92,7 @@ st_adjustment_constructed (GObject *object)
 {
   GObjectClass *g_class;
   StAdjustment *self = ST_ADJUSTMENT (object);
+  StAdjustmentPrivate *priv = st_adjustment_get_instance_private (self);
 
   g_class = G_OBJECT_CLASS (st_adjustment_parent_class);
   /* The docs say we're suppose to chain up, but would crash without
@@ -100,8 +103,8 @@ st_adjustment_constructed (GObject *object)
       g_class->constructed (object);
     }
 
-  ST_ADJUSTMENT (self)->priv->is_constructing = FALSE;
-  st_adjustment_clamp_page (self, self->priv->lower, self->priv->upper);
+  priv->is_constructing = FALSE;
+  st_adjustment_clamp_page (self, priv->lower, priv->upper);
 }
 
 static void
@@ -110,7 +113,7 @@ st_adjustment_get_property (GObject    *gobject,
                             GValue     *value,
                             GParamSpec *pspec)
 {
-  StAdjustmentPrivate *priv = ST_ADJUSTMENT (gobject)->priv;
+  StAdjustmentPrivate *priv = st_adjustment_get_instance_private (ST_ADJUSTMENT (gobject));
 
   switch (prop_id)
     {
@@ -271,9 +274,8 @@ st_adjustment_class_init (StAdjustmentClass *klass)
 static void
 st_adjustment_init (StAdjustment *self)
 {
-  self->priv = st_adjustment_get_instance_private (self);
-
-  self->priv->is_constructing = TRUE;
+  StAdjustmentPrivate *priv = st_adjustment_get_instance_private (self);
+  priv->is_constructing = TRUE;
 }
 
 StAdjustment *
@@ -297,13 +299,9 @@ st_adjustment_new (gdouble value,
 gdouble
 st_adjustment_get_value (StAdjustment *adjustment)
 {
-  StAdjustmentPrivate *priv;
-
   g_return_val_if_fail (ST_IS_ADJUSTMENT (adjustment), 0);
 
-  priv = adjustment->priv;
-
-  return priv->value;
+  return ((StAdjustmentPrivate *)st_adjustment_get_instance_private (adjustment))->value;
 }
 
 void
@@ -314,7 +312,7 @@ st_adjustment_set_value (StAdjustment *adjustment,
 
   g_return_if_fail (ST_IS_ADJUSTMENT (adjustment));
 
-  priv = adjustment->priv;
+  priv = st_adjustment_get_instance_private (adjustment);
 
   /* Defer clamp until after construction. */
   if (!priv->is_constructing)
@@ -342,7 +340,7 @@ st_adjustment_clamp_page (StAdjustment *adjustment,
 
   g_return_if_fail (ST_IS_ADJUSTMENT (adjustment));
 
-  priv = adjustment->priv;
+  priv = st_adjustment_get_instance_private (adjustment);
 
   lower = CLAMP (lower, priv->lower, priv->upper - priv->page_size);
   upper = CLAMP (upper, priv->lower + priv->page_size, priv->upper);
@@ -369,7 +367,7 @@ static gboolean
 st_adjustment_set_lower (StAdjustment *adjustment,
                          gdouble       lower)
 {
-  StAdjustmentPrivate *priv = adjustment->priv;
+  StAdjustmentPrivate *priv = st_adjustment_get_instance_private (adjustment);
 
   if (priv->lower != lower)
     {
@@ -393,7 +391,7 @@ static gboolean
 st_adjustment_set_upper (StAdjustment *adjustment,
                          gdouble       upper)
 {
-  StAdjustmentPrivate *priv = adjustment->priv;
+  StAdjustmentPrivate *priv = st_adjustment_get_instance_private (adjustment);
 
   if (priv->upper != upper)
     {
@@ -417,7 +415,7 @@ static gboolean
 st_adjustment_set_step_increment (StAdjustment *adjustment,
                                   gdouble       step)
 {
-  StAdjustmentPrivate *priv = adjustment->priv;
+  StAdjustmentPrivate *priv = st_adjustment_get_instance_private (adjustment);
 
   if (priv->step_increment != step)
     {
@@ -437,7 +435,7 @@ static gboolean
 st_adjustment_set_page_increment (StAdjustment *adjustment,
                                   gdouble       page)
 {
-  StAdjustmentPrivate *priv = adjustment->priv;
+  StAdjustmentPrivate *priv = st_adjustment_get_instance_private (adjustment);
 
   if (priv->page_increment != page)
     {
@@ -457,7 +455,7 @@ static gboolean
 st_adjustment_set_page_size (StAdjustment *adjustment,
                              gdouble       size)
 {
-  StAdjustmentPrivate *priv = adjustment->priv;
+  StAdjustmentPrivate *priv = st_adjustment_get_instance_private (adjustment);
 
   if (priv->page_size != size)
     {
@@ -494,7 +492,7 @@ st_adjustment_set_values (StAdjustment *adjustment,
   g_return_if_fail (step_increment >= 0 && step_increment <= G_MAXDOUBLE);
   g_return_if_fail (page_increment >= 0 && page_increment <= G_MAXDOUBLE);
 
-  priv = adjustment->priv;
+  priv = st_adjustment_get_instance_private (adjustment);
 
   emit_changed = FALSE;
 
@@ -543,7 +541,7 @@ st_adjustment_get_values (StAdjustment *adjustment,
 
   g_return_if_fail (ST_IS_ADJUSTMENT (adjustment));
 
-  priv = adjustment->priv;
+  priv = st_adjustment_get_instance_private (adjustment);
 
   if (lower)
     *lower = priv->lower;
@@ -585,7 +583,7 @@ st_adjustment_adjust_for_scroll_event (StAdjustment *adjustment,
 
   g_return_if_fail (ST_IS_ADJUSTMENT (adjustment));
 
-  priv = adjustment->priv;
+  priv = st_adjustment_get_instance_private (adjustment);
 
   scroll_unit = pow (priv->page_size, 2.0 / 3.0);
 

src/st/st-adjustment.h
@@ -30,31 +30,8 @@
 
 G_BEGIN_DECLS
 
-#define ST_TYPE_ADJUSTMENT            (st_adjustment_get_type())
-#define ST_ADJUSTMENT(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), ST_TYPE_ADJUSTMENT, StAdjustment))
-#define ST_IS_ADJUSTMENT(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), ST_TYPE_ADJUSTMENT))
-#define ST_ADJUSTMENT_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), ST_TYPE_ADJUSTMENT, StAdjustmentClass))
-#define ST_IS_ADJUSTMENT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), ST_TYPE_ADJUSTMENT))
-#define ST_ADJUSTMENT_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), ST_TYPE_ADJUSTMENT, StAdjustmentClass))
-
-typedef struct _StAdjustment          StAdjustment;
-typedef struct _StAdjustmentPrivate   StAdjustmentPrivate;
-typedef struct _StAdjustmentClass     StAdjustmentClass;
-
-/**
- * StAdjustment:
- *
- * Class for handling an interval between to values. The contents of
- * the #StAdjustment are private and should be accessed using the
- * public API.
- */
-struct _StAdjustment
-{
-  /*< private >*/
-  GObject parent_instance;
-
-  StAdjustmentPrivate *priv;
-};
+#define ST_TYPE_ADJUSTMENT (st_adjustment_get_type())
+G_DECLARE_DERIVABLE_TYPE (StAdjustment, st_adjustment, ST, ADJUSTMENT, GObject)
 
 /**
  * StAdjustmentClass:
@@ -70,8 +47,6 @@ struct _StAdjustmentClass
   /*< public >*/
   void (* changed) (StAdjustment *adjustment);
 };
-
-GType st_adjustment_get_type (void) G_GNUC_CONST;
 
 StAdjustment *st_adjustment_new         (gdouble       value,
                                          gdouble       lower,

src/st/st-bin.c
@@ -38,6 +38,7 @@
 #include "st-enum-types.h"
 #include "st-private.h"
 
+typedef struct _StBinPrivate          StBinPrivate;
 struct _StBinPrivate
 {
   ClutterActor *child;
@@ -78,10 +79,11 @@ static void
 st_bin_remove (ClutterContainer *container,
                ClutterActor     *actor)
 {
-  StBinPrivate *priv = ST_BIN (container)->priv;
+  StBin *bin = ST_BIN (container);
+  StBinPrivate *priv = st_bin_get_instance_private (bin);
 
   if (priv->child == actor)
-    st_bin_set_child (ST_BIN (container), NULL);
+    st_bin_set_child (bin, NULL);
 }
 
 static void
@@ -96,7 +98,7 @@ st_bin_allocate (ClutterActor          *self,
                  const ClutterActorBox *box,
                  ClutterAllocationFlags flags)
 {
-  StBinPrivate *priv = ST_BIN (self)->priv;
+  StBinPrivate *priv = st_bin_get_instance_private (ST_BIN (self));
 
   clutter_actor_set_allocation (self, box, flags);
 
@@ -122,7 +124,7 @@ st_bin_get_preferred_width (ClutterActor *self,
                             gfloat       *min_width_p,
                             gfloat       *natural_width_p)
 {
-  StBinPrivate *priv = ST_BIN (self)->priv;
+  StBinPrivate *priv = st_bin_get_instance_private (ST_BIN (self));
   StThemeNode *theme_node = st_widget_get_theme_node (ST_WIDGET (self));
 
   st_theme_node_adjust_for_height (theme_node, &for_height);
@@ -151,7 +153,7 @@ st_bin_get_preferred_height (ClutterActor *self,
                              gfloat       *min_height_p,
                              gfloat       *natural_height_p)
 {
-  StBinPrivate *priv = ST_BIN (self)->priv;
+  StBinPrivate *priv = st_bin_get_instance_private (ST_BIN (self));
   StThemeNode *theme_node = st_widget_get_theme_node (ST_WIDGET (self));
 
   st_theme_node_adjust_for_width (theme_node, &for_width);
@@ -177,7 +179,7 @@ st_bin_get_preferred_height (ClutterActor *self,
 static void
 st_bin_dispose (GObject *gobject)
 {
-  StBinPrivate *priv = ST_BIN (gobject)->priv;
+  StBinPrivate *priv = st_bin_get_instance_private (ST_BIN (gobject));
 
   if (priv->child)
     clutter_actor_destroy (priv->child);
@@ -189,7 +191,7 @@ st_bin_dispose (GObject *gobject)
 static void
 st_bin_popup_menu (StWidget *widget)
 {
-  StBinPrivate *priv = ST_BIN (widget)->priv;
+  StBinPrivate *priv = st_bin_get_instance_private (ST_BIN (widget));
 
   if (priv->child && ST_IS_WIDGET (priv->child))
     st_widget_popup_menu (ST_WIDGET (priv->child));
@@ -200,7 +202,7 @@ st_bin_navigate_focus (StWidget         *widget,
                        ClutterActor     *from,
                        GtkDirectionType  direction)
 {
-  StBinPrivate *priv = ST_BIN (widget)->priv;
+  StBinPrivate *priv = st_bin_get_instance_private (ST_BIN (widget));
   ClutterActor *bin_actor = CLUTTER_ACTOR (widget);
 
   if (st_widget_get_can_focus (widget))
@@ -231,6 +233,7 @@ st_bin_set_property (GObject      *gobject,
                      GParamSpec   *pspec)
 {
   StBin *bin = ST_BIN (gobject);
+  StBinPrivate *priv = st_bin_get_instance_private (bin);
 
   switch (prop_id)
     {
@@ -241,24 +244,24 @@ st_bin_set_property (GObject      *gobject,
     case PROP_X_ALIGN:
       st_bin_set_alignment (bin,
                             g_value_get_enum (value),
-                            bin->priv->y_align);
+                            priv->y_align);
       break;
 
     case PROP_Y_ALIGN:
       st_bin_set_alignment (bin,
-                            bin->priv->x_align,
+                            priv->x_align,
                             g_value_get_enum (value));
       break;
 
     case PROP_X_FILL:
       st_bin_set_fill (bin,
                        g_value_get_boolean (value),
-                       bin->priv->y_fill);
+                       priv->y_fill);
       break;
 
     case PROP_Y_FILL:
       st_bin_set_fill (bin,
-                       bin->priv->x_fill,
+                       priv->x_fill,
                        g_value_get_boolean (value));
       break;
 
@@ -273,7 +276,7 @@ st_bin_get_property (GObject    *gobject,
                      GValue     *value,
                      GParamSpec *pspec)
 {
-  StBinPrivate *priv = ST_BIN (gobject)->priv;
+  StBinPrivate *priv = st_bin_get_instance_private (ST_BIN (gobject));
 
   switch (prop_id)
     {
@@ -389,10 +392,10 @@ st_bin_class_init (StBinClass *klass)
 static void
 st_bin_init (StBin *bin)
 {
-  bin->priv = st_bin_get_instance_private (bin);
+  StBinPrivate *priv = st_bin_get_instance_private (bin);
 
-  bin->priv->x_align = ST_ALIGN_MIDDLE;
-  bin->priv->y_align = ST_ALIGN_MIDDLE;
+  priv->x_align = ST_ALIGN_MIDDLE;
+  priv->y_align = ST_ALIGN_MIDDLE;
 }
 
 /**
@@ -426,7 +429,7 @@ st_bin_set_child (StBin        *bin,
   g_return_if_fail (ST_IS_BIN (bin));
   g_return_if_fail (child == NULL || CLUTTER_IS_ACTOR (child));
 
-  priv = bin->priv;
+  priv = st_bin_get_instance_private (bin);
 
   if (priv->child == child)
     return;
@@ -460,7 +463,7 @@ st_bin_get_child (StBin *bin)
 {
   g_return_val_if_fail (ST_IS_BIN (bin), NULL);
 
-  return bin->priv->child;
+  return ((StBinPrivate *)st_bin_get_instance_private (bin))->child;
 }
 
 /**
@@ -482,7 +485,7 @@ st_bin_set_alignment (StBin  *bin,
 
   g_return_if_fail (ST_IS_BIN (bin));
 
-  priv = bin->priv;
+  priv = st_bin_get_instance_private (bin);
 
   g_object_freeze_notify (G_OBJECT (bin));
 
@@ -524,7 +527,7 @@ st_bin_get_alignment (StBin   *bin,
 
   g_return_if_fail (ST_IS_BIN (bin));
 
-  priv = bin->priv;
+  priv = st_bin_get_instance_private (bin);
 
   if (x_align)
     *x_align = priv->x_align;
@@ -552,7 +555,7 @@ st_bin_set_fill (StBin   *bin,
 
   g_return_if_fail (ST_IS_BIN (bin));
 
-  priv = bin->priv;
+  priv = st_bin_get_instance_private (bin);
 
   g_object_freeze_notify (G_OBJECT (bin));
 
@@ -591,11 +594,15 @@ st_bin_get_fill (StBin    *bin,
                  gboolean *x_fill,
                  gboolean *y_fill)
 {
+  StBinPrivate *priv;
+
   g_return_if_fail (ST_IS_BIN (bin));
 
+  priv = st_bin_get_instance_private (bin);
+
   if (x_fill)
-    *x_fill = bin->priv->x_fill;
+    *x_fill = priv->x_fill;
 
   if (y_fill)
-    *y_fill = bin->priv->y_fill;
+    *y_fill = priv->y_fill;
 }

src/st/st-bin.h
@@ -30,28 +30,7 @@
 G_BEGIN_DECLS
 
 #define ST_TYPE_BIN                   (st_bin_get_type ())
-#define ST_BIN(obj)                   (G_TYPE_CHECK_INSTANCE_CAST ((obj), ST_TYPE_BIN, StBin))
-#define ST_IS_BIN(obj)                (G_TYPE_CHECK_INSTANCE_TYPE ((obj), ST_TYPE_BIN))
-#define ST_BIN_CLASS(klass)           (G_TYPE_CHECK_CLASS_CAST ((klass), ST_TYPE_BIN, StBinClass))
-#define ST_IS_BIN_CLASS(klass)        (G_TYPE_CHECK_CLASS_TYPE ((klass), ST_TYPE_BIN))
-#define ST_BIN_GET_CLASS(obj)         (G_TYPE_INSTANCE_GET_CLASS ((obj), ST_TYPE_BIN, StBinClass))
-
-typedef struct _StBin                 StBin;
-typedef struct _StBinPrivate          StBinPrivate;
-typedef struct _StBinClass            StBinClass;
-
-/**
- * StBin:
- *
- * The #StBin struct contains only private data
- */
-struct _StBin
-{
-  /*< private >*/
-  StWidget parent_instance;
-
-  StBinPrivate *priv;
-};
+G_DECLARE_DERIVABLE_TYPE (StBin, st_bin, ST, BIN, StWidget)
 
 /**
  * StBinClass:
@@ -63,8 +42,6 @@ struct _StBinClass
   /*< private >*/
   StWidgetClass parent_class;
 };
-
-GType st_bin_get_type (void) G_GNUC_CONST;
 
 StWidget   *  st_bin_new           (void);
 void          st_bin_set_child     (StBin        *bin,

src/st/st-border-image.h
@@ -28,17 +28,9 @@ G_BEGIN_DECLS
 
 /* A StBorderImage encapsulates an image with specified unscaled borders on each edge.
  */
-typedef struct _StBorderImage      StBorderImage;
-typedef struct _StBorderImageClass StBorderImageClass;
 
 #define ST_TYPE_BORDER_IMAGE             (st_border_image_get_type ())
-#define ST_BORDER_IMAGE(object)          (G_TYPE_CHECK_INSTANCE_CAST ((object), ST_TYPE_BORDER_IMAGE, StBorderImage))
-#define ST_BORDER_IMAGE_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), ST_TYPE_BORDER_IMAGE, StBorderImageClass))
-#define ST_IS_BORDER_IMAGE(object)       (G_TYPE_CHECK_INSTANCE_TYPE ((object), ST_TYPE_BORDER_IMAGE))
-#define ST_IS_BORDER_IMAGE_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), ST_TYPE_BORDER_IMAGE))
-#define ST_BORDER_IMAGE_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), ST_TYPE_BORDER_IMAGE, StBorderImageClass))
-
-GType             st_border_image_get_type          (void) G_GNUC_CONST;
+G_DECLARE_FINAL_TYPE (StBorderImage, st_border_image, ST, BORDER_IMAGE, GObject)
 
 StBorderImage *st_border_image_new (GFile      *file,
                                     int         border_top,

src/st/st-box-layout-child.h
@@ -27,29 +27,9 @@
 G_BEGIN_DECLS
 
 #define ST_TYPE_BOX_LAYOUT_CHILD st_box_layout_child_get_type()
-
-#define ST_BOX_LAYOUT_CHILD(obj) \
-  (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
-  ST_TYPE_BOX_LAYOUT_CHILD, StBoxLayoutChild))
-
-#define ST_BOX_LAYOUT_CHILD_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_CAST ((klass), \
-  ST_TYPE_BOX_LAYOUT_CHILD, StBoxLayoutChildClass))
-
-#define ST_IS_BOX_LAYOUT_CHILD(obj) \
-  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
-  ST_TYPE_BOX_LAYOUT_CHILD))
-
-#define ST_IS_BOX_LAYOUT_CHILD_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_TYPE ((klass), \
-  ST_TYPE_BOX_LAYOUT_CHILD))
-
-#define ST_BOX_LAYOUT_CHILD_GET_CLASS(obj) \
-  (G_TYPE_INSTANCE_GET_CLASS ((obj), \
-  ST_TYPE_BOX_LAYOUT_CHILD, StBoxLayoutChildClass))
+G_DECLARE_FINAL_TYPE (StBoxLayoutChild, st_box_layout_child, ST, BOX_LAYOUT_CHILD, ClutterChildMeta)
 
 typedef struct _StBoxLayoutChild StBoxLayoutChild;
-typedef struct _StBoxLayoutChildClass StBoxLayoutChildClass;
 typedef struct _StBoxLayoutChildPrivate StBoxLayoutChildPrivate;
 
 /**
@@ -66,13 +46,6 @@ struct _StBoxLayoutChild
   gboolean x_fill_set;
   gboolean y_fill_set;
 };
-
-struct _StBoxLayoutChildClass
-{
-  ClutterChildMetaClass parent_class;
-};
-
-GType st_box_layout_child_get_type (void);
 
 G_END_DECLS
 

src/st/st-box-layout.h
@@ -30,29 +30,9 @@
 G_BEGIN_DECLS
 
 #define ST_TYPE_BOX_LAYOUT st_box_layout_get_type()
-
-#define ST_BOX_LAYOUT(obj) \
-  (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
-  ST_TYPE_BOX_LAYOUT, StBoxLayout))
-
-#define ST_BOX_LAYOUT_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_CAST ((klass), \
-  ST_TYPE_BOX_LAYOUT, StBoxLayoutClass))
-
-#define ST_IS_BOX_LAYOUT(obj) \
-  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
-  ST_TYPE_BOX_LAYOUT))
-
-#define ST_IS_BOX_LAYOUT_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_TYPE ((klass), \
-  ST_TYPE_BOX_LAYOUT))
-
-#define ST_BOX_LAYOUT_GET_CLASS(obj) \
-  (G_TYPE_INSTANCE_GET_CLASS ((obj), \
-  ST_TYPE_BOX_LAYOUT, StBoxLayoutClass))
+G_DECLARE_FINAL_TYPE (StBoxLayout, st_box_layout, ST, BOX_LAYOUT, StWidget)
 
 typedef struct _StBoxLayout StBoxLayout;
-typedef struct _StBoxLayoutClass StBoxLayoutClass;
 typedef struct _StBoxLayoutPrivate StBoxLayoutPrivate;
 
 /**
@@ -68,13 +48,6 @@ struct _StBoxLayout
 
   StBoxLayoutPrivate *priv;
 };
-
-struct _StBoxLayoutClass
-{
-  StWidgetClass parent_class;
-};
-
-GType st_box_layout_get_type (void);
 
 StWidget *st_box_layout_new (void);
 

commit 94a0ae1ec38392e119bc7e7be0b1601caa4369cd

entry: Fix *-icon-clicked handler
Commit ffe4eaf00d8 broke the handler by fetching the instance private
from the wrong actor - as we don't use the ::primary-icon-clicked signal,
and the ::secondary-icon-clicked signal still works by accident, nobody
noticed until now ...

https://bugzilla.gnome.org/show_bug.cgi?id=782190

src/st/st-entry.c
@@ -1142,7 +1142,7 @@ _st_entry_icon_press_cb (ClutterActor       *actor,
                          ClutterButtonEvent *event,
                          StEntry            *entry)
 {
-  StEntryPrivate *priv = ST_ENTRY_PRIV (actor);
+  StEntryPrivate *priv = ST_ENTRY_PRIV (entry);
 
   if (actor == priv->primary_icon)
     g_signal_emit (entry, entry_signals[PRIMARY_ICON_CLICKED], 0);