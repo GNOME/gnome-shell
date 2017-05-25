c987d3d2c97687a3656043f8e5dfffd9aa2e31e8
Florian Müllner <fmuellner@gnome.org>
2017-05-25 02:16:27 +0200

    build: Support the meson build system
    Meson is on track to replace autotools as the build system of choice,
    so support it in addition to autotools. If all goes well, we'll
    eventually be able to drop the latter ...
    
    https://bugzilla.gnome.org/show_bug.cgi?id=783229

--- data/theme/parse-sass.sh
+++ data/theme/parse-sass.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/sh
 
 srcdir=`dirname $0`
+stamp=${1}
 for scss in $srcdir/*.scss
 do
-  sassc -a $scss ${scss%%.scss}.css
+  sassc -a $scss ${scss%%.scss}.css || exit 1
 done
+
+[ "$stamp" ] && touch $stamp
