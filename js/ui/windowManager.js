410b8181446da5a67fd12fe8afbca62333d8bca9
Mario Sanchez Prada <mario@endlessm.com>
2017-06-14 18:16:16 +0100

    workspaceMonitor: Animate transition to desktop when minimizing the last window
    When minimizing the last window (or when closing the side component with no
    windows open) we run a animation to transition from the desaturated state
    of the icon grid to the solid one. Similarly, for consistency, we also run
    the "opposite" animation when unmimizing the first window.
    
    https://phabricator.endlessm.com/T17662
    https://phabricator.endlessm.com/T19824
    https://phabricator.endlessm.com/T20661

--- js/ui/windowManager.js
+++ js/ui/windowManager.js
@@ -980,6 +980,7 @@ var WindowManager = class {
         // The desktop overlay needs to replicate the background's functionality;
         // when clicked, we animate the side component out before emitting "background-clicked".
         this._desktopOverlay.connect('clicked', () => {
+            Main.layoutManager.prepareToEnterOverview();
             this._slideSideComponentOut(this._shellwm,
                                         this._desktopOverlay.overlayActor,
                                         () => Main.layoutManager.emit('background-clicked'));
@@ -1666,6 +1667,7 @@ var WindowManager = class {
         }
 
         this._unminimizing.push(actor);
+        Main.layoutManager.prepareToLeaveOverview();
 
         if (actor.meta_window.is_monitor_sized()) {
             actor.opacity = 0;
@@ -2164,6 +2166,8 @@ var WindowManager = class {
             if (!this._showDesktopOnDestroyDone && SideComponent.shouldHideOtherWindows(actor.meta_window)) {
                 // reveal other windows while we slide out the side component
                 this._showOtherWindows(actor, true);
+            } else if (this._showDesktopOnDestroyDone) {
+                Main.layoutManager.prepareToEnterOverview();
             }
 
             return;
