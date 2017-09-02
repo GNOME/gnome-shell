f6258bae6e658c2faf10a2384045a98667d91b1f
Marco Trevisan (Treviño) <mail@3v1n0.net>
2017-09-01 21:08:55 -0500

    st-private: Scale shadows accordingly to actor resource scaling
    https://bugzilla.gnome.org/show_bug.cgi?id=765011

--- src/st/st-private.c
+++ src/st/st-private.c
@@ -430,6 +430,7 @@ _st_create_shadow_pipeline_from_actor (StShadow     *shadow_spec,
                                        ClutterActor *actor)
 {
   CoglPipeline *shadow_pipeline = NULL;
+  float resource_scale;
   float width, height;
 
   g_return_val_if_fail (clutter_actor_has_allocation (actor), NULL);
@@ -438,6 +439,12 @@ _st_create_shadow_pipeline_from_actor (StShadow     *shadow_spec,
 
   if (width == 0 || height == 0)
     return NULL;
+
+  if (!clutter_actor_get_resource_scale (actor, &resource_scale))
+    return NULL;
+
+  width *= resource_scale;
+  height *= resource_scale;
 
   if (CLUTTER_IS_TEXTURE (actor))
     {
@@ -489,6 +496,7 @@ _st_create_shadow_pipeline_from_actor (StShadow     *shadow_spec,
       cogl_framebuffer_clear (fb, COGL_BUFFER_BIT_COLOR, &clear_color);
       cogl_framebuffer_translate (fb, -x, -y, 0);
       cogl_framebuffer_orthographic (fb, 0, 0, width, height, 0, 1.0);
+      cogl_framebuffer_scale (fb, resource_scale, resource_scale, 1);
 
       clutter_actor_set_opacity_override (actor, 255);
       clutter_actor_paint (actor);
