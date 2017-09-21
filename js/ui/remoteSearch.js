GNOME/gnome-shell

commit 289f982949c26603efe1136859bbd9bb469469fb

remoteSearch: Allow search providers not to auto-start
This would be used by search providers which only operate on data in the
running instance, such as the terminal's search provider which finds the
shell in the tab matching the search text.

https://bugzilla.gnome.org/show_bug.cgi?id=785380

js/ui/remoteSearch.js
@@ -117,6 +117,13 @@ function loadRemoteSearchProviders(searchSettings, callback) {
                 // ignore error
             }
 
+            remoteProvider.autoStart = true;
+            try {
+                remoteProvider.autoStart = keyfile.get_boolean(group, 'AutoStart');
+            } catch(e) {
+                // ignore error
+            }
+
             objectPaths[objectPath] = remoteProvider;
             loadedProviders.push(remoteProvider);
         } catch(e) {
@@ -188,13 +195,18 @@ var RemoteSearchProvider = new Lang.Class({
         if (!proxyInfo)
             proxyInfo = SearchProviderProxyInfo;
 
+        let g_flags = Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES;
+        if (remoteProvider.autoStart)
+            g_flags |= Gio.DBusProxyFlags.DO_NOT_AUTO_START_AT_CONSTRUCTION;
+        else
+            g_flags |= Gio.DBusProxyFlags.DO_NOT_AUTO_START;
+
         this.proxy = new Gio.DBusProxy({ g_bus_type: Gio.BusType.SESSION,
                                          g_name: dbusName,
                                          g_object_path: dbusPath,
                                          g_interface_info: proxyInfo,
                                          g_interface_name: proxyInfo.name,
-                                         g_flags: (Gio.DBusProxyFlags.DO_NOT_AUTO_START_AT_CONSTRUCTION |
-                                                   Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES) });
+                                         g_flags });
         this.proxy.init_async(GLib.PRIORITY_DEFAULT, null, null);
 
         this.appInfo = appInfo;

commit f5a28c2f24398bbd2284513460945c71396059b4

remoteSearch: Fix remote search providers
Commit 289f982949c2 broke all remote providers when adding support for
non-auto-started search providers: Whether the provider should be
auto-started needs to be known in the constructor, so setting the
property on the constructed object doesn't work.

https://bugzilla.gnome.org/show_bug.cgi?id=787986

js/ui/remoteSearch.js
@@ -98,6 +98,13 @@ function loadRemoteSearchProviders(searchSettings, callback) {
                 return;
             }
 
+            let autoStart = true;
+            try {
+                autoStart = keyfile.get_boolean(group, 'AutoStart');
+            } catch(e) {
+                // ignore error
+            }
+
             let version = '1';
             try {
                 version = keyfile.get_string(group, 'Version');
@@ -106,20 +113,13 @@ function loadRemoteSearchProviders(searchSettings, callback) {
             }
 
             if (version >= 2)
-                remoteProvider = new RemoteSearchProvider2(appInfo, busName, objectPath);
+                remoteProvider = new RemoteSearchProvider2(appInfo, busName, objectPath, autoStart);
             else
-                remoteProvider = new RemoteSearchProvider(appInfo, busName, objectPath);
+                remoteProvider = new RemoteSearchProvider(appInfo, busName, objectPath, autoStart);
 
             remoteProvider.defaultEnabled = true;
             try {
                 remoteProvider.defaultEnabled = !keyfile.get_boolean(group, 'DefaultDisabled');
-            } catch(e) {
-                // ignore error
-            }
-
-            remoteProvider.autoStart = true;
-            try {
-                remoteProvider.autoStart = keyfile.get_boolean(group, 'AutoStart');
             } catch(e) {
                 // ignore error
             }
@@ -191,12 +191,12 @@ function loadRemoteSearchProviders(searchSettings, callback) {
 var RemoteSearchProvider = new Lang.Class({
     Name: 'RemoteSearchProvider',
 
-    _init: function(appInfo, dbusName, dbusPath, proxyInfo) {
+    _init: function(appInfo, dbusName, dbusPath, autoStart, proxyInfo) {
         if (!proxyInfo)
             proxyInfo = SearchProviderProxyInfo;
 
         let g_flags = Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES;
-        if (remoteProvider.autoStart)
+        if (autoStart)
             g_flags |= Gio.DBusProxyFlags.DO_NOT_AUTO_START_AT_CONSTRUCTION;
         else
             g_flags |= Gio.DBusProxyFlags.DO_NOT_AUTO_START;
@@ -319,8 +319,8 @@ var RemoteSearchProvider2 = new Lang.Class({
     Name: 'RemoteSearchProvider2',
     Extends: RemoteSearchProvider,
 
-    _init: function(appInfo, dbusName, dbusPath) {
-        this.parent(appInfo, dbusName, dbusPath, SearchProvider2ProxyInfo);
+    _init: function(appInfo, dbusName, dbusPath, autoStart) {
+        this.parent(appInfo, dbusName, dbusPath, autoStart, SearchProvider2ProxyInfo);
 
         this.canLaunchSearch = true;
     },