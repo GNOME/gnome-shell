jadahl/mutter

backends: Split out CRTC/output management to MetaGpu
In order to eventually support multilpe GPUs with their own connectors,
split out related meta data management (i.e. outputs, CRTCs and CRTC
modes) into a new MetaGpu GObject.

The Xrandr backend always assumes there is always only a single "GPU" as
the GPU is abstracted by the X server; only the native backend (aside
from the test backend) will eventually see more than one GPU.

The Xrandr backend still moves some management to MetaGpuXrandr, in
order to behave more similarly to the KMS counterparts.

https://bugzilla.gnome.org/show_bug.cgi?id=785381

src/backends/meta-gpu.c
@@ -1,0 +1,224 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+
+/*
+ * Copyright (C) 2017 Red Hat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#include "config.h"
+
+#include "backends/meta-gpu.h"
+
+#include "backends/meta-output.h"
+
+enum
+{
+  PROP_0,
+
+  PROP_MONITOR_MANAGER,
+
+  PROP_LAST
+};
+
+static GParamSpec *obj_props[PROP_LAST];
+
+typedef struct _MetaGpuPrivate
+{
+  MetaMonitorManager *monitor_manager;
+
+  GList *outputs;
+  GList *crtcs;
+  GList *modes;
+} MetaGpuPrivate;
+
+G_DEFINE_TYPE_WITH_PRIVATE (MetaGpu, meta_gpu, G_TYPE_OBJECT)
+
+gboolean
+meta_gpu_has_hotplug_mode_update (MetaGpu *gpu)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+  GList *l;
+
+  for (l = priv->outputs; l; l = l->next)
+    {
+      MetaOutput *output = l->data;
+
+      if (output->hotplug_mode_update)
+        return TRUE;
+    }
+
+  return FALSE;
+}
+
+gboolean
+meta_gpu_read_current (MetaGpu *gpu,
+                       GError **error)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+  gboolean ret;
+  GList *old_outputs;
+  GList *old_crtcs;
+  GList *old_modes;
+
+  /* TODO: Get rid of this when objects incref:s what they need instead */
+  old_outputs = priv->outputs;
+  old_crtcs = priv->crtcs;
+  old_modes = priv->modes;
+
+  ret = META_GPU_GET_CLASS (gpu)->read_current (gpu, error);
+
+  g_list_free_full (old_outputs, g_object_unref);
+  g_list_free_full (old_modes, g_object_unref);
+  g_list_free_full (old_crtcs, g_object_unref);
+
+  return ret;
+}
+
+MetaMonitorManager *
+meta_gpu_get_monitor_manager (MetaGpu *gpu)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  return priv->monitor_manager;
+}
+
+GList *
+meta_gpu_get_outputs (MetaGpu *gpu)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  return priv->outputs;
+}
+
+GList *
+meta_gpu_get_crtcs (MetaGpu *gpu)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  return priv->crtcs;
+}
+
+GList *
+meta_gpu_get_modes (MetaGpu *gpu)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  return priv->modes;
+}
+
+void
+meta_gpu_set_outputs (MetaGpu *gpu,
+                      GList   *outputs)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  priv->outputs = outputs;
+}
+
+void
+meta_gpu_set_crtcs (MetaGpu *gpu,
+                    GList   *crtcs)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  priv->crtcs = crtcs;
+}
+
+void
+meta_gpu_set_modes (MetaGpu *gpu,
+                    GList   *modes)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  priv->modes = modes;
+}
+
+static void
+meta_gpu_set_property (GObject      *object,
+                       guint         prop_id,
+                       const GValue *value,
+                       GParamSpec   *pspec)
+{
+  MetaGpu *gpu = META_GPU (object);
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  switch (prop_id)
+    {
+    case PROP_MONITOR_MANAGER:
+      priv->monitor_manager = g_value_get_object (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
+
+static void
+meta_gpu_get_property (GObject    *object,
+                       guint       prop_id,
+                       GValue     *value,
+                       GParamSpec *pspec)
+{
+  MetaGpu *gpu = META_GPU (object);
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  switch (prop_id)
+    {
+    case PROP_MONITOR_MANAGER:
+      g_value_set_object (value, priv->monitor_manager);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
+
+static void
+meta_gpu_finalize (GObject *object)
+{
+  MetaGpu *gpu = META_GPU (object);
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  g_list_free_full (priv->outputs, g_object_unref);
+  g_list_free_full (priv->modes, g_object_unref);
+  g_list_free_full (priv->crtcs, g_object_unref);
+
+  G_OBJECT_CLASS (meta_gpu_parent_class)->finalize (object);
+}
+
+static void
+meta_gpu_init (MetaGpu *gpu)
+{
+}
+
+static void
+meta_gpu_class_init (MetaGpuClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->set_property = meta_gpu_set_property;
+  object_class->get_property = meta_gpu_get_property;
+  object_class->finalize = meta_gpu_finalize;
+
+  obj_props[PROP_MONITOR_MANAGER] =
+    g_param_spec_object ("monitor-manager",
+                         "monitor-manager",
+                         "MetaMonitorManager",
+                         META_TYPE_MONITOR_MANAGER,
+                         G_PARAM_READWRITE |
+                         G_PARAM_CONSTRUCT_ONLY |
+                         G_PARAM_STATIC_STRINGS);
+  g_object_class_install_properties (object_class, PROP_LAST, obj_props);
+}

src/backends/meta-gpu.h
@@ -1,0 +1,66 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+
+/*
+ * Copyright (C) 2017 Red Hat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#ifndef META_GPU_H
+#define META_GPU_H
+
+#include <glib-object.h>
+
+#include "backends/meta-monitor-manager-private.h"
+
+#define META_TYPE_GPU (meta_gpu_get_type ())
+G_DECLARE_DERIVABLE_TYPE (MetaGpu, meta_gpu, META, GPU, GObject)
+
+struct _MetaGpuClass
+{
+  GObjectClass parent_class;
+
+  gboolean (* read_current) (MetaGpu *gpu,
+                             GError **error);
+};
+
+int meta_gpu_get_kms_fd (MetaGpu *gpu);
+
+const char * meta_gpu_get_kms_file_path (MetaGpu *gpu);
+
+gboolean meta_gpu_read_current (MetaGpu *gpu,
+                                GError **error);
+
+gboolean meta_gpu_has_hotplug_mode_update (MetaGpu *gpu);
+
+MetaMonitorManager * meta_gpu_get_monitor_manager (MetaGpu *gpu);
+
+GList * meta_gpu_get_outputs (MetaGpu *gpu);
+
+GList * meta_gpu_get_crtcs (MetaGpu *gpu);
+
+GList * meta_gpu_get_modes (MetaGpu *gpu);
+
+void meta_gpu_set_outputs (MetaGpu *gpu,
+                           GList   *outputs);
+
+void meta_gpu_set_crtcs (MetaGpu *gpu,
+                         GList   *crtcs);
+
+void meta_gpu_set_modes (MetaGpu *gpu,
+                         GList   *modes);
+
+#endif /* META_GPU_H */

src/tests/meta-monitor-manager-test.c
@@ -23,12 +23,15 @@
 
 #include "backends/meta-backend-private.h"
 #include "backends/meta-crtc.h"
+#include "backends/meta-gpu.h"
 #include "backends/meta-monitor-config-manager.h"
 #include "backends/meta-output.h"
 
 struct _MetaMonitorManagerTest
 {
   MetaMonitorManager parent;
+
+  MetaGpu *gpu;
 
   gboolean is_lid_closed;
   gboolean handles_transforms;
@@ -41,12 +44,25 @@ struct _MetaMonitorManagerTest
 G_DEFINE_TYPE (MetaMonitorManagerTest, meta_monitor_manager_test,
                META_TYPE_MONITOR_MANAGER)
 
+struct _MetaGpuTest
+{
+  MetaGpu parent;
+};
+
+G_DEFINE_TYPE (MetaGpuTest, meta_gpu_test, META_TYPE_GPU)
+
 static MetaMonitorTestSetup *_initial_test_setup = NULL;
 
 void
 meta_monitor_manager_test_init_test_setup (MetaMonitorTestSetup *test_setup)
 {
   _initial_test_setup = test_setup;
+}
+
+MetaGpu *
+meta_monitor_manager_test_get_gpu (MetaMonitorManagerTest *manager_test)
+{
+  return manager_test->gpu;
 }
 
 void
@@ -91,20 +107,24 @@ static void
 meta_monitor_manager_test_read_current (MetaMonitorManager *manager)
 {
   MetaMonitorManagerTest *manager_test = META_MONITOR_MANAGER_TEST (manager);
+  MetaGpu *gpu = manager_test->gpu;
   GList *l;
 
   g_assert (manager_test->test_setup);
 
   for (l = manager_test->test_setup->outputs; l; l = l->next)
-    META_OUTPUT (l->data)->monitor_manager = manager;
+    META_OUTPUT (l->data)->gpu = gpu;
   for (l = manager_test->test_setup->crtcs; l; l = l->next)
-    META_CRTC (l->data)->monitor_manager = manager;
+    META_CRTC (l->data)->gpu = gpu;
 
-  manager->modes = manager_test->test_setup->modes;
+  meta_gpu_set_modes (manager_test->gpu,
+                      manager_test->test_setup->modes);
 
-  manager->crtcs = manager_test->test_setup->crtcs;
+  meta_gpu_set_crtcs (manager_test->gpu,
+                      manager_test->test_setup->crtcs);
 
-  manager->outputs = manager_test->test_setup->outputs;
+  meta_gpu_set_outputs (manager_test->gpu,
+                        manager_test->test_setup->outputs);
 }
 
 static gboolean
@@ -142,6 +162,7 @@ apply_crtc_assignments (MetaMonitorManager *manager,
                         MetaOutputInfo    **outputs,
                         unsigned int        n_outputs)
 {
+  MetaMonitorManagerTest *manager_test = META_MONITOR_MANAGER_TEST (manager);
   GList *l;
   unsigned int i;
 
@@ -207,7 +228,7 @@ apply_crtc_assignments (MetaMonitorManager *manager,
     }
 
   /* Disable CRTCs not mentioned in the list */
-  for (l = manager->crtcs; l; l = l->next)
+  for (l = meta_gpu_get_crtcs (manager_test->gpu); l; l = l->next)
     {
       MetaCrtc *crtc = l->data;
 
@@ -227,7 +248,7 @@ apply_crtc_assignments (MetaMonitorManager *manager,
     }
 
   /* Disable outputs not mentioned in the list */
-  for (l = manager->outputs; l; l = l->next)
+  for (l = meta_gpu_get_outputs (manager_test->gpu); l; l = l->next)
     {
       MetaOutput *output = l->data;
 
@@ -468,11 +489,18 @@ meta_monitor_manager_test_dispose (GObject *object)
 static void
 meta_monitor_manager_test_init (MetaMonitorManagerTest *manager_test)
 {
+  MetaMonitorManager *manager = META_MONITOR_MANAGER (manager_test);
+
   g_assert (_initial_test_setup);
 
   manager_test->handles_transforms = TRUE;
 
   manager_test->test_setup = _initial_test_setup;
+
+  manager_test->gpu = g_object_new (META_TYPE_GPU_TEST,
+                                    "monitor-manager", manager,
+                                    NULL);
+  meta_monitor_manager_add_gpu (manager, manager_test->gpu);
 }
 
 static void
@@ -483,7 +511,6 @@ meta_monitor_manager_test_class_init (MetaMonitorManagerTestClass *klass)
 
   object_class->dispose = meta_monitor_manager_test_dispose;
 
-  manager_class->read_current = meta_monitor_manager_test_read_current;
   manager_class->is_lid_closed = meta_monitor_manager_test_is_lid_closed;
   manager_class->ensure_initial_config = meta_monitor_manager_test_ensure_initial_config;
   manager_class->apply_monitors_config = meta_monitor_manager_test_apply_monitors_config;
@@ -495,4 +522,28 @@ meta_monitor_manager_test_class_init (MetaMonitorManagerTestClass *klass)
   manager_class->get_capabilities = meta_monitor_manager_test_get_capabilities;
   manager_class->get_max_screen_size = meta_monitor_manager_test_get_max_screen_size;
   manager_class->get_default_layout_mode = meta_monitor_manager_test_get_default_layout_mode;
+}
+
+static gboolean
+meta_gpu_test_read_current (MetaGpu *gpu,
+                            GError **error)
+{
+  MetaMonitorManager *manager = meta_gpu_get_monitor_manager (gpu);
+
+  meta_monitor_manager_test_read_current (manager);
+
+  return TRUE;
+}
+
+static void
+meta_gpu_test_init (MetaGpuTest *gpu_test)
+{
+}
+
+static void
+meta_gpu_test_class_init (MetaGpuTestClass *klass)
+{
+  MetaGpuClass *gpu_class = META_GPU_CLASS (klass);
+
+  gpu_class->read_current = meta_gpu_test_read_current;
 }

GNOME/gnome-shell

src/backends/meta-gpu.c
@@ -1,0 +1,224 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+
+/*
+ * Copyright (C) 2017 Red Hat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#include "config.h"
+
+#include "backends/meta-gpu.h"
+
+#include "backends/meta-output.h"
+
+enum
+{
+  PROP_0,
+
+  PROP_MONITOR_MANAGER,
+
+  PROP_LAST
+};
+
+static GParamSpec *obj_props[PROP_LAST];
+
+typedef struct _MetaGpuPrivate
+{
+  MetaMonitorManager *monitor_manager;
+
+  GList *outputs;
+  GList *crtcs;
+  GList *modes;
+} MetaGpuPrivate;
+
+G_DEFINE_TYPE_WITH_PRIVATE (MetaGpu, meta_gpu, G_TYPE_OBJECT)
+
+gboolean
+meta_gpu_has_hotplug_mode_update (MetaGpu *gpu)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+  GList *l;
+
+  for (l = priv->outputs; l; l = l->next)
+    {
+      MetaOutput *output = l->data;
+
+      if (output->hotplug_mode_update)
+        return TRUE;
+    }
+
+  return FALSE;
+}
+
+gboolean
+meta_gpu_read_current (MetaGpu  *gpu,
+                       GError  **error)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+  gboolean ret;
+  GList *old_outputs;
+  GList *old_crtcs;
+  GList *old_modes;
+
+  /* TODO: Get rid of this when objects incref:s what they need instead */
+  old_outputs = priv->outputs;
+  old_crtcs = priv->crtcs;
+  old_modes = priv->modes;
+
+  ret = META_GPU_GET_CLASS (gpu)->read_current (gpu, error);
+
+  g_list_free_full (old_outputs, g_object_unref);
+  g_list_free_full (old_modes, g_object_unref);
+  g_list_free_full (old_crtcs, g_object_unref);
+
+  return ret;
+}
+
+MetaMonitorManager *
+meta_gpu_get_monitor_manager (MetaGpu *gpu)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  return priv->monitor_manager;
+}
+
+GList *
+meta_gpu_get_outputs (MetaGpu *gpu)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  return priv->outputs;
+}
+
+GList *
+meta_gpu_get_crtcs (MetaGpu *gpu)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  return priv->crtcs;
+}
+
+GList *
+meta_gpu_get_modes (MetaGpu *gpu)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  return priv->modes;
+}
+
+void
+meta_gpu_take_outputs (MetaGpu *gpu,
+                       GList   *outputs)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  priv->outputs = outputs;
+}
+
+void
+meta_gpu_take_crtcs (MetaGpu *gpu,
+                    GList   *crtcs)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  priv->crtcs = crtcs;
+}
+
+void
+meta_gpu_take_modes (MetaGpu *gpu,
+                     GList   *modes)
+{
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  priv->modes = modes;
+}
+
+static void
+meta_gpu_set_property (GObject      *object,
+                       guint         prop_id,
+                       const GValue *value,
+                       GParamSpec   *pspec)
+{
+  MetaGpu *gpu = META_GPU (object);
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  switch (prop_id)
+    {
+    case PROP_MONITOR_MANAGER:
+      priv->monitor_manager = g_value_get_object (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
+
+static void
+meta_gpu_get_property (GObject    *object,
+                       guint       prop_id,
+                       GValue     *value,
+                       GParamSpec *pspec)
+{
+  MetaGpu *gpu = META_GPU (object);
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  switch (prop_id)
+    {
+    case PROP_MONITOR_MANAGER:
+      g_value_set_object (value, priv->monitor_manager);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
+
+static void
+meta_gpu_finalize (GObject *object)
+{
+  MetaGpu *gpu = META_GPU (object);
+  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
+
+  g_list_free_full (priv->outputs, g_object_unref);
+  g_list_free_full (priv->modes, g_object_unref);
+  g_list_free_full (priv->crtcs, g_object_unref);
+
+  G_OBJECT_CLASS (meta_gpu_parent_class)->finalize (object);
+}
+
+static void
+meta_gpu_init (MetaGpu *gpu)
+{
+}
+
+static void
+meta_gpu_class_init (MetaGpuClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->set_property = meta_gpu_set_property;
+  object_class->get_property = meta_gpu_get_property;
+  object_class->finalize = meta_gpu_finalize;
+
+  obj_props[PROP_MONITOR_MANAGER] =
+    g_param_spec_object ("monitor-manager",
+                         "monitor-manager",
+                         "MetaMonitorManager",
+                         META_TYPE_MONITOR_MANAGER,
+                         G_PARAM_READWRITE |
+                         G_PARAM_CONSTRUCT_ONLY |
+                         G_PARAM_STATIC_STRINGS);
+  g_object_class_install_properties (object_class, PROP_LAST, obj_props);
+}

src/backends/meta-gpu.h
@@ -1,0 +1,66 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+
+/*
+ * Copyright (C) 2017 Red Hat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#ifndef META_GPU_H
+#define META_GPU_H
+
+#include <glib-object.h>
+
+#include "backends/meta-monitor-manager-private.h"
+
+#define META_TYPE_GPU (meta_gpu_get_type ())
+G_DECLARE_DERIVABLE_TYPE (MetaGpu, meta_gpu, META, GPU, GObject)
+
+struct _MetaGpuClass
+{
+  GObjectClass parent_class;
+
+  gboolean (* read_current) (MetaGpu  *gpu,
+                             GError  **error);
+};
+
+int meta_gpu_get_kms_fd (MetaGpu *gpu);
+
+const char * meta_gpu_get_kms_file_path (MetaGpu *gpu);
+
+gboolean meta_gpu_read_current (MetaGpu  *gpu,
+                                GError  **error);
+
+gboolean meta_gpu_has_hotplug_mode_update (MetaGpu *gpu);
+
+MetaMonitorManager * meta_gpu_get_monitor_manager (MetaGpu *gpu);
+
+GList * meta_gpu_get_outputs (MetaGpu *gpu);
+
+GList * meta_gpu_get_crtcs (MetaGpu *gpu);
+
+GList * meta_gpu_get_modes (MetaGpu *gpu);
+
+void meta_gpu_take_outputs (MetaGpu *gpu,
+                            GList   *outputs);
+
+void meta_gpu_take_crtcs (MetaGpu *gpu,
+                          GList   *crtcs);
+
+void meta_gpu_take_modes (MetaGpu *gpu,
+                          GList   *modes);
+
+#endif /* META_GPU_H */

src/tests/meta-monitor-manager-test.c
@@ -23,12 +23,15 @@
 
 #include "backends/meta-backend-private.h"
 #include "backends/meta-crtc.h"
+#include "backends/meta-gpu.h"
 #include "backends/meta-monitor-config-manager.h"
 #include "backends/meta-output.h"
 
 struct _MetaMonitorManagerTest
 {
   MetaMonitorManager parent;
+
+  MetaGpu *gpu;
 
   gboolean is_lid_closed;
   gboolean handles_transforms;
@@ -41,12 +44,25 @@ struct _MetaMonitorManagerTest
 G_DEFINE_TYPE (MetaMonitorManagerTest, meta_monitor_manager_test,
                META_TYPE_MONITOR_MANAGER)
 
+struct _MetaGpuTest
+{
+  MetaGpu parent;
+};
+
+G_DEFINE_TYPE (MetaGpuTest, meta_gpu_test, META_TYPE_GPU)
+
 static MetaMonitorTestSetup *_initial_test_setup = NULL;
 
 void
 meta_monitor_manager_test_init_test_setup (MetaMonitorTestSetup *test_setup)
 {
   _initial_test_setup = test_setup;
+}
+
+MetaGpu *
+meta_monitor_manager_test_get_gpu (MetaMonitorManagerTest *manager_test)
+{
+  return manager_test->gpu;
 }
 
 void
@@ -91,20 +107,24 @@ static void
 meta_monitor_manager_test_read_current (MetaMonitorManager *manager)
 {
   MetaMonitorManagerTest *manager_test = META_MONITOR_MANAGER_TEST (manager);
+  MetaGpu *gpu = manager_test->gpu;
   GList *l;
 
   g_assert (manager_test->test_setup);
 
   for (l = manager_test->test_setup->outputs; l; l = l->next)
-    META_OUTPUT (l->data)->monitor_manager = manager;
+    META_OUTPUT (l->data)->gpu = gpu;
   for (l = manager_test->test_setup->crtcs; l; l = l->next)
-    META_CRTC (l->data)->monitor_manager = manager;
+    META_CRTC (l->data)->gpu = gpu;
 
-  manager->modes = manager_test->test_setup->modes;
+  meta_gpu_take_modes (manager_test->gpu,
+                       manager_test->test_setup->modes);
 
-  manager->crtcs = manager_test->test_setup->crtcs;
+  meta_gpu_take_crtcs (manager_test->gpu,
+                       manager_test->test_setup->crtcs);
 
-  manager->outputs = manager_test->test_setup->outputs;
+  meta_gpu_take_outputs (manager_test->gpu,
+                         manager_test->test_setup->outputs);
 }
 
 static gboolean
@@ -139,6 +159,7 @@ apply_crtc_assignments (MetaMonitorManager *manager,
                         MetaOutputInfo    **outputs,
                         unsigned int        n_outputs)
 {
+  MetaMonitorManagerTest *manager_test = META_MONITOR_MANAGER_TEST (manager);
   GList *l;
   unsigned int i;
 
@@ -204,7 +225,7 @@ apply_crtc_assignments (MetaMonitorManager *manager,
     }
 
   /* Disable CRTCs not mentioned in the list */
-  for (l = manager->crtcs; l; l = l->next)
+  for (l = meta_gpu_get_crtcs (manager_test->gpu); l; l = l->next)
     {
       MetaCrtc *crtc = l->data;
 
@@ -224,7 +245,7 @@ apply_crtc_assignments (MetaMonitorManager *manager,
     }
 
   /* Disable outputs not mentioned in the list */
-  for (l = manager->outputs; l; l = l->next)
+  for (l = meta_gpu_get_outputs (manager_test->gpu); l; l = l->next)
     {
       MetaOutput *output = l->data;
 
@@ -453,11 +474,18 @@ meta_monitor_manager_test_dispose (GObject *object)
 static void
 meta_monitor_manager_test_init (MetaMonitorManagerTest *manager_test)
 {
+  MetaMonitorManager *manager = META_MONITOR_MANAGER (manager_test);
+
   g_assert (_initial_test_setup);
 
   manager_test->handles_transforms = TRUE;
 
   manager_test->test_setup = _initial_test_setup;
+
+  manager_test->gpu = g_object_new (META_TYPE_GPU_TEST,
+                                    "monitor-manager", manager,
+                                    NULL);
+  meta_monitor_manager_add_gpu (manager, manager_test->gpu);
 }
 
 static void
@@ -468,7 +496,6 @@ meta_monitor_manager_test_class_init (MetaMonitorManagerTestClass *klass)
 
   object_class->dispose = meta_monitor_manager_test_dispose;
 
-  manager_class->read_current = meta_monitor_manager_test_read_current;
   manager_class->is_lid_closed = meta_monitor_manager_test_is_lid_closed;
   manager_class->ensure_initial_config = meta_monitor_manager_test_ensure_initial_config;
   manager_class->apply_monitors_config = meta_monitor_manager_test_apply_monitors_config;
@@ -480,4 +507,28 @@ meta_monitor_manager_test_class_init (MetaMonitorManagerTestClass *klass)
   manager_class->get_capabilities = meta_monitor_manager_test_get_capabilities;
   manager_class->get_max_screen_size = meta_monitor_manager_test_get_max_screen_size;
   manager_class->get_default_layout_mode = meta_monitor_manager_test_get_default_layout_mode;
+}
+
+static gboolean
+meta_gpu_test_read_current (MetaGpu  *gpu,
+                            GError  **error)
+{
+  MetaMonitorManager *manager = meta_gpu_get_monitor_manager (gpu);
+
+  meta_monitor_manager_test_read_current (manager);
+
+  return TRUE;
+}
+
+static void
+meta_gpu_test_init (MetaGpuTest *gpu_test)
+{
+}
+
+static void
+meta_gpu_test_class_init (MetaGpuTestClass *klass)
+{
+  MetaGpuClass *gpu_class = META_GPU_CLASS (klass);
+
+  gpu_class->read_current = meta_gpu_test_read_current;
 }