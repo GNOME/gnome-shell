aa0858529edd94f4bbef8d018447ea387b71c1cd
Florian Müllner <fmuellner@gnome.org>
2017-10-20 15:29:07 +0200

    network: Spawn wifi panel for further WPA enterprise configuration
    Settings recently split Wi-Fi configuration from the Network panel,
    so launch that instead.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=789231

--- js/ui/status/network.js
+++ js/ui/status/network.js
@@ -951,7 +951,7 @@ var NMWirelessDialog = new Lang.Class({
                 || (accessPoints[0]._secType == NMAccessPointSecurity.WPA_ENT)) {
                 // 802.1x-enabled APs require further configuration, so they're
                 // handled in gnome-control-center
-                Util.spawn(['gnome-control-center', 'network', 'connect-8021x-wifi',
+                Util.spawn(['gnome-control-center', 'wifi', 'connect-8021x-wifi',
                             this._device.get_path(), accessPoints[0].dbus_path]);
             } else {
                 let connection = new NetworkManager.Connection();
