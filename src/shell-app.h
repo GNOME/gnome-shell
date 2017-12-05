29338a8f41c19a82fd96f3abfdedd03509fd4e41
Mario Sanchez Prada <mario@endlessm.com>
2017-12-05 16:19:46 +0000

    shell-app: Add new convenience function to get the app's icon, if any
    This will be used by the next commit.
    
    https://phabricator.endlessm.com/T18012

--- src/shell-app.h
+++ src/shell-app.h
@@ -25,6 +25,8 @@ GDesktopAppInfo *shell_app_get_app_info (ShellApp *app);
 ClutterActor *shell_app_create_icon_texture (ShellApp *app, int size);
 const char *shell_app_get_name (ShellApp *app);
 const char *shell_app_get_description (ShellApp *app);
+GIcon *shell_app_get_icon (ShellApp *app);
+
 gboolean shell_app_is_window_backed (ShellApp *app);
 
 void shell_app_activate_window (ShellApp *app, MetaWindow *window, guint32 timestamp);
