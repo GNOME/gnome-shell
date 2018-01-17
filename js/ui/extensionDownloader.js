fcb13abfd9c9393242f07c3031e8822484c2e4b3
Didier Roche <didrocks@ubuntu.com>
2018-01-17 13:43:11 +0100

    ExtensionSystem: enable removing session mode extension
    Add a 'disabled-extensions' to list session mode extension that shouldn't
    be loaded. This key is taking precedence of 'enabled-extensions'.
    Ensure depending in the case (user/system manually added extensions or
    extension enabled by a mode) that we update the correct key.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=789852

--- js/ui/extensionDownloader.js
+++ js/ui/extensionDownloader.js
@@ -239,16 +239,12 @@ var InstallExtensionDialog = new Lang.Class({
         }
 
         function callback() {
-            // Add extension to 'enabled-extensions' for the user, always...
-            let enabledExtensions = global.settings.get_strv(ExtensionSystem.ENABLED_EXTENSIONS_KEY);
-            if (enabledExtensions.indexOf(uuid) == -1) {
-                enabledExtensions.push(uuid);
-                global.settings.set_strv(ExtensionSystem.ENABLED_EXTENSIONS_KEY, enabledExtensions);
-            }
-
             try {
                 let extension = ExtensionUtils.createExtensionObject(uuid, dir, ExtensionUtils.ExtensionType.PER_USER);
                 ExtensionSystem.loadExtension(extension);
+                if (!ExtensionSystem.writeExtensionEnablementState(uuid, true)) {
+                    throw new Error("Can't add " + uuid + " to enabled gsettings key");
+                }
             } catch(e) {
                 uninstallExtension(uuid);
                 errback('LoadExtensionError', e);
