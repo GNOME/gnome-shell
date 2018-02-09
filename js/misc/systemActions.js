8bdcdeda47571fa4924360c32c79f7482a68cbd8
Mario Sanchez Prada <mario@endlessm.com>
2018-02-09 18:02:37 +0000

    systemActions: Don't hide the overview when logging out
    This is the current usptream behaviour, but we don't want to do that
    on Endless (where the overview is visible most of the time) since it
    would mean showing the desaturated clone of the grid for no reason.

--- js/misc/systemActions.js
+++ js/misc/systemActions.js
@@ -421,7 +421,6 @@ const SystemActions = GObject.registerClass({
         if (!this._actions.get(LOGOUT_ACTION_ID).available)
             throw new Error('The logout action is not available!');
 
-        Main.overview.hide();
         this._session.LogoutRemote(0);
     }
 
