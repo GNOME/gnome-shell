f1b9fae79f53bde49cd0d1851dafdf44548972de
Mario Sanchez Prada <mario@endlessm.com>
2018-02-14 11:19:51 +0000

    hotCorner: Add Endless' Hot Corner to the right side of the bottom panel
    Clicking the hot corner will toggle the windows picker, as well
    as moving the mouse against it if enabled.

---
+++ js/ui/hotCorner.js
@@ -0,0 +1,60 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
+
+const { Clutter, Gdk, Gio, GObject, St } = imports.gi;
+
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+
+var HOT_CORNER_ENABLED_KEY = 'hot-corner-enabled';
+
+var HotCorner = GObject.registerClass(
+class HotCorner extends PanelMenu.SingleIconButton {
+    _init() {
+        super._init(_("Hot Corner"), Clutter.ActorAlign.END, Clutter.ActorAlign.END);
+        this.add_style_class_name('hot-corner');
+
+        let iconFile;
+        if (this.get_text_direction() == Clutter.TextDirection.RTL)
+            iconFile = Gio.File.new_for_uri('resource:///org/gnome/shell/theme/hot-corner-rtl-symbolic.svg');
+        else
+            iconFile = Gio.File.new_for_uri('resource:///org/gnome/shell/theme/hot-corner-symbolic.svg');
+        this.setIcon(new Gio.FileIcon({ file: iconFile }));
+
+        this.container.set_fill(false, false);
+        this.container.set_alignment(St.Align.END, St.Align.END);
+
+        this._enableMenuItem = this.menu.addAction(_("Enable Hot Corner"), () => {
+            global.settings.set_boolean(HOT_CORNER_ENABLED_KEY, true);
+        });
+
+        this._disableMenuItem = this.menu.addAction(_("Disable Hot Corner"), () => {
+            global.settings.set_boolean(HOT_CORNER_ENABLED_KEY, false);
+        });
+
+        if (global.settings.get_boolean(HOT_CORNER_ENABLED_KEY))
+            this._enableMenuItem.actor.visible = false;
+        else
+            this._disableMenuItem.actor.visible = false;
+
+        this.menu.connect('menu-closed', () => {
+            let isEnabled = global.settings.get_boolean(HOT_CORNER_ENABLED_KEY);
+            this._enableMenuItem.actor.visible = !isEnabled;
+            this._disableMenuItem.actor.visible = isEnabled;
+        });
+    }
+
+    // overrides default implementation from PanelMenu.Button
+    _onEvent(actor, event) {
+        if (this.menu &&
+            (event.type() == Clutter.EventType.TOUCH_BEGIN ||
+             event.type() == Clutter.EventType.BUTTON_PRESS)) {
+            let button = event.get_button();
+            if (button == Gdk.BUTTON_PRIMARY && Main.overview.shouldToggleByCornerOrButton())
+                Main.overview.toggle();
+            else if (button == Gdk.BUTTON_SECONDARY)
+                this.menu.toggle();
+        }
+
+        return Clutter.EVENT_PROPAGATE;
+    }
+});
