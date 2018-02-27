GNOME/gnome-shell

commit 8d4855f1008b9d0d9e579c0ac81fd2ce076c3faa

volume: Add smooth scrolling to adjust output volume
Allow users to smoothly scroll on the volume indicator icon
to adjust the volume. Do this by simply passing the scroll
event to the slider inside the menu.

https://bugzilla.gnome.org/show_bug.cgi?id=687573

js/ui/status/volume.js
@@ -69,26 +69,8 @@ const VolumeMenu = new Lang.Class({
         this._onControlStateChanged();
     },
 
-    scroll: function(direction) {
-        let currentVolume = this._output.volume;
-
-        if (direction == Clutter.ScrollDirection.DOWN) {
-            let prev_muted = this._output.is_muted;
-            this._output.volume = Math.max(0, currentVolume - this._volumeMax * VOLUME_ADJUSTMENT_STEP);
-            if (this._output.volume < 1) {
-                this._output.volume = 0;
-                if (!prev_muted)
-                    this._output.change_is_muted(true);
-            }
-            this._output.push_volume();
-        }
-        else if (direction == Clutter.ScrollDirection.UP) {
-            this._output.volume = Math.min(this._volumeMax, currentVolume + this._volumeMax * VOLUME_ADJUSTMENT_STEP);
-            this._output.change_is_muted(false);
-            this._output.push_volume();
-        }
-
-        this._notifyVolumeChange();
+    scroll: function(event) {
+        this._outputSlider.scroll(event);
     },
 
     _onControlStateChanged: function() {
@@ -279,6 +261,6 @@ const Indicator = new Lang.Class({
     },
 
     _onScrollEvent: function(actor, event) {
-        this._volumeMenu.scroll(event.get_scroll_direction());
+        this._volumeMenu.scroll(event);
     }
 });

js/ui/popupMenu.js
@@ -636,7 +636,7 @@ const PopupSliderMenuItem = new Lang.Class({
         return true;
     },
 
-    _onScrollEvent: function (actor, event) {
+    scroll: function(event) {
         let direction = event.get_scroll_direction();
         let delta;
 
@@ -655,8 +655,13 @@ const PopupSliderMenuItem = new Lang.Class({
         }
 
         this._value = Math.min(Math.max(0, this._value + delta), 1);
+
         this._slider.queue_repaint();
         this.emit('value-changed', this._value);
+    },
+
+    _onScrollEvent: function(actor, event) {
+        this.scroll(event);
     },
 
     _motionEvent: function(actor, event) {

commit 40273e47a61d405f19d7053595c7da019a51c6b7

volume: Add back sound feedback on scroll
Commit 8d4855f1008 accidentally removed the volume change feedback
for scroll events. Add it back to be consistent again with moving
the slider via arrow keys, slider drags/clicks and gsd's media keys
handling.

https://gitlab.gnome.org/GNOME/gnome-shell/issues/53

js/ui/status/volume.js
@@ -4,6 +4,7 @@ const Clutter = imports.gi.Clutter;
 const Lang = imports.lang;
 const Gio = imports.gi.Gio;
 const Gvc = imports.gi.Gvc;
+const Mainloop = imports.mainloop;
 const St = imports.gi.St;
 const Signals = imports.signals;
 
@@ -35,9 +36,16 @@ var StreamSlider = new Lang.Class({
 
         this.item = new PopupMenu.PopupBaseMenuItem({ activate: false });
 
+        this._inDrag = false;
+        this._notifyVolumeChangeId = 0;
+
         this._slider = new Slider.Slider(0);
+        this._slider.connect('drag-begin', () => { this._inDrag = true; });
         this._slider.connect('value-changed', this._sliderChanged.bind(this));
-        this._slider.connect('drag-end', this._notifyVolumeChange.bind(this));
+        this._slider.connect('drag-end', () => {
+            this._inDrag = false;
+            this._notifyVolumeChange();
+        });
 
         this._icon = new St.Icon({ style_class: 'popup-menu-icon' });
         this.item.actor.add(this._icon);
@@ -119,6 +127,13 @@ var StreamSlider = new Lang.Class({
                 this._stream.change_is_muted(false);
         }
         this._stream.push_volume();
+
+        if (!this._notifyVolumeChangeId && !this._inDrag)
+            this._notifyVolumeChangeId = Mainloop.timeout_add(30, () => {
+                this._notifyVolumeChange();
+                this._notifyVolumeChangeId = 0;
+                return GLib.SOURCE_REMOVE;
+            });
     },
 
     _notifyVolumeChange() {