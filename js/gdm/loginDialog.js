5b42b895f9c7e8a25813f175fd9702d59d47b827
verdre <verdre@v0yd.nl>
2018-04-07 21:23:13 +0200

    loginDialog: Don't display timed login indicator if unused
    This fixes the slightly bigger padding underneath the login item compared to the padding above

--- js/gdm/loginDialog.js
+++ js/gdm/loginDialog.js
@@ -86,7 +86,8 @@ var UserListItem = new Lang.Class({
                                              GObject.BindingFlags.SYNC_CREATE);
 
         this._timedLoginIndicator = new St.Bin({ style_class: 'login-dialog-timed-login-indicator',
-                                                 scale_x: 0 });
+                                                 scale_x: 0,
+                                                 visible: false });
         layout.add(this._timedLoginIndicator);
 
         this.actor.connect('clicked', this._onClicked.bind(this));
@@ -126,6 +127,8 @@ var UserListItem = new Lang.Class({
 
         this.hideTimedLoginIndicator();
 
+        this._timedLoginIndicator.visible = true;
+
         let startTime = GLib.get_monotonic_time();
 
         this._timedLoginTimeoutId = GLib.timeout_add (GLib.PRIORITY_DEFAULT, 33,
@@ -152,6 +155,8 @@ var UserListItem = new Lang.Class({
             GLib.source_remove(this._timedLoginTimeoutId);
             this._timedLoginTimeoutId = 0;
         }
+
+        this._timedLoginIndicator.visible = false;
         this._timedLoginIndicator.scale_x = 0.;
     }
 });
