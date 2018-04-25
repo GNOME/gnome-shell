1de8814acec4c3f793eef430ce2f3bff337e9a3a
verdre <gitlab@v0yd.nl>
2018-04-26 01:05:25 +0200

    altTab: Don't remove this._thumbnails after destroying thumbnails actor
    Removing this._thumbnails after destroying the thumbnails actor will
    also try to remove this._thumbnails after fading out the actor.
    If this._thumbnails is newly created while an animation to destroy an
    old actor is still running, it will cause the reference to the new
    ThumbnailSwitcher to be removed after the (old) animation is done (while
    the new actor will remain visible).
    
    This caused a bug where the actors of already destroyed thumbnail
    switchers would still be shown when creating and destroying thumbnail
    switchers faster than THUMBNAIL_FADE_TIME.

--- js/ui/altTab.js
+++ js/ui/altTab.js
@@ -385,6 +385,8 @@ var AppSwitcherPopup = new Lang.Class({
 
     _destroyThumbnails() {
         let thumbnailsActor = this._thumbnails.actor;
+        this._thumbnails = null;
+
         Tweener.addTween(thumbnailsActor,
                          { opacity: 0,
                            time: THUMBNAIL_FADE_TIME,
@@ -394,7 +396,7 @@ var AppSwitcherPopup = new Lang.Class({
                                this.thumbnailsVisible = false;
                            }
                          });
-        this._thumbnails = null;
+
         if  (this._switcherList._items[this._selectedIndex])
             this._switcherList._items[this._selectedIndex].remove_accessible_state (Atk.StateType.EXPANDED);
     },
@@ -405,7 +407,6 @@ var AppSwitcherPopup = new Lang.Class({
         this._thumbnails.connect('item-entered', this._windowEntered.bind(this));
         this._thumbnails.connect('item-removed', this._windowRemoved.bind(this));
         this._thumbnails.actor.connect('destroy', () => {
-            this._thumbnails = null;
             this._thumbnailsFocused = false;
         });
 
