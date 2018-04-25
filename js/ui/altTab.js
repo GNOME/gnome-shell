4492394543c5073e8f670f94b00e1d3eafa89275
Jonas Dreßler <verdre@v0yd.nl>
2018-04-25 22:48:27 +0200

    altTab: Also return Clutter.EVENT_PROPAGATE in override of _onItemEnter
    Since _onItemEnter() is used as a signal handler for a motion event,
    Clutter expects a return value, so make sure we also return
    Clutter.EVENT_PROPAGATE in the override function.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/169

--- js/ui/altTab.js
+++ js/ui/altTab.js
@@ -801,6 +801,8 @@ class AppSwitcher extends SwitcherPopup.SwitcherList {
         } else {
             this._itemEntered(index);
         }
+
+        return Clutter.EVENT_PROPAGATE;
     }
 
     _enterItem(index) {
