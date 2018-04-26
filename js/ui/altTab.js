264db7ce376e0ac00f485cc5f698dbc1f0cd6c4e
verdre <gitlab@v0yd.nl>
2018-04-26 11:19:40 +0200

    switcherPopup: Implement functions to add and remove accessible states
    Set the accessible states of the switcherList items by calling a
    function instead of manipulating class-internal variables from the
    outside.

--- js/ui/altTab.js
+++ js/ui/altTab.js
@@ -394,8 +394,7 @@ var AppSwitcherPopup = new Lang.Class({
                            onComplete: () => thumbnailsActor.destroy()
                          });
 
-        if  (this._switcherList._items[this._selectedIndex])
-            this._switcherList._items[this._selectedIndex].remove_accessible_state (Atk.StateType.EXPANDED);
+        this._switcherList.removeAccessibleState(this._selectedIndex, Atk.StateType.EXPANDED);
     },
 
     _createThumbnails() {
@@ -418,7 +417,7 @@ var AppSwitcherPopup = new Lang.Class({
                          });
 
         this.thumbnailsVisible = true;
-        this._switcherList._items[this._selectedIndex].add_accessible_state (Atk.StateType.EXPANDED);
+        this._switcherList.addAccessibleState(this._selectedIndex, Atk.StateType.EXPANDED);
     }
 });
 
