9dd15a7e25612926b52fec779befd87b17a154ea
Roddy Shuler <roddy@endlessm.com>
2018-04-27 01:22:09 -0700

    bluetooth: Use more descriptive on/off label
    Many users don't know the bluetooth symbol, so it is helpful
    if the menu label includes the word "Bluetooth".
    
    https://phabricator.endlessm.com/T17492

--- js/ui/status/bluetooth.js
+++ js/ui/status/bluetooth.js
@@ -131,9 +131,9 @@ class Indicator extends PanelMenu.SystemIndicator {
             /* Translators: this is the number of connected bluetooth devices */
             this._item.label.text = ngettext("%d Connected", "%d Connected", nConnectedDevices).format(nConnectedDevices);
         else if (isBluetoothOff)
-            this._item.label.text = _("Off");
+            this._item.label.text = _("Bluetooth Off");
         else
-            this._item.label.text = _("On");
+            this._item.label.text = _("Bluetooth On");
 
         this._toggleItem.label.text = isBluetoothOff ? _("Turn On") : _("Turn Off");
     }
