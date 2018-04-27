3160982d2ca5869a1ced0ddad5eef0c72465e0bc
Mario Sanchez Prada <mario@endlessm.com>
2018-04-27 15:43:27 +0100

    messageTray: Hide the overview when activating notifications
    In Endless, we normally enter the Overview when the last window
    gets hidden by minimizing it, which would cause issues if we try
    to indirectly interact with such applications while in that state
    (e.g. via a notification) since, even if minimized, such application
    is not expected to be invisible if the Overview is visible, since
    in upstream GNOME Shell we'd be in the Window Picker mode.
    
    Therefore, we need to make sure to hide the Overview whenever we
    activate a notification with an action associated to it, similar
    to what we do already in FdoNotificationDaemonSource::OpenApp(),
    but for actions activated remotely in the source app via D-Bus.
    
    https://phabricator.endlessm.com/T22100

--- js/ui/notificationDaemon.js
+++ js/ui/notificationDaemon.js
@@ -408,6 +408,7 @@ var FdoNotificationDaemon = class FdoNotificationDaemon {
     }
 
     _emitActionInvoked(id, action) {
+        Main.overview.hide();
         this._dbusImpl.emit_signal('ActionInvoked',
                                    GLib.Variant.new('(us)', [id, action]));
     }
