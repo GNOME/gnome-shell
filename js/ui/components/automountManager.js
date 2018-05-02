55ef2ecececb18f0d03cce6a29dbd07b9dd25752
segfault <segfault@riseup.net>
2018-05-02 19:11:08 +0200

    Support TCRYPT in ShellMountPasswordDialog

--- js/ui/components/automountManager.js
+++ js/ui/components/automountManager.js
@@ -198,8 +198,18 @@ var AutomountManager = new Lang.Class({
             // FIXME: we will always get G_IO_ERROR_FAILED from the gvfs udisks
             // backend in this case, see 
             // https://bugs.freedesktop.org/show_bug.cgi?id=51271
-            if (e.message.indexOf('No key available with this passphrase') != -1) {
+            if (e.message.indexOf('No key available with this passphrase') != -1 ||
+                e.message.indexOf('Failed to load device\'s parameters: Operation not permitted') != -1) {
                 this._reaskPassword(volume);
+            } else if (e.message.indexOf('Compiled against a version of libcryptsetup that does not support the VeraCrypt PIM setting') != -1) {
+                let existingDialog = volume._operation ? volume._operation.borrowDialog() : null;
+                if (existingDialog) {
+                    existingDialog._pimEntry.text = "";
+                    existingDialog._pimEntry.can_focus = false;
+                    existingDialog._pimEntry.reactive = false;
+                    existingDialog._pimEntry.clutter_text.editable = false;
+                }
+                this._reaskPassword(volume, { errorMessage: _("The installed udisks version does not support the PIM setting") });
             } else {
                 if (!e.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.FAILED_HANDLED))
                     log('Unable to mount volume ' + volume.get_name() + ': ' + e.toString());
@@ -214,11 +224,13 @@ var AutomountManager = new Lang.Class({
             this._volumeQueue.filter(element => (element != volume));
     },
 
-    _reaskPassword(volume) {
+    _reaskPassword(volume, params) {
+        params = Params.parse(params, { errorMessage: null });
         let existingDialog = volume._operation ? volume._operation.borrowDialog() : null;
         let operation = 
             new ShellMountOperation.ShellMountOperation(volume,
-                                                        { existingDialog: existingDialog });
+                                                        { existingDialog: existingDialog,
+                                                          errorMessage: errorMessage });
         this._mountVolume(volume, operation);
     },
 
