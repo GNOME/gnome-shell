3587dc5854af3955a2e44c9ba49f04371a7fabc7
verdre <gitlab@v0yd.nl>
2018-05-04 23:46:46 +0200

    altTab: Disconnect click- and motion-events with removal of thumbnails
    Since we are disconnecting the event handlers before animating the
    fade-out of the thumbnails now, also disconnect the click and
    motion-event handlers before the ThumbnailSwitcher is destroyed.
    
    This fixes a bug where thumbnails of a newly selected item will be
    selected if the mouse enters an old thumbnail while the thumnail
    switcher is being faded out.

--- js/ui/altTab.js
+++ js/ui/altTab.js
@@ -964,6 +964,11 @@ var ThumbnailSwitcher = new Lang.Class({
             if (clone.source)
                 clone.source.disconnect(clone._destroyId);
         });
+
+        this._items.forEach(item => {
+            item.disconnect(item._clickEventId);
+            item.disconnect(item._motionEventId);
+        });
     }
 });
 
