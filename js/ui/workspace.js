128a364435de6763e998eaae827350618ea040b3
verdre <gitlab@v0yd.nl>
2018-06-19 12:24:13 +0200

    workspace: Use globals for delays and animation times

--- js/ui/workspace.js
+++ js/ui/workspace.js
@@ -22,7 +22,10 @@ var WINDOW_DND_SIZE = 256;
 
 var WINDOW_CLONE_MAXIMUM_SCALE = 1.0;
 
-var CLOSE_BUTTON_FADE_TIME = 0.1;
+var WINDOW_OVERLAY_IDLE_HIDE_TIMEOUT = 750;
+var WINDOW_OVERLAY_FADE_TIME = 0.1;
+
+var WINDOW_REPOSITIONING_DELAY = 750;
 
 var DRAGGING_WINDOW_OPACITY = 100;
 
@@ -461,7 +464,7 @@ var WindowOverlay = new Lang.Class({
         let button = new St.Button({ style_class: 'window-close' });
         button._overlap = 0;
 
-        this._idleToggleCloseId = 0;
+        this._idleHideOverlayId = 0;
         button.connect('clicked', this._closeWindow.bind(this));
 
         windowClone.actor.connect('destroy', this._onDestroy.bind(this));
@@ -627,9 +630,9 @@ var WindowOverlay = new Lang.Class({
             this._workspace.disconnect(this._windowAddedId);
             this._windowAddedId = 0;
         }
-        if (this._idleToggleCloseId > 0) {
-            Mainloop.source_remove(this._idleToggleCloseId);
-            this._idleToggleCloseId = 0;
+        if (this._idleHideOverlayId > 0) {
+            Mainloop.source_remove(this._idleHideOverlayId);
+            this._idleHideOverlayId = 0;
         }
         this._windowClone.metaWindow.disconnect(this._updateCaptionId);
         this.title.destroy();
@@ -649,7 +652,7 @@ var WindowOverlay = new Lang.Class({
             a.opacity = 0;
             Tweener.addTween(a,
                              { opacity: 255,
-                               time: CLOSE_BUTTON_FADE_TIME,
+                               time: WINDOW_OVERLAY_FADE_TIME,
                                transition: 'easeOutQuad' });
         });
     },
@@ -659,7 +662,7 @@ var WindowOverlay = new Lang.Class({
             a.opacity = 255;
             Tweener.addTween(a,
                              { opacity: 0,
-                               time: CLOSE_BUTTON_FADE_TIME,
+                               time: WINDOW_OVERLAY_FADE_TIME,
                                transition: 'easeInQuad' });
         });
     },
@@ -677,14 +680,14 @@ var WindowOverlay = new Lang.Class({
     },
 
     _onHideChrome() {
-        if (this._idleToggleCloseId == 0) {
-            this._idleToggleCloseId = Mainloop.timeout_add(750, this._idleToggleCloseButton.bind(this));
-            GLib.Source.set_name_by_id(this._idleToggleCloseId, '[gnome-shell] this._idleToggleCloseButton');
+        if (this._idleHideOverlayId == 0) {
+            this._idleHideOverlayId = Mainloop.timeout_add(WINDOW_OVERLAY_IDLE_HIDE_TIMEOUT, this._idleToggleCloseButton.bind(this));
+            GLib.Source.set_name_by_id(this._idleHideOverlayId, '[gnome-shell] this._idleToggleCloseButton');
         }
     },
 
     _idleToggleCloseButton() {
-        this._idleToggleCloseId = 0;
+        this._idleHideOverlayId = 0;
 
         if (!this._windowClone.actor['has-pointer'] &&
             !this.closeButton['has-pointer'])
@@ -694,9 +697,9 @@ var WindowOverlay = new Lang.Class({
     },
 
     hideCloseButton() {
-        if (this._idleToggleCloseId > 0) {
-            Mainloop.source_remove(this._idleToggleCloseId);
-            this._idleToggleCloseId = 0;
+        if (this._idleHideOverlayId > 0) {
+            Mainloop.source_remove(this._idleHideOverlayId);
+            this._idleHideOverlayId = 0;
         }
         this.closeButton.hide();
         this.border.hide();
@@ -1482,7 +1485,7 @@ var Workspace = new Lang.Class({
         this._cursorY = y;
 
         this._currentLayout = null;
-        this._repositionWindowsId = Mainloop.timeout_add(750,
+        this._repositionWindowsId = Mainloop.timeout_add(WINDOW_REPOSITIONING_DELAY,
             this._delayedWindowRepositioning.bind(this));
         GLib.Source.set_name_by_id(this._repositionWindowsId, '[gnome-shell] this._delayedWindowRepositioning');
     },
