e48c0e8416fd546dc955b19870a626dce285244d
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2018-07-06 10:01:39 +0200

    thumbnailBox: Remove Shell.GenericContainer
    This was another straight port from Shell.GenericContainer.
    The important thing to notice is that the calculation is
    broken if the StThemeNode helpers (adjust_preferred_* and
    adjust_for_*) aren't used.
    
    The downside of this patch is that it removed the skip_paint
    from the thumbnails. Keeping it would add an unecessarily
    large amount of code.

--- js/ui/overviewControls.js
+++ js/ui/overviewControls.js
@@ -250,11 +250,11 @@ var ThumbnailsSlider = new Lang.Class({
         this.actor.request_mode = Clutter.RequestMode.WIDTH_FOR_HEIGHT;
         this.actor.reactive = true;
         this.actor.track_hover = true;
-        this.actor.add_actor(this._thumbnailsBox.actor);
+        this.actor.add_actor(this._thumbnailsBox);
 
         Main.layoutManager.connect('monitors-changed', this._updateSlide.bind(this));
         this.actor.connect('notify::hover', this._updateSlide.bind(this));
-        this._thumbnailsBox.actor.bind_property('visible', this.actor, 'visible', GObject.BindingFlags.SYNC_CREATE);
+        this._thumbnailsBox.bind_property('visible', this.actor, 'visible', GObject.BindingFlags.SYNC_CREATE);
     },
 
     _getAlwaysZoomOut() {
