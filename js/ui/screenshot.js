da537cda4345374a68570846a91d8d5f2c836d7e
Florian Müllner <fmuellner@gnome.org>
2018-07-19 15:54:33 +0200

    screenshot: Adopt GIO's async pattern
    A custom callback type is more convenient, but only as long as no
    other callback type is required. We are about to add functionality
    that does not return the filename to a screenshot saved on disk, so
    prepare for that by moving to GIO's generic async callback pattern.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/issues/286

--- js/ui/screenshot.js
+++ js/ui/screenshot.js
@@ -110,7 +110,7 @@ var ScreenshotService = new Lang.Class({
                y + height <= global.screen_height;
     },
 
-    _onScreenshotComplete(obj, result, area, filenameUsed, flash, invocation) {
+    _onScreenshotComplete(result, area, filenameUsed, flash, invocation) {
         if (result) {
             if (flash) {
                 let flashspot = new Flashspot(area);
@@ -157,9 +157,15 @@ var ScreenshotService = new Lang.Class({
         if (!screenshot)
             return;
         screenshot.screenshot_area (x, y, width, height, filename,
-            (obj, result, area, filenameUsed) => {
-                this._onScreenshotComplete(obj, result, area, filenameUsed,
-                                           flash, invocation);
+            (o, res) => {
+                try {
+                    let [result, area, filenameUsed] =
+                        screenshot.screenshot_area_finish(res);
+                    this._onScreenshotComplete(result, area, filenameUsed,
+                                               flash, invocation);
+                } catch (e) {
+                    invocation.return_gerror (e);
+                }
             });
     },
 
@@ -169,9 +175,15 @@ var ScreenshotService = new Lang.Class({
         if (!screenshot)
             return;
         screenshot.screenshot_window (include_frame, include_cursor, filename,
-            (obj, result, area, filenameUsed) => {
-                this._onScreenshotComplete(obj, result, area, filenameUsed,
-                                           flash, invocation);
+            (o, res) => {
+                try {
+                    let [result, area, filenameUsed] =
+                        screenshot.screenshot_window_finish(res);
+                    this._onScreenshotComplete(result, area, filenameUsed,
+                                               flash, invocation);
+                } catch (e) {
+                    invocation.return_gerror (e);
+                }
             });
     },
 
@@ -181,9 +193,15 @@ var ScreenshotService = new Lang.Class({
         if (!screenshot)
             return;
         screenshot.screenshot(include_cursor, filename,
-            (obj, result, area, filenameUsed) => {
-                this._onScreenshotComplete(obj, result, area, filenameUsed,
-                                           flash, invocation);
+            (o, res) => {
+                try {
+                    let [result, area, filenameUsed] =
+                        screenshot.screenshot_finish(res);
+                    this._onScreenshotComplete(result, area, filenameUsed,
+                                               flash, invocation);
+                } catch (e) {
+                    invocation.return_gerror (e);
+                }
             });
     },
 
