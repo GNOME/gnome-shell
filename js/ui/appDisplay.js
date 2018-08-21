bece4fcf27ea537175b39076ee9fc786d2f1dc4a
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2018-08-21 06:21:26 -0300

    iconGrid: Rename ::key-focus-in signal
    As part of our quest to obsolete Shell.GenericContainer, IconGrid will
    become a Clutter.Actor subclass. As the ::key-focus-in signal would
    clash with Clutter.Actor::key-focus-in, rename it to ::child-focused.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/153

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -129,8 +129,8 @@ var BaseAppView = new Lang.Class({
         else
             this._grid = new IconGrid.IconGrid(gridParams);
 
-        this._grid.connect('key-focus-in', (grid, actor) => {
-            this._keyFocusIn(actor);
+        this._grid.connect('child-focused', (grid, actor) => {
+            this._childFocused(actor);
         });
         // Standard hack for ClutterBinLayout
         this._grid.actor.x_expand = true;
@@ -139,7 +139,7 @@ var BaseAppView = new Lang.Class({
         this._allItems = [];
     },
 
-    _keyFocusIn(actor) {
+    _childFocused(actor) {
         // Nothing by default
     },
 
@@ -720,7 +720,7 @@ var AllView = new Lang.Class({
         });
     },
 
-    _keyFocusIn(icon) {
+    _childFocused(icon) {
         let itemPage = this._grid.getItemPage(icon);
         this.goToPage(itemPage);
     },
@@ -1157,7 +1157,7 @@ var FolderView = new Lang.Class({
         this.actor.add_action(action);
     },
 
-    _keyFocusIn(actor) {
+    _childFocused(actor) {
         Util.ensureActorVisibleInScrollView(this.actor, actor);
     },
 
