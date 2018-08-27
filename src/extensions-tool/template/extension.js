e63a4acf7c93d66f9e2c05b300a7a4768538802d
Florian Müllner <fmuellner@gnome.org>
2018-08-27 03:24:10 +0200

    extensions-tool: Implement create command
    This implements more functionality of the existing tool and, as
    'reload' is an unreliable feature that doesn't work more often
    than not, the last bit that we will replicate.
    
    The command follows the original for the most part, with the most
    important difference being the installed template, which doesn't
    provide any sample functionality and uses modern JS syntax.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/issues/1234

--- src/extensions-tool/template/extension.js
+++ src/extensions-tool/template/extension.js
@@ -1,10 +1,8 @@
-/* common.h
- *
- * Copyright 2018 Florian Müllner <fmuellner@gnome.org>
+/* extension.js
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -15,19 +13,22 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-#pragma once
+/* exported init */
 
-#include <gio/gio.h>
+class Extension {
+    constructor() {
+    }
 
-G_BEGIN_DECLS
+    enable() {
+    }
 
-void show_help (GOptionContext *context,
-                const char     *message);
+    disable() {
+    }
+}
 
-GDBusProxy *get_shell_proxy (GError **error);
-GSettings  *get_shell_settings (void);
-
-G_END_DECLS
+function init() {
+    return new Extension();
+}
