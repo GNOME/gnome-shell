5c7ecdd5fdb46d7d61c130482c55ccafbc13e27c
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-08-30 07:11:24 +0200

    search: Ignore search provider results metas if search is cancelled
    Call updateSearch callback with no results when the search provider has been
    cancelled.

--- js/ui/search.js
+++ js/ui/search.js
@@ -227,8 +227,8 @@ var SearchResultsBase = new Lang.Class({
 
             this.provider.getResultMetas(metasNeeded, metas => {
                 if (metas.length != metasNeeded.length) {
-                    log('Wrong number of result metas returned by search provider ' + this.provider.id +
-                        ': expected ' + metasNeeded.length + ' but got ' + metas.length);
+                    if (!this._cancellable.is_cancelled())
+                        log(`Search provider ${this.provider.id} returned results after the request was canceled`);
                     callback(false);
                     return;
                 }
