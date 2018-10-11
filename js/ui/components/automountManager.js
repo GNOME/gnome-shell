4705a91adc8e4e23b19a4fd0c14a38b21c7d6595
Sebastian Pinnau <spinnau@gmail.com>
2018-10-11 19:14:25 +0200

    automountManager: Add handling of udisk errors for no/wrong passwords
    If no password or a wrong password is entered after automounting an
    encrypted device, then the password should be reasked. However, this
    does not happen because the relevant udsik error messages for this
    cases are missing in the exception handler that calls _reaskPassword.
    
    Fix this issue by adding the relevant udisk error strings to the
    exception handling in the _onVolumeMounted method.

--- js/ui/components/automountManager.js
+++ js/ui/components/automountManager.js
@@ -199,7 +199,9 @@ var AutomountManager = new Lang.Class({
             // FIXME: we will always get G_IO_ERROR_FAILED from the gvfs udisks
             // backend in this case, see 
             // https://bugs.freedesktop.org/show_bug.cgi?id=51271
-            if (e.message.indexOf('No key available with this passphrase') != -1) {
+            if (e.message.indexOf('No key available with this passphrase') != -1 || // cryptsetup
+                e.message.indexOf('No key available to unlock device') != -1 ||     // Udisks (no password)
+                e.message.indexOf('Error unlocking') != -1 ) {                      // Udisks (wrong password)
                 this._reaskPassword(volume);
             } else {
                 if (!e.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.FAILED_HANDLED))
