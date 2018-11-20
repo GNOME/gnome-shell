3217c10ff272f44d50afc881ce2a2dbb911dc1e8
daniruiz <daniruizdealegria@gmail.com>
2018-11-20 17:55:31 +0100

    theme: Replace calendar arrow images with symbolic icons and CSS
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/301

--- js/ui/calendar.js
+++ js/ui/calendar.js
@@ -446,6 +446,7 @@ var Calendar = new Lang.Class({
         this._backButton = new St.Button({ style_class: 'calendar-change-month-back pager-button',
                                            accessible_name: _("Previous month"),
                                            can_focus: true });
+        this._backButton.add_actor(new St.Icon({ icon_name: 'pan-start-symbolic' }));
         this._topBox.add(this._backButton);
         this._backButton.connect('clicked', this._onPrevMonthButtonClicked.bind(this));
 
@@ -456,6 +457,7 @@ var Calendar = new Lang.Class({
         this._forwardButton = new St.Button({ style_class: 'calendar-change-month-forward pager-button',
                                               accessible_name: _("Next month"),
                                               can_focus: true });
+        this._forwardButton.add_actor(new St.Icon({ icon_name: 'pan-end-symbolic' }));
         this._topBox.add(this._forwardButton);
         this._forwardButton.connect('clicked', this._onNextMonthButtonClicked.bind(this));
 
