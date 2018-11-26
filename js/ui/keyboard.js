62afe8eff73ac2354d8fd20288b6b442815d89d1
Carlos Garnacho <carlosg@gnome.org>
2018-11-26 13:39:57 +0100

    keyboard: Drop GDK API usage
    Use the misnamed but equivalent Clutter function.

--- js/ui/keyboard.js
+++ js/ui/keyboard.js
@@ -2,7 +2,6 @@
 
 const Atspi = imports.gi.Atspi;
 const Clutter = imports.gi.Clutter;
-const Gdk = imports.gi.Gdk;
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
 const Lang = imports.lang;
@@ -294,7 +293,7 @@ var Key = new Lang.Class({
 
     _getKeyval(key) {
         let unicode = String.charCodeAt(key, 0);
-        return Gdk.unicode_to_keyval(unicode);
+        return Clutter.unicode_to_keysym(unicode);
     },
 
     _press(key) {
