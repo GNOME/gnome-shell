628d6f4518c8171c6a1cdc0e8435d224622f37f3
Olivier Fourdan <ofourdan@redhat.com>
2018-12-12 16:02:29 +0100

    introspect: Add GetWindows method
    The `GetWindows` method gives access to the list of windows for each
    application with some of their properties, so utilities such as dogtail
    can pick the window of their choice to interfere with using the provided
    window id.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/326

--- js/misc/introspect.js
+++ js/misc/introspect.js
@@ -114,6 +114,17 @@ var IntrospectService = new Lang.Class({
         this._activeApplicationDirty = false;
     },
 
+    _isEligibleWindow(window) {
+        if (window.is_override_redirect())
+            return false;
+
+        let type = window.get_window_type();
+        return (type == Meta.WindowType.NORMAL ||
+                type == Meta.WindowType.DIALOG ||
+                type == Meta.WindowType.MODAL_DIALOG ||
+                type == Meta.WindowType.UTILITY);
+    },
+
     GetRunningApplicationsAsync(params, invocation) {
         if (!this._isIntrospectEnabled() &&
             !this._isSenderWhitelisted(invocation.get_sender())) {
@@ -124,5 +135,49 @@ var IntrospectService = new Lang.Class({
         }
 
         invocation.return_value(new GLib.Variant('(a{sa{sv}})', [this._runningApplications]));
+    },
+
+    GetWindowsAsync(params, invocation) {
+        let focusWindow = global.display.get_focus_window();
+        let apps = this._appSystem.get_running();
+        let windowsList = {};
+
+        if (!this._isIntrospectEnabled()) {
+            invocation.return_error_literal(Gio.DBusError,
+                                            Gio.DBusError.ACCESS_DENIED,
+                                            'App introspection not allowed');
+            return;
+        }
+
+        for (var app of apps) {
+            let windows = app.get_windows();
+            for (var window of windows) {
+
+                if (!this._isEligibleWindow(window))
+                    continue;
+
+                let window_id = window.get_id();
+                let frameRect = window.get_frame_rect();
+                let title = window.get_title();
+                let wm_class = window.get_wm_class();
+
+                windowsList[window_id] = {
+                    'app-id': GLib.Variant.new('s', app.get_id()),
+                    'client-type': GLib.Variant.new('u', window.get_client_type()),
+                    'is-hidden': GLib.Variant.new('b', window.is_hidden()),
+                    'has-focus': GLib.Variant.new('b', (window == focusWindow)),
+                    'width': GLib.Variant.new('u', frameRect.width),
+                    'height': GLib.Variant.new('u', frameRect.height)
+                };
+
+                // These properties may not be available for all windows:
+                if (title != null)
+                    windowsList[window_id]['title'] = GLib.Variant.new('s', title);
+
+                if (wm_class != null)
+                    windowsList[window_id]['wm-class'] = GLib.Variant.new('s', wm_class);
+            }
+        }
+        invocation.return_value(new GLib.Variant('(a{ta{sv}})', [windowsList]));
     }
 });
