df96b78fda723a704a786bc80d8defcfc5c10a5c
verdre <verdre@v0yd.nl>
2019-01-11 23:50:09 +0100

    workspaceThumbnail: Clean up porthole/workarea setting and updating
    Instead of unnecessarily updating the porthole on every call to the
    layout vfuncs and returning widths and heights of 0 when the overview is
    hidden, only update it on actual workarea changes.
    
    Fixes https://gitlab.gnome.org/GNOME/gnome-shell/issues/892, https://gitlab.gnome.org/GNOME/gnome-shell/issues/517
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/343

--- js/ui/workspaceThumbnail.js
+++ js/ui/workspaceThumbnail.js
@@ -633,6 +633,9 @@ var ThumbnailsBox = new Lang.Class({
         this._indicator = indicator;
         this.add_actor(indicator);
 
+        // The porthole is the part of the screen we're showing in the thumbnails
+        this._porthole = { width: 0, height: 0, x: 0, y: 0 };
+
         this._dropWorkspace = -1;
         this._dropPlaceholderPos = -1;
         this._dropPlaceholder = new St.Bin({ style_class: 'placeholder' });
@@ -683,6 +686,9 @@ var ThumbnailsBox = new Lang.Class({
             if (Main.overview.visible)
                 this._createThumbnails();
         });
+
+        global.display.connect('workareas-changed',
+                               this._updatePorthole.bind(this));
 
         this._switchWorkspaceNotifyId = 0;
         this._nWorkspacesNotifyId = 0;
@@ -921,7 +927,6 @@ var ThumbnailsBox = new Lang.Class({
         for (let w = 0; w < this._thumbnails.length; w++)
             this._thumbnails[w].destroy();
         this._thumbnails = [];
-        this._porthole = null;
     },
 
     _workspacesChanged() {
@@ -954,8 +959,6 @@ var ThumbnailsBox = new Lang.Class({
     addThumbnails(start, count) {
         let workspaceManager = global.workspace_manager;
 
-        if (!this._ensurePorthole())
-            return;
         for (let k = start; k < start + count; k++) {
             let metaWorkspace = workspaceManager.get_workspace_by_index(k);
             let thumbnail = new WorkspaceThumbnail(metaWorkspace);
@@ -1135,10 +1138,6 @@ var ThumbnailsBox = new Lang.Class({
         // Note that for getPreferredWidth/Height we cheat a bit and skip propagating
         // the size request to our children because we know how big they are and know
         // that the actors aren't depending on the virtual functions being called.
-
-        if (!this._ensurePorthole())
-            return [0, 0];
-
         let workspaceManager = global.workspace_manager;
         let themeNode = this.get_theme_node();
 
@@ -1152,9 +1151,6 @@ var ThumbnailsBox = new Lang.Class({
     },
 
     vfunc_get_preferred_width(forHeight) {
-        if (!this._ensurePorthole())
-            return [0, 0];
-
         let workspaceManager = global.workspace_manager;
         let themeNode = this.get_theme_node();
 
@@ -1174,16 +1170,14 @@ var ThumbnailsBox = new Lang.Class({
         return themeNode.adjust_preferred_width(width, width);
     },
 
-    // The "porthole" is the portion of the screen that we show in the
-    // workspaces
-    _ensurePorthole() {
-        if (!Main.layoutManager.primaryMonitor || !Main.overview.visible)
-            return false;
-
-        if (!this._porthole)
+    _updatePorthole() {
+        if (!Main.layoutManager.primaryMonitor)
+            this._porthole = { width: global.stage.width, height: global.stage.height,
+                               x: global.stage.x, y: global.stage.y };
+        else
             this._porthole = Main.layoutManager.getWorkAreaForMonitor(Main.layoutManager.primaryIndex);
 
-        return true;
+        this.queue_relayout();
     },
 
     vfunc_allocate(box, flags) {
