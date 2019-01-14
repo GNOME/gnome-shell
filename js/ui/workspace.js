5c04ed8b3984894a51df36c9fdf1c265f890aeda
verdre <verdre@v0yd.nl>
2019-01-14 22:10:43 +0100

    workspace: Rename window overlay caption to title
    We're using title in most other places to describe the label, be
    consistent and rename it to title.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/346

--- js/ui/workspace.js
+++ js/ui/workspace.js
@@ -442,7 +442,7 @@ Signals.addSignalMethods(WindowClone.prototype);
 /**
  * @windowClone: Corresponding window clone
  * @parentActor: The actor which will be the parent of all overlay items
- *               such as app icon and window caption
+ *               such as close button and window title
  */
 var WindowOverlay = class {
     constructor(windowClone, parentActor) {
@@ -457,15 +457,15 @@ var WindowOverlay = class {
         this.borderSize = 0;
         this.border = new St.Bin({ style_class: 'window-clone-border' });
 
-        this.title = new St.Label({ style_class: 'window-caption',
-                                    text: this._getCaption() });
+        this.title = new St.Label({ style_class: 'window-title',
+                                    text: this._getTitle() });
         this.title.clutter_text.ellipsize = Pango.EllipsizeMode.END;
         windowClone.actor.label_actor = this.title;
 
         this._maxTitleWidth = -1;
 
-        this._updateCaptionId = metaWindow.connect('notify::title', w => {
-            this.title.text = this._getCaption();
+        this._updateTitleId = metaWindow.connect('notify::title', w => {
+            this.title.text = this._getTitle();
             this.relayout(false);
         });
 
@@ -595,7 +595,7 @@ var WindowOverlay = class {
         }
     }
 
-    _getCaption() {
+    _getTitle() {
         let metaWindow = this._windowClone.metaWindow;
         if (metaWindow.title)
             return metaWindow.title;
@@ -628,8 +628,7 @@ var WindowOverlay = class {
             Mainloop.source_remove(this._idleHideOverlayId);
             this._idleHideOverlayId = 0;
         }
-
-        this._windowClone.metaWindow.disconnect(this._updateCaptionId);
+        this._windowClone.metaWindow.disconnect(this._updateTitleId);
         this.title.destroy();
         this.closeButton.destroy();
         this.border.destroy();
