dc05b70e7aa053927f747190602f861a83d175ba
verdre <verdre@v0yd.nl>
2019-01-14 23:48:27 +0100

    Revert "workspace: Show window chrome on long-press"
    This workaround is no longer needed now that we show all window overlays
    on touchscreen devices.
    
    This reverts commit 6c472d88891c7862ad0619b9434c50bd507f959b.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/345

--- js/ui/workspace.js
+++ js/ui/workspace.js
@@ -397,9 +397,8 @@ var WindowClone = new Lang.Class({
                 action.release();
                 this._draggable.startDrag(x, y, global.get_current_time(), this._dragTouchSequence);
             });
-        } else {
-            this.emit('show-chrome');
         }
+
         return true;
     },
 
