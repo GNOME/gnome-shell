GNOME/gnome-shell

commit e0a992af735348110c07e4ba4cefa75de26e3c81

notificationDaemon: Fix warning
Since commit 33b8537bf5, we unconditionally access the 'image-path'
hint, resulting in a gjs warning if the hint is not defined.

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/309

js/ui/notificationDaemon.js
@@ -77,8 +77,10 @@ var FdoNotificationDaemon = new Lang.Class({
                  bitsPerSample, nChannels, data] = hints['image-data'];
             return Shell.util_create_pixbuf_from_data(data, GdkPixbuf.Colorspace.RGB, hasAlpha,
                                                       bitsPerSample, width, height, rowStride);
+        } else if (hints['image-path']) {
+            return this._iconForNotificationData(hints['image-path'];
         }
-        return this._iconForNotificationData(hints['image-path']);
+        return null;
     },
 
     _fallbackIconForNotificationData(hints) {

commit 4b28b90e0fb941c4bb7ffbdb3795c12e52ff65e5

notificationDaemon.js: Fix a typo (missing ')')
Introduced in e0a992af7353 it was preventing gnome-shell from starting.

https://gitlab.gnome.org/GNOME/gnome-shell/issues/915

js/ui/notificationDaemon.js
@@ -78,7 +78,7 @@ var FdoNotificationDaemon = new Lang.Class({
             return Shell.util_create_pixbuf_from_data(data, GdkPixbuf.Colorspace.RGB, hasAlpha,
                                                       bitsPerSample, width, height, rowStride);
         } else if (hints['image-path']) {
-            return this._iconForNotificationData(hints['image-path'];
+            return this._iconForNotificationData(hints['image-path']);
         }
         return null;
     },