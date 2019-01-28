12844a9f24dcd788260bc348d35cd3a559dc5f41
Xiaoguang Wang <xwang@suse.com>
2019-01-28 09:31:32 +0800

    screenShield: Dim screen when lockscreen is active
    https://gitlab.gnome.org/GNOME/gnome-shell/issues/900

--- js/ui/screenShield.js
+++ js/ui/screenShield.js
@@ -460,6 +460,7 @@ var ScreenShield = class {
         this._arrowAnimationId = 0;
         this._arrowWatchId = 0;
         this._arrowActiveWatchId = 0;
+        this._lockScreenIdleWatchId = 0;
         this._arrowContainer = new St.BoxLayout({ style_class: 'screen-shield-arrows',
                                                   vertical: true,
                                                   x_align: Clutter.ActorAlign.CENTER,
@@ -885,6 +886,17 @@ var ScreenShield = class {
         } else {
             this.deactivate(false);
         }
+
+        if (!this._lockScreenIdleWatchId)
+            this._lockScreenIdleWatchId = this.idleMonitor.add_idle_watch(STANDARD_FADE_TIME * 1000,
+                                                                          this._fadeLockScreen.bind(this))
+    },
+
+    _fadeLockScreen() {
+        this.idleMonitor.remove_watch(this._lockScreenIdleWatchId);
+        this._lockScreenIdleWatchId = 0;
+
+        this._activateFade(this._longLightbox, STANDARD_FADE_TIME);
     }
 
     _onLongLightboxShown() {
@@ -1199,6 +1211,11 @@ var ScreenShield = class {
             Main.sessionMode.popMode('lock-screen');
         if (Main.sessionMode.currentMode == 'unlock-dialog')
             Main.sessionMode.popMode('unlock-dialog');
+
+        if (this._lockScreenIdleWatchId) {
+            this.idleMonitor.remove_watch(this._lockScreenIdleWatchId);
+            this._lockScreenIdleWatchId = 0;
+        }
 
         if (this._isGreeter) {
             // We don't want to "deactivate" any more than
