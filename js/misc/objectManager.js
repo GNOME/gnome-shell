3337bd96715f2020dcf52a804042d487e6e7e909
Florian Müllner <fmuellner@gnome.org>
2019-01-29 23:55:05 +0100

    objectManager: Fix undefined variable
    Spotted by eslint.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/375

--- js/misc/objectManager.js
+++ js/misc/objectManager.js
@@ -161,7 +161,7 @@ var ObjectManager = new Lang.Class({
         try {
             initable.init_finish(result);
         } catch(e) {
-            logError(e, 'could not initialize object manager for object ' + params.name);
+            logError(e, 'could not initialize object manager for object ' + this._serviceName);
 
             this._tryToCompleteLoad();
             return;
