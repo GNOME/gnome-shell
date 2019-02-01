e13f33925325693e86f6e38fb509ea11aab5cd28
Olivier Fourdan <ofourdan@redhat.com>
2019-02-01 17:44:05 +0100

    inhibitShortcuts: Save choice in permission store
    Use the permission store to remember the user's decision as to whether
    or not grant the shortcuts request when the application is known.

--- js/ui/inhibitShortcutsDialog.js
+++ js/ui/inhibitShortcutsDialog.js
@@ -1,5 +1,6 @@
 const Clutter = imports.gi.Clutter;
 const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
 const GObject = imports.gi.GObject;
 const Gtk = imports.gi.Gtk;
 const Meta = imports.gi.Meta;
@@ -8,10 +9,13 @@ const St = imports.gi.St;
 
 const Dialog = imports.ui.dialog;
 const ModalDialog = imports.ui.modalDialog;
+const PermissionStore = imports.misc.permissionStore;
 
 const WAYLAND_KEYBINDINGS_SCHEMA = 'org.gnome.mutter.wayland.keybindings';
 
 const APP_WHITELIST = ['gnome-control-center.desktop'];
+const APP_PERMISSIONS_TABLE = 'gnome';
+const APP_PERMISSIONS_ID = 'shortcuts-inhibitor';
 
 var DialogResponse = Meta.InhibitShortcutsDialogResponse;
 
@@ -42,11 +46,41 @@ var InhibitShortcutsDialog = GObject.registerClass({
         return windowTracker.get_window_app(this._window);
     }
 
+    get _appInfo() {
+        let app = this._app;
+        if (!app)
+            return null;
+
+        let appInfo = app.get_app_info();
+        if (!appInfo)
+            return null;
+
+        return appInfo.get_id();
+    }
+
     _getRestoreAccel() {
         let settings = new Gio.Settings({ schema_id: WAYLAND_KEYBINDINGS_SCHEMA });
         let accel = settings.get_strv('restore-shortcuts')[0] || '';
         return Gtk.accelerator_get_label.apply(null,
                                                Gtk.accelerator_parse(accel));
+    }
+
+    _saveToPermissionStore(grant) {
+        if (this._appInfo == null || this._permStore == null)
+            return;
+
+        this._permissions[this._appInfo] = [grant.toString()];
+        let data = GLib.Variant.new('av', {});
+
+        this._permStore.SetRemote(APP_PERMISSIONS_TABLE,
+                                  true,
+                                  APP_PERMISSIONS_ID,
+                                  this._permissions,
+                                  data,
+                                  (result, error) => {
+            if (error != null)
+                log(error.message);
+        });
     }
 
     _buildLayout() {
@@ -70,12 +104,14 @@ var InhibitShortcutsDialog = GObject.registerClass({
 
         this._dialog.addButton({ label: _("Deny"),
                                  action: () => {
+                                     this._saveToPermissionStore(false);
                                      this._emitResponse(DialogResponse.DENY);
                                  },
                                  key: Clutter.KEY_Escape });
 
         this._dialog.addButton({ label: _("Allow"),
                                  action: () => {
+                                     this._saveToPermissionStore(true);
                                      this._emitResponse(DialogResponse.ALLOW);
                                  },
                                  default: true });
@@ -87,10 +123,39 @@ var InhibitShortcutsDialog = GObject.registerClass({
     }
 
     vfunc_show() {
-        if (this._app && APP_WHITELIST.indexOf(this._app.get_id()) != -1)
+        if (this._app && APP_WHITELIST.indexOf(this._app.get_id()) != -1) {
             this._emitResponse(DialogResponse.ALLOW);
-        else
-            this._dialog.open();
+            return;
+        }
+
+        /* Ask the permission-store */
+        this._permStore = new PermissionStore.PermissionStore((proxy, error) => {
+            if (error) {
+                log(error.message);
+                this._dialog.open();
+                return;
+            }
+
+            if (this._appInfo) {
+                this._permStore.LookupRemote(APP_PERMISSIONS_TABLE,
+                                             APP_PERMISSIONS_ID,
+                                             (res, error) => {
+                    if (error) {
+                        this._dialog.open();
+                        log(error.message);
+                        return;
+                    }
+
+                    let [permissions, data] = res;
+                    if (permissions[this._appInfo] === undefined) // Not found
+                        this._dialog.open();
+                    else if (permissions[this._appInfo] == "true")
+                        this._emitResponse(DialogResponse.ALLOW);
+                    else
+                        this._emitResponse(DialogResponse.DENY);
+               });
+            }
+        });
     }
 
     vfunc_hide() {
