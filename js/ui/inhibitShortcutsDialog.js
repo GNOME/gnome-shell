e39034c61d7592a732eb8213c6856fe3e144da1f
Olivier Fourdan <ofourdan@redhat.com>
2019-02-01 17:44:05 +0100

    inhibitShortcuts: Add checkbox to remember choice
    If the application is known, add a checkbox to save the user's decision
    as to whether or not grant the shortcuts request.

--- js/ui/inhibitShortcutsDialog.js
+++ js/ui/inhibitShortcutsDialog.js
@@ -6,8 +6,10 @@ const Meta = imports.gi.Meta;
 const Shell = imports.gi.Shell;
 const St = imports.gi.St;
 
+const Main = imports.ui.main;
 const Dialog = imports.ui.dialog;
 const ModalDialog = imports.ui.modalDialog;
+const CheckBox = imports.ui.checkBox;
 
 const WAYLAND_KEYBINDINGS_SCHEMA = 'org.gnome.mutter.wayland.keybindings';
 
@@ -42,11 +44,38 @@ var InhibitShortcutsDialog = GObject.registerClass({
         return windowTracker.get_window_app(this._window);
     }
 
+    get _appInfo() {
+       let app = this._app;
+       if (!app)
+           return null;
+
+       let appInfo = app.get_app_info();
+       if (!appInfo)
+           return null;
+
+       return appInfo.get_id();
+    }
+
     _getRestoreAccel() {
         let settings = new Gio.Settings({ schema_id: WAYLAND_KEYBINDINGS_SCHEMA });
         let accel = settings.get_strv('restore-shortcuts')[0] || '';
         return Gtk.accelerator_get_label.apply(null,
                                                Gtk.accelerator_parse(accel));
+    }
+
+    _isWhitelisted() {
+        return APP_WHITELIST.includes(this._app.get_id());
+    }
+
+    _isInList(listName) {
+       let list = global.settings.get_strv(listName);
+       return list.includes(this._appInfo);
+    }
+
+    _addToList(listName) {
+       let list = global.settings.get_strv(listName);
+       list.push(this._appInfo);
+       global.settings.set_strv(listName, list);
     }
 
     _buildLayout() {
@@ -66,16 +95,27 @@ var InhibitShortcutsDialog = GObject.registerClass({
                 _("You can restore shortcuts by pressing %s.").format(restoreAccel);
 
         let content = new Dialog.MessageDialogContent(contentParams);
+
+        let check = new CheckBox.CheckBox();
+        check.getLabelActor().text = _("Do not ask again for this application");
+        check.actor.checked = false;
+        if (this._appInfo)
+            content.insertBeforeBody(check.actor);
+
         this._dialog.contentLayout.add_actor(content);
 
         this._dialog.addButton({ label: _("Deny"),
                                  action: () => {
+                                     if (this._appInfo && check.actor.checked)
+                                         this._addToList('inhibit-shortcuts-denied');
                                      this._emitResponse(DialogResponse.DENY);
                                  },
                                  key: Clutter.KEY_Escape });
 
         this._dialog.addButton({ label: _("Allow"),
                                  action: () => {
+                                     if (this._appInfo && check.actor.checked)
+                                         this._addToList('inhibit-shortcuts-granted');
                                      this._emitResponse(DialogResponse.ALLOW);
                                  },
                                  default: true });
@@ -87,10 +127,17 @@ var InhibitShortcutsDialog = GObject.registerClass({
     }
 
     vfunc_show() {
-        if (this._app && APP_WHITELIST.indexOf(this._app.get_id()) != -1)
-            this._emitResponse(DialogResponse.ALLOW);
-        else
-            this._dialog.open();
+        if (this._app) {
+            if (this._isWhitelisted() || this._isInList('inhibit-shortcuts-granted')) {
+                this._emitResponse(DialogResponse.ALLOW);
+                return;
+            }
+            if (this._isInList('inhibit-shortcuts-denied')) {
+                this._emitResponse(DialogResponse.DENY);
+                return;
+            }
+        }
+        this._dialog.open();
     }
 
     vfunc_hide() {
