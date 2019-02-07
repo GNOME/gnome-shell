ab6f4b923d0d78cc78ebaae08893aedffbbd6301
Ray Strode <rstrode@redhat.com>
2019-02-07 14:27:35 -0500

    windowManager: ensure actor resize clone dies with actor
    If a window gets destroyed right before it's resize
    animation starts the user can get confronted with an undead
    zombie clone that doesn't go away.
    
    This commit makes sure said clones get reaped with their
    actors.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/issues/1166

--- js/ui/windowManager.js
+++ js/ui/windowManager.js
@@ -1468,8 +1468,13 @@ var WindowManager = class {
         if (this._clearAnimationInfo(actor))
             this._shellwm.completed_size_change(actor);
 
+        let destroyId = actor.connect('destroy', () => {
+            this._clearAnimationInfo(actor);
+        });
+
         actor.__animationInfo = { clone: actorClone,
-                                  oldRect: oldFrameRect };
+                                  oldRect: oldFrameRect,
+                                  destroyId: destroyId };
     }
 
     _sizeChangedWindow(shellwm, actor) {
@@ -1530,6 +1535,7 @@ var WindowManager = class {
     _clearAnimationInfo(actor) {
         if (actor.__animationInfo) {
             actor.__animationInfo.clone.destroy();
+            actor.disconnect(actor.__animationInfo.destroyId);
             delete actor.__animationInfo;
             return true;
         }
