38334222107d855d1e95e1a9cc93af5ce7567bed
Jonas Dreßler <verdre@v0yd.nl>
2019-02-10 00:19:08 +0100

    background: Add function to get background image characteristics
    Use the get_color_info API of meta-background to get the color
    information about a particular area of the background image and apply
    different thresholds to the mean luminance, the mean acutance and the
    standard deviation of the luminance to find specific characteristics of
    the image.
    
    We don't do this for animated backgrounds since those might be updated
    as often as once every second, and while the calulation is not resource
    intensive if used with a small area, it has the potential to block the
    main loop for several seconds if the area is large enough.

--- js/ui/background.js
+++ js/ui/background.js
@@ -111,6 +111,11 @@ const BACKGROUND_STYLE_KEY = 'picture-options';
 const PICTURE_OPACITY_KEY = 'picture-opacity';
 const PICTURE_URI_KEY = 'picture-uri';
 
+const LUMINANCE_DARK_THRESHOLD = 60;
+const LUMINANCE_BRIGHT_THRESHOLD = 160;
+const LUMINANCE_STD_BUSY_THRESHOLD = 48;
+const ACUTANCE_BUSY_THRESHOLD = 8;
+
 var FADE_ANIMATION_TIME = 1.0;
 
 // These parameters affect how often we redraw.
@@ -288,6 +293,10 @@ var Background = class Background {
         if (this._settingsChangedSignalId != 0)
             this._settings.disconnect(this._settingsChangedSignalId);
         this._settingsChangedSignalId = 0;
+    }
+
+    isAnimated() {
+        return this._animation != null;
     }
 
     updateResolution() {
@@ -687,6 +696,43 @@ var BackgroundManager = class BackgroundManager {
             this.backgroundActor.destroy();
             this.backgroundActor = null;
         }
+    }
+
+    getCharacteristicsForArea(x, y, width, height) {
+        let background = this._backgroundSource.getBackground(this._monitorIndex);
+        let metaBackground = background.background;
+
+        let areaIsBusy, areaIsDark, areaIsBright;
+        areaIsBusy = areaIsDark = areaIsBright = false;
+
+        // Always return false for animated backgrounds, we don't want to
+        // do those calculations on every animation frame.
+        if (background.isAnimated())
+            return [false];
+
+        let [retval, averageR, averageG, averageB,
+             meanLuminance, luminanceVariance, meanAcutance] =
+            metaBackground.get_color_info(this._monitorIndex, x, y, width, height);
+
+        if (!retval)
+            return [false];
+
+        let luminanceStd = Math.sqrt(luminanceVariance);
+
+        if (meanLuminance < LUMINANCE_DARK_THRESHOLD)
+            areaIsDark = true;
+        else if (meanLuminance > LUMINANCE_BRIGHT_THRESHOLD)
+            areaIsBright = true;
+
+        if (meanAcutance > ACUTANCE_BUSY_THRESHOLD ||
+            luminanceStd > LUMINANCE_STD_BUSY_THRESHOLD ||
+            (areaIsDark &&
+             meanLuminance + luminanceStd > LUMINANCE_BRIGHT_THRESHOLD) ||
+            (areaIsBright &&
+             meanLuminance - luminanceStd < LUMINANCE_DARK_THRESHOLD))
+            areaIsBusy = true;
+
+        return [true, areaIsBusy, areaIsDark, areaIsBright];
     }
 
     _swapBackgroundActor() {
