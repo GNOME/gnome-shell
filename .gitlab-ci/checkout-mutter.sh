054e97b57db4742e430b99e0dbf42800b4199409
Florian Müllner <fmuellner@gnome.org>
2019-02-12 17:53:42 +0100

    ci: Add build stage
    So far we are only performing a basic syntax check on javascript
    sources; it's time to test the C code as well. As mutter is tightly
    coupled, we bite the bullet and build it as well, either using a
    matching branch (if it exists), or current master.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/408

---
+++ .gitlab-ci/checkout-mutter.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash -w
+
+shell_branch=$(git describe --contains --all HEAD)
+mutter_target=
+
+git clone https://gitlab.gnome.org/GNOME/mutter.git
+
+if [ $? -ne 0 ]; then
+  echo Checkout failed
+  exit 1
+fi
+
+cd mutter
+
+if [ "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" ]; then
+  merge_request_remote=${CI_MERGE_REQUEST_PROJECT_URL//gnome-shell/mutter}
+  merge_request_branch=$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
+
+  echo Looking for $merge_request_branch on remote ...
+  if git fetch mr-origin $merge_request_branch >/dev/null 2>&1; then
+    mutter_target=FETCH_HEAD
+  fi
+fi
+
+if [ -z "$mutter_target" ]; then
+  mutter_target=$(git branch -r -l origin/$shell_branch)
+  mutter_target=${mutter_target:-origin/master}
+  echo Using $mutter_target instead
+fi
+
+git checkout $mutter_target
