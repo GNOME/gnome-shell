158b269c3940264ac096523431f156c362787900
Jonas Dreßler <verdre@v0yd.nl>
2019-02-12 13:04:55 +0100

    workspace: Make title of overlay reactive
    Also make the title of the overlay reactive so we can keep the overlay
    visible if the mouse is hovering over the title.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/683

--- js/ui/workspace.js
+++ js/ui/workspace.js
@@ -461,7 +461,8 @@ var WindowOverlay = class {
         this.border = new St.Bin({ style_class: 'window-clone-border' });
 
         this.title = new St.Label({ style_class: 'window-caption',
-                                    text: this._getCaption() });
+                                    text: this._getCaption(),
+                                    reactive: true });
         this.title.clutter_text.ellipsize = Pango.EllipsizeMode.END;
         windowClone.label_actor = this.title;
 
@@ -486,7 +487,6 @@ var WindowOverlay = class {
         this.closeButton.hide();
 
         // Don't block drop targets
-        Shell.util_set_hidden_from_pick(this.title, true);
         Shell.util_set_hidden_from_pick(this.border, true);
 
         parentActor.add_actor(this.border);
@@ -686,7 +686,8 @@ var WindowOverlay = class {
     }
 
     _idleHideOverlay() {
-        if (this.closeButton['has-pointer'])
+        if (this.closeButton['has-pointer'] ||
+            this.title['has-pointer'])
             return GLib.SOURCE_CONTINUE;
 
         if (!this._windowClone['has-pointer'])
