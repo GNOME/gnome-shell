9620bd0f225bb844c8541c29b81aa1764986ce20
Arnaud Bonatti <arnaud.bonatti@gmail.com>
2019-02-13 13:23:47 +0100

    Add a Taquin renamed desktop id.
    Taquin is org.gnome.Taquin.

--- js/ui/appFavorites.js
+++ js/ui/appFavorites.js
@@ -44,6 +44,7 @@ const RENAMED_DESKTOP_IDS = {
     'iagno.desktop': 'org.gnome.Reversi.desktop',
     'nautilus.desktop': 'org.gnome.Nautilus.desktop',
     'org.gnome.gnome-2048.desktop': 'org.gnome.TwentyFortyEight.desktop',
+    'org.gnome.taquin.desktop': 'org.gnome.Taquin.desktop',
     'org.gnome.Weather.Application.desktop': 'org.gnome.Weather.desktop',
     'polari.desktop': 'org.gnome.Polari.desktop',
     'totem.desktop': 'org.gnome.Totem.desktop',
