c0292f689cb259b85d897f920e5faef69897666f
Florian Müllner <fmuellner@gnome.org>
2019-02-16 17:44:27 +0100

    ci: Sync check-commit-log script with mutter
    A couple of cleanups came out from the review in mutter, catch
    up with those.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/408

--- .gitlab-ci/check-commit-log.sh
+++ .gitlab-ci/check-commit-log.sh
@@ -11,9 +11,21 @@ branch_point=$(git merge-base HEAD FETCH_HEAD)
 
 commits=$(git log --format='format:%H' $branch_point..$CI_COMMIT_SHA)
 
-test -z "$commits" && { echo Commit range empty; exit 1; }
+if [ -z "$commits" ]; then
+  echo Commit range empty
+  exit 1
+fi
+
+function commit_message_has_url() {
+  commit=$1
+  commit_message=$(git show -s --format='format:%b' $commit)
+  echo "$commit_message" | grep -qe "\($CI_MERGE_REQUEST_PROJECT_URL/\(issues\|merge_requests\)/[0-9]\+\|https://bugzilla.gnome.org/show_bug.cgi?id=[0-9]\+\)"
+  return $?
+}
 
 for commit in $commits; do
-  git show -s --format='format:%b' $commit | grep -qe "\($CI_MERGE_REQUEST_PROJECT_URL/\(issues\|merge_requests\)/[0-9]\+\|https://bugzilla.gnome.org/show_bug.cgi?id=[0-9]\+\)" ||
-    { echo "Missing merge request or issue URL on commit $(echo $commit | cut -c -8)"; exit 1; }
+  if ! commit_message_has_url $commit; then
+    echo "Missing merge request or issue URL on commit $(echo $commit | cut -c -8)"
+    exit 1
+  fi
 done
