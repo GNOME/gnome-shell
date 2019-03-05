ef8df077433eae8cc566acfcadafa915059a5fe9
Florian Müllner <fmuellner@gnome.org>
2019-03-05 19:31:01 +0100

    main: Wait for startup completion before running perf script
    The scripts generally assume a functional shell, so wait until
    that is the case.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/439

--- js/ui/main.js
+++ js/ui/main.js
@@ -211,13 +211,6 @@ function _initializeUI() {
 
     _startDate = new Date();
 
-    let perfModuleName = GLib.getenv("SHELL_PERF_MODULE");
-    if (perfModuleName) {
-        let perfOutput = GLib.getenv("SHELL_PERF_OUTPUT");
-        let module = eval('imports.perf.' + perfModuleName + ';');
-        Scripting.runPerfScript(module, perfOutput);
-    }
-
     ExtensionDownloader.init();
     ExtensionSystem.init();
 
@@ -238,6 +231,13 @@ function _initializeUI() {
             sessionMode.currentMode != 'initial-setup') {
             Shell.Global.log_structured('GNOME Shell started at ' + _startDate,
                                         ['MESSAGE_ID=' + GNOMESHELL_STARTED_MESSAGE_ID]);
+        }
+
+        let perfModuleName = GLib.getenv("SHELL_PERF_MODULE");
+        if (perfModuleName) {
+            let perfOutput = GLib.getenv("SHELL_PERF_OUTPUT");
+            let module = eval('imports.perf.' + perfModuleName + ';');
+            Scripting.runPerfScript(module, perfOutput);
         }
     });
 }
