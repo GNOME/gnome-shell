8f805024314ea65925660e038eaa4e6a1e34d91e
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-03-11 23:47:09 +0000

    magnifier: Scale the sprite to match the current monitor scaling
    Compute the sprite texture scale (unfortunately using a workaround, based on
    mutter cursor size preferences), and use this value to compute the scaling that
    should be applied to the sprite in order to match the current monitor scaling.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/454

--- js/ui/magnifier.js
+++ js/ui/magnifier.js
@@ -51,6 +51,8 @@ var MouseSpriteContent = GObject.registerClass({
 }, class MouseSpriteContent extends GObject.Object {
     _init() {
         super._init();
+        this._scale = 1.0;
+        this._monitorScale = 1.0;
         this._texture = null;
     }
 
@@ -58,7 +60,10 @@ var MouseSpriteContent = GObject.registerClass({
         if (!this._texture)
             return [false, 0, 0];
 
-        return [true, this._texture.get_width(), this._texture.get_height()];
+        let width = this._texture.get_width() / this._scale;
+        let height = this._texture.get_height() / this._scale;
+
+        return [true, width, height];
     }
 
     vfunc_paint_content(actor, node) {
@@ -75,6 +80,28 @@ var MouseSpriteContent = GObject.registerClass({
         textureNode.add_rectangle(actor.get_content_box());
     }
 
+    _textureScale() {
+        if (!this._texture)
+            return 1;
+
+        /* This is a workaround to guess the sprite scale; while it works
+         * fine in normal scenarios, it's not garanteed to work in all the cases,
+         * and so we should actually add an API to mutter that will allow us
+         * to know the real spirte texture scaling in order to adapt it to the
+         * wanted one. */
+        let avgSize = (this._texture.get_width() + this._texture.get_height()) / 2;
+        return avgSize / Meta.prefs_get_cursor_size();
+    }
+
+    _recomputeScale() {
+        let scale = this._textureScale() / this._monitorScale;
+
+        if (this._scale != scale) {
+            this._scale = scale;
+            this.invalidate_size();
+        }
+    }
+
     get texture() {
         return this._texture;
     }
@@ -88,10 +115,20 @@ var MouseSpriteContent = GObject.registerClass({
 
         if (!oldTexture || !coglTexture ||
             oldTexture.get_width() != coglTexture.get_width() ||
-            oldTexture.get_height() != coglTexture.get_height())
+            oldTexture.get_height() != coglTexture.get_height()) {
+            this._recomputeScale();
             this.invalidate_size();
-        else
+        } else
             this.invalidate();
+    }
+
+    get scale() {
+        return this._scale;
+    }
+
+    set monitorScale(monitorScale) {
+        this._monitorScale = monitorScale;
+        this._recomputeScale();
     }
 });
 
@@ -234,6 +271,9 @@ var Magnifier = class Magnifier {
         if (xMouse != this.xMouse || yMouse != this.yMouse) {
             this.xMouse = xMouse;
             this.yMouse = yMouse;
+
+            let monitor = Main.layoutManager.findMonitorForActor(this._mouseSprite);
+            this._mouseSprite.content.monitorScale = monitor.geometry_scale;
 
             let sysMouseOverAny = false;
             this._zoomRegions.forEach((zoomRegion, index, array) => {
