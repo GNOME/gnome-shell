62233a4db4ff94cfe091f6696dbca4f17a3cffdc
Carlos Garnacho <carlosg@gnome.org>
2019-03-12 02:14:07 +0100

    dnd: Multiply drag threshold by output scale
    So it comes out right on hidpi, and consistent with clients.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/455

--- js/ui/dnd.js
+++ js/ui/dnd.js
@@ -450,7 +450,8 @@ var _Draggable = class _Draggable {
         let [stageX, stageY] = event.get_coords();
 
         // See if the user has moved the mouse enough to trigger a drag
-        let threshold = St.Settings.get().drag_threshold;
+        let scaleFactor = St.ThemeContext.get_for_stage(global.stage).scale_factor;
+        let threshold = St.Settings.get().drag_threshold * scaleFactor;
         if (!currentDraggable &&
             (Math.abs(stageX - this._dragStartX) > threshold ||
              Math.abs(stageY - this._dragStartY) > threshold)) {
