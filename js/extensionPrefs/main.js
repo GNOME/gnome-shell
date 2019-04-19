feb571de53061fed3a53f69dfd893913a71d70ca
Benjamin Berg <bberg@redhat.com>
2019-04-20 00:17:25 +0200

    extensionPrefs: Delete disabled warning marker
    It isn't possible to easily delete the marker from an XDG autostart
    file, so make delete it on startup to simplify that.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/507

--- js/extensionPrefs/main.js
+++ js/extensionPrefs/main.js
@@ -6,7 +6,7 @@ const _ = Gettext.gettext;
 
 const Config = imports.misc.config;
 const ExtensionUtils = imports.misc.extensionUtils;
-const { loadInterfaceXML } = imports.misc.fileUtils;
+const { loadInterfaceXML, deleteGFile } = imports.misc.fileUtils;
 
 const GnomeShellIface = loadInterfaceXML('org.gnome.Shell.Extensions');
 const GnomeShellProxy = Gio.DBusProxy.makeProxyWrapper(GnomeShellIface);
@@ -329,6 +329,12 @@ var Application = class {
                     app.quit();
 
                 this._disabled_infobar.set_revealed(true);
+
+                let file = GLib.build_filenamev ([GLib.get_user_config_dir(), 'gnome-shell-extensions-disabled-warning']);
+                let gfile = Gio.File.new_for_path(file);
+                if (gfile.query_exists(null))
+                    deleteGFile(gfile);
+
                 return 0;
             }
 
