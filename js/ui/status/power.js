4187a0767463b60337b5eb7b55687c5ae7f3ceac
Florian Müllner <fmuellner@gnome.org>
2019-05-02 13:18:57 +0200

    power: Remove submenu
    The submenu contains a single menu item - a link to Power Settings - that
    mainly exits as an excuse for the menu itself, which serves as an indicator
    for the charge-level and time estimate.
    
    However we already have an option for showing the charge-level in the top bar,
    so revealing it every time the menu is opened can take over the indicator
    functionality without taking up precious space in the system menu.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/issues/1241

--- js/ui/status/power.js
+++ js/ui/status/power.js
@@ -41,85 +41,27 @@ var Indicator = class extends PanelMenu.SystemIndicator {
                                                 this._sync();
                                             });
 
-        this._item = new PopupMenu.PopupSubMenuMenuItem("", true);
-        this._item.menu.addSettingsAction(_("Power Settings"), 'gnome-power-panel.desktop');
-        this.menu.addMenuItem(this._item);
-
-        Main.sessionMode.connect('updated', this._sessionUpdated.bind(this));
-        this._sessionUpdated();
-    }
-
-    _sessionUpdated() {
-        let sensitive = !Main.sessionMode.isLocked && !Main.sessionMode.isGreeter;
-        this.menu.setSensitive(sensitive);
-    }
-
-    _getStatus() {
-        let seconds = 0;
-
-        if (this._proxy.State == UPower.DeviceState.FULLY_CHARGED)
-            return _("Fully Charged");
-        else if (this._proxy.State == UPower.DeviceState.CHARGING)
-            seconds = this._proxy.TimeToFull;
-        else if (this._proxy.State == UPower.DeviceState.DISCHARGING)
-            seconds = this._proxy.TimeToEmpty;
-        else if (this._proxy.State == UPower.DeviceState.PENDING_CHARGE)
-            return _("Not Charging");
-        // state is PENDING_DISCHARGE
-        else
-            return _("Estimating…");
-
-        let time = Math.round(seconds / 60);
-        if (time == 0) {
-            // 0 is reported when UPower does not have enough data
-            // to estimate battery life
-            return _("Estimating…");
-        }
-
-        let minutes = time % 60;
-        let hours = Math.floor(time / 60);
-
-        if (this._proxy.State == UPower.DeviceState.DISCHARGING) {
-            // Translators: this is <hours>:<minutes> Remaining (<percentage>)
-            return _("%d\u2236%02d Remaining (%d\u2009%%)").format(hours, minutes, this._proxy.Percentage);
-        }
-
-        if (this._proxy.State == UPower.DeviceState.CHARGING) {
-            // Translators: this is <hours>:<minutes> Until Full (<percentage>)
-            return _("%d\u2236%02d Until Full (%d\u2009%%)").format(hours, minutes, this._proxy.Percentage);
-        }
-
-        return null;
+        this.menu.connect('open-state-changed', this._sync.bind(this));
     }
 
     _sync() {
         // Do we have batteries or a UPS?
-        let visible = this._proxy.IsPresent;
-        if (visible) {
-            this._item.actor.show();
-            this._percentageLabel.visible = this._desktopSettings.get_boolean(SHOW_BATTERY_PERCENTAGE);
-        } else {
+        if (!this._proxy.IsPresent) {
             // If there's no battery, then we use the power icon.
-            this._item.actor.hide();
             this._indicator.icon_name = 'system-shutdown-symbolic';
             this._percentageLabel.hide();
             return;
         }
 
-        // The icons
-        let icon = this._proxy.IconName;
-        this._indicator.icon_name = icon;
-        this._item.icon.icon_name = icon;
+        this._indicator.icon_name = this._proxy.IconName;
 
-        // The icon label
         let label
         if (this._proxy.State == UPower.DeviceState.FULLY_CHARGED)
           label = _("%d\u2009%%").format(100);
         else
           label = _("%d\u2009%%").format(this._proxy.Percentage);
         this._percentageLabel.clutter_text.set_markup('<span size="smaller">' + label + '</span>');
-
-        // The status label
-        this._item.label.text = this._getStatus();
+        this._percentageLabel.visible = this.menu._getTopMenu().isOpen ||
+                                        this._desktopSettings.get_boolean(SHOW_BATTERY_PERCENTAGE);
     }
 };
