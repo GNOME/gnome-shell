c80854448d6701fd4f17707cbed2c990b0a8fd56
Jonas Dreßler <verdre@v0yd.nl>
2019-05-02 17:49:21 +0200

    workspace: Return results of forwarded acceptDrop
    Return the results of calls to acceptDrop that we forwarded to the
    Workspace object. This fixes a bug where app icons that were dragged and
    released above a window clone would get animated back in place even
    though they opened correctly.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/121

--- js/ui/workspace.js
+++ js/ui/workspace.js
@@ -404,7 +404,7 @@ var WindowClone = class {
     }
 
     acceptDrop(source, actor, x, y, time) {
-        this._workspace.acceptDrop(source, actor, x, y, time);
+        return this._workspace.acceptDrop(source, actor, x, y, time);
     }
 
     _onDragCancelled(draggable, time) {
