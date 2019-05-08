b47dabfcd7718c8993ce6bd105275542453464c2
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-05-09 01:49:49 +0200

    lightbox: Create LightboxIface and Implement in actors
    Use an actor inheriting from St.Bin for a simple light-box with no radial
    effect, while inherit from RadialShaderQuad instead.
    
    Instead of using a factory to create the proper class depending on parameters
    and hardware support, just use two different fake-classes since we only care
    about one parameter, and there are only two possible outputs.
    
    So add Lightbox var that always create a SimpleLightBox, and RadialLightbox that
    tries to use a GLSL radial effect if the hardware supports it, othwerwise it
    just fallbacks to a LightBox.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/559

--- js/ui/lightbox.js
+++ js/ui/lightbox.js
@@ -1,7 +1,6 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 
 const { Clutter, GObject, Shell, St } = imports.gi;
-const Signals = imports.signals;
 
 const Params = imports.misc.params;
 const Tweener = imports.ui.tweener;
@@ -61,68 +60,29 @@ class RadialShaderQuad extends Shell.GLSLQuad {
     }
 });
 
-/**
- * Lightbox:
- * @container: parent Clutter.Container
- * @params: (optional) additional parameters:
- *           - inhibitEvents: whether to inhibit events for @container
- *           - width: shade actor width
- *           - height: shade actor height
- *           - fadeInTime: seconds used to fade in
- *           - fadeOutTime: seconds used to fade out
- *
- * Lightbox creates a dark translucent "shade" actor to hide the
- * contents of @container, and allows you to specify particular actors
- * in @container to highlight by bringing them above the shade. It
- * tracks added and removed actors in @container while the lightboxing
- * is active, and ensures that all actors are returned to their
- * original stacking order when the lightboxing is removed. (However,
- * if actors are restacked by outside code while the lightboxing is
- * active, the lightbox may later revert them back to their original
- * order.)
- *
- * By default, the shade window will have the height and width of
- * @container and will track any changes in its size. You can override
- * this by passing an explicit width and height in @params.
- */
-var Lightbox = class Lightbox {
-    constructor(container, params) {
-        params = Params.parse(params, { inhibitEvents: false,
-                                        width: null,
-                                        height: null,
-                                        fadeFactor: DEFAULT_FADE_FACTOR,
-                                        radialEffect: false,
-                                      });
-
+var LightboxIface = GObject.registerClass({
+    Requires: [Clutter.Actor],
+    Signals: { 'shown': {} }
+}, class LightboxIface extends GObject.Interface {
+    _init(container, params) {
         this._container = container;
         this._children = container.get_children();
         this._fadeFactor = params.fadeFactor;
-        this._radialEffect = Clutter.feature_available(Clutter.FeatureFlags.SHADERS_GLSL) && params.radialEffect;
-        if (this._radialEffect)
-            this.actor = new RadialShaderQuad({ x: 0,
-                                                y: 0,
-                                                reactive: params.inhibitEvents });
-        else
-            this.actor = new St.Bin({ x: 0,
-                                      y: 0,
-                                      opacity: 0,
-                                      style_class: 'lightbox',
-                                      reactive: params.inhibitEvents });
+        this.visible = false;
 
-        container.add_actor(this.actor);
-        this.actor.raise_top();
-        this.actor.hide();
+        container.add_actor(this);
+        this.raise_top();
         this.shown = false;
 
-        this.actor.connect('destroy', this._onDestroy.bind(this));
+        this.connect('destroy', this._onDestroy.bind(this));
 
         if (params.width && params.height) {
-            this.actor.width = params.width;
-            this.actor.height = params.height;
+            this.width = params.width;
+            this.height = params.height;
         } else {
             let constraint = new Clutter.BindConstraint({ source: container,
                                                           coordinate: Clutter.BindCoordinate.ALL });
-            this.actor.add_constraint(constraint);
+            this.add_constraint(constraint);
         }
 
         this._actorAddedSignalId = container.connect('actor-added', this._actorAdded.bind(this));
@@ -133,14 +93,14 @@ var Lightbox = class Lightbox {
 
     _actorAdded(container, newChild) {
         let children = this._container.get_children();
-        let myIndex = children.indexOf(this.actor);
+        let myIndex = children.indexOf(this);
         let newChildIndex = children.indexOf(newChild);
 
         if (newChildIndex > myIndex) {
             // The child was added above the shade (presumably it was
             // made the new top-most child). Move it below the shade,
             // and add it to this._children as the new topmost actor.
-            newChild.lower(this.actor);
+            newChild.lower(this);
             this._children.push(newChild);
         } else if (newChildIndex == 0) {
             // Bottom of stack
@@ -153,62 +113,11 @@ var Lightbox = class Lightbox {
         }
     }
 
-    show(fadeInTime) {
-        fadeInTime = fadeInTime || 0;
+    setShown(shown) {
+        this.shown = shown;
 
-        Tweener.removeTweens(this.actor);
-        if (this._radialEffect) {
-            Tweener.addTween(this.actor,
-                             { brightness: VIGNETTE_BRIGHTNESS,
-                               vignetteSharpness: VIGNETTE_SHARPNESS,
-                               time: fadeInTime,
-                               transition: 'easeOutQuad',
-                               onComplete: () => {
-                                   this.shown = true;
-                                   this.emit('shown');
-                               }
-                             });
-        } else {
-            Tweener.addTween(this.actor,
-                             { opacity: 255 * this._fadeFactor,
-                               time: fadeInTime,
-                               transition: 'easeOutQuad',
-                               onComplete: () => {
-                                   this.shown = true;
-                                   this.emit('shown');
-                               }
-                             });
-        }
-
-        this.actor.show();
-    }
-
-    hide(fadeOutTime) {
-        fadeOutTime = fadeOutTime || 0;
-
-        this.shown = false;
-        Tweener.removeTweens(this.actor);
-        if (this._radialEffect) {
-            Tweener.addTween(this.actor,
-                             { brightness: 1.0,
-                               vignetteSharpness: 0.0,
-                               opacity: 0,
-                               time: fadeOutTime,
-                               transition: 'easeOutQuad',
-                               onComplete: () => {
-                                   this.actor.hide();
-                               }
-                             });
-        } else {
-            Tweener.addTween(this.actor,
-                             { opacity: 0,
-                               time: fadeOutTime,
-                               transition: 'easeOutQuad',
-                               onComplete: () => {
-                                   this.actor.hide();
-                               }
-                             });
-        }
+        if (shown)
+            this.emit('shown');
     }
 
     _actorRemoved(container, child) {
@@ -238,7 +147,7 @@ var Lightbox = class Lightbox {
         // case we may need to indicate some *other* actor as the new
         // sibling of the to-be-lowered one.
 
-        let below = this.actor;
+        let below = this;
         for (let i = this._children.length - 1; i >= 0; i--) {
             if (this._children[i] == window)
                 this._children[i].raise_top();
@@ -249,15 +158,6 @@ var Lightbox = class Lightbox {
         }
 
         this._highlighted = window;
-    }
-
-    /**
-     * destroy:
-     *
-     * Destroys the lightbox.
-     */
-    destroy() {
-        this.actor.destroy();
     }
 
     /**
@@ -272,5 +172,159 @@ var Lightbox = class Lightbox {
 
         this.highlight(null);
     }
+});
+
+var SimpleLightBox = GObject.registerClass({
+    Implements: [LightboxIface]
+}, class SimpleLightBox extends St.Bin {
+    _init(container, params) {
+        super._init({
+            x: 0, y: 0,
+            opacity: 0,
+            style_class: 'lightbox',
+            reactive: params.inhibitEvents
+        });
+        LightboxIface._init(this, container, params);
+    }
+
+    show(fadeInTime) {
+        fadeInTime = fadeInTime || 0;
+
+        Tweener.removeTweens(this);
+        Tweener.addTween(this, {
+            opacity: 255 * this._fadeFactor,
+            time: fadeInTime,
+            transition: 'easeOutQuad',
+            onComplete: () => LightboxIface.setShown(this, true)
+        });
+        super.show();
+    }
+
+    hide(fadeOutTime) {
+        fadeOutTime = fadeOutTime || 0;
+
+        LightboxIface.setShown(this, false);
+        Tweener.removeTweens(this);
+        Tweener.addTween(this, {
+            opacity: 0,
+            time: fadeOutTime,
+            transition: 'easeOutQuad',
+            onComplete: () => super.hide()
+        });
+    }
+});
+
+var RadialShaderLightBox = GObject.registerClass({
+    Implements: [LightboxIface]
+}, class RadialShaderLightBox extends RadialShaderQuad {
+    _init(container, params) {
+        super._init({
+            x: 0, y: 0,
+            reactive: params.inhibitEvents
+        });
+        LightboxIface._init(this, container, params);
+    }
+
+    show(fadeInTime) {
+        fadeInTime = fadeInTime || 0;
+
+        Tweener.removeTweens(this);
+        Tweener.addTween(this, {
+            brightness: VIGNETTE_BRIGHTNESS,
+            vignetteSharpness: VIGNETTE_SHARPNESS,
+            time: fadeInTime,
+            transition: 'easeOutQuad',
+            onComplete: () => LightboxIface.setShown(this, true)
+        });
+        super.show();
+    }
+
+    hide(fadeOutTime) {
+        fadeOutTime = fadeOutTime || 0;
+        LightboxIface.setShown(this, false);
+        Tweener.removeTweens(this);
+        Tweener.addTween(this, {
+            brightness: 1.0,
+            vignetteSharpness: 0.0,
+            opacity: 0,
+            time: fadeOutTime,
+            transition: 'easeOutQuad',
+            onComplete: () => super.hide()
+        });
+    }
+});
+
+var LightboxClass = function(params) {
+    params = Params.parse(params, {
+        inhibitEvents: false,
+        width: null,
+        height: null,
+        fadeFactor: DEFAULT_FADE_FACTOR,
+        radialEffect: false,
+    });
+
+    if (params.radialEffect &&
+        Clutter.feature_available(Clutter.FeatureFlags.SHADERS_GLSL))
+        return RadialShaderLightBox;
+
+    return SimpleLightBox;
 };
-Signals.addSignalMethods(Lightbox.prototype);
+
+/**
+ * Lightbox:
+ * @container: parent Clutter.Container
+ * @params: (optional) additional parameters:
+ *           - inhibitEvents: whether to inhibit events for @container
+ *           - width: shade actor width
+ *           - height: shade actor height
+ *           - fadeInTime: seconds used to fade in
+ *           - fadeOutTime: seconds used to fade out
+ *
+ * Lightbox creates a dark translucent "shade" actor to hide the
+ * contents of @container, and allows you to specify particular actors
+ * in @container to highlight by bringing them above the shade. It
+ * tracks added and removed actors in @container while the lightboxing
+ * is active, and ensures that all actors are returned to their
+ * original stacking order when the lightboxing is removed. (However,
+ * if actors are restacked by outside code while the lightboxing is
+ * active, the lightbox may later revert them back to their original
+ * order.)
+ *
+ * By default, the shade window will have the height and width of
+ * @container and will track any changes in its size. You can override
+ * this by passing an explicit width and height in @params.
+ */
+var Lightbox = function() {
+    return LightboxClass();
+}();
+
+/**
+ * RadialLightbox:
+ * @container: parent Clutter.Container
+ * @params: (optional) additional parameters:
+ *           - inhibitEvents: whether to inhibit events for @container
+ *           - width: shade actor width
+ *           - height: shade actor height
+ *           - fadeInTime: seconds used to fade in
+ *           - fadeOutTime: seconds used to fade out
+ *
+ * RadialLightbox creates a dark translucent "shade" actor to hide the
+ * contents of @container, and allows you to specify particular actors
+ * in @container to highlight by bringing them above the shade. It
+ * tracks added and removed actors in @container while the lightboxing
+ * is active, and ensures that all actors are returned to their
+ * original stacking order when the lightboxing is removed. (However,
+ * if actors are restacked by outside code while the lightboxing is
+ * active, the lightbox may later revert them back to their original
+ * order.)
+ *
+ * By default, the shade window will have the height and width of
+ * @container and will track any changes in its size. You can override
+ * this by passing an explicit width and height in @params.
+ *
+ * If GLSL shaders are supported, a radial effect will be applied,
+ * otherwise we fallback to a simple #Lightbox actor.
+ */
+var RadialLightbox = function () {
+    return LightboxClass({ radialEffect: true });
+}();
