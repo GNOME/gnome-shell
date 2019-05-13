b057788937fcc27d7d787de2390ebae22294e4f8
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-05-13 23:32:31 +0200

    messageTray: Inherit Notification, Source and NotificationPolicy from GObject
    Register notifications, sources and policies as GObject gtypes so that they can
    be passed in signals and use native properties and signals.
    
    Reimplement all the extending classes.
    
    This may be reverted when gjs will support passing Javascript objects
    natively.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/559

--- js/ui/main.js
+++ js/ui/main.js
@@ -387,7 +387,7 @@ function notify(msg, details) {
     messageTray.add(source);
     let notification = new MessageTray.Notification(source, msg, details);
     notification.setTransient(true);
-    source.notify(notification);
+    source.addNotification(notification);
 }
 
 /**
