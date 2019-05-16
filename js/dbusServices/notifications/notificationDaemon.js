9f83a4bda70ccb2c24334fbf0b015a34cb461191
Florian Müllner <fmuellner@gnome.org>
2019-05-16 21:08:06 +0200

    dbusServices/notifications: Add a separate notification daemon
    Add a small service that exposes the Fdo notification API under the
    well-known name, and forwards any requests to the actual implementation
    in the shell.
    
    That way any app with permission to talk to org.freedesktop.Notifications
    will get exactly that, and nothing more.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/547

---
+++ js/dbusServices/notifications/notificationDaemon.js
@@ -0,0 +1,80 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
+/* exported NotificationDaemon */
+
+const { Gio, GLib } = imports.gi;
+
+const { loadInterfaceXML } = imports.misc.fileUtils;
+const { ServiceImplementation } = imports.dbusService;
+
+const NotificationsIface = loadInterfaceXML('org.freedesktop.Notifications');
+const NotificationsProxy = Gio.DBusProxy.makeProxyWrapper(NotificationsIface);
+
+var NotificationDaemon = class extends ServiceImplementation {
+    constructor() {
+        super(NotificationsIface, '/org/freedesktop/Notifications');
+
+        this._autoShutdown = false;
+
+        this._proxy = new NotificationsProxy(Gio.DBus.session,
+            'org.gnome.Shell',
+            '/org/freedesktop/Notifications',
+            (proxy, error) => {
+                if (error)
+                    log(error.message);
+            });
+
+        this._proxy.connectSignal('ActionInvoked',
+            (proxy, sender, params) => {
+                this._dbusImpl.emit_signal('ActionInvoked',
+                    new GLib.Variant('(us)', params));
+            });
+        this._proxy.connectSignal('NotificationClosed',
+            (proxy, sender, params) => {
+                this._dbusImpl.emit_signal('NotificationClosed',
+                    new GLib.Variant('(uu)', params));
+            });
+    }
+
+    register() {
+        Gio.DBus.session.own_name(
+            'org.freedesktop.Notifications',
+            Gio.BusNameOwnerFlags.REPLACE,
+            null, null);
+    }
+
+    NotifyAsync(params, invocation) {
+        this._proxy.NotifyRemote(...params, (res, error) => {
+            if (this._handleError(invocation, error))
+                return;
+
+            invocation.return_value(new GLib.Variant('(u)', res));
+        });
+    }
+
+    CloseNotificationAsync(params, invocation) {
+        this._proxy.CloseNotificationRemote(...params, (res, error) => {
+            if (this._handleError(invocation, error))
+                return;
+
+            invocation.return_value(null);
+        });
+    }
+
+    GetCapabilitiesAsync(params, invocation) {
+        this._proxy.GetCapabilitiesRemote(...params, (res, error) => {
+            if (this._handleError(invocation, error))
+                return;
+
+            invocation.return_value(new GLib.Variant('(as)', res));
+        });
+    }
+
+    GetServerInformationAsync(params, invocation) {
+        this._proxy.GetServerInformationRemote(...params, (res, error) => {
+            if (this._handleError(invocation, error))
+                return;
+
+            invocation.return_value(new GLib.Variant('(ssss)', res));
+        });
+    }
+};
