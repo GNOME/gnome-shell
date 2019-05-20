dc3dcdce4a00e8718eb73df16d1bf6c249b35d77
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-05-20 23:27:48 +0200

    lookingGlass: Throw a clearer error on referencing invalid result index
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/559

--- js/ui/lookingGlass.js
+++ js/ui/lookingGlass.js
@@ -1001,7 +1001,11 @@ var LookingGlass = class LookingGlass {
     }
 
     getResult(idx) {
-        return this._results[idx - this._offset].o;
+        try {
+            return this._results[idx - this._offset].o;
+        } catch (e) {
+            throw new Error(`Unknown result at index ${idx}`);
+        }
     }
 
     toggle() {
