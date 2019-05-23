570b7f7ed484482cc92ed8d856792c7bb7a7d23e
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-05-23 06:12:56 +0200

    realmd: Verify that loginFormat is defined
    Otherwise when checking an undefined property we would get a warning instead
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/559

--- js/gdm/realmd.js
+++ js/gdm/realmd.js
@@ -79,7 +79,9 @@ var Manager = class {
             }
         }
 
-        if (this._loginFormat != newLoginFormat) {
+        if (this._loginFormat !== undefined &&
+            newLoginFormat !== undefined &&
+            this._loginFormat != newLoginFormat) {
             this._loginFormat = newLoginFormat;
             this.emit('login-format-changed', newLoginFormat);
         }
