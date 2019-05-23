79d57b308e8ccbcd190202bfeed33d02a39564e3
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-05-22 20:16:22 -0500

    Crossharis: Bind visibility property to clones
    No need to override the show/hide methods as we can just bind the property
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/559

--- js/ui/magnifier.js
+++ js/ui/magnifier.js
@@ -1656,8 +1656,11 @@ class Crosshairs extends Clutter.Actor {
                 if (this.get_parent() != null) {
                     crosshairsActor = new Clutter.Clone({ source: this });
                     this._clones.push(crosshairsActor);
+
+                    // Clones don't share visibility.
+                    this.bind_property('visible', crosshairsActor, 'visible',
+                                       GObject.BindingFlags.SYNC_CREATE);
                 }
-                crosshairsActor.visible = this.visible;
 
                 container.add_actor(crosshairsActor);
                 container.raise_child(magnifiedMouse, crosshairsActor);
@@ -1786,28 +1789,6 @@ class Crosshairs extends Clutter.Actor {
             this.reCenter();
         }
      }
-
-    /**
-     * show:
-     * Show the crosshairs.
-     */
-    vfunc_show() {
-        super.vfunc_show();
-        // Clones don't share visibility.
-        for (let i = 0; i < this._clones.length; i++)
-            this._clones[i].show();
-    }
-
-    /**
-     * hide:
-     * Hide the crosshairs.
-     */
-    vfunc_hide() {
-        super.vfunc_hide();
-        // Clones don't share visibility.
-        for (let i = 0; i < this._clones.length; i++)
-            this._clones[i].hide();
-    }
 
     /**
      * reCenter:
