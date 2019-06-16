21de131c92c21d81f51340b805304a2afc9c973c
Jonas Dreßler <verdre@v0yd.nl>
2019-06-16 16:28:29 +0200

    js: Update gestures for updated gesture tracker
    With the new gesture tracker, we no longer need the grab-op-begin signal
    handlers since the gesture tracker automatically takes care of this in
    X11 sessions.
    
    Also the gesture trigger edge property has an updated name now because
    it has been moved into it's own class, so use that name.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/596

--- js/ui/viewSelector.js
+++ js/ui/viewSelector.js
@@ -68,13 +68,9 @@ var ShowOverviewAction = GObject.registerClass({
     _init() {
         super._init();
         this.set_n_touch_points(3);
-
-        global.display.connect('grab-op-begin', () => {
-            this.cancel();
-        });
     }
 
-    vfunc_gesture_prepare(actor) {
+    vfunc_gesture_prepare(actor, point) {
         return Main.actionMode == Shell.ActionMode.NORMAL &&
                this.get_n_current_points() == this.get_n_touch_points();
     }
@@ -108,12 +104,12 @@ var ShowOverviewAction = GObject.registerClass({
                                     height: maxY - minY });
     }
 
-    vfunc_gesture_begin(actor) {
+    vfunc_gesture_begin(actor, point) {
         this._initialRect = this._getBoundingRect(false);
         return true;
     }
 
-    vfunc_gesture_end(actor) {
+    vfunc_gesture_end(actor, point) {
         let rect = this._getBoundingRect(true);
         let oldArea = this._initialRect.width * this._initialRect.height;
         let newArea = rect.width * rect.height;
