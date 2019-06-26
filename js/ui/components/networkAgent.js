34e9513e1fd4457a202d4174e98510e302456d0f
Allan Day <allanpday@gmail.com>
2019-06-26 14:52:09 +0100

    adjust UI to new text styles
    With the new text styles, some UI needs to adjusted, primarily to
    accommodate larger heading text.

--- js/ui/components/networkAgent.js
+++ js/ui/components/networkAgent.js
@@ -310,7 +310,7 @@ class NetworkSecretDialog extends ModalDialog.ModalDialog {
         case '802-11-wireless':
             wirelessSetting = this._connection.get_setting_wireless();
             ssid = NM.utils_ssid_to_utf8(wirelessSetting.get_ssid().get_data());
-            content.title = _("Authentication required by wireless network");
+            content.title = _("Authentication required");
             content.message = _("Passwords or encryption keys are required to access the wireless network “%s”.").format(ssid);
             this._getWirelessSecrets(content.secrets, wirelessSetting);
             break;
@@ -338,7 +338,7 @@ class NetworkSecretDialog extends ModalDialog.ModalDialog {
             // fall through
         case 'cdma':
         case 'bluetooth':
-            content.title = _("Mobile broadband network password");
+            content.title = _("Mobile broadband password");
             content.message = _("A password is required to connect to “%s”.").format(connectionSetting.get_id());
             this._getMobileSecrets(content.secrets, connectionType);
             break;
@@ -683,7 +683,7 @@ var NetworkAgent = class {
         case '802-11-wireless':
             let wirelessSetting = connection.get_setting_wireless();
             let ssid = NM.utils_ssid_to_utf8(wirelessSetting.get_ssid().get_data());
-            title = _("Authentication required by wireless network");
+            title = _("Authentication required");
             body = _("Passwords or encryption keys are required to access the wireless network “%s”.").format(ssid);
             break;
         case '802-3-ethernet':
