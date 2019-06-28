8a89e43eaafccd887151cdb6392263b6b057c599
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2019-06-28 19:44:20 -0300

    appDisplay: Add event blocker inhibition API
    The event blocker is an actor that is added in between the
    icon grid and the app folder popup in order to guarantee
    that clicking outside the app folder will collapse it.
    
    However, the next patch will require allowing dragging events
    to be passed to folder icons, and the event blocker gets in
    our way here, preventing drag n' drop to work properly.
    
    Add an API to inhibit the event blocker. This API will be
    used by the app folders while an item is dragged.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/603

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -318,6 +318,8 @@ var AllView = class AllView extends BaseAppView {
         this._folderSettings.connect('changed::folder-children', () => {
             Main.queueDeferredWork(this._redisplayWorkId);
         });
+
+        this._nEventBlockerInhibits = 0;
     }
 
     removeAll() {
@@ -630,6 +632,16 @@ var AllView = class AllView extends BaseAppView {
         // Update folder views
         for (let i = 0; i < this.folderIcons.length; i++)
             this.folderIcons[i].adaptToSize(availWidth, availHeight);
+    }
+
+    inhibitEventBlocker() {
+        this._nEventBlockerInhibits++;
+        this._eventBlocker.visible = this._nEventBlockerInhibits == 0;
+    }
+
+    uninhibitEventBlocker() {
+        this._nEventBlockerInhibits--;
+        this._eventBlocker.visible = this._nEventBlockerInhibits == 0;
     }
 };
 Signals.addSignalMethods(AllView.prototype);
