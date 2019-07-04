GNOME/gnome-shell

commit d5a1a888d91b9456632a8393cf14c443e6df6a5b

st: Replace global grabs with input device grabs
Global grabs are being removed from Clutter in favour of input device
grabs, so only grab the input device instead and replace priv->grabbed
with checks for priv->grab_device.

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/495

src/st/st-button.c
@@ -192,7 +192,7 @@ st_button_button_press (ClutterActor       *actor,
   if (priv->button_mask & mask)
     {
       if (priv->grabbed == 0)
-        clutter_grab_pointer (actor);
+        clutter_input_device_grab (device, actor);
 
       priv->grabbed |= mask;
       st_button_press (button, device, mask, NULL);
@@ -221,7 +221,7 @@ st_button_button_release (ClutterActor       *actor,
 
       priv->grabbed &= ~mask;
       if (priv->grabbed == 0)
-        clutter_ungrab_pointer ();
+        clutter_input_device_ungrab (device);
 
       return TRUE;
     }
@@ -787,7 +787,7 @@ st_button_fake_release (StButton *button)
   if (priv->grabbed)
     {
       priv->grabbed = 0;
-      clutter_ungrab_pointer ();
+      clutter_input_device_ungrab (priv->device);
     }
 
   priv->device = NULL;

src/st/st-scroll-bar.c
@@ -49,9 +49,10 @@ struct _StScrollBarPrivate
 {
   StAdjustment *adjustment;
 
-  gboolean      grabbed;
   gfloat        x_origin;
   gfloat        y_origin;
+
+  ClutterInputDevice *grab_device;
 
   ClutterActor *trough;
   ClutterActor *handle;
@@ -601,13 +602,13 @@ static void
 stop_scrolling (StScrollBar *bar)
 {
   StScrollBarPrivate *priv = st_scroll_bar_get_instance_private (bar);
-  if (!priv->grabbed)
+  if (!priv->grab_device)
     return;
 
   st_widget_remove_style_pseudo_class (ST_WIDGET (priv->handle), "active");
 
-  clutter_ungrab_pointer ();
-  priv->grabbed = FALSE;
+  clutter_input_device_ungrab (priv->grab_device);
+  priv->grab_device = NULL;
   g_signal_emit (bar, signals[SCROLL_STOP], 0);
 }
 
@@ -617,7 +618,7 @@ handle_motion_event_cb (ClutterActor       *trough,
                         StScrollBar        *bar)
 {
   StScrollBarPrivate *priv = st_scroll_bar_get_instance_private (bar);
-  if (!priv->grabbed)
+  if (!priv->grab_device)
     return FALSE;
 
   move_slider (bar, event->x, event->y);
@@ -642,6 +643,7 @@ handle_button_press_event_cb (ClutterActor       *actor,
                               StScrollBar        *bar)
 {
   StScrollBarPrivate *priv = st_scroll_bar_get_instance_private (bar);
+  ClutterInputDevice *device = clutter_event_get_device ((ClutterEvent*) event);
 
   if (event->button != 1)
     return FALSE;
@@ -659,10 +661,10 @@ handle_button_press_event_cb (ClutterActor       *actor,
   priv->x_origin += clutter_actor_get_x (priv->trough);
   priv->y_origin += clutter_actor_get_y (priv->trough);
 
-  g_assert (!priv->grabbed);
+  g_assert (!priv->grab_device);
 
-  clutter_grab_pointer (priv->handle);
-  priv->grabbed = TRUE;
+  clutter_input_device_grab (device, priv->handle);
+  priv->grab_device = device;
   g_signal_emit (bar, signals[SCROLL_START], 0);
 
   return TRUE;

commit 218ae84a58f9b6ab27d358b48419f9cec87e3022

st/button: Ungrab device before calling st_button_release
Since priv->device gets set to NULL inside st_button_release, ungrab the
input device before calling st_button_release and avoid
clutter_input_device_ungrab failing with a critical error.

This fixes a regression introduced with
d5a1a888d91b9456632a8393cf14c443e6df6a5b

src/st/st-button.c
@@ -780,15 +780,15 @@ st_button_fake_release (StButton *button)
                                             priv->press_sequence);
     }
 
-  if (priv->pressed || priv->press_sequence)
-    st_button_release (button, priv->device,
-                       priv->pressed, 0, NULL);
-
   if (priv->grabbed)
     {
       priv->grabbed = 0;
       clutter_input_device_ungrab (priv->device);
     }
+
+  if (priv->pressed || priv->press_sequence)
+    st_button_release (button, priv->device,
+                       priv->pressed, 0, NULL);
 
   priv->device = NULL;
 }