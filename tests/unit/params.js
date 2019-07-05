GNOME/gnome-shell

commit 46874eed05b9bb9e1ad8a5eee469c1f6eacb323f

params: Simplify code
Standard javascript now has Object.assign() which is very similar to
Params.parse(), except that the latter by default disallows "extra"
parameters. We can still leverage the standard API by simply
implementing the error check, and then call out to Object.assign()
for the actual parameter merging.

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/612

js/misc/params.js
@@ -14,22 +14,12 @@
 //
 // Return value: a new object, containing the merged parameters from
 // @params and @defaults
-function parse(params, defaults, allowExtras) {
-    let ret = {}, prop;
-
-    if (!params)
-        params = {};
-
-    for (prop in params) {
-        if (!(prop in defaults) && !allowExtras)
-            throw new Error('Unrecognized parameter "' + prop + '"');
-        ret[prop] = params[prop];
+function parse(params = {}, defaults, allowExtras) {
+    if (!allowExtras) {
+        for (let prop in params)
+            if (!(prop in defaults))
+                throw new Error(`Unrecognized parameter "${prop}"`);
     }
 
-    for (prop in defaults) {
-        if (!(prop in params))
-            ret[prop] = defaults[prop];
-    }
-
-    return ret;
+    return Object.assign(defaults, params);
 }

commit 1778adae0d029a53e35c219098def8382040711c

tests: Add Params.parse() unit tests
Commit 46874eed0 accidentally changed the behavior of the function in
an incompatible way. Before addressing the actual issue, add a reproducer
to the unit tests to hopefully prevent future breakage.

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/615

tests/meson.build
@@ -10,7 +10,7 @@ run_test = configure_file(
 testenv = environment()
 testenv.set('GSETTINGS_SCHEMA_DIR', join_paths(meson.build_root(), 'data'))
 
-foreach test : ['insertSorted', 'jsParse', 'markup', 'url']
+foreach test : ['insertSorted', 'jsParse', 'markup', 'params', 'url']
   test(test, run_test,
     args: 'unit/@0@.js'.format(test),
     env: testenv,

tests/unit/params.js
@@ -1,0 +1,32 @@
+const JsUnit = imports.jsUnit;
+const Params = imports.misc.params;
+
+function assertParamsEqual(params, expected) {
+    for (let p in params) {
+        JsUnit.assertTrue(p in expected);
+        JsUnit.assertEquals(params[p], expected[p]);
+    }
+}
+
+let defaults = {
+    foo: 'This is a test',
+    bar: null,
+    baz: 42
+};
+
+assertParamsEqual(
+    Params.parse(null, defaults),
+    defaults);
+
+assertParamsEqual(
+    Params.parse({ bar: 23 }, defaults),
+    { foo: 'This is a test', bar: 23, baz: 42 });
+
+JsUnit.assertRaises(
+    () => {
+        Params.parse({ extraArg: 'quz' }, defaults);
+    });
+
+assertParamsEqual(
+    Params.parse({ extraArg: 'quz' }, defaults, true),
+    { foo: 'This is a test', bar: null, baz: 42, extraArg: 'quz' });