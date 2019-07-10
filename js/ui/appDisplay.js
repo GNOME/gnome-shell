GNOME/gnome-shell

commit f6b4b96737d36a138de4b4350ef59df4a944e379

cleanup: Use Array.includes() to check for element existence
We can use that newer method where we don't care about the actual position
of an element inside the array.

(Array.includes() and Array.indexOf() do behave differently in edge cases,
for example in the handling of NaN, but those don't matter to us)

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/152

js/ui/appDisplay.js
@@ -69,7 +69,7 @@ function _getCategories(info) {
 
 function _listsIntersect(a, b) {
     for (let itemA of a)
-        if (b.indexOf(itemA) >= 0)
+        if (b.includes(itemA))
             return true;
     return false;
 }
@@ -1165,7 +1165,7 @@ var FolderIcon = class FolderIcon {
         let excludedApps = this._folder.get_strv('excluded-apps');
         let appSys = Shell.AppSystem.get_default();
         let addAppId = appId => {
-            if (excludedApps.indexOf(appId) >= 0)
+            if (excludedApps.includes(appId))
                 return;
 
             let app = appSys.lookup_app(appId);
@@ -1728,7 +1728,7 @@ var AppIconMenu = class AppIconMenu extends PopupMenu.PopupMenu {
             let appInfo = this._source.app.get_app_info();
             let actions = appInfo.list_actions();
             if (this._source.app.can_open_new_window() &&
-                actions.indexOf('new-window') == -1) {
+                actions.includes('new-window')) {
                 this._newWindowMenuItem = this._appendMenuItem(_("New Window"));
                 this._newWindowMenuItem.connect('activate', () => {
                     if (this._source.app.state == Shell.AppState.STOPPED)
@@ -1742,7 +1742,7 @@ var AppIconMenu = class AppIconMenu extends PopupMenu.PopupMenu {
 
             if (discreteGpuAvailable &&
                 this._source.app.state == Shell.AppState.STOPPED &&
-                actions.indexOf('activate-discrete-gpu') == -1) {
+                actions.includes('activate-discrete-gpu')) {
                 this._onDiscreteGpuMenuItem = this._appendMenuItem(_("Launch using Dedicated Graphics Card"));
                 this._onDiscreteGpuMenuItem.connect('activate', () => {
                     if (this._source.app.state == Shell.AppState.STOPPED)

js/extensionPrefs/main.js
@@ -582,12 +582,12 @@ class ExtensionRow extends Gtk.ListBoxRow {
 
     _isEnabled() {
         let extensions = this._settings.get_strv('enabled-extensions');
-        return extensions.indexOf(this.uuid) != -1;
+        return extensions.includes(this.uuid);
     }
 
     _enable() {
         let extensions = this._settings.get_strv('enabled-extensions');
-        if (extensions.indexOf(this.uuid) != -1)
+        if (extensions.includes(this.uuid))
             return;
 
         extensions.push(this.uuid);

js/misc/extensionUtils.js
@@ -31,7 +31,7 @@ function getCurrentExtension() {
     // Search for an occurrence of an extension stack frame
     // Start at 1 because 0 is the stack frame of this function
     for (let i = 1; i < stack.length; i++) {
-        if (stack[i].indexOf('/gnome-shell/extensions/') > -1) {
+        if (stack[i].includes('/gnome-shell/extensions/')) {
             extensionStackLine = stack[i];
             break;
         }

js/misc/keyboardManager.js
@@ -125,7 +125,7 @@ var KeyboardManager = class {
 
     _getLocaleLayout() {
         let locale = GLib.get_language_names()[0];
-        if (locale.indexOf('_') == -1)
+        if (!locale.includes('_'))
             locale = DEFAULT_LOCALE;
 
         let [found, , id] = GnomeDesktop.get_input_source_from_locale(locale);

js/ui/calendar.js
@@ -38,7 +38,7 @@ function isToday(date) {
 function _isWorkDay(date) {
     /* Translators: Enter 0-6 (Sunday-Saturday) for non-work days. Examples: "0" (Sunday) "6" (Saturday) "06" (Sunday and Saturday). */
     let days = C_('calendar-no-work', "06");
-    return days.indexOf(date.getDay().toString()) == -1;
+    return !days.includes(date.getDay().toString());
 }
 
 function _getBeginningOfDay(date) {

js/ui/components/__init__.js
@@ -13,13 +13,13 @@ var ComponentManager = class {
         let newEnabledComponents = Main.sessionMode.components;
 
         newEnabledComponents.filter(
-            name => this._enabledComponents.indexOf(name) == -1
+            name => !this._enabledComponents.includes(name)
         ).forEach(name => {
             this._enableComponent(name);
         });
 
         this._enabledComponents.filter(
-            name => newEnabledComponents.indexOf(name) == -1
+            name => !newEnabledComponents.includes(name)
         ).forEach(name => {
             this._disableComponent(name);
         });

js/ui/components/autorunManager.js
@@ -40,7 +40,7 @@ function isMountRootHidden(root) {
     let path = root.get_path();
 
     // skip any mounts in hidden directory hierarchies
-    return (path.indexOf('/.') != -1);
+    return (path.includes('/.'));
 }
 
 function isMountNonLocal(mount) {
@@ -192,15 +192,15 @@ var AutorunDispatcher = class {
 
     _getAutorunSettingForType(contentType) {
         let runApp = this._settings.get_strv(SETTING_START_APP);
-        if (runApp.indexOf(contentType) != -1)
+        if (runApp.includes(contentType))
             return AutorunSetting.RUN;
 
         let ignore = this._settings.get_strv(SETTING_IGNORE);
-        if (ignore.indexOf(contentType) != -1)
+        if (ignore.includes(contentType))
             return AutorunSetting.IGNORE;
 
         let openFiles = this._settings.get_strv(SETTING_OPEN_FOLDER);
-        if (openFiles.indexOf(contentType) != -1)
+        if (openFiles.includes(contentType))
             return AutorunSetting.FILES;
 
         return AutorunSetting.ASK;

js/ui/components/networkAgent.js
@@ -327,7 +327,7 @@ class NetworkSecretDialog extends ModalDialog.ModalDialog {
             this._getPPPoESecrets(content.secrets);
             break;
         case 'gsm':
-            if (this._hints.indexOf('pin') != -1) {
+            if (this._hints.includes('pin')) {
                 let gsmSetting = this._connection.get_setting_gsm();
                 content.title = _("PIN code required");
                 content.message = _("PIN code is needed for the mobile broadband device");
@@ -695,7 +695,7 @@ var NetworkAgent = class {
             body = _("A password is required to connect to “%s”.".format(connection.get_id()));
             break;
         case 'gsm':
-            if (hints.indexOf('pin') != -1) {
+            if (hints.includes('pin')) {
                 let gsmSetting = connection.get_setting_gsm();
                 title = _("PIN code required");
                 body = _("PIN code is needed for the mobile broadband device");

commit 17d2181d6f6cb1580cffecb3f74e77bf9e8a522a

appDisplay: Fix logic error
Commit f6b4b96737d3 accidentally swapped the conditions here from
!includes() to includes().

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/621

js/ui/appDisplay.js
@@ -1713,7 +1713,7 @@ var AppIconMenu = class AppIconMenu extends PopupMenu.PopupMenu {
             let appInfo = this._source.app.get_app_info();
             let actions = appInfo.list_actions();
             if (this._source.app.can_open_new_window() &&
-                actions.includes('new-window')) {
+                !actions.includes('new-window')) {
                 this._newWindowMenuItem = this._appendMenuItem(_("New Window"));
                 this._newWindowMenuItem.connect('activate', () => {
                     if (this._source.app.state == Shell.AppState.STOPPED)
@@ -1727,7 +1727,7 @@ var AppIconMenu = class AppIconMenu extends PopupMenu.PopupMenu {
 
             if (discreteGpuAvailable &&
                 this._source.app.state == Shell.AppState.STOPPED &&
-                actions.includes('activate-discrete-gpu')) {
+                !actions.includes('activate-discrete-gpu')) {
                 this._onDiscreteGpuMenuItem = this._appendMenuItem(_("Launch using Dedicated Graphics Card"));
                 this._onDiscreteGpuMenuItem.connect('activate', () => {
                     if (this._source.app.state == Shell.AppState.STOPPED)