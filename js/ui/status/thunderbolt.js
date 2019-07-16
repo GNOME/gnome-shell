b0827642793e4c0b4256c9dae6f2989d15b73d0d
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-07-16 11:24:13 +0200

    cleanup: Use inheritance for Actor classes instead of composition
    Remove the `this.actor = ...` and `this.actor._delegate = this` patterns in most
    of classes, by inheriting all the actor container classes.
    
    Uses interfaces when needed for making sure that multiple classes will implement
    some required methods or to avoid redefining the same code multiple times.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/559

--- js/ui/status/thunderbolt.js
+++ js/ui/status/thunderbolt.js
@@ -3,7 +3,7 @@
 
 // the following is a modified version of bolt/contrib/js/client.js
 
-const { Gio, GLib, Polkit, Shell } = imports.gi;
+const { Gio, GLib, GObject, Polkit, Shell } = imports.gi;
 const Signals = imports.signals;
 
 const Main = imports.ui.main;
@@ -221,9 +221,11 @@ Signals.addSignalMethods(AuthRobot.prototype);
 
 /* eof client.js  */
 
-var Indicator = class extends PanelMenu.SystemIndicator {
-    constructor() {
-        super();
+var Indicator = GObject.registerClass({
+    GTypeName: 'Thunderbolt_Indicator'
+}, class Indicator extends PanelMenu.SystemIndicator {
+    _init() {
+        super._init();
 
         this._indicator = this._addIndicator();
         this._indicator.icon_name = 'thunderbolt-symbolic';
@@ -334,4 +336,4 @@ var Indicator = class extends PanelMenu.SystemIndicator {
         const body = _("Could not authorize the Thunderbolt device: %s").format(error.message);
         this._notify(title, body);
     }
-};
+});
