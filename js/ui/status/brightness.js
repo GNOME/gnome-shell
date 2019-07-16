6d6b64bbe0b51a1567c97e9fc33bddfcf524693b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-07-16 11:24:13 +0200

    cleanup: Use inheritance for Actor classes instead of composition
    Remove the `this.actor = ...` and `this.actor._delegate = this` patterns in most
    of classes, by inheriting all the actor container classes.
    
    Uses interfaces when needed for making sure that multiple classes will implement
    some required methods or to avoid redefining the same code multiple times.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/559

--- js/ui/status/brightness.js
+++ js/ui/status/brightness.js
@@ -1,7 +1,7 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 /* exported Indicator */
 
-const { Gio, St } = imports.gi;
+const { Gio, GObject, St } = imports.gi;
 
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
@@ -15,9 +15,11 @@ const OBJECT_PATH = '/org/gnome/SettingsDaemon/Power';
 const BrightnessInterface = loadInterfaceXML('org.gnome.SettingsDaemon.Power.Screen');
 const BrightnessProxy = Gio.DBusProxy.makeProxyWrapper(BrightnessInterface);
 
-var Indicator = class extends PanelMenu.SystemIndicator {
-    constructor() {
-        super('display-brightness-symbolic');
+var Indicator = GObject.registerClass({
+    GTypeName: 'Brightness_Indicator'
+}, class Indicator extends PanelMenu.SystemIndicator {
+    _init() {
+        super._init();
         this._proxy = new BrightnessProxy(Gio.DBus.session, BUS_NAME, OBJECT_PATH,
                                           (proxy, error) => {
                                               if (error) {
@@ -60,4 +62,4 @@ var Indicator = class extends PanelMenu.SystemIndicator {
         if (visible)
             this._slider.value = this._proxy.Brightness / 100.0;
     }
-};
+});
