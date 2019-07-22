a621340a9cb97b81030c78028805e552c0642e4f
Ray Strode <rstrode@redhat.com>
2019-07-22 10:57:57 -0400

    appDisplay: add destroy handler for FolderIcon
    It is important that the FolderView of a FolderIcon always
    gets destroyed before the AppFolderPopup, since the view
    may or may not be in the popup, and the view should
    get cleaned up exactly once in either case.
    
    This commit adds a destroy handler on FolderIcon to ensure
    things get taken down in the right order, and to make sure
    the view isn't leaked if it's not yet part of the popup.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/628

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1141,6 +1141,7 @@ var FolderIcon = class FolderIcon {
             this.view.actor.vscroll.adjustment.value = 0;
             this._openSpaceForPopup();
         });
+        this.actor.connect('destroy', this.onDestroy.bind(this));
         this.actor.connect('notify::mapped', () => {
             if (!this.actor.mapped && this._popup)
                 this._popup.popdown();
@@ -1148,6 +1149,13 @@ var FolderIcon = class FolderIcon {
 
         this._folder.connect('changed', this._redisplay.bind(this));
         this._redisplay();
+    }
+
+    onDestroy() {
+        this.view.actor.destroy();
+
+        if (this._popup)
+            this._popup.actor.destroy();
     }
 
     getAppIds() {
@@ -1324,7 +1332,6 @@ var AppFolderPopup = class AppFolderPopup {
 
         global.focus_manager.add_group(this.actor);
 
-        source.actor.connect('destroy', () => this.actor.destroy());
         this._grabHelper = new GrabHelper.GrabHelper(this.actor, {
             actionMode: Shell.ActionMode.POPUP
         });
