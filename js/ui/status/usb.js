0de2ce7435f57b9f69d65d2db5040b0b1a2f0fbd
Ludovico de Nittis <aasonykk@gmail.com>
2019-07-29 08:37:53 +0000

    Apply suggestion to js/ui/status/usb.js

--- js/ui/status/usb.js
+++ js/ui/status/usb.js
@@ -1,6 +1,6 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 
-const Gio = imports.gi.Gio;
+const { Gio } = imports.gi;
 const Lang = imports.lang;
 
 const Main = imports.ui.main;
