b28488ee995ea39847c04fb214ecd80d59e8952b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-07-31 14:10:31 +0200

    lightbox: Use common ease parameters and avoid similar codepaths
    Easing calls on show/hide functions have some parameters in common whether the
    radial effect is enabled or not.
    
    So instead, of doing repeated calls with similar parameters, initialize common
    values in params objects.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/559

--- js/ui/lightbox.js
+++ js/ui/lightbox.js
@@ -176,9 +176,12 @@ var Lightbox = class Lightbox {
     }
 
     lightOn(fadeInTime) {
-        fadeInTime = fadeInTime || 0;
-
         this.actor.remove_all_transitions();
+
+        let easeProps = {
+            duration: fadeInTime || 0,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD
+        };
 
         let onComplete = () => {
             this._active = true;
@@ -187,56 +190,40 @@ var Lightbox = class Lightbox {
 
         if (this._radialEffect) {
             this.actor.ease_property(
-                '@effects.radial.brightness', VIGNETTE_BRIGHTNESS, {
-                    duration: fadeInTime,
-                    mode: Clutter.AnimationMode.EASE_OUT_QUAD
-                });
+                '@effects.radial.brightness', VIGNETTE_BRIGHTNESS, easeProps);
             this.actor.ease_property(
-                '@effects.radial.sharpness', VIGNETTE_SHARPNESS, {
-                    duration: fadeInTime,
-                    mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-                    onComplete
-                });
+                '@effects.radial.sharpness', VIGNETTE_SHARPNESS,
+                Object.assign(easeProps, onComplete));
         } else {
-            this.actor.ease({
+            this.actor.ease(Object.assign(easeProps, {
                 opacity: 255 * this._fadeFactor,
-                duration: fadeInTime,
-                mode: Clutter.AnimationMode.EASE_OUT_QUAD,
                 onComplete
-            });
+            }));
         }
 
         this.actor.show();
     }
 
     lightOff(fadeOutTime) {
-        fadeOutTime = fadeOutTime || 0;
+        this.actor.remove_all_transitions();
 
         this._active = false;
         this.emit('active-changed');
-        this.actor.remove_all_transitions();
+
+        let easeProps = {
+            duration: fadeOutTime || 0,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD
+        };
 
         let onComplete = () => this.actor.hide();
 
         if (this._radialEffect) {
             this.actor.ease_property(
-                '@effects.radial.brightness', 1.0, {
-                    duration: fadeOutTime,
-                    mode: Clutter.AnimationMode.EASE_OUT_QUAD
-                });
+                '@effects.radial.brightness', 1.0, easeProps);
             this.actor.ease_property(
-                '@effects.radial.sharpness', 0.0, {
-                    duration: fadeOutTime,
-                    mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-                    onComplete
-                });
+                '@effects.radial.sharpness', 0.0, Object.assign(easeProps, onComplete));
         } else {
-            this.actor.ease({
-                opacity: 0,
-                duration: fadeOutTime,
-                mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-                onComplete
-            });
+            this.actor.ease(Object.assign(easeProps, { opacity: 0, onComplete }));
         }
     }
 
