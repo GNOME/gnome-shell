GNOME/gnome-shell

commit 8f3554ff3e2478cfd0b349db41f424152518260c

st: Use g_object_notify_by_pspec()
`g_object_notify()` actually takes a global lock to look up the property
by its name, which means there is a performance hit (albeit tiny) every
time this function is called. For this reason, always try to use
`g_object_notify_by_pspec()` instead.

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/652

src/st/st-widget.c
@@ -115,8 +115,12 @@ enum
   PROP_CAN_FOCUS,
   PROP_LABEL_ACTOR,
   PROP_ACCESSIBLE_ROLE,
-  PROP_ACCESSIBLE_NAME
+  PROP_ACCESSIBLE_NAME,
+
+  N_PROPS
 };
+
+static GParamSpec *props[N_PROPS] = { NULL, };
 
 enum
 {
@@ -834,7 +838,6 @@ st_widget_class_init (StWidgetClass *klass)
 {
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
   ClutterActorClass *actor_class = CLUTTER_ACTOR_CLASS (klass);
-  GParamSpec *pspec;
 
   gobject_class->set_property = st_widget_set_property;
   gobject_class->get_property = st_widget_get_property;
@@ -869,25 +872,24 @@ st_widget_class_init (StWidgetClass *klass)
    * The pseudo-class of the actor. Typical values include "hover", "active",
    * "focus".
    */
-  g_object_class_install_property (gobject_class,
-                                   PROP_PSEUDO_CLASS,
-                                   g_param_spec_string ("pseudo-class",
-                                                        "Pseudo Class",
-                                                        "Pseudo class for styling",
-                                                        "",
-                                                        ST_PARAM_READWRITE));
+  props[PROP_PSEUDO_CLASS] =
+    g_param_spec_string ("pseudo-class",
+                         "Pseudo Class",
+                         "Pseudo class for styling",
+                         "",
+                         ST_PARAM_READWRITE);
+
   /**
    * StWidget:style-class:
    *
    * The style-class of the actor for use in styling.
    */
-  g_object_class_install_property (gobject_class,
-                                   PROP_STYLE_CLASS,
-                                   g_param_spec_string ("style-class",
-                                                        "Style Class",
-                                                        "Style class for styling",
-                                                        "",
-                                                        ST_PARAM_READWRITE));
+  props[PROP_STYLE_CLASS] =
+    g_param_spec_string ("style-class",
+                         "Style Class",
+                         "Style class for styling",
+                         "",
+                         ST_PARAM_READWRITE);
 
   /**
    * StWidget:style:
@@ -895,13 +897,12 @@ st_widget_class_init (StWidgetClass *klass)
    * Inline style information for the actor as a ';'-separated list of
    * CSS properties.
    */
-  g_object_class_install_property (gobject_class,
-                                   PROP_STYLE,
-                                   g_param_spec_string ("style",
-                                                        "Style",
-                                                        "Inline style string",
-                                                        "",
-                                                        ST_PARAM_READWRITE));
+  props[PROP_STYLE] =
+     g_param_spec_string ("style",
+                          "Style",
+                          "Inline style string",
+                          "",
+                          ST_PARAM_READWRITE);
 
   /**
    * StWidget:theme:
@@ -909,13 +910,12 @@ st_widget_class_init (StWidgetClass *klass)
    * A theme set on this actor overriding the global theming for this actor
    * and its descendants
    */
-  g_object_class_install_property (gobject_class,
-                                   PROP_THEME,
-                                   g_param_spec_object ("theme",
-                                                        "Theme",
-                                                        "Theme override",
-                                                        ST_TYPE_THEME,
-                                                        ST_PARAM_READWRITE));
+  props[PROP_THEME] =
+     g_param_spec_object ("theme",
+                          "Theme",
+                          "Theme override",
+                          ST_TYPE_THEME,
+                          ST_PARAM_READWRITE);
 
   /**
    * StWidget:track-hover:
@@ -926,14 +926,12 @@ st_widget_class_init (StWidgetClass *klass)
    * adjusted automatically as the pointer moves in and out of the
    * widget.
    */
-  pspec = g_param_spec_boolean ("track-hover",
-                                "Track hover",
-                                "Determines whether the widget tracks hover state",
-                                FALSE,
-                                ST_PARAM_READWRITE);
-  g_object_class_install_property (gobject_class,
-                                   PROP_TRACK_HOVER,
-                                   pspec);
+  props[PROP_TRACK_HOVER] =
+     g_param_spec_object ("track-hover",
+                          "Track hover",
+                          "Determines whether the widget tracks hover state",
+                          FALSE,
+                          ST_PARAM_READWRITE);
 
   /**
    * StWidget:hover:
@@ -942,68 +940,63 @@ st_widget_class_init (StWidgetClass *klass)
    * only tracked automatically if #StWidget:track-hover is %TRUE, but you can
    * adjust it manually in any case.
    */
-  pspec = g_param_spec_boolean ("hover",
-                                "Hover",
-                                "Whether the pointer is hovering over the widget",
-                                FALSE,
-                                ST_PARAM_READWRITE);
-  g_object_class_install_property (gobject_class,
-                                   PROP_HOVER,
-                                   pspec);
+  props[PROP_HOVER] =
+     g_param_spec_boolean ("hover",
+                           "Hover",
+                           "Whether the pointer is hovering over the widget",
+                           FALSE,
+                           ST_PARAM_READWRITE);
 
   /**
    * StWidget:can-focus:
    *
    * Whether or not the widget can be focused via keyboard navigation.
    */
-  pspec = g_param_spec_boolean ("can-focus",
-                                "Can focus",
-                                "Whether the widget can be focused via keyboard navigation",
-                                FALSE,
-                                ST_PARAM_READWRITE);
-  g_object_class_install_property (gobject_class,
-                                   PROP_CAN_FOCUS,
-                                   pspec);
+  props[PROP_CAN_FOCUS] =
+     g_param_spec_boolean ("can-focus",
+                           "Can focus",
+                           "Whether the widget can be focused via keyboard navigation",
+                           FALSE,
+                           ST_PARAM_READWRITE);
 
   /**
    * ClutterActor:label-actor:
    *
    * An actor that labels this widget.
    */
-  g_object_class_install_property (gobject_class,
-                                   PROP_LABEL_ACTOR,
-                                   g_param_spec_object ("label-actor",
-                                                        "Label",
-                                                        "Label that identifies this widget",
-                                                        CLUTTER_TYPE_ACTOR,
-                                                        ST_PARAM_READWRITE));
+  props[PROP_LABEL_ACTOR] =
+     g_param_spec_object ("label-actor",
+                          "Label",
+                          "Label that identifies this widget",
+                          CLUTTER_TYPE_ACTOR,
+                          ST_PARAM_READWRITE);
+
   /**
    * StWidget:accessible-role:
    *
    * The accessible role of this object
    */
-  g_object_class_install_property (gobject_class,
-                                   PROP_ACCESSIBLE_ROLE,
-                                   g_param_spec_enum ("accessible-role",
-                                                      "Accessible Role",
-                                                      "The accessible role of this object",
-                                                      ATK_TYPE_ROLE,
-                                                      ATK_ROLE_INVALID,
-                                                      ST_PARAM_READWRITE));
-
+  props[PROP_ACCESSIBLE_ROLE] =
+     g_param_spec_enum ("accessible-role",
+                        "Accessible Role",
+                        "The accessible role of this object",
+                        ATK_TYPE_ROLE,
+                        ATK_ROLE_INVALID,
+                        ST_PARAM_READWRITE);
 
   /**
    * StWidget:accessible-name:
    *
    * Object instance's name for assistive technology access.
    */
-  g_object_class_install_property (gobject_class,
-                                   PROP_ACCESSIBLE_NAME,
-                                   g_param_spec_string ("accessible-name",
-                                                        "Accessible name",
-                                                        "Object instance's name for assistive technology access.",
-                                                        NULL,
-                                                        ST_PARAM_READWRITE));
+  props[PROP_ACCESSIBLE_NAME] =
+     g_param_spec_string ("accessible-name",
+                          "Accessible name",
+                          "Object instance's name for assistive technology access.",
+                          NULL,
+                          ST_PARAM_READWRITE);
+
+  g_object_class_install_properties (gobject_class, N_PROPS, props);
 
   /**
    * StWidget::style-changed:
@@ -1077,7 +1070,7 @@ st_widget_set_theme (StWidget  *actor,
 
       st_widget_style_changed (actor);
 
-      g_object_notify (G_OBJECT (actor), "theme");
+      g_object_notify_by_pspec (G_OBJECT (actor), props[PROP_THEME]);
     }
 }
 
@@ -1213,7 +1206,7 @@ st_widget_set_style_class_name (StWidget    *actor,
   if (set_class_list (&priv->style_class, style_class_list))
     {
       st_widget_style_changed (actor);
-      g_object_notify (G_OBJECT (actor), "style-class");
+      g_object_notify_by_pspec (G_OBJECT (actor), props[PROP_STYLE_CLASS]);
     }
 }
 
@@ -1239,7 +1232,7 @@ st_widget_add_style_class_name (StWidget    *actor,
   if (add_class_name (&priv->style_class, style_class))
     {
       st_widget_style_changed (actor);
-      g_object_notify (G_OBJECT (actor), "style-class");
+      g_object_notify_by_pspec (G_OBJECT (actor), props[PROP_STYLE_CLASS]);
     }
 }
 
@@ -1265,7 +1258,7 @@ st_widget_remove_style_class_name (StWidget    *actor,
   if (remove_class_name (&priv->style_class, style_class))
     {
       st_widget_style_changed (actor);
-      g_object_notify (G_OBJECT (actor), "style-class");
+      g_object_notify_by_pspec (G_OBJECT (actor), props[PROP_STYLE_CLASS]);
     }
 }
 
@@ -1376,7 +1369,7 @@ st_widget_set_style_pseudo_class (StWidget    *actor,
   if (set_class_list (&priv->pseudo_class, pseudo_class_list))
     {
       st_widget_style_changed (actor);
-      g_object_notify (G_OBJECT (actor), "pseudo-class");
+      g_object_notify_by_pspec (G_OBJECT (actor), props[PROP_PSEUDO_CLASS]);
     }
 }
 
@@ -1402,7 +1395,7 @@ st_widget_add_style_pseudo_class (StWidget    *actor,
   if (add_class_name (&priv->pseudo_class, pseudo_class))
     {
       st_widget_style_changed (actor);
-      g_object_notify (G_OBJECT (actor), "pseudo-class");
+      g_object_notify_by_pspec (G_OBJECT (actor), props[PROP_PSEUDO_CLASS]);
     }
 }
 
@@ -1427,7 +1420,7 @@ st_widget_remove_style_pseudo_class (StWidget    *actor,
   if (remove_class_name (&priv->pseudo_class, pseudo_class))
     {
       st_widget_style_changed (actor);
-      g_object_notify (G_OBJECT (actor), "pseudo-class");
+      g_object_notify_by_pspec (G_OBJECT (actor), props[PROP_PSEUDO_CLASS]);
     }
 }
 
@@ -1457,7 +1450,7 @@ st_widget_set_style (StWidget  *actor,
 
       st_widget_style_changed (actor);
 
-      g_object_notify (G_OBJECT (actor), "style");
+      g_object_notify_by_pspec (G_OBJECT (actor), props[PROP_STYLE]);
     }
 }
 
@@ -1907,7 +1900,7 @@ st_widget_set_track_hover (StWidget *widget,
   if (priv->track_hover != track_hover)
     {
       priv->track_hover = track_hover;
-      g_object_notify (G_OBJECT (widget), "track-hover");
+      g_object_notify_by_pspec (G_OBJECT (widget), props[PROP_TRACK_HOVER]);
 
       if (priv->track_hover)
         st_widget_sync_hover (widget);
@@ -1962,7 +1955,7 @@ st_widget_set_hover (StWidget *widget,
         st_widget_add_style_pseudo_class (widget, "hover");
       else
         st_widget_remove_style_pseudo_class (widget, "hover");
-      g_object_notify (G_OBJECT (widget), "hover");
+      g_object_notify_by_pspec (G_OBJECT (widget), props[PROP_HOVER]);
     }
 }
 
@@ -2029,7 +2022,7 @@ st_widget_set_can_focus (StWidget *widget,
   if (priv->can_focus != can_focus)
     {
       priv->can_focus = can_focus;
-      g_object_notify (G_OBJECT (widget), "can-focus");
+      g_object_notify_by_pspec (G_OBJECT (widget), props[PROP_CAN_FOCUS]);
     }
 }
 
@@ -2526,7 +2519,7 @@ st_widget_set_label_actor (StWidget     *widget,
       else
         priv->label_actor = NULL;
 
-      g_object_notify (G_OBJECT (widget), "label-actor");
+      g_object_notify_by_pspec (G_OBJECT (widget), props[PROP_LABEL_ACTOR]);
     }
 }
 
@@ -2559,7 +2552,7 @@ st_widget_set_accessible_name (StWidget    *widget,
     g_free (priv->accessible_name);
 
   priv->accessible_name = g_strdup (name);
-  g_object_notify (G_OBJECT (widget), "accessible-name");
+  g_object_notify_by_pspec (G_OBJECT (widget), props[PROP_ACCESSIBLE_NAME]);
 }
 
 /**
@@ -2613,7 +2606,7 @@ st_widget_set_accessible_role (StWidget *widget,
   priv = st_widget_get_instance_private (widget);
   priv->accessible_role = role;
 
-  g_object_notify (G_OBJECT (widget), "accessible-role");
+  g_object_notify_by_pspec (G_OBJECT (widget), props[PROP_ACCESSIBLE_ROLE]);
 }
 
 

src/st/st-adjustment.c
@@ -70,7 +70,11 @@ enum
   PROP_STEP_INC,
   PROP_PAGE_INC,
   PROP_PAGE_SIZE,
+
+  N_PROPS
 };
+
+static GParamSpec *props[N_PROPS] = { NULL, };
 
 enum
 {
@@ -206,72 +210,50 @@ st_adjustment_class_init (StAdjustmentClass *klass)
   object_class->get_property = st_adjustment_get_property;
   object_class->set_property = st_adjustment_set_property;
 
-  g_object_class_install_property (object_class,
-                                   PROP_LOWER,
-                                   g_param_spec_double ("lower",
-                                                        "Lower",
-                                                        "Lower bound",
-                                                        -G_MAXDOUBLE,
-                                                        G_MAXDOUBLE,
-                                                        0.0,
-                                                        ST_PARAM_READWRITE |
-                                                        G_PARAM_CONSTRUCT |
-                                                        G_PARAM_EXPLICIT_NOTIFY));
-  g_object_class_install_property (object_class,
-                                   PROP_UPPER,
-                                   g_param_spec_double ("upper",
-                                                        "Upper",
-                                                        "Upper bound",
-                                                        -G_MAXDOUBLE,
-                                                        G_MAXDOUBLE,
-                                                        0.0,
-                                                        ST_PARAM_READWRITE |
-                                                        G_PARAM_CONSTRUCT |
-                                                        G_PARAM_EXPLICIT_NOTIFY));
-  g_object_class_install_property (object_class,
-                                   PROP_VALUE,
-                                   g_param_spec_double ("value",
-                                                        "Value",
-                                                        "Current value",
-                                                        -G_MAXDOUBLE,
-                                                        G_MAXDOUBLE,
-                                                        0.0,
-                                                        ST_PARAM_READWRITE |
-                                                        G_PARAM_CONSTRUCT |
-                                                        G_PARAM_EXPLICIT_NOTIFY));
-  g_object_class_install_property (object_class,
-                                   PROP_STEP_INC,
-                                   g_param_spec_double ("step-increment",
-                                                        "Step Increment",
-                                                        "Step increment",
-                                                        0.0,
-                                                        G_MAXDOUBLE,
-                                                        0.0,
-                                                        ST_PARAM_READWRITE |
-                                                        G_PARAM_CONSTRUCT |
-                                                        G_PARAM_EXPLICIT_NOTIFY));
-  g_object_class_install_property (object_class,
-                                   PROP_PAGE_INC,
-                                   g_param_spec_double ("page-increment",
-                                                        "Page Increment",
-                                                        "Page increment",
-                                                        0.0,
-                                                        G_MAXDOUBLE,
-                                                        0.0,
-                                                        ST_PARAM_READWRITE |
-                                                        G_PARAM_CONSTRUCT |
-                                                        G_PARAM_EXPLICIT_NOTIFY));
-  g_object_class_install_property (object_class,
-                                   PROP_PAGE_SIZE,
-                                   g_param_spec_double ("page-size",
-                                                        "Page Size",
-                                                        "Page size",
-                                                        0.0,
-                                                        G_MAXDOUBLE,
-                                                        0.0,
-                                                        ST_PARAM_READWRITE |
-                                                        G_PARAM_CONSTRUCT |
-                                                        G_PARAM_EXPLICIT_NOTIFY));
+  props[PROP_LOWER] =
+    g_param_spec_double ("lower", "Lower", "Lower bound",
+                         -G_MAXDOUBLE,  G_MAXDOUBLE, 0.0,
+                         ST_PARAM_READWRITE |
+                         G_PARAM_CONSTRUCT |
+                         G_PARAM_EXPLICIT_NOTIFY);
+
+  props[PROP_UPPER] =
+    g_param_spec_double ("upper", "Upper", "Upper bound",
+                         -G_MAXDOUBLE, G_MAXDOUBLE, 0.0,
+                         ST_PARAM_READWRITE |
+                         G_PARAM_CONSTRUCT |
+                         G_PARAM_EXPLICIT_NOTIFY);
+
+  props[PROP_VALUE] =
+    g_param_spec_double ("value", "Value", "Current value",
+                         -G_MAXDOUBLE, G_MAXDOUBLE, 0.0,
+                         ST_PARAM_READWRITE |
+                         G_PARAM_CONSTRUCT |
+                         G_PARAM_EXPLICIT_NOTIFY);
+
+  props[PROP_STEP_INC] =
+    g_param_spec_double ("step-increment", "Step Increment", "Step increment",
+                         0.0, G_MAXDOUBLE, 0.0,
+                         ST_PARAM_READWRITE |
+                         G_PARAM_CONSTRUCT |
+                         G_PARAM_EXPLICIT_NOTIFY);
+
+  props[PROP_PAGE_INC] =
+    g_param_spec_double ("page-increment", "Page Increment", "Page increment",
+                         0.0, G_MAXDOUBLE, 0.0,
+                         ST_PARAM_READWRITE |
+                         G_PARAM_CONSTRUCT |
+                         G_PARAM_EXPLICIT_NOTIFY);
+
+  props[PROP_PAGE_SIZE] =
+    g_param_spec_double ("page-size", "Page Size", "Page size",
+                         0.0, G_MAXDOUBLE, 0.0,
+                         ST_PARAM_READWRITE |
+                         G_PARAM_CONSTRUCT |
+                         G_PARAM_EXPLICIT_NOTIFY);
+
+  g_object_class_install_properties (object_class, N_PROPS, props);
+
   /**
    * StAdjustment::changed:
    * @self: the #StAdjustment
@@ -342,7 +324,7 @@ st_adjustment_set_value (StAdjustment *adjustment,
     {
       priv->value = value;
 
-      g_object_notify (G_OBJECT (adjustment), "value");
+      g_object_notify_by_pspec (G_OBJECT (adjustment), props[PROP_VALUE]);
     }
 }
 
@@ -376,7 +358,7 @@ st_adjustment_clamp_page (StAdjustment *adjustment,
     }
 
   if (changed)
-    g_object_notify (G_OBJECT (adjustment), "value");
+    g_object_notify_by_pspec (G_OBJECT (adjustment), props[PROP_VALUE]);
 }
 
 static gboolean
@@ -391,7 +373,7 @@ st_adjustment_set_lower (StAdjustment *adjustment,
 
       g_signal_emit (adjustment, signals[CHANGED], 0);
 
-      g_object_notify (G_OBJECT (adjustment), "lower");
+      g_object_notify_by_pspec (G_OBJECT (adjustment), props[PROP_LOWER]);
 
       /* Defer clamp until after construction. */
       if (!priv->is_constructing)
@@ -415,7 +397,7 @@ st_adjustment_set_upper (StAdjustment *adjustment,
 
       g_signal_emit (adjustment, signals[CHANGED], 0);
 
-      g_object_notify (G_OBJECT (adjustment), "upper");
+      g_object_notify_by_pspec (G_OBJECT (adjustment), props[PROP_UPPER]);
 
       /* Defer clamp until after construction. */
       if (!priv->is_constructing)
@@ -439,7 +421,7 @@ st_adjustment_set_step_increment (StAdjustment *adjustment,
 
       g_signal_emit (adjustment, signals[CHANGED], 0);
 
-      g_object_notify (G_OBJECT (adjustment), "step-increment");
+      g_object_notify_by_pspec (G_OBJECT (adjustment), props[PROP_STEP_INC]);
 
       return TRUE;
     }
@@ -459,7 +441,7 @@ st_adjustment_set_page_increment (StAdjustment *adjustment,
 
       g_signal_emit (adjustment, signals[CHANGED], 0);
 
-      g_object_notify (G_OBJECT (adjustment), "page-increment");
+      g_object_notify_by_pspec (G_OBJECT (adjustment), props[PROP_PAGE_INC]);
 
       return TRUE;
     }
@@ -479,7 +461,7 @@ st_adjustment_set_page_size (StAdjustment *adjustment,
 
       g_signal_emit (adjustment, signals[CHANGED], 0);
 
-      g_object_notify (G_OBJECT (adjustment), "page_size");
+      g_object_notify_by_pspec (G_OBJECT (adjustment), props[PROP_PAGE_SIZE]);
 
       /* Well explicitely clamp after construction. */
       if (!priv->is_constructing)

src/st/st-bin.c
@@ -58,8 +58,12 @@ enum
   PROP_X_ALIGN,
   PROP_Y_ALIGN,
   PROP_X_FILL,
-  PROP_Y_FILL
+  PROP_Y_FILL,
+
+  N_PROPS
 };
+
+static GParamSpec *props[N_PROPS] = { NULL, };
 
 static void clutter_container_iface_init (ClutterContainerIface *iface);
 
@@ -311,7 +315,6 @@ st_bin_class_init (StBinClass *klass)
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
   ClutterActorClass *actor_class = CLUTTER_ACTOR_CLASS (klass);
   StWidgetClass *widget_class = ST_WIDGET_CLASS (klass);
-  GParamSpec *pspec;
 
   gobject_class->set_property = st_bin_set_property;
   gobject_class->get_property = st_bin_get_property;
@@ -329,64 +332,66 @@ st_bin_class_init (StBinClass *klass)
    *
    * The child #ClutterActor of the #StBin container.
    */
-  pspec = g_param_spec_object ("child",
-                               "Child",
-                               "The child of the Bin",
-                               CLUTTER_TYPE_ACTOR,
-                               ST_PARAM_READWRITE);
-  g_object_class_install_property (gobject_class, PROP_CHILD, pspec);
+  props[PROP_CHILD] =
+    g_param_spec_object ("child",
+                         "Child",
+                         "The child of the Bin",
+                         CLUTTER_TYPE_ACTOR,
+                         ST_PARAM_READWRITE);
 
   /**
    * StBin:x-align:
    *
    * The horizontal alignment of the #StBin child.
    */
-  pspec = g_param_spec_enum ("x-align",
-                             "X Align",
-                             "The horizontal alignment",
-                             ST_TYPE_ALIGN,
-                             ST_ALIGN_MIDDLE,
-                             ST_PARAM_READWRITE);
-  g_object_class_install_property (gobject_class, PROP_X_ALIGN, pspec);
+  props[PROP_X_ALIGN] =
+    g_param_spec_enum ("x-align",
+                       "X Align",
+                       "The horizontal alignment",
+                       ST_TYPE_ALIGN,
+                       ST_ALIGN_MIDDLE,
+                       ST_PARAM_READWRITE);
 
   /**
    * StBin:y-align:
    *
    * The vertical alignment of the #StBin child.
    */
-  pspec = g_param_spec_enum ("y-align",
-                             "Y Align",
-                             "The vertical alignment",
-                             ST_TYPE_ALIGN,
-                             ST_ALIGN_MIDDLE,
-                             ST_PARAM_READWRITE);
-  g_object_class_install_property (gobject_class, PROP_Y_ALIGN, pspec);
+  props[PROP_Y_ALIGN] =
+    g_param_spec_enum ("y-align",
+                       "Y Align",
+                       "The vertical alignment",
+                       ST_TYPE_ALIGN,
+                       ST_ALIGN_MIDDLE,
+                       ST_PARAM_READWRITE);
 
   /**
    * StBin:x-fill:
    *
    * Whether the child should fill the horizontal allocation
    */
-  pspec = g_param_spec_boolean ("x-fill",
-                                "X Fill",
-                                "Whether the child should fill the "
-                                "horizontal allocation",
-                                FALSE,
-                                ST_PARAM_READWRITE);
-  g_object_class_install_property (gobject_class, PROP_X_FILL, pspec);
+  props[PROP_X_FILL] =
+    g_param_spec_boolean ("x-fill",
+                          "X Fill",
+                          "Whether the child should fill the "
+                          "horizontal allocation",
+                          FALSE,
+                          ST_PARAM_READWRITE);
 
   /**
    * StBin:y-fill:
    *
    * Whether the child should fill the vertical allocation
    */
-  pspec = g_param_spec_boolean ("y-fill",
-                                "Y Fill",
-                                "Whether the child should fill the "
-                                "vertical allocation",
-                                FALSE,
-                                ST_PARAM_READWRITE);
-  g_object_class_install_property (gobject_class, PROP_Y_FILL, pspec);
+  props[PROP_Y_FILL] =
+    g_param_spec_boolean ("y-fill",
+                          "Y Fill",
+                          "Whether the child should fill the "
+                          "vertical allocation",
+                          FALSE,
+                          ST_PARAM_READWRITE);
+
+  g_object_class_install_properties (gobject_class, N_PROPS, props);
 }
 
 static void
@@ -447,7 +452,7 @@ st_bin_set_child (StBin        *bin,
 
   clutter_actor_queue_relayout (CLUTTER_ACTOR (bin));
 
-  g_object_notify (G_OBJECT (bin), "child");
+  g_object_notify_by_pspec (G_OBJECT (bin), props[PROP_CHILD]);
 }
 
 /**
@@ -492,14 +497,14 @@ st_bin_set_alignment (StBin  *bin,
   if (priv->x_align != x_align)
     {
       priv->x_align = x_align;
-      g_object_notify (G_OBJECT (bin), "x-align");
+      g_object_notify_by_pspec (G_OBJECT (bin), props[PROP_X_ALIGN]);
       changed = TRUE;
     }
 
   if (priv->y_align != y_align)
     {
       priv->y_align = y_align;
-      g_object_notify (G_OBJECT (bin), "y-align");
+      g_object_notify_by_pspec (G_OBJECT (bin), props[PROP_Y_ALIGN]);
       changed = TRUE;
     }
 
@@ -564,7 +569,7 @@ st_bin_set_fill (StBin   *bin,
       priv->x_fill = x_fill;
       changed = TRUE;
 
-      g_object_notify (G_OBJECT (bin), "x-fill");
+      g_object_notify_by_pspec (G_OBJECT (bin), props[PROP_X_FILL]);
     }
 
   if (priv->y_fill != y_fill)
@@ -572,7 +577,7 @@ st_bin_set_fill (StBin   *bin,
       priv->y_fill = y_fill;
       changed = TRUE;
 
-      g_object_notify (G_OBJECT (bin), "y-fill");
+      g_object_notify_by_pspec (G_OBJECT (bin), props[PROP_Y_FILL]);
     }
 
   if (changed)

commit 5295866eff4829e8aa90f75eed5415839e351f11

st: Fix property type
Commit 8f3554ff accidentally changed the type of the StWidget:track-hover
property from boolean to object.

https://gitlab.gnome.org/GNOME/gnome-shell/issues/1482

src/st/st-widget.c
@@ -927,11 +927,11 @@ st_widget_class_init (StWidgetClass *klass)
    * widget.
    */
   props[PROP_TRACK_HOVER] =
-     g_param_spec_object ("track-hover",
-                          "Track hover",
-                          "Determines whether the widget tracks hover state",
-                          FALSE,
-                          ST_PARAM_READWRITE);
+     g_param_spec_boolean ("track-hover",
+                           "Track hover",
+                           "Determines whether the widget tracks hover state",
+                           FALSE,
+                           ST_PARAM_READWRITE);
 
   /**
    * StWidget:hover: