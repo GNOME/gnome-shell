6bc15ea6a272182c6f4d8f2dc532d3ec5c356052
Carlos Garnacho <carlosg@gnome.org>
2019-08-15 13:22:24 +0200

    shell: Add helpers to start/stop systemd units
    These just send the respective DBus message to the systemd user service.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/680

--- src/shell-util.h
+++ src/shell-util.h
@@ -59,6 +59,13 @@ cairo_surface_t * shell_util_composite_capture_images (ClutterCapture  *captures
 
 void shell_util_check_cloexec_fds (void);
 
+gboolean shell_util_start_systemd_unit (const char  *unit,
+                                        const char  *mode,
+                                        GError     **error);
+gboolean shell_util_stop_systemd_unit  (const char  *unit,
+                                        const char  *mode,
+                                        GError     **error);
+
 G_END_DECLS
 
 #endif /* __SHELL_UTIL_H__ */
