ba0c477b03ab64955f33ee21d9f2f39ad0575c13
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-08-31 03:51:02 +0200

    overview: Add OverviewActor and use as main actor of the Overlay
    Use the Overview class as controller, while create the actual overlay actor
    using a GObject-derived class.
    
    Replace actual properties with getter-only properties.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/559

--- js/perf/core.js
+++ js/perf/core.js
@@ -127,11 +127,11 @@ function *run() {
     for (let i = 0; i < 2; i++) {
         Scripting.scriptEvent('applicationsShowStart');
         // eslint-disable-next-line require-atomic-updates
-        Main.overview._dash.showAppsButton.checked = true;
+        Main.overview.dash.showAppsButton.checked = true;
         yield Scripting.waitLeisure();
         Scripting.scriptEvent('applicationsShowDone');
         // eslint-disable-next-line require-atomic-updates
-        Main.overview._dash.showAppsButton.checked = false;
+        Main.overview.dash.showAppsButton.checked = false;
         yield Scripting.waitLeisure();
     }
 }
