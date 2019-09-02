034cbda2e763abf53a5b264926ddab5ad581f677
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-09-02 16:44:14 +0200

    js/main: Use logStructured to report the shell started Message
    Now that we've a wrapper that already defines common parameters (such as the
    log domain), we can just use this function.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/697

--- js/ui/main.js
+++ js/ui/main.js
@@ -50,7 +50,6 @@ const PointerA11yTimeout = imports.ui.pointerA11yTimeout;
 
 const A11Y_SCHEMA = 'org.gnome.desktop.a11y.keyboard';
 const STICKY_KEYS_ENABLE = 'stickykeys-enable';
-const LOG_DOMAIN = 'GNOME Shell';
 const GNOMESHELL_STARTED_MESSAGE_ID = 'f3ea493c22934e26811cd62abe8e203a';
 
 var componentManager = null;
@@ -253,10 +252,9 @@ function _initializeUI() {
         }
         if (sessionMode.currentMode != 'gdm' &&
             sessionMode.currentMode != 'initial-setup') {
-            GLib.log_structured(LOG_DOMAIN, GLib.LogLevelFlags.LEVEL_MESSAGE, {
-                'MESSAGE': `GNOME Shell started at ${_startDate}`,
-                'MESSAGE_ID': GNOMESHELL_STARTED_MESSAGE_ID
-            });
+            logStructured(GLib.LogLevelFlags.LEVEL_MESSAGE,
+                `GNOME Shell started at ${_startDate}`,
+                { 'MESSAGE_ID': GNOMESHELL_STARTED_MESSAGE_ID });
         }
 
         LoginManager.registerSessionWithGDM();
