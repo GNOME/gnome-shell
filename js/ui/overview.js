GNOME/gnome-shell

commit 0846238f69ad15d5409d0d37360d7a2476b50316

js: Use implicit animations for animatable properties
We now have everything in place to replace Tweener for all animatable
properties with implicit animations, which has the following benefits:

 - they run entirely in C, while Tweener requires context switches
   to JS each frame

 - they are more reliable, as Tweener only detects when an animation
   is overwritten with another Tween, while Clutter considers any
   property change

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/22

js/ui/overview.js
@@ -427,10 +427,11 @@ var Overview = class {
     fadeInDesktop() {
         this._desktopFade.opacity = 0;
         this._desktopFade.show();
-        Tweener.addTween(this._desktopFade,
-                         { opacity: 255,
-                           time: ANIMATION_TIME / 1000,
-                           transition: 'easeOutQuad' });
+        this._desktopFade.ease({
+            opacity: 255,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+            duration: ANIMATION_TIME
+        });
     }
 
     fadeOutDesktop() {
@@ -444,11 +445,11 @@ var Overview = class {
 
         this._desktopFade.opacity = 255;
         this._desktopFade.show();
-        Tweener.addTween(this._desktopFade,
-                         { opacity: 0,
-                           time: ANIMATION_TIME / 1000,
-                           transition: 'easeOutQuad'
-                         });
+        this._desktopFade.ease({
+            opacity: 0,
+            mode: Clutter.Animates.EASE_OUT_QUAD,
+            duration: ANIMATION_TIME
+        });
     }
 
     // Checks if the Activities button is currently sensitive to
@@ -528,13 +529,12 @@ var Overview = class {
         this.viewSelector.show();
 
         this._overview.opacity = 0;
-        Tweener.addTween(this._overview,
-                         { opacity: 255,
-                           transition: 'easeOutQuad',
-                           time: ANIMATION_TIME / 1000,
-                           onComplete: this._showDone,
-                           onCompleteScope: this
-                         });
+        this._overview.ease({
+            opacity: 255,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+            duration: ANIMATION_TIME,
+            onComplete: () => this._showDone()
+        });
         this._shadeBackgrounds();
 
         this._coverPane.raise_top();
@@ -592,13 +592,12 @@ var Overview = class {
         this.viewSelector.animateFromOverview();
 
         // Make other elements fade out.
-        Tweener.addTween(this._overview,
-                         { opacity: 0,
-                           transition: 'easeOutQuad',
-                           time: ANIMATION_TIME / 1000,
-                           onComplete: this._hideDone,
-                           onCompleteScope: this
-                         });
+        this._overview.ease({
+            opacity: 0,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+            duration: ANIMATION_TIME,
+            onComplete: () => this._hideDone()
+        });
         this._unshadeBackgrounds();
 
         this._coverPane.raise_top();

js/gdm/authPrompt.js
@@ -8,7 +8,6 @@ const Batch = imports.gdm.batch;
 const GdmUtil = imports.gdm.util;
 const Params = imports.misc.params;
 const ShellEntry = imports.ui.shellEntry;
-const Tweener = imports.ui.tweener;
 const UserWidget = imports.ui.userWidget;
 
 var DEFAULT_BUTTON_WELL_ICON_SIZE = 16;
@@ -267,7 +266,7 @@ var AuthPrompt = class {
         let oldActor = this._defaultButtonWellActor;
 
         if (oldActor)
-            Tweener.removeTweens(oldActor);
+            oldActor.remove_all_transitions();
 
         let wasSpinner;
         if (oldActor == this._spinner.actor)
@@ -290,18 +289,18 @@ var AuthPrompt = class {
                         this._spinner.stop();
                 }
             } else {
-                Tweener.addTween(oldActor,
-                                 { opacity: 0,
-                                   time: DEFAULT_BUTTON_WELL_ANIMATION_TIME / 1000,
-                                   delay: DEFAULT_BUTTON_WELL_ANIMATION_DELAY / 1000,
-                                   transition: 'linear',
-                                   onComplete: () => {
-                                       if (wasSpinner) {
-                                           if (this._spinner)
-                                               this._spinner.stop();
-                                       }
-                                   }
-                                 });
+                oldActor.ease({
+                    opacity: 0,
+                    duration: DEFAULT_BUTTON_WELL_ANIMATION_TIME,
+                    delay: DEFAULT_BUTTON_WELL_ANIMATION_DELAY,
+                    mode: Clutter.AnimationMode.LINEAR,
+                    onComplete: () => {
+                        if (wasSpinner) {
+                            if (this._spinner)
+                                this._spinner.stop();
+                        }
+                    }
+                });
             }
         }
 
@@ -312,11 +311,12 @@ var AuthPrompt = class {
             if (!animate)
                 actor.opacity = 255;
             else
-                Tweener.addTween(actor,
-                                 { opacity: 255,
-                                   time: DEFAULT_BUTTON_WELL_ANIMATION_TIME / 1000,
-                                   delay: DEFAULT_BUTTON_WELL_ANIMATION_DELAY / 1000,
-                                   transition: 'linear' });
+                actor.ease({
+                    opacity: 255,
+                    duration: DEFAULT_BUTTON_WELL_ANIMATION_TIME,
+                    delay: DEFAULT_BUTTON_WELL_ANIMATION_DELAY,
+                    mode: Clutter.AnimationMode.LINEAR
+                });
         }
 
         this._defaultButtonWellActor = actor;
@@ -365,12 +365,12 @@ var AuthPrompt = class {
     _fadeOutMessage() {
         if (this._message.opacity == 0)
             return;
-        Tweener.removeTweens(this._message);
-        Tweener.addTween(this._message,
-                         { opacity: 0,
-                           time: MESSAGE_FADE_OUT_ANIMATION_TIME / 1000,
-                           transition: 'easeOutQuad'
-                         });
+        this._message.remove_all_transitions();
+        this._message.ease({
+            opacity: 0,
+            duration: MESSAGE_FADE_OUT_ANIMATION_TIME,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD
+        });
     }
 
     setMessage(message, type) {
@@ -385,7 +385,7 @@ var AuthPrompt = class {
             this._message.remove_style_class_name('login-dialog-message-hint');
 
         if (message) {
-            Tweener.removeTweens(this._message);
+            this._message.remove_all_transitions();
             this._message.text = message;
             this._message.opacity = 255;
         } else {

js/gdm/batch.js
@@ -20,7 +20,7 @@
  * In order for transformation animations to look good, they need to be
  * incremental and have some order to them (e.g., fade out hidden items,
  * then shrink to close the void left over). Chaining animations in this way can
- * be error-prone and wordy using just Tweener callbacks.
+ * be error-prone and wordy using just ease() callbacks.
  *
  * The classes in this file help with this:
  *

js/gdm/loginDialog.js
@@ -759,14 +759,15 @@ var LoginDialog = GObject.registerClass({
 
     _fadeInBannerView() {
         this._bannerView.show();
-        Tweener.addTween(this._bannerView,
-                         { opacity: 255,
-                           time: _FADE_ANIMATION_TIME / 1000,
-                           transition: 'easeOutQuad' });
+        this._bannerView.ease({
+            opacity: 255,
+            duration: _FADE_ANIMATION_TIME,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD
+        });
     }
 
     _hideBannerView() {
-        Tweener.removeTweens(this._bannerView);
+        this._bannerView.remove_all_transitions();
         this._bannerView.opacity = 0;
         this._bannerView.hide();
     }
@@ -859,10 +860,11 @@ var LoginDialog = GObject.registerClass({
             return;
         this._authPrompt.actor.opacity = 0;
         this._authPrompt.actor.show();
-        Tweener.addTween(this._authPrompt.actor,
-                         { opacity: 255,
-                           time: _FADE_ANIMATION_TIME / 1000,
-                           transition: 'easeOutQuad' });
+        this._authPrompt.actor.ease({
+            opacity: 255,
+            duration: _FADE_ANIMATION_TIME,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD
+        });
         this._fadeInBannerView();
     }
 
@@ -921,15 +923,16 @@ var LoginDialog = GObject.registerClass({
             return;
 
         this._bindOpacity();
-        Tweener.addTween(this,
-                         { opacity: 255,
-                           time: _FADE_ANIMATION_TIME / 1000,
-                           transition: 'easeOutQuad',
-                           onComplete: () => {
-                               if (this._authPrompt.verificationStatus != AuthPrompt.AuthPromptStatus.NOT_VERIFYING)
-                                   this._authPrompt.reset();
-                               this._unbindOpacity();
-                           } });
+        this.actor.ease({
+            opacity: 255,
+            duration: _FADE_ANIMATION_TIME,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+            onComplete: () => {
+                if (this._authPrompt.verificationStatus != AuthPrompt.AuthPromptStatus.NOT_VERIFYING)
+                    this._authPrompt.reset();
+                this._unbindOpacity();
+            }
+        });
     }
 
     _gotGreeterSessionProxy(proxy) {
@@ -943,14 +946,15 @@ var LoginDialog = GObject.registerClass({
 
     _startSession(serviceName) {
         this._bindOpacity();
-        Tweener.addTween(this,
-                         { opacity: 0,
-                           time: _FADE_ANIMATION_TIME / 1000,
-                           transition: 'easeOutQuad',
-                           onComplete: () => {
-                               this._greeter.call_start_session_when_ready_sync(serviceName, true, null);
-                               this._unbindOpacity();
-                           } });
+        this.actor.ease({
+            opacity: 0,
+            duration: _FADE_ANIMATION_TIME,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+            onComplete: () => {
+                this._greeter.call_start_session_when_ready_sync(serviceName, true, null);
+                this._unbindOpacity();
+            }
+        });
     }
 
     _onSessionOpened(client, serviceName) {
@@ -1223,10 +1227,11 @@ var LoginDialog = GObject.registerClass({
 
         Main.pushModal(this, { actionMode: Shell.ActionMode.LOGIN_SCREEN });
 
-        Tweener.addTween(this,
-                         { opacity: 255,
-                           time: 1,
-                           transition: 'easeInQuad' });
+        this.ease({
+            opacity: 255,
+            duration: 1000,
+            mode: Clutter.AnimationMode.EASE_IN_QUAD
+        });
 
         return true;
     }

js/gdm/util.js
@@ -11,7 +11,6 @@ const OVirt = imports.gdm.oVirt;
 const Main = imports.ui.main;
 const Params = imports.misc.params;
 const SmartcardManager = imports.misc.smartcardManager;
-const Tweener = imports.ui.tweener;
 
 var PASSWORD_SERVICE_NAME = 'gdm-password';
 var FINGERPRINT_SERVICE_NAME = 'gdm-fingerprint';
@@ -51,16 +50,16 @@ function fadeInActor(actor) {
 
     actor.opacity = 0;
     actor.set_height(0);
-    Tweener.addTween(actor,
-                     { opacity: 255,
-                       height: naturalHeight,
-                       time: FADE_ANIMATION_TIME / 1000,
-                       transition: 'easeOutQuad',
-                       onComplete() {
-                           this.set_height(-1);
-                           hold.release();
-                       },
-                     });
+    actor.ease({
+        opacity: 255,
+        height: naturalHeight,
+        duration: FADE_ANIMATION_TIME,
+        mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+        onComplete: () => {
+            this.set_height(-1);
+            hold.release();
+        }
+    });
 
     return hold;
 }
@@ -73,17 +72,17 @@ function fadeOutActor(actor) {
     }
 
     let hold = new Batch.Hold();
-    Tweener.addTween(actor,
-                     { opacity: 0,
-                       height: 0,
-                       time: FADE_ANIMATION_TIME / 1000,
-                       transition: 'easeOutQuad',
-                       onComplete() {
-                           this.hide();
-                           this.set_height(-1);
-                           hold.release();
-                       },
-                     });
+    actor.ease({
+        opacity: 0,
+        height: 0,
+        duration: FADE_ANIMATION_TIME,
+        mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+        onComplete: () => {
+            this.hide();
+            this.set_height(-1);
+            hold.release();
+        }
+    });
     return hold;
 }
 
@@ -103,15 +102,15 @@ function cloneAndFadeOutActor(actor) {
     clone.set_position(x, y);
 
     let hold = new Batch.Hold();
-    Tweener.addTween(clone,
-                     { opacity: 0,
-                       time: CLONE_FADE_ANIMATION_TIME / 1000,
-                       transition: 'easeOutQuad',
-                       onComplete() {
-                           clone.destroy();
-                           hold.release();
-                       }
-                     });
+    clone.ease({
+        opacity: 0,
+        duration: CLONE_FADE_ANIMATION_TIME,
+        mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+        onComplete: () => {
+            clone.destroy();
+            hold.release();
+        }
+    });
     return hold;
 }
 

js/ui/altTab.js
@@ -7,7 +7,6 @@ const Mainloop = imports.mainloop;
 
 const Main = imports.ui.main;
 const SwitcherPopup = imports.ui.switcherPopup;
-const Tweener = imports.ui.tweener;
 
 var APP_ICON_HOVER_TIMEOUT = 200; // milliseconds
 
@@ -362,15 +361,15 @@ class AppSwitcherPopup extends SwitcherPopup.SwitcherPopup {
 
     _destroyThumbnails() {
         let thumbnailsActor = this._thumbnails;
-        Tweener.addTween(thumbnailsActor,
-                         { opacity: 0,
-                           time: THUMBNAIL_FADE_TIME / 1000,
-                           transition: 'easeOutQuad',
-                           onComplete: () => {
-                               thumbnailsActor.destroy();
-                               this.thumbnailsVisible = false;
-                           }
-                         });
+        this._thumbnails.ease({
+            opacity: 0,
+            duration: THUMBNAIL_FADE_TIME,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+            onComplete: () => {
+                thumbnailsActor.destroy();
+                this.thumbnailsVisible = false;
+            }
+        });
         this._thumbnails = null;
         if (this._switcherList._items[this._selectedIndex])
             this._switcherList._items[this._selectedIndex].remove_accessible_state (Atk.StateType.EXPANDED);
@@ -393,12 +392,14 @@ class AppSwitcherPopup extends SwitcherPopup.SwitcherPopup {
         this._thumbnails.get_allocation_box();
 
         this._thumbnails.opacity = 0;
-        Tweener.addTween(this._thumbnails,
-                         { opacity: 255,
-                           time: THUMBNAIL_FADE_TIME / 1000,
-                           transition: 'easeOutQuad',
-                           onComplete: () => this.thumbnailsVisible = true
-                         });
+        this._thumbnails.ease({
+            opacity: 255,
+            duration: THUMBNAIL_FADE_TIME,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+            onComplete: () => {
+                this.thumbnailsVisible = true;
+            }
+        });
 
         this._switcherList._items[this._selectedIndex].add_accessible_state (Atk.StateType.EXPANDED);
     }

js/ui/animation.js
@@ -1,10 +1,8 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 /* exported Animation, AnimatedIcon, Spinner */
 
-const { GLib, Gio, St } = imports.gi;
+const { Clutter, GLib, Gio, St } = imports.gi;
 const Mainloop = imports.mainloop;
-
-const Tweener = imports.ui.tweener;
 
 var ANIMATED_ICON_UPDATE_TIMEOUT = 16;
 var SPINNER_ANIMATION_TIME = 300;
@@ -138,15 +136,15 @@ var Spinner = class extends AnimatedIcon {
     }
 
     play() {
-        Tweener.removeTweens(this.actor);
+        this.actor.remove_all_transitions();
 
         if (this._animate) {
             super.play();
-            Tweener.addTween(this.actor, {
+            this.actor.ease({
                 opacity: 255,
-                delay: SPINNER_ANIMATION_DELAY / 1000,
-                time: SPINNER_ANIMATION_TIME / 1000,
-                transition: 'linear'
+                delay: SPINNER_ANIMATION_DELAY,
+                duration: SPINNER_ANIMATION_TIME,
+                mode: Clutter.AnimationMode.LINEAR
             });
         } else {
             this.actor.opacity = 255;
@@ -155,16 +153,14 @@ var Spinner = class extends AnimatedIcon {
     }
 
     stop() {
-        Tweener.removeTweens(this.actor);
+        this.actor.remove_all_transitions();
 
         if (this._animate) {
-            Tweener.addTween(this.actor, {
+            this.actor.ease({
                 opacity: 0,
-                time: SPINNER_ANIMATION_TIME / 1000,
+                time: SPINNER_ANIMATION_TIME,
                 transition: 'linear',
-                onComplete: () => {
-                    super.stop();
-                }
+                onComplete: () => super.stop()
             });
         } else {
             this.actor.opacity = 0;

js/ui/appDisplay.js
@@ -206,22 +206,24 @@ class BaseAppView {
     }
 
     animateSwitch(animationDirection) {
-        Tweener.removeTweens(this.actor);
-        Tweener.removeTweens(this._grid);
+        this.actor.remove_all_transitions();
+        this._grid.remove_all_transitions();
 
-        let params = { time: VIEWS_SWITCH_TIME / 1000,
-                       transition: 'easeOutQuad' };
+        let params = {
+            duration: VIEWS_SWITCH_TIME,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD
+        };
         if (animationDirection == IconGrid.AnimationDirection.IN) {
             this.actor.show();
             params.opacity = 255;
-            params.delay = VIEWS_SWITCH_ANIMATION_DELAY / 1000;
+            params.delay = VIEWS_SWITCH_ANIMATION_DELAY;
         } else {
             params.opacity = 0;
             params.delay = 0;
             params.onComplete = () => this.actor.hide();
         }
 
-        Tweener.addTween(this._grid, params);
+        this._grid.ease(params);
     }
 }
 Signals.addSignalMethods(BaseAppView.prototype);
@@ -439,13 +441,12 @@ var AllView = class AllView extends BaseAppView {
 
         if (this._currentPopup && this._displayingPopup &&
             animationDirection == IconGrid.AnimationDirection.OUT)
-            Tweener.addTween(this._currentPopup.actor,
-                             { time: VIEWS_SWITCH_TIME / 1000,
-                               transition: 'easeOutQuad',
-                               opacity: 0,
-                               onComplete() {
-                                   this.opacity = 255;
-                               } });
+            this._currentPopup.actor.ease({
+                opacity: 0,
+                duration: VIEWS_SWITCH_TIME,
+                mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+                onComplete: () => this.opacity = 255
+            });
 
         if (animationDirection == IconGrid.AnimationDirection.OUT)
             this._pageIndicators.animateIndicators(animationDirection);
@@ -615,15 +616,16 @@ var AllView = class AllView extends BaseAppView {
 
     _updateIconOpacities(folderOpen) {
         for (let id in this._items) {
-            let params, opacity;
+            let opacity;
             if (folderOpen && !this._items[id].actor.checked)
                 opacity =  INACTIVE_GRID_OPACITY;
             else
                 opacity = 255;
-            params = { opacity: opacity,
-                       time: INACTIVE_GRID_OPACITY_ANIMATION_TIME / 1000,
-                       transition: 'easeOutQuad' };
-            Tweener.addTween(this._items[id].actor, params);
+            this._items[id].actor.ease({
+                opacity: opacity,
+                duration: INACTIVE_GRID_OPACITY_ANIMATION_TIME,
+                mode: Clutter.AnimationMode.EASE_OUT_QUAD
+            });
         }
     }
 
@@ -831,7 +833,7 @@ var AppDisplay = class AppDisplay {
             if (this._controls.mapped)
                 return;
 
-            Tweener.removeTweens(this._controls);
+            this._controls.remove_all_transitions();
             this._controls.opacity = 255;
         });
 
@@ -897,11 +899,11 @@ var AppDisplay = class AppDisplay {
             finalOpacity = 0;
         }
 
-        Tweener.addTween(this._controls,
-                         { time: IconGrid.ANIMATION_TIME_IN,
-                           transition: 'easeInOutQuad',
-                           opacity: finalOpacity,
-                         });
+        this._controls.ease({
+            opacity: finalOpacity,
+            duration: IconGrid.ANIMATION_TIME_IN,
+            mode: Clutter.AnimationMode.EASE_IN_OUT_QUAD
+        });
 
         currentView.animate(animationDirection, onComplete);
     }

commit 817aec5466eeccf9d00fc1e69b4b77f1aebf44fd

overview: Fix fading out desktop icons
A typo in commit 0846238f69ad1 broke the animation.

Signed-off-by: Harshula Jayasuriya <harshula@hj.id.au>

https://gitlab.gnome.org/GNOME/gnome-shell/issues/1616

js/ui/overview.js
@@ -452,7 +452,7 @@ var Overview = class {
         this._desktopFade.show();
         this._desktopFade.ease({
             opacity: 0,
-            mode: Clutter.Animates.EASE_OUT_QUAD,
+            mode: Clutter.AnimationMode.EASE_OUT_QUAD,
             duration: ANIMATION_TIME
         });
     }