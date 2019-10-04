58da48d60ab1bd7a42e9675071382d0489aa3906
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-10-04 15:02:40 +0200

    environment: Use zero duration for animations if disabled
    If animations are disabled we force the duration to 1ms, however this would
    still imply to pass through the transition code path, instead of applying
    the changes promptly.
    
    Use different adjustment for tweener animations instead to preserve logic
    prior to commit 99b4e047d.
    
    Fixes https://gitlab.gnome.org/GNOME/gnome-shell/issues/1749

--- js/ui/tweener.js
+++ js/ui/tweener.js
@@ -7,7 +7,7 @@ const { Clutter, GLib, Shell } = imports.gi;
 const Signals = imports.signals;
 const Tweener = imports.tweener.tweener;
 
-const { adjustAnimationTime } = imports.ui.environment;
+const Environment = imports.ui.environment;
 
 // This is a wrapper around imports.tweener.tweener that adds a bit of
 // Clutter integration. If the tweening target is a Clutter.Actor, then
@@ -28,7 +28,6 @@ function init() {
     Tweener.setFrameTicker(new ClutterFrameTicker());
 }
 
-
 function addCaller(target, tweeningParameters) {
     _wrapTweening(target, tweeningParameters);
     Tweener.addCaller(target, tweeningParameters);
@@ -37,6 +36,11 @@ function addCaller(target, tweeningParameters) {
 function addTween(target, tweeningParameters) {
     _wrapTweening(target, tweeningParameters);
     Tweener.addTween(target, tweeningParameters);
+}
+
+function adjustAnimationTime(msecs) {
+    let time = Environment.adjustAnimationTime(1000 * msecs) / 1000;
+    return time ? time : 0.000001;
 }
 
 function _wrapTweening(target, tweeningParameters) {
@@ -54,9 +58,9 @@ function _wrapTweening(target, tweeningParameters) {
 
     let { time, delay } = tweeningParameters;
     if (!isNaN(time))
-        tweeningParameters['time'] = adjustAnimationTime(1000 * time) / 1000;
+        tweeningParameters['time'] = adjustAnimationTime(time);
     if (!isNaN(delay))
-        tweeningParameters['delay'] = adjustAnimationTime(1000 * delay) / 1000;
+        tweeningParameters['delay'] = adjustAnimationTime(delay);
 
     _addHandler(target, tweeningParameters, 'onComplete', _tweenCompleted);
 }
