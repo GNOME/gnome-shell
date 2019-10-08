ae34925be8366140a63c9969c8552e57ba0b483d
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-10-08 18:42:06 +0200

    overview: Replace deprecated raise_top() with set_child_above_sibling()
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/752

--- js/ui/overview.js
+++ js/ui/overview.js
@@ -540,7 +540,8 @@ var Overview = class {
         });
         this._shadeBackgrounds();
 
-        this._coverPane.raise_top();
+        Main.layoutManager.overviewGroup.set_child_above_sibling(
+            this._coverPane, null);
         this._coverPane.show();
         this.emit('showing');
     }
@@ -603,7 +604,8 @@ var Overview = class {
         });
         this._unshadeBackgrounds();
 
-        this._coverPane.raise_top();
+        Main.layoutManager.overviewGroup.set_child_above_sibling(
+            this._coverPane, null);
         this._coverPane.show();
         this.emit('hiding');
     }
