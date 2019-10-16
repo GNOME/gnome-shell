09884343c3e463ed1b9c5c676b446ce98202e699
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-10-16 12:38:48 +0200

    search: Define SearchResultInterface and implement valid results with it
    Since all the search result classes are now GObject classes, we can enforce
    the methods we want to have in there (just activate() for now) using an
    interface, to make sure they are implementing what we require and to easily
    group all the classes that can be used as search results, even though they
    are not extending SearchResult.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/559

--- js/ui/search.js
+++ js/ui/search.js
@@ -1,5 +1,5 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
-/* exported SearchResultsView */
+/* exported SearchResultsView, SearchResultInterface */
 
 const { Clutter, Gio, GLib, GObject, Meta, Shell, St } = imports.gi;
 
@@ -32,8 +32,17 @@ class MaxWidthBox extends St.BoxLayout {
     }
 });
 
-var SearchResult = GObject.registerClass(
-class SearchResult extends St.Button {
+var SearchResultInterface = GObject.registerClass({
+    Requires: [Clutter.Actor],
+}, class SearchResultInterface extends GObject.Interface {
+    activate() {
+        throw new GObject.NotImplementedError('activate in %s'.format(this.constructor.name));
+    }
+});
+
+var SearchResult = GObject.registerClass({
+    Implements: [SearchResultInterface]
+}, class SearchResult extends St.Button {
     _init(provider, metaInfo, resultsView) {
         this.provider = provider;
         this.metaInfo = metaInfo;
