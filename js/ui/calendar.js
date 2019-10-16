bc01a7319d770fa57f5247ade9dbd6676218c631
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-10-16 18:47:59 +0200

    calendar: Use a property to hold the selected date
    Instead of using a private property with custom signal and a method, just
    use a GObject property exposing getter and setters to adjust it and notify
    its change.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/766

--- js/ui/calendar.js
+++ js/ui/calendar.js
@@ -325,7 +325,11 @@ var DBusEventSource = class DBusEventSource {
 Signals.addSignalMethods(DBusEventSource.prototype);
 
 var Calendar = GObject.registerClass({
-    Signals: { 'selected-date-changed': { param_types: [GLib.DateTime.$gtype] } }
+    Properties: {
+        'selected-date': GObject.ParamSpec.boxed(
+            'selected-date', 'selected-date', 'selected-date',
+            GObject.ParamFlags.READABLE, GLib.DateTime.$gtype)
+    }
 }, class Calendar extends St.Widget {
     _init() {
         this._weekStart = Shell.util_get_week_start();
@@ -378,15 +382,19 @@ var Calendar = GObject.registerClass({
         this._update();
     }
 
+    get selectedDate() {
+        return this._selectedDate;
+    }
+
     // Sets the calendar to show a specific date
-    setDate(date) {
+    set selectedDate(date) {
         if (sameDay(date, this._selectedDate))
             return;
 
         this._selectedDate = date;
         this._update();
 
-        this.emit('selected-date-changed', this._selectedDate);
+        this.notify('selected-date');
     }
 
     updateTimeZone() {
@@ -468,13 +476,13 @@ var Calendar = GObject.registerClass({
 
     _onPrevMonthButtonClicked() {
         this._backButton.grab_key_focus();
-        this.setDate(this._selectedDate.add_months(-1));
+        this.selectedDate = this.selectedDate.add_months(-1);
     }
 
     _onNextMonthButtonClicked() {
         this._forwardButton.grab_key_focus();
 
-        this.setDate(this._selectedDate.add_months(1));
+        this.selectedDate = this.selectedDate.add_months(1);
     }
 
     _onSettingsChange() {
@@ -542,7 +550,7 @@ var Calendar = GObject.registerClass({
             button._date = dateTimeCopy(iter);
             button.connect('clicked', () => {
                 this._shouldDateGrabFocus = true;
-                this.setDate(button._date);
+                this.selectedDate = button._date;
                 this._shouldDateGrabFocus = false;
             });
 
