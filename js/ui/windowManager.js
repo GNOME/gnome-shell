53702bafd524175d5a22b457396fef2fea3ae295
Carlos Garnacho <carlosg@gnome.org>
2019-11-14 23:56:42 +0100

    windowManager: Implement MetaDisplay::init-xserver hook
    We do spawn gsd-xsettings, and watch its name before notifying on the
    given task, so the mutter bits can proceed with X11 startup.
    
    One notable change is that we only start gsd-xsettings, instead of the
    generic gnome-session-x11-services target. We do so as we have to wait
    on a dbus name to appear in order to deem the initialization done, and
    making it all depend on gsd-xsettings seems tidier.
    
    Less notably, we also use ::shutdown-xserver to shutdown the related
    services. Its major benefit is that it'd allow us to ensure the olderly
    shutdown of those services, but it's unused at the moment.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/836

--- js/ui/windowManager.js
+++ js/ui/windowManager.js
@@ -895,11 +895,34 @@ var WindowManager = class {
             }
         });
 
-        global.display.connect('x11-display-opened', () => {
-            Shell.util_start_systemd_unit('gnome-session-x11-services.target', 'fail');
+        global.display.connect('init-xserver', (display, task) => {
+            Shell.util_start_systemd_unit('gsd-xsettings.target', 'fail');
+
+            /* Leave this watchdog timeout so don't block indefinitely here */
+            let timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 5, () => {
+                Gio.DBus.session.unwatch_name(watchId);
+                task.return_boolean(false);
+                timeoutId = 0;
+                return GLib.SOURCE_REMOVE;
+            });
+
+            /* When gsd-xsettings daemon is started, we are good to resume */
+            let watchId = Gio.DBus.session.watch_name(
+                'org.gnome.SettingsDaemon.XSettings',
+                Gio.BusNameWatcherFlags.NONE,
+                () => {
+                    Gio.DBus.session.unwatch_name(watchId);
+                    if (timeoutId > 0) {
+                        task.return_boolean(true);
+                        GLib.source_remove(timeoutId);
+                    }
+                },
+                null);
+            return true;
         });
-        global.display.connect('x11-display-closing', () => {
-            Shell.util_stop_systemd_unit('gnome-session-x11-services.target', 'fail');
+        global.display.connect('shutdown-xserver', (display, task) => {
+            Shell.util_stop_systemd_unit('gsd-xsettings.target', 'fail');
+            task.return_boolean(true);
         });
 
         Main.overview.connect('showing', () => {
