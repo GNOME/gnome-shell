GNOME/gnome-shell

commit 4dc44304dff2974d3b026b5aa58e0d5c25d38216

mpris: Hide notification when !CanPlay, instead of closing player
I have observed a client in the wild (Chromium again) set CanPlay to
false momentarily while it is loading the next song. Previously, the
code would close the player proxy in that case, meaning that after
playing one track, the MPRIS message would disappear and never come
back.

However, I think this use of CanPlay, while apparently not usual, is not
incorrect according to the spec. We should hide the message instead of
closing the player proxy.

https://gitlab.gnome.org/GNOME/gnome-shell/issues/1362

js/ui/mpris.js
@@ -44,9 +44,17 @@ class MediaMessage extends MessageList.Message {
                 this._player.next();
             });
 
-        this._player.connect('changed', this._update.bind(this));
-        this._player.connect('closed', this.close.bind(this));
+        this._updateHandlerId =
+            this._player.connect('changed', this._update.bind(this));
+        this._closedHandlerId =
+            this._player.connect('closed', this.close.bind(this));
         this._update();
+    }
+
+    _onDestroy() {
+        super._onDestroy();
+        this._player.disconnect(this._updateHandlerId);
+        this._player.disconnect(this._closedHandlerId);
     }
 
     vfunc_clicked() {
@@ -217,7 +225,7 @@ var MprisPlayer = class MprisPlayer {
             if (visible)
                 this.emit('show');
             else
-                this._close();
+                this.emit('hide');
         }
     }
 };
@@ -249,11 +257,14 @@ class MediaSection extends MessageList.MessageListSection {
             () => {
                 this._players.delete(busName);
             });
-        player.connect('show',
-            () => {
-                let message = new MediaMessage(player);
-                this.addMessage(message, true);
-            });
+        player.connect('show', () => {
+            this._message = new MediaMessage(player);
+            this.addMessage(this._message, true);
+        });
+        player.connect('hide', () => {
+            this.removeMessage(this._message, true);
+            this._message = null;
+        });
         this._players.set(busName, player);
     }
 
@@ -274,7 +285,10 @@ class MediaSection extends MessageList.MessageListSection {
         if (!name.startsWith(MPRIS_PLAYER_PREFIX))
             return;
 
-        if (newOwner && !oldOwner)
+        if (newOwner && !oldOwner) {
+            if (this._message)
+                this.removeMessage(this._message);
             this._addPlayer(name);
+        }
     }
 });

commit 75506d9865f0b97602e44968dbd84a4843406744

mpris: Use a scope specific message instead of a global one
Since the mpris implementation of the notification tray supports showing
multiple notification (one for each player), it doesn't make sense to
have only one global property to store the message, since that only
allows referencing one message at a time.

Instead, handle the MediaMessages completely inside the scope of
`_addPlayer()`, this should allow showing more than one message again,
which broke with commit 4dc44304dff2974d3b026b5aa58e0d5c25d38216 [1].

[1] https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/791

js/ui/mpris.js
@@ -256,18 +256,20 @@ class MediaSection extends MessageList.MessageListSection {
             return;
 
         let player = new MprisPlayer(busName);
+        let message = null;
         player.connect('closed',
             () => {
                 this._players.delete(busName);
             });
         player.connect('show', () => {
-            this._message = new MediaMessage(player);
-            this.addMessage(this._message, true);
+            message = new MediaMessage(player);
+            this.addMessage(message, true);
         });
         player.connect('hide', () => {
-            this.removeMessage(this._message, true);
-            this._message = null;
+            this.removeMessage(message, true);
+            message = null;
         });
+
         this._players.set(busName, player);
     }
 
@@ -288,10 +290,7 @@ class MediaSection extends MessageList.MessageListSection {
         if (!name.startsWith(MPRIS_PLAYER_PREFIX))
             return;
 
-        if (newOwner && !oldOwner) {
-            if (this._message)
-                this.removeMessage(this._message);
+        if (newOwner && !oldOwner)
             this._addPlayer(name);
-        }
     }
 });