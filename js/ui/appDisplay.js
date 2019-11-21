b88f7d0655bfac19984b73681da2d2e01238fffa
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2019-11-21 18:21:44 -0300

    baseAppView: Destroy icon when removing
    We cannot rely on the garbage collector to do that in a timely
    manner, so destroy it explicitly.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/841

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -173,10 +173,11 @@ class BaseAppView {
         // Remove old app icons
         removedApps.forEach(icon => {
             let iconIndex = this._allItems.indexOf(icon);
+            let id = icon.id;
 
             this._allItems.splice(iconIndex, 1);
-            this._grid.removeItem(icon);
-            delete this._items[icon.id];
+            icon.destroy();
+            delete this._items[id];
         });
 
         // Add new app icons
