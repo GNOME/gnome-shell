GNOME/gnome-shell

commit 6af25b282c1381f940995e74075355338afa0ef8

animation: Turn Spinner animate parameter into Params option
https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/832

js/ui/components/polkitAgent.js
@@ -103,7 +103,9 @@ var AuthenticationDialog = GObject.registerClass({
             GObject.BindingFlags.SYNC_CREATE);
         this._passwordBox.add_child(this._passwordEntry);
 
-        this._workSpinner = new Animation.Spinner(WORK_SPINNER_ICON_SIZE, true);
+        this._workspinner = new Animation.Spinner(WORK_SPINNER_ICON_SIZE, {
+            animate: true,
+        });
         this._passwordBox.add(this._workSpinner);
 
         this._passwordBox.hide();

js/ui/animation.js
@@ -3,6 +3,8 @@
 
 const { Clutter, GLib, GObject, Gio, St } = imports.gi;
 
+const Params = imports.misc.params;
+
 var ANIMATED_ICON_UPDATE_TIMEOUT = 16;
 var SPINNER_ANIMATION_TIME = 300;
 var SPINNER_ANIMATION_DELAY = 1000;
@@ -136,12 +138,15 @@ class AnimatedIcon extends Animation {
 
 var Spinner = GObject.registerClass(
 class Spinner extends AnimatedIcon {
-    _init(size, animate = false) {
+    _init(size, params) {
+        params = Params.parse(params, {
+            animate: false,
+        });
         let file = Gio.File.new_for_uri('resource:///org/gnome/shell/theme/process-working.svg');
         super._init(file, size);
 
         this.opacity = 0;
-        this._animate = animate;
+        this._animate = params.animate;
     }
 
     _onDestroy() {

js/ui/components/keyring.js
@@ -78,7 +78,9 @@ class KeyringDialog extends ModalDialog.ModalDialog {
             ShellEntry.addContextMenu(this._passwordEntry, { isPassword: true });
             this._passwordEntry.clutter_text.connect('activate', this._onPasswordActivate.bind(this));
 
-            this._workSpinner = new Animation.Spinner(WORK_SPINNER_ICON_SIZE, true);
+            this._workSpinner = new Animation.Spinner(WORK_SPINNER_ICON_SIZE, {
+                animate: true,
+            });
 
             if (rtl) {
                 layout.attach(this._workSpinner, 0, row, 1, 1);

js/ui/panel.js
@@ -237,7 +237,9 @@ var AppMenuButton = GObject.registerClass({
         this._overviewHidingId = Main.overview.connect('hiding', this._sync.bind(this));
         this._overviewShowingId = Main.overview.connect('showing', this._sync.bind(this));
 
-        this._spinner = new Animation.Spinner(PANEL_ICON_SIZE, true);
+        this._spinner = new Animation.Spinner(PANEL_ICON_SIZE, {
+            animate: true,
+        });
         this._container.add_actor(this._spinner);
 
         let menu = new AppMenu(this);

js/ui/shellMountOperation.js
@@ -362,7 +362,9 @@ var ShellMountPasswordDialog = GObject.registerClass({
         this._passwordEntry.clutter_text.set_password_char('\u25cf'); // ● U+25CF BLACK CIRCLE
         ShellEntry.addContextMenu(this._passwordEntry, { isPassword: true });
         this.setInitialKeyFocus(this._passwordEntry);
-        this._workSpinner = new Animation.Spinner(WORK_SPINNER_ICON_SIZE, true);
+        this._workSpinner = new Animation.Spinner(WORK_SPINNER_ICON_SIZE, {
+            animate: true,
+        });
         this._passwordEntry.secondary_icon = this._workSpinner;
 
         if (rtl) {

commit acaa9f7f77361a87a7fcb79f8a37e7eebb7a9f34

polkitAgent: Fix spinner
Commit 6af25b282c accidentally changed the case of the property.

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/840

js/ui/components/polkitAgent.js
@@ -103,7 +103,7 @@ var AuthenticationDialog = GObject.registerClass({
             GObject.BindingFlags.SYNC_CREATE);
         this._passwordBox.add_child(this._passwordEntry);
 
-        this._workspinner = new Animation.Spinner(WORK_SPINNER_ICON_SIZE, {
+        this._workSpinner = new Animation.Spinner(WORK_SPINNER_ICON_SIZE, {
             animate: true,
         });
         this._passwordBox.add(this._workSpinner);