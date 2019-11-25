6c5922641fdbdcf67aa87ab01898b6524b1443a9
Florian Müllner <fmuellner@gnome.org>
2019-11-25 01:49:04 +0100

    extensions-tool: Fix removing from settings list
    When removing a string from a settings list, we iterate over all
    existing entries and copy all strings except the one that's being
    removed to a new list, which is then written to GSettings.
    
    However we currently always increment the index, so we end up with
    a NULL entry in place of the removed entry, which is then interpreted
    as the end of the list. In other words, we also remove all entries
    that follow the removed string.
    
    Fix this by looping over the list entries instead of the index, and
    only increment the index for entries we copy.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/issues/1946

--- src/extensions-tool/main.c
+++ src/extensions-tool/main.c
@@ -142,9 +142,9 @@ settings_list_remove (GSettings  *settings,
 
   n_values = g_strv_length (list);
   new_value = g_new0 (char *, n_values);
-  for (i = 0, s = (const char **)list; i < n_values; i++, s++)
+  for (i = 0, s = (const char **)list; *s != NULL; s++)
     if (!g_str_equal (*s, value))
-      new_value[i] = g_strdup (*s);
+      new_value[i++] = g_strdup (*s);
 
   g_settings_set_strv (settings, key, (const char **)new_value);
   g_settings_sync ();
