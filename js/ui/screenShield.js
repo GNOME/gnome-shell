10006c460cb556c236fa7afe7e37b3bf2de73742
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2019-11-29 12:29:41 -0300

    screenShield: Only animate the unlock dialog
    Remove the slide-up-down animation from the lock shield.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/872

--- js/ui/screenShield.js
+++ js/ui/screenShield.js
@@ -354,7 +354,7 @@ var ScreenShield = class {
 
         this._lockScreenState = MessageTray.State.HIDING;
 
-        this._lockScreenGroup.remove_all_transitions();
+        this._lockDialogGroup.remove_all_transitions();
 
         if (animate) {
             // Tween the lock screen out of screen
@@ -362,15 +362,9 @@ var ScreenShield = class {
             // use the same speed regardless of original position
             // if velocity is specified, it's in pixels per milliseconds
             let h = global.stage.height;
-            let delta = h + this._lockScreenGroup.translation_y;
+            let delta = h + this._lockDialogGroup.translation_y;
             let velocity = global.stage.height / CURTAIN_SLIDE_TIME;
             let duration = delta / velocity;
-
-            this._lockScreenGroup.ease({
-                translation_y: -h,
-                duration,
-                mode: Clutter.AnimationMode.EASE_IN_QUAD,
-            });
 
             this._lockDialogGroup.ease({
                 translation_y: -h,
@@ -433,14 +427,6 @@ var ScreenShield = class {
         let fadeToBlack = params.fadeToBlack;
 
         if (params.animateLockScreen) {
-            this._lockScreenGroup.translation_y = -global.screen_height;
-            this._lockScreenGroup.remove_all_transitions();
-            this._lockScreenGroup.ease({
-                translation_y: 0,
-                duration: MANUAL_FADE_TIME,
-                mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-            });
-
             this._lockDialogGroup.translation_y = -global.screen_height;
             this._lockDialogGroup.remove_all_transitions();
             this._lockDialogGroup.ease({
