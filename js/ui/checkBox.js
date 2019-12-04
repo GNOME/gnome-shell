GNOME/gnome-shell

commit f2bd39b20c89c1896c124f1dccdd587aa544e45c

js: Use generic actor properties to align StBin children
StBin's fill/align properties are now no-ops; get back the intended
child allocation by setting the corresponding x/y-align on the child.

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/803

js/ui/checkBox.js
@@ -11,11 +11,12 @@ class CheckBox extends St.Button {
             button_mask: St.ButtonMask.ONE,
             toggle_mode: true,
             can_focus: true,
-            x_fill: true,
-            y_fill: true
         });
 
-        this._box = new St.Bin();
+        this._box = new St.Bin({
+            x_expand: true,
+            y_expand: true,
+        });
         this._box.set_y_align(Clutter.ActorAlign.START);
         container.add_actor(this._box);
 

js/gdm/authPrompt.js
@@ -88,8 +88,6 @@ var AuthPrompt = GObject.registerClass({
         this.connect('destroy', this._onDestroy.bind(this));
 
         this._userWell = new St.Bin({
-            x_fill: true,
-            x_align: St.Align.START,
             x_expand: true,
             y_expand: true,
         });
@@ -447,6 +445,7 @@ var AuthPrompt = GObject.registerClass({
 
         if (user) {
             let userWidget = new UserWidget.UserWidget(user);
+            userWidget.x_align = Clutter.ActorAlign.START;
             this._userWell.set_child(userWidget);
         }
     }

js/gdm/loginDialog.js
@@ -42,7 +42,10 @@ var UserListItem = GObject.registerClass({
     Signals: { 'activate': {} }
 }, class UserListItem extends St.Button {
     _init(user) {
-        let layout = new St.BoxLayout({ vertical: true });
+        let layout = new St.BoxLayout({
+            vertical: true,
+            x_align: Clutter.ActorAlign.START,
+        });
         super._init({
             style_class: 'login-dialog-user-list-item',
             button_mask: St.ButtonMask.ONE | St.ButtonMask.THREE,
@@ -50,8 +53,6 @@ var UserListItem = GObject.registerClass({
             x_expand: true,
             child: layout,
             reactive: true,
-            x_align: St.Align.START,
-            x_fill: true
         });
 
         this.user = user;
@@ -456,15 +457,18 @@ var LoginDialog = GObject.registerClass({
         // translators: this message is shown below the user list on the
         // login screen. It can be activated to reveal an entry for
         // manually entering the username.
-        let notListedLabel = new St.Label({ text: _("Not listed?"),
-                                            style_class: 'login-dialog-not-listed-label' });
-        this._notListedButton = new St.Button({ style_class: 'login-dialog-not-listed-button',
-                                                button_mask: St.ButtonMask.ONE | St.ButtonMask.THREE,
-                                                can_focus: true,
-                                                child: notListedLabel,
-                                                reactive: true,
-                                                x_align: St.Align.START,
-                                                x_fill: true });
+        let notListedLabel = new St.Label({
+            text: _("Not listed?"),
+            style_class: 'login-dialog-not-listed-label',
+            x_align: Clutter.ActorAlign.START,
+        });
+        this._notListedButton = new St.Button({
+            style_class: 'login-dialog-not-listed-button',
+            button_mask: St.ButtonMask.ONE | St.ButtonMask.THREE,
+            can_focus: true,
+            child: notListedLabel,
+            reactive: true,
+        });
 
         this._notListedButton.connect('clicked', this._hideUserListAskForUsernameAndBeginVerification.bind(this));
 

js/ui/altTab.js
@@ -655,7 +655,7 @@ class AppIcon extends St.BoxLayout {
 
         this.app = app;
         this.icon = null;
-        this._iconBin = new St.Bin({ x_fill: true, y_fill: true });
+        this._iconBin = new St.Bin();
 
         this.add_child(this._iconBin);
         this.label = new St.Label({
@@ -894,12 +894,13 @@ class ThumbnailList extends SwitcherPopup.SwitcherList {
 
             let title = windows[i].get_title();
             if (title) {
-                let name = new St.Label({ text: title });
-                // St.Label doesn't support text-align so use a Bin
-                let bin = new St.Bin({ x_align: St.Align.MIDDLE });
+                let name = new St.Label({
+                    text: title,
+                    // St.Label doesn't support text-align
+                    x_align: Clutter.ActorAlign.CENTER,
+                });
                 this._labels.push(bin);
-                bin.add_actor(name);
-                box.add_actor(bin);
+                box.add_actor(name);
 
                 this.addItem(box, name);
             } else {

js/ui/appDisplay.js
@@ -305,10 +305,7 @@ var AllView = GObject.registerClass({
         this._scrollView = new St.ScrollView({ style_class: 'all-apps',
                                                x_expand: true,
                                                y_expand: true,
-                                               x_fill: true,
-                                               y_fill: false,
-                                               reactive: true,
-                                               y_align: St.Align.START });
+                                               reactive: true, });
         this.add_actor(this._scrollView);
         this._grid._delegate = this;
 
@@ -327,7 +324,10 @@ var AllView = GObject.registerClass({
         this.folderIcons = [];
 
         this._stack = new St.Widget({ layout_manager: new Clutter.BinLayout() });
-        let box = new St.BoxLayout({ vertical: true });
+        let box = new St.BoxLayout({
+            vertical: true,
+            y_align: Clutter.ActorAlign.START,
+        });
 
         this._grid.currentPage = 0;
         this._stack.add_actor(this._grid);
@@ -1046,8 +1046,11 @@ class AppDisplay extends St.BoxLayout {
         this._viewStackLayout.connect('allocated-size-changed', this._onAllocatedSizeChanged.bind(this));
         this.add_actor(this._viewStack);
         let layout = new ControlsBoxLayout({ homogeneous: true });
-        this._controls = new St.Widget({ style_class: 'app-view-controls',
-                                         layout_manager: layout });
+        this._controls = new St.Widget({
+            style_class: 'app-view-controls',
+            layout_manager: layout,
+            x_align: Clutter.ActorAlign.CENTER,
+        });
         this._controls.connect('notify::mapped', () => {
             // controls are faded either with their parent or
             // explicitly in animate(); we can't know how they'll be
@@ -1266,15 +1269,18 @@ class FolderView extends BaseAppView {
 
         this._scrollView = new St.ScrollView({
             overlay_scrollbars: true,
-            x_fill: true,
-            y_fill: true,
             x_expand: true,
             y_expand: true
         });
         this._scrollView.set_policy(St.PolicyType.NEVER, St.PolicyType.AUTOMATIC);
         this.add_actor(this._scrollView);
 
-        let scrollableContainer = new St.BoxLayout({ vertical: true, reactive: true });
+        let scrollableContainer = new St.BoxLayout({
+            vertical: true,
+            reactive: true,
+            x_expand: true,
+            y_expand: true,
+        });
         scrollableContainer.add_actor(this._grid);
         this._scrollView.add_actor(scrollableContainer);
 
@@ -1462,8 +1468,6 @@ var FolderIcon = GObject.registerClass({
             button_mask: St.ButtonMask.ONE,
             toggle_mode: true,
             can_focus: true,
-            x_fill: true,
-            y_fill: true
         });
         this.id = id;
         this.name = '';
@@ -1902,10 +1906,7 @@ var AppFolderPopup = GObject.registerClass({
 
         this._boxPointer = new BoxPointer.BoxPointer(this._arrowSide,
                                                      { style_class: 'app-folder-popup-bin',
-                                                       x_fill: true,
-                                                       y_fill: true,
-                                                       x_expand: true,
-                                                       x_align: St.Align.START });
+                                                       x_expand: true, });
 
         this._boxPointer.style_class = 'app-folder-popup';
         this.add_actor(this._boxPointer);
@@ -2057,8 +2058,6 @@ var AppIcon = GObject.registerClass({
             reactive: true,
             button_mask: St.ButtonMask.ONE | St.ButtonMask.TWO,
             can_focus: true,
-            x_fill: true,
-            y_fill: true
         });
 
         this.app = app;

js/ui/calendar.js
@@ -790,8 +790,8 @@ class EventsSection extends MessageList.MessageListSection {
 
         this._title = new St.Button({ style_class: 'events-section-title',
                                       label: '',
-                                      x_align: St.Align.START,
                                       can_focus: true });
+        this._title.child.x_align = Clutter.ActorAlign.START;
         this.insert_child_below(this._title, null);
 
         this._title.connect('clicked', this._onTitleClicked.bind(this));
@@ -1078,8 +1078,7 @@ class CalendarMessageList extends St.Widget {
 
         this._scrollView = new St.ScrollView({ style_class: 'vfade',
                                                overlay_scrollbars: true,
-                                               x_expand: true, y_expand: true,
-                                               x_fill: true, y_fill: true });
+                                               x_expand: true, y_expand: true, });
         this._scrollView.set_policy(St.PolicyType.NEVER, St.PolicyType.AUTOMATIC);
         box.add_actor(this._scrollView);
 
@@ -1098,6 +1097,7 @@ class CalendarMessageList extends St.Widget {
 
         this._sectionList = new St.BoxLayout({ style_class: 'message-list-sections',
                                                vertical: true,
+                                               x_expand: true,
                                                y_expand: true,
                                                y_align: Clutter.ActorAlign.START });
         this._sectionList.connect('actor-added', this._sync.bind(this));

js/ui/components/autorunManager.js
@@ -1,7 +1,7 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 /* exported Component */
 
-const { Gio, GObject, St } = imports.gi;
+const { Clutter, Gio, GObject, St } = imports.gi;
 
 const GnomeSession = imports.misc.gnomeSession;
 const Main = imports.ui.main;
@@ -320,20 +320,23 @@ class AutorunNotification extends MessageTray.Notification {
     }
 
     _buttonForApp(app) {
-        let box = new St.BoxLayout();
+        let box = new St.BoxLayout({
+            x_expand: true,
+            x_align: Clutter.ActorAlign.START,
+        });
         let icon = new St.Icon({ gicon: app.get_icon(),
                                  style_class: 'hotplug-notification-item-icon' });
         box.add(icon);
 
         let label = new St.Bin({
-            y_align: St.Align.MIDDLE,
-            child: new St.Label({ text: _("Open with %s").format(app.get_name()) }),
+            child: new St.Label({
+                text: _("Open with %s").format(app.get_name()),
+                y_align: Clutter.ActorAlign.CENTER,
+            }),
         });
         box.add(label);
 
         let button = new St.Button({ child: box,
-                                     x_fill: true,
-                                     x_align: St.Align.START,
                                      x_expand: true,
                                      button_mask: St.ButtonMask.ONE,
                                      style_class: 'hotplug-notification-item button' });

js/ui/dash.js
@@ -285,9 +285,12 @@ var DashActor = GObject.registerClass(
 class DashActor extends St.Widget {
     _init() {
         let layout = new Clutter.BoxLayout({ orientation: Clutter.Orientation.VERTICAL });
-        super._init({ name: 'dash',
-                      layout_manager: layout,
-                      clip_to_allocation: true });
+        super._init({
+            name: 'dash',
+            layout_manager: layout,
+            clip_to_allocation: true,
+            y_align: Clutter.ActorAlign.CENTER,
+        });
     }
 
     vfunc_allocate(box, flags) {

commit 04754e3e5c56a1bdf10df1c4221315fdab77c715

checkBox: Fix expand and align properties
There's no need for the checkbox StBin to expand, it has fixed width and
height set in the theme anyway.

This fixes the layout of checkboxes, which broke with
f2bd39b20c89c1896c124f1dccdd587aa544e45c.

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/885

js/ui/checkBox.js
@@ -14,8 +14,7 @@ class CheckBox extends St.Button {
         });
 
         this._box = new St.Bin({
-            x_expand: true,
-            y_expand: true,
+            x_align: Clutter.ActorAlign.START,
             y_align: Clutter.ActorAlign.START,
         });
         container.add_actor(this._box);