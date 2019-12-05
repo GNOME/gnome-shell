d372ffea68c5720370607f9dddb89a54c9060c29
Jonas Dreßler <verdre@v0yd.nl>
2019-12-05 10:55:00 +0100

    dialog: Remove messageBox container from MessageDialogContent
    `MessageDialogContent.messageBox` is not really needed, the styling can
    also be done using other CSS classes and this makes it a bit more
    straightforward to add actors to the MessageDialogContent.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/886

--- js/ui/components/keyring.js
+++ js/ui/components/keyring.js
@@ -145,7 +145,7 @@ class KeyringDialog extends ModalDialog.ModalDialog {
         }
 
         this._controlTable = table;
-        this._content.messageBox.add_child(table);
+        this._content.add_actor(table);
     }
 
     _updateSensitivity(sensitive) {
