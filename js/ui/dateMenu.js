87dd30c27142a1b5f043c8deeb7dff7e5fcff119
Florian Müllner <fmuellner@gnome.org>
2019-12-19 00:43:12 +0100

    dateMenu: Move weather forecast time above icon
    The time is de-emphasized like the header, grouping them together
    helps to further accentuate the more important information.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/issues/1143

--- js/ui/dateMenu.js
+++ js/ui/dateMenu.js
@@ -350,6 +350,11 @@ class WeatherSection extends St.Button {
             });
             const [, tempValue] = fc.get_value_temp(GWeather.TemperatureUnit.DEFAULT);
 
+            let time = new St.Label({
+                style_class: 'weather-forecast-time',
+                text: timeStr,
+                x_align: Clutter.ActorAlign.CENTER,
+            });
             let icon = new St.Icon({
                 style_class: 'weather-forecast-icon',
                 icon_name: fc.get_symbolic_icon_name(),
@@ -361,18 +366,13 @@ class WeatherSection extends St.Button {
                 text: '%.0f°'.format(tempValue),
                 x_align: Clutter.ActorAlign.CENTER,
             });
-            let time = new St.Label({
-                style_class: 'weather-forecast-time',
-                text: timeStr,
-                x_align: Clutter.ActorAlign.CENTER,
-            });
 
             temp.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
             time.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
 
-            layout.attach(icon, col, 0, 1, 1);
-            layout.attach(temp, col, 1, 1, 1);
-            layout.attach(time, col, 2, 1, 1);
+            layout.attach(time, col, 0, 1, 1);
+            layout.attach(icon, col, 1, 1, 1);
+            layout.attach(temp, col, 2, 1, 1);
             col++;
         });
     }
