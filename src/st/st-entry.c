69800716ee593c645bee213bc4d11282f216b2b2
Jonas Dreßler <verdre@v0yd.nl>
2020-01-22 20:53:34 +0100

    st/entry: Show hint actor while entry is focused
    Also show the hint actor of an StEntry while the entry is focused but
    has no text inside it. This is part of the new dialog and lock-screen
    design where there are no labels before entries anymore and labels are
    instead shown as a hint-text of the entry.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/944

--- src/st/st-entry.c
+++ src/st/st-entry.c
@@ -232,8 +232,7 @@ st_entry_update_hint_visibility (StEntry *self)
   StEntryPrivate *priv = ST_ENTRY_PRIV (self);
   gboolean hint_visible =
     priv->hint_actor != NULL &&
-    strcmp (clutter_text_get_text (CLUTTER_TEXT (priv->entry)), "") == 0 &&
-    !HAS_FOCUS (priv->entry);
+    strlen (clutter_text_get_text (CLUTTER_TEXT (priv->entry))) == 0;
 
   if (priv->hint_actor)
     g_object_set (priv->hint_actor, "visible", hint_visible, NULL);
@@ -547,6 +546,8 @@ clutter_text_changed_cb (GObject    *object,
 {
   StEntry *entry = ST_ENTRY (user_data);
   StEntryPrivate *priv = ST_ENTRY_PRIV (entry);
+
+  st_entry_update_hint_visibility (entry);
 
   /* Since the text changed, force a regen of the shadow texture */
   cogl_clear_object (&priv->text_shadow_material);
@@ -1063,8 +1064,6 @@ st_entry_set_text (StEntry     *entry,
   priv = st_entry_get_instance_private (entry);
 
   clutter_text_set_text (CLUTTER_TEXT (priv->entry), text);
-
-  st_entry_update_hint_visibility (entry);
 
   g_object_notify_by_pspec (G_OBJECT (entry), props[PROP_TEXT]);
 }
