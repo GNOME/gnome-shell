3952ef2fbe8851141c19fb2cc19a828ff63a5aa5
Florian Müllner <fmuellner@gnome.org>
2020-01-24 18:09:34 +0100

    extensionPrefs: Add application icon
    We are about to make the tool a user-visible application, so we
    need an icon. Add one (plus its symbolic variant).
    
    https://gitlab.gnome.org/GNOME/gnome-shell/issues/1968

--- meson/postinstall.py
+++ meson/postinstall.py
@@ -8,6 +8,12 @@ datadir = os.path.join(prefix, 'share')
 
 # Packaging tools define DESTDIR and this isn't needed for them
 if 'DESTDIR' not in os.environ:
+    print('Updating icon cache...')
+    icon_cache_dir = os.path.join(datadir, 'icons', 'hicolor')
+    if not os.path.exists(icon_cache_dir):
+        os.makedirs(icon_cache_dir)
+    subprocess.call(['gtk-update-icon-cache', '-qtf', icon_cache_dir])
+
     print('Updating desktop database...')
     desktop_database_dir = os.path.join(datadir, 'applications')
     if not os.path.exists(desktop_database_dir):
