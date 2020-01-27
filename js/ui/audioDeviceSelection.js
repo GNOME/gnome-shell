eec25367fcfac1876522a5368b669129e624df83
Jonas Dreßler <verdre@v0yd.nl>
2020-01-27 22:30:12 +0100

    audioDeviceSelection: Adapt to new dialog design
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/962

--- js/ui/audioDeviceSelection.js
+++ js/ui/audioDeviceSelection.js
@@ -1,6 +1,7 @@
 /* exported AudioDeviceSelectionDBus */
 const { Clutter, Gio, GLib, GObject, Meta, Shell, St } = imports.gi;
 
+const Dialog = imports.ui.dialog;
 const Main = imports.ui.main;
 const ModalDialog = imports.ui.modalDialog;
 
@@ -36,18 +37,17 @@ var AudioDeviceSelectionDialog = GObject.registerClass({
     }
 
     _buildLayout() {
-        let title = new St.Label({ style_class: 'audio-selection-title',
-                                   text: _("Select Audio Device"),
-                                   x_align: Clutter.ActorAlign.CENTER });
-
-        this.contentLayout.style_class = 'audio-selection-content';
-        this.contentLayout.add(title);
+        let content = new Dialog.MessageDialogContent({
+            title: _('Select Audio Device'),
+        });
 
         this._selectionBox = new St.BoxLayout({
             style_class: 'audio-selection-box',
             x_expand: true,
         });
-        this.contentLayout.add_child(this._selectionBox);
+        content.add_child(this._selectionBox);
+
+        this.contentLayout.add_child(content);
 
         if (Main.sessionMode.allowSettings) {
             this.addButton({ action: this._openSettings.bind(this),
