1446219544c40e3e7a0b836607e384d451559532
Jonas Dreßler <verdre@v0yd.nl>
2020-02-10 15:21:29 +0100

    blur-effect: Add some more documentation about performance
    Explicitely mention that this effect works best for large blur radii and
    can be slow if the blurred actor/image is large and not being scaled.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/991

--- src/shell-blur-effect.c
+++ src/shell-blur-effect.c
@@ -42,7 +42,10 @@
  * # Optimizations
  *
  * There are a number of optimizations in place to make this blur implementation
- * real-time.
+ * real-time. All in all, the implementation performs best when using large
+ * blur-radiuses that allow downscaling the texture to smaller sizes, at
+ * small radiuses where no downscaling is possible this can easily halve the
+ * framerate.
  *
  * ## Multipass
  *
