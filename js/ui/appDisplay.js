8961db29210c89e2b22c3e93165215fe14fb4591
Jonas Dreßler <verdre@v0yd.nl>
2020-02-17 14:25:17 +0100

    appDisplay: Use empty icons as placeholders in folder icons
    Use empty StIcons instead of StBins as the placeholders for the
    folder-preview icons. This has the advantage that we can use the
    unscaled size as icon_size for the StIcon and it is also updated on
    scale factor changes, which isn't working right now.
    
    Depends on https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/932

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1346,15 +1346,15 @@ class FolderView extends BaseAppView {
         });
         layout.hookup_style(icon);
         let subSize = Math.floor(FOLDER_SUBICON_FRACTION * size);
-        let scale = St.ThemeContext.get_for_stage(global.stage).scale_factor;
 
         let numItems = this._orderedItems.length;
         let rtl = icon.get_text_direction() == Clutter.TextDirection.RTL;
         for (let i = 0; i < 4; i++) {
-            let bin = new St.Bin({ width: subSize * scale, height: subSize * scale });
-            if (i < numItems)
-                bin.child = this._orderedItems[i].app.create_icon_texture(subSize);
-            layout.attach(bin, rtl ? (i + 1) % 2 : i % 2, Math.floor(i / 2), 1, 1);
+            let subIcon = i < numItems
+                ? this._orderedItems[i].app.create_icon_texture(subSize)
+                : new St.Icon({ icon_size: subSize });
+
+            layout.attach(subIcon, rtl ? (i + 1) % 2 : i % 2, Math.floor(i / 2), 1, 1);
         }
 
         return icon;
