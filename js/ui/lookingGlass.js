GNOME/gnome-shell

commit 9d941f8202125fa235c64f7bfb5277919bc7286e

js: Don't use templates in files with translations
xgettext gained some support for template strings, and no longer
fails when encountering '/' somewhere between backticks.

Unfortunately its support is still buggy as hell, and it is now
silently dropping translatable strings, yay. I hate making the
code worse, but until xgettext really gets its shit together,
the only viable way forward seems to be to not use template
strings in any files listed in POTFILES.

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/1014

js/ui/lookingGlass.js
@@ -244,7 +244,7 @@ function objectToString(o) {
         // special case this since the default is way, way too verbose
         return '<js function>';
     } else {
-        return `${o}`;
+        return o.toString();
     }
 }
 
@@ -291,7 +291,7 @@ class Result extends St.BoxLayout {
         this.add(cmdTxt);
         let box = new St.BoxLayout({});
         this.add(box);
-        let resultTxt = new St.Label({ text: `r(${index}) = ` });
+        let resultTxt = new St.Label({ text: 'r(%d) = '.format(index) });
         resultTxt.clutter_text.ellipsize = Pango.EllipsizeMode.END;
         box.add(resultTxt);
         let objLink = new ObjLink(this._lookingGlass, o);
@@ -331,7 +331,7 @@ var WindowList = GObject.registerClass({
             box.add_child(windowLink);
             let propsBox = new St.BoxLayout({ vertical: true, style: 'padding-left: 6px;' });
             box.add(propsBox);
-            propsBox.add(new St.Label({ text: `wmclass: ${metaWindow.get_wm_class()}` }));
+            propsBox.add(new St.Label({ text: 'wmclass: %s'.format(metaWindow.get_wm_class()) }));
             let app = tracker.get_window_app(metaWindow);
             if (app != null && !app.is_window_backed()) {
                 let icon = app.create_icon_texture(22);
@@ -424,7 +424,7 @@ class ObjInspector extends St.ScrollView {
                     link = new St.Label({ text: '<error>' });
                 }
                 let box = new St.BoxLayout();
-                box.add(new St.Label({ text: `${propName}: ` }));
+                box.add(new St.Label({ text: '%s: '.format(propName) }));
                 box.add(link);
                 this._container.add_actor(box);
             }
@@ -641,9 +641,9 @@ var Inspector = GObject.registerClass({
             this._target = target;
         this._pointerTarget = target;
 
-        let position = `[inspect x: ${stageX} y: ${stageY}]`;
+        let position = '[inspect x: %d y: %d]'.format(stageX, stageY);
         this._displayText.text = '';
-        this._displayText.text = `${position} ${this._target}`;
+        this._displayText.text = '%s %s'.format(position, this._target);
 
         this._lookingGlass.setBorderPaintTarget(this._target);
     }
@@ -846,7 +846,7 @@ class LookingGlass extends St.BoxLayout {
         inspectIcon.connect('button-press-event', () => {
             let inspector = new Inspector(this);
             inspector.connect('target', (i, target, stageX, stageY) => {
-                this._pushResult(`inspect(${Math.round(stageX)}, ${Math.round(stageY)})`, target);
+                this._pushResult('inspect(%d, %d)'.format(Math.round(stageX), Math.round(stageY)), target);
             });
             inspector.connect('closed', () => {
                 this.show();
@@ -952,9 +952,8 @@ class LookingGlass extends St.BoxLayout {
         // monospace font to be bold/oblique/etc. Could easily be added here.
         let size = fontDesc.get_size() / 1024.;
         let unit = fontDesc.get_size_is_absolute() ? 'px' : 'pt';
-        this.style = `
-            font-size: ${size}${unit};
-            font-family: "${fontDesc.get_family()}";`;
+        this.style = 'font-size: %d%s; font-family: "%s";'.format(
+            size, unit, fontDesc.get_family());
     }
 
     setBorderPaintTarget(obj) {
@@ -1035,7 +1034,7 @@ class LookingGlass extends St.BoxLayout {
         this._history.addItem(command);
 
         let lines = command.split(';');
-        lines.push(`return ${lines.pop()}`);
+        lines.push('return %s'.format(lines.pop()));
 
         let fullCmd = commandHeader + lines.join(';');
 
@@ -1043,7 +1042,7 @@ class LookingGlass extends St.BoxLayout {
         try {
             resultObj = Function(fullCmd)();
         } catch (e) {
-            resultObj = `<exception ${e}>`;
+            resultObj = '<exception %s>'.format(e.toString());
         }
 
         this._pushResult(command, resultObj);
@@ -1062,7 +1061,7 @@ class LookingGlass extends St.BoxLayout {
         try {
             return this._resultsArea.get_child_at_index(idx - this._offset).o;
         } catch (e) {
-            throw new Error(`Unknown result at index ${idx}`);
+            throw new Error('Unknown result at index %d'.format(idx));
         }
     }
 

js/extensionPrefs/main.js
@@ -277,10 +277,10 @@ var ExtensionsWindow = GObject.registerClass({
         });
         box.add(expander);
 
-        let errortext = `${exc}\n\nStack trace:\n${
-            // Indent stack trace.
-            exc.stack.split('\n').map(line => `  ${line}`).join('\n')
-        }`;
+        let errortext = '%s\n\nStack trace:\n'.format(exc);
+        // Indent stack trace.
+        errortext +=
+            exc.stack.split('\n').map(line => '  %s'.format(line)).join('\n');
 
         let buffer = new Gtk.TextBuffer({ text: errortext });
         let textview = new Gtk.TextView({
@@ -315,9 +315,9 @@ var ExtensionsWindow = GObject.registerClass({
             let clipboard = Gtk.Clipboard.get_default(w.get_display());
             // markdown for pasting in gitlab issues
             let lines = [
-                `The settings of extension ${row.uuid} had an error:`,
+                'The settings of extension %s had an error:'.format(row.uuid),
                 '```', // '`' (xgettext throws up on odd number of backticks)
-                `${exc}`,
+                exc.toString(),
                 '```', // '`'
                 '',
                 'Stack trace:',
@@ -403,7 +403,7 @@ var ExtensionsWindow = GObject.registerClass({
         this._shellProxy.ListExtensionsRemote(([extensionsMap], e) => {
             if (e) {
                 if (e instanceof Gio.DBusError) {
-                    log(`Failed to connect to shell proxy: ${e}`);
+                    log('Failed to connect to shell proxy: %s'.format(e.toString()));
                     this._mainStack.visible_child_name = 'noshell';
                 } else {
                     throw e;
@@ -703,10 +703,10 @@ var ExtensionRow = GObject.registerClass({
 
         this._updatesIcon.visible = this.hasUpdate;
 
-        this._versionLabel.label = `${this.version}`;
+        this._versionLabel.label = this.version;
         this._versionLabel.visible = this.version !== '';
 
-        this._authorLabel.label = `${this.creator}`;
+        this._authorLabel.label = this.creator;
         this._authorLabel.visible = this.creator !== '';
     }
 
@@ -747,7 +747,7 @@ function initEnvironment() {
         },
 
         logError(s) {
-            log(`ERROR: ${s}`);
+            log('ERROR: %s'.format(s));
         },
 
         userdatadir: GLib.build_filenamev([GLib.get_user_data_dir(), 'gnome-shell']),

js/gdm/loginDialog.js
@@ -421,13 +421,13 @@ var LoginDialog = GObject.registerClass({
 
         this._settings = new Gio.Settings({ schema_id: GdmUtil.LOGIN_SCREEN_SCHEMA });
 
-        this._settings.connect(`changed::${GdmUtil.BANNER_MESSAGE_KEY}`,
+        this._settings.connect('changed::%s'.format(GdmUtil.BANNER_MESSAGE_KEY),
                                this._updateBanner.bind(this));
-        this._settings.connect(`changed::${GdmUtil.BANNER_MESSAGE_TEXT_KEY}`,
+        this._settings.connect('changed::%s'.format(GdmUtil.BANNER_MESSAGE_TEXT_KEY),
                                this._updateBanner.bind(this));
-        this._settings.connect(`changed::${GdmUtil.DISABLE_USER_LIST_KEY}`,
+        this._settings.connect('changed::%s'.format(GdmUtil.DISABLE_USER_LIST_KEY),
                                this._updateDisableUserList.bind(this));
-        this._settings.connect(`changed::${GdmUtil.LOGO_KEY}`,
+        this._settings.connect('changed::%s'.format(GdmUtil.LOGO_KEY),
                                this._updateLogo.bind(this));
 
         this._textureCache = St.TextureCache.get_default();

js/misc/systemActions.js
@@ -151,17 +151,17 @@ const SystemActions = GObject.registerClass({
         this._userManager.connect('user-removed',
                                   () => this._updateMultiUser());
 
-        this._lockdownSettings.connect(`changed::${DISABLE_USER_SWITCH_KEY}`,
+        this._lockdownSettings.connect('changed::%s'.format(DISABLE_USER_SWITCH_KEY),
                                        () => this._updateSwitchUser());
-        this._lockdownSettings.connect(`changed::${DISABLE_LOG_OUT_KEY}`,
+        this._lockdownSettings.connect('changed::%s'.format(DISABLE_LOG_OUT_KEY),
                                        () => this._updateLogout());
-        global.settings.connect(`changed::${ALWAYS_SHOW_LOG_OUT_KEY}`,
+        global.settings.connect('changed::%s'.format(ALWAYS_SHOW_LOG_OUT_KEY),
                                 () => this._updateLogout());
 
-        this._lockdownSettings.connect(`changed::${DISABLE_LOCK_SCREEN_KEY}`,
+        this._lockdownSettings.connect('changed::%s'.format(DISABLE_LOCK_SCREEN_KEY),
                                        () => this._updateLockScreen());
 
-        this._lockdownSettings.connect(`changed::${DISABLE_LOG_OUT_KEY}`,
+        this._lockdownSettings.connect('changed::%s'.format(DISABLE_LOG_OUT_KEY),
                                        () => this._updateHaveShutdown());
 
         this.forceUpdate();

js/misc/util.js
@@ -21,7 +21,7 @@ const _leadingJunk = '[\\s`(\\[{\'\\"<\u00AB\u201C\u2018]';
 const _notTrailingJunk = '[^\\s`!()\\[\\]{};:\'\\".,<>?\u00AB\u00BB\u200E\u200F\u201C\u201D\u2018\u2019\u202A\u202C]';
 
 const _urlRegexp = new RegExp(
-    `(^|${_leadingJunk})` +
+    '(^|%s)'.format(_leadingJunk) +
     '(' +
         '(?:' +
             '(?:http|https|ftp)://' +             // scheme://
@@ -33,12 +33,12 @@ const _urlRegexp = new RegExp(
         '(?:' +                                   // one or more:
             '[^\\s()<>]+' +                       // run of non-space non-()
             '|' +                                 // or
-            `${_balancedParens}` +                // balanced parens
+            '%s'.format(_balancedParens) +        // balanced parens
         ')+' +
         '(?:' +                                   // end with:
-            `${_balancedParens}` +                // balanced parens
+            '%s'.format(_balancedParens) +        // balanced parens
             '|' +                                 // or
-            `${_notTrailingJunk}` +               // last non-junk char
+            '%s'.format(_notTrailingJunk) +       // last non-junk char
         ')' +
     ')', 'gi');
 
@@ -153,7 +153,7 @@ function trySpawnCommandLine(commandLine) {
     } catch (err) {
         // Replace "Error invoking GLib.shell_parse_argv: " with
         // something nicer
-        err.message = err.message.replace(/[^:]*: /, `${_("Could not parse command:")}\n`);
+        err.message = err.message.replace(/[^:]*: /, '%s\n'.format(_('Could not parse command:')));
         throw err;
     }
 

js/portalHelper/main.js
@@ -23,7 +23,6 @@ const PortalHelperSecurityLevel = {
 };
 
 const CONNECTIVITY_CHECK_HOST = 'nmcheck.gnome.org';
-const CONNECTIVITY_CHECK_URI = `http://${CONNECTIVITY_CHECK_HOST}`;
 const CONNECTIVITY_RECHECK_RATELIMIT_TIMEOUT = 30 * GLib.USEC_PER_SEC;
 
 const HelperDBusInterface = loadInterfaceXML('org.gnome.Shell.PortalHelper');
@@ -103,7 +102,7 @@ class PortalWindow extends Gtk.ApplicationWindow {
         this._headerBar.show();
 
         if (!url) {
-            url = CONNECTIVITY_CHECK_URI;
+            url = 'http://%s'.format(CONNECTIVITY_CHECK_HOST);
             this._originalUrlWasGnome = true;
         } else {
             this._originalUrlWasGnome = false;

js/ui/accessDialog.js
@@ -138,7 +138,7 @@ var AccessDialogDBus = class {
         let [handle, appId, parentWindow_, title, description, body, options] = params;
         // We probably want to use parentWindow and global.display.focus_window
         // for this check in the future
-        if (appId && `${appId}.desktop` != this._windowTracker.focus_app.id) {
+        if (appId && '%s.desktop'.format(appId) != this._windowTracker.focus_app.id) {
             invocation.return_error_literal(Gio.DBusError,
                                             Gio.DBusError.ACCESS_DENIED,
                                             'Only the focused app is allowed to show a system access dialog');

js/ui/appDisplay.js
@@ -72,7 +72,7 @@ function _getFolderName(folder) {
 
     if (folder.get_boolean('translate')) {
         let keyfile = new GLib.KeyFile();
-        let path = `desktop-directories/${name}`;
+        let path = 'desktop-directories/%s'.format(name);
 
         try {
             keyfile.load_from_data_dirs(path, GLib.KeyFileFlags.NONE);
@@ -219,7 +219,7 @@ var BaseAppView = GObject.registerClass({
         if (this._items.has(id))
             this._items.get(id).navigate_focus(null, St.DirectionType.TAB_FORWARD, false);
         else
-            log(`No such application ${id}`);
+            log('No such application %s'.format(id));
     }
 
     selectApp(id) {
@@ -293,7 +293,7 @@ var BaseAppView = GObject.registerClass({
     }
 
     adaptToSize(_width, _height) {
-        throw new GObject.NotImplementedError(`adaptToSize in ${this.constructor.name}`);
+        throw new GObject.NotImplementedError('adaptToSize in %s'.format(this.constructor.name));
     }
 });
 
@@ -492,7 +492,7 @@ var AllView = GObject.registerClass({
 
         let folders = this._folderSettings.get_strv('folder-children');
         folders.forEach(id => {
-            let path = `${this._folderSettings.path}folders/${id}/`;
+            let path = '%sfolders/%s/'.format(this._folderSettings.path, id);
             let icon = this._items.get(id);
             if (!icon) {
                 icon = new FolderIcon(id, path, this);
@@ -2278,7 +2278,7 @@ var AppIcon = GObject.registerClass({
 
     shellWorkspaceLaunch(params) {
         let { stack } = new Error();
-        log(`shellWorkspaceLaunch is deprecated, use app.open_new_window() instead\n${stack}`);
+        log('shellWorkspaceLaunch is deprecated, use app.open_new_window() instead\n%s'.format(stack));
 
         params = Params.parse(params, { workspace: -1,
                                         timestamp: 0 });

commit 2407ab95fa4204d1f05936f2ffb5faf3a2fd3244

lookingGlass: Handle undefined in objectToString()
Fallout from commit 9d941f8202: Template strings handle undefined
values just fine, the replacement does not. Fix that.

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/1020

js/ui/lookingGlass.js
@@ -244,7 +244,7 @@ function objectToString(o) {
         // special case this since the default is way, way too verbose
         return '<js function>';
     } else {
-        return o.toString();
+        return o ? o.toString() : 'undefined';
     }
 }
 