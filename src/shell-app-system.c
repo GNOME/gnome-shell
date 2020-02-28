c6fdaf47f873ec9fa5f3e1a249b34880768df8e6
Christian Hergert <chergert@redhat.com>
2020-02-27 19:36:14 -0800

    app-cache: add ShellAppCache for GAppInfo caching
    This caches GAppInfo so that the compositor thread does not have to perform
    costly disk access to load them. Instead, they are loaded from a worker
    thread and the ShellAppCache notifies of changes.
    
    To simplify maintenance, ShellAppCache manages this directly and the
    existing ShellAppSystem wraps the cache. We may want to graft these
    together in the future, but now it provides the easiest way to backport
    changes to older Shell releases.
    
    Another source of compositor thread disk access was in determining the
    name for an application directory. Translations are provided via GKeyFile
    installed in "desktop-directories". Each time we would build the name
    for a label (or update it) we would have to load all of these files.
    
    Instead, the ShellAppCache caches that information and updates the cache
    in bulk when those change. We can reduce this in the future to do less
    work, but chances are these will come together anyway so that is probably
    worth fixing if we ever come across it.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/issues/2282

--- src/shell-app-system.c
+++ src/shell-app-system.c
@@ -9,6 +9,7 @@
 #include <gio/gio.h>
 #include <glib/gi18n.h>
 
+#include "shell-app-cache-private.h"
 #include "shell-app-private.h"
 #include "shell-window-tracker-private.h"
 #include "shell-app-system-private.h"
@@ -94,14 +95,14 @@ static void
 scan_startup_wm_class_to_id (ShellAppSystem *self)
 {
   ShellAppSystemPrivate *priv = self->priv;
-  GList *l;
+  const GList *l;
+  GList *all;
 
   g_hash_table_remove_all (priv->startup_wm_class_to_id);
 
-  g_list_free_full (priv->installed_apps, g_object_unref);
-  priv->installed_apps = g_app_info_get_all ();
+  all = shell_app_cache_get_all (shell_app_cache_get_default ());
 
-  for (l = priv->installed_apps; l != NULL; l = l->next)
+  for (l = all; l != NULL; l = l->next)
     {
       GAppInfo *info = l->data;
       const char *startup_wm_class, *id, *old_id;
@@ -131,7 +132,8 @@ app_is_stale (ShellApp *app)
   if (shell_app_is_window_backed (app))
     return FALSE;
 
-  info = g_desktop_app_info_new (shell_app_get_id (app));
+  info = shell_app_cache_get_info (shell_app_cache_get_default (),
+                                   shell_app_get_id (app));
   if (!info)
     return TRUE;
 
@@ -210,11 +212,9 @@ rescan_icon_theme (ShellAppSystem *self)
 }
 
 static void
-installed_changed (GAppInfoMonitor *monitor,
-                   gpointer         user_data)
+installed_changed (ShellAppCache  *cache,
+                   ShellAppSystem *self)
 {
-  ShellAppSystem *self = user_data;
-
   rescan_icon_theme (self);
   scan_startup_wm_class_to_id (self);
 
@@ -227,7 +227,7 @@ static void
 shell_app_system_init (ShellAppSystem *self)
 {
   ShellAppSystemPrivate *priv;
-  GAppInfoMonitor *monitor;
+  ShellAppCache *cache;
 
   self->priv = priv = shell_app_system_get_instance_private (self);
 
@@ -238,9 +238,9 @@ shell_app_system_init (ShellAppSystem *self)
 
   priv->startup_wm_class_to_id = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, g_free);
 
-  monitor = g_app_info_monitor_get ();
-  g_signal_connect (monitor, "changed", G_CALLBACK (installed_changed), self);
-  installed_changed (monitor, self);
+  cache = shell_app_cache_get_default ();
+  g_signal_connect (cache, "changed", G_CALLBACK (installed_changed), self);
+  installed_changed (cache, self);
 }
 
 static void
@@ -293,7 +293,7 @@ shell_app_system_lookup_app (ShellAppSystem   *self,
   if (app)
     return app;
 
-  info = g_desktop_app_info_new (id);
+  info = shell_app_cache_get_info (shell_app_cache_get_default (), id);
   if (!info)
     return NULL;
 
@@ -506,7 +506,5 @@ shell_app_system_search (const char *search_string)
 GList *
 shell_app_system_get_installed (ShellAppSystem *self)
 {
-  ShellAppSystemPrivate *priv = self->priv;
-
-  return priv->installed_apps;
+  return shell_app_cache_get_all (shell_app_cache_get_default ());
 }
