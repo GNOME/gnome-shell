3f9cf27f8a13158ebcaa50b7c868bbddddaeacaa
Christian Hergert <chergert@redhat.com>
2020-02-27 19:36:14 -0800

    app-cache: add ShellAppCache for GAppInfo caching
    This caches GAppInfo so that the compositor thread does not have to perform
    costly disk access to load them. Instead, they are loaded from a worker
    thread and the ShellAppCache notifies of changes.
    
    To simplfy maintenace, ShellAppCache manages this directly and the
    existing ShellAppSystem wraps the cache. We may want to graft these
    together in the future, but now it provides the easiest way to backport
    changes to older Shell releases.
    
    Another source of compositor thread disk access was in determining the
    name for an application directory. Translations are provided via GKeyFile
    installed in "desktop-directories". Each time we would build the name
    for a label (or update it) we would have to load all of these files.
    
    Instead, the ShellAppCache caches that information and updates the cache
    in bulk when those change. We can reduce this in the future to do less
    work, but chances are these will come together anyway so that is probably
    worth fixing if we every come across it.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/issues/2282

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -70,17 +70,8 @@ function _listsIntersect(a, b) {
 function _getFolderName(folder) {
     let name = folder.get_string('name');
 
-    if (folder.get_boolean('translate')) {
-        let keyfile = new GLib.KeyFile();
-        let path = 'desktop-directories/%s'.format(name);
-
-        try {
-            keyfile.load_from_data_dirs(path, GLib.KeyFileFlags.NONE);
-            name = keyfile.get_locale_string('Desktop Entry', 'Name', null);
-        } catch (e) {
-            return name;
-        }
-    }
+    if (folder.get_boolean('translate'))
+        return Shell.AppCache.get_default().translate_folder(name);
 
     return name;
 }
