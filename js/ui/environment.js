GNOME/gnome-shell

commit 06b690ff21204ea9ea26d2671eb26b1a1f24caa9

environment: reduce calls to g_time_zone_new_local()
Creating a new GTimeZone for the local timezone can be quite expensive if
done repeatedly. It requires an open(), mmap(), and parsing of
/etc/localtime.

This patch was provided by Florian, and I've tested it as far back as
3.28.4 to ensure that we are really reducing the number of open() calls
on the compositor thread.

https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/1051

Signed-off-by: Christian Hergert <chergert@redhat.com>

js/ui/environment.js
@@ -12,6 +12,9 @@ imports.gi.versions.TelepathyLogger = '0.2';
 
 const { Clutter, GLib, GObject, Meta, Shell, St } = imports.gi;
 const Gettext = imports.gettext;
+const System = imports.system;
+
+let _localTimeZone = null;
 
 // We can't import shell JS modules yet, because they may have
 // variable initializations, etc, that depend on init() already having
@@ -304,9 +307,25 @@ function init() {
         }
     };
 
+    // Override to clear our own timezone cache as well
+    const origClearDateCaches = System.clearDateCaches;
+    System.clearDateCaches = function () {
+        _localTimeZone = null;
+        origClearDateCaches();
+    };
+
     // Work around https://bugzilla.mozilla.org/show_bug.cgi?id=508783
     Date.prototype.toLocaleFormat = function (format) {
-        let dt = GLib.DateTime.new_from_unix_local(this.getTime() / 1000);
+        if (_localTimeZone === null)
+            _localTimeZone = GLib.TimeZone.new_local();
+
+        let dt = GLib.DateTime.new(_localTimeZone,
+            this.getYear(),
+            this.getMonth() + 1,
+            this.getDate(),
+            this.getHours(),
+            this.getMinutes(),
+            this.getSeconds());
         return dt ? dt.format(format) : '';
     };
 

commit 0dd171a7c899806e93254d879bc126431d99c36b

environment: Fix date conversion
This is a regression from commit 06b690ff21204:

GLib.DateTime.new() expects the full four-digit year, so passing
the abbreviated year from Date() will result in a bogus datetime.

Today is *not* Saturday March 2nd, 120 ...

https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1061

js/ui/environment.js
@@ -320,7 +320,7 @@ function init() {
             _localTimeZone = GLib.TimeZone.new_local();
 
         let dt = GLib.DateTime.new(_localTimeZone,
-            this.getYear(),
+            this.getFullYear(),
             this.getMonth() + 1,
             this.getDate(),
             this.getHours(),