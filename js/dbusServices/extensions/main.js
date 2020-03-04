a6de298891d4cbed4126db61a6b2398451fedab5
Florian Müllner <fmuellner@gnome.org>
2020-03-04 05:05:08 +0100

    dbusServices/extensions: Take over prefs dialog from app
    As outlined earlier, in order to turn the Extensions app into a properly
    sandboxed application, we need to split out the extension prefs dialog
    and move it elsewhere.
    
    With "elsewhere" being the new Extensions D-Bus service, effectively
    turning it into a shell extensions portal.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1106

--- js/dbusServices/extensions/main.js
+++ js/dbusServices/extensions/main.js
@@ -1,9 +1,16 @@
 /* exported main */
 
+imports.gi.versions.Gdk = '3.0';
+imports.gi.versions.Gtk = '3.0';
+
+const { Gtk } = imports.gi;
+
 const { DBusService } = imports.dbusService;
 const { ExtensionsService } = imports.extensionsService;
 
 function main() {
+    Gtk.init(null);
+
     const service = new DBusService(
         'org.gnome.Shell.Extensions',
         new ExtensionsService());
