508b6729febce53aa995a15853d7f730321dc7ae
Marco Trevisan (Treviño) <mail@3v1n0.net>
2020-03-20 20:06:55 +0100

    telepathyClient: Use GObjects based message objects
    As per commit b5676a2a5 ChatNotification is a GObject, but I was wrongly
    considering that it was using Tp.Message's as children, instead it just
    uses custom-built objects to pass information around through signals.
    
    Given gjs can only use GObjects as signal parameters, create a small wrapper
    class to hold the ChatNotification messages and use it as signal parameter.
    
    Fixes https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/2449
    
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1112

--- js/ui/components/telepathyClient.js
+++ js/ui/components/telepathyClient.js
@@ -2,7 +2,6 @@
 /* exported Component */
 
 const { Clutter, Gio, GLib, GObject, St } = imports.gi;
-const Lang = imports.lang;
 
 var Tpl = null;
 var Tp = null;
@@ -630,11 +629,20 @@ class ChatSource extends MessageTray.Source {
     }
 }) : null;
 
+const ChatNotificationMessage = HAVE_TP ? GObject.registerClass(
+class ChatNotificationMessage extends GObject.Object {
+    _init(props = {}) {
+        super._init();
+
+        Object.assign(this, props);
+    }
+}) : null;
+
 var ChatNotification = HAVE_TP ? GObject.registerClass({
     Signals: {
-        'message-removed': { param_types: [Tp.Message.$gtype] },
-        'message-added': { param_types: [Tp.Message.$gtype] },
-        'timestamp-changed': { param_types: [Tp.Message.$gtype] },
+        'message-removed': { param_types: [ChatNotificationMessage.$gtype] },
+        'message-added': { param_types: [ChatNotificationMessage.$gtype] },
+        'timestamp-changed': { param_types: [ChatNotificationMessage.$gtype] },
     },
 }, class ChatNotification extends MessageTray.Notification {
     _init(source) {
@@ -741,9 +749,10 @@ var ChatNotification = HAVE_TP ? GObject.registerClass({
             GLib.source_remove(this._timestampTimeoutId);
         this._timestampTimeoutId = 0;
 
-        let message = { realMessage: props.group != 'meta',
-                        showTimestamp: false };
-        Lang.copyProperties(props, message);
+        let message = new ChatNotificationMessage(Object.assign({
+            realMessage: props.group != 'meta',
+            showTimestamp: false,
+        }, props));
         delete message.noTimestamp;
 
         this.messages.unshift(message);
