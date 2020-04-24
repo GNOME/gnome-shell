e193ce5d610fa68f97229eeb229de5f54102d488
Matthew Leeds <matthew.leeds@endlessm.com>
2020-04-23 17:42:16 -0700

    screenShield: Fix use of null this._dialog
    On Fedora 32 if you close the laptop lid during gnome-initial-setup,
    gnome-shell hits this error:
    
    JS ERROR: Exception in callback for signal: prepare-for-sleep: TypeError: this._dialog is undefined
    _resetLockScreen@resource:///org/gnome/shell/ui/screenShield.js:434:9
    activate@resource:///org/gnome/shell/ui/screenShield.js:571:14
    lock@resource:///org/gnome/shell/ui/screenShield.js:617:14
    _prepareForSleep@resource:///org/gnome/shell/ui/screenShield.js:219:22
    _emit@resource:///org/gnome/gjs/modules/core/_signals.js:133:47
    _prepareForSleep@resource:///org/gnome/shell/misc/loginManager.js:198:14
    _emit@resource:///org/gnome/gjs/modules/core/_signals.js:133:47
    _convertToNativeSignal@resource:///org/gnome/gjs/modules/core/overrides/Gio.js:169:19
    
    This is because _ensureUnlockDialog() hit its first early return. So
    return early from activate() in that case, so this._dialog doesn't get
    used while it's null.

--- js/ui/screenShield.js
+++ js/ui/screenShield.js
@@ -431,7 +431,9 @@ var ScreenShield = class {
             this._lockScreenShown({ fadeToBlack, animateFade: false });
         }
 
-        this._dialog.grab_key_focus();
+        // In initial setup mode this._dialog is null
+        if (this._dialog)
+            this._dialog.grab_key_focus();
     }
 
     _lockScreenShown(params) {
@@ -561,7 +563,8 @@ var ScreenShield = class {
         if (this._activationTime == 0)
             this._activationTime = GLib.get_monotonic_time();
 
-        this._ensureUnlockDialog(true);
+        if (!this._ensureUnlockDialog(true))
+            return;
 
         this.actor.show();
 
