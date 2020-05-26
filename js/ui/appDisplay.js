51c596438268111e9d6b028065bb01683b204f16
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2020-05-26 01:41:43 -0300

    appDisplay: Using custom function to retrieve item page and position
    It is important that '_loadApps()' return a sorted list -- adding the
    same icons at the same positions but in different orders results in
    a wrong icon grid.
    
    Add support for using a custom positioning function, and implement it
    in AppDisplay. Because FolderView doesn't implement a custom sorting
    function, the items are still sorted alphabetically.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1284

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -291,16 +291,20 @@ var BaseAppView = GObject.registerClass({
         });
 
         // Add new app icons
-        const itemsPerPage = this._grid.itemsPerPage;
-        addedApps.forEach(icon => {
-            let iconIndex = newApps.indexOf(icon);
+        const getItemPosition = item => {
+            if (this._getItemPosition)
+                return this._getItemPosition(item);
 
-            this._orderedItems.splice(iconIndex, 0, icon);
-            this._items.set(icon.id, icon);
-
+            const iconIndex = newApps.indexOf(item);
+            const itemsPerPage = this._grid.itemsPerPage;
             const page = Math.floor(iconIndex / itemsPerPage);
             const position = iconIndex % itemsPerPage;
-            this._grid.addItem(icon, position, page);
+            return [page, position];
+        };
+
+        addedApps.forEach(icon => {
+            const [page, position] = getItemPosition(icon);
+            this._addItem(icon, page, position);
         });
 
         this._viewIsReady = true;
@@ -783,6 +787,24 @@ class AppDisplay extends BaseAppView {
 
     getAppInfos() {
         return this._appInfoList;
+    }
+
+    _getItemPosition(item) {
+        let [page, position] = this._pageManager.getAppPosition(item.id);
+        return [page, position];
+    }
+
+    _compareItems(a, b) {
+        const [aPage, aPosition] = this._pageManager.getAppPosition(a.id);
+        const [bPage, bPosition] = this._pageManager.getAppPosition(b.id);
+
+        if (aPage === -1 && bPage === -1)
+            return a.name.localeCompare(b.name);
+
+        if (aPage !== bPage)
+            return aPage - bPage;
+
+        return aPosition - bPosition;
     }
 
     _loadApps() {
