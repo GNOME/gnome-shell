27d390aadb75e37a5db44b01e1a6845a14e8e759
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2020-05-26 22:29:29 -0300

    appDisplay: Create app folder where the drop happened
    Create the app folder where the icon we drop at is located. This
    ensures the folder icon doesn't go into the last page, hidden and
    solitary.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1284

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1118,6 +1118,9 @@ class AppDisplay extends BaseAppView {
             return false;
         }
 
+        const [folderPage, folderPosition] =
+            this._grid.getItemPosition(this._items.get(apps[0]));
+
         let appItems = apps.map(id => this._items.get(id).app);
         let folderName = _findBestFolderName(appItems);
         if (!folderName)
@@ -1128,7 +1131,11 @@ class AppDisplay extends BaseAppView {
         newFolderSettings.set_strv('apps', apps);
         newFolderSettings.apply();
 
-        this.selectApp(newFolderId);
+        this._redisplay();
+
+        // Move the folder to where the icon target icon was
+        const folderIcon = this._items.get(newFolderId);
+        this.moveItem(folderIcon, folderPage, folderPosition);
 
         return true;
     }
