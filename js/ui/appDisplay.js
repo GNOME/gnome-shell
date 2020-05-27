d3d96d6b2ae3bcb684ac7ee8a97dabb9276aa63e
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2020-05-27 15:49:29 -0300

    appDisplay: Allow overshooting any icon
    Now that the icon being dragged can come from AppDisplay and also
    a folder dialog, the check for when to overshoot is broken. Check
    if the icon is a BaseAppView icon.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1284

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1055,7 +1055,7 @@ class AppDisplay extends BaseAppView {
         // Handle the drag overshoot. When dragging to above the
         // icon grid, move to the page above; when dragging below,
         // move to the page below.
-        if (this._grid.contains(appIcon))
+        if (appIcon instanceof BaseAppIcon)
             this._handleDragOvershoot(dragEvent);
 
         return DND.DragMotionResult.CONTINUE;
