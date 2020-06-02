94911105ad3d432941c98a3d3ee3aa45e340010d
Jonas Dreßler <verdre@v0yd.nl>
2020-06-02 16:23:59 +0200

    workspace: Remove slot property of WindowClone
    Since the slot property was only accessed by the old WindowOverlays to
    get the position and size of the clone, we can safely stop setting it
    and remove that property now.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1298

--- js/ui/workspace.js
+++ js/ui/workspace.js
@@ -202,8 +202,6 @@ var WindowClone = GObject.registerClass({
         this._delegate = this;
 
         this.slotId = 0;
-        this._slot = [0, 0, 0, 0];
-        this._dragSlot = [0, 0, 0, 0];
         this._stackAbove = null;
 
         this._cloneContainer.layout_manager.connect('notify::bounding-box', () =>
@@ -439,17 +437,6 @@ var WindowClone = GObject.registerClass({
         return this.hasAttachedDialogs();
     }
 
-    set slot(slot) {
-        this._slot = slot;
-    }
-
-    get slot() {
-        if (this.inDrag)
-            return this._dragSlot;
-        else
-            return this._slot;
-    }
-
     deleteAll() {
         // Delete all windows, starting from the bottom-most (most-modal) one
         let windows = this.get_children();
@@ -661,7 +648,6 @@ var WindowClone = GObject.registerClass({
     }
 
     _onDragBegin(_draggable, _time) {
-        this._dragSlot = this._slot;
         this.inDrag = true;
         this.hideOverlay(false);
         this.emit('drag-begin');
@@ -1290,7 +1276,6 @@ class Workspace extends St.Widget {
 
             const cloneWidth = cellWidth;
             const cloneHeight = cellHeight;
-            clone.slot = [x, y, cloneWidth, cloneHeight];
 
             if (!clone.positioned) {
                 // This window appeared after the overview was already up
@@ -1487,7 +1472,6 @@ class Workspace extends St.Widget {
             const height = win._overviewHint.height;
             delete win._overviewHint;
 
-            clone.slot = [x, y, width, height];
             clone.positioned = true;
 
             clone.set_position(x, y);
