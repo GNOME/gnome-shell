0f0d638c9e8096fe3bf3f7938dbb454f8c03ed68
Jonas Dreßler <verdre@v0yd.nl>
2020-06-02 16:23:59 +0200

    workspace: Remove slot property of WindowPreview
    Since the slot property was only accessed by the old WindowOverlays to
    get the position and size of the preview, we can safely stop setting it
    and remove that property now.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1307

--- js/ui/workspace.js
+++ js/ui/workspace.js
@@ -231,8 +231,6 @@ var WindowPreview = GObject.registerClass({
         this._delegate = this;
 
         this.slotId = 0;
-        this._slot = [0, 0, 0, 0];
-        this._dragSlot = [0, 0, 0, 0];
         this._stackAbove = null;
 
         this._windowContainer.layout_manager.connect(
@@ -513,17 +511,6 @@ var WindowPreview = GObject.registerClass({
         return this.hasAttachedDialogs();
     }
 
-    set slot(slot) {
-        this._slot = slot;
-    }
-
-    get slot() {
-        if (this.inDrag)
-            return this._dragSlot;
-        else
-            return this._slot;
-    }
-
     deleteAll() {
         const windows = this._windowContainer.layout_manager.getWindows();
 
@@ -732,7 +719,6 @@ var WindowPreview = GObject.registerClass({
     }
 
     _onDragBegin(_draggable, _time) {
-        this._dragSlot = this._slot;
         this.inDrag = true;
         this.hideOverlay(false);
         this.emit('drag-begin');
@@ -1362,7 +1348,6 @@ class Workspace extends St.Widget {
 
             const cloneWidth = cellWidth;
             const cloneHeight = cellHeight;
-            clone.slot = [x, y, cloneWidth, cloneHeight];
 
             if (!clone.positioned) {
                 // This window appeared after the overview was already up
@@ -1559,7 +1544,6 @@ class Workspace extends St.Widget {
             const height = win._overviewHint.height;
             delete win._overviewHint;
 
-            clone.slot = [x, y, width, height];
             clone.positioned = true;
 
             clone.set_position(x, y);
