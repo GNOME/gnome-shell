af6601f450530d123bbc2b650f758b073a932736
Jonas Dreßler <verdre@v0yd.nl>
2020-06-04 17:15:45 +0200

    workspace: Set windowClone offscreen redirect inside constructor
    This can also be moved to constructor, so do it.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1298

--- js/ui/workspace.js
+++ js/ui/workspace.js
@@ -184,6 +184,7 @@ var WindowClone = GObject.registerClass({
             reactive: true,
             can_focus: true,
             accessible_role: Atk.Role.PUSH_BUTTON,
+            offscreen_redirect: Clutter.OffscreenRedirect.AUTOMATIC_FOR_OPACITY,
         });
 
         this._windowContainer = new Clutter.Actor();
@@ -193,8 +194,6 @@ var WindowClone = GObject.registerClass({
         // container
         this._windowContainer.layout_manager = new WindowCloneLayout();
         this.add_child(this._windowContainer);
-
-        this.set_offscreen_redirect(Clutter.OffscreenRedirect.AUTOMATIC_FOR_OPACITY);
 
         this._addWindow(realWindow.meta_window);
 
