d807439943c4e7e7fb195d4ea05ca760710e430f
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2020-06-04 01:37:48 -0300

    appDisplay: Constrain dialog to primary display
    Much like the overview controls themselves are tied to the
    primary display, constrain the folder dialog to the primary
    display as well.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1301

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -8,6 +8,7 @@ const AppFavorites = imports.ui.appFavorites;
 const DND = imports.ui.dnd;
 const GrabHelper = imports.ui.grabHelper;
 const IconGrid = imports.ui.iconGrid;
+const Layout = imports.ui.layout;
 const Main = imports.ui.main;
 const PageIndicators = imports.ui.pageIndicators;
 const ParentalControlsManager = imports.misc.parentalControlsManager;
@@ -1433,6 +1434,11 @@ var AppFolderDialog = GObject.registerClass({
             x_align: Clutter.ActorAlign.CENTER,
             y_align: Clutter.ActorAlign.CENTER,
         });
+
+        this.add_constraint(new Layout.MonitorConstraint({
+            primary: true,
+            work_area: true,
+        }));
 
         this._source = source;
         this._folder = folder;
