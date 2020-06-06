9f9ef8ef86d3571fe85ac1eca69836e212380254
Sergio Costas <raster@rastersoft.com>
2020-06-06 13:38:05 +0200

    Add SafeDesktopArea class
    This new class allows to know the current "safe desktop area".
    This is the area of the desktop that will never be ocluded by
    elements other than a window. This is useful for elements that
    are placed in the desktop, like desktop icons, when there are
    other elements that can cover the desktop. An example is a dock
    with the autohide option enabled: in that case, the dock will
    hide when it is covered by a window, but will appear when no
    window is in its place. This means that the zone occupied by
    the dock will never be visible by the user. Also, since the
    dock hides when a window covers that part, the working area
    (which is the value used to determine the maximized size for
    the windows) is not affected by it, so it can't be used for
    this purpose.
    
    With this class, these dynamic elements can register the zone
    occupied by them, thus allowing desktop icons or other extensions
    (like animated widgets) to avoid putting elements in that zone,
    where the user wouldn't be able to see or manipulate them.

---
+++ js/ui/safeDesktopArea.js
@@ -0,0 +1,131 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
+/* manages dynamic desktop areas */
+
+const { GLib, GObject } = imports.gi;
+
+var SafeDesktopArea = GObject.registerClass({
+    Signals: {
+        'margins-changed': {},
+    },
+}, class SafeDesktopArea extends GObject.Object {
+    _init(layoutManager) {
+        super._init();
+        this._safeMargins = [];
+        this._registered = [];
+        this._idCounter = 0;
+        this._sentSignal = false;
+
+        this._layoutManager = layoutManager;
+        this._monitorsChangedId = this._layoutManager.connect('monitors-changed', () => {
+            this._updateMonitors();
+        });
+        this._updateMonitors();
+    }
+
+    _updateMonitors() {
+        let monitors = this._layoutManager.monitors.length;
+
+        if (this._safeMargins.length == monitors)
+            return;
+
+        if (this._safeMargins.length > monitors) {
+            this._safeMargins = this._safeMargins.slice(0, monitors);
+        } else {
+            for (let i = this._safeMargins.length; i < monitors; i++)
+                this._safeMargins.push({});
+        }
+    }
+
+    _emitChanged() {
+        if (this._sentSignal)
+            return;
+
+        this._sentSignal = true;
+
+        this._sentSignalId = GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
+            this._sentSignal = false;
+            this.emit('margins-changed');
+            this._sentSignalId = 0;
+            return false;
+        });
+    }
+
+    register() {
+        this._idCounter += 1;
+        this._registered.push(this._idCounter);
+        return this._idCounter;
+    }
+
+    unregister(id) {
+        if (!this._registered.includes(id))
+            throw new Error('Trying to unregister an inexistent ID');
+        for(let monitor in this._safeMargins)
+            this.unsetMargins(id, monitor);
+        this._registered.splice(this._registered.indexOf(id));
+    }
+
+    setSafeMargins(id, monitor, top, bottom, left, right) {
+        if (!this._registered.includes(id))
+            throw new Error('Using an inexistent ID to set desktop safe margins');
+        if (monitor >= this._safeMargins.length)
+            return;
+        let margins = {};
+        margins.top = top;
+        margins.bottom = bottom;
+        margins.left = left;
+        margins.right = right;
+        this._safeMargins[monitor][id] = margins;
+        this._emitChanged();
+    }
+
+    getMonitorForCoordinates(x, y) {
+        for(let monitorIndex = 0; monitorIndex < this._layoutManager.monitors.length; monitorIndex++) {
+            let monitor = this._layoutManager.monitors[monitorIndex];
+            if ((x >= monitor.x) && (y >= monitor.y) && (x < (monitor.x + monitor.width)) && (y < (monitor.y + monitor.height))) {
+                return monitorIndex;
+            }
+        }
+        return -1;
+    }
+
+    unsetSafeMargins(id, monitor) {
+        if (!this._registered.includes(id))
+            throw new Error('Using an inexistent ID to unset desktop safe margins');
+        if (monitor == -1) {
+            for(monitor in this._safeMargins) {
+                if (id in this._safeMargins[monitor])
+                    delete this._safeMargins[monitor][id];
+            }
+        } else {
+            if (monitor >= this._safeMargins.length)
+                return;
+            if (!(id in this._safeMargins[monitor]))
+                return;
+            delete this._safeMargins[monitor][id];
+        }
+        this._emitChanged();
+    }
+
+    getSafeMargins(monitor) {
+        let margins = {};
+
+        margins.top = 0;
+        margins.bottom = 0;
+        margins.left = 0;
+        margins.right = 0;
+        if (monitor < this._safeMargins.length) {
+            for(let id in this._safeMargins[monitor]) {
+                let idmargins = this._safeMargins[monitor][id];
+                if (idmargins.top > margins.top)
+                    margins.top = idmargins.top;
+                if (idmargins.bottom > margins.bottom)
+                    margins.bottom = idmargins.bottom;
+                if (idmargins.left > margins.left)
+                    margins.left = idmargins.left;
+                if (idmargins.right > margins.right)
+                    margins.right = idmargins.right;
+            }
+        }
+        return margins;
+    }
+});
