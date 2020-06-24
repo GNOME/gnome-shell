5021e3c9b861fc94636a042c5dc09f83f45e7f4d
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2020-06-24 12:53:36 -0300

    appDisplay: Fully hide icon when dragging
    The partially visible icon causes more problems than
    it solves, visually speaking. Fade it out completely
    while dragging.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1284

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1350,9 +1350,9 @@ class BaseAppIcon extends St.Button {
     scaleAndFade() {
         this.reactive = false;
         this.ease({
-            scale_x: 0.75,
-            scale_y: 0.75,
-            opacity: 128,
+            scale_x: 0.5,
+            scale_y: 0.5,
+            opacity: 0,
         });
     }
 
