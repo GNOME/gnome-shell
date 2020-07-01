951972cba3915fabc1b381a1b50ca1b2cb188cf0
yun341 <5933468@qq.com>
2020-07-02 06:54:55 +0800

    SSO: Add vmware.js to support multi-desktop SSO events
        Add new dbus interface to vmware.js to support Multi-Desktop Session SSO.
    
        vmware.js would receive new session dbus message which included token from
        desktop Application. Then vmware.js would trigger the SSO authentication
        event for specified desktop session.
    
        Closes: https://gitlab.gnome.org/GNOME/gnome-shell/issues/1983

---
+++ js/gdm/vmware.js
@@ -0,0 +1,54 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
+/* exported getVmwareCredentialsManager */
+
+const Gio = imports.gi.Gio;
+const Signals = imports.signals;
+const Credential = imports.gdm.credentialManager;
+
+const dbusPath = '/org/vmware/viewagent/Credentials';
+const dbusInterface = 'org.vmware.viewagent.Credentials';
+
+var SERVICE_NAME = 'gdm-vmwcred';
+
+const VmwareCredentialsIface = '<node> \
+<interface name="' + dbusInterface + '"> \
+<signal name="UserAuthenticated"> \
+    <arg type="s" name="token"/> \
+</signal> \
+</interface> \
+</node>';
+
+
+const VmwareCredentialsInfo = Gio.DBusInterfaceInfo.new_for_xml(VmwareCredentialsIface);
+
+let _vmwareCredentialsManager = null;
+
+function VmwareCredentials() {
+    var self = new Gio.DBusProxy({ g_connection: Gio.DBus.session,
+                                   g_interface_name: VmwareCredentialsInfo.name,
+                                   g_interface_info: VmwareCredentialsInfo,
+                                   g_name: dbusInterface,
+                                   g_object_path: dbusPath,
+                                   g_flags: Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES });
+    self.init(null);
+    return self;
+}
+
+var VmwareCredentialsManager = class VmwareCredentialsManager extends Credential.CredentialManager {
+    constructor() {
+        super(SERVICE_NAME);
+        this._credentials = new VmwareCredentials();
+        this._credentials.connectSignal('UserAuthenticated',
+            (proxy, sender, [token]) => {
+                this.token = token;
+            });
+    }
+};
+Signals.addSignalMethods(VmwareCredentialsManager.prototype);
+
+function getVmwareCredentialsManager() {
+    if (!_vmwareCredentialsManager)
+        _vmwareCredentialsManager = new VmwareCredentialsManager();
+
+    return _vmwareCredentialsManager;
+}
