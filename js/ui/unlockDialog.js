093603b35e085ffa88c49bae6a35fd93ce856e4d
Zephaniah E. Loss-Cutler-Hull <warp@aehallh.com>
2020-08-15 03:23:27 -0700

    unlockDialog: Defend against absence of blur effect
    widget.get_effect(...) can return null while locking the screen, resulting
    in a TypeError. In this situation the screen ends up black with a cursor
    but never going to sleep, and moving the mouse brings the old screen
    contents up but does not allow unlocking.
    
    unlockDialog.js assumes that widget.get_effect will return non-null,
    but other places such as getWindowDimmer in windowManager.js go out of
    their way to be more careful.
    
    [smcv: Add commit message, remove hard tabs, add missing semicolon]
    
    Resolves: https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/3071
    Bug-Debian: https://bugs.debian.org/968440

--- js/ui/unlockDialog.js
+++ js/ui/unlockDialog.js
@@ -647,10 +647,14 @@ var UnlockDialog = GObject.registerClass({
         const themeContext = St.ThemeContext.get_for_stage(global.stage);
 
         for (const widget of this._backgroundGroup) {
-            widget.get_effect('blur').set({
-                brightness: BLUR_BRIGHTNESS,
-                sigma: BLUR_SIGMA * themeContext.scale_factor,
-            });
+            const effect = widget.get_effect('blur');
+
+            if (effect) {
+                effect.set({
+                    brightness: BLUR_BRIGHTNESS,
+                    sigma: BLUR_SIGMA * themeContext.scale_factor,
+                });
+            }
         }
     }
 
