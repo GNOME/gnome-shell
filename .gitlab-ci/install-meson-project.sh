e026e3bc9fd03b1606d1ad777827825a61890c52
Jonas Ådahl <jadahl@gmail.com>
2020-11-06 18:59:19 +0100

    ci: Use ci-templates for building image
    This rebuilds the automaticaly whenever the image tag changes. Whenever
    something in the image needs to change, alter the installation script
    and change the tag to the current date.
    
    This removes the -s (strict) argument from js68, as it doesn't exist in
    js78.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1492>

---
+++ .gitlab-ci/install-meson-project.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+if [[ $# -lt 4 ]]; then
+  echo Usage: $0 [options] [repo-url] [commit] [subdir]
+  echo  Options:
+  echo    -Dkey=val
+  exit 1
+fi
+
+MESON_OPTIONS=()
+
+while [[ $1 =~ ^-D ]]; do
+  MESON_OPTIONS+=( "$1" )
+  shift
+done
+
+REPO_URL="$1"
+COMMIT="$2"
+SUBDIR="$3"
+PREPARE="$4"
+
+REPO_DIR="$(basename ${REPO_URL%.git})"
+
+git clone --depth 1 "$REPO_URL" -b "$COMMIT"
+pushd "$REPO_DIR"
+pushd "$SUBDIR"
+sh -c "$PREPARE"
+meson --prefix=/usr _build "${MESON_OPTIONS[@]}"
+ninja -C _build install
+popd
+popd
+rm -rf "$REPO_DIR"
