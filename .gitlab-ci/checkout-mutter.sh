68dc39644689dc13a714fa3c0dbe3b82723d405d
Florian Müllner <fmuellner@gnome.org>
2020-12-11 14:30:05 +0100

    ci: Improve checkout script output
    We are currently not very good at communicating what's going in,
    in particular for non-merge request pipelines where the output
    is usually just "Using origin/master instead" (instead of what?).
    
    Improve this by consistently communicate what we are looking for,
    whether we found it, and what we end up doing.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1530
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1530>

--- .gitlab-ci/checkout-mutter.sh
+++ .gitlab-ci/checkout-mutter.sh
@@ -2,10 +2,11 @@
 
 mutter_target=
 
-git clone https://gitlab.gnome.org/GNOME/mutter.git
-
-if [ $? -ne 0 ]; then
-  echo Checkout failed
+echo -n Cloning into mutter ...
+if git clone --quiet https://gitlab.gnome.org/GNOME/mutter.git; then
+  echo \ done
+else
+  echo \ failed
   exit 1
 fi
 
@@ -15,19 +16,27 @@ if [ "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" ]; then
   merge_request_remote=${CI_MERGE_REQUEST_SOURCE_PROJECT_URL//gnome-shell/mutter}
   merge_request_branch=$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
 
-  echo Looking for $merge_request_branch on remote ...
+  echo -n Looking for $merge_request_branch on remote ...
   if git fetch -q $merge_request_remote $merge_request_branch 2>/dev/null; then
+    echo \ found
     mutter_target=FETCH_HEAD
   else
+    echo \ not found
     mutter_target=origin/$CI_MERGE_REQUEST_TARGET_BRANCH_NAME
     echo Using $mutter_target instead
   fi
 fi
 
 if [ -z "$mutter_target" ]; then
+  echo -n Looking for $CI_COMMIT_REF on remote ...
   mutter_target=$(git branch -r -l origin/$CI_COMMIT_REF_NAME)
-  mutter_target=${mutter_target:-origin/master}
-  echo Using $mutter_target instead
+  if [ "$mutter_target" ]; then
+    echo \ found
+  else
+    echo \ not found
+    mutter_target=${mutter_target:-origin/master}
+    echo Using $mutter_target instead
+  fi
 fi
 
 git checkout -q $mutter_target
