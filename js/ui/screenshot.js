461c65c93b2918de081b19528e7c0c95e649739c
Jonas Dreßler <verdre@v0yd.nl>
2021-02-11 18:30:10 +0100

    screenshot: Replace template strings with .format()
    CI will complain when we add screenshot.js to the POTFILES in the next
    commit because gettext doesn't like template strings, so make CI happy
    and switch to .format() instead.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1658>

--- js/ui/screenshot.js
+++ js/ui/screenshot.js
@@ -90,11 +90,11 @@ var ScreenshotService = class {
             return null;
 
         yield Gio.File.new_for_path(
-            GLib.build_filenamev([path, `${filename}.png`]));
+            GLib.build_filenamev([path, '%s.png'.format(filename)]));
 
         for (let idx = 1; ; idx++) {
             yield Gio.File.new_for_path(
-                GLib.build_filenamev([path, `${filename}-${idx}.png`]));
+                GLib.build_filenamev([path, '%s-%s.png'.format(filename, idx)]));
         }
     }
 
