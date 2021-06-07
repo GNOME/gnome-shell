4373a91911cc1f34eb80e5db7400db6f7857aa3e
Zander Brown <zbrown@gnome.org>
2021-06-07 01:04:55 +0100

    doc: Switch documentation to gi-docgen
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1873>

---
+++ docs/reference/shell/urlmap.js
@@ -0,0 +1,11 @@
+baseURLs = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+    [ 'GdkPixbuf', 'https://docs.gtk.org/gdk-pixbuf/' ],
+    [ 'Meta', 'https://gnome.pages.gitlab.gnome.org/mutter/meta/' ],
+    [ 'Mtk', 'https://gnome.pages.gitlab.gnome.org/mutter/mtk/' ],
+    [ 'Cogl', 'https://gnome.pages.gitlab.gnome.org/mutter/cogl/' ],
+    [ 'Clutter', 'https://gnome.pages.gitlab.gnome.org/mutter/clutter/' ],
+    [ 'St', 'https://gnome.pages/gitlab.gnome.org/gnome-shell/st/' ],
+]
---
+++ docs/reference/st/urlmap.js
@@ -0,0 +1,10 @@
+baseURLs = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+    [ 'Pango', 'https://docs.gtk.org/pango/' ],
+    [ 'GdkPixbuf', 'https://docs.gtk.org/gdk-pixbuf/' ],
+    [ 'Clutter', 'https://gnome.pages.gitlab.gnome.org/mutter/clutter/' ],
+    [ 'Cogl', 'https://gnome.pages.gitlab.gnome.org/mutter/cogl/' ],
+    [ 'Meta', 'https://gnome.pages.gitlab.gnome.org/mutter/meta/' ],
+]
