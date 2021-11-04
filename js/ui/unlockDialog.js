d3c6dd75bb490ab8ca24476b665b42e56b2f7902
Florian Müllner <fmuellner@gnome.org>
2021-11-04 14:07:34 +0100

    Revert !1999
    The changes introduce crashes, so back out until we
    figure those out.
    
    This reverts commits ceee53aa0a4 and 5d5bfe492cd81.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2022>

--- js/ui/unlockDialog.js
+++ js/ui/unlockDialog.js
@@ -676,14 +676,16 @@ var UnlockDialog = GObject.registerClass({
     }
 
     _ensureAuthPrompt() {
-        if (!this._authPrompt) {
-            this._authPrompt = new AuthPrompt.AuthPrompt(this._gdmClient,
-                AuthPrompt.AuthPromptMode.UNLOCK_ONLY);
-            this._authPrompt.connect('failed', this._fail.bind(this));
-            this._authPrompt.connect('cancelled', this._fail.bind(this));
-            this._authPrompt.connect('reset', this._onReset.bind(this));
-            this._promptBox.add_child(this._authPrompt);
-        }
+        if (this._authPrompt)
+            return;
+
+        this._authPrompt = new AuthPrompt.AuthPrompt(this._gdmClient,
+            AuthPrompt.AuthPromptMode.UNLOCK_ONLY);
+        this._authPrompt.connect('failed', this._fail.bind(this));
+        this._authPrompt.connect('cancelled', this._fail.bind(this));
+        this._authPrompt.connect('reset', this._onReset.bind(this));
+
+        this._promptBox.add_child(this._authPrompt);
 
         this._authPrompt.reset();
         this._authPrompt.updateSensitivity(true);
@@ -870,11 +872,7 @@ var UnlockDialog = GObject.registerClass({
     }
 
     finish(onComplete) {
-        if (!this._authPrompt) {
-            onComplete();
-            return;
-        }
-
+        this._ensureAuthPrompt();
         this._authPrompt.finish(onComplete);
     }
 
