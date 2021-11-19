9e8dc494f92644e7b3d9f748704aa95fc4328969
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-11-20 01:15:37 +0600

    extensionSystem: Ignore spurious disable-extension-version-validation change
    Ignore spurious change notificatons, when the setting didn't actually change
    (triggered by `dconf update`)
    
    Workaround for https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/4808
    
    (cherry picked from commit 12c300a7a23016ef956398a91a3831eca55f12d9)
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2180>

--- js/ui/extensionSystem.js
+++ js/ui/extensionSystem.js
@@ -29,6 +29,7 @@ var ExtensionManager = class {
         this._unloadedExtensions = new Map();
         this._enabledExtensions = [];
         this._extensionOrder = [];
+        this._checkVersion = false;
 
         Main.sessionMode.connect('updated', this._sessionUpdated.bind(this));
     }
@@ -333,9 +334,7 @@ var ExtensionManager = class {
         // Default to error, we set success as the last step
         extension.state = ExtensionState.ERROR;
 
-        let checkVersion = !global.settings.get_boolean(EXTENSION_DISABLE_VERSION_CHECK_KEY);
-
-        if (checkVersion && ExtensionUtils.isOutOfDate(extension)) {
+        if (this._checkVersion && ExtensionUtils.isOutOfDate(extension)) {
             extension.state = ExtensionState.OUT_OF_DATE;
         } else if (!this._canLoad(extension)) {
             this.logExtensionError(extension.uuid, new Error(
@@ -508,6 +507,12 @@ var ExtensionManager = class {
     }
 
     _onVersionValidationChanged() {
+        const checkVersion = !global.settings.get_boolean(EXTENSION_DISABLE_VERSION_CHECK_KEY);
+        if (checkVersion === this._checkVersion)
+            return;
+
+        this._checkVersion = checkVersion;
+
         // Disabling extensions modifies the order array, so use a copy
         let extensionOrder = this._extensionOrder.slice();
 
@@ -559,6 +564,8 @@ var ExtensionManager = class {
             this._onSettingsWritableChanged.bind(this));
         global.settings.connect('writable-changed::%s'.format(DISABLED_EXTENSIONS_KEY),
             this._onSettingsWritableChanged.bind(this));
+
+        this._onVersionValidationChanged();
 
         this._enabledExtensions = this._getEnabledExtensions();
 
