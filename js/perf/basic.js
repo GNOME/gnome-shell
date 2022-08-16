6fc538b7f75e432cae230b2644bc42336dafb86f
Jonas Ådahl <jadahl@gmail.com>
2022-08-16 12:40:34 +0200

    perf/basic: Toggle quick settings instead of old menu
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1349>

--- js/perf/basic.js
+++ js/perf/basic.js
@@ -39,7 +39,7 @@ async function run() {
 
     console.debug('Navigate through top bar');
     Scripting.scriptEvent('topBarNavStart');
-    Main.panel.statusArea.aggregateMenu.menu.open();
+    Main.panel.statusArea.quickSettings.menu.open();
     await Scripting.sleep(400);
 
     const { menuManager } = Main.panel;
