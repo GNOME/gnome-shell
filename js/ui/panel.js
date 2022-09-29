f32ffbf19bab1025bf8cc0a3c010060b811a0fa8
Carlos Garnacho <carlosg@gnome.org>
2022-09-29 19:21:05 +0200

    panel: Update for removal of frame_action argument
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2526>

--- js/ui/panel.js
+++ js/ui/panel.js
@@ -548,7 +548,6 @@ class Panel extends St.Widget {
 
         return dragWindow.begin_grab_op(
             Meta.GrabOp.MOVING,
-            true, /* frame action */
             event.get_time()) ? Clutter.EVENT_STOP : Clutter.EVENT_PROPAGATE;
     }
 
