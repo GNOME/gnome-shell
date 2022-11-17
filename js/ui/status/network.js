GNOME/gnome-shell

commit 637ee7386e778c853e0f381e4ddb022e060eea79

js: Use async D-Bus wrappers
After porting the more complex cases - in particular those that
affect a module's API - we are left with straight-forward D-Bus
method calls that can be moved to promise-based wrappers in one
go.

For consistency, this also switches from Remote to Async where
the call result is ignored.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2344>

js/ui/status/network.js
@@ -50,7 +50,7 @@ var PortalHelperResult = {
 };
 
 const PortalHelperIface = loadInterfaceXML('org.gnome.Shell.PortalHelper');
-const PortalHelperProxy = Gio.DBusProxy.makeProxyWrapper(PortalHelperIface);
+const PortalHelperInfo = Gio.DBusInterfaceInfo.new_for_xml(PortalHelperIface);
 
 function signalToIcon(value) {
     if (value < 20)
@@ -2090,7 +2090,7 @@ class Indicator extends PanelMenu.SystemIndicator {
     _flushConnectivityQueue() {
         if (this._portalHelperProxy) {
             for (let item of this._connectivityQueue)
-                this._portalHelperProxy.CloseRemote(item);
+                this._portalHelperProxy.CloseAsync(item).catch(logError);
         }
 
         this._connectivityQueue = [];
@@ -2101,7 +2101,7 @@ class Indicator extends PanelMenu.SystemIndicator {
 
         if (index >= 0) {
             if (this._portalHelperProxy)
-                this._portalHelperProxy.CloseRemote(path);
+                this._portalHelperProxy.CloseAsync(path).catch(logError);
 
             this._connectivityQueue.splice(index, 1);
         }
@@ -2128,7 +2128,7 @@ class Indicator extends PanelMenu.SystemIndicator {
         }
     }
 
-    _syncConnectivity() {
+    async _syncConnectivity() {
         if (this._mainConnection == null ||
             this._mainConnection.state != NM.ActiveConnectionState.ACTIVATED) {
             this._flushConnectivityQueue();
@@ -2153,24 +2153,26 @@ class Indicator extends PanelMenu.SystemIndicator {
         }
 
         let timestamp = global.get_current_time();
-        if (this._portalHelperProxy) {
-            this._portalHelperProxy.AuthenticateRemote(path, '', timestamp);
-        } else {
-            new PortalHelperProxy(Gio.DBus.session,
-                'org.gnome.Shell.PortalHelper',
-                '/org/gnome/Shell/PortalHelper',
-                (proxy, error) => {
-                    if (error) {
-                        log(`Error launching the portal helper: ${error}`);
-                        return;
-                    }
+        if (!this._portalHelperProxy) {
+            this._portalHelperProxy = new Gio.DBusProxy({
+                g_connection: Gio.DBus.session,
+                g_name: 'org.gnome.Shell.PortalHelper',
+                g_object_path: '/org/gnome/Shell/PortalHelper',
+                g_interface_name: PortalHelperInfo.name,
+                g_interface_info: PortalHelperInfo,
+            });
+            this._portalHelperProxy.connectSignal('Done',
+                () => this._portalHelperDone().catch(logError));
 
-                    this._portalHelperProxy = proxy;
-                    proxy.connectSignal('Done', this._portalHelperDone.bind(this));
-
-                    proxy.AuthenticateRemote(path, '', timestamp);
-                });
+            try {
+                await this._portalHelperProxy.init_async(
+                    GLib.PRIORITY_DEFAULT, null);
+            } catch (e) {
+                console.error(`Error launching the portal helper: ${e.message}`);
+            }
         }
+
+        this._portalHelperProxy?.AuthenticateAsync(path, '', timestamp).catch(logError);
 
         this._connectivityQueue.push(path);
     }

js/dbusServices/extensions/extensionsService.js
@@ -43,87 +43,82 @@ var ExtensionsService = class extends ServiceImplementation {
         this._proxy.UserExtensionsEnabled = enable;
     }
 
-    ListExtensionsAsync(params, invocation) {
-        this._proxy.ListExtensionsRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async ListExtensionsAsync(params, invocation) {
+        try {
+            const res = await this._proxy.ListExtensionsAsync(...params);
             invocation.return_value(new GLib.Variant('(a{sa{sv}})', res));
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    GetExtensionInfoAsync(params, invocation) {
-        this._proxy.GetExtensionInfoRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async GetExtensionInfoAsync(params, invocation) {
+        try {
+            const res = await this._proxy.GetExtensionInfoAsync(...params);
             invocation.return_value(new GLib.Variant('(a{sv})', res));
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    GetExtensionErrorsAsync(params, invocation) {
-        this._proxy.GetExtensionErrorsRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async GetExtensionErrorsAsync(params, invocation) {
+        try {
+            const res = await this._proxy.GetExtensionErrorsAsync(...params);
             invocation.return_value(new GLib.Variant('(as)', res));
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    InstallRemoteExtensionAsync(params, invocation) {
-        this._proxy.InstallRemoteExtensionRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async InstallRemoteExtensionAsync(params, invocation) {
+        try {
+            const res = await this._proxy.InstallRemoteExtensionAsync(...params);
             invocation.return_value(new GLib.Variant('(s)', res));
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    UninstallExtensionAsync(params, invocation) {
-        this._proxy.UninstallExtensionRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async UninstallExtensionAsync(params, invocation) {
+        try {
+            const res = await this._proxy.UninstallExtensionAsync(...params);
             invocation.return_value(new GLib.Variant('(b)', res));
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    EnableExtensionAsync(params, invocation) {
-        this._proxy.EnableExtensionRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async EnableExtensionAsync(params, invocation) {
+        try {
+            const res = await this._proxy.EnableExtensionAsync(...params);
             invocation.return_value(new GLib.Variant('(b)', res));
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    DisableExtensionAsync(params, invocation) {
-        this._proxy.DisableExtensionRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async DisableExtensionAsync(params, invocation) {
+        try {
+            const res = await this._proxy.DisableExtensionAsync(...params);
             invocation.return_value(new GLib.Variant('(b)', res));
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
     LaunchExtensionPrefsAsync([uuid], invocation) {
         this.OpenExtensionPrefsAsync([uuid, '', {}], invocation);
     }
 
-    OpenExtensionPrefsAsync(params, invocation) {
+    async OpenExtensionPrefsAsync(params, invocation) {
         const [uuid, parentWindow, options] = params;
 
-        this._proxy.GetExtensionInfoRemote(uuid, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
+        try {
+            const [serialized] = await this._proxy.GetExtensionInfoAsync(uuid);
 
-            if (this._prefsDialog) {
-                this._handleError(invocation,
-                    new Error('Already showing a prefs dialog'));
-                return;
-            }
+            if (this._prefsDialog)
+                throw new Error('Already showing a prefs dialog');
 
-            const [serialized] = res;
             const extension = ExtensionUtils.deserializeExtension(serialized);
 
             this._prefsDialog = new ExtensionPrefsDialog(extension);
@@ -150,15 +145,17 @@ var ExtensionsService = class extends ServiceImplementation {
             this._prefsDialog.show();
 
             invocation.return_value(null);
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    CheckForUpdatesAsync(params, invocation) {
-        this._proxy.CheckForUpdatesRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async CheckForUpdatesAsync(params, invocation) {
+        try {
+            await this._proxy.CheckForUpdatesAsync(...params);
             invocation.return_value(null);
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 };

js/dbusServices/notifications/notificationDaemon.js
@@ -103,45 +103,44 @@ var NotificationDaemon = class extends ServiceImplementation {
             'sender-pid': new GLib.Variant('u', pid),
         };
 
-        this._proxy.NotifyRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
-            const [id] = res;
+        try {
+            const [id] = await this._proxy.NotifyAsync(...params);
             this._activeNotifications.set(id, sender);
-            invocation.return_value(new GLib.Variant('(u)', res));
-        });
+            invocation.return_value(new GLib.Variant('(u)', [id]));
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    CloseNotificationAsync(params, invocation) {
+    async CloseNotificationAsync(params, invocation) {
         const [id] = params;
         if (!this._checkNotificationId(invocation, id))
             return;
 
-        this._proxy.CloseNotificationRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+        try {
+            await this._proxy.CloseNotificationAsync(...params);
             invocation.return_value(null);
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    GetCapabilitiesAsync(params, invocation) {
-        this._proxy.GetCapabilitiesRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async GetCapabilitiesAsync(params, invocation) {
+        try {
+            const res = await this._proxy.GetCapabilitiesAsync(...params);
             invocation.return_value(new GLib.Variant('(as)', res));
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    GetServerInformationAsync(params, invocation) {
-        this._proxy.GetServerInformationRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async GetServerInformationAsync(params, invocation) {
+        try {
+            const res = await this._proxy.GetServerInformationAsync(...params);
             invocation.return_value(new GLib.Variant('(ssss)', res));
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
     async _getSenderPid(sender) {

js/dbusServices/screensaver/screenSaverService.js
@@ -32,39 +32,39 @@ var ScreenSaverService = class extends ServiceImplementation {
             () => this._dbusImpl.emit_signal('WakeUpScreen', null));
     }
 
-    LockAsync(params, invocation) {
-        this._proxy.LockRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async LockAsync(params, invocation) {
+        try {
+            await this._proxy.LockAsync(...params);
             invocation.return_value(null);
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    GetActiveAsync(params, invocation) {
-        this._proxy.GetActiveRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async GetActiveAsync(params, invocation) {
+        try {
+            const res = await this._proxy.GetActiveAsync(...params);
             invocation.return_value(new GLib.Variant('(b)', res));
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    SetActiveAsync(params, invocation) {
-        this._proxy.SetActiveRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async SetActiveAsync(params, invocation) {
+        try {
+            await this._proxy.SetActiveAsync(...params);
             invocation.return_value(null);
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 
-    GetActiveTimeAsync(params, invocation) {
-        this._proxy.GetActiveTimeRemote(...params, (res, error) => {
-            if (this._handleError(invocation, error))
-                return;
-
+    async GetActiveTimeAsync(params, invocation) {
+        try {
+            const res = await this._proxy.GetActiveTimeAsync(...params);
             invocation.return_value(new GLib.Variant('(u)', res));
-        });
+        } catch (error) {
+            this._handleError(invocation, error);
+        }
     }
 };

js/gdm/realmd.js
@@ -100,9 +100,9 @@ var Manager = class extends Signals.EventEmitter {
 
     release() {
         Service(Gio.DBus.system,
-                'org.freedesktop.realmd',
-                '/org/freedesktop/realmd',
-                service => service.ReleaseRemote());
+            'org.freedesktop.realmd',
+            '/org/freedesktop/realmd',
+            service => service.ReleaseAsync().catch(logError));
         this._aggregateProvider.disconnectObject(this);
         this._realms = { };
         this._updateLoginFormat();

js/gdm/util.js
@@ -376,7 +376,7 @@ var ShellUserVerifier = class extends Signals.EventEmitter {
         this.emit('show-message', null, null, MessageType.NONE);
     }
 
-    _checkForFingerprintReader() {
+    async _checkForFingerprintReader() {
         this._fingerprintReaderType = FingerprintReaderType.NONE;
 
         if (!this._settings.get_boolean(FINGERPRINT_AUTHENTICATION_KEY) ||
@@ -385,21 +385,19 @@ var ShellUserVerifier = class extends Signals.EventEmitter {
             return;
         }
 
-        this._fprintManager.GetDefaultDeviceRemote(Gio.DBusCallFlags.NONE, this._cancellable,
-            (params, error) => {
-                if (!error && params) {
-                    const [device] = params;
-                    const fprintDeviceProxy = new FprintDeviceProxy(Gio.DBus.system,
-                        'net.reactivated.Fprint',
-                        device);
-                    const fprintDeviceType = fprintDeviceProxy['scan-type'];
+        try {
+            const [device] = await this._fprintManager.GetDefaultDeviceAsync(
+                Gio.DBusCallFlags.NONE, this._cancellable);
+            const fprintDeviceProxy = new FprintDeviceProxy(Gio.DBus.system,
+                'net.reactivated.Fprint',
+                device);
+            const fprintDeviceType = fprintDeviceProxy['scan-type'];
 
-                    this._fingerprintReaderType = fprintDeviceType === 'swipe'
-                        ? FingerprintReaderType.SWIPE
-                        : FingerprintReaderType.PRESS;
-                    this._updateDefaultService();
-                }
-            });
+            this._fingerprintReaderType = fprintDeviceType === 'swipe'
+                ? FingerprintReaderType.SWIPE
+                : FingerprintReaderType.PRESS;
+            this._updateDefaultService();
+        } catch (e) {}
     }
 
     _onCredentialManagerAuthenticated(credentialManager, _token) {

js/misc/modemManager.js
@@ -153,24 +153,24 @@ class ModemGsm extends ModemBase {
         this._proxy.connectSignal('RegistrationInfo', (proxy, sender, [_status, code, name]) => {
             this._setOperatorName(_findProviderForMccMnc(name, code));
         });
-        this._proxy.GetRegistrationInfoRemote(([result], err) => {
-            if (err) {
-                log(err);
-                return;
-            }
+        this._getInitialState();
+    }
 
-            let [status_, code, name] = result;
+    async _getInitialState() {
+        try {
+            const [
+                [status_, code, name],
+                [quality],
+            ] = await Promise.all([
+                this._proxy.GetRegistrationInfoAsync(),
+                this._proxy.GetSignalQualityAsync(),
+            ]);
             this._setOperatorName(_findProviderForMccMnc(name, code));
-        });
-        this._proxy.GetSignalQualityRemote((result, err) => {
-            if (err) {
-                // it will return an error if the device is not connected
-                this._setSignalQuality(0);
-            } else {
-                let [quality] = result;
-                this._setSignalQuality(quality);
-            }
-        });
+            this._setSignalQuality(quality);
+        } catch (err) {
+            // it will return an error if the device is not connected
+            this._setSignalQuality(0);
+        }
     }
 });
 
@@ -188,27 +188,28 @@ class ModemCdma extends ModemBase {
             if (this.operator_name == null)
                 this._refreshServingSystem();
         });
-        this._proxy.GetSignalQualityRemote((result, err) => {
-            if (err) {
-                // it will return an error if the device is not connected
-                this._setSignalQuality(0);
-            } else {
-                let [quality] = result;
-                this._setSignalQuality(quality);
-            }
-        });
+        this._getSignalQuality();
     }
 
-    _refreshServingSystem() {
-        this._proxy.GetServingSystemRemote(([result], err) => {
-            if (err) {
-                // it will return an error if the device is not connected
-                this._setOperatorName(null);
-            } else {
-                let [bandClass_, band_, sid] = result;
-                this._setOperatorName(_findProviderForSid(sid));
-            }
-        });
+    async _getSignalQuality() {
+        try {
+            const [quality] = await this._proxy.GetSignalQualityAsync();
+            this._setSignalQuality(quality);
+        } catch (err) {
+            // it will return an error if the device is not connected
+            this._setSignalQuality(0);
+        }
+    }
+
+    async _refreshServingSystem() {
+        try {
+            const [bandClass_, band_, sid] =
+                await this._proxy.GetServingSystemAsync();
+            this._setOperatorName(_findProviderForSid(sid));
+        } catch (err) {
+            // it will return an error if the device is not connected
+            this._setOperatorName(null);
+        }
     }
 });
 

js/misc/systemActions.js
@@ -329,11 +329,14 @@ const SystemActions = GObject.registerClass({
         this.notify('can-lock-screen');
     }
 
-    _updateHaveShutdown() {
-        this._session.CanShutdownRemote((result, error) => {
-            this._canHavePowerOff = error ? false : result[0];
-            this._updatePowerOff();
-        });
+    async _updateHaveShutdown() {
+        try {
+            const [canShutdown] = await this._session.CanShutdownAsync();
+            this._canHavePowerOff = canShutdown;
+        } catch (e) {
+            this._canHavePowerOff = false;
+        }
+        this._updatePowerOff();
     }
 
     _updatePowerOff() {
@@ -431,21 +434,21 @@ const SystemActions = GObject.registerClass({
             throw new Error('The logout action is not available!');
 
         Main.overview.hide();
-        this._session.LogoutRemote(0);
+        this._session.LogoutAsync(0).catch(logError);
     }
 
     activatePowerOff() {
         if (!this._actions.get(POWER_OFF_ACTION_ID).available)
             throw new Error('The power-off action is not available!');
 
-        this._session.ShutdownRemote(0);
+        this._session.ShutdownAsync(0).catch(logError);
     }
 
     activateRestart() {
         if (!this._actions.get(RESTART_ACTION_ID).available)
             throw new Error('The restart action is not available!');
 
-        this._session.RebootRemote();
+        this._session.RebootAsync().catch(logError);
     }
 
     activateSuspend() {

commit 14f061ffab11156691379e6a4ead6f6e558160ff

status/network: Fix handling portal result
Commit 637ee7386e accidentally dropped all parameters from the
method invocation when changing a .bind() handler to an arrow
function.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2545>

js/ui/status/network.js
@@ -1998,7 +1998,7 @@ class Indicator extends SystemIndicator {
             this._portalHelperProxy?.CloseAsync(path);
     }
 
-    async _portalHelperDone(proxy, emitter, parameters) {
+    async _portalHelperDone(parameters) {
         let [path, result] = parameters;
 
         if (result == PortalHelperResult.CANCELLED) {
@@ -2051,7 +2051,9 @@ class Indicator extends SystemIndicator {
                 g_interface_info: PortalHelperInfo,
             });
             this._portalHelperProxy.connectSignal('Done',
-                () => this._portalHelperDone().catch(logError));
+                (proxy, emitter, params) => {
+                    this._portalHelperDone(params).catch(logError);
+                });
 
             try {
                 await this._portalHelperProxy.init_async(