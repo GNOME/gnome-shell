GNOME/gnome-shell

commit 26235bbe5433c48b81bd3917aaf9ef14ff1929b2

js: Use (dis)connectObject()
Start using the new methods to simplify signal cleanup. For now,
focus on replacing existing cleanups; in most cases this means
signals connected in the constructor and disconnected on destroy,
but also other cases with a similarly defined lifetime (say: from
show to hide).

This doesn't change signal connections that only exist for a short
time (say: once), handlers that are connected on-demand (say: the
first time a particular method is called), or connections that
aren't tracked (read: disconnected) at all.

We will eventually replace the latter with connectObject() as
well - especially from actor subclasses - but the changeset is
already big enough as-is :-)

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1953>

js/ui/dialog.js
@@ -20,7 +20,6 @@ class Dialog extends St.Widget {
         this.connect('destroy', this._onDestroy.bind(this));
 
         this._initialKeyFocus = null;
-        this._initialKeyFocusDestroyId = 0;
         this._pressedKey = null;
         this._buttonKeys = {};
         this._createDialog();
@@ -61,9 +60,7 @@ class Dialog extends St.Widget {
     }
 
     makeInactive() {
-        if (this._eventId != 0)
-            this._parentActor.disconnect(this._eventId);
-        this._eventId = 0;
+        this._parentActor.disconnectObject(this);
 
         this.buttonLayout.get_children().forEach(c => c.set_reactive(false));
     }
@@ -99,15 +96,12 @@ class Dialog extends St.Widget {
     }
 
     _setInitialKeyFocus(actor) {
-        if (this._initialKeyFocus)
-            this._initialKeyFocus.disconnect(this._initialKeyFocusDestroyId);
+        this._initialKeyFocus?.disconnectObject(this);
 
         this._initialKeyFocus = actor;
 
-        this._initialKeyFocusDestroyId = actor.connect('destroy', () => {
-            this._initialKeyFocus = null;
-            this._initialKeyFocusDestroyId = 0;
-        });
+        actor.connectObject('destroy',
+            () => (this._initialKeyFocus = null), this);
     }
 
     get initialKeyFocus() {

js/gdm/loginDialog.js
@@ -55,10 +55,8 @@ var UserListItem = GObject.registerClass({
         });
 
         this.user = user;
-        this._userChangedId = this.user.connect('changed',
-                                                this._onUserChanged.bind(this));
+        this.user.connectObject('changed', this._onUserChanged.bind(this), this);
 
-        this.connect('destroy', this._onDestroy.bind(this));
         this.connect('notify::hover', () => {
             this._setSelected(this.hover);
         });
@@ -98,10 +96,6 @@ var UserListItem = GObject.registerClass({
             this.add_style_pseudo_class('logged-in');
         else
             this.remove_style_pseudo_class('logged-in');
-    }
-
-    _onDestroy() {
-        this.user.disconnect(this._userChangedId);
     }
 
     vfunc_clicked() {
@@ -441,8 +435,8 @@ var LoginDialog = GObject.registerClass({
                                this._updateLogo.bind(this));
 
         this._textureCache = St.TextureCache.get_default();
-        this._updateLogoTextureId = this._textureCache.connect('texture-file-changed',
-                                                               this._updateLogoTexture.bind(this));
+        this._textureCache.connectObject('texture-file-changed',
+            this._updateLogoTexture.bind(this), this);
 
         this._userSelectionBox = new St.BoxLayout({
             style_class: 'login-dialog-user-selection-box',
@@ -533,16 +527,16 @@ var LoginDialog = GObject.registerClass({
         this._userListLoaded = false;
 
         this._realmManager = new Realmd.Manager();
-        this._realmSignalId = this._realmManager.connect('login-format-changed',
-                                                         this._showRealmLoginHint.bind(this));
+        this._realmManager.connectObject('login-format-changed',
+            this._showRealmLoginHint.bind(this), this);
 
         LoginManager.getLoginManager().getCurrentSessionProxy(this._gotGreeterSessionProxy.bind(this));
 
         // If the user list is enabled, it should take key focus; make sure the
         // screen shield is initialized first to prevent it from stealing the
         // focus later
-        this._startupCompleteId = Main.layoutManager.connect('startup-complete',
-                                                             this._updateDisableUserList.bind(this));
+        Main.layoutManager.connectObject('startup-complete',
+            this._updateDisableUserList.bind(this), this);
     }
 
     _getBannerAllocation(dialogBox) {
@@ -755,12 +749,11 @@ var LoginDialog = GObject.registerClass({
 
     _ensureUserListLoaded() {
         if (!this._userManager.is_loaded) {
-            this._userManagerLoadedId = this._userManager.connect('notify::is-loaded',
+            this._userManager.connectObject('notify::is-loaded',
                 () => {
                     if (this._userManager.is_loaded) {
+                        this._userManager.disconnectObject(this);
                         this._loadUserList();
-                        this._userManager.disconnect(this._userManagerLoadedId);
-                        this._userManagerLoadedId = 0;
                     }
                 });
         } else {
@@ -864,12 +857,10 @@ var LoginDialog = GObject.registerClass({
 
             this._greeter = this._gdmClient.get_greeter_sync(null);
 
-            this._defaultSessionChangedId = this._greeter.connect('default-session-name-changed',
-                                                                  this._onDefaultSessionChanged.bind(this));
-            this._sessionOpenedId = this._greeter.connect('session-opened',
-                                                          this._onSessionOpened.bind(this));
-            this._timedLoginRequestedId = this._greeter.connect('timed-login-requested',
-                                                                this._onTimedLoginRequested.bind(this));
+            this._greeter.connectObject(
+                'default-session-name-changed', this._onDefaultSessionChanged.bind(this),
+                'session-opened', this._onSessionOpened.bind(this),
+                'timed-login-requested', this._onTimedLoginRequested.bind(this), this);
         }
     }
 
@@ -994,11 +985,10 @@ var LoginDialog = GObject.registerClass({
 
     _gotGreeterSessionProxy(proxy) {
         this._greeterSessionProxy = proxy;
-        this._greeterSessionProxyChangedId =
-            proxy.connect('g-properties-changed', () => {
-                if (proxy.Active)
-                    this._loginScreenSessionActivated();
-            });
+        proxy.connectObject('g-properties-changed', () => {
+            if (proxy.Active)
+                this._loginScreenSessionActivated();
+        }, this);
     }
 
     _startSession(serviceName) {
@@ -1214,44 +1204,14 @@ var LoginDialog = GObject.registerClass({
     }
 
     _onDestroy() {
-        if (this._userManagerLoadedId) {
-            this._userManager.disconnect(this._userManagerLoadedId);
-            this._userManagerLoadedId = 0;
-        }
-        if (this._userAddedId) {
-            this._userManager.disconnect(this._userAddedId);
-            this._userAddedId = 0;
-        }
-        if (this._userRemovedId) {
-            this._userManager.disconnect(this._userRemovedId);
-            this._userRemovedId = 0;
-        }
-        if (this._userChangedId) {
-            this._userManager.disconnect(this._userChangedId);
-            this._userChangedId = 0;
-        }
-        this._textureCache.disconnect(this._updateLogoTextureId);
-        Main.layoutManager.disconnect(this._startupCompleteId);
         if (this._settings) {
             this._settings.run_dispose();
             this._settings = null;
         }
-        if (this._greeter) {
-            this._greeter.disconnect(this._defaultSessionChangedId);
-            this._greeter.disconnect(this._sessionOpenedId);
-            this._greeter.disconnect(this._timedLoginRequestedId);
-            this._greeter = null;
-        }
-        if (this._greeterSessionProxy) {
-            this._greeterSessionProxy.disconnect(this._greeterSessionProxyChangedId);
-            this._greeterSessionProxy = null;
-        }
-        if (this._realmManager) {
-            this._realmManager.disconnect(this._realmSignalId);
-            this._realmSignalId = 0;
-            this._realmManager.release();
-            this._realmManager = null;
-        }
+        this._greeter = null;
+        this._greeterSessionProxy = null;
+        this._realmManager?.release();
+        this._realmManager = null;
     }
 
     _loadUserList() {
@@ -1267,26 +1227,22 @@ var LoginDialog = GObject.registerClass({
 
         this._updateDisableUserList();
 
-        this._userAddedId = this._userManager.connect('user-added',
-            (userManager, user) => {
+        this._userManager.connectObject(
+            'user-added', (userManager, user) => {
                 this._userList.addUser(user);
                 this._updateDisableUserList();
-            });
-
-        this._userRemovedId = this._userManager.connect('user-removed',
-            (userManager, user) => {
+            },
+            'user-removed', (userManager, user) => {
                 this._userList.removeUser(user);
                 this._updateDisableUserList();
-            });
-
-        this._userChangedId = this._userManager.connect('user-changed',
-            (userManager, user) => {
+            },
+            'user-changed', (userManager, user) => {
                 if (this._userList.containsUser(user) && user.locked)
                     this._userList.removeUser(user);
                 else if (!this._userList.containsUser(user) && !user.locked)
                     this._userList.addUser(user);
                 this._updateDisableUserList();
-            });
+            }, this);
 
         return GLib.SOURCE_REMOVE;
     }

js/gdm/realmd.js
@@ -23,11 +23,11 @@ var Manager = class {
         this._realms = {};
         this._loginFormat = null;
 
-        this._signalId = this._aggregateProvider.connect('g-properties-changed',
+        this._aggregateProvider.connectObject('g-properties-changed',
             (proxy, properties) => {
                 if ('Realms' in properties.deep_unpack())
                     this._reloadRealms();
-            });
+            }, this);
     }
 
     _reloadRealms() {
@@ -100,7 +100,7 @@ var Manager = class {
                 'org.freedesktop.realmd',
                 '/org/freedesktop/realmd',
                 service => service.ReleaseRemote());
-        this._aggregateProvider.disconnect(this._signalId);
+        this._aggregateProvider.disconnectObject(this);
         this._realms = { };
         this._updateLoginFormat();
     }

js/gdm/util.js
@@ -165,10 +165,9 @@ var ShellUserVerifier = class {
         this.smartcardDetected = false;
         this._checkForSmartcard();
 
-        this._smartcardInsertedId = this._smartcardManager.connect('smartcard-inserted',
-                                                                   this._checkForSmartcard.bind(this));
-        this._smartcardRemovedId = this._smartcardManager.connect('smartcard-removed',
-                                                                  this._checkForSmartcard.bind(this));
+        this._smartcardManager.connectObject(
+            'smartcard-inserted', this._checkForSmartcard.bind(this),
+            'smartcard-removed', this._checkForSmartcard.bind(this), this);
 
         this._messageQueue = [];
         this._messageQueueTimeoutId = 0;
@@ -187,9 +186,8 @@ var ShellUserVerifier = class {
                     this._credentialManagers[service].token);
             }
 
-            this._credentialManagers[service]._authenticatedSignalId =
-                this._credentialManagers[service].connect('user-authenticated',
-                                                          this._onCredentialManagerAuthenticated.bind(this));
+            this._credentialManagers[service].connectObject('user-authenticated',
+                this._onCredentialManagerAuthenticated.bind(this), this);
         }
     }
 
@@ -259,13 +257,12 @@ var ShellUserVerifier = class {
         this._settings.run_dispose();
         this._settings = null;
 
-        this._smartcardManager.disconnect(this._smartcardInsertedId);
-        this._smartcardManager.disconnect(this._smartcardRemovedId);
+        this._smartcardManager.disconnectObject(this);
         this._smartcardManager = null;
 
         for (let service in this._credentialManagers) {
             let credentialManager = this._credentialManagers[service];
-            credentialManager.disconnect(credentialManager._authenticatedSignalId);
+            credentialManager.disconnectObject(this);
             credentialManager = null;
         }
     }
@@ -495,35 +492,26 @@ var ShellUserVerifier = class {
 
     _connectSignals() {
         this._disconnectSignals();
-        this._signalIds = [];
 
-        let id = this._userVerifier.connect('info', this._onInfo.bind(this));
-        this._signalIds.push(id);
-        id = this._userVerifier.connect('problem', this._onProblem.bind(this));
-        this._signalIds.push(id);
-        id = this._userVerifier.connect('info-query', this._onInfoQuery.bind(this));
-        this._signalIds.push(id);
-        id = this._userVerifier.connect('secret-info-query', this._onSecretInfoQuery.bind(this));
-        this._signalIds.push(id);
-        id = this._userVerifier.connect('conversation-stopped', this._onConversationStopped.bind(this));
-        this._signalIds.push(id);
-        id = this._userVerifier.connect('service-unavailable', this._onServiceUnavailable.bind(this));
-        this._signalIds.push(id);
-        id = this._userVerifier.connect('reset', this._onReset.bind(this));
-        this._signalIds.push(id);
-        id = this._userVerifier.connect('verification-complete', this._onVerificationComplete.bind(this));
-        this._signalIds.push(id);
+        this._userVerifier.connectObject(
+            'info', this._onInfo.bind(this),
+            'problem', this._onProblem.bind(this),
+            'info-query', this._onInfoQuery.bind(this),
+            'secret-info-query', this._onSecretInfoQuery.bind(this),
+            'conversation-stopped', this._onConversationStopped.bind(this),
+            'service-unavailable', this._onServiceUnavailable.bind(this),
+            'reset', this._onReset.bind(this),
+            'verification-complete', this._onVerificationComplete.bind(this),
+            this);
 
-        if (this._userVerifierChoiceList)
-            this._userVerifierChoiceList.connect('choice-query', this._onChoiceListQuery.bind(this));
+        if (this._userVerifierChoiceList) {
+            this._userVerifierChoiceList.connectObject('choice-query',
+                this._onChoiceListQuery.bind(this), this);
+        }
     }
 
     _disconnectSignals() {
-        if (!this._signalIds || !this._userVerifier)
-            return;
-
-        this._signalIds.forEach(s => this._userVerifier.disconnect(s));
-        this._signalIds = [];
+        this._userVerifier?.disconnectObject(this);
     }
 
     _getForegroundService() {

js/misc/util.js
@@ -315,10 +315,9 @@ function createTimeLabel(date, params) {
         _desktopSettings = new Gio.Settings({ schema_id: 'org.gnome.desktop.interface' });
 
     let label = new St.Label({ text: formatTime(date, params) });
-    let id = _desktopSettings.connect('changed::clock-format', () => {
-        label.text = formatTime(date, params);
-    });
-    label.connect('destroy', () => _desktopSettings.disconnect(id));
+    _desktopSettings.connectObject(
+        'changed::clock-format', () => (label.text = formatTime(date, params)),
+        label);
     return label;
 }
 

js/ui/altTab.js
@@ -400,7 +400,6 @@ class CyclerHighlight extends St.Widget {
     _init() {
         super._init({ layout_manager: new Clutter.BinLayout() });
         this._window = null;
-        this._sizeChangedId = 0;
 
         this._clone = new Clutter.Clone();
         this.add_actor(this._clone);
@@ -421,8 +420,7 @@ class CyclerHighlight extends St.Widget {
         if (this._window == w)
             return;
 
-        if (this._sizeChangedId)
-            this._window.disconnect(this._sizeChangedId);
+        this._window?.disconnectObject(this);
 
         this._window = w;
 
@@ -438,8 +436,8 @@ class CyclerHighlight extends St.Widget {
 
         if (this._window) {
             this._onSizeChanged();
-            this._sizeChangedId = this._window.connect('size-changed',
-                this._onSizeChanged.bind(this));
+            this._window.connectObject('size-changed',
+                this._onSizeChanged.bind(this), this);
         } else {
             this._highlight.set_size(0, 0);
             this._highlight.hide();
@@ -723,9 +721,8 @@ class AppSwitcher extends SwitcherPopup.SwitcherList {
         if (this._mouseTimeOutId != 0)
             GLib.source_remove(this._mouseTimeOutId);
 
-        this.icons.forEach(icon => {
-            icon.app.disconnect(icon._stateChangedId);
-        });
+        this.icons.forEach(
+            icon => icon.app.disconnectObject(this));
     }
 
     _setIconSize() {
@@ -868,10 +865,10 @@ class AppSwitcher extends SwitcherPopup.SwitcherList {
         this.icons.push(appIcon);
         let item = this.addItem(appIcon, appIcon.label);
 
-        appIcon._stateChangedId = appIcon.app.connect('notify::state', app => {
+        appIcon.app.connectObject('notify::state', app => {
             if (app.state != Shell.AppState.RUNNING)
                 this._removeIcon(app);
-        });
+        }, this);
 
         let arrow = new St.DrawingArea({ style_class: 'switcher-arrow' });
         arrow.connect('repaint', () => SwitcherPopup.drawArrow(arrow, St.Side.BOTTOM));
@@ -962,9 +959,8 @@ class ThumbnailSwitcher extends SwitcherPopup.SwitcherList {
             this._thumbnailBins[i].set_height(binHeight);
             this._thumbnailBins[i].add_actor(clone);
 
-            clone._destroyId = mutterWindow.connect('destroy', source => {
-                this._removeThumbnail(source, clone);
-            });
+            mutterWindow.connectObject('destroy',
+                source => this._removeThumbnail(source, clone), this);
             this._clones.push(clone);
         }
 
@@ -989,10 +985,8 @@ class ThumbnailSwitcher extends SwitcherPopup.SwitcherList {
     }
 
     _onDestroy() {
-        this._clones.forEach(clone => {
-            if (clone.source)
-                clone.source.disconnect(clone._destroyId);
-        });
+        this._clones.forEach(
+            clone => clone?.source.disconnectObject(this));
     }
 });
 
@@ -1077,18 +1071,16 @@ class WindowSwitcher extends SwitcherPopup.SwitcherList {
             this.addItem(icon, icon.label);
             this.icons.push(icon);
 
-            icon._unmanagedSignalId = icon.window.connect('unmanaged', window => {
-                this._removeWindow(window);
-            });
+            icon.window.connectObject('unmanaged',
+                window => this._removeWindow(window), this);
         }
 
         this.connect('destroy', this._onDestroy.bind(this));
     }
 
     _onDestroy() {
-        this.icons.forEach(icon => {
-            icon.window.disconnect(icon._unmanagedSignalId);
-        });
+        this.icons.forEach(
+            icon => icon.window.disconnectObject(this));
     }
 
     vfunc_get_preferred_height(forWidth) {

js/ui/animation.js
@@ -22,11 +22,11 @@ class Animation extends St.Bin {
         this.connect('resource-scale-changed',
             this._loadFile.bind(this, file, width, height));
 
-        this._scaleChangedId = themeContext.connect('notify::scale-factor',
+        themeContext.connectObject('notify::scale-factor',
             () => {
                 this._loadFile(file, width, height);
                 this.set_size(width * themeContext.scale_factor, height * themeContext.scale_factor);
-            });
+            }, this);
 
         this._speed = speed;
 
@@ -122,11 +122,6 @@ class Animation extends St.Bin {
 
     _onDestroy() {
         this.stop();
-
-        let themeContext = St.ThemeContext.get_for_stage(global.stage);
-        if (this._scaleChangedId)
-            themeContext.disconnect(this._scaleChangedId);
-        this._scaleChangedId = 0;
     }
 });
 

js/ui/appDisplay.js
@@ -330,15 +330,13 @@ var BaseAppView = GObject.registerClass({
 
         // Filter the apps through the user’s parental controls.
         this._parentalControlsManager = ParentalControlsManager.getDefault();
-        this._appFilterChangedId =
-            this._parentalControlsManager.connect('app-filter-changed', () => {
-                this._redisplay();
-            });
+        this._parentalControlsManager.connectObject('app-filter-changed',
+            () => this._redisplay(), this);
 
         // Don't duplicate favorites
         this._appFavorites = AppFavorites.getAppFavorites();
-        this._appFavoritesChangedId =
-            this._appFavorites.connect('changed', () => this._redisplay());
+        this._appFavorites.connectObject('changed',
+            () => this._redisplay(), this);
 
         // Drag n' Drop
         this._lastOvershoot = -1;
@@ -355,16 +353,6 @@ var BaseAppView = GObject.registerClass({
     }
 
     _onDestroy() {
-        if (this._appFilterChangedId > 0) {
-            this._parentalControlsManager.disconnect(this._appFilterChangedId);
-            this._appFilterChangedId = 0;
-        }
-
-        if (this._appFavoritesChangedId > 0) {
-            this._appFavorites.disconnect(this._appFavoritesChangedId);
-            this._appFavoritesChangedId = 0;
-        }
-
         if (this._swipeTracker) {
             this._swipeTracker.destroy();
             delete this._swipeTracker;
@@ -1165,14 +1153,9 @@ var BaseAppView = GObject.registerClass({
         return (translationX - baseOffset) * page;
     }
 
-    _getPagePreviewAdjustment(page) {
-        const previewedPage = this._previewedPages.get(page);
-        return previewedPage?.adjustment;
-    }
-
     _syncClip() {
-        const nextPageAdjustment = this._getPagePreviewAdjustment(1);
-        const prevPageAdjustment = this._getPagePreviewAdjustment(-1);
+        const nextPageAdjustment = this._previewedPages.get(1);
+        const prevPageAdjustment = this._previewedPages.get(-1);
         this._grid.clip_to_view =
             (!prevPageAdjustment || prevPageAdjustment.value === 0) &&
             (!nextPageAdjustment || nextPageAdjustment.value === 0);
@@ -1180,7 +1163,7 @@ var BaseAppView = GObject.registerClass({
 
     _setupPagePreview(page, state) {
         if (this._previewedPages.has(page))
-            return this._previewedPages.get(page).adjustment;
+            return this._previewedPages.get(page);
 
         const adjustment = new St.Adjustment({
             actor: this,
@@ -1191,7 +1174,7 @@ var BaseAppView = GObject.registerClass({
         const indicator = page > 0
             ? this._nextPageIndicator : this._prevPageIndicator;
 
-        const notifyId = adjustment.connect('notify::value', () => {
+        adjustment.connectObject('notify::value', () => {
             const nextPage = this._grid.currentPage + page;
             const hasFollowingPage = nextPage >= 0 &&
                 nextPage < this._grid.nPages;
@@ -1229,23 +1212,20 @@ var BaseAppView = GObject.registerClass({
                 });
             }
             this._syncClip();
-        });
+        }, this);
 
-        this._previewedPages.set(page, {
-            adjustment,
-            notifyId,
-        });
+        this._previewedPages.set(page, adjustment);
 
         return adjustment;
     }
 
     _teardownPagePreview(page) {
-        const previewedPage = this._previewedPages.get(page);
-        if (!previewedPage)
+        const adjustment = this._previewedPages.get(page);
+        if (!adjustment)
             return;
 
-        previewedPage.adjustment.value = 1;
-        previewedPage.adjustment.disconnect(previewedPage.notifyId);
+        adjustment.value = 1;
+        adjustment.disconnectObject(this);
         this._previewedPages.delete(page);
     }
 
@@ -1266,7 +1246,7 @@ var BaseAppView = GObject.registerClass({
             this._prevPageIndicator.remove_style_class_name('dnd');
         }
 
-        adjustment = this._getPagePreviewAdjustment(1);
+        adjustment = this._previewedPages.get(1);
         if (showingNextPage) {
             adjustment = this._setupPagePreview(1, state);
 
@@ -1289,7 +1269,7 @@ var BaseAppView = GObject.registerClass({
             });
         }
 
-        adjustment = this._getPagePreviewAdjustment(-1);
+        adjustment = this._previewedPages.get(-1);
         if (showingPrevPage) {
             adjustment = this._setupPagePreview(-1, state);
 
@@ -1403,7 +1383,6 @@ class AppDisplay extends BaseAppView {
 
         this._currentDialog = null;
         this._displayingDialog = false;
-        this._currentDialogDestroyId = 0;
 
         this._placeholder = null;
 
@@ -1698,19 +1677,14 @@ class AppDisplay extends BaseAppView {
     addFolderDialog(dialog) {
         Main.layoutManager.overviewGroup.add_child(dialog);
         dialog.connect('open-state-changed', (o, isOpen) => {
-            if (this._currentDialog) {
-                this._currentDialog.disconnect(this._currentDialogDestroyId);
-                this._currentDialogDestroyId = 0;
-            }
+            this._currentDialog?.disconnectObject(this);
 
             this._currentDialog = null;
 
             if (isOpen) {
                 this._currentDialog = dialog;
-                this._currentDialogDestroyId = dialog.connect('destroy', () => {
-                    this._currentDialog = null;
-                    this._currentDialogDestroyId = 0;
-                });
+                this._currentDialog.connectObject('destroy',
+                    () => (this._currentDialog = null), this);
             }
             this._displayingDialog = isOpen;
         });
@@ -2418,8 +2392,8 @@ var FolderIcon = GObject.registerClass({
 
         this.view = new FolderView(this._folder, id, parentView);
 
-        this._folderChangedId = this._folder.connect(
-            'changed', this._sync.bind(this));
+        this._folder.connectObject(
+            'changed', this._sync.bind(this), this);
         this._sync();
     }
 
@@ -2430,11 +2404,6 @@ var FolderIcon = GObject.registerClass({
             this._dialog.destroy();
         else
             this.view.destroy();
-
-        if (this._folderChangedId) {
-            this._folder.disconnect(this._folderChangedId);
-            delete this._folderChangedId;
-        }
     }
 
     vfunc_clicked() {
@@ -3110,9 +3079,8 @@ var AppIcon = GObject.registerClass({
         this._menuManager = new PopupMenu.PopupMenuManager(this);
 
         this._menuTimeoutId = 0;
-        this._stateChangedId = this.app.connect('notify::state', () => {
-            this._updateRunningStyle();
-        });
+        this.app.connectObject('notify::state',
+            () => this._updateRunningStyle(), this);
         this._updateRunningStyle();
     }
 
@@ -3123,10 +3091,7 @@ var AppIcon = GObject.registerClass({
             GLib.source_remove(this._folderPreviewId);
             this._folderPreviewId = 0;
         }
-        if (this._stateChangedId > 0)
-            this.app.disconnect(this._stateChangedId);
 
-        this._stateChangedId = 0;
         this._removeMenuTimeout();
     }
 
@@ -3221,12 +3186,8 @@ var AppIcon = GObject.registerClass({
                 if (!isPoppedUp)
                     this._onMenuPoppedDown();
             });
-            let id = Main.overview.connect('hiding', () => {
-                this._menu.close();
-            });
-            this.connect('destroy', () => {
-                Main.overview.disconnect(id);
-            });
+            Main.overview.connectObject('hiding',
+                () => this._menu.close(), this);
 
             Main.uiGroup.add_actor(this._menu.actor);
             this._menuManager.addMenu(this._menu);

commit 7f4db052a0b36f5409baac4f80c58550b9689d5a

dialog: Remove incorrect disconnectObject()
It was added, as there was a previous signal disconnect in this
function, but it happened to be dead code, and the disconnectObject()
call was added in error. Remove it.

Fixes: 26235bbe5433c48b81bd3917aaf9ef14ff1929b2
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1349>

js/ui/dialog.js
@@ -60,8 +60,6 @@ class Dialog extends St.Widget {
     }
 
     makeInactive() {
-        this._parentActor.disconnectObject(this);
-
         this.buttonLayout.get_children().forEach(c => c.set_reactive(false));
     }
 