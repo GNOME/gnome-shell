c2412dbe592aadc948cd5d4d34023eae059da3a7
Jonas Ådahl <jadahl@gmail.com>
2023-03-24 11:28:11 +0100

    tests/closeWithActiveWindows: Also test input method tear down
    From a test case perspective, it's simple - make the test window have a
    text entry, and make sure we have a virtual keyboard making GTK3 enable
    the text entry. This would without a fix trigger
    https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/6535.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2722>

--- js/perf/closeWithActiveWindows.js
+++ js/perf/closeWithActiveWindows.js
@@ -2,6 +2,8 @@
 /* exported run */
 /* eslint camelcase: ["error", { properties: "never", allow: ["^script_", "^malloc", "^glx", "^clutter"] }] */
 
+const {Clutter} = imports.gi;
+
 const Main = imports.ui.main;
 const Scripting = imports.ui.scripting;
 
@@ -12,6 +14,10 @@ async function run() {
     /* Make created windows remain visible during exit. */
     Scripting.disableHelperAutoExit();
 
+    const seat = Clutter.get_default_backend().get_default_seat();
+    const virtualDevice_ =
+        seat.create_virtual_device(Clutter.InputDeviceType.KEYBOARD_DEVICE);
+
     Main.overview.hide();
     await Scripting.waitLeisure();
     await Scripting.sleep(1000);
@@ -19,6 +25,7 @@ async function run() {
     await Scripting.createTestWindow({
         width: 640,
         height: 480,
+        textInput: true,
     });
 
     await Scripting.waitTestWindows();
