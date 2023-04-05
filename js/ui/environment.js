9168e2fc63f7e0384e6d4d6cf4c568544c3e41a0
Florian Müllner <fmuellner@gnome.org>
2023-04-05 17:27:21 +0200

    environment: Remove old compatibility code
    St.set_slow_down_factor() has been deprecated for over 3 years
    (since 3.34). That's plenty of time for extensions to switch to
    St.Settings.get().slow_down_factor.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2743>

--- js/ui/environment.js
+++ js/ui/environment.js
@@ -398,12 +398,6 @@ function init() {
         return Shell.util_touch_file_finish(this, result);
     };
 
-    St.set_slow_down_factor = function (factor) {
-        let { stack } = new Error();
-        log(`St.set_slow_down_factor() is deprecated, use St.Settings.slow_down_factor\n${stack}`);
-        St.Settings.get().slow_down_factor = factor;
-    };
-
     let origToString = Object.prototype.toString;
     Object.prototype.toString = function () {
         let base = origToString.call(this);
