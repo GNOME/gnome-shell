prototypefund/verdre-gnome-shell

screencastService: Set PipelineState to PLAYING on actual state-change
Right now when we tell gstreamer to move the pipeline to the state
PLAYING, we pretend that happens immediately and set our PipelineState
to PLAYING right afterwards.

In reality though it's more complicated than that: Gstreamer changes
states asynchronously and returns us a Gst.StateChangeReturn.ASYNC. In
that case we should wait until the according STATE_CHANGED event happens
on the gst bus, and only then set our PipelineState to PLAYING.

js/dbusServices/screencast/screencastService.js
@@ -187,11 +187,15 @@ var Recorder = class {
         const bus = this._pipeline.get_bus();
         bus.add_watch(bus, this._onBusMessage.bind(this));
 
-        this._pipeline.set_state(Gst.State.PLAYING);
-        this._pipelineState = PipelineState.PLAYING;
+        const stateChangeReturn = this._pipeline.set_state(Gst.State.PLAYING);
 
-        this._requestStartPromise.resolve();
-        delete this._requestStartPromise;
+        if (stateChangeReturn === Gst.StateChangeReturn.SUCCESS ||
+            stateChangeReturn === Gst.StateChangeReturn.ASYNC) {
+            // We'll wait for the state change message to PLAYING on the bus
+        } else {
+            this._teardownPipeline();
+            this._handleFatalPipelineError("Failed to start pipeline");
+        }
     }
 
     startRecording() {
@@ -232,6 +236,20 @@ var Recorder = class {
 
     _onBusMessage(bus, message, _) {
         switch (message.type) {
+        case Gst.MessageType.STATE_CHANGED:
+            const [oldState, newState, pendingState] = message.parse_state_changed();
+
+            if (this._pipelineState === PipelineState.INIT &&
+                message.src === this._pipeline &&
+                oldState === Gst.State.PAUSED && newState === Gst.State.PLAYING) {
+                this._pipelineState = PipelineState.PLAYING;
+
+                this._requestStartPromise.resolve();
+                delete this._requestStartPromise;
+            }
+
+            break;
+
         case Gst.MessageType.EOS:
             if (!this._teardownPipeline())
                 break;

GNOME/gnome-shell

states asynchronously and set_state() returns a Gst.StateChangeReturn.ASYNC.
In that case we should wait until the according STATE_CHANGED event happens
on the bus, and only then set our PipelineState to PLAYING.
Since the STATE_CHANGED event will also happen when set_state() returns
SUCCESS, we can use the same async logic for that.
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2197>
(cherry picked from commit cad01d154c3da966d9679bd1dbf7f030416c2911)

js/dbusServices/screencast/screencastService.js
@@ -183,11 +183,14 @@ var Recorder = class {
         const bus = this._pipeline.get_bus();
         bus.add_watch(bus, this._onBusMessage.bind(this));
 
-        this._pipeline.set_state(Gst.State.PLAYING);
-        this._pipelineState = PipelineState.PLAYING;
+        const retval = this._pipeline.set_state(Gst.State.PLAYING);
 
-        this._startRequest.resolve();
-        delete this._startRequest;
+        if (retval === Gst.StateChangeReturn.SUCCESS ||
+            retval === Gst.StateChangeReturn.ASYNC) {
+            // We'll wait for the state change message to PLAYING on the bus
+        } else {
+            this._handleFatalPipelineError('Failed to start pipeline');
+        }
     }
 
     startRecording() {
@@ -230,6 +233,21 @@ var Recorder = class {
 
     _onBusMessage(bus, message, _) {
         switch (message.type) {
+        case Gst.MessageType.STATE_CHANGED: {
+            const [, newState] = message.parse_state_changed();
+
+            if (this._pipelineState === PipelineState.INIT &&
+                message.src === this._pipeline &&
+                newState === Gst.State.PLAYING) {
+                this._pipelineState = PipelineState.PLAYING;
+
+                this._startRequest.resolve();
+                delete this._startRequest;
+            }
+
+            break;
+        }
+
         case Gst.MessageType.EOS:
             switch (this._pipelineState) {
             case PipelineState.STOPPED: