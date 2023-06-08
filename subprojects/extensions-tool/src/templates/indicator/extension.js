b81beece36c1d4fdcf8876017d3fbc6d882b3334
Evan Welsh <contact@evanwelsh.com>
2023-06-07 21:05:58 -0700

    extensions: Port templates to ESM
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/1499>

--- subprojects/extensions-tool/src/templates/indicator/extension.js
+++ subprojects/extensions-tool/src/templates/indicator/extension.js
@@ -20,10 +20,10 @@ import GObject from 'gi://GObject';
 import St from 'gi://St';
 
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
