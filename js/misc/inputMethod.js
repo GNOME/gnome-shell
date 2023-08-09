GNOME/gnome-shell

commit 17d9ec5788fba48e2c1fd62dedf23097f829e811

inputMethod: Keep Capabilite.FOCUS before context.focus_in/focus_out
If context.focus_out() is called *after* context.set_capabilities(0),
The FocusOut D-Bus method is ignored because of no FOCUS capability.
If context.focus_out() is called *before* context.set_capabilities(0),
The 0 capability is set to the next focused context and the
FocusIn D-Bus method is ignored because of no FOCUS capability.
So context.set_capabilities(0) should not be called.

Closes: https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/6415
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2666>

js/misc/inputMethod.js
@@ -188,6 +188,7 @@ export const InputMethod = GObject.registerClass({
     vfunc_focus_in(focus) {
         this._currentFocus = focus;
         if (this._context) {
+            this.update();
             this._context.focus_in();
             this._emitRequestSurrounding();
         }
@@ -371,7 +372,6 @@ export const InputMethod = GObject.registerClass({
     _fullReset() {
         this._context.set_content_type(0, 0);
         this._context.set_cursor_location(0, 0, 0, 0);
-        this._context.set_capabilities(0);
         this._context.reset();
     }
 

commit 5e36a06835587ea823bdd7241a9062dc12a288ce

inputMethod: Check that there is an existing cursor location
Commit 17d9ec5788fb made the input method call update() more eagerly,
but also at times that it does not have a cursor position yet. Make
it bail out correctly in that situation.

Fixes: 17d9ec5788fb ("inputMethod: Keep Capabilite.FOCUS before context.focus_in/focus_out")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2876>

js/misc/inputMethod.js
@@ -380,9 +380,11 @@ export const InputMethod = GObject.registerClass({
             return;
         this._updateCapabilities();
         this._context.set_content_type(this._purpose, this._hints);
-        this._context.set_cursor_location(
-            this._cursorRect.x, this._cursorRect.y,
-            this._cursorRect.width, this._cursorRect.height);
+        if (this._cursorRect) {
+            this._context.set_cursor_location(
+                this._cursorRect.x, this._cursorRect.y,
+                this._cursorRect.width, this._cursorRect.height);
+        }
         this._emitRequestSurrounding();
     }
 });