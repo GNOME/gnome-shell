GNOME/gnome-shell

commit a42f7c23842ba186655f201db7edaf4b24590604

cleanup: Use type-safe comparisons
We have been using type-safe comparisons in new code for quite a while
now, however old code has only been adapted slowly.

Change all the remaining bits to get rid of another legacy style
difference.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2866>

js/ui/osdWindow.js
@@ -65,15 +65,15 @@ class OsdWindow extends Clutter.Actor {
     }
 
     setLabel(label) {
-        this._label.visible = label != undefined;
+        this._label.visible = label !== undefined;
         if (label)
             this._label.text = label;
         this._updateBoxVisibility();
     }
 
     setLevel(value) {
-        this._level.visible = value != undefined;
-        if (value != undefined) {
+        this._level.visible = value !== undefined;
+        if (value !== undefined) {
             if (this.visible) {
                 this._level.ease_property('value', value, {
                     mode: Clutter.AnimationMode.EASE_OUT_QUAD,
@@ -154,7 +154,7 @@ export class OsdWindowManager {
 
     _monitorsChanged() {
         for (let i = 0; i < Main.layoutManager.monitors.length; i++) {
-            if (this._osdWindows[i] == undefined)
+            if (this._osdWindows[i] === undefined)
                 this._osdWindows[i] = new OsdWindow(i);
         }
 
@@ -175,9 +175,9 @@ export class OsdWindowManager {
     }
 
     show(monitorIndex, icon, label, level, maxLevel) {
-        if (monitorIndex != -1) {
+        if (monitorIndex !== -1) {
             for (let i = 0; i < this._osdWindows.length; i++) {
-                if (i == monitorIndex)
+                if (i === monitorIndex)
                     this._showOsdWindow(i, icon, label, level, maxLevel);
                 else
                     this._osdWindows[i].cancel();

commit 215282a320ca8f2b44b950191bed193db2fe4f0c

osdWindow: Allow label and level to be null
Allow passing 'null' to setLevel and setLabel, so 'null' and 'undefined'
behave the same way.

Fixes a regression introduced by a42f7c23, which caused parts of old
notifications to persist into new notifications, or elements being
incorrectly made visible

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2917>

js/ui/osdWindow.js
@@ -65,15 +65,15 @@ class OsdWindow extends Clutter.Actor {
     }
 
     setLabel(label) {
-        this._label.visible = label !== undefined;
-        if (label)
+        this._label.visible = label != null;
+        if (this._label.visible)
             this._label.text = label;
         this._updateBoxVisibility();
     }
 
     setLevel(value) {
-        this._level.visible = value !== undefined;
-        if (value !== undefined) {
+        this._level.visible = value != null;
+        if (this._level.visible) {
             if (this.visible) {
                 this._level.ease_property('value', value, {
                     mode: Clutter.AnimationMode.EASE_OUT_QUAD,