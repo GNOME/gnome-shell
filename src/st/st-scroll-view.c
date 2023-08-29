GNOME/gnome-shell

commit 23b439dc509490984b73ffad8ff227efe4094de6

st/scroll-view: Use ClutterEvent getter methods
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2860>

src/st/st-scroll-view.c
@@ -764,6 +764,7 @@ st_scroll_view_scroll_event (ClutterActor       *self,
 {
   StScrollViewPrivate *priv = ST_SCROLL_VIEW (self)->priv;
   ClutterTextDirection direction;
+  ClutterScrollDirection scroll_direction;
 
   /* don't handle scroll events if requested not to */
   if (!priv->mouse_scroll)
@@ -773,8 +774,9 @@ st_scroll_view_scroll_event (ClutterActor       *self,
     return TRUE;
 
   direction = clutter_actor_get_text_direction (self);
+  scroll_direction = clutter_event_get_scroll_direction ((ClutterEvent *) event);
 
-  switch (event->direction)
+  switch (scroll_direction)
     {
     case CLUTTER_SCROLL_SMOOTH:
       {
@@ -790,7 +792,7 @@ st_scroll_view_scroll_event (ClutterActor       *self,
       break;
     case CLUTTER_SCROLL_UP:
     case CLUTTER_SCROLL_DOWN:
-      adjust_with_direction (priv->vadjustment, event->direction);
+      adjust_with_direction (priv->vadjustment, direction);
       break;
     case CLUTTER_SCROLL_LEFT:
     case CLUTTER_SCROLL_RIGHT:
@@ -798,13 +800,13 @@ st_scroll_view_scroll_event (ClutterActor       *self,
         {
           ClutterScrollDirection dir;
 
-          dir = event->direction == CLUTTER_SCROLL_LEFT ? CLUTTER_SCROLL_RIGHT
+          dir = scroll_direction == CLUTTER_SCROLL_LEFT ? CLUTTER_SCROLL_RIGHT
                                                         : CLUTTER_SCROLL_LEFT;
           adjust_with_direction (priv->hadjustment, dir);
         }
       else
         {
-          adjust_with_direction (priv->hadjustment, event->direction);
+          adjust_with_direction (priv->hadjustment, direction);
         }
       break;
     default:

commit 3cfc9189762f18084d7707d7d4325f735d9de95d

st/scroll-view: Fix issue in st_scroll_view_scroll_event()
adjust_with_direction() uses a ClutterScrollDirection not ClutterTextDirection

Fixes: 23b439dc ("st/scroll-view: Use ClutterEvent getter methods")
Closes https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/6937

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2920>

src/st/st-scroll-view.c
@@ -793,7 +793,7 @@ st_scroll_view_scroll_event (ClutterActor *self,
       break;
     case CLUTTER_SCROLL_UP:
     case CLUTTER_SCROLL_DOWN:
-      adjust_with_direction (priv->vadjustment, direction);
+      adjust_with_direction (priv->vadjustment, scroll_direction);
       break;
     case CLUTTER_SCROLL_LEFT:
     case CLUTTER_SCROLL_RIGHT:
@@ -807,7 +807,7 @@ st_scroll_view_scroll_event (ClutterActor *self,
         }
       else
         {
-          adjust_with_direction (priv->hadjustment, direction);
+          adjust_with_direction (priv->hadjustment, scroll_direction);
         }
       break;
     default: