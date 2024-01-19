GNOME/gnome-shell

commit 29ba8ab03c16fc9359e3c6c765d67a6d5c4bf718

panel: Use connectObject() for overview signals
We expect top bar and activities button to be around for the
entire duration of the session, but some extra caution doesn't
hurt.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3126>

js/ui/panel.js
@@ -427,12 +427,12 @@ class ActivitiesButton extends PanelMenu.Button {
 
         this.add_child(new WorkspaceIndicators());
 
-        Main.overview.connect('showing', () => {
-            this.add_style_pseudo_class('checked');
-        });
-        Main.overview.connect('hiding', () => {
-            this.remove_style_pseudo_class('checked');
-        });
+        Main.overview.connectObject('showing',
+            () => this.add_style_pseudo_class('checked'),
+            this);
+        Main.overview.connectObject('hiding',
+            () => this.remove_style_pseudo_class('checked'),
+            this);
 
         this._xdndTimeOut = 0;
     }
@@ -668,12 +668,12 @@ class Panel extends St.Widget {
         this.connect('button-press-event', this._onButtonPress.bind(this));
         this.connect('touch-event', this._onTouchEvent.bind(this));
 
-        Main.overview.connect('showing', () => {
-            this.add_style_pseudo_class('overview');
-        });
-        Main.overview.connect('hiding', () => {
-            this.remove_style_pseudo_class('overview');
-        });
+        Main.overview.connectObject('showing',
+            () => this.add_style_pseudo_class('overview'),
+            this);
+        Main.overview.connect('hiding',
+            () => this.remove_style_pseudo_class('overview'),
+            this);
 
         Main.layoutManager.panelBox.add_child(this);
         Main.ctrlAltTabManager.addGroup(this,

commit 731bd212c15e2f9d4b8f7ee2e12fab7b815de83e

panel: Actually switch to connectObject()
Fixes: 29ba8ab03c ("panel: Use connectObject() for overview signals")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3129>

js/ui/panel.js
@@ -671,7 +671,7 @@ class Panel extends St.Widget {
         Main.overview.connectObject('showing',
             () => this.add_style_pseudo_class('overview'),
             this);
-        Main.overview.connect('hiding',
+        Main.overview.connectObject('hiding',
             () => this.remove_style_pseudo_class('overview'),
             this);
 