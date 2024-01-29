7b4054564e4b89f0bee570938d2b822e0dc08661
Florian Müllner <fmuellner@gnome.org>
2024-01-29 21:12:44 +0100

    windowManager: Keep dim effect in overview
    Removing the effect dates back to the time when we didn't show
    attached dialogs in the overview. Since we do show attached
    dialogs as such nowadays, it makes sense to keep the effect.
    
    Close https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/6265
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3151>

--- js/ui/windowManager.js
+++ js/ui/windowManager.js
@@ -512,8 +512,6 @@ export class WindowManager {
         this._resizePending = new Set();
         this._destroying = new Set();
 
-        this._dimmedWindows = [];
-
         this._skippedActors = new Set();
 
         this._allowedKeybindings = {};
@@ -928,15 +926,6 @@ export class WindowManager {
             this._stopX11Services(null);
 
             IBusManager.getIBusManager().restartDaemon();
-        });
-
-        Main.overview.connect('showing', () => {
-            for (let i = 0; i < this._dimmedWindows.length; i++)
-                this._undimWindow(this._dimmedWindows[i]);
-        });
-        Main.overview.connect('hiding', () => {
-            for (let i = 0; i < this._dimmedWindows.length; i++)
-                this._dimWindow(this._dimmedWindows[i]);
         });
 
         this._windowMenuManager = new WindowMenu.WindowMenuManager();
@@ -1426,12 +1415,9 @@ export class WindowManager {
 
         if (shouldDim && !window._dimmed) {
             window._dimmed = true;
-            this._dimmedWindows.push(window);
             this._dimWindow(window);
         } else if (!shouldDim && window._dimmed) {
             window._dimmed = false;
-            this._dimmedWindows =
-                this._dimmedWindows.filter(win => win !== window);
             this._undimWindow(window);
         }
     }
@@ -1560,10 +1546,6 @@ export class WindowManager {
     _destroyWindow(shellwm, actor) {
         let window = actor.meta_window;
         window.disconnectObject(actor);
-        if (window._dimmed) {
-            this._dimmedWindows =
-                this._dimmedWindows.filter(win => win !== window);
-        }
 
         if (window.is_attached_dialog())
             this._checkDimming(window.get_transient_for());
