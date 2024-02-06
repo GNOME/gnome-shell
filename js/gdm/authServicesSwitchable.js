bf793a8efd564d585cbf22188410a19fffc7f3b0
Joan Torres Lopez <joantolo@redhat.com>
2024-02-06 14:09:34 -0500

    gdm: Add authServicesSwitchable
    This new authService child is used by SSSD to control multiple authentication
    mechanisms from a single PAM conversation using 'gdm-switchable-auth'
    service. It relies on the new JSON extension with a new 'auth-selection'
    protocol provided by pam_sss.
    
    This initial commit just supports password mechanism. Future commits will add
    support for other mechanisms.
    
    The basic workflow:
    1. Begin verification starts the only service 'gdm-switchable-auth'.
    2. _userVerifierCustomJson emits 'request' signal with a JSON message.
       That message contains the list of available mechanisms and a priority list
       that indicates which mechanism should be used first.
    3. Those mechanisms are formated and stored in _enabledMechanisms. The
       default mechanism is selected and the 'mechanisms-changed' signal is emitted.
    4. When the mechanism is selected, it uses its role with a switch to
       handle the different authentication flows. Depending on the role
       different signals will be emitted, like 'ask-question'...
    5. When handling user feedback, it uses the selected mechanism role with
       a switch to determine how to format the response and sends it using
       _userVerifierCustomJson.
    6. When the verification succeeds, the 'verification-complete' signal
       is emitted, if it fails, 'verification-failed' is emitted.
    
    Now authServicesLegacy and authServicesSwitchable are mutually exclusive, but in
    the next commit they won't be.
    
    When a mechanism is selected, both authServices try to store it.
    Only the one that has it in _enabledMechanisms keeps it; the other gets null.
    The stored selected mechanism indicates whether authServicesLegacy or
    authServicesSwitchable will handle interactions.

---
+++ js/gdm/authServicesSwitchable.js
@@ -0,0 +1,162 @@
+import GObject from 'gi://GObject';
+
+import * as Const from './const.js';
+import * as Util from './util.js';
+import {AuthServices} from './authServices.js';
+
+export const AuthServicesSwitchable = GObject.registerClass({
+}, class AuthServicesSwitchable extends AuthServices {
+    static SupportedRoles = [
+        Const.PASSWORD_ROLE_NAME,
+    ];
+
+    static RoleToService = {
+        [Const.PASSWORD_ROLE_NAME]: Const.SWITCHABLE_AUTH_SERVICE_NAME,
+    };
+
+    _init(params) {
+        super._init(params);
+    }
+
+    _handleAnswerQuery(serviceName, answer) {
+        if (serviceName !== this._selectedMechanism?.serviceName)
+            return;
+
+        let response;
+        switch (this._selectedMechanism.role) {
+        case Const.PASSWORD_ROLE_NAME:
+            response = this._formatResponse(answer);
+            this._sendResponse(response);
+            break;
+        }
+    }
+
+    _handleSelectMechanism() {
+        switch (this._selectedMechanism?.role) {
+        case Const.PASSWORD_ROLE_NAME:
+            this._startPasswordLogin();
+            break;
+        }
+    }
+
+    _handleReset() {
+        this._savedMechanism = null;
+    }
+
+    _handleCancel() {
+        if (this._selectedMechanism)
+            this._savedMechanism = this._selectedMechanism;
+    }
+
+    _handleClear() {
+        this._mechanisms = null;
+        this._priorityList = null;
+        this._enabledMechanisms = null;
+        this._selectedMechanism = null;
+    }
+
+    _handleOnCustomJSONRequest(_serviceName, _protocol, _version, json) {
+        let requestObject;
+
+        try {
+            requestObject = JSON.parse(json);
+        } catch (e) {
+            logError(e);
+            return;
+        }
+
+        const {authSelection} = requestObject;
+        if (authSelection) {
+            this._mechanisms = authSelection.mechanisms;
+            this._priorityList = authSelection.priority;
+
+            if (this._mechanisms)
+                this._updateEnabledMechanisms();
+        }
+    }
+
+    _handleUpdateEnabledMechanisms() {
+        this._enabledMechanisms.push(...Object.keys(this._mechanisms)
+            .map(id => ({
+                serviceName: Const.SWITCHABLE_AUTH_SERVICE_NAME,
+                id,
+                ...this._mechanisms[id],
+            }))
+            // filter out mechanisms with roles that are not enabled
+            .filter(m => this._enabledRoles.includes(m.role)));
+
+        const selectedMechanism =
+            this._enabledMechanisms
+                .find(m => this._savedMechanism?.role === m.role) ??
+            this._priorityList
+                .map(id => this._enabledMechanisms.find(m => m.id === id))[0] ??
+            this._enabledMechanisms[0];
+        this.selectMechanism(selectedMechanism);
+
+        this._savedMechanism = null;
+    }
+
+    _handleOnInfo(serviceName, info) {
+        if (serviceName === this._selectedMechanism?.serviceName)
+            this.emit('queue-message', serviceName, info, Util.MessageType.INFO);
+    }
+
+    _handleOnProblem(serviceName, problem) {
+        if (serviceName === this._selectedMechanism?.serviceName) {
+            this.emit('queue-priority-message',
+                serviceName,
+                problem,
+                Util.MessageType.ERROR);
+        }
+    }
+
+    _handleOnConversationStopped(serviceName) {
+        if (serviceName !== this._selectedMechanism?.serviceName)
+            return;
+
+        if (this._unavailableServices.has(serviceName))
+            return;
+
+        this._failCounter++;
+        this._verificationFailed(serviceName, true);
+    }
+
+    _handleCanStartService(serviceName) {
+        return serviceName === Const.SWITCHABLE_AUTH_SERVICE_NAME &&
+            !this._enabledMechanisms;
+    }
+
+    _formatResponse(answer) {
+        const {role, id} = this._selectedMechanism;
+
+        let response;
+        switch (role) {
+        case Const.PASSWORD_ROLE_NAME: {
+            response = {password: answer};
+            break;
+        }
+        default:
+            throw new GObject.NotImplementedError(`formatResponse: ${role}`);
+        }
+
+        return {
+            authSelection: {
+                status: 'Ok',
+                [id]: response,
+            },
+        };
+    }
+
+    _sendResponse(response) {
+        const {serviceName} = this._selectedMechanism;
+
+        this._userVerifierCustomJSON.call_reply(
+            serviceName, JSON.stringify(response), this._cancellable, null);
+    }
+
+    _startPasswordLogin() {
+        const {serviceName, prompt} = this._selectedMechanism;
+
+        this.emit('ask-question', serviceName, prompt, true);
+    }
+});
