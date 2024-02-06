9e6abb5633efb18d30a544a5bbff2cc7fdc21770
Ray Strode <rstrode@redhat.com>
2024-02-06 12:59:56 -0500

    loginDialog: Move authPrompt.begin to helper function
    Soon, we're going to need to do some other things than firing
    off the auth prompt when beginning verification.
    
    In prepration for that, this commit creates a wrapper function,
    _beginVerification, that can be easily augmented later.

--- js/gdm/loginDialog.js
+++ js/gdm/loginDialog.js
@@ -37,6 +37,7 @@ import * as LoginManager from '../misc/loginManager.js';
 import * as Main from '../ui/main.js';
 import * as MessageTray from '../ui/messageTray.js';
 import * as ModalDialog from '../ui/modalDialog.js';
+import * as Params from '../misc/params.js';
 import * as PopupMenu from '../ui/popupMenu.js';
 import * as Realmd from './realmd.js';
 import * as UserWidget from '../ui/userWidget.js';
@@ -1019,7 +1020,7 @@ export const LoginDialog = GObject.registerClass({
         if (previousUser && beginRequest === AuthPrompt.BeginRequestType.REUSE_USERNAME) {
             this._user = previousUser;
             this._authPrompt.setUser(this._user);
-            this._authPrompt.begin({userName: previousUser.get_user_name()});
+            this._beginVerification({userName: previousUser.get_user_name()});
         } else if (beginRequest === AuthPrompt.BeginRequestType.PROVIDE_USERNAME) {
             if (!this._disableUserList)
                 this._showUserList();
@@ -1087,7 +1088,7 @@ export const LoginDialog = GObject.registerClass({
                 let answer = this._authPrompt.getAnswer();
                 this._user = this._userManager.get_user(answer);
                 this._authPrompt.clear();
-                this._authPrompt.begin({userName: answer});
+                this._beginVerification({userName: answer});
                 this._updateCancelButton();
             });
         this._updateCancelButton();
@@ -1381,6 +1382,15 @@ export const LoginDialog = GObject.registerClass({
         });
     }
 
+    _beginVerification(params) {
+        params = Params.parse(params, {
+            userName: null,
+            hold: null,
+        });
+
+        this._authPrompt.begin(params);
+    }
+
     _setUserListExpanded(expanded) {
         this._userList.updateStyle(expanded);
         this._userSelectionBox.visible = expanded;
@@ -1399,7 +1409,7 @@ export const LoginDialog = GObject.registerClass({
 
     _hideUserListAndBeginVerification() {
         this._hideUserList();
-        this._authPrompt.begin();
+        this._beginVerification();
     }
 
     _showUserList() {
@@ -1419,7 +1429,8 @@ export const LoginDialog = GObject.registerClass({
         let userName = item.user.get_user_name();
         let hold = new Batch.Hold();
 
-        this._authPrompt.begin({userName, hold});
+        this._beginVerification({userName, hold});
+
         return hold;
     }
 
