cb1b54b9542d88d183318678529ac05ec215c728
Julian Sparber <julian@sparber.net>
2024-02-14 11:37:22 +0100

    notification: Remove old setter methods of MessageTray.Notification
    We now have nice GObject properties for the properties that can be set
    with in different ways, since we already changing the API so much let's
    drop the old setter methods.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3173>

--- js/ui/messageTray.js
+++ js/ui/messageTray.js
@@ -494,26 +494,6 @@ export class Notification extends GObject.Object {
         this._actions = [];
     }
 
-    setUrgency(urgency) {
-        this.urgency = urgency;
-    }
-
-    setResident(resident) {
-        this.resident = resident;
-    }
-
-    setTransient(isTransient) {
-        this.isTransient = isTransient;
-    }
-
-    setForFeedback(forFeedback) {
-        this.forFeedback = forFeedback;
-    }
-
-    setPrivacyScope(privacyScope) {
-        this.privacyScope = privacyScope;
-    }
-
     playSound() {
         if (this._soundPlayed)
             return;
