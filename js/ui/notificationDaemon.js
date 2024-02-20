8b07e8e18731eaa667c48b20531a96b8fc570a3e
Florian Müllner <fmuellner@gnome.org>
2024-02-20 19:39:31 +0100

    notificationDaemon: Namespace private `sender-pid` hint
    The hint is a private implementation detail between the public
    and internal services, not something anybody else should set
    (*cough* libnotify *cough*).
    
    Prefix the name to hopefully make that clearer.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3204>
    (cherry picked from commit f0b1cf5f8de2168004c67d3e7318a772c644a627)

--- js/ui/notificationDaemon.js
+++ js/ui/notificationDaemon.js
@@ -179,7 +179,7 @@ class FdoNotificationDaemon {
         this._notifications[id] = ndata;
 
         let sender = invocation.get_sender();
-        let pid = hints['sender-pid'];
+        const pid = hints['x-shell-sender-pid'];
 
         let source = this._getSource(appName, pid, ndata, sender, null);
         this._notifyForSource(source, ndata);
