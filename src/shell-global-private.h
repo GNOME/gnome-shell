61985169a2f492b33d4ecd10bd2075057ff3d3e3
Florian Müllner <fmuellner@gnome.org>
2024-02-21 16:09:32 +0100

    shell: Remove left-over function declaration
    The function itself was removed in commit 4b80cbe1cd9e back
    in 2016.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3206>
    (cherry picked from commit a1dd5d5e1e12d598c90283e54e3972a1402ac27d)

--- src/shell-global-private.h
+++ src/shell-global-private.h
@@ -19,9 +19,6 @@ GjsContext *_shell_global_get_gjs_context (ShellGlobal  *global);
 
 ShellAppCache * shell_global_get_app_cache (ShellGlobal *global);
 
-gboolean _shell_global_check_xdnd_event (ShellGlobal  *global,
-                                         XEvent       *xev);
-
 void _shell_global_locate_pointer (ShellGlobal  *global);
 
 void _shell_global_notify_shutdown (ShellGlobal *global);
