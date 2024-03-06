GNOME/gnome-shell

commit b3f5fdcb6bb68c8b672ce611850c5b50ff44cffe

endSessionDialog: Connect to events in the dialog itself
Connecting to stage events won't work from a modal dialog, since the
grab will take events from the portions of the actor hierarchy above
the grabbing actor.

Connect to events from the dialog itself, so that the end session
dialog can again show the "boot options" easter egg.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2140>

js/ui/endSessionDialog.js
@@ -467,7 +467,7 @@ class EndSessionDialog extends ModalDialog.ModalDialog {
                     label: C_('button', 'Boot Options'),
                 });
                 this._rebootButtonAlt.visible = false;
-                this._capturedEventId = global.stage.connect('captured-event',
+                this._capturedEventId = this.connect('captured-event',
                     this._onCapturedEvent.bind(this));
             }
         }

commit a60d454cca558aa1f8d727dd43182ff319ab5eb2

endSessionDialog: Disconnect signal from the right object
This signal id comes from a signal handler on self, use the right
object when disconnecting it. Spotted by Eugene Popov at
https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2140#note_2039721.

Fixes: b3f5fdcb6 ("endSessionDialog: Connect to events in the dialog itself")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3225>

js/ui/endSessionDialog.js
@@ -476,7 +476,7 @@ class EndSessionDialog extends ModalDialog.ModalDialog {
 
     _stopAltCapture() {
         if (this._capturedEventId > 0) {
-            global.stage.disconnect(this._capturedEventId);
+            this.disconnect(this._capturedEventId);
             this._capturedEventId = 0;
         }
         this._rebootButton = null;