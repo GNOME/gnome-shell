GNOME/gnome-shell

commit cc60ce7f946eab84cd248719f81d43c8f4ff90c0

notification: Stop using 'update()' from MessageTray.Notification
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3173>

js/ui/notificationDaemon.js
@@ -200,13 +200,15 @@ class FdoNotificationDaemon {
         const soundFile = 'sound-file' in hints
             ? Gio.File.new_for_path(hints['sound-file']) : null;
 
-        notification.acknowledged = false;
-        notification.update(summary, body, {
+        notification.set({
+            title: summary,
+            body,
             gicon,
             bannerMarkup: true,
-            clear: true,
             sound: new MessageTray.Sound(soundFile, hints['sound-name']),
+            acknowledged: false,
         });
+        notification.clearActions();
 
         let hasDefaultAction = false;
 
@@ -444,7 +446,9 @@ class GtkNotificationDaemonNotification extends MessageTray.Notification {
         this._defaultAction = defaultAction?.unpack();
         this._defaultActionTarget = defaultActionTarget;
 
-        this.update(title.unpack(), body?.unpack(), {
+        this.set({
+            title: title.unpack(),
+            body: body?.unpack(),
             gicon: gicon
                 ? Gio.icon_deserialize(gicon) : null,
             datetime: time

js/ui/overview.js
@@ -28,7 +28,7 @@ const DND_WINDOW_SWITCH_TIMEOUT = 750;
 const OVERVIEW_ACTIVATION_TIMEOUT = 0.5;
 
 class ShellInfo {
-    setMessage(text, options) {
+    setMessage(title, options) {
         options = Params.parse(options, {
             undoCallback: null,
             forFeedback: false,
@@ -41,14 +41,14 @@ class ShellInfo {
         if (!this._notification) {
             this._notification = new MessageTray.Notification({
                 source,
-                title: text,
                 isTransient: true,
                 forFeedback,
             });
             this._notification.connect('destroy', () => delete this._notification);
-        } else {
-            this._notification.update(text, null, {clear: true});
         }
+        this._notification.set({title});
+
+        this._notification.clearActions();
 
         if (undoCallback)
             this._notification.addAction(_('Undo'), () => undoCallback());

js/ui/shellMountOperation.js
@@ -218,7 +218,7 @@ export class ShellMountOperation {
         if (!this._notification)
             this._createNotification(title, body);
         else
-            this._notification.update(title, body);
+            this._notification.set({title, body});
     }
 }
 

js/ui/windowAttentionHandler.js
@@ -53,7 +53,7 @@ export class WindowAttentionHandler {
 
         window.connectObject('notify::title', () => {
             [title, body] = this._getTitleAndBanner(app, window);
-            notification.update(title, body);
+            notification.set({title, body});
         }, source);
     }
 }

commit 7f476e051cce72ec92c8c041eef485f82108eb0f

notificationDaemon: Use correct property name to enable markup
The property is named useBodyMarkup, not bannerMarkup. This was causing
markup to be disabled for FDO notifications.

Fixes: cc60ce7f94 ("notification: Stop using 'update()' from MessageTray.Notification")
Closes: https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/7495
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3247>

js/ui/notificationDaemon.js
@@ -204,7 +204,7 @@ class FdoNotificationDaemon {
             title: summary,
             body,
             gicon,
-            bannerMarkup: true,
+            useBodyMarkup: true,
             sound: new MessageTray.Sound(soundFile, hints['sound-name']),
             acknowledged: false,
         });