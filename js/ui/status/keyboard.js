GNOME/gnome-shell

commit 04aaa4b67bffbfe6d472e7f25c8e892f43151ed2

keyboard: Spawn "tecla" to show keyboard map
This is a modern replacement for gkbd-keyboard-display, stuck in gtk3
and X11 (libxklavier).

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2834>

js/ui/status/keyboard.js
@@ -1099,6 +1099,6 @@ class InputSourceIndicator extends PanelMenu.Button {
         if (xkbVariant.length > 0)
             description = `${description}\t${xkbVariant}`;
 
-        Util.spawn(['gkbd-keyboard-display', '-l', description]);
+        Util.spawn(['tecla', description]);
     }
 });

commit 658beda9a94607f7443fb3a8cb822149be8d679b

status/keyboard: Use '+' to separate variant when launching tecla
The code was not changed when switching from gkbd-keyboard-display which
was using tab to separate layout and variant. Tecla uses '+' instead.

This fix was suggested by Neil Mayhew.

Fixes: 04aaa4b67 ("keyboard: Spawn "tecla" to show keyboard map")
Closes: https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/7638
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3324>

js/ui/status/keyboard.js
@@ -1129,7 +1129,7 @@ class InputSourceIndicator extends PanelMenu.Button {
 
         let description = xkbLayout;
         if (xkbVariant.length > 0)
-            description = `${description}\t${xkbVariant}`;
+            description = `${description}+${xkbVariant}`;
 
         Util.spawn(['tecla', description]);
     }