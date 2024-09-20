3c2d38ea5ace3adeb7d060b4ee4943fada51eb99
Florian Müllner <fmuellner@gnome.org>
2024-09-20 21:18:37 +0200

    ci: Adjust to dnf5 plugin changes
    F41 includes dnf5, which involves changes to the config-manager
    plugin.

--- .gitlab-ci/build-toolbox-image.sh
+++ .gitlab-ci/build-toolbox-image.sh
@@ -38,7 +38,7 @@ build_container() {
   local debug_packages=(
     glib2 # makes gdb much more useful
   )
-  buildah run $build_cntr dnf config-manager --set-disabled '*-openh264'
+  buildah run $build_cntr dnf config-manager setopt '*-openh264.enabled=0'
   buildah run $build_cntr dnf install -y "${extra_packages[@]}"
   buildah run $build_cntr dnf debuginfo-install -y "${debug_packages[@]}"
   buildah run $build_cntr dnf clean all
