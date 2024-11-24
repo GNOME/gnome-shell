d541aebf03125892b3d9cbf3719cdd7e3c36dbb8
Ray Strode <rstrode@redhat.com>
2024-11-24 12:53:50 -0500

    unlockDialog: Don't user switch until clock is back
    Right now we switch VTs right in the middle of the clock transition,
    which looks rather buggy.
    
    This commit waits until the auth dialog is destroyed before proceeding
    with the VT switch.

--- js/ui/unlockDialog.js
+++ js/ui/unlockDialog.js
@@ -888,8 +888,8 @@ export const UnlockDialog = GObject.registerClass({
     }
 
     _otherUserClicked() {
-        Gdm.goto_login_session_sync(null);
-
+        this._authPrompt.connectObject('destroy', () =>
+            Gdm.goto_login_session_sync(null));
         this._authPrompt.cancel();
     }
 
