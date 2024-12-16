GNOME/gnome-shell

commit 4034e69d0d8e1c722cde84e99c5f68d04bcca3ce

Adapt to fixed Meta functions namespace
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3546>

js/ui/magnifier.js
@@ -5,7 +5,6 @@ import GDesktopEnums from 'gi://GDesktopEnums';
 import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
-import Meta from 'gi://Meta';
 import Shell from 'gi://Shell';
 import St from 'gi://St';
 import * as Signals from '../misc/signals.js';
@@ -100,8 +99,7 @@ export class Magnifier extends Signals.EventEmitter {
         this._zoomRegions = [];
 
         // Create small clutter tree for the magnified mouse.
-        let cursorTracker = Meta.CursorTracker.get_for_display(global.display);
-        this._cursorTracker = cursorTracker;
+        this._cursorTracker = global.backend.get_cursor_tracker();
 
         this._mouseSprite = new Clutter.Actor({request_mode: Clutter.RequestMode.CONTENT_SIZE});
         this._mouseSprite.content = new MouseSpriteContent();
@@ -188,12 +186,12 @@ export class Magnifier extends Signals.EventEmitter {
             this._updateMouseSprite();
             this._cursorTracker.connectObject(
                 'cursor-changed', this._updateMouseSprite.bind(this), this);
-            Meta.disable_unredirect_for_display(global.display);
+            global.display.disable_unredirect();
             this.startTrackingMouse();
         } else {
             this._cursorTracker.disconnectObject(this);
             this._mouseSprite.content.texture = null;
-            Meta.enable_unredirect_for_display(global.display);
+            global.display.enable_unredirect();
             this.stopTrackingMouse();
         }
 

js/ui/boxpointer.js
@@ -1,6 +1,5 @@
 import Clutter from 'gi://Clutter';
 import GObject from 'gi://GObject';
-import Meta from 'gi://Meta';
 import St from 'gi://St';
 
 import * as Main from './main.js';
@@ -53,9 +52,9 @@ export const BoxPointer = GObject.registerClass({
 
         this.connect('notify::visible', () => {
             if (this.visible)
-                Meta.disable_unredirect_for_display(global.display);
+                global.compositor.disable_unredirect();
             else
-                Meta.enable_unredirect_for_display(global.display);
+                global.compositor.enable_unredirect();
         });
     }
 

js/ui/closeDialog.js
@@ -147,7 +147,7 @@ export const CloseDialog = GObject.registerClass({
         if (this._dialog != null)
             return;
 
-        Meta.disable_unredirect_for_display(global.display);
+        global.compositor.disable_unredirect();
 
         this._timeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, ALIVE_TIMEOUT,
             () => {
@@ -182,7 +182,7 @@ export const CloseDialog = GObject.registerClass({
         if (this._dialog == null)
             return;
 
-        Meta.enable_unredirect_for_display(global.display);
+        global.compositor.enable_unredirect();
 
         GLib.source_remove(this._timeoutId);
         this._timeoutId = 0;

js/ui/environment.js
@@ -126,11 +126,11 @@ function _easeActor(actor, params) {
     delete params.mode;
 
     const prepare = () => {
-        Meta.disable_unredirect_for_display(global.display);
+        global.compositor.disable_unredirect();
         global.begin_work();
     };
     const cleanup = () => {
-        Meta.enable_unredirect_for_display(global.display);
+        global.compositor.enable_unredirect();
         global.end_work();
     };
     let callback = _makeEaseCallback(params, cleanup);
@@ -202,11 +202,11 @@ function _easeActorProperty(actor, propName, target, params) {
         duration = 0;
 
     const prepare = () => {
-        Meta.disable_unredirect_for_display(global.display);
+        global.compositor.disable_unredirect();
         global.begin_work();
     };
     const cleanup = () => {
-        Meta.enable_unredirect_for_display(global.display);
+        global.compositor.enable_unredirect();
         global.end_work();
     };
     let callback = _makeEaseCallback(params, cleanup);

js/ui/keyboard.js
@@ -1788,7 +1788,7 @@ export const Keyboard = GObject.registerClass({
     }
 
     _animateShow() {
-        Meta.disable_unredirect_for_display(global.display);
+        global.compositor.disable_unredirect();
 
         if (this._focusWindow)
             this._animateWindow(this._focusWindow, true);
@@ -1844,7 +1844,7 @@ export const Keyboard = GObject.registerClass({
 
     _animateHideComplete() {
         Main.layoutManager.keyboardBox.hide();
-        Meta.enable_unredirect_for_display(global.display);
+        global.compositor.enable_unredirect();
     }
 
     gestureProgress(delta) {

js/ui/main.js
@@ -688,7 +688,7 @@ export function pushModal(actor, params = {}) {
     let grab = global.stage.grab(actor);
 
     if (modalCount === 0)
-        Meta.disable_unredirect_for_display(global.display);
+        global.compositor.disable_unredirect();
 
     modalCount += 1;
     let actorDestroyId = actor.connect('destroy', () => {
@@ -782,7 +782,7 @@ export function popModal(grab) {
         return;
 
     layoutManager.modalEnded();
-    Meta.enable_unredirect_for_display(global.display);
+    global.compositor.enable_unredirect();
     actionMode = Shell.ActionMode.NORMAL;
 }
 

js/ui/messageTray.js
@@ -1130,7 +1130,7 @@ export const MessageTray = GObject.registerClass({
         this._bannerBin.y = -this._banner.height;
         this.show();
 
-        Meta.disable_unredirect_for_display(global.display);
+        global.compositor.disable_unredirect();
         this._updateShowingNotification();
 
         let [x, y] = global.get_pointer();
@@ -1267,7 +1267,7 @@ export const MessageTray = GObject.registerClass({
 
         this._pointerInNotification = false;
         this._notificationRemoved = false;
-        Meta.enable_unredirect_for_display(global.display);
+        global.compositor.enable_unredirect();
 
         this._banner.destroy();
         this._banner = null;

js/ui/osdMonitorLabeler.js
@@ -1,7 +1,6 @@
 import Clutter from 'gi://Clutter';
 import Gio from 'gi://Gio';
 import GObject from 'gi://GObject';
-import Meta from 'gi://Meta';
 import St from 'gi://St';
 
 
@@ -29,9 +28,9 @@ class OsdMonitorLabel extends St.Widget {
         Main.uiGroup.set_child_above_sibling(this, null);
         this._position();
 
-        Meta.disable_unredirect_for_display(global.display);
+        global.compositor.disable_unredirect();
         this.connect('destroy', () => {
-            Meta.enable_unredirect_for_display(global.display);
+            global.compositor.enable_unredirect();
         });
     }
 

commit f89276fc5604ab013735f403648db9e3cddc14c3

magnifier: Fix (un)inhibiting unredirection
The methods for (un)inhibiting unredirection are on the compositor,
not the display.

Fixes: 4034e69d0d ("Adapt to fixed Meta functions namespace")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3575>

js/ui/magnifier.js
@@ -186,12 +186,12 @@ export class Magnifier extends Signals.EventEmitter {
             this._updateMouseSprite();
             this._cursorTracker.connectObject(
                 'cursor-changed', this._updateMouseSprite.bind(this), this);
-            global.display.disable_unredirect();
+            global.compositor.disable_unredirect();
             this.startTrackingMouse();
         } else {
             this._cursorTracker.disconnectObject(this);
             this._mouseSprite.content.texture = null;
-            global.display.enable_unredirect();
+            global.compositor.enable_unredirect();
             this.stopTrackingMouse();
         }
 