GNOME/gnome-shell

commit b75b4abaf0fdae7cb6f28b0c69830656e5706b14

js: Set BoxLayout orientation
Use new `orientation` property instead of `vertical`.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3614>

js/ui/ibusCandidatePopup.js
@@ -29,7 +29,7 @@ const CandidateArea = GObject.registerClass({
 }, class CandidateArea extends St.BoxLayout {
     _init() {
         super._init({
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
             reactive: true,
             visible: false,
         });
@@ -158,7 +158,7 @@ class IbusCandidatePopup extends BoxPointer.BoxPointer {
 
         const box = new St.BoxLayout({
             style_class: 'candidate-popup-content',
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
         });
         this.bin.set_child(box);
 

js/gdm/authList.js
@@ -74,7 +74,7 @@ export const AuthList = GObject.registerClass({
 }, class AuthList extends St.BoxLayout {
     _init() {
         super._init({
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
             style_class: 'login-dialog-auth-list-layout',
             x_align: Clutter.ActorAlign.START,
             y_align: Clutter.ActorAlign.CENTER,
@@ -84,7 +84,7 @@ export const AuthList = GObject.registerClass({
         this.add_child(this.label);
 
         this._box = new St.BoxLayout({
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
             style_class: 'login-dialog-auth-list',
             pseudo_class: 'expanded',
         });

js/gdm/authPrompt.js
@@ -55,7 +55,7 @@ export const AuthPrompt = GObject.registerClass({
     _init(gdmClient, mode) {
         super._init({
             style_class: 'login-dialog-prompt-layout',
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
             x_expand: true,
             x_align: Clutter.ActorAlign.CENTER,
             reactive: true,
@@ -146,7 +146,7 @@ export const AuthPrompt = GObject.registerClass({
     _initInputRow() {
         this._mainBox = new St.BoxLayout({
             style_class: 'login-dialog-button-box',
-            vertical: false,
+            orientation: Clutter.Orientation.HORIZONTAL,
         });
         this.add_child(this._mainBox);
 

js/gdm/loginDialog.js
@@ -51,7 +51,7 @@ export const UserListItem = GObject.registerClass({
 }, class UserListItem extends St.Button {
     _init(user) {
         let layout = new St.BoxLayout({
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
             x_expand: true,
         });
         super._init({
@@ -174,7 +174,7 @@ const UserList = GObject.registerClass({
         });
 
         this._box = new St.BoxLayout({
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
             style_class: 'login-dialog-user-list',
             pseudo_class: 'expanded',
         });
@@ -434,7 +434,7 @@ export const ConflictingSessionDialog = GObject.registerClass({
 
         let textLayout = new St.BoxLayout({
             style_class: 'conflicting-session-dialog-content',
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
             x_expand: true,
         });
 
@@ -521,7 +521,7 @@ export const LoginDialog = GObject.registerClass({
             style_class: 'login-dialog-user-selection-box',
             x_align: Clutter.ActorAlign.CENTER,
             y_align: Clutter.ActorAlign.CENTER,
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
             visible: false,
         });
         this.add_child(this._userSelectionBox);
@@ -558,7 +558,9 @@ export const LoginDialog = GObject.registerClass({
 
         this._userSelectionBox.add_child(this._notListedButton);
 
-        const bannerBox = new St.BoxLayout({vertical: true});
+        const bannerBox = new St.BoxLayout({
+            orientation: Clutter.Orientation.VERTICAL,
+        });
 
         this._bannerView = new St.ScrollView({
             style_class: 'login-dialog-banner-view',

js/misc/breakManager.js
@@ -1378,7 +1378,7 @@ class OsdBreakCountdownLabel extends St.Widget {
         });
 
         this._box = new St.BoxLayout({
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
         });
         this.add_child(this._box);
 

js/ui/altTab.js
@@ -667,7 +667,7 @@ class AppIcon extends St.BoxLayout {
     _init(app) {
         super._init({
             style_class: 'alt-tab-app',
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
         });
 
         this.app = app;
@@ -919,7 +919,7 @@ class ThumbnailSwitcher extends SwitcherPopup.SwitcherList {
         for (let i = 0; i < windows.length; i++) {
             const box = new St.BoxLayout({
                 style_class: 'thumbnail-box',
-                vertical: true,
+                orientation: Clutter.Orientation.VERTICAL,
             });
 
             let bin = new St.Bin({style_class: 'thumbnail'});
@@ -1001,7 +1001,7 @@ class WindowIcon extends St.BoxLayout {
     _init(window, mode) {
         super._init({
             style_class: 'alt-tab-app',
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
         });
 
         this.window = window;

js/ui/appDisplay.js
@@ -610,7 +610,7 @@ var BaseAppView = GObject.registerClass({
         scrollContainer._delegate = this;
 
         this._box = new St.BoxLayout({
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
             x_expand: true,
             y_expand: true,
         });
@@ -2527,7 +2527,7 @@ export const AppFolderDialog = GObject.registerClass({
             y_expand: true,
             x_align: Clutter.ActorAlign.CENTER,
             y_align: Clutter.ActorAlign.CENTER,
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
         });
 
         this.child = new St.Bin({

js/ui/audioDeviceSelection.js
@@ -98,7 +98,7 @@ const AudioDeviceSelectionDialog = GObject.registerClass({
     _addDevice(device) {
         const box = new St.BoxLayout({
             style_class: 'audio-selection-device-box',
-            vertical: true,
+            orientation: Clutter.Orientation.VERTICAL,
         });
         box.connect('notify::height', () => {
             const laters = global.compositor.get_laters();

commit e6e0ccdf2714aadfed5c20c6b574fc3bfdbee6e3

ibusCandidatePopup: Fix live orientation changes based on IBus info
This is rare, and was missed in the replacements towards the new
'orientation' property.

Closes: https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/8177
Fixes: b75b4abaf ("js: Set BoxLayout orientation")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3618>

js/ui/ibusCandidatePopup.js
@@ -100,13 +100,13 @@ const CandidateArea = GObject.registerClass({
         this._orientation = orientation;
 
         if (this._orientation === IBus.Orientation.HORIZONTAL) {
-            this.vertical = false;
+            this.orientation = Clutter.Orientation.HORIZONTAL;
             this.remove_style_class_name('vertical');
             this.add_style_class_name('horizontal');
             this._previousButton.icon_name = 'go-previous-symbolic';
             this._nextButton.icon_name = 'go-next-symbolic';
         } else {                // VERTICAL || SYSTEM
-            this.vertical = true;
+            this.orientation = Clutter.Orientation.VERTICAL;
             this.add_style_class_name('vertical');
             this.remove_style_class_name('horizontal');
             this._previousButton.icon_name = 'go-up-symbolic';