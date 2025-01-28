GNOME/gnome-shell

commit e53f69f9d3fe17df09df1225e09bd4adab591455

shellDBus: Add AcceleratorDeactivated D-Bus signal
Add this signal to complement AcceleratorActivated, meant to happen
on the release action of shortcuts, e.g. on push-to-talk.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3598>

js/ui/shellDBus.js
@@ -37,6 +37,10 @@ export class GnomeShell {
             (display, action, device, timestamp) => {
                 this._emitAcceleratorActivated(action, device, timestamp);
             });
+        global.display.connect('accelerator-deactivated',
+            (display, action, device, timestamp) => {
+                this._emitAcceleratorDeactivated(action, device, timestamp);
+            });
 
         this._cachedOverviewVisible = false;
         Main.overview.connect('showing',

data/dbus-interfaces/org.gnome.Shell.xml
@@ -40,6 +40,10 @@
       <arg name="action" type="u"/>
       <arg name="parameters" type="a{sv}"/>
     </signal>
+    <signal name="AcceleratorDeactivated">
+      <arg name="action" type="u"/>
+      <arg name="parameters" type="a{sv}"/>
+    </signal>
     <property name="Mode" type="s" access="read"/>
     <property name="OverviewActive" type="b" access="readwrite"/>
     <property name="ShellVersion" type="s" access="read"/>

commit fecd5cdd6c585dfada5681891016f0865c7881a7

shellDBus: Actually emit AcceleratorDeactivated signal
Add the missing plumbing to have this signal emitted via D-Bus.

Fixes: e53f69f9d ("shellDBus: Add AcceleratorDeactivated D-Bus signal")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3617>

js/ui/shellDBus.js
@@ -276,7 +276,7 @@ export class GnomeShell {
         invocation.return_value(null);
     }
 
-    _emitAcceleratorActivated(action, device, timestamp) {
+    _emitAcceleratorSignal(signal, action, device, timestamp) {
         let destination = this._grabbedAccelerators.get(action);
         if (!destination)
             return;
@@ -296,8 +296,18 @@ export class GnomeShell {
             destination,
             this._dbusImpl.get_object_path(),
             info?.name ?? null,
-            'AcceleratorActivated',
+            signal,
             GLib.Variant.new('(ua{sv})', [action, params]));
+    }
+
+    _emitAcceleratorActivated(action, device, timestamp) {
+        this._emitAcceleratorSignal(
+            'AcceleratorActivated', action, device, timestamp);
+    }
+
+    _emitAcceleratorDeactivated(action, device, timestamp) {
+        this._emitAcceleratorSignal(
+            'AcceleratorDeactivated', action, device, timestamp);
     }
 
     _grabAcceleratorForSender(accelerator, modeFlags, grabFlags, sender) {