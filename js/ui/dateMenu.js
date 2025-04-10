23f424878755d5836e1d0bc62eaf20bafe30ab96
Florian Müllner <fmuellner@gnome.org>
2025-04-10 20:17:16 +0200

    dateMenu: Stop indicating do-not-disturb status
    The control for enabling/disabling do-not-disturb will be moved into
    quick settings. It makes sense to then also move the indicator, so stop
    showing the do-not-disturb status in the top bar's messages indicator.
    
    Instead, just hide the indicator altogether when do-not-disturb
    is active.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3691>

--- js/ui/dateMenu.js
+++ js/ui/dateMenu.js
@@ -744,6 +744,7 @@ class MessagesIndicator extends St.Icon {
     _init() {
         super._init({
             style_class: 'messages-indicator',
+            icon_name: 'message-indicator-symbolic',
             visible: false,
             y_expand: true,
             y_align: Clutter.ActorAlign.CENTER,
@@ -792,11 +793,8 @@ class MessagesIndicator extends St.Icon {
     }
 
     _sync() {
-        let doNotDisturb = !this._settings.get_boolean('show-banners');
-        this.icon_name = doNotDisturb
-            ? 'notifications-disabled-symbolic'
-            : 'message-indicator-symbolic';
-        this.visible = doNotDisturb || this._count > 0;
+        const doNotDisturb = !this._settings.get_boolean('show-banners');
+        this.visible = !doNotDisturb && this._count > 0;
     }
 });
 
