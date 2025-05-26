1c4c900c11b3f76e7702690a197da5748fe5451f
Florian Müllner <fmuellner@gnome.org>
2025-05-26 15:27:22 +0200

    ci: Run package manager commands through shell
    The flatpak package installs a sysuser trigger, which for some reason
    messes up the permissions of /etc/passwd if buildah executes dnf
    directly (probably because it does not pick up the umask setup
    from /etc/bashrc).
    
    The result is that the file ends up being only readable by root,
    which then breaks running mutter/gnome-shell from the container
    until the permissions are fixed manually again.
    
    Avoid this issue by running all package manager commands through
    a shell (as also done in the freedesktop template).

--- .gitlab-ci/build-toolbox-image.sh
+++ .gitlab-ci/build-toolbox-image.sh
@@ -39,9 +39,9 @@ build_container() {
   local debug_packages=(
     glib2 # makes gdb much more useful
   )
-  buildah run $build_cntr dnf config-manager setopt '*-openh264.enabled=0'
-  buildah run $build_cntr dnf install -y "${extra_packages[@]}"
-  buildah run $build_cntr dnf debuginfo-install -y "${debug_packages[@]}"
+  buildah run $build_cntr sh -c "dnf config-manager setopt '*-openh264.enabled=0'"
+  buildah run $build_cntr sh -c "dnf install -y \"${extra_packages[@]}\""
+  buildah run $build_cntr sh -c "dnf debuginfo-install -y \"${debug_packages[@]}\""
   buildah run $build_cntr dnf clean all
   buildah run $build_cntr rm -rf /var/lib/cache/dnf
 
