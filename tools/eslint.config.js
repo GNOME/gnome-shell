90afcbf59826974f793fb599eb038021ee8b1927
Florian Müllner <fmuellner@gnome.org>
2025-06-10 03:11:05 +0200

    tools: Switch to flat config for eslint
    Both the .json and .yml config formats are deprecated in favor
    of flat configs exported from eslint.config.js files.
    
    The good news is that gjs' config is now available as a shared
    config from the `eslint-config-gnome` package in World/javascript,
    so we no longer have to copy the entire thing.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3746>

---
+++ tools/eslint.config.js
@@ -0,0 +1,81 @@
+// SPDX-FileCopyrightText: 2025 Florian Müllner <fmuellner@gnome.org>
+// SPDX-License-Identifier: MIT OR LGPL-2.1-or-later
+
+import {defineConfig} from '@eslint/config-helpers';
+import globals from 'globals';
+import gnome from 'eslint-config-gnome';
+
+export default defineConfig([
+    gnome.configs.recommended,
+    gnome.configs.jsdoc,
+    {
+        ignores: [
+            '_build-*',
+            'data',
+            'docs',
+            'man',
+            'src',
+            'subprojects/gvc',
+            'subprojects/jasmine-gjs',
+            'subprojects/libshew',
+        ],
+    }, {
+        rules: {
+            camelcase: ['error', {
+                properties: 'never',
+            }],
+            'consistent-return': 'error',
+            'eqeqeq': ['error', 'smart'],
+            'key-spacing': ['error', {
+                mode: 'minimum',
+                beforeColon: false,
+                afterColon: true,
+            }],
+            'prefer-arrow-callback': 'error',
+            'jsdoc/require-param-description': 'off',
+            'jsdoc/require-jsdoc': ['error', {
+                exemptEmptyFunctions: true,
+                publicOnly: {
+                    esm: true,
+                },
+            }],
+        },
+    }, {
+        files: [
+            'js/**',
+            'tests/shell/**',
+        ],
+        ignores: [
+            'js/portalHelper/*',
+            'js/extensions/*',
+        ],
+        languageOptions: {
+            globals: {
+                global: 'readonly',
+                _: 'readonly',
+                C_: 'readonly',
+                N_: 'readonly',
+                ngettext: 'readonly',
+            },
+        },
+    }, {
+        files: ['tests/unit/**'],
+        rules: {
+            'jsdoc/require-jsdoc': 'off',
+        },
+        languageOptions: {
+            globals: {
+                ...globals.jasmine,
+            },
+        },
+    }, {
+        files: ['subprojects/extensions-app/js/**'],
+        languageOptions: {
+            globals: {
+                _: 'readonly',
+                C_: 'readonly',
+                N_: 'readonly',
+            },
+        },
+    },
+]);
