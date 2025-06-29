GNOME/gnome-shell

commit 91c6366e0199eacfb62787ba669bd90fefd6c6d9

Adjust to cursor tracker visibility API changes
The old `Meta.CursorTracker.set_pointer_visible` method has been
replaced by `inhibit_cursor_visibility` and
`uninhibit_cursor_visibility`.

Change the code to use the new API which makes the code more robust
because we don't have to listen for changes and then try to force it
back to what we want.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3772>

js/ui/screenShield.js
@@ -369,11 +369,21 @@ export class ScreenShield extends Signals.EventEmitter {
     }
 
     _showPointer() {
-        this._cursorTracker.set_pointer_visible(true);
+        if (this._cursorVisibleInhibited) {
+            this._cursorTracker.uninhibit_cursor_visibility();
+            this._cursorVisibleInhibited = false;
+        }
 
         if (this._motionId) {
             global.stage.disconnect(this._motionId);
             this._motionId = 0;
+        }
+    }
+
+    _hidePointer() {
+        if (!this._cursorVisibleInhibited) {
+            this._cursorTracker.inhibit_cursor_visibility();
+            this._cursorVisibleInhibited = true;
         }
     }
 
@@ -384,7 +394,7 @@ export class ScreenShield extends Signals.EventEmitter {
 
             return Clutter.EVENT_PROPAGATE;
         });
-        this._cursorTracker.set_pointer_visible(false);
+        this._hidePointer();
     }
 
     _hideLockScreen(animate) {

js/ui/magnifier.js
@@ -137,11 +137,9 @@ export class Magnifier extends Signals.EventEmitter {
             this._cursorUnfocusInhibited = false;
         }
 
-        if (this._cursorVisibilityChangedId) {
-            this._cursorTracker.disconnect(this._cursorVisibilityChangedId);
-            delete this._cursorVisibilityChangedId;
-
-            this._cursorTracker.set_pointer_visible(true);
+        if (this._cursorVisibleInhibited) {
+            this._cursorTracker.uninhibit_cursor_visibility();
+            this._cursorVisibleInhibited = false;
         }
     }
 
@@ -155,12 +153,9 @@ export class Magnifier extends Signals.EventEmitter {
             this._cursorUnfocusInhibited = true;
         }
 
-        if (!this._cursorVisibilityChangedId) {
-            this._cursorTracker.set_pointer_visible(false);
-            this._cursorVisibilityChangedId = this._cursorTracker.connect('visibility-changed', () => {
-                if (this._cursorTracker.get_pointer_visible())
-                    this._cursorTracker.set_pointer_visible(false);
-            });
+        if (!this._cursorVisibleInhibited) {
+            this._cursorTracker.inhibit_cursor_visibility();
+            this._cursorVisibleInhibited = true;
         }
     }
 

commit 7b804eda9c2a62de9dcaf02b64186bac42877656

screenShield: Fix pointer showing on motion
As an additional side effect of switching to the cursor visibility
inhibition API in the cursor tracker, there is no longer this
infighting in Mutter where pointer motion tries to set pointer
visibility on (since it's the last device used), and other places
wanting to inhibit it, such as the screenshield.

This resulted on the cursor wanting to show by itself, and somehow
trigger captured motion events on the ClutterStage. This no longer
happens, which results in the pointer staying hidden, seat unfocus
remaining uninhibited, and no motion events being sent in (since
the pointer device now does remain unfocused).

Fix this by:
- Setting a seat unfocus inhibition, so the pointer triggers motion
  and focus despite being hidden
- Connecting to captured events on the screen lock group widget,
  since captured events will respect the existing grab

This results in pointer motion again resulting on the cursor being
shown, when waking up the lockscreen.

Fixes: 91c6366e0 ("Adjust to cursor tracker visibility API changes")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3781>

js/ui/screenShield.js
@@ -370,25 +370,30 @@ export class ScreenShield extends Signals.EventEmitter {
 
     _showPointer() {
         if (this._cursorVisibleInhibited) {
+            const seat = global.stage.context.get_backend().get_default_seat();
             this._cursorTracker.uninhibit_cursor_visibility();
+            seat.uninhibit_unfocus();
             this._cursorVisibleInhibited = false;
         }
 
         if (this._motionId) {
-            global.stage.disconnect(this._motionId);
+            this._lockDialogGroup.disconnect(this._motionId);
             this._motionId = 0;
         }
     }
 
     _hidePointer() {
         if (!this._cursorVisibleInhibited) {
+            const seat = global.stage.context.get_backend().get_default_seat();
+            seat.inhibit_unfocus();
             this._cursorTracker.inhibit_cursor_visibility();
             this._cursorVisibleInhibited = true;
         }
     }
 
     _hidePointerUntilMotion() {
-        this._motionId = global.stage.connect('captured-event', (stage, event) => {
+        const eventActor = this._lockDialogGroup;
+        this._motionId = eventActor.connect('captured-event', (_, event) => {
             if (event.type() === Clutter.EventType.MOTION)
                 this._showPointer();
 