Rays-Robotics/project-lumos-DE

wip! gdm: Start to add switchable auth smartcard login support
This still needs to handle smartcard insertion events at the user
list better, and also provide the certificate list dialog, mocked
up here:

https://gitlab.gnome.org/Teams/Design/os-mockups/-/blob/master/lock-login/login-unlock.png

js/gdm/util.js
@@ -53,6 +53,7 @@ const AUTH_SELECTION_COMPLETION_STATUS = 'Ok';
 
 const SWITCHABLE_AUTH_SUPPORTED_ROLES = [
     PASSWORD_ROLE_NAME,
+    SMARTCARD_ROLE_NAME,
     WEB_LOGIN_ROLE_NAME,
 ];
 
@@ -299,6 +300,8 @@ export class ShellUserVerifier extends Signals.EventEmitter {
             await this._handlePendingMessages();
             if (this._pendingMechanisms.has(PASSWORD_ROLE_NAME))
                 this._replyWithAuthSelectionResponse(serviceName, PASSWORD_ROLE_NAME, {password: answer});
+            else if (this._pendingMechanisms.has(SMARTCARD_ROLE_NAME))
+                this._replyWithAuthSelectionResponse(serviceName, SMARTCARD_ROLE_NAME, {pin: answer});
             else
                 this._userVerifier.call_answer_query(serviceName, answer, this._cancellable, null);
         } catch (e) {
@@ -554,10 +557,19 @@ export class ShellUserVerifier extends Signals.EventEmitter {
         if (smartcardDetected !== this.smartcardDetected) {
             this.smartcardDetected = smartcardDetected;
 
-            if (this.smartcardDetected)
-                this.setForegroundService(SMARTCARD_SERVICE_NAME);
-            else if (this._preemptingService === SMARTCARD_SERVICE_NAME)
-                this.setForegroundService(null);
+            const smartcardService = SWITCHABLE_AUTH_SUPPORTED_ROLES.includes(SMARTCARD_ROLE_NAME)?
+                SWITCHABLE_AUTH_SERVICE_NAME: SMARTCARD_SERVICE_NAME;
+
+            // FIXME: It doesn't seem like pam_sss sends JSON if no username
+            // is specified. Need to fix that before we can act on smartcard
+            // insertion events with switchable auth. See also this message in
+            // gdm/authPrompt.js
+            if (smartcardService !== SWITCHABLE_AUTH_SERVICE_NAME) {
+                if (this.smartcardDetected)
+                    this.setForegroundService(smartcardService);
+                else if (this._preemptingService === smartcardService)
+                    this.setForegroundService(null);
+            }
 
             this.emit('smartcard-status-changed');
         }
@@ -728,7 +740,16 @@ export class ShellUserVerifier extends Signals.EventEmitter {
                 return true;
         }
 
-        return this.serviceIsForeground(SMARTCARD_SERVICE_NAME);
+        const foregroundMechanism = this.getForegroundMechanism();
+        if (foregroundMechanism?.role === SMARTCARD_ROLE_NAME)
+            return true;
+
+        const smartcardService =
+            SWITCHABLE_AUTH_SUPPORTED_ROLES.includes(SMARTCARD_ROLE_NAME) &&
+            this.smartcardDetected ? SWITCHABLE_AUTH_SERVICE_NAME:
+            SMARTCARD_SERVICE_NAME;
+
+        return this.serviceIsForeground(smartcardService);
     }
 
     serviceIsDefault(serviceName) {
@@ -858,6 +879,7 @@ export class ShellUserVerifier extends Signals.EventEmitter {
         const roleHandlers = {
             [WEB_LOGIN_ROLE_NAME]: this._startWebLogin,
             [PASSWORD_ROLE_NAME]: this._startPasswordLogin,
+            [SMARTCARD_ROLE_NAME]: this._startSmartcardLogin,
         };
 
         const handler = roleHandlers[mechanism.role];
@@ -943,6 +965,21 @@ export class ShellUserVerifier extends Signals.EventEmitter {
 
         this._pendingMechanisms.set(role, mechanismId);
         this.emit('ask-question', serviceName, prompt, true);
+    }
+
+    _startSmartcardLogin(serviceName, mechanismId) {
+        const mechanisms = this._publishedMechanisms.get(serviceName);
+
+        if (!mechanisms)
+            return;
+
+        if (!mechanisms[mechanismId])
+            return;
+
+        const {pin_prompt, init_instruction, role} = mechanisms[mechanismId];
+
+        this._pendingMechanisms.set(role, mechanismId);
+        this.emit('ask-question', serviceName, pin_prompt, true);
     }
 
     async _replyWithAuthSelectionResponse(serviceName, role, response) {

GNOME/gnome-shell

js/gdm/util.js
@@ -667,6 +667,13 @@ export class ShellUserVerifier extends Signals.EventEmitter {
             'smartcard-removed', () => this._checkForSmartcard(), this);
     }
 
+    getSmartcardServiceName() {
+        if (this._getSwitchableServiceForRole(Const.SMARTCARD_ROLE_NAME))
+            return SWITCHABLE_AUTH_SERVICE_NAME;
+
+        return SMARTCARD_SERVICE_NAME;
+    }
+
     _checkForSmartcard() {
         let smartcardDetected;
 
@@ -680,10 +687,18 @@ export class ShellUserVerifier extends Signals.EventEmitter {
         if (smartcardDetected !== this.smartcardDetected) {
             this.smartcardDetected = smartcardDetected;
 
-            if (this.smartcardDetected)
-                this.setForegroundService(SMARTCARD_SERVICE_NAME);
-            else if (this._preemptingService === SMARTCARD_SERVICE_NAME)
-                this.setForegroundService(null);
+            const smartcardService = this.getSmartcardServiceName();
+
+            // FIXME: It doesn't seem like pam_sss sends JSON if no username
+            // is specified. Need to fix that before we can act on smartcard
+            // insertion events with switchable auth. See also this message in
+            // gdm/authPrompt.js
+            if (smartcardService !== SWITCHABLE_AUTH_SERVICE_NAME) {
+                if (this.smartcardDetected)
+                    this.setForegroundService(smartcardService);
+                else if (this._preemptingService === smartcardService)
+                    this.setForegroundService(null);
+            }
 
             this.emit('smartcard-status-changed');
         }
@@ -867,7 +882,14 @@ export class ShellUserVerifier extends Signals.EventEmitter {
                 return true;
         }
 
-        return this.serviceIsForeground(SMARTCARD_SERVICE_NAME);
+        const foregroundMechanism = this.getForegroundMechanism();
+        if (foregroundMechanism?.role === Const.SMARTCARD_ROLE_NAME)
+            return true;
+
+        if (!this.smartcardDetected)
+            return this.serviceIsForeground(SMARTCARD_SERVICE_NAME);
+
+        return this.serviceIsForeground(this.getSmartcardServiceName());
     }
 
     serviceIsDefault(serviceName) {