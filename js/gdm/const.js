188ce5363262bedb4dfaefe896a45d136f0aa3c6
Joan Torres Lopez <joantolo@redhat.com>
2025-08-18 12:30:50 +0200

    gdm: Extract authentication service and role constants to const.js
    Create a dedicated const.js module to centralize GDM authentication-related
    constants that are shared across multiple files.
    
    Also, in utils, add isSelectable() and getIconName() to get mechanisms metadata.
    
    This prepares the codebase for upcoming commits that will use these
    constants.

---
+++ js/gdm/const.js
@@ -0,0 +1,10 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
+
+export const PASSWORD_ROLE_NAME = 'password';
+export const SMARTCARD_ROLE_NAME = 'smartcard';
+export const FINGERPRINT_ROLE_NAME = 'fingerprint';
+
+export const PASSWORD_SERVICE_NAME = 'gdm-password';
+export const SMARTCARD_SERVICE_NAME = 'gdm-smartcard';
+export const FINGERPRINT_SERVICE_NAME = 'gdm-fingerprint';
+export const SWITCHABLE_AUTH_SERVICE_NAME = 'gdm-switchable-auth';
