GNOME/gnome-shell

commit 71accdd97834d14bc369c0d09f89796116f81af3

st: Adapt to CoglObject removal
Also use more memory management macro helpers

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2964>

src/st/st-private.c
@@ -420,7 +420,7 @@ _st_create_shadow_pipeline (StShadow    *shadow_spec,
   fb = COGL_FRAMEBUFFER (offscreen);
   if (!cogl_framebuffer_allocate (fb, &error))
     {
-      cogl_clear_object (&texture);
+      g_clear_object (&texture);
       return NULL;
     }
 
@@ -450,7 +450,7 @@ _st_create_shadow_pipeline (StShadow    *shadow_spec,
 
       texture_pipeline = cogl_pipeline_new (ctx);
       cogl_pipeline_add_snippet (texture_pipeline, snippet);
-      cogl_object_unref (snippet);
+      g_object_unref (snippet);
 
       cogl_context_set_named_pipeline (ctx,
                                        &texture_pipeline_key,
@@ -493,7 +493,7 @@ _st_create_shadow_pipeline (StShadow    *shadow_spec,
   pipeline = cogl_pipeline_copy (shadow_pipeline_template);
   cogl_pipeline_set_layer_texture (pipeline, 0, texture);
 
-  cogl_clear_object (&texture);
+  g_clear_object (&texture);
 
   return pipeline;
 }
@@ -535,7 +535,7 @@ _st_create_shadow_pipeline_from_actor (StShadow     *shadow_spec,
 
   if (shadow_pipeline == NULL)
     {
-      CoglTexture *buffer;
+      g_autoptr(CoglTexture) buffer = NULL;
       CoglOffscreen *offscreen;
       CoglFramebuffer *fb;
       CoglContext *ctx;
@@ -556,7 +556,6 @@ _st_create_shadow_pipeline_from_actor (StShadow     *shadow_spec,
         {
           g_error_free (catch_error);
           g_object_unref (offscreen);
-          cogl_object_unref (buffer);
           return NULL;
         }
 
@@ -582,10 +581,9 @@ _st_create_shadow_pipeline_from_actor (StShadow     *shadow_spec,
 
       g_object_unref (fb);
 
-      shadow_pipeline = _st_create_shadow_pipeline (shadow_spec, buffer,
+      shadow_pipeline = _st_create_shadow_pipeline (shadow_spec,
+                                                    g_steal_pointer (&buffer),
                                                     resource_scale);
-
-      cogl_object_unref (buffer);
     }
 
   return shadow_pipeline;

src/shell-blur-effect.c
@@ -138,7 +138,7 @@ create_brightness_pipeline (void)
                                   brightness_glsl_declarations,
                                   brightness_glsl);
       cogl_pipeline_add_snippet (brightness_pipeline, snippet);
-      cogl_object_unref (snippet);
+      g_object_unref (snippet);
     }
 
   return cogl_pipeline_copy (brightness_pipeline);
@@ -188,7 +188,7 @@ update_fbo (FramebufferData *data,
   CoglContext *ctx =
     clutter_backend_get_cogl_context (clutter_get_default_backend ());
 
-  g_clear_pointer (&data->texture, cogl_object_unref);
+  g_clear_object (&data->texture);
   g_clear_object (&data->framebuffer);
 
   float new_width = floorf (width / downscale_factor);
@@ -269,7 +269,7 @@ update_background_fbo (ShellBlurEffect *self,
 static void
 clear_framebuffer_data (FramebufferData *fb_data)
 {
-  g_clear_pointer (&fb_data->texture, cogl_object_unref);
+  g_clear_object (&fb_data->texture);
   g_clear_object (&fb_data->framebuffer);
 }
 
@@ -697,9 +697,9 @@ shell_blur_effect_finalize (GObject *object)
   clear_framebuffer_data (&self->background_fb);
   clear_framebuffer_data (&self->brightness_fb);
 
-  g_clear_pointer (&self->actor_fb.pipeline, cogl_object_unref);
-  g_clear_pointer (&self->background_fb.pipeline, cogl_object_unref);
-  g_clear_pointer (&self->brightness_fb.pipeline, cogl_object_unref);
+  g_clear_object (&self->actor_fb.pipeline);
+  g_clear_object (&self->background_fb.pipeline);
+  g_clear_object (&self->brightness_fb.pipeline);
 
   G_OBJECT_CLASS (shell_blur_effect_parent_class)->finalize (object);
 }

src/shell-glsl-effect.c
@@ -31,7 +31,7 @@ shell_glsl_effect_create_pipeline (ClutterOffscreenEffect *effect,
 
   cogl_pipeline_set_layer_texture (priv->pipeline, 0, texture);
 
-  return cogl_object_ref (priv->pipeline);
+  return g_object_ref (priv->pipeline);
 }
 
 /**
@@ -76,7 +76,7 @@ shell_glsl_effect_add_glsl_snippet (ShellGLSLEffect  *effect,
   else
     cogl_pipeline_add_layer_snippet (klass->base_pipeline, 0, snippet);
 
-  cogl_object_unref (snippet);
+  g_object_unref (snippet);
 }
 
 static void
@@ -87,7 +87,7 @@ shell_glsl_effect_dispose (GObject *gobject)
 
   priv = shell_glsl_effect_get_instance_private (self);
 
-  g_clear_pointer (&priv->pipeline, cogl_object_unref);
+  g_clear_object (&priv->pipeline);
 
   G_OBJECT_CLASS (shell_glsl_effect_parent_class)->dispose (gobject);
 }

src/shell-invert-lightness-effect.c
@@ -80,7 +80,7 @@ shell_glsl_effect_create_pipeline (ClutterOffscreenEffect *effect,
 
   cogl_pipeline_set_layer_texture (self->pipeline, 0, texture);
 
-  return cogl_object_ref (self->pipeline);
+  return g_object_ref (self->pipeline);
 }
 
 static void
@@ -88,11 +88,7 @@ shell_invert_lightness_effect_dispose (GObject *gobject)
 {
   ShellInvertLightnessEffect *self = SHELL_INVERT_LIGHTNESS_EFFECT (gobject);
 
-  if (self->pipeline != NULL)
-    {
-      cogl_object_unref (self->pipeline);
-      self->pipeline = NULL;
-    }
+  g_clear_object (&self->pipeline);
 
   G_OBJECT_CLASS (shell_invert_lightness_effect_parent_class)->dispose (gobject);
 }
@@ -128,7 +124,7 @@ shell_invert_lightness_effect_init (ShellInvertLightnessEffect *self)
                                   NULL);
       cogl_snippet_set_replace (snippet, invert_lightness_source);
       cogl_pipeline_add_layer_snippet (klass->base_pipeline, 0, snippet);
-      cogl_object_unref (snippet);
+      g_object_unref (snippet);
 
       cogl_pipeline_set_layer_null_texture (klass->base_pipeline, 0);
     }

src/shell-screenshot.c
@@ -525,7 +525,7 @@ grab_screenshot_content (ShellScreenshot *screenshot,
       unsigned int width, height;
       CoglContext *ctx;
       CoglPipeline *pipeline;
-      CoglTexture2D *texture;
+      CoglTexture *texture;
       CoglOffscreen *offscreen;
       ClutterStageView *view;
 
@@ -548,12 +548,12 @@ grab_screenshot_content (ShellScreenshot *screenshot,
                                                 pipeline,
                                                 -1, 1, 1, -1,
                                                 0, 0, 1, 1);
-      cogl_object_unref (pipeline);
+      g_object_unref (pipeline);
       g_object_unref (offscreen);
 
       priv->cursor_content =
         clutter_texture_content_new_from_texture (texture, NULL);
-      cogl_object_unref (texture);
+      g_object_unref (texture);
 
       priv->cursor_scale = meta_cursor_tracker_get_scale (tracker);
 
@@ -1313,7 +1313,7 @@ shell_screenshot_composite_to_stream (CoglTexture         *texture,
                          cairo_image_surface_get_data (surface));
   cairo_surface_mark_dirty (surface);
 
-  cogl_object_unref (sub_texture);
+  g_object_unref (sub_texture);
 
   cairo_surface_set_device_scale (surface, scale, scale);
 

src/st/st-entry.c
@@ -216,7 +216,7 @@ st_entry_dispose (GObject *object)
   StEntry *entry = ST_ENTRY (object);
   StEntryPrivate *priv = ST_ENTRY_PRIV (entry);
 
-  cogl_clear_object (&priv->text_shadow_material);
+  g_clear_object (&priv->text_shadow_material);
 
   G_OBJECT_CLASS (st_entry_parent_class)->dispose (object);
 }
@@ -254,7 +254,7 @@ st_entry_style_changed (StWidget *self)
   if (!priv->shadow_spec || !shadow_spec ||
       !st_shadow_equal (shadow_spec, priv->shadow_spec))
     {
-      g_clear_pointer (&priv->text_shadow_material, cogl_object_unref);
+      g_clear_object (&priv->text_shadow_material);
 
       g_clear_pointer (&priv->shadow_spec, st_shadow_unref);
       if (shadow_spec)
@@ -555,7 +555,7 @@ clutter_text_cursor_changed (ClutterText *text,
 
   st_entry_update_hint_visibility (entry);
 
-  g_clear_pointer (&priv->text_shadow_material, cogl_object_unref);
+  g_clear_object (&priv->text_shadow_material);
 }
 
 static void
@@ -569,7 +569,7 @@ clutter_text_changed_cb (GObject    *object,
   st_entry_update_hint_visibility (entry);
 
   /* Since the text changed, force a regen of the shadow texture */
-  cogl_clear_object (&priv->text_shadow_material);
+  g_clear_object (&priv->text_shadow_material);
 
   g_object_notify_by_pspec (G_OBJECT (entry), props[PROP_TEXT]);
 }
@@ -581,7 +581,7 @@ invalidate_shadow_pipeline (GObject    *object,
 {
   StEntryPrivate *priv = ST_ENTRY_PRIV (entry);
 
-  g_clear_pointer (&priv->text_shadow_material, cogl_object_unref);
+  g_clear_object (&priv->text_shadow_material);
 }
 
 static void
@@ -839,7 +839,7 @@ st_entry_paint (ClutterActor        *actor,
         {
           CoglPipeline *material;
 
-          cogl_clear_object (&priv->text_shadow_material);
+          g_clear_object (&priv->text_shadow_material);
 
           material = _st_create_shadow_pipeline_from_actor (priv->shadow_spec,
                                                             priv->entry);

src/st/st-icon.c
@@ -173,7 +173,7 @@ st_icon_dispose (GObject *gobject)
   g_clear_object (&priv->gicon);
   g_clear_object (&priv->fallback_gicon);
   g_clear_pointer (&priv->colors, st_icon_colors_unref);
-  g_clear_pointer (&priv->shadow_pipeline, cogl_object_unref);
+  g_clear_object (&priv->shadow_pipeline);
   g_clear_pointer (&priv->shadow_spec, st_shadow_unref);
 
   G_OBJECT_CLASS (st_icon_parent_class)->dispose (gobject);
@@ -377,7 +377,7 @@ st_icon_clear_shadow_pipeline (StIcon *icon)
 {
   StIconPrivate *priv = icon->priv;
 
-  g_clear_pointer (&priv->shadow_pipeline, cogl_object_unref);
+  g_clear_object (&priv->shadow_pipeline);
   graphene_size_init (&priv->shadow_size, 0, 0);
 }
 

src/st/st-label.c
@@ -130,7 +130,7 @@ st_label_style_changed (StWidget *self)
   if (!priv->shadow_spec || !shadow_spec ||
       !st_shadow_equal (shadow_spec, priv->shadow_spec))
     {
-      g_clear_pointer (&priv->text_shadow_pipeline, cogl_object_unref);
+      g_clear_object (&priv->text_shadow_pipeline);
 
       g_clear_pointer (&priv->shadow_spec, st_shadow_unref);
       if (shadow_spec)
@@ -199,7 +199,7 @@ st_label_dispose (GObject   *object)
   StLabelPrivate *priv = ST_LABEL (object)->priv;
 
   priv->label = NULL;
-  g_clear_pointer (&priv->text_shadow_pipeline, cogl_object_unref);
+  g_clear_object (&priv->text_shadow_pipeline);
 
   G_OBJECT_CLASS (st_label_parent_class)->dispose (object);
 }
@@ -230,7 +230,7 @@ st_label_paint (ClutterActor        *actor,
           width != priv->shadow_width ||
           height != priv->shadow_height)
         {
-          g_clear_pointer (&priv->text_shadow_pipeline, cogl_object_unref);
+          g_clear_object (&priv->text_shadow_pipeline);
 
           priv->shadow_width = width;
           priv->shadow_height = height;
@@ -261,7 +261,7 @@ st_label_resource_scale_changed (ClutterActor *actor)
 {
   StLabelPrivate *priv = ST_LABEL (actor)->priv;
 
-  g_clear_pointer (&priv->text_shadow_pipeline, cogl_object_unref);
+  g_clear_object (&priv->text_shadow_pipeline);
 
   if (CLUTTER_ACTOR_CLASS (st_label_parent_class)->resource_scale_changed)
     CLUTTER_ACTOR_CLASS (st_label_parent_class)->resource_scale_changed (actor);
@@ -321,7 +321,7 @@ invalidate_shadow_pipeline (GObject    *object,
 {
   StLabelPrivate *priv = st_label_get_instance_private (label);
 
-  g_clear_pointer (&priv->text_shadow_pipeline, cogl_object_unref);
+  g_clear_object (&priv->text_shadow_pipeline);
 }
 
 static void
@@ -416,7 +416,7 @@ st_label_set_text (StLabel     *label,
   if (clutter_text_get_editable (ctext) ||
       g_strcmp0 (clutter_text_get_text (ctext), text) != 0)
     {
-      g_clear_pointer (&priv->text_shadow_pipeline, cogl_object_unref);
+      g_clear_object (&priv->text_shadow_pipeline);
 
       clutter_text_set_text (ctext, text);
 

commit 6f9483c434c08e5dbaeec919cddbcb050ab4d2da

st/private: Fix texture leak when creating shadow from actor
cogl_pipeline_set_layer_texture() adds its own ref to the texture in
_cogl_pipeline_set_layer_texture_data() when called via
_st_create_shadow_pipeline(), so the initial reference is leaked when
the pointer is stolen.

Fix this by not stealing the pointer, so autoptr can unref the texture.
Before the code was changed to autoptr in 71accdd97 the texture was also
manually unrefed after calling _st_create_shadow_pipeline().

Fixes: 71accdd97 ("st: Adapt to CoglObject removal")
Closes: https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/8509
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3854>

src/st/st-private.c
@@ -604,7 +604,7 @@ _st_create_shadow_pipeline_from_actor (StShadow            *shadow_spec,
 
       shadow_pipeline = _st_create_shadow_pipeline (shadow_spec,
                                                     paint_context,
-                                                    g_steal_pointer (&buffer),
+                                                    buffer,
                                                     resource_scale);
     }
 