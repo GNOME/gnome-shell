GNOME/gnome-shell

commit b7992b807ccedacb60a7f1cb35dcc7c13b3c3f6d

ci: Include latest malcontent in the toolbox
Currently the toolbox lacks malcontent >= 0.14.0, because that version
has not been released yet. However, we need it to test the upcoming
parental controls integration, so build it in the toolbox.

Related: https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3841
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3870>

.gitlab-ci/build-toolbox-image.sh
@@ -45,6 +45,9 @@ build_container() {
   buildah run $build_cntr dnf clean all
   buildah run $build_cntr rm -rf /var/lib/cache/dnf
 
+  # add latest malcontent to the toolbox image for testing future integration
+  buildah run $build_cntr .gitlab-ci/install-meson-project.sh https://gitlab.freedesktop.org/pwithnall/malcontent.git main
+
   # somehow the sysusers trigger from the flatpak package messes up the
   # permissions of /etc/passwd to be only readable by root
   buildah run $build_cntr chmod 644 /etc/passwd

commit 851cb6a201b7d06c95c8d31a70f786fda44b1de7

ci: Fix building toolbox with latest malcontent
While introducing latest malcontent in the toolbox, mistakenly wrong
path to install-meson-project.sh was used. Correct this by using the
proper path, after copying the script there.

Moreover, disable building documentation in malcontent subproject
libgsystemservice, so that we don't have to install more dependencies.

Fixes: b7992b80 ("ci: Include latest malcontent in the toolbox")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3873>

.gitlab-ci/build-toolbox-image.sh
@@ -41,12 +41,10 @@ build_container() {
   )
   buildah run $build_cntr dnf config-manager setopt '*-openh264.enabled=0'
   buildah run $build_cntr dnf install -y "${extra_packages[@]}"
+  buildah run $build_cntr dnf builddep malcontent -y # for building libmalcontent
   buildah run $build_cntr dnf debuginfo-install -y "${debug_packages[@]}"
   buildah run $build_cntr dnf clean all
   buildah run $build_cntr rm -rf /var/lib/cache/dnf
-
-  # add latest malcontent to the toolbox image for testing future integration
-  buildah run $build_cntr .gitlab-ci/install-meson-project.sh https://gitlab.freedesktop.org/pwithnall/malcontent.git main
 
   # somehow the sysusers trigger from the flatpak package messes up the
   # permissions of /etc/passwd to be only readable by root
@@ -60,6 +58,9 @@ build_container() {
 
   local srcdir=$(realpath $(dirname $0))
   buildah copy --chmod 755 $build_cntr $srcdir/install-meson-project.sh /usr/libexec
+
+  # add latest malcontent to the toolbox image for testing future integration
+  buildah run $build_cntr /usr/libexec/install-meson-project.sh https://gitlab.freedesktop.org/pwithnall/malcontent.git main -Dlibgsystemservice:gtk_doc=false
 
   # include convenience script for updating mutter dependency
   local update_mutter=$(mktemp)