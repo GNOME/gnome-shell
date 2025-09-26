GNOME/gnome-shell

commit bb73bf548ca7ebc607bcb2f9e89aaa5786233f93

FdoNotificationDaemon: Emit ActivationToken before ActionInvoked signal
Two years ago FDO notifications spec gained the ability to pass an
activation token (often called startup id) let's finally implement it.

See: https://gitlab.freedesktop.org/xdg/xdg-specs/-/commit/b9a470004d

Closes: https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/358
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3199>

js/ui/notificationDaemon.js
@@ -217,6 +217,7 @@ class FdoNotificationDaemon {
                     hasDefaultAction = true;
                 } else {
                     notification.addAction(label, () => {
+                        this._emitActivationToken(source, ndata.id);
                         this._emitActionInvoked(ndata.id, actionId);
                     });
                 }
@@ -225,6 +226,7 @@ class FdoNotificationDaemon {
 
         if (hasDefaultAction) {
             notification.connect('activated', () => {
+                this._emitActivationToken(source, ndata.id);
                 this._emitActionInvoked(ndata.id, 'default');
             });
         } else {
@@ -300,6 +302,16 @@ class FdoNotificationDaemon {
     _emitActionInvoked(id, action) {
         this._dbusImpl.emit_signal('ActionInvoked',
             GLib.Variant.new('(us)', [id, action]));
+    }
+
+    _emitActivationToken(source, id) {
+        const context = global.create_app_launch_context(0, -1);
+        const info = source.app?.get_app_info();
+        if (info) {
+            const token = context.get_startup_notify_id(info, []);
+            this._dbusImpl.emit_signal('ActivationToken',
+                GLib.Variant.new('(us)', [id, token]));
+        }
     }
 }
 

js/dbusServices/notifications/notificationDaemon.js
@@ -27,6 +27,14 @@ export const NotificationDaemon = class extends ServiceImplementation {
                     log(error.message);
             });
 
+        this._proxy.connectSignal('ActivationToken',
+            (proxy, sender, params) => {
+                const [id] = params;
+                this._emitSignal(
+                    this._activeNotifications.get(id),
+                    'ActivationToken',
+                    new GLib.Variant('(us)', params));
+            });
         this._proxy.connectSignal('ActionInvoked',
             (proxy, sender, params) => {
                 const [id] = params;

data/dbus-interfaces/org.freedesktop.Notifications.xml
@@ -31,5 +31,9 @@
       <arg type="u"/>
       <arg type="s"/>
     </signal>
+    <signal name="ActivationToken">
+      <arg type="u"/>
+      <arg type="s"/>
+    </signal>
   </interface>
 </node>

commit f85ee952a02245e59fb9e1da63a1c4586dc9372b

notificationDamon: Always send the activation token
In case of FDO notifications, we may receive a notification coming from
non-desktop-applications (e.g. gsd does that), but in any case when
activating an action we should always provide back the activation-token.

So send the startup notification ID in any case, even if the appId is
unset (in fact get_startup_notify_id() uses a nullable parameter for a
reason since GNOME/glib@6210708f).

In wayland this more than enough, while in X11 we could have used the
gnome-shell desktop file as fallback, but that's gone for good now!

Fixes: bb73bf5
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3891>

js/ui/notificationDaemon.js
@@ -304,12 +304,10 @@ class FdoNotificationDaemon {
 
     _emitActivationToken(source, id) {
         const context = global.create_app_launch_context(0, -1);
-        const info = source.app?.get_app_info();
-        if (info) {
-            const token = context.get_startup_notify_id(info, []);
-            this._dbusImpl.emit_signal('ActivationToken',
-                GLib.Variant.new('(us)', [id, token]));
-        }
+        const appInfo = source.app?.get_app_info() ?? null;
+        const token = context.get_startup_notify_id(appInfo, []);
+        this._dbusImpl.emit_signal('ActivationToken',
+            GLib.Variant.new('(us)', [id, token]));
     }
 }
 