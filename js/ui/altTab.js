GNOME/gnome-shell

commit 314137c8c97237753675e8195980c8c56f434499

switcherPopup: Move to a ClutterGesture for closing the popup
When the user presses a mouse button outside of the switcherPopup, the
popup should close. This broke when we moved StButton to ClutterGesture,
because the button_press_event() handler in switcherPopup will close the
popup before the click gesture recognizes.

Since we're trying to move away from direct event handling anyway, port
this functionality to a LongPressGesture with a duration of 0, so that
the popup closes immediately on button press as it used to.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2853>

js/ui/switcherPopup.js
@@ -68,6 +68,22 @@ export const SwitcherPopup = GObject.registerClass({
             coordinate: Clutter.BindCoordinate.ALL,
         }));
 
+        const longPressGesture = new Clutter.LongPressGesture({
+            long_press_duration_ms: 0,
+        });
+        longPressGesture.connect('may-recognize', () => {
+            const coords = longPressGesture.get_coords_abs();
+            const actorAtCoords =
+                global.stage.get_actor_at_pos(Clutter.PickMode.REACTIVE, coords.x, coords.y);
+
+            return !this._switcherList?.contains(actorAtCoords);
+        });
+        longPressGesture.connect('recognize', () => {
+            // User clicked outside
+            this.fadeAndDestroy();
+        });
+        this.add_action(longPressGesture);
+
         // Initially disable hover so we ignore the enter-event if
         // the switcher appears underneath the current pointer location
         this._disableHover();
@@ -221,12 +237,6 @@ export const SwitcherPopup = GObject.registerClass({
         }
 
         return Clutter.EVENT_STOP;
-    }
-
-    vfunc_button_press_event() {
-        /* We clicked outside */
-        this.fadeAndDestroy();
-        return Clutter.EVENT_PROPAGATE;
     }
 
     _scrollHandler(direction) {

commit 811364ae59340195aa1c9c3edcc0d784d39105c2

switcherPopup: Fix switch-group mouse click regression
The LongPressGesture 'may-recognize' callback in commit 314137c8c was
checking if clicks were inside '_switcherList', but thumbnails shown by
switch-group are siblings of _switcherList, not children.

This caused the gesture to incorrectly identify thumbnail clicks as
'outside' clicks, closing the popup before the click could complete.

Fix by adding a _isActorOutside() helper method that subclasses can
override to check additional containers. AppSwitcherPopup now properly
checks both _switcherList and _thumbnails.

Tested on Ubuntu 25.10 with GNOME Shell 49.0. Confirmed:
- switch-group mouse clicks now work
- switch-applications and switch-windows unaffected
- clicking outside the popup still closes it correctly
- no regressions introduced

Fixes: 314137c8c9 ("switcherPopup: Move to a ClutterGesture for closing the popup")
Closes: https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/8812
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3959>

js/ui/altTab.js
@@ -294,6 +294,11 @@ class AppSwitcherPopup extends SwitcherPopup.SwitcherPopup {
         super._onDestroy();
     }
 
+    _isActorOutside(actor) {
+        return super._isActorOutside(actor) &&
+               !this._thumbnails?.contains(actor);
+    }
+
     /**
      * _select:
      *

js/ui/switcherPopup.js
@@ -76,7 +76,7 @@ export const SwitcherPopup = GObject.registerClass({
             const actorAtCoords =
                 global.stage.get_actor_at_pos(Clutter.PickMode.REACTIVE, coords.x, coords.y);
 
-            return !this._switcherList?.contains(actorAtCoords);
+            return this._isActorOutside(actorAtCoords);
         });
         longPressGesture.connect('recognize', () => {
             // User clicked outside
@@ -307,6 +307,10 @@ export const SwitcherPopup = GObject.registerClass({
         this._motionTimeoutId = 0;
         this.mouseActive = true;
         return GLib.SOURCE_REMOVE;
+    }
+
+    _isActorOutside(actor) {
+        return !this._switcherList?.contains(actor);
     }
 
     _resetNoModsTimeout() {