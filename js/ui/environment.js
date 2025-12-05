422d6d85525ef638e20843ebe8b8e6afdade77df
Marco Trevisan (Treviño) <mail@3v1n0.net>
2025-12-05 23:39:23 +0100

    environment: Split out the prepare and cleanup animation code
    We do the same twice, and it's not trivial code anymore, so let's
    separate this into another function which returns the prepare and cleanup
    functions
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/3992>
    (cherry picked from commit 7aacc1cab033fd77370b8d03b037892b3dd28a7f)
    (cherry picked from commit f7cfc7ff4a67a32363e57474bef4f95ecea3e0c3)

--- js/ui/environment.js
+++ js/ui/environment.js
@@ -70,6 +70,19 @@ function _makeEaseCallback(params, cleanup) {
     };
 }
 
+function _makeEasePrepareAndCleanup() {
+    const prepare = () => {
+        global.compositor.disable_unredirect();
+        global.begin_work();
+    };
+    const cleanup = () => {
+        global.compositor.enable_unredirect();
+        global.end_work();
+    };
+
+    return {prepare, cleanup};
+}
+
 function _getPropertyTarget(actor, propName) {
     if (!propName.startsWith('@'))
         return [actor, propName];
@@ -127,15 +140,8 @@ function _easeActor(actor, params) {
         actor.set_easing_mode(params.mode);
     delete params.mode;
 
-    const prepare = () => {
-        global.compositor.disable_unredirect();
-        global.begin_work();
-    };
-    const cleanup = () => {
-        global.compositor.enable_unredirect();
-        global.end_work();
-    };
-    let callback = _makeEaseCallback(params, cleanup);
+    const {prepare, cleanup} = _makeEasePrepareAndCleanup();
+    const callback = _makeEaseCallback(params, cleanup);
 
     // cancel overwritten transitions
     let animatedProps = Object.keys(params).map(p => p.replace('_', '-', 'g'));
@@ -208,15 +214,8 @@ function _easeAnimatableProperty(animatable, propName, target, params) {
     if (!actor?.mapped)
         duration = 0;
 
-    const prepare = () => {
-        global.compositor.disable_unredirect();
-        global.begin_work();
-    };
-    const cleanup = () => {
-        global.compositor.enable_unredirect();
-        global.end_work();
-    };
-    let callback = _makeEaseCallback(params, cleanup);
+    const {prepare, cleanup} = _makeEasePrepareAndCleanup();
+    const callback = _makeEaseCallback(params, cleanup);
 
     // cancel overwritten transition
     animatable.remove_transition(propName);
