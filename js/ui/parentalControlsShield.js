09bfd8d785588aca3b75edf171961c98a535e893
Ignacy Kuchciński <ignacykuchcinski@gnome.org>
2025-12-12 02:32:45 +0100

    authPrompt: Implement screen time ignore button
    Implement screen time ignore button which allows the parent to extend
    the child’s computer time for the rest of the day.
    
    Closes: https://gitlab.gnome.org/GNOME/gnome-shell/-/work_items/8578

--- js/ui/parentalControlsShield.js
+++ js/ui/parentalControlsShield.js
@@ -3,8 +3,14 @@
 // when parental controls session limits are reached
 
 import Clutter from 'gi://Clutter';
+import Gio from 'gi://Gio';
 import GObject from 'gi://GObject';
 import St from 'gi://St';
+
+import {loadInterfaceXML} from '../misc/fileUtils.js';
+
+const TimerChildIface = loadInterfaceXML('org.freedesktop.MalcontentTimer1.Child');
+const TimerChildProxy = Gio.DBusProxy.makeProxyWrapper(TimerChildIface);
 
 export const ParentalControlsShield = GObject.registerClass(
 class ParentalControlsShield extends St.BoxLayout {
@@ -14,6 +20,24 @@ class ParentalControlsShield extends St.BoxLayout {
             orientation: Clutter.Orientation.VERTICAL,
             x_align: Clutter.ActorAlign.CENTER,
         });
+
+        this._requestExtensionCookie = null;
+
+        this._timerChildProxy = TimerChildProxy(Gio.DBus.system,
+            'org.freedesktop.MalcontentTimer1',
+            '/org/freedesktop/MalcontentTimer1',
+            (proxy, error) => {
+                if (error)
+                    console.error(`Failed to get TimerChild proxy: ${error}`);
+            },
+            null, /* cancellable */
+            Gio.DBusProxyFlags.DO_NOT_AUTO_START_AT_CONSTRUCTION
+        );
+
+        this._timerChildProxy.connectSignal('ExtensionResponse', (proxy, sender, params) =>
+            this._onExtensionResponse(proxy, sender, params));
+
+        this.connect('destroy', this._onDestroy.bind(this));
 
         this._titleLabel = new St.Label({
             style_class: 'parental-controls-shield-title',
@@ -27,5 +51,44 @@ class ParentalControlsShield extends St.BoxLayout {
         });
         this._descriptionLabel.clutter_text.line_wrap = true;
         this.add_child(this._descriptionLabel);
+
+        this._ignoreButton = new St.Button({
+            style_class: 'parental-controls-shield-button',
+            // Translators: this is for ignoring a screen time limit for parental controls
+            label: _('Ignore'),
+            x_align: Clutter.ActorAlign.CENTER,
+        });
+        this._ignoreButton.connect('clicked',
+            () => this._onIgnoreButtonClicked().catch(logError));
+        this.add_child(this._ignoreButton);
+    }
+
+    _onDestroy() {
+        this._requestExtensionCookie = null;
+    }
+
+    async _onIgnoreButtonClicked() {
+        if (this._requestExtensionCookie)
+            return;
+
+        try {
+            [this._requestExtensionCookie] = await this._timerChildProxy.RequestExtensionAsync(
+                'login-session',
+                '',
+                0,
+                {},
+                Gio.DBusCallFlags.ALLOW_INTERACTIVE_AUTHORIZATION
+            );
+        } catch (e) {
+            console.warn(`Failed to obtain screen time extension: ${e.message}`);
+        }
+    }
+
+    _onExtensionResponse(proxy, sender, [_, cookie]) {
+        if (this._requestExtensionCookie === null ||
+            cookie !== this._requestExtensionCookie)
+            return;
+
+        this._requestExtensionCookie = null;
     }
 });
