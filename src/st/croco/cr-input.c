e8c004126e441a71d6aca91594a4f558328417d8
Florian Müllner <fmuellner@gnome.org>
2026-04-26 01:25:47 +0200

    croco/input: Only accept UTF-8 input
    We never pass anything else, so no need to convert other encodings.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4195>

--- src/st/croco/cr-input.c
+++ src/st/croco/cr-input.c
@@ -24,7 +24,6 @@
 #include "stdio.h"
 #include <string.h>
 #include "cr-input.h"
-#include "cr-enc-handler.h"
 
 /**
  *@CRInput:
@@ -132,51 +131,20 @@ cr_input_new_from_buf (guchar * a_buf,
                        gboolean a_free_buf)
 {
         CRInput *result = NULL;
-        enum CRStatus status = CR_OK;
-        CREncHandler *enc_handler = NULL;
-        gulong len = a_len;
 
         g_return_val_if_fail (a_buf, NULL);
+        g_return_val_if_fail (a_enc != CR_UTF_8, NULL);
 
         result = cr_input_new_real ();
         g_return_val_if_fail (result, NULL);
 
-        /*transform the encoding in utf8 */
-        if (a_enc != CR_UTF_8) {
-                enc_handler = cr_enc_handler_get_instance (a_enc);
-                if (!enc_handler) {
-                        goto error;
-                }
-
-                status = cr_enc_handler_convert_input
-                        (enc_handler, a_buf, &len,
-                         &PRIVATE (result)->in_buf,
-                         &PRIVATE (result)->in_buf_size);
-                if (status != CR_OK)
-                        goto error;
-                PRIVATE (result)->free_in_buf = TRUE;
-                if (a_free_buf == TRUE && a_buf) {
-                        g_free (a_buf) ;
-                        a_buf = NULL ;
-                }                
-                PRIVATE (result)->nb_bytes = PRIVATE (result)->in_buf_size;
-        } else {
-                PRIVATE (result)->in_buf = (guchar *) a_buf;
-                PRIVATE (result)->in_buf_size = a_len;
-                PRIVATE (result)->nb_bytes = a_len;
-                PRIVATE (result)->free_in_buf = a_free_buf;
-        }
+        PRIVATE (result)->in_buf = (guchar *) a_buf;
+        PRIVATE (result)->in_buf_size = a_len;
+        PRIVATE (result)->nb_bytes = a_len;
+        PRIVATE (result)->free_in_buf = a_free_buf;
         PRIVATE (result)->line = 1;
         PRIVATE (result)->col =  0;
         return result;
-
- error:
-        if (result) {
-                cr_input_destroy (result);
-                result = NULL;
-        }
-
-        return NULL;
 }
 
 /**
