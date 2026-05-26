GNOME/gnome-shell

commit c3b936e4d76b6b9c1f76ded8d841ffb2792f7ff8

js: Use new St.ButtonMask names
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4224>

js/gdm/loginDialog.js
@@ -62,7 +62,7 @@ export const UserListItem = GObject.registerClass({
         });
         super._init({
             style_class: 'login-dialog-user-list-item',
-            button_mask: St.ButtonMask.ONE | St.ButtonMask.THREE,
+            button_mask: St.ButtonMask.PRIMARY | St.ButtonMask.SECONDARY,
             can_focus: true,
             x_expand: true,
             child: layout,
@@ -604,7 +604,7 @@ export const LoginDialog = GObject.registerClass({
         });
         this._notListedButton = new St.Button({
             style_class: 'login-dialog-not-listed-button',
-            button_mask: St.ButtonMask.ONE | St.ButtonMask.THREE,
+            button_mask: St.ButtonMask.PRIMARY | St.ButtonMask.SECONDARY,
             can_focus: true,
             child: notListedLabel,
             reactive: true,

js/gdm/authList.js
@@ -37,7 +37,7 @@ const AuthListItem = GObject.registerClass({
 
         super._init({
             style_class: 'login-dialog-auth-list-item',
-            button_mask: St.ButtonMask.ONE | St.ButtonMask.THREE,
+            button_mask: St.ButtonMask.PRIMARY | St.ButtonMask.SECONDARY,
             can_focus: true,
             child: label,
             reactive: true,

js/gdm/authPrompt.js
@@ -254,7 +254,7 @@ export const AuthPrompt = GObject.registerClass({
         this.cancelButton = new St.Button({
             style_class: 'login-dialog-button cancel-button',
             accessible_name: _('Cancel'),
-            button_mask: St.ButtonMask.ONE | St.ButtonMask.THREE,
+            button_mask: St.ButtonMask.PRIMARY | St.ButtonMask.SECONDARY,
             reactive: this._hasCancelButton,
             can_focus: this._hasCancelButton,
             x_align: Clutter.ActorAlign.START,

js/ui/appDisplay.js
@@ -1852,7 +1852,7 @@ class AppViewItem extends St.Button {
         super._init({
             pivot_point: new Graphene.Point({x: 0.5, y: 0.5}),
             reactive: true,
-            button_mask: St.ButtonMask.ONE | St.ButtonMask.TWO,
+            button_mask: St.ButtonMask.PRIMARY | St.ButtonMask.MIDDLE,
             can_focus: true,
             ...params,
         });
@@ -2280,7 +2280,7 @@ export const FolderIcon = GObject.registerClass({
     _init(id, path, parentView) {
         super._init({
             style_class: 'overview-tile app-folder',
-            button_mask: St.ButtonMask.ONE,
+            button_mask: St.ButtonMask.PRIMARY,
             can_focus: true,
         }, global.settings.is_writable('app-picker-layout'));
 
@@ -2571,7 +2571,7 @@ export const AppFolderDialog = GObject.registerClass({
         // Edit button
         this._editButton = new St.Button({
             style_class: 'icon-button',
-            button_mask: St.ButtonMask.ONE,
+            button_mask: St.ButtonMask.PRIMARY,
             toggle_mode: true,
             reactive: true,
             can_focus: true,

js/ui/checkBox.js
@@ -14,7 +14,7 @@ class CheckBox extends St.Button {
         super._init({
             style_class: 'check-box',
             child: container,
-            button_mask: St.ButtonMask.ONE,
+            button_mask: St.ButtonMask.PRIMARY,
             toggle_mode: true,
             can_focus: true,
         });

js/ui/dialog.js
@@ -126,7 +126,7 @@ class Dialog extends St.Widget {
 
         const button = new St.Button({
             style_class: 'modal-dialog-button',
-            button_mask: St.ButtonMask.ONE | St.ButtonMask.THREE,
+            button_mask: St.ButtonMask.PRIMARY | St.ButtonMask.SECONDARY,
             reactive: true,
             can_focus: true,
             x_expand: true,

js/ui/pageIndicators.js
@@ -55,9 +55,9 @@ export const PageIndicators = GObject.registerClass({
                 const pageIndex = this._nPages + i;
                 const indicator = new St.Button({
                     style_class: 'page-indicator',
-                    button_mask: St.ButtonMask.ONE |
-                                 St.ButtonMask.TWO |
-                                 St.ButtonMask.THREE,
+                    button_mask: St.ButtonMask.PRIMARY |
+                                 St.ButtonMask.MIDDLE |
+                                 St.ButtonMask.SECONDARY,
                     reactive: this._reactive,
                     child: new St.Widget({
                         style_class: 'page-indicator-icon',

js/ui/unlockDialog.js
@@ -617,7 +617,7 @@ export const UnlockDialog = GObject.registerClass({
         this._otherUserButton = new St.Button({
             style_class: 'login-dialog-button switch-user-button',
             accessible_name: _('Log in as another user'),
-            button_mask: St.ButtonMask.ONE | St.ButtonMask.THREE,
+            button_mask: St.ButtonMask.PRIMARY | St.ButtonMask.SECONDARY,
             reactive: false,
             opacity: 0,
             x_align: Clutter.ActorAlign.END,

commit 63dbb8c11bafcb6863cb512ce5b3af8c8dcea904

loginDialog: Use new St.ButtonMask names
Whoops, forgot to update commit c3b936e4d7 with the additions
from commit 8f80504e.

Fixes: c3b936e4d7 ("js: Use new St.ButtonMask names")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4228>

js/gdm/loginDialog.js
@@ -326,7 +326,7 @@ const SessionMenuButton = GObject.registerClass({
             can_focus: true,
             accessible_name: _('Choose Session'),
             accessible_role: Atk.Role.MENU,
-            button_mask: St.ButtonMask.ONE | St.ButtonMask.THREE,
+            button_mask: St.ButtonMask.PRIMARY | St.ButtonMask.SECONDARY,
             x_align: Clutter.ActorAlign.CENTER,
             y_align: Clutter.ActorAlign.CENTER,
         });
@@ -420,7 +420,7 @@ class A11yMenuButton extends St.Button {
             icon_name: 'org.gnome.Settings-accessibility-symbolic',
             accessible_name: _('Accessibility'),
             accessible_role: Atk.Role.MENU,
-            button_mask: St.ButtonMask.ONE | St.ButtonMask.THREE,
+            button_mask: St.ButtonMask.PRIMARY | St.ButtonMask.SECONDARY,
             can_focus: true,
         });
 