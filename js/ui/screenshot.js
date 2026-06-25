GNOME/gnome-shell

commit 8769fdddcf9cccbd03d49aa945ef56e441e7fb7a

screenshot: Snap cursor to the center of the handle on press
When dragging the screenshot area handle from the outer sides, it is
currently impossible to move them to the edges or corners of the screen,
as the cursor is constrained to the monitor.

Fix this by snapping the cursor to the center of the handle when starting
the resize operation.

Closes: https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/9113
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4158>
(cherry picked from commit 9c2712214a1ddc02fb37b605dfdd8a5dd7a4db8b)

js/ui/screenshot.js
@@ -482,6 +482,44 @@ const UIAreaSelector = GObject.registerClass({
             const rightX = leftX + width - 1;
             const bottomY = topY + height - 1;
 
+            let snapX = this._dragStartX;
+            let snapY = this._dragStartY;
+
+            switch (cursor) {
+            case Clutter.CursorType.NW_RESIZE:
+                snapX = leftX;
+                snapY = topY;
+                break;
+            case Clutter.CursorType.NE_RESIZE:
+                snapX = rightX;
+                snapY = topY;
+                break;
+            case Clutter.CursorType.SW_RESIZE:
+                snapX = leftX;
+                snapY = bottomY;
+                break;
+            case Clutter.CursorType.SE_RESIZE:
+                snapX = rightX;
+                snapY = bottomY;
+                break;
+            case Clutter.CursorType.N_RESIZE:
+                snapY = topY;
+                break;
+            case Clutter.CursorType.S_RESIZE:
+                snapY = bottomY;
+                break;
+            case Clutter.CursorType.W_RESIZE:
+                snapX = leftX;
+                break;
+            case Clutter.CursorType.E_RESIZE:
+                snapX = rightX;
+                break;
+            }
+
+            const backend = this.get_context().get_backend();
+            backend.get_default_seat().warp_pointer(snapX, snapY);
+            [this._dragStartX, this._dragStartY] = [snapX, snapY];
+
             // For moving, start X and Y are the top left corner, while
             // last X and Y are the bottom right corner.
             if (cursor === Meta.Cursor.MOVE) {

commit 3705e1155b74b37b09c89002cdaf63f84f9429c9

screenshot: Use 49.x compatible cursor enum
When 8769fdddc was backported to 49, the cursor enum used was still
Clutter.CursorType, which only was introduced in Mutter 50. This was
breaking screenshot selection resizing from the top/left corner and
causing error messages.

Replace these enum uses with the 49 equivalent Meta.Cursor.

Fixes: 8769fdddc ("screenshot: Snap cursor to the center of the handle on press")
Closes: https://gitlab.gnome.org/GNOME/gnome-shell/-/work_items/9271
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4265>

js/ui/screenshot.js
@@ -486,32 +486,32 @@ const UIAreaSelector = GObject.registerClass({
             let snapY = this._dragStartY;
 
             switch (cursor) {
-            case Clutter.CursorType.NW_RESIZE:
+            case Meta.Cursor.NW_RESIZE:
                 snapX = leftX;
                 snapY = topY;
                 break;
-            case Clutter.CursorType.NE_RESIZE:
+            case Meta.Cursor.NE_RESIZE:
                 snapX = rightX;
                 snapY = topY;
                 break;
-            case Clutter.CursorType.SW_RESIZE:
+            case Meta.Cursor.SW_RESIZE:
                 snapX = leftX;
                 snapY = bottomY;
                 break;
-            case Clutter.CursorType.SE_RESIZE:
+            case Meta.Cursor.SE_RESIZE:
                 snapX = rightX;
                 snapY = bottomY;
                 break;
-            case Clutter.CursorType.N_RESIZE:
+            case Meta.Cursor.N_RESIZE:
                 snapY = topY;
                 break;
-            case Clutter.CursorType.S_RESIZE:
+            case Meta.Cursor.S_RESIZE:
                 snapY = bottomY;
                 break;
-            case Clutter.CursorType.W_RESIZE:
+            case Meta.Cursor.W_RESIZE:
                 snapX = leftX;
                 break;
-            case Clutter.CursorType.E_RESIZE:
+            case Meta.Cursor.E_RESIZE:
                 snapX = rightX;
                 break;
             }