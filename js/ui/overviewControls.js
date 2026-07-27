GNOME/gnome-shell

commit 95b7b722e8aec40f425ca2b1ad53ba4c7974260c

searchController: Handle activation from search entry differently
There is already a ClutterText::activate action signal, triggered
when return/enter keys are pressed on the entry. Use that for activation.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4248>

js/ui/searchController.js
@@ -59,6 +59,9 @@ export const SearchController = GObject.registerClass({
         this._text.connect('key-focus-out', () => {
             this._searchResults.highlightDefault(false);
         });
+        this._text.connect('activate', () => {
+            this._searchResults.activateDefault();
+        });
         this._entry.connect('popup-menu', () => {
             if (!this._searchActive)
                 return;
@@ -301,9 +304,6 @@ export const SearchController = GObject.registerClass({
                 return Clutter.EVENT_STOP;
             } else if (symbol === arrowNext && this._text.cursor_position === -1) {
                 this._searchResults.navigateFocus(nextDirection);
-                return Clutter.EVENT_STOP;
-            } else if (symbol === Clutter.KEY_Return || symbol === Clutter.KEY_KP_Enter) {
-                this._searchResults.activateDefault();
                 return Clutter.EVENT_STOP;
             }
         }

commit c0e84e1778e1726ba97ec2b481a7653857551109

search: Handle Ctrl+Enter in search entry
When the search controller was ported to use the `ClutterText::activate`
signal on the search entry, this broke activating search results with
Ctrl+Enter as this combination does not trigger the signal.

This now adds a new `SearchEntry` class that emits a
`activate-new-instance` signal on Ctrl+Enter. The actual decision
whether to launch a new instance however is still handled in
`AppIcon::activate()` for now and based on the held modifiers.

Fixes: 95b7b722e ("searchController: Handle activation from search entry differently")
Closes: https://gitlab.gnome.org/GNOME/gnome-shell/-/work_items/9312
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4303>

js/ui/overviewControls.js
@@ -10,6 +10,7 @@ import * as AppDisplay from './appDisplay.js';
 import * as Dash from './dash.js';
 import * as Main from './main.js';
 import * as Overview from './overview.js';
+import * as Search from './search.js';
 import * as SearchController from './searchController.js';
 import * as Util from '../misc/util.js';
 import * as WindowManager from './windowManager.js';
@@ -321,7 +322,7 @@ class ControlsManager extends St.Widget {
 
         this._ignoreShowAppsButtonToggle = false;
 
-        this._searchEntry = new St.Entry({
+        this._searchEntry = new Search.SearchEntry({
             style_class: 'search-entry',
             /* Translators: this is the text displayed
                in the search entry when no search is

js/ui/search.js
@@ -23,6 +23,46 @@ const SEARCH_PROVIDERS_SCHEMA = 'org.gnome.desktop.search-providers';
 const MAX_LIST_SEARCH_RESULTS_ROWS = 5;
 const SEARCH_SPINNER_SIZE = 64;
 
+export class SearchEntry extends St.Entry {
+    static [GObject.signals] = {
+        'activate-new-instance': {},
+    };
+
+    static {
+        GObject.registerClass(this);
+
+        const bindingPool = this.get_binding_pool();
+
+        bindingPool.install_closure(
+            'activate-new-instance',
+            Clutter.KEY_Return,
+            Clutter.ModifierType.CONTROL_MASK,
+            obj => {
+                obj.emit('activate-new-instance');
+                return Clutter.EVENT_STOP;
+            }
+        );
+        bindingPool.install_closure(
+            'activate-new-instance',
+            Clutter.KEY_KP_Enter,
+            Clutter.ModifierType.CONTROL_MASK,
+            obj => {
+                obj.emit('activate-new-instance');
+                return Clutter.EVENT_STOP;
+            }
+        );
+        bindingPool.install_closure(
+            'activate-new-instance',
+            Clutter.KEY_ISO_Enter,
+            Clutter.ModifierType.CONTROL_MASK,
+            obj => {
+                obj.emit('activate-new-instance');
+                return Clutter.EVENT_STOP;
+            }
+        );
+    }
+}
+
 const MaxWidthBox = GObject.registerClass(
 class MaxWidthBox extends St.BoxLayout {
     vfunc_allocate(box) {

js/ui/searchController.js
@@ -61,6 +61,9 @@ export const SearchController = GObject.registerClass({
         this._text.connect('activate', () => {
             this._searchResults.activateDefault();
         });
+        this._entry.connect('activate-new-instance', () => {
+            this._searchResults.activateDefault();
+        });
         this._entry.connect('popup-menu', () => {
             if (!this._searchActive)
                 return;