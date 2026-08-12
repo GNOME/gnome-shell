GNOME/gnome-shell

commit 43f3ac3db25f1aabbb8643939340efda97aaf4e3

screenshot: Introduce keyboard navigation to move selection
Area is moved when the first modifier key (left alt in most cases) is
pressed alongside the arrow keys. The area moves directly in the arrow
direction pressed, and does not consider the previous directions,
copying the behavior of moving windows in Gnome-Shell using the
arrow keys

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4013>

js/ui/screenshot.js
@@ -366,6 +366,59 @@ const UIAreaSelector = GObject.registerClass({
         this._updateSelectionRect();
     }
 
+    moveInDirection(direction) {
+        const [,, selectionWidth, selectionHeight] = this.getGeometry();
+
+        let newStartX = this._startX;
+        let newStartY = this._startY;
+        let newLastX = this._lastX;
+        let newLastY = this._lastY;
+
+        switch (direction) {
+        case St.DirectionType.LEFT:
+            newStartX -= SELECTION_KEYBOARD_INCREMENT;
+            newLastX -= SELECTION_KEYBOARD_INCREMENT;
+            break;
+        case St.DirectionType.RIGHT:
+            newStartX += SELECTION_KEYBOARD_INCREMENT;
+            newLastX += SELECTION_KEYBOARD_INCREMENT;
+            break;
+        case St.DirectionType.UP:
+            newStartY -= SELECTION_KEYBOARD_INCREMENT;
+            newLastY -= SELECTION_KEYBOARD_INCREMENT;
+            break;
+        case St.DirectionType.DOWN:
+            newStartY += SELECTION_KEYBOARD_INCREMENT;
+            newLastY += SELECTION_KEYBOARD_INCREMENT;
+            break;
+        }
+
+        // Ensure area does not move off the stage edge.
+        if (newStartX < 0 || newLastX < 0) {
+            newStartX = 0;
+            newLastX = newStartX + (selectionWidth - 1);
+        } else if (newLastX > this.width - 1) {
+            newLastX = this.width - 1;
+            newStartX = newLastX - (selectionWidth - 1);
+        }
+
+        if (newStartY < 0 || newLastY < 0) {
+            newStartY = 0;
+            newLastY = newStartY + (selectionHeight - 1);
+        } else if (newLastY > this.height - 1) {
+            newLastY = this.height - 1;
+            newStartY = newLastY - (selectionHeight - 1);
+        }
+
+        // Update selection rectangle props.
+        this._startX = newStartX;
+        this._startY = newStartY;
+        this._lastX = newLastX;
+        this._lastY = newLastY;
+
+        this._updateSelectionRect();
+    }
+
     resizeInDirection(direction) {
         let newStartX = this._startX;
         let newStartY = this._startY;
@@ -1250,6 +1303,36 @@ export class ScreenshotUI extends St.Widget {
                 return Clutter.EVENT_STOP;
             }
         );
+
+        bindingPool.install_closure(
+            'move-selection-left', Clutter.KEY_Left, Clutter.ModifierType.MOD1_MASK,
+            obj => {
+                obj._areaSelector.moveInDirection(St.DirectionType.LEFT);
+                return Clutter.EVENT_STOP;
+            }
+        );
+        bindingPool.install_closure(
+            'move-selection-right', Clutter.KEY_Right, Clutter.ModifierType.MOD1_MASK,
+            obj => {
+                obj._areaSelector.moveInDirection(St.DirectionType.RIGHT);
+                return Clutter.EVENT_STOP;
+            }
+        );
+        bindingPool.install_closure(
+            'move-selection-up', Clutter.KEY_Up, Clutter.ModifierType.MOD1_MASK,
+            obj => {
+                obj._areaSelector.moveInDirection(St.DirectionType.UP);
+                return Clutter.EVENT_STOP;
+            }
+        );
+        bindingPool.install_closure(
+            'move-selection-down', Clutter.KEY_Down, Clutter.ModifierType.MOD1_MASK,
+            obj => {
+                obj._areaSelector.moveInDirection(St.DirectionType.DOWN);
+                return Clutter.EVENT_STOP;
+            }
+        );
+
         bindingPool.install_closure(
             'reset', Clutter.KEY_r, 0,
             obj => {

commit 32635e70cda88c1f751c5ba5e05e168bfa401e68

screenshot: Restrict move bindings to selection mode
Unlike the resize bindings, the move bindings currently also apply
while the selection area is hidden. Moving an invisible selection
doesn't really make sense, so restrict the binding to selection
mode like we already do for resizing.

Fixes: 43f3ac3db2 ("screenshot: Introduce keyboard navigation to move selection")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4332>

js/ui/screenshot.js
@@ -1418,28 +1418,28 @@ export class ScreenshotUI extends St.Widget {
         bindingPool.install_closure(
             'move-selection-left', Clutter.KEY_Left, Clutter.ModifierType.MOD1_MASK,
             (obj, _actionName, _keyVal, modifier) => {
-                obj._areaSelector.moveInDirection(St.DirectionType.LEFT, obj._getIncrement(modifier));
+                obj._moveSelection(St.DirectionType.LEFT, modifier);
                 return Clutter.EVENT_STOP;
             }
         );
         bindingPool.install_closure(
             'move-selection-right', Clutter.KEY_Right, Clutter.ModifierType.MOD1_MASK,
             (obj, _actionName, _keyVal, modifier) => {
-                obj._areaSelector.moveInDirection(St.DirectionType.RIGHT, obj._getIncrement(modifier));
+                obj._moveSelection(St.DirectionType.RIGHT, modifier);
                 return Clutter.EVENT_STOP;
             }
         );
         bindingPool.install_closure(
             'move-selection-up', Clutter.KEY_Up, Clutter.ModifierType.MOD1_MASK,
             (obj, _actionName, _keyVal, modifier) => {
-                obj._areaSelector.moveInDirection(St.DirectionType.UP, obj._getIncrement(modifier));
+                obj._moveSelection(St.DirectionType.UP, modifier);
                 return Clutter.EVENT_STOP;
             }
         );
         bindingPool.install_closure(
             'move-selection-down', Clutter.KEY_Down, Clutter.ModifierType.MOD1_MASK,
             (obj, _actionName, _keyVal, modifier) => {
-                obj._areaSelector.moveInDirection(St.DirectionType.DOWN, obj._getIncrement(modifier));
+                obj._moveSelection(St.DirectionType.DOWN, modifier);
                 return Clutter.EVENT_STOP;
             }
         );
@@ -1454,49 +1454,49 @@ export class ScreenshotUI extends St.Widget {
         bindingPool.install_closure(
             'move-selection-left-shift', Clutter.KEY_Left, Clutter.ModifierType.MOD1_MASK | Clutter.ModifierType.SHIFT_MASK,
             (obj, _actionName, _keyVal, modifier) => {
-                obj._areaSelector.moveInDirection(St.DirectionType.LEFT, obj._getIncrement(St.DirectionType.LEFT, modifier));
+                obj._moveSelection(St.DirectionType.LEFT, modifier);
                 return Clutter.EVENT_STOP;
             }
         );
         bindingPool.install_closure(
             'move-selection-right-control', Clutter.KEY_Right, Clutter.ModifierType.MOD1_MASK | Clutter.ModifierType.CONTROL_MASK,
             (obj, _actionName, _keyVal, modifier) => {
-                obj._areaSelector.moveInDirection(St.DirectionType.RIGHT, obj._getIncrement(St.DirectionType.RIGHT, modifier));
+                obj._moveSelection(St.DirectionType.RIGHT, modifier);
                 return Clutter.EVENT_STOP;
             }
         );
         bindingPool.install_closure(
             'move-selection-right-shift', Clutter.KEY_Right, Clutter.ModifierType.MOD1_MASK | Clutter.ModifierType.SHIFT_MASK,
             (obj, _actionName, _keyVal, modifier) => {
-                obj._areaSelector.moveInDirection(St.DirectionType.RIGHT, obj._getIncrement(St.DirectionType.RIGHT, modifier));
+                obj._moveSelection(St.DirectionType.RIGHT, modifier);
                 return Clutter.EVENT_STOP;
             }
         );
         bindingPool.install_closure(
             'move-selection-up-control', Clutter.KEY_Up, Clutter.ModifierType.MOD1_MASK | Clutter.ModifierType.CONTROL_MASK,
             (obj, _actionName, _keyVal, modifier) => {
-                obj._areaSelector.moveInDirection(St.DirectionType.UP, obj._getIncrement(St.DirectionType.UP, modifier));
+                obj._moveSelection(St.DirectionType.UP, modifier);
                 return Clutter.EVENT_STOP;
             }
         );
         bindingPool.install_closure(
             'move-selection-up-shift', Clutter.KEY_Up, Clutter.ModifierType.MOD1_MASK | Clutter.ModifierType.SHIFT_MASK,
             (obj, _actionName, _keyVal, modifier) => {
-                obj._areaSelector.moveInDirection(St.DirectionType.UP, obj._getIncrement(St.DirectionType.UP, modifier));
+                obj._moveSelection(St.DirectionType.UP, modifier);
                 return Clutter.EVENT_STOP;
             }
         );
         bindingPool.install_closure(
             'move-selection-down-control', Clutter.KEY_Down, Clutter.ModifierType.MOD1_MASK | Clutter.ModifierType.CONTROL_MASK,
             (obj, _actionName, _keyVal, modifier) => {
-                obj._areaSelector.moveInDirection(St.DirectionType.DOWN, obj._getIncrement(St.DirectionType.DOWN, modifier));
+                obj._moveSelection(St.DirectionType.DOWN, modifier);
                 return Clutter.EVENT_STOP;
             }
         );
         bindingPool.install_closure(
             'move-selection-down-shift', Clutter.KEY_Down, Clutter.ModifierType.MOD1_MASK | Clutter.ModifierType.SHIFT_MASK,
             (obj, _actionName, _keyVal, modifier) => {
-                obj._areaSelector.moveInDirection(St.DirectionType.DOWN, obj._getIncrement(St.DirectionType.DOWN, modifier));
+                obj._moveSelection(St.DirectionType.DOWN, modifier);
                 return Clutter.EVENT_STOP;
             }
         );
@@ -2599,6 +2599,11 @@ export class ScreenshotUI extends St.Widget {
         }
 
         return increment;
+    }
+
+    _moveSelection(direction, modifier) {
+        if (this._selectionButton.checked)
+            this._areaSelector.moveInDirection(direction, this._getIncrement(direction, modifier));
     }
 
     _resizeSelection(direction, modifier) {