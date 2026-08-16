GNOME/gnome-shell

commit 0b653e2c48f9f55a0fbf3da05cdba8a03199b941

screenshot: Use Clutter.MotionController to track pixel picking
Move away from motion event handlers.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4248>

js/ui/screenshot.js
@@ -3068,6 +3068,12 @@ class PickPixel extends St.Widget {
         });
         this.add_action(clickGesture);
 
+        const motionController = new Clutter.MotionController();
+        motionController.connect('motion', (_sprite, x, y) => {
+            this._pickColor(x, y);
+        });
+        this.add_action(motionController);
+
         this._recolorEffect = new RecolorEffect({
             chroma: new Cogl.Color({
                 red: 80,
@@ -3121,12 +3127,6 @@ class PickPixel extends St.Widget {
 
         this._recolorEffect.color = this._color;
         this._previewCursor.show();
-    }
-
-    vfunc_motion_event(event) {
-        const [x, y] = event.get_coords();
-        this._pickColor(x, y);
-        return Clutter.EVENT_PROPAGATE;
     }
 });
 

commit a097f660630c7b711462c17b0393c6fd48f2b262

screenshot: Fix incorrect cursor position on pixel picking
The first parameter to the signal handler should be the
Clutter.MotionController itself. Otherwise, `pickColor` is called with
the Clutter.Sprite as the x-coordinate.

Fixes: 0b653e2c48f9f55a0fbf3da05cdba8a03199b941
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4342>

js/ui/screenshot.js
@@ -3440,7 +3440,7 @@ class PickPixel extends St.Widget {
         this.add_action(clickGesture);
 
         const motionController = new Clutter.MotionController();
-        motionController.connect('motion', (_sprite, x, y) => {
+        motionController.connect('motion', (_controller, _sprite, x, y) => {
             this._pickColor(x, y);
         });
         this.add_action(motionController);