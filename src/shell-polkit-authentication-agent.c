b86097d89e373266b4cfe465fb6fe47e7b4d1611
Florian Müllner <fmuellner@gnome.org>
2026-08-20 23:19:39 +0200

    shell/polkit-agent: Rename "handler_id" request member
    We will soon track a second handler, so the name is too generic.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4348>
    (cherry picked from commit 27818f91507fbeb0a031e28ed9bc182d77f25d84)

--- src/shell-polkit-authentication-agent.c
+++ src/shell-polkit-authentication-agent.c
@@ -179,7 +179,7 @@ struct _AuthRequest {
   /* not holding ref */
   ShellPolkitAuthenticationAgent *agent;
   GCancellable *cancellable;
-  gulong handler_id;
+  gulong cancelled_id;
 
   /* copies */
   gchar          *action_id;
@@ -333,7 +333,7 @@ auth_request_complete (AuthRequest *request,
 
   if (!is_current)
     agent->scheduled_requests = g_list_remove (agent->scheduled_requests, request);
-  g_cancellable_disconnect (request->cancellable, request->handler_id);
+  g_cancellable_disconnect (request->cancellable, request->cancelled_id);
 
   if (dismissed)
     g_task_return_new_error (request->task,
@@ -397,10 +397,10 @@ initiate_authentication (PolkitAgentListener  *listener,
   g_list_foreach (request->identities, (GFunc) g_object_ref, NULL);
   request->cancellable = cancellable;
   request->task = g_task_new (listener, NULL, callback, user_data);
-  request->handler_id = g_cancellable_connect (request->cancellable,
-                                               G_CALLBACK (on_request_cancelled),
-                                               request,
-                                               NULL); /* GDestroyNotify for request */
+  request->cancelled_id = g_cancellable_connect (request->cancellable,
+                                                 G_CALLBACK (on_request_cancelled),
+                                                 request,
+                                                 NULL); /* GDestroyNotify for request */
 
   print_debug ("SCHEDULING %s cookie %s", request->action_id, request->cookie);
   agent->scheduled_requests = g_list_append (agent->scheduled_requests, request);
