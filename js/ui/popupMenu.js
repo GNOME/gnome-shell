GNOME/gnome-shell

commit ebee8bb4d9182b3821e26efb50650bde5429a378

popupMenu: Factor out base `open()`/`close()` methods
Menus are assumed to have `open()` and `close()` methods, but it
is up to the subclasses to provide them. This doesn't only result
in some duplication, but also in inconsistencies in the parameters
(specifically whether animations are controlled by a PopupAnimation
value or a bool).

Start addressing this by factoring out base methods that
implementations can chain up to.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4240>

js/ui/popupMenu.js
@@ -937,6 +937,27 @@ export class PopupMenuBase extends Signals.EventEmitter {
         }
     }
 
+    open(_animate) {
+        if (this.isOpen)
+            return false;
+
+        if (this.isEmpty())
+            return false;
+
+        this.isOpen = true;
+        this.emit('open-state-changed', true);
+        return true;
+    }
+
+    close(_animate) {
+        if (!this.isOpen)
+            return false;
+
+        this.isOpen = false;
+        this.emit('open-state-changed', false);
+        return true;
+    }
+
     toggle() {
         if (this.isOpen)
             this.close(BoxPointer.PopupAnimation.FULL);
@@ -1050,18 +1071,13 @@ export class PopupMenu extends PopupMenuBase {
     }
 
     open(animate) {
-        if (this.isOpen)
-            return;
-
-        if (this.isEmpty())
-            return;
+        if (!super.open())
+            return false;
 
         if (!this._systemModalOpenedId) {
             this._systemModalOpenedId =
                 Main.layoutManager.connect('system-modal-opened', () => this.close());
         }
-
-        this.isOpen = true;
 
         this._boxPointer.setPosition(this.sourceActor, this._arrowAlignment);
         this._boxPointer.open(animate);
@@ -1075,7 +1091,7 @@ export class PopupMenu extends PopupMenuBase {
         if (!this.sourceActor?.has_allocation())
             this.sourceActor?.get_parent().queue_relayout();
 
-        this.emit('open-state-changed', true);
+        return true;
     }
 
     close(animate) {
@@ -1088,11 +1104,7 @@ export class PopupMenu extends PopupMenuBase {
             });
         }
 
-        if (!this.isOpen)
-            return;
-
-        this.isOpen = false;
-        this.emit('open-state-changed', false);
+        return super.close();
     }
 
     destroy() {
@@ -1183,14 +1195,8 @@ export class PopupSubMenu extends PopupMenuBase {
     }
 
     open(animate) {
-        if (this.isOpen)
-            return;
-
-        if (this.isEmpty())
-            return;
-
-        this.isOpen = true;
-        this.emit('open-state-changed', true);
+        if (!super.open())
+            return false;
 
         this.actor.show();
 
@@ -1230,14 +1236,12 @@ export class PopupSubMenu extends PopupMenuBase {
             duration,
             mode: Clutter.AnimationMode.EASE_OUT_EXPO,
         });
+        return true;
     }
 
     close(animate) {
-        if (!this.isOpen)
-            return;
-
-        this.isOpen = false;
-        this.emit('open-state-changed', false);
+        if (!super.close())
+            return false;
 
         if (this._activeMenuItem)
             this._activeMenuItem.active = false;
@@ -1260,6 +1264,7 @@ export class PopupSubMenu extends PopupMenuBase {
             duration,
             mode: Clutter.AnimationMode.EASE_OUT_EXPO,
         });
+        return true;
     }
 
     _onKeyPressEvent(actor, event) {

js/ui/quickSettings.js
@@ -457,11 +457,10 @@ class QuickToggleMenu extends PopupMenu.PopupMenuBase {
     }
 
     open(animate) {
-        if (this.isOpen)
-            return;
+        if (!super.open())
+            return false;
 
         this.actor.show();
-        this.isOpen = true;
 
         const previousHeight = this.actor.height;
         this.actor.height = -1;
@@ -485,12 +484,12 @@ class QuickToggleMenu extends PopupMenu.PopupMenuBase {
                 this.actor.height = -1;
             },
         });
-        this.emit('open-state-changed', true);
+        return true;
     }
 
     close(animate) {
-        if (!this.isOpen)
-            return;
+        if (!super.close())
+            return false;
 
         const {opacity} = this.box;
         const duration = animate !== PopupAnimation.NONE
@@ -512,8 +511,7 @@ class QuickToggleMenu extends PopupMenu.PopupMenuBase {
             },
         });
 
-        this.isOpen = false;
-        this.emit('open-state-changed', false);
+        return true;
     }
 
     _syncChecked() {
@@ -840,13 +838,19 @@ export const QuickSettingsMenu = class extends PopupMenu.PopupMenu {
     }
 
     open(animate) {
+        if (!super.open(animate))
+            return false;
+
         this.actor.show();
-        super.open(animate);
+        return true;
     }
 
     close(animate) {
+        if (!super.close(animate))
+            return false;
+
         this._activeMenu?.close(animate);
-        super.close(animate);
+        return true;
     }
 
     _setDimmed(dim) {

js/ui/keyboard.js
@@ -199,14 +199,20 @@ class LanguageSelectionPopup extends PopupMenu.PopupMenu {
     }
 
     open(animate) {
-        super.open(animate);
+        if (!super.open(animate))
+            return false;
+
         global.stage.connectObject(
             'captured-event', this._onCapturedEvent.bind(this), this);
+        return true;
     }
 
     close(animate) {
-        super.close(animate);
+        if (!super.close(animate))
+            return false;
+
         global.stage.disconnectObject(this);
+        return true;
     }
 
     destroy() {

js/ui/shellEntry.js
@@ -48,17 +48,20 @@ export class EntryMenu extends PopupMenu.PopupMenu {
     }
 
     open(animate) {
+        if (!super.open(animate))
+            return false;
+
         this._updatePasteItem();
         this._updateCopyItem();
         if (this._passwordItem)
             this._updatePasswordItem();
 
-        super.open(animate);
         this._entry.add_style_pseudo_class('focus');
 
         const direction = St.DirectionType.TAB_FORWARD;
         if (!this.actor.navigate_focus(null, direction, false))
             this.actor.grab_key_focus();
+        return true;
     }
 
     _updateCopyItem() {

commit 0e52b265231be8bae89cc515eaf21153b72b48cc

popupMenu: Show actor in base open()
In the next commit, we will focus the first item when a menu was
opened via the keyboard.

In order to ensure consistent behavior between different menu types,
it makes sense to handle this in the base open() method. However keynav
requires an actor to be visible, so move showing the actor into the
base open() method as well.

This doesn't interfere with any animations in subclasses, and in fact
fixes a regression from commit ebee8bb4, where the quick settings menu
lost its opening animation due to the actor still being hidden while
chaining up.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4189>

js/ui/popupMenu.js
@@ -949,6 +949,7 @@ export class PopupMenuBase extends Signals.EventEmitter {
 
         this.isOpen = true;
         this.emit('open-state-changed', true);
+        this.actor.show();
         return true;
     }
 
@@ -1238,8 +1239,6 @@ export class PopupSubMenu extends PopupMenuBase {
     open(params = {}) {
         if (!super.open(params))
             return false;
-
-        this.actor.show();
 
         let {animate = true} = params;
         const needsScrollbar = this._needsScrollbar();

js/ui/quickSettings.js
@@ -462,8 +462,6 @@ class QuickToggleMenu extends PopupMenu.PopupMenuBase {
         if (!super.open(params))
             return false;
 
-        this.actor.show();
-
         const previousHeight = this.actor.height;
         this.actor.height = -1;
         const [targetHeight] = this.actor.get_preferred_height(-1);
@@ -845,14 +843,6 @@ export const QuickSettingsMenu = class extends PopupMenu.PopupMenu {
 
     getFirstItem() {
         return this._grid.get_first_child();
-    }
-
-    open(params = {}) {
-        if (!super.open(params))
-            return false;
-
-        this.actor.show();
-        return true;
     }
 
     close(params = {}) {