4e73ab34d9a65c2395d30e8eb3bc221447af8a28
Florian Müllner <fmuellner@gnome.org>
2026-08-27 21:53:21 +0200

    messageList: Handle unset actor when updating adjustment
    When updating the actor, we handle the case where the passed actor
    is null, however we then ignore it when updating the adjustment.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4362>
    (cherry picked from commit a37793e5ef84ccb8b9161cbf9c17ffaeea27e383)

--- js/ui/messageList.js
+++ js/ui/messageList.js
@@ -2010,7 +2010,7 @@ const FadeEffect = GObject.registerClass({
     }
 
     _vadjustmentChanged() {
-        const newAdj = this.actor.vadjustment;
+        const newAdj = this.actor?.vadjustment;
         if (this._vadjustment === newAdj)
             return;
 
