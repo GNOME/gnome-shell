37b199bda13386c3260688ed1d909de70e16566a
Florian Müllner <fmuellner@gnome.org>
2026-09-01 21:44:24 +0200

    extensionSystem: Log errors of async D-Bus call
    The OpenExtensionPrefs D-Bus method may return an error — for example
    when there already is an open dialog — so log the error to avoid
    "unhandled promise rejection" warnings in the log.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4375>
    (cherry picked from commit 2c536f5b3b0957e34da54fb401a70efd01f9f235)

--- js/ui/extensionSystem.js
+++ js/ui/extensionSystem.js
@@ -324,7 +324,7 @@ export class ExtensionManager extends Signals.EventEmitter {
             null,
             Gio.DBusCallFlags.NONE,
             -1,
-            null);
+            null).catch(logError);
         return true;
     }
 
