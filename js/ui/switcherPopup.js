GNOME/gnome-shell

commit b6bb62cf3012d04c10bfbc68db14370704af5f9f

switcherPopup: Use motion controller for item hovering checks
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4248>

js/ui/switcherPopup.js
@@ -467,7 +467,11 @@ export const SwitcherList = GObject.registerClass({
         this._list.add_child(bbox);
 
         bbox.connect('clicked', () => this._onItemClicked(bbox));
-        bbox.connect('motion-event', () => this._onItemMotion(bbox));
+
+        const motionController = new Clutter.MotionController();
+        motionController.connect('motion', () => this._onItemMotion(bbox));
+        motionController.connect('enter', () => this._onItemMotion(bbox));
+        bbox.add_action(motionController);
 
         bbox.label_actor = label;
 

commit a787c42fb59ae10a6dfa066bff242e5876bf2251

switcherPopup: Ignore (again) implicit crossing due to geometry changes
Drop the ClutterMotionController::enter handling, and ignore again everything
else but explicit user motion. Fixes SwitcherPopup child popups spuriously
selecting an item if the pointer happened to be in its coordinates.

Fixes: b6bb62cf3 ("switcherPopup: Use motion controller for item hovering checks")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4371>

js/ui/switcherPopup.js
@@ -470,7 +470,6 @@ export const SwitcherList = GObject.registerClass({
 
         const motionController = new Clutter.MotionController();
         motionController.connect('motion', () => this._onItemMotion(bbox));
-        motionController.connect('enter', () => this._onItemMotion(bbox));
         bbox.add_action(motionController);
 
         bbox.label_actor = label;