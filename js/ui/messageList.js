336a4b3c58ecef1b50e147eafa92fd21633604b1
Florian Müllner <fmuellner@gnome.org>
2026-09-04 20:45:23 +0200

    messageList: Limit body/title length
    The strings typically come from apps, and can therefore be of
    arbitrary length.
    
    Excessively long strings will end up ellsized, but they still
    affect performance when computing the Pango layout or searching
    for URLs, as those operate on the entire string.
    
    Address this by imposing a character limit on the strings, so that
    the amount of hidden characters to consider for URLs and layout is
    capped.
    
    The picked limits are based on sequences of "l", which should be more
    dense than any real text, and therefore guarantee that we only cut
    off invisible parts of the strings.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4387>

--- js/ui/messageList.js
+++ js/ui/messageList.js
@@ -29,6 +29,9 @@ const WIDTH_OFFSET_STACKED = 6;
 const HEIGHT_OFFSET_STACKED = 10;
 const HEIGHT_OFFSET_REDUCTION_STACKED = 1.4;
 
+const MAX_TITLE_LENGTH = 150;
+const MAX_BODY_LENGTH = 850;
+
 export const URLHighlighter = GObject.registerClass(
 class URLHighlighter extends St.Label {
     _init(text = '', lineWrap, allowMarkup) {
@@ -588,7 +591,7 @@ export class Message extends St.Button {
     }
 
     set title(text) {
-        this._titleText = text;
+        this._titleText = this._limitString(text, MAX_TITLE_LENGTH);
         const title = text ? Util.fixMarkup(text.replace(/\n/g, ' '), false) : '';
         this.titleLabel.clutter_text.set_markup(title);
         this.notify('title');
@@ -599,7 +602,7 @@ export class Message extends St.Button {
     }
 
     set body(text) {
-        this._bodyText = text;
+        this._bodyText = this._limitString(text, MAX_BODY_LENGTH);
         this._bodyLabel.setMarkup(text ? text.replace(/\n/g, ' ') : '',
             this._useBodyMarkup);
         this.notify('body');
@@ -698,6 +701,15 @@ export class Message extends St.Button {
     _closeIfAllowed() {
         if (this.canClose())
             this.close();
+    }
+
+    _limitString(string, limit) {
+        const segmenter = new Intl.Segmenter();
+        const graphemes =
+            Array.from(segmenter.segment(string))
+            .splice(0, limit)
+            .map(o => o.segment);
+        return graphemes.join('');
     }
 }
 
