e5c2018d3d6326088fe6d302748d63bcc33ab891
Carlos Garnacho <carlosg@gnome.org>
2026-09-07 16:13:27 +0200

    status/network: Restore chain up on activate() with a Clutter.Event
    This requirement did not go away, this was mistakenly removed in commit
    d59800c6b. Restore it so that network menu items do the activate() call
    chain with an event in the places where it matters.
    
    Fixes: d59800c6b ("popupMenu: Handle keyboard activation through a binding pool")
    Closes: https://gitlab.gnome.org/GNOME/gnome-shell/-/work_items/9396
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4391>

--- js/ui/status/network.js
+++ js/ui/status/network.js
@@ -187,6 +187,10 @@ const NMMenuItem = GObject.registerClass({
         return 0;
     }
 
+    activate() {
+        super.activate(Clutter.get_current_event());
+    }
+
     _activeConnectionStateChanged() {
         this.notify('is-active');
         this.notify('icon-name');
