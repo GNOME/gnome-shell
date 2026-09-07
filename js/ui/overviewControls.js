GNOME/gnome-shell

commit 9b3da69a0b9ec9e3b01b61711e126e87e2988e95

overviewControls: Handle keyboard focus in overview differently
Make the focus more explicit depending on the current view, and explicitly
handled by it. Also drop the focus-on-map shenanigans from AppDisplay. While
unmapped, it should be irrelevant if the actor is reactive or not, so always
keep it enabled.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4304>

js/ui/overviewControls.js
@@ -318,6 +318,7 @@ class ControlsManager extends St.Widget {
             x_expand: true,
             y_expand: true,
             clip_to_allocation: true,
+            reactive: true,
         });
 
         this._ignoreShowAppsButtonToggle = false;
@@ -379,6 +380,8 @@ class ControlsManager extends St.Widget {
         this.add_child(this._thumbnailsBox);
         this.add_child(this._workspacesDisplay);
 
+        global.focus_manager.add_group(this);
+
         this.layout_manager = new ControlsManagerLayout(
             this._searchEntryBin,
             this._appDisplay,
@@ -439,41 +442,6 @@ class ControlsManager extends St.Widget {
                 Main.overview.toggle();
         }, this);
 
-        // connect_after to give search controller first dibs on the event
-        global.stage.connect_after('key-press-event', (actor, event) => {
-            if (this._searchController.searchActive)
-                return Clutter.EVENT_PROPAGATE;
-
-            if (global.stage.key_focus &&
-                !this.contains(global.stage.key_focus))
-                return Clutter.EVENT_PROPAGATE;
-
-            const {finalState} =
-                this._stateAdjustment.getStateTransitionParams();
-            let keynavDisplay;
-
-            if (finalState === ControlsState.WINDOW_PICKER)
-                keynavDisplay = this._workspacesDisplay;
-            else if (finalState === ControlsState.APP_GRID)
-                keynavDisplay = this._appDisplay;
-
-            if (!keynavDisplay)
-                return Clutter.EVENT_PROPAGATE;
-
-            const symbol = event.get_key_symbol();
-            if (symbol === Clutter.KEY_Tab || symbol === Clutter.KEY_Down) {
-                keynavDisplay.navigate_focus(
-                    null, St.DirectionType.TAB_FORWARD, false);
-                return Clutter.EVENT_STOP;
-            } else if (symbol === Clutter.KEY_ISO_Left_Tab) {
-                keynavDisplay.navigate_focus(
-                    null, St.DirectionType.TAB_BACKWARD, false);
-                return Clutter.EVENT_STOP;
-            }
-
-            return Clutter.EVENT_PROPAGATE;
-        });
-
         Main.wm.addKeybinding(
             'toggle-application-view',
             new Gio.Settings({schema_id: WindowManager.SHELL_KEYBINDINGS_SCHEMA}),
@@ -496,6 +464,31 @@ class ControlsManager extends St.Widget {
         this._update();
 
         this.connect('destroy', this._onDestroy.bind(this));
+    }
+
+    vfunc_navigate_focus(from, _direction) {
+        if (from === null) {
+            const {finalState} =
+                  this._stateAdjustment.getStateTransitionParams();
+            let keynavDisplay;
+
+            if (finalState === ControlsState.WINDOW_PICKER)
+                keynavDisplay = this._workspacesDisplay;
+            else if (finalState === ControlsState.APP_GRID)
+                keynavDisplay = this._appDisplay;
+            else
+                return false;
+
+            keynavDisplay.grab_key_focus();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    vfunc_key_focus_in() {
+        super.vfunc_key_focus_in();
+        this.navigate_focus(null, St.DirectionType.TAB_FORWARD, false);
     }
 
     _getFitModeForState(state) {
@@ -605,6 +598,7 @@ class ControlsManager extends St.Widget {
 
         this._updateThumbnailsBox();
         this._updateAppDisplayVisibility(params);
+        this.grab_key_focus();
     }
 
     _onSearchChanged() {

js/ui/appDisplay.js
@@ -1347,6 +1347,7 @@ export class AppDisplay extends BaseAppView {
             layout_manager: new Clutter.BinLayout(),
             x_expand: true,
             y_expand: true,
+            reactive: true,
         });
 
         this._pageManager = new PageManager();
@@ -1387,15 +1388,13 @@ export class AppDisplay extends BaseAppView {
         }
     }
 
-    vfunc_map() {
-        super.vfunc_map();
-        this.reactive = true;
-        this.grab_key_focus();
-    }
-
-    vfunc_unmap() {
-        this.reactive = false;
-        super.vfunc_unmap();
+    vfunc_navigate_focus(from, direction) {
+        if (from === this && direction === St.DirectionType.DOWN) {
+            return super.vfunc_navigate_focus(
+                null, St.DirectionType.TAB_FORWARD);
+        } else {
+            return super.vfunc_navigate_focus(from, direction);
+        }
     }
 
     _redisplay() {

js/ui/workspacesView.js
@@ -979,6 +979,9 @@ class WorkspacesDisplay extends St.Widget {
     }
 
     vfunc_navigate_focus(from, direction) {
+        if (from === this && direction === St.DirectionType.DOWN)
+            direction = St.DirectionType.TAB_FORWARD;
+
         return this._getPrimaryView()?.navigate_focus(from, direction, false);
     }
 

commit 92e0cb5aa8b186595868bcafa0a02af7a87102a4

overviewControls: Only change focus on state changes
The `_update()` function is called for every adjustment change and was
moving the focus to the `ControlsManager`, which then was moving focus
to one of its child widgets via `vfunc_navigate_focus()`. As a result
for every step of the animation the focus was oscillating between these
two widgets. And with each focus change the pseudo classes of the
respective style nodes were changed, triggering a notification and
recompute cascade. This was causing things to slow down to a very
noticeable stutter when moving to/from the app grid.

This now only changes focus when the state has actually been changed,
i.e. when the progress hits 1.0.

Fixes: 9b3da69a0 ("overviewControls: Handle keyboard focus in overview differently")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4379>

js/ui/overviewControls.js
@@ -598,7 +598,9 @@ class ControlsManager extends St.Widget {
 
         this._updateThumbnailsBox();
         this._updateAppDisplayVisibility(params);
-        this.grab_key_focus();
+
+        if (params.progress === 1.0)
+            this.grab_key_focus();
     }
 
     _onSearchChanged() {