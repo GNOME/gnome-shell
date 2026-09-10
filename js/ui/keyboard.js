d84ea6c3b45adcea8649a67a946292ea857d807a
Marco Trevisan (Treviño) <mail@3v1n0.net>
2026-09-10 14:17:18 +0200

    keyboard: Properly use modifier keys as a Map
    Modifier keys was initialized as a JS Map, but then we used it using the
    Object API, attaching data to a map rather than using the actual Map
    features
    
    Fixes: 2920738d ("keyboard: Add/handle "modifier" action keys")
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4396>

--- js/ui/keyboard.js
+++ js/ui/keyboard.js
@@ -1414,9 +1414,9 @@ export const Keyboard = GObject.registerClass({
             }
 
             if (key.action === 'modifier') {
-                const modifierKeys = this._modifierKeys[key.keyval] || [];
+                const modifierKeys = this._modifierKeys.get(key.keyval) ?? [];
                 modifierKeys.push(button);
-                this._modifierKeys[key.keyval] = modifierKeys;
+                this._modifierKeys.set(key.keyval, modifierKeys);
             }
 
             if (key.action || key.keyval)
@@ -1437,7 +1437,7 @@ export const Keyboard = GObject.registerClass({
         else
             this._modifiers.delete(keyval);
 
-        for (const key of this._modifierKeys[keyval])
+        for (const key of this._modifierKeys.get(keyval))
             key.setLatched(enabled);
     }
 
