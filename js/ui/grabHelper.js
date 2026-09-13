GNOME/gnome-shell

commit 5e5b0f49d35ae2c1626eb2ee386befb608a8d5e4

keyboard: Set up OSK in input panel group
As actors in this group have a golden pass during grabs, the
maybeHandleEvent() business from captured events is no longer
necessary, so these hacks have been removed project-wide.

Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4283>

js/ui/grabHelper.js
@@ -178,12 +178,6 @@ export class GrabHelper {
             this._owner.add_action_full(
                 'grab-helper-key-capture', Clutter.EventPhase.CAPTURE,
                 this._keyController);
-
-            this._capturedEventId = this._owner.connect('captured-event',
-                (actor, event) => {
-                    Main.keyboard.maybeHandleEvent(event);
-                    return Clutter.EVENT_PROPAGATE;
-                });
         }
 
         this._modalCount++;

js/ui/keyboard.js
@@ -1026,22 +1026,6 @@ export class KeyboardManager extends Signals.EventEmitter {
     setSuggestionsVisible(visible) {
         this._keyboard?.setSuggestionsVisible(visible);
     }
-
-    maybeHandleEvent(event) {
-        if (!this._keyboard)
-            return false;
-
-        const actor = global.stage.get_event_actor(event);
-
-        if (Main.layoutManager.keyboardBox.contains(actor) ||
-            !!actor._extendedKeys || !!actor.extendedKey) {
-            actor.event(event, true);
-            actor.event(event, false);
-            return true;
-        }
-
-        return false;
-    }
 }
 
 export const Keyboard = GObject.registerClass({

js/ui/layout.js
@@ -303,7 +303,7 @@ export const LayoutManager = GObject.registerClass({
             reactive: true,
             track_hover: true,
         });
-        this.addTopChrome(this.keyboardBox);
+        inputPanelGroup.add_child(this.keyboardBox);
         this._keyboardHeightNotifyId = 0;
 
         this.screenshotUIGroup = new St.Widget({

js/ui/lookingGlass.js
@@ -1563,13 +1563,6 @@ export class LookingGlass extends St.BoxLayout {
         this._resize();
     }
 
-    vfunc_captured_event(event) {
-        if (Main.keyboard.maybeHandleEvent(event))
-            return Clutter.EVENT_STOP;
-
-        return Clutter.EVENT_PROPAGATE;
-    }
-
     setBorderPaintTarget(obj) {
         if (this._borderPaintTarget != null)
             this._borderPaintTarget.remove_effect(this._redBorderEffect);

js/ui/modalDialog.js
@@ -117,13 +117,6 @@ export const ModalDialog = GObject.registerClass({
         return Clutter.EVENT_PROPAGATE;
     }
 
-    vfunc_captured_event(event) {
-        if (Main.keyboard.maybeHandleEvent(event))
-            return Clutter.EVENT_STOP;
-
-        return Clutter.EVENT_PROPAGATE;
-    }
-
     clearButtons() {
         this.dialogLayout.clearButtons();
     }

js/ui/unlockDialog.js
@@ -695,13 +695,6 @@ export const UnlockDialog = GObject.registerClass({
         return Clutter.EVENT_PROPAGATE;
     }
 
-    vfunc_captured_event(event) {
-        if (Main.keyboard.maybeHandleEvent(event))
-            return Clutter.EVENT_STOP;
-
-        return Clutter.EVENT_PROPAGATE;
-    }
-
     _createBackground(monitorIndex) {
         const monitor = Main.layoutManager.monitors[monitorIndex];
         const widget = new St.Widget({

commit 961a5d412c4b1333fc366673d979ed0b2e94e298

grabHelper: Don't try to disconnect non-existing handler
The handler this was trying to disconnect was removed in 5e5b0f49d.

Fixes: 5e5b0f49d ("keyboard: Set up OSK in input panel group")
Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4423>

js/ui/grabHelper.js
@@ -189,7 +189,6 @@ export class GrabHelper {
         if (this._modalCount > 0)
             return;
 
-        this._owner.disconnect(this._capturedEventId);
         this._owner.remove_action(this._clickGesture);
 
         Main.popModal(this._grab);