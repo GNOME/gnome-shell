39a0f9a9225902a1972c56a1a8ea809d3e3ddcdc
Sebastian Keller <skeller@gnome.org>
2026-09-24 01:58:05 +0200

    dialog: Don't unnecessarily use signal trackers
    The controller is owned by the actor itself and destroyed upon its
    destruction. There is no need to use a signal tracker here and this is
    not done for other controllers in the code base either.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/4446>

--- js/ui/dialog.js
+++ js/ui/dialog.js
@@ -27,33 +27,31 @@ class Dialog extends St.Widget {
         this._parentActor.add_child(this);
 
         const keyController = new Clutter.KeyController();
-        keyController.connectObject(
-            'key-press', () => {
-                const [, symbol] = keyController.get_key();
-                this._pressedKey = symbol;
+        keyController.connect('key-press', () => {
+            const [, symbol] = keyController.get_key();
+            this._pressedKey = symbol;
+            return Clutter.EVENT_PROPAGATE;
+        });
+        keyController.connect('key-release', () => {
+            const pressedKey = this._pressedKey;
+            this._pressedKey = null;
+
+            const [, symbol] = keyController.get_key();
+            if (symbol !== pressedKey)
                 return Clutter.EVENT_PROPAGATE;
-            },
-            'key-release', () => {
-                const pressedKey = this._pressedKey;
-                this._pressedKey = null;
 
-                const [, symbol] = keyController.get_key();
-                if (symbol !== pressedKey)
-                    return Clutter.EVENT_PROPAGATE;
-
-                const buttonInfo = this._buttonKeys[symbol];
-                if (!buttonInfo)
-                    return Clutter.EVENT_PROPAGATE;
-
-                const {button, action} = buttonInfo;
-
-                if (action && button.reactive) {
-                    action();
-                    return Clutter.EVENT_STOP;
-                }
+            const buttonInfo = this._buttonKeys[symbol];
+            if (!buttonInfo)
                 return Clutter.EVENT_PROPAGATE;
-            },
-            this);
+
+            const {button, action} = buttonInfo;
+
+            if (action && button.reactive) {
+                action();
+                return Clutter.EVENT_STOP;
+            }
+            return Clutter.EVENT_PROPAGATE;
+        });
         this.add_action(keyController);
     }
 
